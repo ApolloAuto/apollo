43c8ea4ce07d9a5ea3e855651a01247f6f7b01d8
Zhang Liangliang <zhangliangliang@baidu.com>
2017-07-05 13:45:46 -0700

    changed coding style in modules/drivers

--- modules/drivers/gnss/include/util/macros.h
+++ modules/drivers/gnss/include/util/macros.h
@@ -23,18 +23,16 @@
 #include <cstddef>
 
 // Put this in the declarations for a class to be uncopyable.
-#define DISABLE_COPY(TypeName) \
-    TypeName(const TypeName&) = delete
+#define DISABLE_COPY(TypeName) TypeName(const TypeName&) = delete
 
 // Put this in the declarations for a class to be unassignable.
-#define DISABLE_ASSIGN(TypeName) \
-    void operator=(const TypeName&) = delete
+#define DISABLE_ASSIGN(TypeName) void operator=(const TypeName&) = delete
 
 // A macro to disallow the copy constructor and operator= functions.
 // This should be used in the private: declarations for a class.
 #define DISABLE_COPY_AND_ASSIGN(TypeName) \
-    DISABLE_COPY(TypeName);               \
-    DISABLE_ASSIGN(TypeName)
+  DISABLE_COPY(TypeName);                 \
+  DISABLE_ASSIGN(TypeName)
 
 // A macro to disallow all the implicit constructors, namely the
 // default constructor, copy constructor and operator= functions.
@@ -43,24 +41,27 @@
 // that wants to prevent anyone from instantiating it. This is
 // especially useful for classes containing only static methods.
 #define DISABLE_IMPLICIT_CONSTRUCTORS(TypeName) \
-    TypeName() = delete;                        \
-    DISABLE_COPY_AND_ASSIGN(TypeName)
+  TypeName() = delete;                          \
+  DISABLE_COPY_AND_ASSIGN(TypeName)
 
 // Creates a thread-safe singleton.
-#define MAKE_SINGLETON(TypeName)   \
-public:                            \
-    static TypeName * instance() { \
-        static TypeName instance;  \
-        return &instance;          \
-    }                              \
-private:                           \
-    DISABLE_COPY_AND_ASSIGN(TypeName)
+#define MAKE_SINGLETON(TypeName) \
+ public:                         \
+  static TypeName* instance() {  \
+    static TypeName instance;    \
+    return &instance;            \
+  }                              \
+                                 \
+ private:                        \
+  DISABLE_COPY_AND_ASSIGN(TypeName)
 
 namespace apollo {
 
 // array_size(a) returns the number of elements in a.
 template <class T, size_t N>
-constexpr size_t array_size(T (&)[N]) { return N; }
+constexpr size_t array_size(T (&)[N]) {
+  return N;
+}
 
 }  // namespace apollo
 
