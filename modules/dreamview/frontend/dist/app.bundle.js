b9acc67fa5bb0309d28ebb4b9d00830761a24370
siyangy <omega115210@gmail.com>
2017-07-06 14:27:57 -0700

    Keep Dreamview rendering when the webpage is not in focus

--- modules/dreamview/frontend/dist/app.bundle.js
+++ modules/dreamview/frontend/dist/app.bundle.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3f18525f8fa5580feb677d788b5d616756b28e0650cf40badaab4ae25ea065e8
-size 815438
+oid sha256:b4353bdd4377127996fc93f763e71eacda430510c4d5ee7d398f953b18890ed3
+size 814709
