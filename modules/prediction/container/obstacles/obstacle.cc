739be967e457ef35d9a147c659da893ea57aeb73
Kecheng Xu <xukecheng@baidu.com>
2017-07-13 14:01:19 -0700

    add getters in obstacle class

--- modules/prediction/container/obstacles/obstacle.cc
+++ modules/prediction/container/obstacles/obstacle.cc
@@ -20,6 +20,7 @@ namespace apollo {
 namespace prediction {
 
 using apollo::perception::PerceptionObstacle;
+using apollo::common::math::KalmanFilter;
 
 std::mutex Obstacle::_mutex;
 
@@ -40,6 +41,56 @@ Obstacle::~Obstacle() {
   is_motion_tracker_enabled_ = false;
   kf_lane_tracker_map_.clear();
   // TODO(author) current_lanes_.clear();
+}
+
+int Obstacle::id() const {
+  std::lock_guard<std::mutex> lock(_mutex);
+  return id_;
+}
+
+double Obstacle::timestamp() const {
+  std::lock_guard<std::mutex> lock(_mutex);
+  if (feature_history_.size() > 0) {
+    return feature_history_.front().timestamp();
+  } else {
+    return 0.0;
+  }
+}
+
+const Feature& Obstacle::feature(size_t i) {
+  std::lock_guard<std::mutex> lock(_mutex);
+  CHECK(i < feature_history_.size());
+  return feature_history_[i];
+}
+
+Feature* Obstacle::mutable_feature(size_t i) {
+  std::lock_guard<std::mutex> lock(_mutex);
+  CHECK(i < feature_history_.size());
+  return &feature_history_[i];
+}
+
+const Feature& Obstacle::latest_feature() {
+  std::lock_guard<std::mutex> lock(_mutex);
+  CHECK(feature_history_.size() > 0);
+  return feature_history_.front();
+}
+
+Feature* Obstacle::mutable_latest_feature() {
+  std::lock_guard<std::mutex> lock(_mutex);
+
+  CHECK(feature_history_.size() > 0);
+  return &(feature_history_.front());
+}
+
+size_t Obstacle::history_size() const {
+  std::lock_guard<std::mutex> lock(_mutex);
+  return feature_history_.size();
+}
+
+const KalmanFilter<double, 4, 2, 0>& Obstacle::kf_lane_tracker(
+      const std::string& lane_id) {
+    CHECK(kf_lane_tracker_map_.find(lane_id) != kf_lane_tracker_map_.end());
+    return kf_lane_tracker_map_[lane_id];
 }
 
 void Obstacle::Insert(
