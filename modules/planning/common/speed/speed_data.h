4ea889bb0994f57e25eb8d49de0433e83d6027ca
Jiangtao Hu <ycool@users.noreply.github.com>
2017-07-18 23:09:07 -0700

    add speed data structure from houston. (#31)

---
+++ modules/planning/common/speed/speed_data.h
@@ -0,0 +1,58 @@
+/******************************************************************************
+ * Copyright 2017 The Apollo Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *****************************************************************************/
+
+/**
+ * @file speed_data.h
+ **/
+#ifndef MODULES_PLANNING_COMMON_SPEED_SPEED_DATA_H
+#define MODULES_PLANNING_COMMON_SPEED_SPEED_DATA_H
+
+#include <vector>
+
+#include "modules/planning/common/speed/speed_point.h"
+
+namespace apollo {
+namespace planning {
+
+class SpeedData {
+public:
+    SpeedData() = default;
+
+    SpeedData(std::vector<SpeedPoint> speed_points);
+
+    std::vector<SpeedPoint>* mutable_speed_vector();
+
+    const std::vector<SpeedPoint>& speed_vector() const;
+
+    void set_speed_vector(const std::vector<SpeedPoint>& speed_points);
+
+    virtual std::string DebugString() const;
+
+    bool get_speed_point_with_time(const double time, SpeedPoint* const speed_point) const;
+
+    double total_time() const;
+private:
+    std::size_t find_index(const double s) const;
+
+    std::vector<SpeedPoint> speed_vector_;
+};
+
+} // namespace planning
+} // namespace apollo
+
+#endif // MODULES_PLANNING_COMMON_SPEED_SPEED_DATA_H
+
+
