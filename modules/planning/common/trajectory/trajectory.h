54f33ee201647bb3e7de36cd53c0f726569d8f25
Zhang Liangliang <zhangliangliang@baidu.com>
2017-07-19 11:53:29 -0700

    added trajectory and fixed minor bug in planning/math/interpolation.h

---
+++ modules/planning/common/trajectory/trajectory.h
@@ -0,0 +1,49 @@
+/******************************************************************************
+ * Copyright 2017 The Apollo Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *****************************************************************************/
+
+/**
+ * @file trajectory.h
+ **/
+
+#ifndef MODULES_PLANNING_COMMON_TRAJECTORY_TRAJECTORY_H_
+#define MODULES_PLANNING_COMMON_TRAJECTORY_TRAJECTORY_H_
+
+#include <cstdlib>
+
+#include "modules/common/proto/path_point.pb.h"
+
+namespace adu {
+namespace planning {
+
+class Trajectory {
+ public:
+  Trajectory() = default;
+
+  virtual ~Trajectory() = default;
+
+  virtual double time_length() const = 0;
+
+  virtual TrajectoryPoint evaluate(const double relative_time) const = 0;
+
+  virtual TrajectoryPoint start_point() const = 0;
+
+  virtual TrajectoryPoint end_point() const = 0;
+};
+
+}  // namespace planning
+}  // namespace adu
+
+#endif  // MODULES_PLANNING_COMMON_TRAJECTORY_TRAJECTORY_H_
