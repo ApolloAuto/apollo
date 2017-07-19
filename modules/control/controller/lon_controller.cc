615f3df8534987113826e4174d68afc820ea426f
lianglia-apollo <zhangliangliang@baidu.com>
2017-07-19 13:56:25 -0700

    moved path_point into trajectory_point (#40)

--- modules/control/controller/lon_controller.cc
+++ modules/control/controller/lon_controller.cc
@@ -136,7 +136,7 @@ void LonController::LoadControlCalibrationTable(
   AINFO << "Control calibration table size is "
         << control_table.calibration_size();
   Interpolation2D::DataType xyz;
-  for (const auto& calibration : control_table.calibration()) {
+  for (const auto &calibration : control_table.calibration()) {
     xyz.push_back(std::make_tuple(calibration.speed(),
                                   calibration.acceleration(),
                                   calibration.command()));
@@ -329,12 +329,12 @@ void LonController::ComputeLongitudinalErrors(
   ADEBUG << "matched point:" << matched_point.DebugString();
   ADEBUG << "reference point:" << reference_point.DebugString();
   ADEBUG << "preview point:" << preview_point.DebugString();
-  debug->set_station_error(reference_point.s() - s_matched);
+  debug->set_station_error(reference_point.path_point().s() - s_matched);
   debug->set_speed_error(reference_point.v() - s_dot_matched);
 
-  debug->set_station_reference(reference_point.s());
+  debug->set_station_reference(reference_point.path_point().s());
   debug->set_speed_reference(reference_point.v());
-  debug->set_preview_station_error(preview_point.s() - s_matched);
+  debug->set_preview_station_error(preview_point.path_point().s() - s_matched);
   debug->set_preview_speed_error(preview_point.v() - s_dot_matched);
   debug->set_preview_speed_reference(preview_point.v());
   debug->set_preview_acceleration_reference(preview_point.a());
