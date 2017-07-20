57228ccf2de4392dfb0614f2838a59b39b597cef
Yifei Jiang <jiangyifei@gmail.com>
2017-07-20 15:14:04 -0700

    added dp path optimizer (#95)

---
+++ modules/planning/optimizer/dp_poly_path/graph_vertex.cpp
@@ -0,0 +1,143 @@
+/******************************************************************************
+ * Copyright 2017 The Apollo Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *****************************************************************************/
+
+/**
+ * @file sgraph_vertex.cpp
+ **/
+
+#include "modules/planning/optimizer/dp_poly_path/graph_vertex.h"
+
+#include <limits>
+#include <sstream>
+
+namespace apollo {
+namespace planning {
+
+GraphVertex::GraphVertex(const ::apollo::common::FrenetFramePoint &frame_point,
+                         const size_t index, const size_t level) :
+    _frame_point(frame_point),
+    _index(index),
+    _level(level),
+    _type(Type::REGULAR),
+    _accumulated_cost(std::numeric_limits<double>::max()) {
+}
+
+const ::apollo::common::FrenetFramePoint &GraphVertex::frame_point() const {
+  return _frame_point;
+}
+
+::apollo::common::FrenetFramePoint *GraphVertex::mutable_frame_point() {
+  return &_frame_point;
+}
+
+void GraphVertex::set_type(const Type type) {
+  _type = type;
+}
+
+void GraphVertex::set_accumulated_cost(const double accumulated_cost) {
+  _accumulated_cost = accumulated_cost;
+}
+
+void GraphVertex::set_index(const size_t index) {
+  _index = index;
+}
+
+void GraphVertex::set_level(const size_t level) {
+  _level = level;
+}
+
+bool GraphVertex::is_graph_head() const {
+  return _type == Type::GRAPH_HEAD;
+}
+
+bool GraphVertex::is_dead_end() const {
+  return _type == Type::DEAD_END;
+}
+
+double GraphVertex::accumulated_cost() const {
+  return _accumulated_cost;
+}
+
+size_t GraphVertex::index() const {
+  return _index;
+}
+
+size_t GraphVertex::level() const {
+  return _level;
+}
+
+void GraphVertex::add_out_vertex(const size_t vertex_index) {
+  _vertices_out.push_back(vertex_index);
+}
+
+void GraphVertex::add_out_vertex(const std::vector<size_t> &vertices) {
+  _vertices_out.insert(_vertices_out.end(), vertices.begin(), vertices.end());
+}
+
+void GraphVertex::add_in_vertex(const size_t vertex_index) {
+  _vertices_in.push_back(vertex_index);
+}
+
+void GraphVertex::add_in_vertex(const std::vector<size_t> &vertices) {
+  _vertices_in.insert(_vertices_in.end(), vertices.begin(), vertices.end());
+}
+
+void GraphVertex::add_out_edge(const size_t edge_index) {
+  _edges_out.push_back(edge_index);
+}
+
+void GraphVertex::add_out_edge(const std::vector<size_t> &edges) {
+  _edges_out.insert(_edges_out.end(), edges.begin(), edges.end());
+}
+
+void GraphVertex::add_in_edge(const size_t edge_index) {
+  _edges_in.push_back(edge_index);
+}
+
+void GraphVertex::add_in_edge(const std::vector<size_t> &edges) {
+  _edges_in.insert(_edges_in.end(), edges.begin(), edges.end());
+}
+
+const std::vector<size_t> &GraphVertex::vertices_out() const {
+  return _vertices_out;
+}
+
+const std::vector<size_t> &GraphVertex::vertices_in() const {
+  return _vertices_in;
+}
+
+const std::vector<size_t> &GraphVertex::edges_out() const {
+  return _edges_out;
+}
+
+const std::vector<size_t> &GraphVertex::edges_in() const {
+  return _edges_in;
+}
+
+std::string GraphVertex::to_string() const {
+  std::ostringstream sout;
+  sout << "index = " << _index << "; accumulated_cost = " << _accumulated_cost
+       << "s = " << _frame_point.s() << "; l = " << _frame_point.l()
+       << "; is_dead_end = " << std::boolalpha << is_dead_end() << std::endl;
+  for (const auto &v : _vertices_out) {
+    sout << "_vertices_out[" << v << "] ";
+  }
+  sout.flush();
+  return sout.str();
+}
+
+}  // planning
+}  // apollo
---
+++ modules/planning/optimizer/dp_poly_path/trajectory_cost.h
@@ -0,0 +1,60 @@
+/******************************************************************************
+ * Copyright 2017 The Apollo Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *****************************************************************************/
+
+/**
+ * @file trjactory_cost.h
+ **/
+
+#ifndef MODULES_PLANNING_OPTIMIZER_DP_POLY_PATH_TRAJECTORY_COST_H
+#define MODULES_PLANNING_OPTIMIZER_DP_POLY_PATH_TRAJECTORY_COST_H
+
+#include <vector>
+
+#include "modules/planning/common/decision_data.h"
+#include "modules/planning/common/speed/speed_data.h"
+#include "modules/planning/common/obstacle.h"
+#include "modules/common/math/box2d.h"
+#include "modules/planning/math/curve1d/quintic_polynomial_curve1d.h"
+#include "modules/planning/optimizer/dp_poly_path/dp_poly_path_config.h"
+#include "modules/planning/reference_line/reference_line.h"
+#include "dp_poly_path_config.h"
+
+namespace apollo {
+namespace planning {
+
+class TrajectoryCost {
+ public:
+  explicit TrajectoryCost(
+      const DpPolyPathConfig &config,
+      const SpeedData &heuristic_speed_data,
+      const DecisionData &decision_data);
+  double calculate(
+      const QuinticPolynomialCurve1d &curve,
+      const double start_s, const double end_s,
+      const double length, const double width,
+      const ReferenceLine &reference_line) const;
+ private:
+  DpPolyPathConfig _config;
+  SpeedData _heuristic_speed_data;
+  std::vector<std::vector<::apollo::common::math::Box2d>> _obstacle_trajectory;
+  std::vector<double> _obstacle_probability;
+  size_t _evaluate_times;
+};
+
+}  // planning
+}  // apollo
+
+#endif  // MODULES_PLANNING_OPTIMIZER_DP_POLY_PATH_TRAJECTORY_COST_H
