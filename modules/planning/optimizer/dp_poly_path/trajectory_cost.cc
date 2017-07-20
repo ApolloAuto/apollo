2a902f1d897980f4bf37cd90583d4919e9a19c66
Zhang Liangliang <zhangliangliang@baidu.com>
2017-07-20 16:16:24 -0700

    added st_graph_point

--- modules/planning/optimizer/dp_poly_path/trajectory_cost.cc
+++ modules/planning/optimizer/dp_poly_path/trajectory_cost.cc
@@ -22,33 +22,34 @@
 
 #include <cmath>
 
-#include "modules/planning/common/planning_gflags.h"
 #include "modules/common/math/vec2d.h"
 #include "modules/common/proto/path_point.pb.h"
+#include "modules/planning/common/planning_gflags.h"
 
 namespace apollo {
 namespace planning {
 
-TrajectoryCost::TrajectoryCost(
-    const DpPolyPathConfig &config,
-    const SpeedData &heuristic_speed_data,
-    const DecisionData &decision_data) :
-    _config(config),
-    _heuristic_speed_data(heuristic_speed_data) {
+TrajectoryCost::TrajectoryCost(const DpPolyPathConfig &config,
+                               const SpeedData &heuristic_speed_data,
+                               const DecisionData &decision_data)
+    : _config(config), _heuristic_speed_data(heuristic_speed_data) {
   const auto &static_obstacles = decision_data.StaticObstacles();
   const double total_time =
       std::min(_heuristic_speed_data.total_time(), FLAGS_prediction_total_time);
-  _evaluate_times = static_cast<size_t>(std::floor(total_time / config.eval_time_interval()));
+  _evaluate_times =
+      static_cast<size_t>(std::floor(total_time / config.eval_time_interval()));
 
   // Mapping Static obstacle
   for (size_t i = 0; i < static_obstacles.size(); ++i) {
     std::vector<::apollo::common::math::Box2d> obstacle_by_time;
     ::apollo::common::TrajectoryPoint traj_point =
         static_obstacles[i]->prediction_trajectories()[0].evaluate(0.0);
-    ::apollo::common::math::Vec2d center_point = {traj_point.path_point().x(), traj_point.path_point().y()};
-    ::apollo::common::math::Box2d obstacle_box = {center_point, traj_point.path_point().theta(),
-                                                  static_obstacles[i]->BoundingBox().length(),
-                                                  static_obstacles[i]->BoundingBox().width()};
+    ::apollo::common::math::Vec2d center_point = {traj_point.path_point().x(),
+                                                  traj_point.path_point().y()};
+    ::apollo::common::math::Box2d obstacle_box = {
+        center_point, traj_point.path_point().theta(),
+        static_obstacles[i]->BoundingBox().length(),
+        static_obstacles[i]->BoundingBox().width()};
     for (size_t j = 0; i <= _evaluate_times; ++j) {
       obstacle_by_time.push_back(obstacle_box);
     }
@@ -66,17 +67,19 @@ TrajectoryCost::TrajectoryCost(
       for (size_t time = 0; time <= _evaluate_times; ++time) {
         TrajectoryPoint traj_point =
             trajectory.evaluate(time * config.eval_time_interval());
-        ::apollo::common::math::Vec2d center_point = {traj_point.path_point().x(), traj_point.path_point().y()};
-        ::apollo::common::math::Box2d obstacle_box = {center_point, traj_point.path_point().theta(),
-                                                      static_obstacles[i]->BoundingBox().length(),
-                                                      static_obstacles[i]->BoundingBox().width()};
+        ::apollo::common::math::Vec2d center_point = {
+            traj_point.path_point().x(), traj_point.path_point().y()};
+        ::apollo::common::math::Box2d obstacle_box = {
+            center_point, traj_point.path_point().theta(),
+            static_obstacles[i]->BoundingBox().length(),
+            static_obstacles[i]->BoundingBox().width()};
         obstacle_by_time.push_back(obstacle_box);
       }
       _obstacle_trajectory.push_back(obstacle_by_time);
       _obstacle_probability.push_back(trajectory.probability());
     }
   }
-  //CHECK(_obstacle_probability.size() == _obstacle_trajectory.size());
+  // CHECK(_obstacle_probability.size() == _obstacle_trajectory.size());
 }
 
 double TrajectoryCost::calculate(const QuinticPolynomialCurve1d &curve,
@@ -87,23 +90,26 @@ double TrajectoryCost::calculate(const QuinticPolynomialCurve1d &curve,
   for (size_t i = 0; i < _evaluate_times; ++i) {
     double eval_time = i * _config.eval_time_interval();
     SpeedPoint speed_point;
-    if (!_heuristic_speed_data.get_speed_point_with_time(eval_time, &speed_point)
-        || start_s <= speed_point.s()) {
+    if (!_heuristic_speed_data.get_speed_point_with_time(eval_time,
+                                                         &speed_point) ||
+        start_s <= speed_point.st_point().s()) {
       continue;
     }
-    if (speed_point.s() >= end_s) {
+    if (speed_point.st_point().s() >= end_s) {
       break;
     }
-    double l = curve.evaluate(1, speed_point.s() - start_s);
-    total_cost += l; // need refine l cost;
+    double l = curve.evaluate(1, speed_point.st_point().s() - start_s);
+    total_cost += l;  // need refine l cost;
 
-    ::apollo::common::math::Vec2d car_point = {speed_point.s(), l};
-    ReferencePoint reference_point = reference_line.get_reference_point(car_point.x());
-    ::apollo::common::math::Box2d car_box = {car_point, reference_point.heading(), length, width};
+    ::apollo::common::math::Vec2d car_point = {speed_point.st_point().s(), l};
+    ReferencePoint reference_point =
+        reference_line.get_reference_point(car_point.x());
+    ::apollo::common::math::Box2d car_box = {
+        car_point, reference_point.heading(), length, width};
     for (size_t j = 0; j < _obstacle_trajectory.size(); ++j) {
       ::apollo::common::math::Box2d obstacle_box = _obstacle_trajectory[j][i];
       double distance = car_box.DistanceTo(obstacle_box);
-      total_cost += distance * _obstacle_probability[j]; // obstacle cost
+      total_cost += distance * _obstacle_probability[j];  // obstacle cost
     }
   }
   return total_cost;
