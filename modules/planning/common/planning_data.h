df3965e4087bb2bdbfab5d6ee1e53782dbbae3dd
Zhang Liangliang <zhangliangliang@baidu.com>
2017-07-20 14:26:13 -0700

    added planning_data.h/cc; TODO: fixed cyclic dependency in reference_line_decider.

--- modules/planning/common/planning_data.h
+++ modules/planning/common/planning_data.h
@@ -21,12 +21,39 @@
 #ifndef MODULES_PLANNING_COMMON_PLANNING_DATA_H_
 #define MODULES_PLANNING_COMMON_PLANNING_DATA_H_
 
+#include "modules/common/proto/path_point.pb.h"
+#include "modules/planning/common/decision_data.h"
+#include "modules/planning/common/trajectory/publishable_trajectory.h"
+#include "modules/planning/reference_line/reference_line.h"
+
 namespace apollo {
 namespace planning {
 
 class PlanningData {
  public:
   PlanningData() = default;
+
+  // copy reference line to planning data
+  void set_reference_line(std::unique_ptr<ReferenceLine>& reference_line);
+  void set_decision_data(std::unique_ptr<DecisionData>& decision_data);
+  void set_init_planning_point(const TrajectoryPoint& init_planning_point);
+  void set_computed_trajectory(PublishableTrajectory computed_trajectory);
+
+  const ReferenceLine& reference_line() const;
+  const DecisionData& decision_data() const;
+  const PublishableTrajectory& computed_trajectory() const;
+  const TrajectoryPoint& init_planning_point() const;
+
+  DecisionData* mutable_decision_data() const;
+  PublishableTrajectory* mutable_computed_trajectory();
+
+  virtual std::string type() const = 0;
+
+ protected:
+  std::unique_ptr<ReferenceLine> _reference_line;
+  std::unique_ptr<DecisionData> _decision_data;
+  PublishableTrajectory _computed_trajectory;
+  TrajectoryPoint _init_planning_point;
 };
 
 }  // namespace planning
