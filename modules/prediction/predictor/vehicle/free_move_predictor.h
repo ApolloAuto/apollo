3e04d5863c79fc053d919345f209042f50ad612c
Calvin Miao <jhmiao@gmail.com>
2017-07-21 09:59:20 -0700

    Added lane sequence and free move predictor framework (#127)
    * Fixed planning styling issues, and resolved a conflict
    
    Fixed planning styling issues
    
    Fixed planning styling issues
    
    Fixed planning styling issues
    
    Fixed some styling errors
    
    Resolved a conflict
    
    * Added mlp evaluator in evaluator factory
    
    * modified prediction conf in pb
    
    * Added free move predictor framework

--- modules/prediction/predictor/vehicle/free_move_predictor.h
+++ modules/prediction/predictor/vehicle/free_move_predictor.h
@@ -38,11 +38,14 @@ class FreeMovePredictor : public Predictor {
    * @brief Destructor
    */
   virtual ~FreeMovePredictor() = default;
+<<<<<<< HEAD
 
   /**
    * @brief Make prediction
    */
   void Predict() const override;
+=======
+>>>>>>> Added lane sequence and free move predictor framework (#127)
 };
 
 }  // prediction
