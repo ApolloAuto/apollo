86101fc35c8000c87916deeb9c744c375e8365a1
Calvin Miao <jhmiao@gmail.com>
2017-07-20 19:38:27 -0700

    Added lane sequence predictor framework (#112)
    * Fixed planning styling issues, and resolved a conflict
    
    Fixed planning styling issues
    
    Fixed planning styling issues
    
    Fixed planning styling issues
    
    Fixed some styling errors
    
    Resolved a conflict
    
    * Added mlp evaluator in evaluator factory
    
    * modified prediction conf in pb

---
+++ modules/prediction/predictor/vehicle/lane_sequence_predictor.h
@@ -0,0 +1,51 @@
+/******************************************************************************
+ * Copyright 2017 The Apollo Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *****************************************************************************/
+
+#ifndef MODULES_PREDICTION_PREDICTOR_VEHICLE_LANE_SEQUENCE_PREDICTOR_H_
+#define MODULES_PREDICTION_PREDICTOR_VEHICLE_LANE_SEQUENCE_PREDICTOR_H_
+
+#include "modules/prediction/predictor/predictor.h"
+#include "modules/prediction/proto/prediction_obstacle.pb.h"
+
+namespace apollo {
+namespace prediction {
+
+class LaneSequencePredictor : public Predictor {
+ public:
+  /**
+   * @brief Constructor
+   */
+  LaneSequencePredictor() = default;
+
+  /**
+   * @brief Destructor
+   */
+  virtual ~LaneSequencePredictor() = default;
+
+  /**
+   * @brief Get prediction obstacles
+   * @return Prediction obstacles
+   */
+  const PredictionObstacle& get_prediction_obstacle();
+
+ private:
+  PredictionObstacle prediction_obstacle_;
+};
+
+}  // prediction
+}  // apollo
+
+#endif  // MODULES_PREDICTION_PREDICTOR_VEHICLE_LANE_SEQUENCE_PREDICTOR_H_
