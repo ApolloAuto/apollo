c891fa9dd3e8876de63b634dc8569142c2011865
lifangzhen <lifangzhen@baidu.com>
2017-07-27 19:25:36 +0800

    remove the comment of hdmap

--- modules/perception/obstacle/onboard/hdmap_input.cc
+++ modules/perception/obstacle/onboard/hdmap_input.cc
@@ -16,6 +16,7 @@
 
 #include "modules/perception/obstacle/onboard/hdmap_input.h"
 #include <stdlib.h>
+#include <Eigen/Core>
 #include <algorithm>
 #include <vector>
 
@@ -26,19 +27,9 @@
 namespace apollo {
 namespace perception {
 
-bool HDMapInput::Init() {
-  inited_ = true;
-  return true;
-}
-
-bool HDMapInput::GetROI(const pcl_util::PointD& pointd, HdmapStructPtr mapptr) {
-  return true;
-}
-
-/*
 using apollo::hdmap::LineSegment;
 using apollo::hdmap::BoundaryEdge;
-using apollo::hdmap::RoadBoundaryPtr;
+using apollo::hdmap::RoadROIBoundaryPtr;
 using apollo::hdmap::JunctionInfoConstPtr;
 using apollo::hdmap::BoundaryEdge_Type_LEFT_BOUNDARY;
 using apollo::hdmap::BoundaryEdge_Type_RIGHT_BOUNDARY;
@@ -79,7 +70,7 @@ bool HDMapInput::GetROI(const PointD& pointd, HdmapStructPtr mapptr) {
   point.set_x(pointd.x);
   point.set_y(pointd.y);
   point.set_z(pointd.z);
-  std::vector<RoadBoundaryPtr> boundary_vec;
+  std::vector<RoadROIBoundaryPtr> boundary_vec;
   std::vector<JunctionInfoConstPtr> junctions_vec;
 
   int status = hdmap_->get_road_boundaries(point, FLAGS_map_radius,
@@ -99,7 +90,7 @@ bool HDMapInput::GetROI(const PointD& pointd, HdmapStructPtr mapptr) {
 }
 
 int HDMapInput::MergeBoundaryJunction(
-    std::vector<RoadBoundaryPtr>& boundaries,
+    std::vector<RoadROIBoundaryPtr>& boundaries,
     std::vector<JunctionInfoConstPtr>& junctions, HdmapStructPtr mapptr) {
   if (mapptr == nullptr) {
     AERROR << "the HdmapStructPtr mapptr is null";
@@ -183,8 +174,8 @@ void HDMapInput::DownSampleBoundary(const apollo::hdmap::LineSegment& line,
     const PointD& point_0 = raw_cloud->points[spt];
     const PointD& point_1 = raw_cloud->points[i - 1];
     const PointD& point_2 = raw_cloud->points[i];
-    common::math::Vec2d v1(point_1.x - point_0.x, point_1.y - point_0.y);
-    common::math::Vec2d v2(point_2.x - point_1.x, point_2.y - point_1.y);
+    Eigen::Vector2d v1(point_1.x - point_0.x, point_1.y - point_0.y);
+    Eigen::Vector2d v2(point_2.x - point_1.x, point_2.y - point_1.y);
     double vector_dist =
         sqrt(v1.cwiseProduct(v1).sum()) * sqrt(v2.cwiseProduct(v2).sum());
     // judge duplicate points
@@ -208,7 +199,6 @@ void HDMapInput::DownSampleBoundary(const apollo::hdmap::LineSegment& line,
   // the last point
   out_boundary_line->push_back(raw_cloud->points[raw_cloud_size - 1]);
 }
-*/
 
 }  // namespace perception
 }  // namespace apollo
