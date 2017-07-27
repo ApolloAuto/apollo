4d2e90c3492a72ebe01f5be062e794522b182d51
Aaron Xiao <xiaoxiangquan@gmail.com>
2017-07-27 09:36:16 -0700

    Populate trajectory points directly in planner.

--- modules/planning/planning_test.cc
+++ modules/planning/planning_test.cc
@@ -29,7 +29,6 @@
 namespace apollo {
 namespace planning {
 
-using TrajectoryPb = ADCTrajectory;
 using apollo::common::TrajectoryPoint;
 
 class PlanningTest : public ::testing::Test {
@@ -50,36 +49,39 @@ TEST_F(PlanningTest, ComputeTrajectory) {
   common::VehicleState::instance()->set_linear_velocity(0.15);
   common::VehicleState::instance()->set_angular_velocity(0.0);
 
-  std::vector<TrajectoryPoint> trajectory1;
+  ADCTrajectory trajectory1;
   double time1 = 0.1;
   planning.Init();
   planning.Plan(false, time1, &trajectory1);
 
-  EXPECT_EQ(trajectory1.size(), (std::uint32_t)FLAGS_rtk_trajectory_forward);
-  const auto& p1_start = trajectory1.front();
-  const auto& p1_end = trajectory1.back();
+  EXPECT_EQ(trajectory1.trajectory_point_size(), FLAGS_rtk_trajectory_forward);
+  const auto& p1_start = trajectory1.trajectory_point().begin();
+  const auto& p1_end = trajectory1.trajectory_point().rbegin();
 
-  EXPECT_DOUBLE_EQ(p1_start.path_point().x(), 586385.782841);
-  EXPECT_DOUBLE_EQ(p1_end.path_point().x(), 586355.063786);
+  EXPECT_DOUBLE_EQ(p1_start->path_point().x(), 586385.782841);
+  EXPECT_DOUBLE_EQ(p1_end->path_point().x(), 586355.063786);
 
-  std::vector<TrajectoryPoint> trajectory2;
+  ADCTrajectory trajectory2;
   double time2 = 0.5;
   planning.Plan(true, time2, &trajectory2);
 
-  EXPECT_EQ(trajectory2.size(), (std::uint32_t)FLAGS_rtk_trajectory_forward +
-                                    (int)FLAGS_rtk_trajectory_backward);
+  EXPECT_EQ(trajectory2.trajectory_point_size(),
+            FLAGS_rtk_trajectory_forward + FLAGS_rtk_trajectory_backward);
 
-  const auto& p2_backward = trajectory2.front();
-  const auto& p2_start = trajectory2[FLAGS_rtk_trajectory_backward];
-  const auto& p2_end = trajectory2.back();
+  const auto& p2_backward = trajectory2.trajectory_point().begin();
+  const auto& p2_start =
+      trajectory2.trajectory_point(FLAGS_rtk_trajectory_backward);
+  const auto& p2_end = trajectory2.trajectory_point().rbegin();
 
-  EXPECT_DOUBLE_EQ(p2_backward.path_point().x(), 586385.577255);
+  EXPECT_DOUBLE_EQ(p2_backward->path_point().x(), 586385.577255);
   EXPECT_DOUBLE_EQ(p2_start.path_point().x(), 586385.486723);
-  EXPECT_DOUBLE_EQ(p2_end.path_point().x(), 586353.262913);
+  EXPECT_DOUBLE_EQ(p2_end->path_point().x(), 586353.262913);
 
-  double absolute_time1 = trajectory1[100].relative_time() + time1;
+  double absolute_time1 =
+      trajectory1.trajectory_point(100).relative_time() + time1;
   double absolute_time2 =
-      trajectory2[60 + FLAGS_rtk_trajectory_backward].relative_time() + time2;
+      trajectory2.trajectory_point(60 + FLAGS_rtk_trajectory_backward)
+          .relative_time() + time2;
 
   EXPECT_NEAR(absolute_time1, absolute_time2, 0.001);
 }
@@ -97,7 +99,7 @@ TEST_F(PlanningTest, ComputeTrajectoryNoRTKFile) {
   common::VehicleState::instance()->set_angular_velocity(0.0);
 
   double time = 0.1;
-  std::vector<TrajectoryPoint> trajectory;
+  ADCTrajectory trajectory;
   bool res_planning = planning.Plan(false, time, &trajectory);
   EXPECT_FALSE(res_planning);
 
