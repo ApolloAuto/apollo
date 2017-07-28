26b2be52142d892da2db1a0e1e269101a4328035
zhuweicheng <zhuweicheng@baidu.com>
2017-07-28 14:35:55 -0700

    fix bug in dp poly path

--- modules/planning/optimizer/dp_poly_path/dp_road_graph.cc
+++ modules/planning/optimizer/dp_poly_path/dp_road_graph.cc
@@ -46,7 +46,7 @@ using apollo::common::Status;
 DpRoadGraph::DpRoadGraph(const DpPolyPathConfig &config,
                          const ::apollo::common::TrajectoryPoint &init_point,
                          const SpeedData &speed_data)
-    : _config(config), _init_point(init_point), _speed_data(speed_data) {}
+    : config_(config), init_point_(init_point), speed_data_(speed_data) {}
 
 bool DpRoadGraph::find_tunnel(const ReferenceLine &reference_line,
                               DecisionData *const decision_data,
@@ -63,8 +63,8 @@ bool DpRoadGraph::find_tunnel(const ReferenceLine &reference_line,
     return false;
   }
   std::vector<common::FrenetFramePoint> frenet_path;
-  double accumulated_s = _init_point.path_point().s();
-  const double path_resolution = _config.path_resolution();
+  double accumulated_s = init_sl_point_.s();
+  const double path_resolution = config_.path_resolution();
   for (size_t i = 1; i < min_cost_path.size(); ++i) {
     const auto &prev_node = min_cost_path[i - 1];
     const auto &cur_node = min_cost_path[i];
@@ -130,28 +130,28 @@ bool DpRoadGraph::find_tunnel(const ReferenceLine &reference_line,
 }
 
 bool DpRoadGraph::init(const ReferenceLine &reference_line) {
-  common::math::Vec2d xy_point(_init_point.path_point().x(),
-                               _init_point.path_point().y());
+  common::math::Vec2d xy_point(init_point_.path_point().x(),
+                               init_point_.path_point().y());
 
-  if (!reference_line.get_point_in_frenet_frame(xy_point, &_init_sl_point)) {
+  if (!reference_line.get_point_in_frenet_frame(xy_point, &init_sl_point_)) {
     AERROR << "Fail to map init point to sl coordinate!";
     return false;
   }
 
   ReferencePoint reference_point =
-      reference_line.get_reference_point(_init_sl_point.s());
+      reference_line.get_reference_point(init_sl_point_.s());
 
   double init_dl = SLAnalyticTransformation::calculate_lateral_derivative(
-      reference_point.heading(), _init_point.path_point().theta(),
-      _init_sl_point.l(), reference_point.kappa());
+      reference_point.heading(), init_point_.path_point().theta(),
+      init_sl_point_.l(), reference_point.kappa());
   double init_ddl =
       SLAnalyticTransformation::calculate_second_order_lateral_derivative(
-          reference_point.heading(), _init_point.path_point().theta(),
-          reference_point.kappa(), _init_point.path_point().kappa(),
-          reference_point.dkappa(), _init_sl_point.l());
+          reference_point.heading(), init_point_.path_point().theta(),
+          reference_point.kappa(), init_point_.path_point().kappa(),
+          reference_point.dkappa(), init_sl_point_.l());
   common::FrenetFramePoint init_frenet_frame_point;
-  init_frenet_frame_point.set_s(_init_sl_point.s());
-  init_frenet_frame_point.set_l(_init_sl_point.l());
+  init_frenet_frame_point.set_s(init_sl_point_.s());
+  init_frenet_frame_point.set_l(init_sl_point_.l());
   init_frenet_frame_point.set_dl(init_dl);
   init_frenet_frame_point.set_ddl(init_ddl);
 
@@ -166,20 +166,20 @@ bool DpRoadGraph::generate_graph(const ReferenceLine &reference_line,
     return false;
   }
   std::vector<std::vector<common::SLPoint>> points;
-  PathSampler path_sampler(_config);
-  if (!path_sampler.sample(reference_line, _init_point, _init_sl_point,
+  PathSampler path_sampler(config_);
+  if (!path_sampler.sample(reference_line, init_point_, init_sl_point_,
                            &points)) {
     AERROR << "Fail to sampling point with path sampler!";
     return false;
   }
-  points.insert(points.begin(), std::vector<common::SLPoint>{_init_sl_point});
-  const auto &vehicle_config =
+  points.insert(points.begin(), std::vector<common::SLPoint>{init_sl_point_});
+  const auto &vehicleconfig_ =
       common::VehicleConfigHelper::instance()->GetConfig();
-  TrajectoryCost trajectory_cost(_config, reference_line,
-                                 vehicle_config.vehicle_param(), _speed_data,
+  TrajectoryCost trajectory_cost(config_, reference_line,
+                                 vehicleconfig_.vehicle_param(), speed_data_,
                                  *decision_data);
   std::vector<std::vector<DpNode>> dp_nodes(points.size());
-  dp_nodes[0].push_back(DpNode(_init_sl_point, nullptr, 0.0));
+  dp_nodes[0].push_back(DpNode(init_sl_point_, nullptr, 0.0));
   const double zero_dl = 0.0;   // always use 0 dl
   const double zero_ddl = 0.0;  // always use 0 ddl
   for (std::size_t level = 1; level < points.size(); ++level) {
@@ -217,6 +217,10 @@ bool DpRoadGraph::generate_graph(const ReferenceLine &reference_line,
     min_cost_path->push_back(*min_cost_node);
   }
   std::reverse(min_cost_path->begin(), min_cost_path->end());
+  for (const auto &node : *min_cost_path) {
+    AINFO << "Best path : (" << node.sl_point.s() << ", "
+          << node.sl_point.l() << ")";
+  }
   return true;
 }
 
