1d227eec437453068f0c4005d0cd22a60ec0424e
siyangy <yusiyang@baidu.com>
2017-07-27 19:15:20 -0700

    Add prediction to dreamview (frontend)

--- modules/dreamview/frontend/src/store/options.js
+++ modules/dreamview/frontend/src/store/options.js
@@ -5,8 +5,12 @@ import PARAMETERS from "store/config/parameters.yml";
 export default class Options {
     @observable showConsole = PARAMETERS.options.defaults.showConsole;
     @observable showPlanning = PARAMETERS.options.defaults.showPlanning;
+
+    // TODO: Consider having a per-type config about whether to show obstacles
     @observable showObstacles = PARAMETERS.options.defaults.showObstacles;
     @observable showDecision = PARAMETERS.options.defaults.showDecision;
+    @observable showPredictionMajor = PARAMETERS.options.defaults.showPredictionMajor;
+    @observable showPredictionMinor = PARAMETERS.options.defaults.showPredictionMinor;
 
     @action toggleShowConsole() {
         this.showConsole = !this.showConsole;
