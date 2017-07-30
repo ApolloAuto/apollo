19638043220ae15d1497319bb88bd189f2f39fb9
Qi Luo <qi.stephen.luo@gmail.com>
2017-07-29 18:48:27 -0700

    Update lat gain scheduler and test

--- modules/control/common/interpolation_1d_test.cc
+++ modules/control/common/interpolation_1d_test.cc
@@ -2,11 +2,10 @@
 
 #include <string>
 #include <utility>
-#include "gmock/gmock.h"
-#include "gtest/gtest.h"
-#include "modules/common/log.h"
 
-#include "modules/common/util/util.h"
+#include "gmock/gmock.h"
+#include "modules/common/log.h"
+#include "modules/common/util/file.h"
 #include "modules/control/proto/control_conf.pb.h"
 
 using ::testing::ElementsAre;
@@ -17,9 +16,9 @@ namespace control {
 class Interpolation1DTest : public ::testing::Test {
  public:
   virtual void SetUp() {
-    std::string control_conf_file = "modules/control/testdata/lincoln.pb.txt";
+    std::string control_conf_file = "modules/control/testdata/conf/lincoln.pb.txt";
     CHECK(
-        ::apollo::util::load_file_to_proto(control_conf_file, &control_conf_));
+        ::apollo::common::util::GetProtoFromFile(control_conf_file, &control_conf_));
   }
 
  protected:
@@ -48,25 +47,6 @@ TEST_F(Interpolation1DTest, normal) {
 TEST_F(Interpolation1DTest, unordered) {
   Interpolation1D::DataType xy{{15, 12}, {5, 5}, {40, 25}, {30, 17}};
 
-  Interpolation1D estimator;
-  EXPECT_TRUE(estimator.Init(xy));
-
-  for (unsigned i = 0; i < xy.size(); i++) {
-    EXPECT_DOUBLE_EQ(xy[i].second, estimator.Interpolate(xy[i].first));
-  }
-}
-
-TEST_F(Interpolation1DTest, brake_table) {
-  const auto& brake_table =
-      control_conf_.simple_longitudinal_conf().brake_table();
-  AINFO << "Brake calibration table:" << brake_table.DebugString();
-
-  Interpolation1D::DataType xy;
-
-  for (int i = 0; i < brake_table.calibration_size(); ++i) {
-    xy.push_back(std::make_pair(brake_table.calibration(i).acceleration(),
-                                brake_table.calibration(i).brake()));
-  }
   Interpolation1D estimator;
   EXPECT_TRUE(estimator.Init(xy));
 
