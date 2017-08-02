37767776a6ea8224fd76c631be8fd8bf35ef692d
fanzhu1985 <30510791+fanzhu1985@users.noreply.github.com>
2017-08-02 12:46:25 +0800

    Routing: create BUILD for strategy (#327)
    * Resolve conflict
    
    * add bazel for strategy

--- modules/routing/strategy/a_star_strategy.cc
+++ modules/routing/strategy/a_star_strategy.cc
@@ -80,8 +80,8 @@ void AStarStrategy::clear() {
 
 double AStarStrategy::heuristic_cost(const TopoNode* src_node,
                                      const TopoNode* dest_node) {
-  const ::apollo::hdmap::Point& src_point = src_node->anchor_point();
-  const ::apollo::hdmap::Point& dest_point = dest_node->anchor_point();
+  const ::apollo::common::PointENU& src_point = src_node->anchor_point();
+  const ::apollo::common::PointENU& dest_point = dest_node->anchor_point();
   double distance = sqrt(pow(src_point.x() - dest_point.x(), 2) +
                          pow(src_point.y() - dest_point.y(), 2) +
                          pow(src_point.z() - dest_point.z(), 2));
