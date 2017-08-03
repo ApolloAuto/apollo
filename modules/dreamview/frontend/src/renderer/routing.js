d8bc779a829cd7004598975cf8464a6444af1449
siyangy <yu.siyang@yahoo.com>
2017-08-03 14:55:40 -0700

    Correct routing update logic (#416)

--- modules/dreamview/frontend/src/renderer/routing.js
+++ modules/dreamview/frontend/src/renderer/routing.js
@@ -9,23 +9,28 @@ const _ = require('lodash');
 export default class Routing {
     constructor() {
         this.routePath = null;
+        this.lastRoutingTime = -1;
     }
 
     update(world, coordinates, scene) {
         if (this.routePath) {
             this.routePath.visible = STORE.options.showRouting;
         }
-        // Route will only be sent once if it remains unchanged.
-        if (_.isEmpty(world.route)) {
+        // There has not been a new routing published since last time.
+        if (this.lastRoutingTime === world.routingTime) {
             return;
         }
+
+        this.lastRoutingTime = world.routingTime;
 
         if (this.routePath) {
             scene.remove(this.routePath);
             this.routePath.material.dispose();
             this.routePath.geometry.dispose();
         }
-        const points = coordinates.applyOffsetToArray(world.route);
+
+        const points = world.route === undefined ? []
+            : coordinates.applyOffsetToArray(world.route);
         this.routePath = drawSegmentsFromPoints(
             points, 0xFF0000 /* red */, 10, 5, true, true, 0.6);
         this.routePath.visible = STORE.options.showRouting;
