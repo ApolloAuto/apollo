87d6f77f712a4fce8c2cc125d71e9b1f95de1b83
FangzhenLi-hust <30279558+FangzhenLi-hust@users.noreply.github.com>
2017-08-03 10:28:04 -0700

    add timer for perception,and modify hdmap_roi_config file path (#385)
    * add timer for perception
    
    * merge conflict

---
+++ modules/perception/lib/base/timer.cc
@@ -0,0 +1,46 @@
+/******************************************************************************
+ * Copyright 2017 The Apollo Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *****************************************************************************/
+
+#include "modules/perception/lib/base/timer.h"
+#include <sys/time.h>
+
+#include "modules/common/log.h"
+
+namespace apollo {
+namespace perception {
+
+using std::string;
+using std::chrono::duration_cast;
+using std::chrono::milliseconds;
+
+void Timer::start() {
+  _start_time = std::chrono::system_clock::now();
+}
+
+uint64_t Timer::end(const string& msg) {
+  _end_time = std::chrono::system_clock::now();
+  uint64_t elapsed_time =
+      duration_cast<milliseconds>(_end_time - _start_time).count();
+
+  AINFO << "TIMER " << msg << " elapsed_time: " << elapsed_time << " ms";
+
+  // start new timer.
+  _start_time = _end_time;
+  return elapsed_time;
+}
+
+}  // namespace perception
+}  // namespace apollo
