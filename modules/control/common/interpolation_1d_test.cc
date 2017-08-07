af07e063ec7051d1a390a9a41be42dd692e137a7
Capri2014 <qi.stephen.luo@gmail.com>
2017-08-07 16:32:28 -0700

    [control] cherry-pick and update add interpolation_1d (#582)
    * Add gain scheduler gflag
    
    * Add interpolation_1d
    
    * [control] cherry-pick and update add interpolation_1d

---
+++ modules/control/common/interpolation_1d_test.cc
@@ -0,0 +1,59 @@
+#include "modules/control/common/interpolation_1d.h"
+
+#include <string>
+#include <utility>
+
+#include "gmock/gmock.h"
+#include "modules/common/log.h"
+#include "modules/common/util/file.h"
+#include "modules/control/proto/control_conf.pb.h"
+
+using ::testing::ElementsAre;
+
+namespace apollo {
+namespace control {
+
+class Interpolation1DTest : public ::testing::Test {
+ public:
+  virtual void SetUp() {
+    std::string control_conf_file = "modules/control/testdata/conf/lincoln.pb.txt";
+    CHECK(
+        ::apollo::common::util::GetProtoFromFile(control_conf_file, &control_conf_));
+  }
+
+ protected:
+  ControlConf control_conf_;
+};
+
+TEST_F(Interpolation1DTest, normal) {
+  Interpolation1D::DataType xy{{0, 0}, {15, 12}, {30, 17}};
+
+  Interpolation1D estimator;
+  EXPECT_TRUE(estimator.Init(xy));
+
+  for (unsigned i = 0; i < xy.size(); i++) {
+    EXPECT_DOUBLE_EQ(xy[i].second, estimator.Interpolate(xy[i].first));
+  }
+
+  EXPECT_DOUBLE_EQ(4.7777777777777777, estimator.Interpolate(5));
+  EXPECT_DOUBLE_EQ(8.7777777777777786, estimator.Interpolate(10));
+  EXPECT_DOUBLE_EQ(14.444444444444445, estimator.Interpolate(20));
+
+  // out of x range
+  EXPECT_DOUBLE_EQ(0, estimator.Interpolate(-1));
+  EXPECT_DOUBLE_EQ(17, estimator.Interpolate(30));
+}
+
+TEST_F(Interpolation1DTest, unordered) {
+  Interpolation1D::DataType xy{{15, 12}, {5, 5}, {40, 25}, {30, 17}};
+
+  Interpolation1D estimator;
+  EXPECT_TRUE(estimator.Init(xy));
+
+  for (unsigned i = 0; i < xy.size(); i++) {
+    EXPECT_DOUBLE_EQ(xy[i].second, estimator.Interpolate(xy[i].first));
+  }
+}
+
+}  // namespace control
+}  // namespace apollo
