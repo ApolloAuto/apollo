4269216dfeb60346e4127841f11a0d618c5c21da
Capri2014 <qi.stephen.luo@gmail.com>
2017-08-07 17:56:03 -0700

    [Control]  … Replace vehicle related parameter in lateral controller conf with one in modules/common/configs (#593)
    * [control] cherry-pick and update add interpolation_1d
    
    * [Control] Cherry-pick and update gain scheduler test
    
    * [Control] Cherry-pick add heading direction change in analytical match function
    
    * Add reverse gear in state analytic matching
    
    * Retire state exact match gflag
    
    * [Control]
    Replace vehicle related parameter in lateral controller conf with one in modules/common/configs

--- modules/control/controller/lat_controller.h
+++ modules/control/controller/lat_controller.h
@@ -34,6 +34,7 @@
 #include "modules/control/filters/digital_filter.h"
 #include "modules/control/filters/digital_filter_coefficients.h"
 #include "modules/control/filters/mean_filter.h"
+#include "modules/common/configs/proto/vehicle_config.pb.h"
 
 /**
  * @namespace apollo::control
@@ -101,7 +102,7 @@ class LatController : public Controller {
 
  protected:
   void UpdateState(SimpleLateralDebug *debug);
-  
+
   void UpdateStateAnalyticalMatching(SimpleLateralDebug *debug);
 
   void UpdateMatrix();
@@ -126,6 +127,9 @@ class LatController : public Controller {
                    const canbus::Chassis *chassis);
 
   void CloseLogFile();
+
+  // vehicle parameter
+  ::apollo::common::VehicleParam vehicle_param_;
 
   // a proxy to analyze the planning trajectory
   TrajectoryAnalyzer trajectory_analyzer_;
