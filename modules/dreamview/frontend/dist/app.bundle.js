abdbdd8f971a833d43fb503fc7ce11f24c211924
Yu Cao <caoyu05@baidu.com>
2017-08-08 15:04:23 -0700

    update decision fence types

--- modules/dreamview/frontend/dist/app.bundle.js
+++ modules/dreamview/frontend/dist/app.bundle.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:16c6f377bbbc06cb5a40cba1d813a0c5c34ca0e8d2ae0dd129cbba15dbd41771
-size 920166
+oid sha256:1b703f3ac729319b8d7a94f771f4b5364a2343be8157305d3da2874b0f02f940
+size 920823
