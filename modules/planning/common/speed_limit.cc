eb36c09112587fe5429491edc1b25298bd1220d5
Zhang Liangliang <zhangliangliang@baidu.com>
2017-08-09 12:08:36 -0700

    Planning: revert changes of speed_limit because t info is needed. fixed some bugs in qp_spline_st_graph.

--- modules/planning/common/speed_limit.cc
+++ modules/planning/common/speed_limit.cc
@@ -29,42 +29,85 @@
 namespace apollo {
 namespace planning {
 
-void SpeedLimit::AppendSpeedLimitInfo(std::pair<double, double> speed_limit_info) {
-  if (!speed_limit_info_.empty()) {
-    CHECK(speed_limit_info_.back().first < speed_limit_info.first);
-  }
-  speed_limit_info_.push_back(std::move(speed_limit_info));
+using common::SpeedPoint;
+
+void SpeedLimit::AddSpeedLimit(const SpeedPoint& speed_point) {
+  speed_points_.push_back(speed_point);
 }
 
-const std::vector<std::pair<double, double>>& SpeedLimit::speed_limit_info() const {
-  return speed_limit_info_;
+const std::vector<SpeedPoint>& SpeedLimit::speed_points() const {
+  return speed_points_;
 }
 
-double SpeedLimit::GetSpeedLimit(const double s) const {
-  if (speed_limit_info_.size() == 0) {
+double SpeedLimit::GetSpeedLimitByS(const double s) const {
+  if (speed_points_.size() == 0) {
+    AWARN << "Speed points is empty.";
     return 0.0;
   }
 
-  if (speed_limit_info_.size() == 1) {
-    return speed_limit_info_.front().second;
+  if (speed_points_.size() == 1) {
+    return speed_points_.front().v();
   }
 
-  auto func = [](const std::pair<double, double>& sp, const double s)
-      { return sp.first < s; };
+  if (s > speed_points_.back().s()) {
+    AWARN << "s is larger than the last point of Speed points.";
+    return speed_points_.back().v();
+  }
+
+  if (s < speed_points_.front().s()) {
+    return speed_points_.front().v();
+  }
+
+  auto func = [](const SpeedPoint& sp, const double s) { return sp.s() < s; };
 
   auto it_lower =
-      std::lower_bound(speed_limit_info_.begin(), speed_limit_info_.end(), s, func);
-  if (it_lower == speed_limit_info_.begin()) {
-    return speed_limit_info_.front().second;
-  } else if (it_lower == speed_limit_info_.end()) {
-    return speed_limit_info_.back().second;
+      std::lower_bound(speed_points_.begin(), speed_points_.end(), s, func);
+  if (it_lower == speed_points_.begin()) {
+    return speed_points_.front().v();
   }
 
-  double s0 = (it_lower - 1)->first;
-  double v0 = (it_lower - 1)->second;
-  double s1 = it_lower->first;
-  double v1 = it_lower->second;
-  return common::math::lerp(v0, s0, v1, s1, s);
+  double weight = 0.0;
+  double range = (*it_lower).s() - (*(it_lower - 1)).s();
+  if (range > 0) {
+    weight = (s - (*(it_lower - 1)).s()) / range;
+  }
+  return util::interpolate(*(it_lower - 1), *it_lower, weight).v();
+}
+
+double SpeedLimit::GetSpeedLimitByT(const double t) const {
+  if (speed_points_.size() == 0) {
+    return 0.0;
+  }
+
+  if (speed_points_.size() == 1) {
+    return speed_points_.front().v();
+  }
+
+  if (t > speed_points_.back().t()) {
+    return speed_points_.back().v();
+  }
+
+  if (t < speed_points_.front().t()) {
+    return speed_points_.front().v();
+  }
+
+  auto func = [](const SpeedPoint& sp, const double t) { return sp.t() < t; };
+
+  auto it_lower =
+      std::lower_bound(speed_points_.begin(), speed_points_.end(), t, func);
+  if (it_lower == speed_points_.begin()) {
+    return speed_points_.front().v();
+  } else if (it_lower == speed_points_.end()) {
+    return speed_points_.back().v();
+  }
+
+  double v0 = (it_lower - 1)->v();
+  double t0 = (it_lower - 1)->t();
+
+  double v1 = it_lower->v();
+  double t1 = it_lower->t();
+
+  return common::math::lerp(v0, t0, v1, t1, t);
 }
 
 }  // namespace planning
