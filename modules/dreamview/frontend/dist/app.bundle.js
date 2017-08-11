674f744bfa3c796cd6b156c495a102d6590c2c3a
Yu Cao <caoyu05@baidu.com>
2017-08-11 14:16:42 -0700

    support small device resolution

--- modules/dreamview/frontend/dist/app.bundle.js
+++ modules/dreamview/frontend/dist/app.bundle.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5260be3343e1a43e31925fe9d5bd6527b68f68b7041649bdecad5ad51d200e09
-size 920178
+oid sha256:ce765cac1ae8dad23407808bcbc7495735d31a15f430f1258a13be0e429a03fc
+size 920813
