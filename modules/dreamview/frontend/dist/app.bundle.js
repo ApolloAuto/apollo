2cc81166e54b9fba0d63a49a10b50c151d623212
Yu Cao <caoyu05@baidu.com>
2017-08-15 14:30:13 -0700

    display obstacle id in Dreamview

--- modules/dreamview/frontend/dist/app.bundle.js
+++ modules/dreamview/frontend/dist/app.bundle.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5cf560b30c449318cc8e4a92a981d780ae65f777e13fc3b93357f4003c761c69
-size 921350
+oid sha256:de4ebe9fe30016ac1282c2f5cae65ffb93111985705b0af07fe7c3420145932b
+size 923927
