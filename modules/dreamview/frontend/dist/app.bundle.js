0ac2e67ff3a364d33ef982c1961355c355dea32c
unacao <yufcao@gmail.com>
2017-08-16 11:32:16 -0700

    temp. fix for updated map lane boundary (#861)

--- modules/dreamview/frontend/dist/app.bundle.js
+++ modules/dreamview/frontend/dist/app.bundle.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:de4ebe9fe30016ac1282c2f5cae65ffb93111985705b0af07fe7c3420145932b
-size 923927
+oid sha256:e2c37ef6a8d79e6f59db3f8074c9eb3671296378eecfaa6bc224fb217843fd23
+size 923967
