4f6ce2d386e77dd65f1c9e97b7803fee65b626fe
unacao <yufcao@gmail.com>
2017-08-18 13:18:58 -0700

    enforce that transparent image is on top (#958)

--- modules/dreamview/frontend/dist/app.bundle.js
+++ modules/dreamview/frontend/dist/app.bundle.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b75c25d89997b2cdfa0f77cacff6ccc441a417910982f366b51f18b3b38a4dc4
-size 924481
+oid sha256:5f20e0494bdbeaeff59219f47be9f539aaed8bf18c3d2fa250ab3fecc1acd0da
+size 924495
