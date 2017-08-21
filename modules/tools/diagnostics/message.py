3c3a8b3a605ce6d7ad4fe8bd7c445559b89fd21e
Aaron Xiao <xiaoxiangquan@gmail.com>
2017-08-21 13:00:14 -0700

    Tools: Simple refactor on diagnostics.

--- modules/tools/diagnostics/message.py
+++ modules/tools/diagnostics/message.py
@@ -22,7 +22,23 @@ import curses
 
 import rospy
 
-import batch_include
+from modules.canbus.proto import chassis_detail_pb2
+from modules.canbus.proto import chassis_pb2
+from modules.common.configs.proto import vehicle_config_pb2
+from modules.common.monitor.proto import monitor_pb2
+from modules.common.proto import geometry_pb2
+from modules.common.proto import header_pb2
+from modules.control.proto import control_cmd_pb2
+from modules.control.proto import pad_msg_pb2
+from modules.localization.proto import gps_pb2
+from modules.localization.proto import imu_pb2
+from modules.localization.proto import localization_pb2
+from modules.perception.proto import perception_obstacle_pb2
+from modules.perception.proto import traffic_light_detection_pb2
+from modules.planning.proto import planning_internal_pb2
+from modules.planning.proto import planning_pb2
+from modules.prediction.proto import prediction_obstacle_pb2
+from modules.routing.proto import routing_pb2
 
 Refreshrate = 16
 
@@ -36,8 +52,8 @@ class Message(object):
         self.name = name
         self.topic = topic
         self.lock = lock
-        self.proto_name = 'batch_include.' + proto_name[:-2]
-        self.proto = eval('batch_include.' + proto_name)
+        self.proto_name = proto_name[:-2]
+        self.proto = eval(proto_name)
         self.period = float(period)
 
         self.msg_received = False
@@ -51,6 +67,11 @@ class Message(object):
         self.field = Field(self.proto, window, self.proto.DESCRIPTOR)
 
         self.display_time = rospy.get_time()
+
+    def subscribe(self):
+        """Subscribe the topic in ROS."""
+        return rospy.Subscriber(
+            self.topic, eval(self.proto_name), self.callback, queue_size=10)
 
     def callback(self, data):
         """
