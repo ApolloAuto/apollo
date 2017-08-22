70687fad93f75a16995c9414d2c7365218a6d51e
Vivian Lin <linvivian@baidu.com>
2017-08-22 16:01:06 -0700

    dreamview: implemented orbit control in Map view (#1055)

--- modules/dreamview/frontend/dist/app.bundle.js
+++ modules/dreamview/frontend/dist/app.bundle.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cd940cd2dac815ecafa8e9fc4c2171483994a41623245f12ae044fa92a245d01
-size 924962
+oid sha256:eb27e45013c73d9d9fc0a1d53223705a15170399a036d32adf18f414f3f0b9f6
+size 936031
