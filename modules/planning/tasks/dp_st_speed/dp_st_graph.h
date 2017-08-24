f20d485e861881cd3b5a6f8ff9b201ebbcd4416c
Dong Li <lidong.main@gmail.com>
2017-08-24 16:10:49 -0700

    planning: move st_graph_point to dp_st_speed
    * st_graph_point is actually an interal helper class for the dp algorithm

--- modules/planning/tasks/dp_st_speed/dp_st_graph.h
+++ modules/planning/tasks/dp_st_speed/dp_st_graph.h
@@ -33,8 +33,8 @@
 #include "modules/planning/common/speed/speed_data.h"
 #include "modules/planning/common/speed/st_point.h"
 #include "modules/planning/tasks/dp_st_speed/dp_st_cost.h"
+#include "modules/planning/tasks/dp_st_speed/st_graph_point.h"
 #include "modules/planning/tasks/st_graph/st_graph_data.h"
-#include "modules/planning/tasks/st_graph/st_graph_point.h"
 
 namespace apollo {
 namespace planning {
