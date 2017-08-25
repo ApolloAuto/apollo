9441e5fc88db2c58c69b5b51eb95ca1158f50118
Dong Li <lidong.main@gmail.com>
2017-08-24 18:12:25 -0700

    planning add st_boundary calculation in path_obstacle

--- modules/planning/common/path_obstacle.cc
+++ modules/planning/common/path_obstacle.cc
@@ -21,12 +21,21 @@
 #include <limits>
 #include <unordered_map>
 
+#include "modules/common/configs/vehicle_config_helper.h"
 #include "modules/common/log.h"
+#include "modules/common/util/util.h"
 #include "modules/planning/common/path_obstacle.h"
 #include "modules/planning/common/planning_gflags.h"
+#include "modules/planning/common/speed/st_boundary.h"
 
 namespace apollo {
 namespace planning {
+
+using common::VehicleConfigHelper;
+
+namespace {
+const double kStBoundaryDeltaS = 0.2;
+}
 
 const std::unordered_map<ObjectDecisionType::ObjectTagCase, int,
                          PathObstacle::ObjectTagCaseHash>
@@ -55,15 +64,100 @@ bool PathObstacle::Init(const ReferenceLine& reference_line) {
     AERROR << "Failed to get sl boundary for obstacle: " << id_;
     return false;
   }
-  if (!BuildStBoundary(reference_line, obstacle_)) {
-  }
+  BuildStBoundary(reference_line);
   return true;
 }
 
-bool PathObstacle::BuildStBoundary(const ReferenceLine& reference_line,
-                                   const Obstacle* obstacle) {
+void PathObstacle::BuildStBoundary(const ReferenceLine& reference_line) {
+  if (obstacle_->IsStatic() ||
+      obstacle_->Trajectory().trajectory_point().empty()) {
+    std::vector<STPoint> points;
+    points.emplace_back(perception_sl_boundary_.start_s(), 0.0);
+    points.emplace_back(perception_sl_boundary_.start_s(), FLAGS_st_max_t);
+    points.emplace_back(perception_sl_boundary_.end_s(), FLAGS_st_max_t);
+    points.emplace_back(perception_sl_boundary_.end_s(), 0.0);
+    st_boundary_ = StBoundary(points);
+  } else {
+    if (BuildTrajectoryStBoundary(reference_line, &st_boundary_)) {
+      ADEBUG << "Found st_boundary for obstacle " << id_;
+    } else {
+      ADEBUG << "No st_boundary for obstacle " << id_;
+    }
+  }
+}
+
+bool PathObstacle::BuildTrajectoryStBoundary(
+    const ReferenceLine& reference_line, StBoundary* const st_boundary) {
+  const auto& object_id = obstacle_->Id();
+  const auto& perception = obstacle_->Perception();
+  const double object_width = perception.width();
+  const double object_length = perception.length();
+  const auto& trajectory_points = obstacle_->Trajectory().trajectory_point();
+  if (trajectory_points.empty()) {
+    AWARN << "object " << object_id << " has no trajectory points";
+    return false;
+  }
+  const auto& adc_param =
+      VehicleConfigHelper::instance()->GetConfig().vehicle_param();
+  const double adc_length = adc_param.length();
+  const double adc_half_length = adc_length / 2.0;
+  const double adc_width = adc_param.width();
+  common::math::Box2d min_box({0, 0}, 1.0, 1.0, 1.0);
+  common::math::Box2d max_box({0, 0}, 1.0, 1.0, 1.0);
+  const double s_step = kStBoundaryDeltaS;
+  std::vector<common::math::Vec2d> polygon_points;
+  for (int i = 1; i < trajectory_points.size(); ++i) {
+    const auto& first_traj_point = trajectory_points[i - 1];
+    const auto& second_traj_point = trajectory_points[i - 1];
+    const auto& first_point = first_traj_point.path_point();
+    const auto& second_point = second_traj_point.path_point();
+    double total_length =
+        object_length + common::util::Distance2D(first_point, second_point);
+    common::math::Vec2d center((first_point.x() + second_point.x()) / 2.0,
+                               (first_point.y() + second_point.y()) / 2.0);
+    common::math::Box2d object_moving_box(center, first_point.theta(),
+                                          total_length, object_width);
+    SLBoundary object_boundary;
+    if (!reference_line.GetSLBoundary(object_moving_box, &object_boundary)) {
+      AERROR << "failed to calculate boundary";
+      return false;
+    }
+    const double object_s_diff =
+        object_boundary.end_s() - object_boundary.start_s();
+    if (object_boundary.end_s() < 0) {
+      // skip if behind adc
+      continue;
+    }
+    const double delta_t =
+        second_traj_point.relative_time() - first_traj_point.relative_time();
+    for (double s = std::max(object_boundary.start_s() - adc_half_length, 0.0);
+         s <
+         std::min(object_boundary.end_s() + adc_half_length, FLAGS_st_max_s);
+         s += s_step) {
+      auto adc_trajectory_point = reference_line.GetReferencePoint(s);
+      common::math::Box2d tmp_adc_box(adc_trajectory_point,
+                                      adc_trajectory_point.heading(),
+                                      adc_length, adc_width);
+      if (tmp_adc_box.HasOverlap(object_moving_box)) {
+        double mean_t = first_traj_point.relative_time();
+        if (object_s_diff > common::math::kMathEpsilon) {
+          mean_t = (first_traj_point.relative_time() +
+                    std::fabs((s - object_boundary.start_s()) / object_s_diff) *
+                        delta_t);
+        }
+        polygon_points.emplace_back(mean_t, s);
+      }
+    }
+  }
+  if (polygon_points.size() < 3) {
+    ADEBUG << "object " << object_id << " has no st overlap";
+    return false;
+  }
+  common::math::Polygon2d::ComputeConvexHull(polygon_points, st_boundary);
   return true;
 }
+
+const StBoundary& PathObstacle::st_boundary() const { return st_boundary_; }
 
 bool PathObstacle::IsLateralDecision(const ObjectDecisionType& decision) {
   return decision.has_ignore() || decision.has_nudge();
