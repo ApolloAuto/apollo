4e65d8b205a0b21598c4a5df20c0127085d7b832
Aaron Xiao <xiaoxiangquan@gmail.com>
2017-08-28 11:07:42 -0700

    Tools: Add proto flattening util and refactor routing tools.

--- modules/tools/common/proto_utils.py
+++ modules/tools/common/proto_utils.py
@@ -19,28 +19,72 @@
 import google.protobuf.text_format as text_format
 
 
-def get_pb_from_text_file(filename, proto_obj):
+def get_pb_from_text_file(filename, pb_value):
     """Get a proto from given text file."""
     with open(filename, 'r') as file_in:
-        return text_format.Merge(file_in.read(), proto_obj)
+        return text_format.Merge(file_in.read(), pb_value)
 
 
-def get_pb_from_bin_file(filename, proto_obj):
+def get_pb_from_bin_file(filename, pb_value):
     """Get a proto from given binary file."""
     with open(filename, 'rb') as file_in:
-        proto_obj.ParseFromString(file_in.read())
-    return proto_obj
+        pb_value.ParseFromString(file_in.read())
+    return pb_value
 
-def get_pb_from_file(filename, proto_obj):
+
+def get_pb_from_file(filename, pb_value):
     """Get a proto from given file by trying binary mode and text mode."""
     try:
-        return get_pb_from_bin_file(filename, proto_obj)
+        return get_pb_from_bin_file(filename, pb_value)
     except:
         print 'Info: Cannot parse %s as binary proto.' % filename
 
     try:
-        return get_pb_from_text_file(filename, proto_obj)
+        return get_pb_from_text_file(filename, pb_value)
     except:
         print 'Error: Cannot parse %s as text proto' % filename
 
     return None
+
+
+def flatten(pb_value, selectors):
+    """
+    Get a flattened tuple from pb_value. Selectors is a list of sub-fields.
+
+    Usage:
+    For a pb_value of:
+        total_pb = {
+            me: { name: 'myself' }
+            children: [{ name: 'child0' }, { name: 'child1' }]
+        }
+    my_name, child0_name = flatten(total_pb, ['me.name', 'children[0].name'])
+    # You get (my_name='myself', child0_name='child0')
+
+    children_names = flatten(total_pb, 'children.name')
+    # You get (children_names=['child0', 'child1'])
+    """
+    def __select_field(val, field):
+        if hasattr(val, '__len__'):
+            # Flatten repeated field.
+            return [__select_field(elem, field) for elem in val]
+        if not field.endswith(']'):
+            # Simple field.
+            return val.__getattribute__(field)
+        # field contains index: "field[index]".
+        field, index = field.split('[')
+        val = val.__getattribute__(field)
+        index = int(index[:-1])
+        return val[index] if index < len(val) else None
+
+    def __select(val, selector):
+        for field in selector.split('.'):
+            val = __select_field(val, field)
+            if val is None:
+                return None
+        return val
+
+    # Return the single result for single selector.
+    if type(selectors) is str:
+        return __select(pb_value, selectors)
+    # Return tuple result for multiple selectors.
+    return (__select(pb_value, selector) for selector in selectors)
