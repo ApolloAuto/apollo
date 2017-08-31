8f0d372b6acd192aae6f132c390c3537e002b9e3
Lei Wang <bestwanglei@gmail.com>
2017-08-31 13:57:31 -0700

    Change pcl install path to fix ros velodyne build.

--- docker/scripts/dev_start.sh
+++ docker/scripts/dev_start.sh
@@ -18,7 +18,7 @@
 
 VERSION=""
 ARCH=$(uname -m)
-VERSION_X86_64="dev-x86_64-20170829_1405"
+VERSION_X86_64="dev-x86_64-20170831_1347"
 VERSION_AARCH64="dev-aarch64-20170712_1533"
 if [[ $# == 1 ]];then
     VERSION=$1
