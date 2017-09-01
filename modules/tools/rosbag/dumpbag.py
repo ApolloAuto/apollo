3ba2d2cd892ca4a39e77e3bd179b0ffeab869451
Jiangtao Hu <ycool@users.noreply.github.com>
2017-09-01 10:24:05 -0700

    tool: change rosbag dump based on planning topic to align sequence number. (#1425)

--- modules/tools/rosbag/dumpbag.py
+++ modules/tools/rosbag/dumpbag.py
@@ -31,7 +31,6 @@ g_args = None
 
 g_delta_t = 0.5  # 1 second approximate time match region.
 
-
 def write_to_file(file_path, topic_pb):
     """write pb message to file"""
     f = file(file_path, 'w')
@@ -39,21 +38,46 @@ def write_to_file(file_path, topic_pb):
     f.close()
 
 
-def dump_bag(in_bag, out_dir, filter_topic):
+def dump_bag(in_bag, out_dir):
     """out_bag = in_bag + routing_bag"""
     bag = rosbag.Bag(in_bag, 'r')
     seq = 0
     global g_args
+    topic_name_map = {
+        "/apollo/localization/pose" : ["localization", None],
+        "/apollo/canbus/chassis" : ["chassis", None],
+        "/apollo/routing_response" : ["routing", None],
+        "/apollo/routing_resquest" : ["routing_request", None],
+        "/apollo/perception/obstacles" : ["perception", None],
+        "/apollo/prediction" : ["prediction", None],
+        "/apollo/planning" : ["planning", None],
+        "/apollo/control" : ["control", None]
+    }
+    first_time = None
+    record_num = 0
     for topic, msg, t in bag.read_messages():
-        if g_args.time and (t.secs < g_args.time - g_delta_t
-                            or t.secs > g_args.time + g_delta_t):
+        record_num += 1
+        if record_num % 1000 == 0:
+            print "Processing record_num:", record_num
+        if first_time is None:
+            first_time = t
+        if topic not in topic_name_map:
             continue
-        if not filter_topic or (filter_topic and topic == filter_topic):
-            message_file = topic.replace("/", "_")
-            file_path = os.path.join(out_dir,
-                                     str(seq) + message_file + ".pb.txt")
-            write_to_file(file_path, msg)
-        seq += 1
+        relative_time = (t - first_time).secs - g_args.start_time
+        if ((g_args.time_duration > 0) and
+            (relative_time < 0 or relative_time > g_args.time_duration)):
+            continue
+        if topic == "/apollo/planning":
+            seq += 1
+            topic_name_map[topic][1] = msg
+            print "Generating seq:", seq
+            for t, name_pb in topic_name_map.iteritems():
+                if name_pb[1] is None:
+                    continue
+                file_path = os.path.join(out_dir,
+                                        str(seq) + "_" + name_pb[0] + ".pb.txt")
+                write_to_file(file_path, msg)
+        topic_name_map[topic][1] = msg
 
 
 if __name__ == "__main__":
@@ -67,20 +91,22 @@ if __name__ == "__main__":
         action="store",
         help="the output directory for the dumped file")
     parser.add_argument(
-        "--topic",
-        action="store",
-        help="""the topic that you want to dump. If this option is not provided,
-        the tool will dump all the messages regardless of the message topic."""
-    )
-    parser.add_argument(
-        "--time",
+        "--start_time",
         type=float,
         action="store",
-        help="""The time range to extract""")
+        default=0.0,
+        help="""The time range to extract in second""")
+    parser.add_argument(
+        "--time_duration",
+        type=float,
+        action="store",
+        default=-1,
+        help="""time duration to extract in second, negative to extract all""")
+
 
     g_args = parser.parse_args()
 
     if os.path.exists(g_args.out_dir):
         shutil.rmtree(g_args.out_dir)
     os.makedirs(g_args.out_dir)
-    dump_bag(g_args.in_rosbag, g_args.out_dir, g_args.topic)
+    dump_bag(g_args.in_rosbag, g_args.out_dir)
