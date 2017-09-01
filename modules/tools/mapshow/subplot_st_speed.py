bfd4e2d04a1054a5027c5938e41a7989f7d1673e
jiangyifei <jiangyifei@gmail.com>
2017-09-01 16:41:02 -0700

    [planning] added speed limits for st plot

--- modules/tools/mapshow/subplot_st_speed.py
+++ modules/tools/mapshow/subplot_st_speed.py
@@ -1,18 +1,32 @@
-from planning import Planning
-from modules.planning.proto import planning_pb2
-from modules.planning.proto import planning_config_pb2
+#!/usr/bin/env python
+
+###############################################################################
+# Copyright 2017 The Apollo Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+###############################################################################
 
 class StSpeedSubplot:
 
     def __init__(self, ax, st_name):
         self.speed_limit_line = ax.plot([0], [0], "r-",
-                                        lw=6, alpha=0.5, label="speed limits")[0]
+                                        lw=6, alpha=0.5, label="limits")[0]
         self.speed_line = ax.plot([0], [0], "k-",
-                                  lw=3, alpha=0.5, label="speed planned")[0]
+                                  lw=3, alpha=0.5, label="planned")[0]
         self.speed_upper_bound_line = \
-            ax.plot([0], [0], "b-", lw=1, alpha=1, label="speed upper bound")[0]
+            ax.plot([0], [0], "b-", lw=1, alpha=1, label="upper")[0]
         self.speed_lower_bound_line = \
-            ax.plot([0], [0], "b-", lw=3, alpha=1, label="speed lower bound")[0]
+            ax.plot([0], [0], "b-", lw=3, alpha=1, label="lower")[0]
         self.st_name = st_name
         ax.set_xlim(-10, 70)
         ax.set_ylim(-1, 15)
