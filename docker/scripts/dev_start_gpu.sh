15c5d659978dab18c07de256045580a087ec7223
Lei Wang <bestwanglei@gmail.com>
2017-09-02 15:02:45 -0700

    Setup nvidia devices.

--- docker/scripts/dev_start_gpu.sh
+++ docker/scripts/dev_start_gpu.sh
@@ -54,21 +54,6 @@ fi
 
 source ${LOCAL_DIR}/scripts/apollo_base.sh
 
-function find_device() {
-    # ${1} = device pattern
-    local device_list=$(find /dev -name "${1}")
-    if [ -z "${device_list}" ]; then
-        warning "Failed to find device with pattern \"${1}\" ..."
-    else
-        local devices=""
-        for device in $(find /dev -name "${1}"); do
-            ok "Found device: ${device}."
-            devices="${devices} --device ${device}:${device}"
-        done
-        echo "${devices}"
-    fi
-}
-
 function main(){
     #FIX ME: remove login when open source.
     docker login -u autoapollo -p baidu123
@@ -86,10 +71,7 @@ function main(){
         display="${DISPLAY}"
     fi
 
-    # setup CAN device
-    if [ ! -e /dev/can0 ]; then
-        sudo mknod --mode=a+rw /dev/can0 c 52 0
-    fi
+    setup_device
 
     local devices=""
     devices="${devices} $(find_device ttyUSB*)"
