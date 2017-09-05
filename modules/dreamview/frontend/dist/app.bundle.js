7b112dc930a4baf0ed7a5f53e445e6bfdcce9959
Vivian Lin <linvivian@baidu.com>
2017-09-05 13:47:39 -0700

    dreamview: updated drive mode font size (#1511)

--- modules/dreamview/frontend/dist/app.bundle.js
+++ modules/dreamview/frontend/dist/app.bundle.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5166942d7b765388d49aec2141772b8bb5288d5841a5329f7fa382174c071b37
-size 949161
+oid sha256:478d4e91f18bacf876ec437fb477af5df29f3b3930a75bcc6a80cac12c029b65
+size 949510
