0d255739ea55d6217065e85a07274dfdf0e30a26
Zhang Liangliang <zhangliangliang@baidu.com>
2017-09-07 10:44:27 -0700

    Planning: removed low velocity threshold. Planning will report the idea trajectory and control will set a cut off number in its own algorithm.

--- modules/planning/tasks/qp_spline_st_speed/qp_spline_st_graph.cc
+++ modules/planning/tasks/qp_spline_st_speed/qp_spline_st_graph.cc
@@ -118,11 +118,6 @@ Status QpSplineStGraph::Search(const StGraphData& st_graph_data,
     double v = spline.Derivative(time);
     double a = spline.SecondOrderDerivative(time);
     double da = spline.ThirdOrderDerivative(time);
-    if (v < FLAGS_qp_st_low_velocity_threshold) {
-      v = 0.0;
-      a = 0.0;
-      da = 0.0;
-    }
     speed_data->AppendSpeedPoint(s, time, v, a, da);
     time += t_output_resolution;
   }
