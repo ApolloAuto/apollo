11a8f634e619905ae9885874b2770f083b8b07db
Vivian Lin <linvivian@baidu.com>
2017-09-08 14:19:40 -0700

    dreamview: added tip for routing editor (#1646)

--- modules/dreamview/frontend/dist/app.bundle.js
+++ modules/dreamview/frontend/dist/app.bundle.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b0361bad7fdd9508a07026ce53f94a9ab46a354afa87730f83ddddca3f22faa2
-size 948949
+oid sha256:d080aa7e250639b34a02515f379bf7635717e5b5a6eb42b0d7766a679c8523c7
+size 951710
