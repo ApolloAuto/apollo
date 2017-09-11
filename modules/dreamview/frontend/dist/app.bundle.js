764ce99b150e6eae3e1a9a91bbd7f2eb7d894391
siyangy <yu.siyang@yahoo.com>
2017-09-11 14:39:47 -0700

    Dreamview: Add heartbeat package from frontend (#1673)

--- modules/dreamview/frontend/dist/app.bundle.js
+++ modules/dreamview/frontend/dist/app.bundle.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:01299e392f5e5855482d8dcab804a0864b29f8a77adc7fcecc0ff51d79f86f84
-size 951728
+oid sha256:581715b96a364030b86048918de83b093d3af8f4c5a282abed2704458c154c7f
+size 951884
