8ff31014a72f8a3720281cd305ce07bacb893e4b
Dong Li <lidong.main@gmail.com>
2017-09-11 22:49:18 -0700

    planning: added GetPointAtTime for static obstacle.

--- modules/planning/common/obstacle.cc
+++ modules/planning/common/obstacle.cc
@@ -101,20 +101,36 @@ bool Obstacle::IsVirtualObstacle(
 
 common::TrajectoryPoint Obstacle::GetPointAtTime(
     const double relative_time) const {
-  auto comp = [](const common::TrajectoryPoint p, const double time) {
-    return p.relative_time() < time;
-  };
-
   const auto& points = trajectory_.trajectory_point();
-  auto it_lower =
-      std::lower_bound(points.begin(), points.end(), relative_time, comp);
+  if (points.size() < 2) {
+    common::TrajectoryPoint point;
+    point.mutable_path_point()->set_x(perception_obstacle_.position().x());
+    point.mutable_path_point()->set_y(perception_obstacle_.position().y());
+    point.mutable_path_point()->set_z(perception_obstacle_.position().z());
+    point.mutable_path_point()->set_theta(perception_obstacle_.theta());
+    point.mutable_path_point()->set_s(0.0);
+    point.mutable_path_point()->set_kappa(0.0);
+    point.mutable_path_point()->set_dkappa(0.0);
+    point.mutable_path_point()->set_ddkappa(0.0);
+    point.set_v(0.0);
+    point.set_a(0.0);
+    point.set_relative_time(0.0);
+    return point;
+  } else {
+    auto comp = [](const common::TrajectoryPoint p, const double time) {
+      return p.relative_time() < time;
+    };
 
-  if (it_lower == points.begin()) {
-    return *points.begin();
-  } else if (it_lower == points.end()) {
-    return *points.rbegin();
+    auto it_lower =
+        std::lower_bound(points.begin(), points.end(), relative_time, comp);
+
+    if (it_lower == points.begin()) {
+      return *points.begin();
+    } else if (it_lower == points.end()) {
+      return *points.rbegin();
+    }
+    return util::interpolate(*(it_lower - 1), *it_lower, relative_time);
   }
-  return util::interpolate(*(it_lower - 1), *it_lower, relative_time);
 }
 
 common::math::Box2d Obstacle::GetBoundingBox(
