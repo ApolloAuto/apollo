ff98f4047ff4dd39b40a788acc8e9b90761b707a
Dong Li <lidong.main@gmail.com>
2017-09-12 16:17:02 -0700

    planning: change default speed limit to 12.5 m/s

--- modules/planning/common/planning_gflags.cc
+++ modules/planning/common/planning_gflags.cc
@@ -56,7 +56,7 @@ DEFINE_bool(enable_reference_line_provider_thread, false,
 DEFINE_double(default_reference_line_width, 4.0,
               "Default reference line width");
 
-DEFINE_double(planning_upper_speed_limit, 7.5,
+DEFINE_double(planning_upper_speed_limit, 12.5,
               "Maximum speed (m/s) in planning.");
 
 DEFINE_double(trajectory_time_length, 8.0, "Trajectory time length");
