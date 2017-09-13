b1cec66286ae91e09fdd56706a16b44108348cb8
Vivian Lin <linvivian@baidu.com>
2017-09-13 16:48:44 -0700

    Dreamview: increase ground size for route editing (#1756)

--- modules/dreamview/frontend/dist/app.bundle.js
+++ modules/dreamview/frontend/dist/app.bundle.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e827d6e84cc3a488a36e02e94c002595f6bc86ed1965e2db739df79d058efbcc
-size 954570
+oid sha256:a5c0031e9623441c4a74a671b8bf22b18972d5d48789b70f19d94542177c71f4
+size 954918
