5e351c6a5dd73551316466dd927a6f188206fdb0
siyangy <yu.siyang@yahoo.com>
2017-09-13 14:57:37 -0700

    Dreamview: remove unused param (#1744)

--- modules/dreamview/frontend/dist/app.bundle.js
+++ modules/dreamview/frontend/dist/app.bundle.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7586ce33862d75f8281449cd99f6a2fc09fed0a565ad44e45e1db99a13f67570
-size 954942
+oid sha256:e827d6e84cc3a488a36e02e94c002595f6bc86ed1965e2db739df79d058efbcc
+size 954570
