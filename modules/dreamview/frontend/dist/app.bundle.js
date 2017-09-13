124f518e727eceaf3e40a4df7519fc8d741d57d9
Vivian Lin <linvivian@baidu.com>
2017-09-13 13:34:42 -0700

    Dreamview: added threeJS performance monitor (#1740)
    * dreamview: added threeJS performance monitor
    
    * updated performance monitor default value
    
    * updated dist

--- modules/dreamview/frontend/dist/app.bundle.js
+++ modules/dreamview/frontend/dist/app.bundle.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a49416e8e5345bf44ee0a8df5842a6d0e76b70131028225420a1059d5933bd36
-size 952451
+oid sha256:7545aa85a16ed5abd9ecd692ed9bd69c2393bfc6c30754ca2f0f629d0540fcbc
+size 955104
