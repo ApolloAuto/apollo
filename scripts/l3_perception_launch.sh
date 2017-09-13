c748dbd0042a5f653d3c7c22390276e2461581b7
Zhang LI <tim.boilermaker.2016@gmail.com>
2017-09-13 13:57:52 -0700

    add launch files

---
+++ scripts/l3_perception_launch.sh
@@ -0,0 +1,1 @@
+./bazel-bin/modules/l3_perception/l3_perception --flagfile=modules/l3_perception/conf/l3_perception.conf
---
+++ scripts/mobileye_launch.sh
@@ -0,0 +1,1 @@
+./bazel-bin/modules/drivers/mobileye/mobileye --flagfile=modules/drivers/mobileye/conf/mobileye.conf
