77f4a4495c05df9ad78ed0dc0dcc16b0de83f5bb
Zhang Liangliang <zhangliangliang@baidu.com>
2017-09-14 15:40:22 -0700

    ALL: fixed all lint errors.

--- modules/planning/tasks/dp_poly_path/trajectory_cost.cc
+++ modules/planning/tasks/dp_poly_path/trajectory_cost.cc
@@ -15,7 +15,7 @@
  *****************************************************************************/
 
 /**
- * @file trjactory_cost.h
+ * @file
  **/
 
 #include "modules/planning/tasks/dp_poly_path/trajectory_cost.h"
@@ -35,15 +35,14 @@ using apollo::common::math::Vec2d;
 using apollo::common::TrajectoryPoint;
 
 TrajectoryCost::TrajectoryCost(
-    const DpPolyPathConfig &config,
-    const ReferenceLine &reference_line,
-    const std::vector<const PathObstacle*>& obstacles,
+    const DpPolyPathConfig &config, const ReferenceLine &reference_line,
+    const std::vector<const PathObstacle *> &obstacles,
     const common::VehicleParam &vehicle_param,
-    const SpeedData &heuristic_speed_data) :
-    config_(config),
-    reference_line_(&reference_line),
-    vehicle_param_(vehicle_param),
-    heuristic_speed_data_(heuristic_speed_data) {
+    const SpeedData &heuristic_speed_data)
+    : config_(config),
+      reference_line_(&reference_line),
+      vehicle_param_(vehicle_param),
+      heuristic_speed_data_(heuristic_speed_data) {
   const double total_time =
       std::min(heuristic_speed_data_.TotalTime(), FLAGS_prediction_total_time);
 
@@ -56,13 +55,13 @@ TrajectoryCost::TrajectoryCost(
     }
     const auto ptr_obstacle = ptr_path_obstacle->obstacle();
     if (ptr_obstacle->PerceptionId() < 0) {
-      //Virtual obsatcle
+      // Virtual obsatcle
       continue;
     }
     std::vector<Box2d> box_by_time;
     for (uint32_t t = 0; t <= num_of_time_stamps_; ++t) {
       TrajectoryPoint trajectory_point =
-        ptr_obstacle->GetPointAtTime(t * config.eval_time_interval());
+          ptr_obstacle->GetPointAtTime(t * config.eval_time_interval());
       Box2d obs_box = ptr_obstacle->GetBoundingBox(trajectory_point);
       box_by_time.push_back(obs_box);
     }
@@ -77,15 +76,15 @@ double TrajectoryCost::Calculate(const QuinticPolynomialCurve1d &curve,
   // path_cost
   double path_s = 0.0;
   while (path_s < (end_s - start_s)) {
-    double l = std::fabs(curve.Evaluate(0, path_s));
+    const double l = std::fabs(curve.Evaluate(0, path_s));
     total_cost += l;
 
-    double dl = std::fabs(curve.Evaluate(1, path_s));
+    const double dl = std::fabs(curve.Evaluate(1, path_s));
     total_cost += dl;
 
     path_s += config_.path_resolution();
   }
-  
+
   // obstacle cost
   uint32_t start_index = 0;
   bool is_found_start_index = false;
@@ -109,24 +108,24 @@ double TrajectoryCost::Calculate(const QuinticPolynomialCurve1d &curve,
   for (; start_index < end_index; ++start_index) {
     common::SpeedPoint speed_point;
     heuristic_speed_data_.EvaluateByTime(
-                  start_index * config_.eval_time_interval(), &speed_point);
-    double s = speed_point.s() - start_s;
-    double l = curve.Evaluate(0, s);
-    double dl = curve.Evaluate(1, s);
+        start_index * config_.eval_time_interval(), &speed_point);
+    const double s = speed_point.s() - start_s;
+    const double l = curve.Evaluate(0, s);
+    const double dl = curve.Evaluate(1, s);
     Vec2d ego_xy_point;
     common::SLPoint sl;
     sl.set_s(speed_point.s());
     sl.set_l(l);
     reference_line_->SLToXY(sl, &ego_xy_point);
-    ReferencePoint reference_point = 
-          reference_line_->GetReferencePoint(speed_point.s());
+    ReferencePoint reference_point =
+        reference_line_->GetReferencePoint(speed_point.s());
 
     double one_minus_kappa_r_d = 1 - reference_point.kappa() * l;
     double delta_theta = std::atan2(dl, one_minus_kappa_r_d);
-    double theta = common::math::NormalizeAngle(
-                                      delta_theta + reference_point.heading());
-    Box2d ego_box = {ego_xy_point, theta, 
-                     vehicle_param_.length(), vehicle_param_.width()};
+    double theta =
+        common::math::NormalizeAngle(delta_theta + reference_point.heading());
+    Box2d ego_box = {ego_xy_point, theta, vehicle_param_.length(),
+                     vehicle_param_.width()};
     for (const auto &obstacle_trajectory : obstacle_boxes_) {
       auto &obstacle_box = obstacle_trajectory[start_index];
       double distance = obstacle_box.DistanceTo(ego_box);
