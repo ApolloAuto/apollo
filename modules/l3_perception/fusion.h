48d744ce750e022a91bf3e039ddbd18cf8a439ea
Zhang Li <lizhang@baidu.com>
2017-09-25 14:10:11 -0700

    add mobilieye radar fusion

---
+++ modules/l3_perception/fusion.h
@@ -0,0 +1,45 @@
+/******************************************************************************
+ * Copyright 2017 The Apollo Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *****************************************************************************/
+
+/**
+ * @file
+ */
+
+#ifndef MODEULES_L3_PERCEPTION_FUSION_H_
+#define MODEULES_L3_PERCEPTION_FUSION_H_
+
+#include "modules/perception/proto/perception_obstacle.pb.h"
+
+/**
+ * @namespace apollo::l3_perception::fusion
+ * @brief apollo::l3_perception
+ */
+namespace apollo {
+namespace l3_perception {
+namespace fusion {
+
+using apollo::perception::PerceptionObstacles;
+
+void MobileyeRadarFusion(
+    PerceptionObstacles* const mobileye_obstacles, PerceptionObstacles* const radar_obstacles);
+
+}  // namespace fusion 
+}  // namespace l3_perception
+}  // namespace apollo
+
+#endif  // MODULES_L3_PERCEPTION_FUSION_H_
+
+
