60d9ec8fdbdf3b93174fef8f254c39cf96b407c3
Dong Li <lidong.main@gmail.com>
2017-09-25 16:11:36 -0700

    planning: migrate the storage location for StBoundary (#417)

--- modules/planning/tasks/st_graph/st_graph_data.h
+++ modules/planning/tasks/st_graph/st_graph_data.h
@@ -34,11 +34,11 @@ namespace planning {
 
 class StGraphData {
  public:
-  StGraphData(const std::vector<StBoundary>& st_boundaries,
+  StGraphData(const std::vector<const StBoundary*>& st_boundaries,
               const apollo::common::TrajectoryPoint& init_point,
               const SpeedLimit& speed_limit, const double path_data_length);
 
-  const std::vector<StBoundary>& st_boundaries() const;
+  const std::vector<const StBoundary*>& st_boundaries() const;
 
   const apollo::common::TrajectoryPoint& init_point() const;
 
@@ -47,7 +47,7 @@ class StGraphData {
   double path_data_length() const;
 
  private:
-  std::vector<StBoundary> st_boundaries_;
+  std::vector<const StBoundary*> st_boundaries_;
   apollo::common::TrajectoryPoint init_point_;
 
   SpeedLimit speed_limit_;
