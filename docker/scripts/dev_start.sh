6a9225d123ce3203dba871b1a3739d98385b13c0
Lei Wang <wanglei63@baidu.com>
2017-09-26 19:10:48 -0700

    Add gevent package for hmi [skip ci].

--- docker/scripts/dev_start.sh
+++ docker/scripts/dev_start.sh
@@ -18,7 +18,7 @@
 
 VERSION=""
 ARCH=$(uname -m)
-VERSION_X86_64="dev-x86_64-20170920_1652"
+VERSION_X86_64="dev-x86_64-20170926_1848"
 VERSION_AARCH64="dev-aarch64-20170921_1158"
 if [[ $# == 1 ]];then
     VERSION=$1
