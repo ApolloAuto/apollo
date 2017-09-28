cf3e8dff62259e3b10bf3f404f2b32561f22fbdf
Calvin Miao <jhmiao@gmail.com>
2017-09-28 12:02:27 -0700

    Prediction: added move sequence predictor (#469)

---
+++ modules/prediction/predictor/move_sequence/move_sequence_predictor.h
@@ -0,0 +1,52 @@
+/******************************************************************************
+ * Copyright 2017 The Apollo Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *****************************************************************************/
+
+/**
+ * @file
+ * @brief Define move sequence predictor
+ */
+
+#ifndef MODULES_PREDICTION_PREDICTOR_MOVE_SEQUENCE_MOVE_SEQUENCE_PREDICTOR_H_
+#define MODULES_PREDICTION_PREDICTOR_MOVE_SEQUENCE_MOVE_SEQUENCE_PREDICTOR_H_
+
+#include "modules/prediction/predictor/predictor.h"
+
+namespace apollo {
+namespace prediction {
+
+class MoveSequencePredictor : public Predictor {
+ public:
+  /**
+   * @brief Constructor
+   */
+  MoveSequencePredictor() = default;
+
+  /**
+   * @brief Destructor
+   */
+  virtual ~MoveSequencePredictor() = default;
+
+  /**
+   * @brief Make prediction
+   * @param Obstacle pointer
+   */
+  void Predict(Obstacle* obstacle) override;
+};
+
+}  // namespace prediction
+}  // namespace apollo
+
+#endif  // MODULES_PREDICTION_PREDICTOR_MOVE_SEQUENCE_MOVE_SEQUENCE_PREDICTOR_H_
