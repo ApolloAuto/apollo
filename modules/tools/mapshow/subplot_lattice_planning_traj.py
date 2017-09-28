021b7df78de6cb4134679e14f5640d6d992fc72d
liyunADU <liliyun@baidu.com>
2017-09-28 16:36:40 -0700

    Plotting prediction polygon and lattice paths

--- modules/tools/mapshow/subplot_lattice_planning_traj.py
+++ modules/tools/mapshow/subplot_lattice_planning_traj.py
@@ -26,26 +26,14 @@ class LatticeSubPlot:
             map = Map()
             map.load(map_file)
             map.draw_lanes(ax, False, [])
-        
-        self.path_lines = []
-        self.path_lines_size = 3
-        """
-        colors = ['b', 'g', 'r', 'k']
-        for i in range(self.path_lines_size):
-            line, = ax.plot(
-                [0], [0],
-                colors[i % len(colors)],
-                lw=3 + i * 3,
-                alpha=0.4)
-            self.path_lines.append(line)
-        """
+
         self.lattice_path_lines = []
         self.lattice_path_lines_size = 12
         for i in range(self.lattice_path_lines_size):
             line, = ax.plot(
                 [0], [0], 'r',
                 lw=3.8,
-                alpha=1.0)
+                alpha=0.8)
             self.lattice_path_lines.append(line)
 
         self.vehicle_position_line, = ax.plot([0], [0], 'go', alpha=0.3)
@@ -57,7 +45,7 @@ class LatticeSubPlot:
         self.prediction_path_lines_size = 10
         for i in range(self.prediction_path_lines_size):
             line, = ax.plot(
-                [0], [0], 'k',
+                [0], [0], 'g',
                 lw=1.8,
                 alpha=0.5)
             self.prediction_path_lines.append(line)
@@ -68,15 +56,13 @@ class LatticeSubPlot:
             line, = ax.plot(
                 [0], [0], 'k',
                 lw=2.8,
-                alpha=0.0)
+                alpha=0.5)
             self.prediction_polygon_lines.append(line)
 
         self.set_visible(False)
         ax.set_title("PLANNING LATTICE PATH")
 
     def set_visible(self, visible):
-        #for line in self.path_lines:
-        #    line.set_visible(visible)
         for line in self.lattice_path_lines:
             line.set_visible(visible)
         self.vehicle_position_line.set_visible(False)
@@ -88,26 +74,7 @@ class LatticeSubPlot:
             line.set_visible(visible)
 
     def show(self, planning, localization, prediction):
-        cnt = 0
-        planning.path_data_lock.acquire()
-        for name in planning.path_data_x.keys():
-            if cnt >= self.path_lines_size:
-                print "WARNING: number of path lines is more than " \
-                      + str(self.path_lines_size)
-                continue
-            if len(planning.path_data_x[name]) <= 1:
-                continue
-            path_line = self.path_lines[cnt]
-            path_line.set_visible(True)
-            path_line.set_xdata(planning.path_data_x[name])
-            path_line.set_ydata(planning.path_data_y[name])
-            path_line.set_label(name[0:5]) #????
-            cnt += 1
-        planning.path_data_lock.release()
-
-        ###########
-        cnt = 0
-        planning.lattice_lock.acquire()
+        # planning        planning.lattice_lock.acquire()
         if len(planning.lattice_path_data_x) != len(planning.lattice_path_data_y):
             # sth is wrong
             print "ERROR lattice_path_data_x size[" + str(len(lattice_path_data_x)) \
@@ -117,38 +84,66 @@ class LatticeSubPlot:
                 + "] > maximum lattice path lines size[" + str(self.lattice_path_lines_size) + "]"
 
         for i in range(len(planning.lattice_path_data_x)):
-            print "      debugggin...... trying to draw line " + str(i)
+            print "      debugggin...... trying to draw planning line " + str(i)
             #if len(planning.lattice_path_data_x) <= 1:
             #    print "xxxxx  continued  from i in range " + str(len(planning.lattice_path_data_x))
             #    continue
+            if i >= len(self.lattice_path_lines):
+                break 
             lattice_path_line = self.lattice_path_lines[i]
             lattice_path_line.set_visible(True)
             lattice_path_line.set_xdata(planning.lattice_path_data_x[i])
             lattice_path_line.set_ydata(planning.lattice_path_data_y[i])
             #lattice_path_line.set_label(name[0:5]) #????
-            #cnt += 1
-        planning.lattice_lock.release()
-        ###########
 
-        planning.init_point_lock.acquire()
         self.init_point_line.set_xdata(planning.init_point_x)
         self.init_point_line.set_ydata(planning.init_point_y)
         self.init_point_line.set_visible(True)
         planning.init_point_lock.release()
 
+        # localization
         localization.localization_data_lock.acquire()
         self.draw_vehicle(localization)
         try:
-            self.ax.set_xlim(localization.localization_pb.pose.position.x - self.map_width,
-                        localization.localization_pb.pose.position.x + self.map_width)
-            self.ax.set_ylim(localization.localization_pb.pose.position.y - self.map_width,
-                        localization.localization_pb.pose.position.y + self.map_width)
+            self.ax.set_xlim(localization.localization_pb.pose.position.x - 2* self.map_width,
+                        localization.localization_pb.pose.position.x + 2 * self.map_width)
+            self.ax.set_ylim(localization.localization_pb.pose.position.y - 2 * self.map_width,
+                        localization.localization_pb.pose.position.y + 2 * self.map_width)
         except:
             pass
         localization.localization_data_lock.release()
 
-        # prediction stuff
-        # tbd
+        # prediction
+        prediction.prediction_lock.acquire()
+        if len(prediction.prediction_path_data_x) != len(prediction_path_data_y):
+            print "ERROR prediction_path_data_x size[" + str(len(prediction.prediction_path_data_x)) \
+                + "] != prediction_path_data_y size[" + str(len(prediction_path_data_y)) + "]"
+        if len(prediction.prediction_path_data_x) > self.prediction_path_lines_size:
+            print "ERROR prediction_path_data_x size[" + str(len(prediction_path_data_x)) \
+                + "] > maximum prediction path line size[" + str(len(self.prediction_path_lines_size)) + "]"
+        if len(prediction.prediction_polygons) > self.prediction_polygon_lines_size:
+            print "ERROR prediction_polygons size[" + str(len(prediction.prediction_polygons)) \
+                + "] != maximum prediction polygon size[" + str(self.prediction_polygon_lines_size) + "]"
+        for i in range(prediction.prediction_path_data_x):
+            if i > len(self.prediction_path_line):
+                break
+            prediction_path_line = self.prediction_path_lines[i]
+            prediction_path_line.set_visible(True)
+            prediction_path_line.set_xdata(prediction.prediction_path_data_x[i])
+            prediction_path_line.set_ydata(prediction.prediction_path_data_y[i])
+
+        for i in range(prediction.prediction_polygons):
+            if i > len(self.prediction_polygon_lines_size):
+                break
+            prediction_polygon_line = self.prediction_polygon_lines[i]
+            polygon_x = []
+            polygon_y = []
+            for pt in prediction.prediction_polygons[i]:
+                polygon_x.append(pt[0])
+                polygon_y.append(pt[1])
+            prediction_polygon_line.set_xdata()
+            prediction_polygon_line.set_ydata()
+        prediction.prediction_lock.release()
 
         self.ax.autoscale_view()
         self.ax.relim()
