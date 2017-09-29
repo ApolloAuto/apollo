e42f7030d8d1c7d1b07c05e428ffc0d7539ba540
Liangliang Zhang <zhangliangliang@baidu.com>
2017-09-28 17:22:37 -0700

    Planning: added TestPiecewiseLinearKernel - add_regularization (#481)
    * Planning: added TestPiecewiseLinearKernel - add_regularization
    
    * Planning: added TestPiecewiseLinearKernel - add_reference_line_kernel_matrix.

---
+++ modules/planning/math/smoothing_spline/piecewise_linear_kernel_test.cc
@@ -0,0 +1,93 @@
+/******************************************************************************
+ * Copyright 2017 The Apollo Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *****************************************************************************/
+
+/**
+ * @file
+ **/
+#include "modules/planning/math/smoothing_spline/piecewise_linear_kernel.h"
+
+#include "gtest/gtest.h"
+
+namespace apollo {
+namespace planning {
+
+using Eigen::MatrixXd;
+
+TEST(TestPiecewiseLinearKernel, add_regularization) {
+  PiecewiseLinearKernel kernel(10, 0.1);
+
+  kernel.AddRegularization(0.2);
+
+  const auto mat = kernel.kernel_matrix();
+  const auto offset = kernel.offset_matrix();
+
+  std::cout << mat << std::endl;
+  std::cout << offset << std::endl;
+
+  MatrixXd mat_golden(10, 10);
+  // clang-format off
+  mat_golden <<
+    0.2,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+      0, 0.2,   0,   0,   0,   0,   0,   0,   0,   0,
+      0,   0, 0.2,   0,   0,   0,   0,   0,   0,   0,
+      0,   0,   0, 0.2,   0,   0,   0,   0,   0,   0,
+      0,   0,   0,   0, 0.2,   0,   0,   0,   0,   0,
+      0,   0,   0,   0,   0, 0.2,   0,   0,   0,   0,
+      0,   0,   0,   0,   0,   0, 0.2,   0,   0,   0,
+      0,   0,   0,   0,   0,   0,   0, 0.2,   0,   0,
+      0,   0,   0,   0,   0,   0,   0,   0, 0.2,   0,
+      0,   0,   0,   0,   0,   0,   0,   0,   0, 0.2;
+  // clang-format on
+  EXPECT_EQ(mat, mat_golden);
+
+  MatrixXd offset_golden(10, 1);
+  offset_golden << 0, 0, 0, 0, 0, 0, 0, 0, 0, 0;
+  EXPECT_EQ(offset, offset_golden);
+
+  kernel.AddRegularization(0.3);
+  mat_golden = MatrixXd::Identity(10, 10) * 0.5;
+  EXPECT_EQ(kernel.kernel_matrix(), mat_golden);
+}
+
+TEST(TestPiecewiseLinearKernel, add_reference_line_kernel_matrix) {
+  PiecewiseLinearKernel kernel(10, 0.1);
+
+  std::vector<uint32_t> index_list;
+  std::vector<double> pos_list;
+  for (int i = 0; i < 10; ++i) {
+    index_list.push_back(i);
+    pos_list.push_back(i * 2);
+  }
+
+  kernel.AddReferenceLineKernelMatrix(index_list, pos_list, 10.0);
+
+  const auto mat = kernel.kernel_matrix();
+  const auto offset = kernel.offset_matrix();
+
+  std::cout << mat << std::endl;
+  std::cout << offset << std::endl;
+
+  MatrixXd mat_golden = MatrixXd::Identity(10, 10) * 10.0;
+  EXPECT_EQ(mat, mat_golden);
+
+  MatrixXd offset_golden(10, 1);
+  offset_golden << 0, -40, -80, -120, -160, -200, -240, -280, -320, -360;
+
+  EXPECT_EQ(offset, offset_golden);
+}
+
+}  // namespace planning
+}  // namespace apollo
