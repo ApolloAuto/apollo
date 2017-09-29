eb80a9b665f7e5dfdc8fdb4b867580d73c342293
Liangliang Zhang <zhangliangliang@baidu.com>
2017-09-29 14:18:35 -0700

    Planning: use piecewise linear method to solve qp problem when spline (#493)
    fails. fixed init point printout bug.

--- modules/planning/common/planning_gflags.cc
+++ modules/planning/common/planning_gflags.cc
@@ -134,18 +134,16 @@ DEFINE_bool(enable_rule_layer, true,
 /// common
 DEFINE_double(stop_max_distance_buffer, 4.0,
               "distance buffer of passing stop line");
-DEFINE_double(stop_min_speed, 0.1,
-              "min speed for computing stop");
-DEFINE_double(stop_max_deceleration, 6.0,
-              "max deceleration");
+DEFINE_double(stop_min_speed, 0.1, "min speed for computing stop");
+DEFINE_double(stop_max_deceleration, 6.0, "max deceleration");
 /// traffic light
 DEFINE_bool(enable_signal_lights, false, "enable signal_lights");
 DEFINE_string(signal_light_virtual_object_id_prefix, "SL_",
               "prefix for converting signal id to virtual object id");
 DEFINE_double(max_deacceleration_for_yellow_light_stop, 2.0,
               "treat yellow light as red when deceleration (abstract value"
-                  " in m/s^2) is less than this threshold; otherwise treated"
-                  " as green light");
+              " in m/s^2) is less than this threshold; otherwise treated"
+              " as green light");
 /// crosswalk
 DEFINE_bool(enable_crosswalk, false, "enable crosswalk");
 DEFINE_string(crosswalk_virtual_object_id_prefix, "CW_",
