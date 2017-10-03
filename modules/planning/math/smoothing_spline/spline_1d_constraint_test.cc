cceec05b38f50ae9ecc140d7120d4a1004540539
Zhang Liangliang <zhangliangliang@baidu.com>
2017-10-03 16:31:48 -0700

    Planning: Added first and second order derivative constraint test in spline 1d constraint.

--- modules/planning/math/smoothing_spline/spline_1d_constraint_test.cc
+++ modules/planning/math/smoothing_spline/spline_1d_constraint_test.cc
@@ -64,5 +64,83 @@ TEST(Spline1dConstraint, add_boundary) {
   }
 }
 
+TEST(Spline1dConstraint, add_derivative_boundary) {
+  std::vector<double> x_knots = {0.0, 1.0};
+  int32_t spline_order = 6;
+  Spline1dConstraint constraint(x_knots, spline_order);
+
+  std::vector<double> x_coord = {0.0, 0.5, 1.0};
+  std::vector<double> lower_bound = {1.0, 1.0, 1.0};
+  std::vector<double> upper_bound = {5.0, 5.0, 5.0};
+
+  constraint.AddDerivativeBoundary(x_coord, lower_bound, upper_bound);
+  const auto mat = constraint.inequality_constraint().constraint_matrix();
+  const auto boundary =
+      constraint.inequality_constraint().constraint_boundary();
+
+  // clang-format off
+  Eigen::MatrixXd ref_mat = Eigen::MatrixXd::Zero(6, 6);
+  ref_mat <<
+      0,       1,       0,       0,       0,       0,
+      0,       1,       1,    0.75,     0.5,  0.3125,
+      0,       1,       2,       3,       4,       5,
+      0,      -1,      -0,      -0,      -0,      -0,
+      0,      -1,      -1,   -0.75,    -0.5, -0.3125,
+      0,      -1,      -2,      -3,      -4,      -5;
+  // clang-format on
+
+  for (int i = 0; i < mat.rows(); ++i) {
+    for (int j = 0; j < mat.cols(); ++j) {
+      EXPECT_DOUBLE_EQ(mat(i, j), ref_mat(i, j));
+    }
+  }
+
+  Eigen::MatrixXd ref_boundary = Eigen::MatrixXd::Zero(6, 1);
+  ref_boundary << 1.0, 1.0, 1.0, -5.0, -5.0, -5.0;
+
+  for (int i = 0; i < ref_boundary.rows(); ++i) {
+    EXPECT_DOUBLE_EQ(boundary(i, 0), ref_boundary(i, 0));
+  }
+}
+
+TEST(Spline1dConstraint, add_second_derivative_boundary) {
+  std::vector<double> x_knots = {0.0, 1.0};
+  int32_t spline_order = 6;
+  Spline1dConstraint constraint(x_knots, spline_order);
+
+  std::vector<double> x_coord = {0.0, 0.5, 1.0};
+  std::vector<double> lower_bound = {1.0, 1.0, 1.0};
+  std::vector<double> upper_bound = {5.0, 5.0, 5.0};
+
+  constraint.AddSecondDerivativeBoundary(x_coord, lower_bound, upper_bound);
+  const auto mat = constraint.inequality_constraint().constraint_matrix();
+  const auto boundary =
+      constraint.inequality_constraint().constraint_boundary();
+
+  // clang-format off
+  Eigen::MatrixXd ref_mat = Eigen::MatrixXd::Zero(6, 6);
+  ref_mat <<
+   0,    0,    2,    0,    0,    0,
+   0,    0,    2,    3,    3,  2.5,
+   0,    0,    2,    6,   12,   20,
+   0,    0,   -2,   -0,   -0,   -0,
+   0,    0,   -2,   -3,   -3, -2.5,
+   0,    0,   -2,   -6,  -12,  -20;
+  // clang-format on
+
+  for (int i = 0; i < mat.rows(); ++i) {
+    for (int j = 0; j < mat.cols(); ++j) {
+      EXPECT_DOUBLE_EQ(mat(i, j), ref_mat(i, j));
+    }
+  }
+
+  Eigen::MatrixXd ref_boundary = Eigen::MatrixXd::Zero(6, 1);
+  ref_boundary << 1.0, 1.0, 1.0, -5.0, -5.0, -5.0;
+
+  for (int i = 0; i < ref_boundary.rows(); ++i) {
+    EXPECT_DOUBLE_EQ(boundary(i, 0), ref_boundary(i, 0));
+  }
+}
+
 }  // namespace planning
 }  // namespace apollo
