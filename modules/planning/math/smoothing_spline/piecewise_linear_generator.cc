907f6ad2d9b071d562834867d97927b68af62d35
Liangliang Zhang <zhangliangliang@baidu.com>
2017-10-03 19:03:54 -0700

    Planning: set enableRegularization to BT_TRUE to enable pos/semi-pos (#545)
    definite Hessians in qp solver.

--- modules/planning/math/smoothing_spline/piecewise_linear_generator.cc
+++ modules/planning/math/smoothing_spline/piecewise_linear_generator.cc
@@ -67,7 +67,7 @@ bool PiecewiseLinearGenerator::Solve() {
       equality_constraint_boundary));
 
   qp_solver_->EnableCholeskyRefactorisation(1);
-  qp_solver_->set_pos_semi_definite_hessian();
+  qp_solver_->set_pos_definite_hessian();
 
   if (!qp_solver_->Solve()) {
     return false;
