29d195a094e301bddcbd14d75f1147af06ecbc74
kechxu <kechxu89@gmail.com>
2017-10-05 02:15:51 -0700

    Prediction: wrap up the top-level method. Remaining (1) sl convert to xy, (2) sampling times, (3) cost

--- modules/prediction/predictor/move_sequence/move_sequence_predictor.cc
+++ modules/prediction/predictor/move_sequence/move_sequence_predictor.cc
@@ -160,17 +160,108 @@ void MoveSequencePredictor::DrawManeuverTrajectoryPoints(
     const LaneSequence& lane_sequence,
     const double total_time, const double freq,
     std::vector<TrajectoryPoint>* points) {
-  // longitudinal polynomial
 
-  // lateral polynomial
+  const Feature& feature = obstacle.latest_feature();
+  if (!feature.has_position() || !feature.has_velocity() ||
+      !feature.position().has_x() || !feature.position().has_y()) {
+    AERROR << "Obstacle [" << obstacle.id()
+           << " is missing position or velocity";
+    return;
+  }
 
-  // for t 0 -> prediction_total_time
-  //     get lateral l
-  //     get longitudinal s
-  //     convert (s, l) to (x, y)
-  //     store (x, y) in points
+  // TODO(kechxu) take care of enable_kf_tracking
+  Eigen::Vector2d position(feature.position().x(), feature.position().y());
+  double time_to_lane_center = ComputeTimeToLaneCenter(obstacle, lane_sequence);
 
-  // extent the points along reference line if needed
+  std::array<double, 6> lateral_coeffs;
+  std::array<double, 5> longitudinal_coeffs;
+  GetLateralPolynomial(obstacle, lane_sequence, time_to_lane_center,
+      &lateral_coeffs);
+  GetLongitudinalPolynomial(obstacle, lane_sequence, time_to_lane_center,
+      &longitudinal_coeffs);
+
+  int lane_segment_index = 0;
+  std::string lane_id =
+      lane_sequence.lane_segment(lane_segment_index).lane_id();
+
+  PredictionMap* map = PredictionMap::instance();
+  std::shared_ptr<const LaneInfo> lane_info = map->LaneById(lane_id);
+  double lane_s = 0.0;
+  double lane_l = 0.0;
+  if (!map->GetProjection(position, lane_info, &lane_s, &lane_l)) {
+    AERROR << "Failed in getting lane s and lane l";
+    return;
+  }
+
+  size_t total_num = static_cast<size_t>(total_time / freq);
+  // size_t num_to_center = static_cast<size_t>(time_to_lane_center / freq);
+  for (size_t i = 0; i < total_num; ++i) {
+    double relative_time = static_cast<double>(i) * freq;
+    Eigen::Vector2d point;
+    double theta = M_PI;
+    // TODO(kechxu) take care of the situation i > num_to_center
+    lane_l = EvaluateLateralPolynomial(lateral_coeffs, relative_time, 0);
+    double curr_s =
+        EvaluateLongitudinalPolynomial(longitudinal_coeffs, relative_time, 0);
+    double prev_s = (i > 0) ?
+        EvaluateLongitudinalPolynomial(longitudinal_coeffs,
+            relative_time - freq, 0) : 0.0;
+    lane_s += (curr_s - prev_s);
+
+    if (!map->SmoothPointFromLane(lane_id, lane_s, lane_l, &point, &theta)) {
+      AERROR << "Unable to get smooth point from lane [" << lane_id
+             << "] with s [" << lane_s << "] and l [" << lane_l << "]";
+      break;
+    }
+
+    if (points->size() > 0) {
+      PathPoint* prev_point = points->back().mutable_path_point();
+      double x_diff = point.x() - prev_point->x();
+      double y_diff = point.y() - prev_point->y();
+      if (std::fabs(x_diff) > std::numeric_limits<double>::epsilon() ||
+          std::fabs(y_diff) > std::numeric_limits<double>::epsilon()) {
+        theta = std::atan2(y_diff, x_diff);
+        prev_point->set_theta(theta);
+      } else {
+        theta = prev_point->theta();
+      }
+    }
+
+    double vs = EvaluateLongitudinalPolynomial(longitudinal_coeffs,
+                    relative_time, 1);
+    double vl = EvaluateLateralPolynomial(lateral_coeffs, relative_time, 1);
+    double lane_speed = std::hypot(vs, vl);
+
+    double as = EvaluateLongitudinalPolynomial(longitudinal_coeffs,
+                    relative_time, 2);
+    double al = EvaluateLateralPolynomial(lateral_coeffs, relative_time, 2);
+    double lane_acc = std::hypot(as, al);
+
+    TrajectoryPoint trajectory_point;
+    PathPoint path_point;
+    path_point.set_x(point.x());
+    path_point.set_y(point.y());
+    path_point.set_z(0.0);
+    path_point.set_theta(theta);
+    trajectory_point.mutable_path_point()->CopyFrom(path_point);
+    trajectory_point.set_v(lane_speed);
+    trajectory_point.set_a(lane_acc);
+    trajectory_point.set_relative_time(relative_time);
+    points->emplace_back(std::move(trajectory_point));
+
+    while (lane_s > map->LaneById(lane_id)->total_length() &&
+           lane_segment_index + 1 < lane_sequence.lane_segment_size()) {
+      lane_segment_index += 1;
+      lane_s = lane_s - map->LaneById(lane_id)->total_length();
+      lane_id = lane_sequence.lane_segment(lane_segment_index).lane_id();
+    }
+  }
+}
+
+bool ConvertSLToXY(const LaneSequence& lane_sequence,
+    const double s, const double l, double* x, double* y) {
+  // TODO(kechxu) implement
+  return true;
 }
 
 void MoveSequencePredictor::GetLongitudinalPolynomial(
@@ -262,6 +353,60 @@ void MoveSequencePredictor::GetLateralPolynomial(
   coefficients->operator[](5) = (6.0 * c0 - 3.0 * c1 + 0.5 * c2) / p2;
 }
 
+double MoveSequencePredictor::EvaluateLateralPolynomial(
+    const std::array<double, 6>& coeffs, const double t, const uint32_t order) {
+  switch (order) {
+    case 0: {
+      return ((((coeffs[5] * t + coeffs[4]) * t + coeffs[3]) * t +
+          coeffs[2]) * t + coeffs[1]) * t + coeffs[0];
+    }
+    case 1: {
+      return (((5.0 * coeffs[5] * t + 4.0 * coeffs[4]) * t +
+          3.0 * coeffs[3]) * t + 2.0 * coeffs[2]) * t + coeffs[1];
+    }
+    case 2: {
+      return (((20.0 * coeffs[5] * t + 12.0 * coeffs[4]) * t) +
+          6.0 * coeffs[3]) * t + 2.0 * coeffs[2];
+    }
+    case 3: {
+      return (60.0 * coeffs[5] * t + 24.0 * coeffs[4]) * t + 6.0 * coeffs[3];
+    }
+    case 4: {
+      return 120.0 * coeffs[5] * t + 24.0 * coeffs[4];
+    }
+    case 5: {
+      return 120.0 * coeffs[5];
+    }
+    default:
+      return 0.0;
+  }
+}
+
+double MoveSequencePredictor::EvaluateLongitudinalPolynomial(
+    const std::array<double, 5>& coeffs, const double t, const uint32_t order) {
+  switch (order) {
+    case 0: {
+      return (((coeffs[4] * t + coeffs[3]) * t + coeffs[2]) * t +
+          coeffs[1]) * t + coeffs[0];
+    }
+    case 1: {
+      return ((4.0 * coeffs[4] * t + 3.0 * coeffs[3]) * t +
+          2.0 * coeffs[2]) * t + coeffs[1];
+    }
+    case 2: {
+      return (12.0 * coeffs[4] * t + 6.0 * coeffs[3]) * t + 2.0 * coeffs[2];
+    }
+    case 3: {
+      return 24.0 * coeffs[4] * t + 6.0 * coeffs[3];
+    }
+    case 4: {
+      return 24.0 * coeffs[4];
+    }
+    default:
+      return 0.0;
+  }
+}
+
 void MoveSequencePredictor::DrawMotionTrajectoryPoints(
     const Obstacle& obstacle,
     const double total_time, const double freq,
@@ -315,11 +460,40 @@ void MoveSequencePredictor::DrawMotionTrajectoryPoints(
   }
 }
 
+double MoveSequencePredictor::ComputeTimeToLaneCenter(
+    const Obstacle& obstacle, const LaneSequence& lane_sequence) {
+  std::vector<double> candidate_times;
+  GenerateCandidateTimes(&candidate_times);
+  if (candidate_times.empty()) {
+    AWARN << "No candidate times found, use default value.";
+    return 2.0;
+  }
+  double t_best = candidate_times[0];
+  double cost_min = std::numeric_limits<double>::max();
+  for (double t : candidate_times) {
+    std::array<double, 6> lateral_coeffs;
+    std::array<double, 5> longitudinal_coeffs;
+    GetLateralPolynomial(obstacle, lane_sequence, t, &lateral_coeffs);
+    GetLongitudinalPolynomial(obstacle, lane_sequence, t, &longitudinal_coeffs);
+    double cost = Cost(t, lateral_coeffs, longitudinal_coeffs);
+    if (cost < cost_min) {
+      t_best = t;
+      cost_min = cost;
+    }
+  }
+  return t_best;
+}
+
 double MoveSequencePredictor::Cost(const double t,
-    const std::array<double, COEFF_SIZE>& coeffs,
-    const double alpha) {
+    const std::array<double, 6>& lateral_coeffs,
+    const std::array<double, 5>& longitudinal_coeffs) {
   // TODO(kechxu) implement
   return 0.0;
+}
+
+void MoveSequencePredictor::GenerateCandidateTimes(
+    std::vector<double>* candidate_times) {
+  // TODO(kechxu) implement
 }
 
 double MoveSequencePredictor::MotionWeight(const double t) {
