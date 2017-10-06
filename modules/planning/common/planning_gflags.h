7a3e45c7dafd74521cd5fc86cd8fec559343566d
Dong Li <lidong.main@gmail.com>
2017-10-05 22:27:49 -0700

    planning: added gflag to disable/eable trajectory stitcher

--- modules/planning/common/planning_gflags.h
+++ modules/planning/common/planning_gflags.h
@@ -30,6 +30,7 @@ DECLARE_double(look_forward_distance);
 DECLARE_bool(enable_smooth_reference_line);
 DECLARE_double(max_collision_distance);
 DECLARE_bool(publish_estop);
+DECLARE_bool(enable_trajectory_stitcher);
 
 DECLARE_int32(max_history_frame_num);
 
