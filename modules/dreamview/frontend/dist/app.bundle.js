84aa4aa0dcb1b1194f81dd2b94f2ec3d83b63454
siyangy <yu.siyang@yahoo.com>
2017-10-09 13:28:15 -0700

    Dreamview: update icon (#610)

--- modules/dreamview/frontend/dist/app.bundle.js
+++ modules/dreamview/frontend/dist/app.bundle.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:391d0bdd8cb5ce3bfab4fb4fccef6cea3a23fdb76c1d0c157d3f890fc4f604e5
-size 1007221
+oid sha256:2673cacf1e300f203ad80b0e4c57e3a4e2ae1e5bd668f2944c15d68985bb918b
+size 1007304
