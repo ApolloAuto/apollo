47a7c35af26c889bcd02af843cd5c58a463854e7
Zhang Liangliang <zhangliangliang@baidu.com>
2017-10-09 12:18:33 -0700

    Planning: expanded reference line smooth range to avoid oscillations.

--- modules/planning/integration_tests/garage_test.cc
+++ modules/planning/integration_tests/garage_test.cc
@@ -90,7 +90,7 @@ TEST_F(GarageTest, out_of_map) {
   FLAGS_test_chassis_file = "out_of_map_chassis.pb.txt";
   PlanningTestBase::SetUp();
   bool run_planning_success = RunPlanning("out_of_map", 0);
-  EXPECT_FALSE(run_planning_success);
+  RUN_GOLDEN_TEST;
 }
 
 /*
@@ -103,7 +103,7 @@ TEST_F(GarageTest, stop_over_line) {
   FLAGS_test_chassis_file = seq_num + "_chassis.pb.txt";
   PlanningTestBase::SetUp();
   bool run_planning_success = RunPlanning("stop_over_line", 0);
-  EXPECT_FALSE(run_planning_success);
+  RUN_GOLDEN_TEST;
 }
 
 }  // namespace planning
