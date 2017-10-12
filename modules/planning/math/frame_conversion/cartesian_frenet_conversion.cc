4a4d6aee0de068533e0d9710f0b8ef28d89a9bf9
Zhang Liangliang <zhangliangliang@baidu.com>
2017-10-12 14:14:29 -0700

    Planning: fixed a bug when calculating derivative of kappa in curve_math.

--- modules/planning/math/frame_conversion/cartesian_frenet_conversion.cc
+++ modules/planning/math/frame_conversion/cartesian_frenet_conversion.cc
@@ -36,41 +36,41 @@ void CartesianFrenetConverter::cartesian_to_frenet(
     const double v, const double a, const double theta, const double kappa,
     std::array<double, 3>* const ptr_s_condition,
     std::array<double, 3>* const ptr_d_condition) {
-  double dx = x - rx;
-  double dy = y - ry;
+  const double dx = x - rx;
+  const double dy = y - ry;
 
-  double cos_theta_r = std::cos(rtheta);
-  double sin_theta_r = std::sin(rtheta);
+  const double cos_theta_r = std::cos(rtheta);
+  const double sin_theta_r = std::sin(rtheta);
 
-  double cross_rd_nd = cos_theta_r * dy - sin_theta_r * dx;
-  (*ptr_d_condition)[0] =
+  const double cross_rd_nd = cos_theta_r * dy - sin_theta_r * dx;
+  ptr_d_condition->at(0) =
       std::copysign(std::sqrt(dx * dx + dy * dy), cross_rd_nd);
 
-  double delta_theta = theta - rtheta;
-  double tan_delta_theta = std::tan(delta_theta);
-  double cos_delta_theta = std::cos(delta_theta);
+  const double delta_theta = theta - rtheta;
+  const double tan_delta_theta = std::tan(delta_theta);
+  const double cos_delta_theta = std::cos(delta_theta);
 
-  double one_minus_kappa_r_d = 1 - rkappa * (*ptr_d_condition)[0];
-  (*ptr_d_condition)[1] = one_minus_kappa_r_d * tan_delta_theta;
+  const double one_minus_kappa_r_d = 1 - rkappa * ptr_d_condition->at(0);
+  ptr_d_condition->at(1) = one_minus_kappa_r_d * tan_delta_theta;
 
-  double kappa_r_d_prime =
-      rdkappa * (*ptr_d_condition)[0] + rkappa * (*ptr_d_condition)[1];
+  const double kappa_r_d_prime =
+      rdkappa * ptr_d_condition->at(0) + rkappa * ptr_d_condition->at(1);
 
-  (*ptr_d_condition)[2] =
+  ptr_d_condition->at(2) =
       -kappa_r_d_prime * tan_delta_theta +
       one_minus_kappa_r_d / cos_delta_theta / cos_delta_theta *
           (kappa * one_minus_kappa_r_d / cos_delta_theta - rkappa);
 
-  (*ptr_s_condition)[0] = rs;
+  ptr_s_condition->at(0) = rs;
 
-  (*ptr_s_condition)[1] = v * cos_delta_theta / one_minus_kappa_r_d;
+  ptr_s_condition->at(1) = v * cos_delta_theta / one_minus_kappa_r_d;
 
-  double delta_theta_prime =
+  const double delta_theta_prime =
       one_minus_kappa_r_d / cos_delta_theta * kappa - rkappa;
-  (*ptr_s_condition)[2] =
+  ptr_s_condition->at(2) =
       (a * cos_delta_theta -
-       (*ptr_s_condition)[1] * (*ptr_s_condition)[1] *
-           ((*ptr_d_condition)[1] * delta_theta_prime - kappa_r_d_prime)) /
+       ptr_s_condition->at(1) * ptr_s_condition->at(1) *
+           (ptr_d_condition->at(1) * delta_theta_prime - kappa_r_d_prime)) /
       one_minus_kappa_r_d;
   return;
 }
@@ -85,39 +85,39 @@ void CartesianFrenetConverter::frenet_to_cartesian(
   CHECK(std::abs(rs - s_condition[0]) < 1.0e-6 &&
         "The reference point s and s_condition[0] don't match");
 
-  double cos_theta_r = std::cos(rtheta);
-  double sin_theta_r = std::sin(rtheta);
+  const double cos_theta_r = std::cos(rtheta);
+  const double sin_theta_r = std::sin(rtheta);
 
   *ptr_x = rx - sin_theta_r * d_condition[0];
   *ptr_y = ry + cos_theta_r * d_condition[0];
 
-  double one_minus_kappa_r_d = 1 - rkappa * d_condition[0];
+  const double one_minus_kappa_r_d = 1 - rkappa * d_condition[0];
 
-  double tan_delta_theta = d_condition[1] / one_minus_kappa_r_d;
-  double delta_theta = std::atan2(d_condition[1], one_minus_kappa_r_d);
-  double cos_delta_theta = std::cos(delta_theta);
+  const double tan_delta_theta = d_condition[1] / one_minus_kappa_r_d;
+  const double delta_theta = std::atan2(d_condition[1], one_minus_kappa_r_d);
+  const double cos_delta_theta = std::cos(delta_theta);
 
   *ptr_theta = common::math::NormalizeAngle(delta_theta + rtheta);
 
-  double kappa_r_d_prime = rdkappa * d_condition[0] + rkappa * d_condition[1];
+  const double kappa_r_d_prime =
+      rdkappa * d_condition[0] + rkappa * d_condition[1];
   *ptr_kappa = (((d_condition[2] + kappa_r_d_prime * tan_delta_theta) *
                  cos_delta_theta * cos_delta_theta) /
                     (one_minus_kappa_r_d) +
                 rkappa) *
                cos_delta_theta / (one_minus_kappa_r_d);
 
-  double d_dot = d_condition[1] * s_condition[1];
+  const double d_dot = d_condition[1] * s_condition[1];
   *ptr_v = std::sqrt(one_minus_kappa_r_d * one_minus_kappa_r_d *
                          s_condition[1] * s_condition[1] +
                      d_dot * d_dot);
 
-  double delta_theta_prime =
+  const double delta_theta_prime =
       one_minus_kappa_r_d / cos_delta_theta * (*ptr_kappa) - rkappa;
 
   *ptr_a = s_condition[2] * one_minus_kappa_r_d / cos_delta_theta +
            s_condition[1] * s_condition[1] / cos_delta_theta *
                (d_condition[1] * delta_theta_prime - kappa_r_d_prime);
-  return;
 }
 
 double CartesianFrenetConverter::CalculateTheta(const double rtheta,
@@ -145,8 +145,8 @@ double CartesianFrenetConverter::CalculateKappa(const double rkappa,
 Vec2d CartesianFrenetConverter::CalculateCartesianPoint(const double rtheta,
                                                         const Vec2d& rpoint,
                                                         const double l) {
-  double x = rpoint.x() - l * std::sin(rtheta);
-  double y = rpoint.y() + l * std::cos(rtheta);
+  const double x = rpoint.x() - l * std::sin(rtheta);
+  const double y = rpoint.y() + l * std::cos(rtheta);
   return Vec2d(x, y);
 }
 
@@ -162,9 +162,9 @@ double CartesianFrenetConverter::CalculateSecondOrderLateralDerivative(
   const double dl = CalculateLateralDerivative(rtheta, theta, l, rkappa);
   const double theta_diff = theta - rtheta;
   const double cos_theta_diff = std::cos(theta_diff);
-  double res = -(rdkappa * l + rkappa * dl) * std::tan(theta - rtheta) +
-               (1 - rkappa * l) / (cos_theta_diff * cos_theta_diff) *
-                   (kappa * (1 - rkappa * l) / cos_theta_diff - rkappa);
+  const double res = -(rdkappa * l + rkappa * dl) * std::tan(theta - rtheta) +
+                     (1 - rkappa * l) / (cos_theta_diff * cos_theta_diff) *
+                         (kappa * (1 - rkappa * l) / cos_theta_diff - rkappa);
   if (std::isinf(res)) {
     AWARN << "result is inf when calculate second order lateral "
              "derivative. input values are rtheta:"
