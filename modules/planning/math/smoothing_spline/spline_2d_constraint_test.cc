1397ee40a3000a5ce2a325834afeedc5eda88284
Zhang Liangliang <zhangliangliang@baidu.com>
2017-10-12 11:27:57 -0700

    Planning: Added add boundary with multiple splines test for 2d spline

--- modules/planning/math/smoothing_spline/spline_2d_constraint_test.cc
+++ modules/planning/math/smoothing_spline/spline_2d_constraint_test.cc
@@ -110,5 +110,47 @@ TEST(Spline2dConstraint, add_boundary_02) {
   }
 }
 
+// test add boundary with multiple splines
+TEST(Spline2dConstraint, add_boundary_03) {
+  std::vector<double> x_knots = {0.0, 1.0, 2.0};
+  int32_t spline_order = 4;
+  Spline2dConstraint constraint(x_knots, spline_order);
+
+  std::vector<double> t_coord = {1.0};
+  std::vector<double> angle = {0.2};
+  std::vector<Vec2d> ref_point;
+  ref_point.emplace_back(Vec2d(0.0, 0.0));
+  std::vector<double> longitidinal_bound = {1.0};
+  std::vector<double> lateral_bound = {2.0};
+
+  constraint.Add2dBoundary(t_coord, angle, ref_point, longitidinal_bound,
+                           lateral_bound);
+  const auto mat = constraint.inequality_constraint().constraint_matrix();
+  const auto boundary =
+      constraint.inequality_constraint().constraint_boundary();
+
+  // clang-format off
+  Eigen::MatrixXd ref_mat = Eigen::MatrixXd::Zero(4, 16);
+  ref_mat <<
+        0,         0,         0,         0,         0,         0,         0,         0, -0.198669,        -0,        -0,        -0,  0.980067,         0,         0,         0, // NOLINT
+        0,         0,         0,         0,         0,         0,         0,         0,  0.198669,         0,         0,         0, -0.980067,        -0,        -0,        -0, // NOLINT
+        0,         0,         0,         0,         0,         0,         0,         0,  0.980067,         0,         0,         0,  0.198669,         0,         0,         0, // NOLINT
+        0,         0,         0,         0,         0,         0,         0,         0, -0.980067,        -0,        -0,        -0, -0.198669,        -0,        -0,        -0; // NOLINT
+  // clang-format on
+
+  for (int i = 0; i < mat.rows(); ++i) {
+    for (int j = 0; j < mat.cols(); ++j) {
+      EXPECT_NEAR(mat(i, j), ref_mat(i, j), 1e-6);
+    }
+  }
+
+  Eigen::MatrixXd ref_boundary = Eigen::MatrixXd::Zero(4, 1);
+  ref_boundary << -1.0, -1.0, -2.0, -2.0;
+
+  for (int i = 0; i < ref_boundary.rows(); ++i) {
+    EXPECT_NEAR(boundary(i, 0), ref_boundary(i, 0), 1e-5);
+  }
+}
+
 }  // namespace planning
 }  // namespace apollo
