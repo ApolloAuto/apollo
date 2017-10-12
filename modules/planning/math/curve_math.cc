1ba90631359cd3a010a646e7c8c0de67bcebc881
Liangliang Zhang <zhangliangliang@baidu.com>
2017-10-12 14:28:04 -0700

    Planning: fixed a bug when calculating derivative of kappa in curve_math. (#646)

--- modules/planning/math/curve_math.cc
+++ modules/planning/math/curve_math.cc
@@ -24,12 +24,12 @@
 namespace apollo {
 namespace planning {
 
+// kappa = (dx * d2y - dy * d2x) / [(dx * dx + dy * dy)^(3/2)]
 double CurveMath::ComputeCurvature(const double dx, const double d2x,
                                    const double dy, const double d2y) {
-  double a = dx * d2y - dy * d2x;
-  double dx_dy_norm_square = dx * dx + dy * dy;
-  double dx_dy_norm = std::sqrt(dx_dy_norm_square);
-  double b = dx_dy_norm_square * dx_dy_norm;
+  const double a = dx * d2y - dy * d2x;
+  constexpr double kOrder = 1.5;
+  const double b = std::pow(dx * dx + dy * dy, kOrder);
   return a / b;
 }
 
@@ -37,18 +37,12 @@ double CurveMath::ComputeCurvatureDerivative(const double dx, const double d2x,
                                              const double d3x, const double dy,
                                              const double d2y,
                                              const double d3y) {
-  double a = dx * d2y - dy * d2x;
-  double dx_dy_norm_square = dx * dx + dy * dy;
-  double dx_dy_norm = std::sqrt(dx_dy_norm_square);
-  double b = dx_dy_norm_square * dx_dy_norm;
+  const double a = dx * d2y - dy * d2x;
+  const double b = dx * d3y - dy * d3x;
+  const double c = dx * d2x + dy * d2y;
+  const double d = dx * dx + dy * dy;
 
-  double a_dot = dx * d3y - dy * d3x;
-  double b_dot = 3.0 * dx_dy_norm * (dx * d2x + dy * d2y);
-
-  double dkappa =
-      (a_dot * b - a * b_dot) /
-      (dx_dy_norm_square * dx_dy_norm_square * dx_dy_norm_square * dx_dy_norm);
-  return dkappa;
+  return (b * d - 3.0 * a * c) / std::pow(d, 3.0);
 }
 
 }  // namespace planning
