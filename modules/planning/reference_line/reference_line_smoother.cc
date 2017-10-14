3768a19f03e34062dd2fb166f9e4053897e08fa0
Liangliang Zhang <zhangliangliang@baidu.com>
2017-10-13 22:14:59 -0700

    Planning: added three level protection for speed optimization. (#672)

--- modules/planning/reference_line/reference_line_smoother.cc
+++ modules/planning/reference_line/reference_line_smoother.cc
@@ -153,9 +153,6 @@ bool ReferenceLineSmoother::Sampling(const ReferenceLine& raw_reference_line) {
 bool ReferenceLineSmoother::ApplyConstraint(
     const ReferenceLine& raw_reference_line) {
   uint32_t constraint_num = 3 * (t_knots_.size() - 1) + 1;
-  //  std::max(static_cast<uint32_t>(raw_reference_line.Length() /
-  //                                 smoother_config_.max_constraint_length()),
-  //           3u);
 
   std::vector<double> evaluated_t;
   common::util::uniform_slice(t_knots_.front(), t_knots_.back(),
