d845b0f9eac0d73f65cc1a385c8229162cb505a8
Jiangtao Hu <boris.hu@gmail.com>
2017-10-14 10:54:01 -0700

    planning: fix QP path order const with config value.

--- modules/planning/tasks/qp_spline_path/qp_spline_path_optimizer.cc
+++ modules/planning/tasks/qp_spline_path/qp_spline_path_optimizer.cc
@@ -36,7 +36,8 @@ QpSplinePathOptimizer::QpSplinePathOptimizer()
 bool QpSplinePathOptimizer::Init(const PlanningConfig& config) {
   qp_spline_path_config_ = config.em_planner_config().qp_spline_path_config();
   std::vector<double> init_knots;
-  spline_generator_.reset(new Spline1dGenerator(init_knots, 5));
+  spline_generator_.reset(new Spline1dGenerator(init_knots,
+      qp_spline_path_config_.spline_order()));
   is_init_ = true;
   return true;
 }
