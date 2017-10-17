e319d44f9f207d686e2609fe0388541f92c8ce53
Rongqi Qiu <rongqi.qiu@gmail.com>
2017-10-17 16:46:32 -0700

    add third_party_perception module

---
+++ modules/third_party_perception/common/third_party_perception_gflags.h
@@ -0,0 +1,60 @@
+/******************************************************************************
+ * Copyright 2017 The Apollo Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *****************************************************************************/
+
+#ifndef MODULES_THIRD_PARTY_PERCEPTION_THIRD_PARTY_PERCEPTION_GFLAGS_H_
+#define MODULES_THIRD_PARTY_PERCEPTION_THIRD_PARTY_PERCEPTION_GFLAGS_H_
+
+#include "gflags/gflags.h"
+
+DECLARE_string(node_namespace);
+DECLARE_string(node_name);
+DECLARE_string(hmi_name);
+
+DECLARE_string(adapter_config_filename);
+
+DECLARE_double(third_party_perception_freq);
+DECLARE_bool(enable_mobileye);
+DECLARE_bool(enable_delphi_esr);
+
+// flags to calibrate mobileye, radar and localization
+DECLARE_double(mobileye_pos_adjust);
+DECLARE_double(delphi_esr_pos_adjust);
+
+// object id offset
+DECLARE_int32(mobileye_id_offset);
+DECLARE_int32(delphi_esr_id_offset);
+
+// flags to create fake bounding box
+DECLARE_double(default_car_length);
+DECLARE_double(default_truck_length);
+DECLARE_double(default_bike_length);
+DECLARE_double(default_ped_length);
+DECLARE_double(default_unknown_length);
+DECLARE_double(default_car_width);
+DECLARE_double(default_truck_width);
+DECLARE_double(default_bike_width);
+DECLARE_double(default_ped_width);
+DECLARE_double(default_unknown_width);
+DECLARE_double(default_height);
+
+// flags to filter radar obstacles
+DECLARE_double(filter_y_distance);
+DECLARE_double(movable_speed_threshold);
+DECLARE_double(movable_heading_threshold);
+DECLARE_int32(movable_frames_count_threshold);
+DECLARE_int32(keep_delphi_esr_frames);
+
+#endif
---
+++ modules/third_party_perception/conversion.h
@@ -0,0 +1,60 @@
+/******************************************************************************
+ * Copyright 2017 The Apollo Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *****************************************************************************/
+
+/**
+ * @file
+ */
+
+#ifndef MODEULES_THIRD_PARTY_PERCEPTION_CONVERSION_H_
+#define MODEULES_THIRD_PARTY_PERCEPTION_CONVERSION_H_
+
+#include <queue>
+
+#include "modules/drivers/proto/delphi_esr.pb.h"
+#include "modules/drivers/proto/mobileye.pb.h"
+#include "modules/localization/proto/localization.pb.h"
+#include "modules/perception/proto/perception_obstacle.pb.h"
+#include "modules/third_party_perception/proto/radar_obstacle.pb.h"
+
+/**
+ * @namespace apollo::third_party_perception::conversion
+ * @brief apollo::third_party_perception
+ */
+namespace apollo {
+namespace third_party_perception {
+namespace conversion {
+
+using ::apollo::perception::PerceptionObstacles;
+using ::apollo::drivers::Mobileye;
+using ::apollo::drivers::DelphiESR;
+using ::apollo::localization::LocalizationEstimate;
+using ::apollo::third_party_perception::RadarObstacles;
+
+PerceptionObstacles MobileyeToPerceptionObstacles(
+    const Mobileye& mobileye, const LocalizationEstimate& localization);
+
+RadarObstacles DelphiToRadarObstacles(
+    const DelphiESR& delphi_esr, const LocalizationEstimate& localization,
+    const RadarObstacles& last_radar_obstacles);
+
+PerceptionObstacles RadarObstaclesToPerceptionObstacles(
+    const RadarObstacles& radar_obstacles);
+
+}  // namespace conversion
+}  // namespace third_party_perception
+}  // namespace apollo
+
+#endif  // MODULES_THIRD_PARTY_PERCEPTION_FUSION_H_
