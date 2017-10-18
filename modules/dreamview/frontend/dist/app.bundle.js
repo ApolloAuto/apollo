15ad1d0baa42b439541841c2434d7cc32bed9e1a
Vivian Lin <linvivian@baidu.com>
2017-10-17 17:01:58 -0700

    Dreamview: upgraded react to 16.0.0 (#708)

--- modules/dreamview/frontend/dist/app.bundle.js
+++ modules/dreamview/frontend/dist/app.bundle.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b335eda5be6d38b8ef0f17c1393dc2924e6de298cc23d5ee9be7df47b22b0a72
-size 1533379
+oid sha256:76acaba1b60d0b613e9a48e888a871e0ccf1fb3759267a51849117fa8ef4f41b
+size 1503206
