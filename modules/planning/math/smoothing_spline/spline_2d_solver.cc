b784ca24c7d0df29adc92f67595c4b5bf82bc19c
Zhang Liangliang <zhangliangliang@baidu.com>
2017-10-19 10:48:56 -0700

    Planning: used proper options for qp solver.

--- modules/planning/math/smoothing_spline/spline_2d_solver.cc
+++ modules/planning/math/smoothing_spline/spline_2d_solver.cc
@@ -93,12 +93,7 @@ bool Spline2dSolver::Solve() {
     sqp_solver_.reset(new ::qpOASES::SQProblem(num_param, num_constraint,
                                                ::qpOASES::HST_SEMIDEF));
     ::qpOASES::Options my_options;
-    my_options.enableCholeskyRefactorisation = 1;
-    my_options.enableRegularisation = ::qpOASES::BT_TRUE;
-    my_options.epsNum = FLAGS_default_active_set_eps_num;
-    my_options.epsDen = FLAGS_default_active_set_eps_den;
-    my_options.epsIterRef = FLAGS_default_active_set_eps_iter_ref;
-    my_options.terminationTolerance = 1.0e-4;
+    my_options.setToReliable();
     sqp_solver_->setOptions(my_options);
     if (!FLAGS_default_enable_active_set_debug_info) {
       sqp_solver_->setPrintLevel(qpOASES::PL_NONE);
