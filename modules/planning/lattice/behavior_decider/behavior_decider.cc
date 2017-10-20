3511dbb68169e6794a43dd8b96e5e0a71dd93f00
YajiaZhang <yajia.zhang@gmail.com>
2017-10-20 15:47:56 -0700

    planning: formated code style for lattice planner

--- modules/planning/lattice/behavior_decider/behavior_decider.cc
+++ modules/planning/lattice/behavior_decider/behavior_decider.cc
@@ -28,67 +28,75 @@
 #include "modules/planning/lattice/lattice_util.h"
 #include "modules/common/log.h"
 
-namespace apollo{
+namespace apollo {
 namespace planning {
 
-BehaviorDecider::BehaviorDecider() {
-}
+BehaviorDecider::BehaviorDecider() {}
 
-PlanningObject BehaviorDecider::analyze(Frame* frame,
-  const common::TrajectoryPoint& init_planning_point,
-  const std::array<double, 3>& lon_init_state,
-  std::vector<ReferenceLine>& candidate_reference_lines) {
+PlanningObject BehaviorDecider::analyze(
+    Frame* frame, const common::TrajectoryPoint& init_planning_point,
+    const std::array<double, 3>& lon_init_state,
+    const std::vector<ReferenceLine>& candidate_reference_lines) {
   PlanningObject ret;
   CHECK(frame != nullptr);
   // Only handles one reference line
-  CHECK(candidate_reference_lines.size() > 0);
+  CHECK_GT(candidate_reference_lines.size(), 0);
   const ReferenceLine& ref_line = candidate_reference_lines[0];
   const std::vector<ReferencePoint>& ref_points = ref_line.reference_points();
 
   std::vector<common::PathPoint> discretized_ref_points =
-    apollo::planning::ToDiscretizedReferenceLine(ref_points);
+      apollo::planning::ToDiscretizedReferenceLine(ref_points);
 
   for (size_t i = 0; i < discretized_ref_points.size(); ++i) {
-    ret.mutable_discretized_reference_line()->add_discretized_reference_line_point()->CopyFrom(
-      discretized_ref_points[i]);
+    ret.mutable_discretized_reference_line()
+        ->add_discretized_reference_line_point()
+        ->CopyFrom(discretized_ref_points[i]);
   }
 
-  LatticeSamplingConfig* lattice_sampling_config = ret.mutable_lattice_sampling_config();
-  LonSampleConfig* lon_sample_config = lattice_sampling_config->mutable_lon_sample_config();
-  LatSampleConfig* lat_sample_config = lattice_sampling_config->mutable_lat_sample_config();
-  //lon_sample_config->mutable_lon_end_condition()->set_s(0.0);
-  lon_sample_config->mutable_lon_end_condition()->set_ds(FLAGS_default_cruise_speed);
+  LatticeSamplingConfig* lattice_sampling_config =
+      ret.mutable_lattice_sampling_config();
+  LonSampleConfig* lon_sample_config =
+      lattice_sampling_config->mutable_lon_sample_config();
+  LatSampleConfig* lat_sample_config =
+      lattice_sampling_config->mutable_lat_sample_config();
+  // lon_sample_config->mutable_lon_end_condition()->set_s(0.0);
+  lon_sample_config->mutable_lon_end_condition()->set_ds(
+      FLAGS_default_cruise_speed);
   lon_sample_config->mutable_lon_end_condition()->set_dds(0.0);
   ret.set_decision_type(PlanningObject::GO);
   return ret;
 }
 
-PlanningObject BehaviorDecider::analyze(Frame* frame,
-  const common::TrajectoryPoint& init_planning_point,
-  const std::array<double, 3>& lon_init_state,
-  const std::vector<common::PathPoint>& discretized_reference_line) {
+PlanningObject BehaviorDecider::analyze(
+    Frame* frame, const common::TrajectoryPoint& init_planning_point,
+    const std::array<double, 3>& lon_init_state,
+    const std::vector<common::PathPoint>& discretized_reference_line) {
   PlanningObject ret;
   CHECK(frame != nullptr);
   // Only handles one reference line
-  CHECK(discretized_reference_line.size() > 0);
+  CHECK_GT(discretized_reference_line.size(), 0);
 
   for (size_t i = 0; i < discretized_reference_line.size(); ++i) {
-    ret.mutable_discretized_reference_line()->add_discretized_reference_line_point()->CopyFrom(
-      discretized_reference_line[i]);
+    ret.mutable_discretized_reference_line()
+        ->add_discretized_reference_line_point()
+        ->CopyFrom(discretized_reference_line[i]);
   }
 
-  LatticeSamplingConfig* lattice_sampling_config = ret.mutable_lattice_sampling_config();
-  LonSampleConfig* lon_sample_config = lattice_sampling_config->mutable_lon_sample_config();
-  LatSampleConfig* lat_sample_config = lattice_sampling_config->mutable_lat_sample_config();
-  //lon_sample_config->mutable_lon_end_condition()->set_s(0.0);
-  lon_sample_config->mutable_lon_end_condition()->set_ds(FLAGS_default_cruise_speed);
+  LatticeSamplingConfig* lattice_sampling_config =
+      ret.mutable_lattice_sampling_config();
+  LonSampleConfig* lon_sample_config =
+      lattice_sampling_config->mutable_lon_sample_config();
+  LatSampleConfig* lat_sample_config =
+      lattice_sampling_config->mutable_lat_sample_config();
+  // lon_sample_config->mutable_lon_end_condition()->set_s(0.0);
+  lon_sample_config->mutable_lon_end_condition()->set_ds(
+      FLAGS_default_cruise_speed);
   lon_sample_config->mutable_lon_end_condition()->set_dds(0.0);
   ret.set_decision_type(PlanningObject::GO);
   return ret;
 }
 
-
-} // namespace planning
-} // namespace apollo
+}  // namespace planning
+}  // namespace apollo
 
 #endif /* MODULES_PLANNING_DECISION_ANALYZER_H */
