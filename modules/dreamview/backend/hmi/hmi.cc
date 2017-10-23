922bde17e63d3e144ec3b17806bad8bbe12c98ef
Aaron Xiao <xiaoxiangquan@gmail.com>
2017-10-23 11:57:40 -0700

    Dreamview: Broadcast HMIStatus to clients when updated.

--- modules/dreamview/backend/hmi/hmi.cc
+++ modules/dreamview/backend/hmi/hmi.cc
@@ -16,6 +16,9 @@
 
 #include "modules/dreamview/backend/hmi/hmi.h"
 
+#include "google/protobuf/util/json_util.h"
+#include "third_party/json/json.hpp"
+
 #include "modules/common/adapters/adapter_manager.h"
 #include "modules/common/util/util.h"
 #include "modules/dreamview/backend/common/dreamview_gflags.h"
@@ -24,6 +27,7 @@ namespace apollo {
 namespace dreamview {
 
 using apollo::common::adapter::AdapterManager;
+using Json = nlohmann::json;
 
 HMI::HMI(WebSocketHandler *websocket) : websocket_(websocket) {
   CHECK(common::util::GetProtoFromFile(FLAGS_hmi_config_filename, &config_))
@@ -45,7 +49,7 @@ void HMI::Start() {
 void HMI::OnHMIStatus(const HMIStatus &hmi_status) {
   // Note that we only merged status of modules and hardware, which matches the
   // hmi_status_helper.
-  for (const auto& new_status : hmi_status.modules()) {
+  for (const auto &new_status : hmi_status.modules()) {
     auto *old_status = GetModuleStatus(new_status.name());
     if (old_status) {
       old_status->MergeFrom(new_status);
@@ -53,7 +57,7 @@ void HMI::OnHMIStatus(const HMIStatus &hmi_status) {
       AERROR << "Updating HMIStatus, unknown module " << new_status.name();
     }
   }
-  for (const auto& new_status : hmi_status.hardware()) {
+  for (const auto &new_status : hmi_status.hardware()) {
     auto *old_status = GetHardwareStatus(new_status.name());
     if (old_status) {
       old_status->MergeFrom(new_status);
@@ -62,9 +66,23 @@ void HMI::OnHMIStatus(const HMIStatus &hmi_status) {
     }
   }
 
-  if (websocket_) {
-    // TODO(xiaoxq): Broadcast the new HMIStatus to clients through websocket.
+  BroadcastHMIStatus();
+}
+
+void HMI::BroadcastHMIStatus() const {
+  // In unit tests, we may leave websocket_ as NULL and skip broadcasting.
+  if (websocket_ == nullptr) {
+    return;
   }
+
+  // Broadcast jsonified HMIStatus to clients via websocket.
+  std::string status_json_string;
+  google::protobuf::util::MessageToJsonString(status_, &status_json_string);
+
+  Json status_json;
+  status_json["type"] = "HMIStatus";
+  status_json["data"] = Json::parse(status_json_string);
+  websocket_->BroadcastData(status_json.dump());
 }
 
 ModuleStatus* HMI::GetModuleStatus(const std::string &module_name) {
