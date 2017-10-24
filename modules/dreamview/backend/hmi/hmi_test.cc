43a4b5eda7e1486f03594fb0fa786a8d5c24be5f
Aaron Xiao <xiaoxiangquan@gmail.com>
2017-10-24 14:06:55 -0700

    Dreamview: Update HMI protos to leverage Map.

--- modules/dreamview/backend/hmi/hmi_test.cc
+++ modules/dreamview/backend/hmi/hmi_test.cc
@@ -21,7 +21,7 @@
 namespace apollo {
 namespace dreamview {
 
-using google::protobuf::RepeatedPtrField;
+using google::protobuf::Map;
 
 TEST(HMITest, UpdateHMIStatus) {
   HMI hmi(nullptr);
@@ -30,49 +30,42 @@ TEST(HMITest, UpdateHMIStatus) {
   EXPECT_EQ(hmi.status_.modules_size(), hmi.config_.modules_size());
 
   // The initial status should be UNINITIALIZED.
-  const auto &module_name = hmi.config_.modules(0).name();
-  EXPECT_EQ(ModuleStatus::UNINITIALIZED,
-            hmi.GetModuleStatus(module_name)->status());
+  const auto &module_status_iter = hmi.status_.modules().begin();
+  EXPECT_EQ(ModuleStatus::UNINITIALIZED, module_status_iter->second.status());
 
   // Set it to STARTED.
+  ModuleStatus new_module_status;
+  new_module_status.set_status(ModuleStatus::STARTED);
   HMIStatus new_status;
-  auto *module_status = new_status.add_modules();
-  module_status->set_name(module_name);
-  module_status->set_status(ModuleStatus::STARTED);
+  new_status.mutable_modules()->insert({module_status_iter->first,
+                                        new_module_status});
   hmi.OnHMIStatus(new_status);
 
-  EXPECT_EQ(ModuleStatus::STARTED, hmi.GetModuleStatus(module_name)->status());
+  EXPECT_EQ(ModuleStatus::STARTED, module_status_iter->second.status());
 }
 
 TEST(HMITest, ExecuteComponentCommand) {
-  RepeatedPtrField<Component> modules;
-  // Fail.
+  Map<std::string, Component> modules;
+  // Fail on not-exist component.
   EXPECT_NE(0, HMI::ExecuteComponentCommand(modules, "Module", "Any"));
 
-  auto *module = modules.Add();
-  module->set_name("Module");
+  auto *module_commands = modules.insert({"Module", {}}).first
+      ->second.mutable_supported_commands();
   {
-    auto *command = module->add_supported_commands();
-    command->set_name("SingleCommand");
-    *command->add_command() = "echo OK";
-    // Succeed.
+    // Succeed on single command.
+    module_commands->insert({"SingleCommand", "ls"});
     EXPECT_EQ(0, HMI::ExecuteComponentCommand(modules, "Module",
                                               "SingleCommand"));
   }
   {
-    auto *command = module->add_supported_commands();
-    command->set_name("ComplexCommand");
-    *command->add_command() = "ls";
-    *command->add_command() = "/dev/null";
-    // Succeed.
+    // Succeed on complex command.
+    module_commands->insert({"ComplexCommand", "ls /dev/null"});
     EXPECT_EQ(0, HMI::ExecuteComponentCommand(modules, "Module",
                                               "ComplexCommand"));
   }
   {
-    auto *command = module->add_supported_commands();
-    command->set_name("BadCommand");
-    *command->add_command() = "ls /dev/null/not_exist";
-    // Fail.
+    // Fail on bad command.
+    module_commands->insert({"BadCommand", "ls /dev/null/not_exist"});
     EXPECT_NE(0, HMI::ExecuteComponentCommand(modules, "Module", "BadCommand"));
   }
 }
