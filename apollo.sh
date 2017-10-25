c21eced843c314b4284d3196f27d96e0b1b80e0f
Jiangtao Hu <ycool@users.noreply.github.com>
2017-10-24 21:34:37 -0700

    script: build common/proto/gnss_status.proto py lib. (#824)

--- apollo.sh
+++ apollo.sh
@@ -165,7 +165,9 @@ function build_py_proto() {
   fi
   mkdir py_proto
   PROTOC='./bazel-out/host/bin/external/com_google_protobuf/protoc'
-  find modules/ -name "*.proto" | grep -v gnss | xargs ${PROTOC} --python_out=py_proto
+  find modules/ -name "*.proto" \
+      | grep -v modules/drivers/gnss \
+      | xargs ${PROTOC} --python_out=py_proto
   find py_proto/* -type d -exec touch "{}/__init__.py" \;
 }
 
