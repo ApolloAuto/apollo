3b7d9bd5aa329a643529895364773ed490d75dc5
jiangyifei <jiangyifei@gmail.com>
2017-10-27 17:14:04 -0700

    tools: updated plot mobileye

--- modules/tools/mobileye_viewer/plot_mobileye.py
+++ modules/tools/mobileye_viewer/plot_mobileye.py
@@ -20,27 +20,45 @@ import rospy
 import argparse
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
-
+from modules.planning.proto import planning_pb2
 from modules.drivers.proto import mobileye_pb2
 from mobileye_data import MobileyeData
+from localization_data import LocalizationData
+from planning_data import PlanningData
 from view_subplot import ViewSubplot
+from modules.localization.proto import localization_pb2
 
 mobileye = MobileyeData()
+localization = LocalizationData()
+planning = PlanningData()
 
 def update(frame_number):
-    view_subplot.show(mobileye)
+    view_subplot.show(mobileye, localization, planning)
 
+def localization_callback(localization_pb):
+    localization.update(localization_pb)
 
 def mobileye_callback(mobileye_pb):
     mobileye.update(mobileye_pb)
     mobileye.compute_lanes()
+    #mobileye.compute_next_lanes()
     mobileye.compute_obstacles()
+    planning.compute_path()
 
+def planning_callback(planning_pb):
+    planning.update(planning_pb)
 
 def add_listener():
     rospy.init_node('mobileye_plot', anonymous=True)
-    rospy.Subscriber('/apollo/sensor/mobileye', mobileye_pb2.Mobileye,
+    rospy.Subscriber('/apollo/sensor/mobileye',
+                     mobileye_pb2.Mobileye,
                      mobileye_callback)
+    rospy.Subscriber('/apollo/planning_lite',
+                     planning_pb2.ADCTrajectory,
+                     planning_callback)
+    rospy.Subscriber('/apollo/localization/pose',
+                     localization_pb2.LocalizationEstimate,
+                     localization_callback)
 
 
 if __name__ == '__main__':
@@ -48,11 +66,13 @@ if __name__ == '__main__':
     add_listener()
     fig = plt.figure()
 
-    ax = plt.subplot2grid((1, 1), (0, 0), rowspan=2, colspan=2)
+    ax = plt.subplot2grid((1, 2), (0, 0), rowspan=1, colspan=1)
 
     view_subplot = ViewSubplot(ax)
 
     ani = animation.FuncAnimation(fig, update, interval=100)
 
-    ax.axis('equal')
+    #ax.axis('equal')
+    ax.axvline(x=0.0, alpha=0.3)
+    ax.axhline(y=0.0, alpha=0.3)
     plt.show()
--- modules/tools/mobileye_viewer/view_subplot.py
+++ modules/tools/mobileye_viewer/view_subplot.py
@@ -15,6 +15,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 ###############################################################################
+import math
 
 class ViewSubplot:
 
@@ -27,30 +28,73 @@ class ViewSubplot:
             [0], [0], 'g-', lw=3, alpha=0.4)
         self.obstacles, =  ax.plot(
             [0], [0], 'r.', ms=20, alpha=0.8)
+        self.ref_lane, = ax.plot(
+            [0], [0], 'k--', lw=3, alpha=0.4)
+
+        self.speed_line, = ax.plot([0], [0], 'r-', lw=3, alpha=0.4)
+        self.acc_line, = ax.plot([0], [0], 'y-', lw=3, alpha=1)
+
         ax.set_xlim([-10, 10])
         ax.set_ylim([-10, 100])
         ax.relim()
         ax.set_xlabel("lat(m)")
+        self.next_lanes = []
+        for i in range(8):
+            lane, = ax.plot([0], [0], 'b-', lw=3, alpha=0.4)
+            self.next_lanes.append(lane)
+
         self.left_lane.set_visible(False)
         self.right_lane.set_visible(False)
+        self.ref_lane.set_visible(False)
 
-
-    def show(self, mobileye_data):
+    def show(self, mobileye_data, localization_data, planning_data):
         self.left_lane.set_visible(True)
         self.right_lane.set_visible(True)
+        self.ref_lane.set_visible(True)
 
         mobileye_data.lane_data_lock.acquire()
         self.right_lane.set_xdata(mobileye_data.right_lane_x)
         self.right_lane.set_ydata(mobileye_data.right_lane_y)
         self.left_lane.set_xdata(mobileye_data.left_lane_x)
         self.left_lane.set_ydata(mobileye_data.left_lane_y)
+        #self.ref_lane.set_xdata(mobileye_data.ref_lane_x)
+        #self.ref_lane.set_ydata(mobileye_data.ref_lane_y)
         mobileye_data.lane_data_lock.release()
-        #self.ax.autoscale_view()
-        #self.ax.relim()
+
+        self.ref_lane.set_xdata(planning_data.path_x)
+        self.ref_lane.set_ydata(planning_data.path_y)
 
         mobileye_data.obstacle_data_lock.acquire()
         self.obstacles.set_ydata(mobileye_data.obstacle_x)
         self.obstacles.set_xdata(mobileye_data.obstacle_y)
         mobileye_data.obstacle_data_lock.release()
 
+        mobileye_data.next_lane_data_lock.acquire()
+        #print range(len(mobileye_data.next_lanes_x))
+        for i in range(len(mobileye_data.next_lanes_x)):
+            if i >= len(self.next_lanes):
+                mobileye_data.next_lane_data_lock.release()
+                break
+            #print mobileye_data.next_lanes_x[i]
+            self.next_lanes[i].set_xdata(mobileye_data.next_lanes_x[i])
+            self.next_lanes[i].set_ydata(mobileye_data.next_lanes_y[i])
+        mobileye_data.next_lane_data_lock.release()
 
+        if localization_data.localization_pb is None:
+            return
+        speed_x = localization_data.localization_pb.pose.linear_velocity.x
+        speed_y = localization_data.localization_pb.pose.linear_velocity.y
+        acc_x = localization_data.localization_pb.pose.linear_acceleration.x
+        acc_y = localization_data.localization_pb.pose.linear_acceleration.y
+        heading = localization_data.localization_pb.pose.heading
+        #print heading
+        new_speed_x = math.cos(-heading + math.pi/2)*speed_x - math.sin(-heading+ math.pi/2)* speed_y
+        new_speed_y = math.sin(-heading+ math.pi/2)*speed_x + math.cos(-heading+ math.pi/2)* speed_y
+
+        new_acc_x = math.cos(-heading + math.pi/2) * acc_x - math.sin(-heading+ math.pi/2) * acc_y
+        new_acc_y = math.sin(-heading+ math.pi/2) * acc_x + math.cos(-heading+ math.pi/2) * acc_y
+
+        #self.speed_line.set_xdata([0, new_speed_x])
+        #self.speed_line.set_ydata([0, new_speed_y])
+        #self.acc_line.set_xdata([0, new_acc_x])
+        #self.acc_line.set_ydata([0, new_acc_y])
