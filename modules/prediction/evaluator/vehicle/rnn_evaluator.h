60c7ee25c95929bc30bc33bc06d28770e085f0d3
Calvin Miao <jhmiao@gmail.com>
2017-10-30 13:53:53 -0700

    Prediction: added RNN evaluator framework

---
+++ modules/prediction/evaluator/vehicle/rnn_evaluator.h
@@ -0,0 +1,67 @@
+/******************************************************************************
+ * Copyright 2017 The Apollo Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *****************************************************************************/
+
+#ifndef MODULES_PREDICTION_EVALUATOR_VEHICLE_RNN_EVALUATOR_H_
+#define MODULES_PREDICTION_EVALUATOR_VEHICLE_RNN_EVALUATOR_H_
+
+#include <memory>
+#include <string>
+
+#include "modules/prediction/container/obstacles/obstacle.h"
+#include "modules/prediction/evaluator/evaluator.h"
+#include "modules/prediction/proto/network_model.pb.h"
+
+namespace apollo {
+namespace prediction {
+
+class RNNEvaluator : public Evaluator {
+ public:
+  /**
+   * @brief Constructor
+   */
+  RNNEvaluator();
+
+  /**
+   * @brief Destructor
+   */
+  virtual ~RNNEvaluator() = default;
+
+  /**
+   * @brief Override Evaluate
+   * @param Obstacle pointer
+   */
+  void Evaluate(Obstacle* obstacle_ptr) override;
+
+  /**
+   * @brief Clear
+   */
+  void Clear();
+
+ private:
+  /**
+   * @brief Load mode file
+   * @param Model file name
+   */
+  void LoadModel(const std::string& model_file);
+
+ private:
+  std::unique_ptr<NetParameter> model_ptr_;
+};
+
+}  // namespace prediction
+}  // namespace apollo
+
+#endif  // MODULES_PREDICTION_EVALUATOR_VEHICLE_RNN_EVALUATOR_H_
