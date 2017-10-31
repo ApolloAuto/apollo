28883892a3632a02835eab4c034f9330541c943f
unacao <yufcao@gmail.com>
2017-10-31 12:01:52 -0700

    allow multiple POI as default routing end point

--- modules/dreamview/frontend/src/components/RouteEditingBar/index.js
+++ modules/dreamview/frontend/src/components/RouteEditingBar/index.js
@@ -1,12 +1,12 @@
 import React from "react";
-import { inject } from "mobx-react";
+import { inject, observer } from "mobx-react";
 
 import Image from "components/common/Image";
 import logoApollo from "assets/images/logo_apollo.png";
 import EditingPanel from "components/RouteEditingBar/EditingPanel";
 import EditingTip from "components/RouteEditingBar/EditingTip";
 import helpIcon from "assets/images/icons/help.png";
-
+import POI from "components/SideBar/POI";
 
 class EditorExitButton extends React.Component {
     render() {
@@ -18,30 +18,36 @@ class EditorExitButton extends React.Component {
     }
 }
 
-@inject("store")
+@inject("store") @observer
 export default class RouteEditingMenu extends React.Component {
     render() {
-        const { routeEditingManager } = this.props.store;
+        const { routeEditingManager, options } = this.props.store;
 
         return (
+            <div>
             <div className="route-editing-bar">
                 <EditingTip />
                 <EditingPanel clickRemoveLast={() => {
-                                    routeEditingManager.removeLastRoutingPoint();
+                                  routeEditingManager.removeLastRoutingPoint();
                               }}
                               clickRemoveAll={() => {
-                                    routeEditingManager.removeAllRoutingPoints();
+                                  routeEditingManager.removeAllRoutingPoints();
                               }}
                               clickSendRoute={() => {
-                                    routeEditingManager.sendRoutingRequest();
+                                  routeEditingManager.sendRoutingRequest();
                               }}
                               clickAddDefaultEndPoint={() => {
-                                    routeEditingManager.addDefaultEndPoint();
+                                  options.toggleShowPOI();
                               }}
                 />
                 <EditorExitButton onClick={ () => {
                     routeEditingManager.disableRouteEditing();
                 }}/>
+            </div>
+            <div className="sidebar">
+                {options.showPOI ? <POI routeEditingManager={routeEditingManager}
+                    options={options} /> : <div/>}
+            </div>
             </div>
         );
     }
