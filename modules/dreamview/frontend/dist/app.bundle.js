037697c15e4bd697e0f4df6f2931470c2ab4793c
Vivian Lin <linvivian@baidu.com>
2017-10-30 17:51:08 -0700

    Dreamview: updated editing map radius to 1500 m (#946)

--- modules/dreamview/frontend/dist/app.bundle.js
+++ modules/dreamview/frontend/dist/app.bundle.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3eaa3a8891288de80302655c8affa8e4a1fbe1fd1cd500f2e5bba221f9bb17d6
-size 1505573
+oid sha256:595764b2fa9e065ba76ae16d5c99928625dab8ccd868301e2a22c10daf484d2b
+size 1505574
