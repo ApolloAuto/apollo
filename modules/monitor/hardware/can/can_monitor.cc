47b08fccf9c675f15fffc37154e8c7826b183f0c
Aaron Xiao <xiaoxiangquan@gmail.com>
2017-10-31 19:28:54 -0700

    Monitor: Add recurrent can monitor library.

---
+++ modules/monitor/hardware/can/can_monitor.cc
@@ -0,0 +1,53 @@
+/******************************************************************************
+ * Copyright 2017 The Apollo Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *****************************************************************************/
+
+#include "modules/monitor/hardware/can/can_monitor.h"
+
+#include "modules/canbus/common/canbus_gflags.h"
+#include "modules/canbus/proto/canbus_conf.pb.h"
+#include "modules/common/log.h"
+#include "modules/common/util/file.h"
+#include "modules/monitor/hardware/can/can_checker_factory.h"
+
+namespace apollo {
+namespace monitor {
+
+using apollo::canbus::CanbusConf;
+
+void CanMonitor::RunOnce(const double current_time) {
+  CanbusConf canbus_conf;
+
+  CHECK(apollo::common::util::GetProtoFromFile(FLAGS_canbus_conf_file,
+                                               &canbus_conf));
+
+  auto *can_chk_factory = CanCheckerFactory::instance();
+  can_chk_factory->RegisterCanCheckers();
+  auto can_chk =
+      can_chk_factory->CreateCanChecker(canbus_conf.can_card_parameter());
+  CHECK(can_chk);
+
+  std::vector<HwCheckResult> can_rslt;
+  can_chk->run_check(&can_rslt);
+  CHECK_EQ(can_rslt.size(), 1);
+
+  status_->set_status(static_cast<HardwareStatus::Status>(can_rslt[0].status));
+  status_->set_msg(can_rslt[0].mssg);
+
+  ADEBUG << "Done checking " << name_ << ", status=" << status_->status();
+}
+
+}  // namespace monitor
+}  // namespace apollo
