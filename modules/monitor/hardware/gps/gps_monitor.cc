fc2032a7d1c3f7bb7480c80564e3f5f3312b3f1e
Aaron Xiao <xiaoxiangquan@gmail.com>
2017-11-01 12:46:35 -0700

    Monitor: Add gps monitor.

---
+++ modules/monitor/hardware/gps/gps_monitor.cc
@@ -0,0 +1,69 @@
+/******************************************************************************
+ * Copyright 2017 The Apollo Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *****************************************************************************/
+
+#include "modules/monitor/hardware/gps/gps_monitor.h"
+
+#include "modules/common/adapters/adapter_manager.h"
+#include "modules/common/log.h"
+
+namespace apollo {
+namespace monitor {
+
+using apollo::common::adapter::AdapterManager;
+using apollo::common::gnss_status::InsStatus;
+
+void GpsMonitor::RunOnce(const double current_time) {
+  // Check Gnss status.
+  auto *gnss_status_adapter = CHECK_NOTNULL(AdapterManager::GetGnssStatus());
+  gnss_status_adapter->Observe();
+  if (gnss_status_adapter->Empty()) {
+    status_->set_status(HardwareStatus::ERR);
+    status_->set_msg("No GNSS status message.");
+    return;
+  }
+  if (!gnss_status_adapter->GetLatestObserved().solution_completed()) {
+    status_->set_status(HardwareStatus::ERR);
+    status_->set_msg("GNSS solution uncompleted.");
+    return;
+  }
+
+  // Check Ins status.
+  auto *ins_status_adapter = CHECK_NOTNULL(AdapterManager::GetInsStatus());
+  ins_status_adapter->Observe();
+  if (ins_status_adapter->Empty()) {
+    status_->set_status(HardwareStatus::ERR);
+    status_->set_msg("No INS status message.");
+    return;
+  }
+  switch (ins_status_adapter->GetLatestObserved().type()) {
+    case InsStatus::CONVERGING:
+      status_->set_status(HardwareStatus::NOT_READY);
+      status_->set_msg("INS ALIGNING");
+      break;
+    case InsStatus::GOOD:
+      status_->set_status(HardwareStatus::OK);
+      status_->set_msg("OK");
+      break;
+    case InsStatus::INVALID:
+    default:
+      status_->set_status(HardwareStatus::ERR);
+      status_->set_msg("INS status invalid.");
+      break;
+  }
+}
+
+}  // namespace monitor
+}  // namespace apollo
