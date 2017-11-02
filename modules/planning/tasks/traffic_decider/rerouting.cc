7c099379cf35b239e23d116d8690ad113fe1c5cc
Dong Li <lidong.main@gmail.com>
2017-11-02 16:28:47 -0700

    planning: use timer to control rerouting frequency

--- modules/planning/tasks/traffic_decider/rerouting.cc
+++ modules/planning/tasks/traffic_decider/rerouting.cc
@@ -23,12 +23,17 @@
 #include "modules/common/proto/pnc_point.pb.h"
 
 #include "modules/common/adapters/adapter_manager.h"
+#include "modules/common/time/time.h"
+#include "modules/common/util/dropbox.h"
 #include "modules/common/vehicle_state/vehicle_state.h"
+#include "modules/planning/common/planning_gflags.h"
 
 namespace apollo {
 namespace planning {
 
 using apollo::common::adapter::AdapterManager;
+using apollo::common::util::Dropbox;
+using apollo::common::time::Clock;
 using apollo::perception::TrafficLight;
 using apollo::perception::TrafficLightDetection;
 
@@ -36,25 +41,20 @@ Rerouting::Rerouting(const RuleConfig& config) : TrafficRule(config) {}
 
 bool Rerouting::ChangeLaneFailRerouting(
     const ReferenceLineInfo* reference_line_info) {
-  const double kPrepareRoutingTime = 3.0;  // seconds
   const auto& segments = reference_line_info->Lanes();
   // 1. If current reference line is drive forward, no rerouting.
   if (segments.NextAction() == routing::FORWARD) {
     // if not current lane, do not check for rerouting
-    ADEBUG << "This is a change lane target reference line, no need to check "
-              "rerouting here.";
     return true;
   }
+
   // 2. If vehicle is not on current reference line yet, no rerouting
   if (!segments.IsOnSegment()) {
-    ADEBUG << "This is a change lane target reference line, no need to check "
-              "rerouting here.";
     return true;
   }
 
   // 3. If current reference line can connect to next passage, no rerouting
   if (segments.CanExit()) {
-    ADEBUG << "This reference line can lead to another passage in routig";
     return true;
   }
 
@@ -62,10 +62,8 @@ bool Rerouting::ChangeLaneFailRerouting(
   const auto& route_end_waypoint =
       reference_line_info->Lanes().RouteEndWaypoint();
   if (!route_end_waypoint.lane) {
-    ADEBUG << "the end of passage is not visible yet, no need rerouting";
     return true;
   }
-  // 5. If the end of current passage region not on reference line, no rerouting
   auto point = route_end_waypoint.lane->GetSmoothPoint(route_end_waypoint.s);
   const auto& reference_line = reference_line_info->reference_line();
   common::SLPoint sl_point;
@@ -74,25 +72,38 @@ bool Rerouting::ChangeLaneFailRerouting(
     return false;
   }
   if (!reference_line.IsOnRoad(sl_point)) {
-    ADEBUG << "end of passage not on road yet, no need rerouting";
     return true;
   }
-  // 6. If the end of current passage region is further than kPrepareRoutingTime
+  // 5. If the end of current passage region is further than kPrepareRoutingTime
   // * speed, no rerouting
   double adc_s = reference_line_info->AdcSlBoundary().end_s();
   const auto* vehicle_state = common::VehicleState::instance();
   double speed = vehicle_state->linear_velocity();
-  const double prepare_distance = speed * kPrepareRoutingTime;
+  const double prepare_distance = speed * FLAGS_prepare_rerouting_time;
   if (sl_point.s() > adc_s + prepare_distance) {
     ADEBUG << "No need rerouting now because still can drive for time: "
-           << kPrepareRoutingTime << " seconds";
+           << FLAGS_prepare_rerouting_time << " seconds";
     return true;
   }
-  // Do rerouting.
+  // 6. Check if we have done rerouting before
+  const std::string last_rerouting_time_key =
+      "kLastReroutingTime_" + reference_line_info->Lanes().Id();
+  double* last_routing_time =
+      common::util::Dropbox<double>::Open()->Get(last_rerouting_time_key);
+  double current_time = Clock::NowInSecond();
+  if (last_routing_time != nullptr &&
+      current_time - *last_routing_time < FLAGS_rerouting_cooldown_time) {
+    ADEBUG << "Skip rerouting and wait for previous rerouting result";
+    return true;
+  }
   if (!Frame::Rerouting()) {
     AERROR << "Failed to send rerouting request";
     return false;
   }
+
+  // store last rerouting time.
+  common::util::Dropbox<double>::Open()->Set(last_rerouting_time_key,
+                                             current_time);
   return true;
 }
 
