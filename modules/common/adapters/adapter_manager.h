b2235156f8d6bad3071f73a007769f62eb17b727
Aaron Xiao <xiaoxiangquan@gmail.com>
2017-11-01 19:36:40 -0700

    Monitor: Add system monitor process.

--- modules/common/adapters/adapter_manager.h
+++ modules/common/adapters/adapter_manager.h
@@ -245,9 +245,9 @@ class AdapterManager {
   REGISTER_ADAPTER(InsStat);
   REGISTER_ADAPTER(InsStatus);
   REGISTER_ADAPTER(GnssStatus);
+  REGISTER_ADAPTER(SystemStatus);
   // TODO(xiaoxq): Retire HMICommand adapter after integration with dreamview.
   REGISTER_ADAPTER(HMICommand);
-  REGISTER_ADAPTER(HMIStatus);
   REGISTER_ADAPTER(Mobileye);
   REGISTER_ADAPTER(DelphiESR);
   REGISTER_ADAPTER(CompressedImage);
