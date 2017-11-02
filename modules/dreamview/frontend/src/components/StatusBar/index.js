63254dc452d32118b5a799d113144e0273abde20
vlin17 <thunder0504@gmail.com>
2017-11-02 14:40:49 -0700

    Dreamview: updated status bar

--- modules/dreamview/frontend/src/components/StatusBar/index.js
+++ modules/dreamview/frontend/src/components/StatusBar/index.js
@@ -5,22 +5,6 @@ import AutoMeter from "components/StatusBar/AutoMeter";
 import Wheel from "components/StatusBar/Wheel";
 import TrafficLightIndicator from "components/StatusBar/TrafficLightIndicator";
 
-class StatusBackground extends React.Component {
-    render() {
-        const boundary = (
-            "M 0 0 H 100 V 100 H 60 C 52 100, 53 90, 52 85 L 48 55 "
-            + "C 47 45, 42 40, 40 40 H 0 L 0 0");
-        return (
-            <div className="status-background">
-                <svg width="100%" height="100%" viewBox="0 0 100 100"
-                     preserveAspectRatio="none">
-                    <path d={boundary} fillOpacity="0.25" />
-                </svg>
-            </div>
-        );
-    }
-}
-
 @observer
 export default class StatusBar extends React.Component {
     render() {
@@ -28,7 +12,6 @@ export default class StatusBar extends React.Component {
 
         return (
             <div className="status-bar">
-                <StatusBackground />
                 <AutoMeter throttlePercent={meters.throttlePercent}
                            brakePercent={meters.brakePercent}
                            speed={meters.speed}
