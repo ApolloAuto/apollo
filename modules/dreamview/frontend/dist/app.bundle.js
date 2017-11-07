a30962f7d5ff913ec0839eb36beef21571ad2000
vlin17 <thunder0504@gmail.com>
2017-11-07 11:14:16 -0800

    set kdappa y-axis range

--- modules/dreamview/frontend/dist/app.bundle.js
+++ modules/dreamview/frontend/dist/app.bundle.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:08bf70c3d8a4a39f27eccfa2ff8a87cd339773bba2699e0c5735902490c389fc
-size 1508786
+oid sha256:049593898ecffacd20ec8809839622f3cc70cbdd885ea40c8e0f73087f4da469
+size 1508803
