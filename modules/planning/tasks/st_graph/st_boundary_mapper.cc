2ca36fa9164b808b5153dae46c6b917c415cf3c2
Dong Li <lidong.main@gmail.com>
2017-11-07 09:51:47 -0800

    planning: fix errors messages when stop at destination

--- modules/planning/tasks/st_graph/st_boundary_mapper.cc
+++ modules/planning/tasks/st_graph/st_boundary_mapper.cc
@@ -168,16 +168,11 @@ bool StBoundaryMapper::MapStopDecision(PathObstacle* stop_obstacle) const {
     return false;
   }
 
-  const double st_stop_s =
-      obstacle_point.s() + stop_decision.stop().distance_s() -
-      vehicle_param_.front_edge_to_center() - FLAGS_decision_valid_stop_range;
-  if (st_stop_s < 0.0) {
-    AERROR << "obstacle " << stop_obstacle->Id() << " st stop_s " << st_stop_s
-           << " is less than 0.";
-    return false;
-  }
+  const double st_stop_s = obstacle_point.s() +
+                           stop_decision.stop().distance_s() -
+                           vehicle_param_.front_edge_to_center();
 
-  const double s_min = st_stop_s;
+  const double s_min = std::max(0.0, st_stop_s);
   const double s_max =
       std::fmax(s_min, std::fmax(planning_distance_, reference_line_.Length()));
 
