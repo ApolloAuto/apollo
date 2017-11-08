a09ff33f2f4b6fd734a7065eb33f8200f58453d6
Zhang Liangliang <zhangliangliang@baidu.com>
2017-11-08 15:46:00 -0800

    Planning: fixed the misuse between spline order and parameter number.

--- modules/planning/math/smoothing_spline/spline_2d_solver_test.cc
+++ modules/planning/math/smoothing_spline/spline_2d_solver_test.cc
@@ -31,7 +31,7 @@ using Eigen::MatrixXd;
 
 TEST(constraint_test, test_suit_one) {
   std::vector<double> t_knots{0, 1, 2, 3, 4, 5};
-  std::size_t order = 6;
+  std::size_t order = 5;
   Spline2dSolver spline_solver(t_knots, order);
 
   Spline2dConstraint* constraint = spline_solver.mutable_constraint();
