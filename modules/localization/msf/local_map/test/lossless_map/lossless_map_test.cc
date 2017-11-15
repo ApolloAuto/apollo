40971e3ce0109551a2e15bf29584399f9a9d21f4
lihao30 <lihao30@baidu.com>
2017-11-15 21:42:15 +0800

    update ut of local_map.

---
+++ modules/localization/msf/local_map/test/lossless_map/lossless_map_test.cc
@@ -0,0 +1,156 @@
+/******************************************************************************
+ * Copyright 2017 The Apollo Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *****************************************************************************/
+
+#include "modules/localization/msf/local_map/lossless_map/lossless_map.h"
+#include <gtest/gtest.h>
+#include <unistd.h>
+#include <boost/filesystem.hpp>
+#include <boost/program_options.hpp>
+#include "modules/localization/msf/local_map/lossless_map/lossless_map_config.h"
+#include "modules/localization/msf/local_map/lossless_map/lossless_map_matrix.h"
+#include "modules/localization/msf/local_map/lossless_map/lossless_map_node.h"
+#include "modules/localization/msf/local_map/lossless_map/lossless_map_pool.h"
+
+namespace apollo {
+namespace localization {
+namespace msf {
+
+class LosslessMapTestSuite : public ::testing::Test {
+ protected:
+  LosslessMapTestSuite() {}
+  virtual ~LosslessMapTestSuite() {}
+  virtual void SetUp() {}
+  virtual void TearDown() {}
+};
+
+/**@brief Test the binary I/O of the map node. */
+TEST_F(LosslessMapTestSuite, MapNodeTest) {
+  // Delete map folder
+  boost::filesystem::remove_all(
+      "modules/localization/msf/local_map/test/testdata/temp_map");
+
+  // Map save
+  {
+    std::string map_base_folder =
+        "modules/localization/msf/local_map/test/testdata";
+    LosslessMapConfig config;
+    LosslessMap map(config);
+    LosslessMapNodePool lossless_map_node_pool(25, 8);
+    lossless_map_node_pool.Initial(&config);
+    map.InitThreadPool(1, 6);
+    map.InitMapNodeCaches(12, 24);
+    map.AttachMapNodePool(&lossless_map_node_pool);
+
+    std::string map_folder_path = map_base_folder + "/temp_map";
+    if (!boost::filesystem::exists(map_folder_path)) {
+      boost::filesystem::create_directory(map_folder_path);
+    }
+    map.SetMapFolderPath(map_folder_path);
+
+    Eigen::Vector3d pt3d;
+    pt3d << 439731.0, 4433988.03, 47.0;
+    map.SetValue(pt3d, 50, 60);
+    map.SetValue(pt3d, 50, 70);
+  }
+
+  // Map load
+  {
+    std::string map_base_folder =
+        "modules/localization/msf/local_map/test/testdata";
+
+    LosslessMapConfig config;
+    LosslessMap map(config);
+    LosslessMapNodePool lossless_map_node_pool(25, 8);
+    lossless_map_node_pool.Initial(&config);
+    map.InitThreadPool(1, 6);
+    map.InitMapNodeCaches(12, 24);
+    map.AttachMapNodePool(&lossless_map_node_pool);
+    std::string map_folder_path = map_base_folder + "/temp_map";
+    if (!boost::filesystem::exists(map_folder_path)) {
+      boost::filesystem::create_directory(map_folder_path);
+    }
+    map.SetMapFolderPath(map_folder_path);
+    {
+      Eigen::Vector3d pt3d;
+      pt3d << 439731.0, 4433988.03, 47.0;
+      ASSERT_EQ(map.GetCountSafe(pt3d, 50, 0), 2);
+      ASSERT_EQ(map.GetValueSafe(pt3d, 50, 0), 65);
+      ASSERT_LT(fabs(map.GetAltSafe(pt3d, 50, 0) - 47.0), 1e-5);
+    }
+  }
+}
+
+TEST_F(LosslessMapTestSuite, MapScheduleTest) {
+  std::string map_folder =
+      "modules/localization/msf/local_map/test/testdata/lossless_single_map";
+  LosslessMapConfig map_config("lossless_map");
+
+  LosslessMapNodePool input_node_pool(25, 8);
+  input_node_pool.Initial(&map_config);
+  LosslessMap map(map_config);
+  map.InitThreadPool(1, 6);
+  map.InitMapNodeCaches(12, 24);
+  map.AttachMapNodePool(&input_node_pool);
+  if (!map.SetMapFolderPath(map_folder)) {
+    std::cerr << "Reflectance map folder is invalid!" << std::endl;
+    return;
+  }
+
+  int zone_id = 50;
+  unsigned int resolution_id = 0;
+
+  Eigen::Vector3d trans_diff;
+  trans_diff[0] = 10;
+  trans_diff[1] = 10;
+
+  MapNodeIndex index;
+  index.m_ = 34635;
+  index.n_ = 3437;
+
+  auto loc = LosslessMapNode::GetLeftTopCorner(map.GetConfig(), index);
+  Eigen::Vector3d location;
+  location[0] = loc[0];
+  location[1] = loc[1];
+
+  for (int i = 0; i < 100; ++i) {
+    map.LoadMapArea(location, resolution_id, zone_id, 21, 21);
+    map.PreloadMapArea(location, trans_diff, resolution_id, zone_id);
+    location += trans_diff;
+  }
+
+  sleep(2);
+
+  LosslessMapNode* node =
+      static_cast<LosslessMapNode*>(map.GetMapNodeSafe(index));
+  node->SetLeftTopCorner(0.0, 0.0);
+
+  std::vector<Eigen::Vector3d> pt3ds_global(200);
+  for (float i = 0; i < 200; ++i) {
+    pt3ds_global[i][0] = i * 8;
+    pt3ds_global[i][1] = i * 5;
+    pt3ds_global[i][2] = 20.0 + i / 200.0;
+  }
+
+  for (size_t i = 0; i < pt3ds_global.size(); ++i) {
+    const Eigen::Vector3d& pt3d = pt3ds_global[i];
+    const unsigned char& intensity = 10 + i % 128;
+    node->SetValueIfInBound(pt3d, intensity);
+  }
+}
+
+}  // namespace msf
+}  // namespace localization
+}  // namespace apollo
--- modules/localization/msf/local_map/test/lossy_map/lossy_map_2d_test.cc
+++ modules/localization/msf/local_map/test/lossy_map/lossy_map_2d_test.cc
@@ -16,15 +16,67 @@
 
 #include "modules/localization/msf/local_map/lossy_map/lossy_map_2d.h"
 #include <gtest/gtest.h>
+#include <unistd.h>
+#include <boost/filesystem.hpp>
+#include <boost/program_options.hpp>
+#include "modules/localization/msf/local_map/lossy_map/lossy_map_matrix_2d.h"
+#include "modules/localization/msf/local_map/lossy_map/lossy_map_node_2d.h"
+#include "modules/localization/msf/local_map/lossy_map/lossy_map_pool_2d.h"
 
 namespace apollo {
 namespace localization {
 namespace msf {
 
-// int main(int argc, char** argv) {
-//   testing::InitGoogleTest(&argc, argv);
-//   return RUN_ALL_TESTS();
-// }
+MapNodeIndex GetMapIndexFromMapFolder(const std::string& map_folder) {
+  MapNodeIndex index;
+  char buf[100];
+  sscanf(map_folder.c_str(), "/%03u/%05s/%02d/%08u/%08u", &index.resolution_id_,
+         buf, &index.zone_id_, &index.m_, &index.n_);
+  std::string zone = buf;
+  std::cout << zone << std::endl;
+  if (zone == "south") {
+    index.zone_id_ = -index.zone_id_;
+  }
+  std::cout << index << std::endl;
+  return index;
+}
+
+bool GetAllMapIndex(const std::string& src_map_folder,
+                    const std::string& dst_map_folder,
+                    std::list<MapNodeIndex>& buf) {
+  std::string src_map_path = src_map_folder + "/map";
+  std::string dst_map_path = dst_map_folder + "/map";
+  boost::filesystem::path src_map_path_boost(src_map_path);
+  boost::filesystem::path dst_map_path_boost(dst_map_path);
+
+  if (!boost::filesystem::exists(dst_map_path)) {
+    boost::filesystem::create_directory(dst_map_path_boost);
+  }
+
+  buf.clear();
+
+  boost::filesystem::recursive_directory_iterator end_iter;
+  boost::filesystem::recursive_directory_iterator iter(src_map_path);
+  for (; iter != end_iter; ++iter) {
+    if (!boost::filesystem::is_directory(*iter)) {
+      if (iter->path().extension() == "") {
+        std::string tmp = iter->path().string();
+        tmp = tmp.substr(src_map_path.length(), tmp.length());
+        buf.push_back(GetMapIndexFromMapFolder(tmp));
+      }
+    } else {
+      std::string tmp = iter->path().string();
+      tmp = tmp.substr(src_map_path.length(), tmp.length());
+      tmp = dst_map_path + tmp;
+      boost::filesystem::path p(tmp);
+      if (!boost::filesystem::exists(p)) {
+        boost::filesystem::create_directory(p);
+      }
+    }
+  }
+
+  return true;
+}
 
 class LossyMap2DTestSuite : public ::testing::Test {
  protected:
@@ -34,11 +86,134 @@ class LossyMap2DTestSuite : public ::testing::Test {
   virtual void TearDown() {}
 };
 
-/**@brief Test the constructor. */
-TEST_F(LossyMap2DTestSuite, test_constructor) {
-  ASSERT_TRUE(true);
+TEST_F(LossyMap2DTestSuite, LossyMapToMapMatrixTest) {
+  typedef LossyMap2D LossyMap;
+  typedef LossyMapNode2D LossyMapNode;
+  typedef LossyMapNodePool2D LossyMapNodePool;
+  typedef LossyMapMatrix2D LossyMapMatrix;
+  typedef LossyMapConfig2D LossyMapConfig;
+
+  std::string src_map_folder =
+      "modules/localization/msf/local_map/test/testdata/lossy_single_map";
+  std::string dst_map_folder =
+      "modules/localization/msf/local_map/test/testdata/temp_output_lossy_map";
+  if (!boost::filesystem::exists(dst_map_folder)) {
+    boost::filesystem::create_directory(dst_map_folder);
+  }
+
+  LossyMapConfig input_config("lossy_full_alt");
+
+  LossyMapNodePool input_node_pool(25, 8);
+  input_node_pool.Initial(&input_config);
+  LossyMap input_map(input_config);
+  input_map.InitThreadPool(1, 6);
+  input_map.InitMapNodeCaches(12, 24);
+  input_map.AttachMapNodePool(&input_node_pool);
+  if (!input_map.SetMapFolderPath(src_map_folder)) {
+    std::cerr << "Reflectance map folder is invalid!" << std::endl;
+    return;
+  }
+
+  std::list<MapNodeIndex> buf;
+  GetAllMapIndex(src_map_folder, dst_map_folder, buf);
+  std::cout << "index size: " << buf.size() << std::endl;
+
+  input_config.Save(dst_map_folder + "/config.xml");
+
+  LossyMapConfig lossy_config("lossy_full_alt");
+  LossyMapNodePool lossy_map_node_pool(25, 8);
+  lossy_map_node_pool.Initial(&lossy_config);
+
+  LossyMap lossy_map(lossy_config);
+  lossy_map.InitThreadPool(1, 6);
+  lossy_map.InitMapNodeCaches(12, 24);
+  lossy_map.AttachMapNodePool(&lossy_map_node_pool);
+  if (!lossy_map.SetMapFolderPath(dst_map_folder)) {
+    std::cout << "lossy_map config xml not exist" << std::endl;
+  }
+
+  int index = 0;
+  auto itr = buf.begin();
+  for (; itr != buf.end(); ++itr, ++index) {
+    LossyMapNode* node =
+        static_cast<LossyMapNode*>(input_map.GetMapNodeSafe(*itr));
+    if (node == NULL) {
+      std::cerr << "index: " << index << " is a NULL pointer!" << std::endl;
+      continue;
+    }
+    LossyMapMatrix& input_matrix =
+        static_cast<LossyMapMatrix&>(node->GetMapCellMatrix());
+
+    LossyMapNode* lossy_node =
+        static_cast<LossyMapNode*>(lossy_map.GetMapNodeSafe(*itr));
+    LossyMapMatrix& lossy_matrix =
+        static_cast<LossyMapMatrix&>(lossy_node->GetMapCellMatrix());
+
+    int rows = input_config.map_node_size_y_;
+    int cols = input_config.map_node_size_x_;
+    for (int row = 0; row < rows; ++row) {
+      for (int col = 0; col < cols; ++col) {
+        lossy_matrix[row][col].intensity = input_matrix[row][col].intensity;
+        lossy_matrix[row][col].intensity_var =
+            input_matrix[row][col].intensity_var;
+        lossy_matrix[row][col].count = input_matrix[row][col].count;
+        lossy_matrix[row][col].altitude = input_matrix[row][col].altitude;
+        lossy_matrix[row][col].altitude_ground =
+            input_matrix[row][col].altitude_ground;
+        lossy_matrix[row][col].is_ground_useful =
+            input_matrix[row][col].is_ground_useful;
+      }
+    }
+    lossy_node->SetIsChanged(true);
+  }
 }
 
-}  // namespace numerical
-}  // namespace core
-}  // namespace car
+TEST_F(LossyMap2DTestSuite, MapScheduleTest) {
+  typedef LossyMap2D LossyMap;
+  typedef LossyMapNodePool2D LossyMapNodePool;
+  typedef LossyMapConfig2D LossyMapConfig;
+
+  std::string map_folder =
+      "modules/localization/msf/local_map/test/testdata/lossy_single_map";
+  LossyMapConfig map_config("lossy_full_alt");
+
+  LossyMapNodePool input_node_pool(25, 8);
+  input_node_pool.Initial(&map_config);
+  LossyMap lossy_map(map_config);
+  lossy_map.InitThreadPool(1, 6);
+  lossy_map.InitMapNodeCaches(12, 24);
+  lossy_map.AttachMapNodePool(&input_node_pool);
+  if (!lossy_map.SetMapFolderPath(map_folder)) {
+    std::cerr << "Reflectance map folder is invalid!" << std::endl;
+    return;
+  }
+
+  unsigned int zone_id = 50;
+  unsigned int resolution_id = 0;
+
+  MapNodeIndex index;
+  index.m_ = 34636;
+  index.n_ = 3436;
+  {
+    Eigen::Vector3d trans_diff;
+    trans_diff[0] = 10;
+    trans_diff[1] = 10;
+
+    auto loc = BaseMapNode::GetLeftTopCorner(lossy_map.GetConfig(), index);
+    Eigen::Vector3d location;
+    location[0] = loc[0];
+    location[1] = loc[1];
+
+    for (int i = 0; i < 100; ++i) {
+      lossy_map.LoadMapArea(location, resolution_id, zone_id, 21, 21);
+      lossy_map.PreloadMapArea(location, trans_diff, resolution_id, zone_id);
+      location += trans_diff;
+    }
+
+    sleep(2);
+  }
+}
+
+}  // namespace msf
+}  // namespace localization
+}  // namespace apollo
