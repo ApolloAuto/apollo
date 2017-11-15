4aae678eb85d3a3acffc68b10393c869ed5b1ff0
Liangliang Zhang <zhangliangliang@baidu.com>
2017-11-15 12:49:44 -0800

    Planning: added stop epsilon buffer to eliminate printout at normal stop. (#1290)

--- modules/planning/tasks/st_graph/st_boundary_mapper.cc
+++ modules/planning/tasks/st_graph/st_boundary_mapper.cc
@@ -172,7 +172,8 @@ bool StBoundaryMapper::MapStopDecision(PathObstacle* stop_obstacle) const {
                            stop_decision.stop().distance_s() -
                            vehicle_param_.front_edge_to_center();
 
-  const double s_min = std::max(0.0, st_stop_s);
+  constexpr double kStopEpsilon = 1e-2;
+  const double s_min = std::max(0.0, st_stop_s - kStopEpsilon);
   const double s_max =
       std::fmax(s_min, std::fmax(planning_distance_, reference_line_.Length()));
 
