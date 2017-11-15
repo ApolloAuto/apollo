e5e7ef0fe47d008ad22628cfaaef3bef22f896b8
Lei Wang <bestwanglei@gmail.com>
2017-11-15 12:53:12 -0800

    Update ros with gnss driver and add pyproj package. (#1289)

--- docker/scripts/dev_start.sh
+++ docker/scripts/dev_start.sh
@@ -18,7 +18,7 @@
 
 VERSION=""
 ARCH=$(uname -m)
-VERSION_X86_64="dev-x86_64-20171114_1011"
+VERSION_X86_64="dev-x86_64-20171115_1105"
 VERSION_AARCH64="dev-aarch64-20170927_1111"
 if [[ $# == 1 ]];then
     VERSION=$1
