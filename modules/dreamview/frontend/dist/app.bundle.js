273ccd96728354e1c7b33d712dea7b821ac1cd64
vlin17 <thunder0504@gmail.com>
2017-11-14 17:49:35 -0800

    updated dreamview dist

--- modules/dreamview/frontend/dist/app.bundle.js
+++ modules/dreamview/frontend/dist/app.bundle.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0b8c75f6b99908585d39b680028912e5097bb514a4f201e281d39da72db4604f
-size 1526123
+oid sha256:940cb10faad5080b07ab0bf0b70da45f64076ebc215cee19622c1d38f8fa3f7d
+size 1525974
