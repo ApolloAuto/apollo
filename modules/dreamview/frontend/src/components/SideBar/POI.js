28ce485dd1cec715ed006f22fb40b3f0c586b85a
Vivian Lin <thunder0504@gmail.com>
2017-11-14 17:10:25 -0800

    Dreamview: updated POI UI (#1276)

---
+++ modules/dreamview/frontend/src/components/SideBar/POI.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { observer } from "mobx-react";
+import classNames from "classnames";
+
+import MenuItemRadio from 'components/common/MenuItemRadio';
+
+@observer
+export default class POI extends React.Component {
+    render() {
+        const { routeEditingManager, options } = this.props;
+
+        const entries = Object.keys(routeEditingManager.defaultRoutingEndPoint)
+            .map(key => {
+                return (
+                    <MenuItemRadio extraClasses={["poi-button"]}
+                                   key={`poi_${key}`} id='poi' title={key}
+                                   onClick={() => {
+                                        routeEditingManager.addDefaultEndPoint(key);
+                                        if (!options.showRouteEditingBar) {
+                                            routeEditingManager.sendRoutingRequest();
+                                        }
+                                        options.showPOI = false;
+                                   }}
+                                   checked={routeEditingManager.currentPOI === key}/>
+                );
+            });
+
+        return (
+            <div className="nav-side-menu" id="poi-list">
+                <div className="card">
+                    <div className="card-header"><span>Point of Interest</span></div>
+                    <div className="card-content-row">{entries}</div>
+                </div>
+            </div>
+        );
+    }
+}
