0193e5816f49c7d3867afbb2a534c3f02ff27ed0
Vivian Lin <thunder0504@gmail.com>
2017-11-15 21:01:04 -0800

    Dreamview: added navigation mode and its google map view (#1305)
    * Dreamview: added navigation
    
    * dist updates for navigation_viewer.html
    
    * added dist/components

--- modules/dreamview/frontend/dist/app.bundle.js
+++ modules/dreamview/frontend/dist/app.bundle.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b976c8df7ea1ce984ba6946d21054b69c22398c2ebcc4114cabc6c1dce7c5bcf
-size 1526563
+oid sha256:0666f33f0766330ef7435497ef940757b13527ef9a16b4a2cc86c8562becc813
+size 1527604
