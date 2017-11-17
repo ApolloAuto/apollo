77c53f5c7df018c9e1483276bac93c1f6985f5cd
jiangyifei <jiangyifei@gmail.com>
2017-11-16 16:23:56 -0800

    tools: added routing display in mobileye viewer.

--- modules/tools/mobileye_viewer/plot_mobileye.py
+++ modules/tools/mobileye_viewer/plot_mobileye.py
@@ -17,6 +17,7 @@
 ###############################################################################
 
 import rospy
+import json
 import argparse
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
@@ -25,6 +26,7 @@ from modules.drivers.proto import mobileye_pb2
 from mobileye_data import MobileyeData
 from localization_data import LocalizationData
 from planning_data import PlanningData
+from routing_data import RoutingData
 from chassis_data import ChassisData
 from view_subplot import ViewSubplot
 from subplot_s_speed import SubplotSSpeed
@@ -39,10 +41,11 @@ mobileye = MobileyeData()
 localization = LocalizationData()
 planning = PlanningData()
 chassis = ChassisData()
+routing_data = RoutingData()
 
 
 def update(frame_number):
-    view_subplot.show(mobileye, localization, planning, chassis)
+    view_subplot.show(mobileye, localization, planning, chassis, routing_data)
     s_speed_subplot.show(planning)
     s_theta_subplot.show(planning)
     s_time_subplot.show(planning)
@@ -70,7 +73,7 @@ def chassis_callback(chassis_pb):
 
 
 def routing_callback(routing_str):
-    pass
+    routing_data.update(routing_str)
 
 
 def add_listener():
