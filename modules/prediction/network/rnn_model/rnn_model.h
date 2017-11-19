5d118fcfce2b8d84800d4b0f2c6ef0a7206bfe93
ZhongpuXia <xiazhongpu5@163.com>
2017-11-19 16:21:49 +0800

    prediction: add documents for network

--- modules/prediction/network/rnn_model/rnn_model.h
+++ modules/prediction/network/rnn_model/rnn_model.h
@@ -34,15 +34,36 @@ namespace apollo {
 namespace prediction {
 namespace network {
 
+/**
+ * @class RnnModel
+ * @brief RnnModel is a derived class from NetModel, it has a specific layers structure.
+ */
 class RnnModel : public NetModel {
  public:
+  /**
+   * @brief Compute the model output from inputs according to a defined layers' flow
+   * @param Inputs to the network
+   * @param Output of the network will be returned
+   */
   virtual void Run(const std::vector<Eigen::MatrixXf>& inputs,
                    Eigen::MatrixXf* output) const;
 
+  /**
+   * @brief Set the internal state of a network model
+   * @param A specified internal state in a vector of Eigen::MatrixXf
+   */
   virtual void SetState(const std::vector<Eigen::MatrixXf>& states);
 
+  /**
+   * @brief Access to the internal state of a network model
+   * @return Internal state in a vector of Eigen::MatrixXf of the model
+   */
   virtual void State(std::vector<Eigen::MatrixXf>* states) const;
 
+  /**
+   * @brief Set the internal state of a model
+   * @param A specified internal state in a vector of Eigen::MatrixXf
+   */
   virtual void ResetState() const;
 
   DECLARE_SINGLETON(RnnModel);
