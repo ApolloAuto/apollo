8e0e185000317d5264e08d01e73f16d89bd647ed
unacao <yufcao@gmail.com>
2017-11-20 15:08:33 -0800

    fetch POI data when there is map change (#1371)

--- modules/dreamview/frontend/dist/app.bundle.js
+++ modules/dreamview/frontend/dist/app.bundle.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:444e18557d87e96a76c13646f1f0d3932adfbef91a6ec284a5f858fb8aec0289
-size 1527586
+oid sha256:d1985d76f66bbb5c3396ad8e3fbd12f126fa9a44db63a54f784d790d60426c17
+size 1527568
