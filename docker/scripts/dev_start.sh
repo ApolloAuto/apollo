454679a9f42fdd637857a0d8603a6b80c94957e5
Lei Wang <bestwanglei@gmail.com>
2017-11-20 13:13:06 -0800

    Update sunnyvale loop map. (#1369)

--- docker/scripts/dev_start.sh
+++ docker/scripts/dev_start.sh
@@ -18,7 +18,7 @@
 
 VERSION=""
 ARCH=$(uname -m)
-VERSION_X86_64="dev-x86_64-20171115_2103"
+VERSION_X86_64="dev-x86_64-20171120_1254"
 VERSION_AARCH64="dev-aarch64-20170927_1111"
 if [[ $# == 1 ]];then
     VERSION=$1
