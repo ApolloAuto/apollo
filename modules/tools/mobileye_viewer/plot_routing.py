508b371a981dd0706b60fe8733f1c4e5d75c09c4
jiangyifei <jiangyifei@gmail.com>
2017-11-22 10:52:42 -0800

    navi: added routing smoother based on spline with constraints; also added related debug tools.

--- modules/tools/mobileye_viewer/plot_routing.py
+++ modules/tools/mobileye_viewer/plot_routing.py
@@ -19,9 +19,9 @@
 import rospy
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
+from std_msgs.msg import String
 from routing_data import RoutingData
 from subplot_routing import SubplotRouting
-from std_msgs.msg import String
 
 routing_data = RoutingData()
 
@@ -29,14 +29,23 @@ routing_data = RoutingData()
 def update(frame_number):
     subplot_routing.show(routing_data)
 
+
 def routing_callback(routing_str):
     routing_data.update(routing_str)
+
+
+def routing_debug_callback(routing_debug_str):
+    if routing_debug_str is not None:
+        routing_data.update_debug(routing_debug_str)
 
 
 def add_listener():
     rospy.init_node('navi_routing_plot', anonymous=True)
     rospy.Subscriber('/apollo/navigation/routing',
                      String, routing_callback)
+    rospy.Subscriber('/apollo/navigation/routing/debug',
+                     String, routing_debug_callback)
+
 
 if __name__ == '__main__':
     add_listener()
