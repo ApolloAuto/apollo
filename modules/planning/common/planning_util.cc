9facf93d9d7c6df989299bd7222d990460c40bd3
YajiaZhang <yajia.zhang@gmail.com>
2017-11-21 16:20:55 -0800

    planning: fixed the interpolation for path points

--- modules/planning/common/planning_util.cc
+++ modules/planning/common/planning_util.cc
@@ -24,6 +24,7 @@
 #include "modules/common/adapters/adapter_manager.h"
 #include "modules/common/math/integral.h"
 #include "modules/common/math/linear_interpolation.h"
+#include "modules/common/math/math_utils.h"
 #include "modules/planning/common/planning_gflags.h"
 #include "modules/planning/math/hermite_spline.h"
 
@@ -42,15 +43,19 @@ PathPoint interpolate(const PathPoint &p0, const PathPoint &p1,
   double s1 = p1.s();
   CHECK(s0 <= s && s <= s1);
 
-  std::array<double, 2> gx0{{p0.theta(), p0.kappa()}};
-  std::array<double, 2> gx1{{p1.theta(), p1.kappa()}};
-  HermiteSpline<double, 3> geometry_spline(gx0, gx1, s0, s1);
-  auto func_cos_theta = [&geometry_spline](const double s) {
-    auto theta = geometry_spline.Evaluate(0, s);
+  double theta_diff = common::math::NormalizeAngle(
+      p1.theta() - p0.theta());
+
+  std::array<double, 3> gx0{{0.0, p0.kappa(), p0.dkappa()}};
+  std::array<double, 3> gx1{{theta_diff, p1.kappa(), p1.dkappa()}};
+
+  HermiteSpline<double, 5> geometry_spline(gx0, gx1, s0, s1);
+  auto func_cos_theta = [&geometry_spline, &p0](const double s) {
+    auto theta = geometry_spline.Evaluate(0, s) + p0.theta();
     return std::cos(theta);
   };
-  auto func_sin_theta = [&geometry_spline](const double s) {
-    auto theta = geometry_spline.Evaluate(0, s);
+  auto func_sin_theta = [&geometry_spline, &p0](const double s) {
+    auto theta = geometry_spline.Evaluate(0, s) + p0.theta();
     return std::sin(theta);
   };
 
@@ -58,7 +63,8 @@ PathPoint interpolate(const PathPoint &p0, const PathPoint &p1,
       p0.x() + common::math::IntegrateByGaussLegendre<5>(func_cos_theta, s0, s);
   double y =
       p0.y() + common::math::IntegrateByGaussLegendre<5>(func_sin_theta, s0, s);
-  double theta = geometry_spline.Evaluate(0, s);
+  double theta = common::math::NormalizeAngle(
+      geometry_spline.Evaluate(0, s) + p0.theta());
   double kappa = geometry_spline.Evaluate(1, s);
   double dkappa = geometry_spline.Evaluate(2, s);
   double d2kappa = geometry_spline.Evaluate(3, s);
