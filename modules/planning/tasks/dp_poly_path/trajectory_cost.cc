0b9e26207a07b76fc7a89722ec35b4481e14fe17
Zhang Liangliang <zhangliangliang@baidu.com>
2017-11-27 15:19:31 -0800

    Planning: refactored trajectory_cost.cc to reduced the number of calling EvaluateByTime by 50%.

--- modules/planning/tasks/dp_poly_path/trajectory_cost.cc
+++ modules/planning/tasks/dp_poly_path/trajectory_cost.cc
@@ -56,7 +56,7 @@ TrajectoryCost::TrajectoryCost(
     }
     const auto ptr_obstacle = ptr_path_obstacle->obstacle();
     if (Obstacle::IsVirtualObstacle(ptr_obstacle->Perception())) {
-      // Virtual obsatcle
+      // Virtual obstacle
       continue;
     }
     std::vector<Box2d> box_by_time;
@@ -70,81 +70,86 @@ TrajectoryCost::TrajectoryCost(
   }
 }
 
+double TrajectoryCost::CalculatePathCost(const QuinticPolynomialCurve1d &curve,
+                                         const double start_s,
+                                         const double end_s) const {
+  double path_cost = 0.0;
+  double path_s = 0.0;
+  while (path_s < (end_s - start_s)) {
+    const double l = std::fabs(curve.Evaluate(0, path_s));
+    path_cost += l * config_.path_l_cost();
+
+    const double dl = std::fabs(curve.Evaluate(1, path_s));
+    path_cost += dl * config_.path_dl_cost();
+
+    path_s += config_.path_resolution();
+  }
+  return path_cost;
+}
+
+double TrajectoryCost::CalculateObstacleCost(
+    const QuinticPolynomialCurve1d &curve, const double start_s,
+    const double end_s) const {
+  double obstacle_cost = 0.0;
+  double time_stamp = 0.0;
+  for (size_t index = 0; index < num_of_time_stamps_;
+       ++index, time_stamp += config_.eval_time_interval()) {
+    common::SpeedPoint speed_point;
+    heuristic_speed_data_.EvaluateByTime(time_stamp, &speed_point);
+    if (speed_point.s() < start_s) {
+      continue;
+    }
+    if (speed_point.s() > end_s) {
+      break;
+    }
+
+    const double s = speed_point.s() - start_s;
+    const double l = curve.Evaluate(0, s);
+    const double dl = curve.Evaluate(1, s);
+
+    common::SLPoint sl;
+    sl.set_s(speed_point.s());
+    sl.set_l(l);
+
+    Vec2d ego_xy_point;
+    reference_line_->SLToXY(sl, &ego_xy_point);
+
+    ReferencePoint reference_point =
+        reference_line_->GetReferencePoint(speed_point.s());
+
+    const double one_minus_kappa_r_d = 1 - reference_point.kappa() * l;
+    const double delta_theta = std::atan2(dl, one_minus_kappa_r_d);
+    const double theta =
+        common::math::NormalizeAngle(delta_theta + reference_point.heading());
+    const Box2d ego_box = {ego_xy_point, theta, vehicle_param_.length(),
+                           vehicle_param_.width()};
+    for (const auto &obstacle_trajectory : obstacle_boxes_) {
+      auto &obstacle_box = obstacle_trajectory.at(index);
+      // Simple version: calculate obstacle cost by distance
+      const double distance = obstacle_box.DistanceTo(ego_box);
+      if (distance > config_.obstacle_ignore_distance()) {
+        continue;
+      } else if (distance <= config_.obstacle_collision_distance()) {
+        obstacle_cost += config_.obstacle_collision_cost();
+      } else if (distance <= config_.obstacle_risk_distance()) {
+        obstacle_cost += RiskDistanceCost(distance);
+      } else {
+        obstacle_cost += RegularDistanceCost(distance);
+      }
+    }
+  }
+  return obstacle_cost;
+}
+
 double TrajectoryCost::Calculate(const QuinticPolynomialCurve1d &curve,
                                  const double start_s,
                                  const double end_s) const {
   double total_cost = 0.0;
-  // Path_cost
-  double path_s = 0.0;
-  while (path_s < (end_s - start_s)) {
-    const double l = std::fabs(curve.Evaluate(0, path_s));
-    total_cost += l;
-
-    const double dl = std::fabs(curve.Evaluate(1, path_s));
-    total_cost += dl;
-
-    path_s += config_.path_resolution();
-  }
+  // path cost
+  total_cost += CalculatePathCost(curve, start_s, end_s);
 
   // Obstacle cost
-  uint32_t start_index = 0;
-  bool is_found_start_index = false;
-  bool is_found_end_index = false;
-  uint32_t end_index = 0;
-  uint32_t index = 0;
-  double time_stamp = 0.0;
-  while (index <= num_of_time_stamps_ &&
-         !(is_found_start_index && is_found_end_index)) {
-    common::SpeedPoint speed_point;
-    heuristic_speed_data_.EvaluateByTime(time_stamp, &speed_point);
-    if (speed_point.s() >= start_s && !is_found_start_index) {
-      start_index = index;
-      is_found_start_index = true;
-    }
-    if (speed_point.s() > end_s && !is_found_end_index) {
-      end_index = index;
-      is_found_end_index = true;
-    }
-    time_stamp += config_.eval_time_interval();
-    ++index;
-  }
-
-  for (; start_index < end_index; ++start_index) {
-    common::SpeedPoint speed_point;
-    heuristic_speed_data_.EvaluateByTime(
-        start_index * config_.eval_time_interval(), &speed_point);
-    const double s = speed_point.s() - start_s;
-    const double l = curve.Evaluate(0, s);
-    const double dl = curve.Evaluate(1, s);
-    Vec2d ego_xy_point;
-    common::SLPoint sl;
-    sl.set_s(speed_point.s());
-    sl.set_l(l);
-    reference_line_->SLToXY(sl, &ego_xy_point);
-    ReferencePoint reference_point =
-        reference_line_->GetReferencePoint(speed_point.s());
-
-    double one_minus_kappa_r_d = 1 - reference_point.kappa() * l;
-    double delta_theta = std::atan2(dl, one_minus_kappa_r_d);
-    double theta =
-        common::math::NormalizeAngle(delta_theta + reference_point.heading());
-    Box2d ego_box = {ego_xy_point, theta, vehicle_param_.length(),
-                     vehicle_param_.width()};
-    for (const auto &obstacle_trajectory : obstacle_boxes_) {
-      auto &obstacle_box = obstacle_trajectory.at(start_index);
-      // Simple version: calculate obstacle cost by distance
-      double distance = obstacle_box.DistanceTo(ego_box);
-      if (distance > config_.obstacle_ignore_distance()) {
-        continue;
-      } else if (distance <= config_.obstacle_collision_distance()) {
-        total_cost += config_.obstacle_collision_cost();
-      } else if (distance <= config_.obstacle_risk_distance()) {
-        total_cost += RiskDistanceCost(distance);
-      } else {
-        total_cost += RegularDistanceCost(distance);
-      }
-    }
-  }
+  total_cost += CalculateObstacleCost(curve, start_s, end_s);
   return total_cost;
 }
 
