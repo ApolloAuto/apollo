d7300e41051496e648f6ef24ed4c1c524b3c8f50
Lei Wang <bestwanglei@gmail.com>
2017-11-29 15:44:03 -0800

    Update caffe libs. (#1483)

--- scripts/docker_adduser.sh
+++ scripts/docker_adduser.sh
@@ -30,31 +30,25 @@ chown -R ${DOCKER_USER}:${DOCKER_GRP} "/home/${DOCKER_USER}"
 
 # grant caros user to access GPS device
 if [ -e /dev/ttyUSB0 ]; then
-    chmod a+rw /dev/ttyUSB0 /dev/ttyUSB1
+  chmod a+rw /dev/ttyUSB0 /dev/ttyUSB1
 fi
 
 # setup camera device
 if [ -e /dev/video0 ]; then
-    mkdir /dev/camera
-    ln -s /dev/video0  /dev/camera/obstacle
-    ln -s /dev/video1  /dev/camera/trafficlights
-    chmod a+rw /dev/video0 /dev/video1 /dev/camera/obstacle /dev/camera/trafficlights
+  mkdir /dev/camera
+  ln -s /dev/video0  /dev/camera/obstacle
+  ln -s /dev/video1  /dev/camera/trafficlights
+  chmod a+rw /dev/video0 /dev/video1 /dev/camera/obstacle /dev/camera/trafficlights
 fi
 
-MACHINE_ARCH=$(uname -m)
-ROS_TAR="ros-indigo-apollo-1.5.3-${MACHINE_ARCH}.tar.gz"
 if [ "$RELEASE_DOCKER" != "1" ];then
   # setup map data
   if [ -e /home/tmp/modules_data ]; then
     cp -r /home/tmp/modules_data/* /apollo/modules/
     chown -R ${DOCKER_USER}:${DOCKER_GRP} "/apollo/modules"
   fi
-# setup ros package
-# this is a temporary solution to avoid ros package downloading.
-ROS="/home/tmp/ros"
-if [ -e "$ROS" ]; then
-  rm -rf $ROS
-fi
-tar xzf "/home/tmp/${ROS_TAR}" -C "/home/tmp"
-chown -R ${DOCKER_USER}:${DOCKER_GRP} "${ROS}"
+  # setup ros package
+  # this is a temporary solution to avoid ros package downloading.
+  ROS="/home/tmp/ros"
+  chown -R ${DOCKER_USER}:${DOCKER_GRP} "${ROS}"
 fi
