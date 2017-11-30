6e4772744edcae83bd58b6c17ebc8482a8f7a3a9
ghdawn31 <ghdwan31@163.com>
2017-11-30 12:18:00 -0800

    common : fix format error

--- modules/common/adapters/adapter_manager.h
+++ modules/common/adapters/adapter_manager.h
@@ -225,35 +225,35 @@ class AdapterManager {
   bool initialized_ = false;
 
   /// The following code registered all the adapters of interest.
-  REGISTER_ADAPTER(Chassis);
-  REGISTER_ADAPTER(ChassisDetail);
-  REGISTER_ADAPTER(ControlCommand);
-  REGISTER_ADAPTER(Gps);
-  REGISTER_ADAPTER(Imu);
-  REGISTER_ADAPTER(Localization);
-  REGISTER_ADAPTER(Monitor);
-  REGISTER_ADAPTER(Pad);
-  REGISTER_ADAPTER(PerceptionObstacles);
-  REGISTER_ADAPTER(Planning);
-  REGISTER_ADAPTER(PointCloud);
-  REGISTER_ADAPTER(ImageShort);
-  REGISTER_ADAPTER(ImageLong);
-  REGISTER_ADAPTER(Prediction);
-  REGISTER_ADAPTER(TrafficLightDetection);
-  REGISTER_ADAPTER(RoutingRequest);
-  REGISTER_ADAPTER(RoutingResponse);
-  REGISTER_ADAPTER(RelativeOdometry);
-  REGISTER_ADAPTER(InsStat);
-  REGISTER_ADAPTER(InsStatus);
-  REGISTER_ADAPTER(GnssStatus);
-  REGISTER_ADAPTER(SystemStatus);
-  
-  REGISTER_ADAPTER(Mobileye);
-  REGISTER_ADAPTER(DelphiESR);
-  REGISTER_ADAPTER(ContiRadar);
-  REGISTER_ADAPTER(CompressedImage);
-  
-  DECLARE_SINGLETON(AdapterManager);
+ REGISTER_ADAPTER(Chassis);
+ REGISTER_ADAPTER(ChassisDetail);
+ REGISTER_ADAPTER(ControlCommand);
+ REGISTER_ADAPTER(Gps);
+ REGISTER_ADAPTER(Imu);
+ REGISTER_ADAPTER(Localization);
+ REGISTER_ADAPTER(Monitor);
+ REGISTER_ADAPTER(Pad);
+ REGISTER_ADAPTER(PerceptionObstacles);
+ REGISTER_ADAPTER(Planning);
+ REGISTER_ADAPTER(PointCloud);
+ REGISTER_ADAPTER(ImageShort);
+ REGISTER_ADAPTER(ImageLong);
+ REGISTER_ADAPTER(Prediction);
+ REGISTER_ADAPTER(TrafficLightDetection);
+ REGISTER_ADAPTER(RoutingRequest);
+ REGISTER_ADAPTER(RoutingResponse);
+ REGISTER_ADAPTER(RelativeOdometry);
+ REGISTER_ADAPTER(InsStat);
+ REGISTER_ADAPTER(InsStatus);
+ REGISTER_ADAPTER(GnssStatus);
+ REGISTER_ADAPTER(SystemStatus);
+
+ REGISTER_ADAPTER(Mobileye);
+ REGISTER_ADAPTER(DelphiESR);
+ REGISTER_ADAPTER(ContiRadar);
+ REGISTER_ADAPTER(CompressedImage);
+
+ DECLARE_SINGLETON(AdapterManager);
 };
 
 }  // namespace adapter
