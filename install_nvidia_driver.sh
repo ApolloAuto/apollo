c5358bb265bab78e18c37592d8ab02fb8a6b8568
yiakwy <yiak.wy@gmail.com>
2017-12-05 13:38:55 +0800

    squash commits of CentOS_gpu_enabled

---
+++ install_nvidia_driver.sh
@@ -0,0 +1,10 @@
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+source "${DIR}/install_gcc.sh"
+
+ln -s /usr/bin/cc /usr/bin/cc1
+
+chmod +x ./NVIDIA-Linux-x86_64-375.51.run
+sudo ./NVIDIA-Linux-x86_64-375.51.run --no-opengl-files -a -s
+
+source "${DIR}/recover_gcc.sh
