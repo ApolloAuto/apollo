323b4bbf99cc98d5f2ac6df783d7cd5be3197365
Dong Li <lidong.main@gmail.com>
2017-12-06 14:59:32 -0800

    tools: support target storage directory in filter planning

--- scripts/filter_planning.sh
+++ scripts/filter_planning.sh
@@ -17,11 +17,12 @@
 ###############################################################################
 
 function print_help() {
-   echo "Example: filter_planning.sh -[pc|np|wp] bag1 bag2 bag3 ..."
-   echo "This script works in one of the three mode:"
-   echo "-pc filter for perfect control, produces *.pc.bag"
-   echo "-np filter for planning dependencies, produces *.np.bag"
-   echo "-wp filter for planning and its dependencies, produces *.wp.bag"
+   echo "Example: filter_planning.sh -[pc|np|wp] [-d|--dir] bag1 bag2 bag3 ..."
+   echo "-d|--dir the target storage directory"
+   echo "This script works in three modes:"
+   echo "  -pc filter for perfect control, produces *.pc.bag"
+   echo "  -np filter for planning dependencies, produces *.np.bag"
+   echo "  -wp filter for planning and its dependencies, produces *.wp.bag"
 }
 
 perfect_control_topic="topic == '/apollo/prediction' \
@@ -43,26 +44,32 @@ is_perfect_control=false
 is_with_planning=false
 #create a rosbag only with planning's dependencies
 is_no_planning=false
-option_num=0
+work_mode_num=0
 
 #argument parsing code from https://stackoverflow.com/a/14203146
 POSITIONAL=()
+target_dir="."
 while [[ $# -gt 0 ]]; do
 key="$1"
 case $key in
     -pc|--perfectcontrol)
     is_perfect_control=true
-    option_num=$((option_num+1))
+    work_mode_num=$((work_mode_num+1))
     shift # past argument
     ;;
     -np|--noplanning)
     is_no_planning=true
-    option_num=$((option_num+1))
+    work_mode_num=$((work_mode_num+1))
     shift # past argument
     ;;
     -wp|--lib)
     is_with_planning=true
-    option_num=$((option_num+1))
+    work_mode_num=$((work_mode_num+1))
+    shift # past value
+    ;;
+    -d|--dir)
+    target_dir="$2"
+    shift # past argument
     shift # past value
     ;;
     -h|--help)
@@ -76,7 +83,7 @@ case $key in
 esac
 done
 
-if [[ $option_num -ne 1 ]]; then
+if [[ $work_mode_num -eq 0 ]]; then
    print_help
    exit 0
 fi
@@ -86,16 +93,17 @@ set -- "${POSITIONAL[@]}" # restore positional parameters
 function filter() {
     if $is_perfect_control; then
         target="${name%.*}.pc.bag"
-        rosbag filter $1 $target "$perfect_control_topic"
-    elif $is_no_planning; then
+        rosbag filter $1 "$target_dir/$target" "$perfect_control_topic"
+    fi
+
+    if $is_no_planning; then
         target="${name%.*}.np.bag"
-        rosbag filter $1 $target "$planning_deps"
-    elif $is_with_planning; then
+        rosbag filter $1 "$target_dir/$target" "$planning_deps"
+    fi
+
+    if $is_with_planning; then
         target="${name%.*}.wp.bag"
-        rosbag filter $1 $target "$planning_all"
-    else
-       print_help;
-       exit 0
+        rosbag filter $1 "$target_dir/$target" "$planning_all"
     fi
 }
 
