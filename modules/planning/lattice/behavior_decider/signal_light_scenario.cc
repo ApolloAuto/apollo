89abd9ac9f503a225fec20a9e1fdc2059cfedf75
liyun <30511797+liyunADU@users.noreply.github.com>
2017-12-07 16:27:06 -0800

    Implement reading traffic light (#1672)

--- modules/planning/lattice/behavior_decider/signal_light_scenario.cc
+++ modules/planning/lattice/behavior_decider/signal_light_scenario.cc
@@ -13,8 +13,18 @@
 
 #include "gflags/gflags.h"
 
+#include "modules/common/adapters/adapter_manager.h"
+#include "modules/common/vehicle_state/vehicle_state_provider.h"
+#include "modules/common/util/map_util.h"
+#include "modules/planning/proto/planning_internal.pb.h"
+
 namespace apollo {
 namespace planning {
+
+using apollo::common::adapter::AdapterManager;
+using apollo::perception::TrafficLight;
+using apollo::perception::TrafficLightDetection;
+using apollo::common::util::WithinBound;
 
 void SignalLightScenario::Reset() {}
 
@@ -52,12 +62,40 @@ int SignalLightScenario::ComputeScenarioDecision(
 
 bool SignalLightScenario::FindValidSignalLight(
     ReferenceLineInfo* const reference_line_info) {
-  return false;
+  const std::vector<hdmap::PathOverlap>& signal_lights =
+      reference_line_info->reference_line().map_path().signal_overlaps();
+  if (signal_lights.size() <= 0) {
+    ADEBUG << "No signal lights from reference line.";
+    return false;
+  }
+  signal_lights_along_reference_line_.clear();
+  for (const hdmap::PathOverlap& signal_light : signal_lights) {
+    if (signal_light.start_s + FLAGS_stop_max_distance_buffer >
+        reference_line_info->AdcSlBoundary().end_s()) {
+      signal_lights_along_reference_line_.push_back(&signal_light);
+    }
+  }
+  return signal_lights_along_reference_line_.size() > 0;
 }
 
 void SignalLightScenario::ReadSignals() {
   detected_signals_.clear();
-  return;
+  detected_signals_.clear();
+  if (AdapterManager::GetTrafficLightDetection()->Empty()) {
+    return;
+  }
+  if (AdapterManager::GetTrafficLightDetection()->GetDelaySec() >
+      FLAGS_signal_expire_time_sec) {
+    ADEBUG << "traffic signals msg is expired: "
+           << AdapterManager::GetTrafficLightDetection()->GetDelaySec();
+    return;
+  }
+  const TrafficLightDetection& detection =
+      AdapterManager::GetTrafficLightDetection()->GetLatestObserved();
+  for (int j = 0; j < detection.traffic_light_size(); j++) {
+    const TrafficLight& signal = detection.traffic_light(j);
+    detected_signals_[signal.id()] = &signal;
+  }
 }
 
 
