ac16d885ef7db270a134b46e6549e539a1c21e75
jiangyifei <jiangyifei@gmail.com>
2017-12-07 16:30:57 -0800

    tools: added tool for map generation.

---
+++ modules/tools/map_gen/plot_path.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+###############################################################################
+# Copyright 2017 The Apollo Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+###############################################################################
+
+import sys
+import matplotlib.pyplot as plt
+
+f = open(sys.argv[1], 'r')
+xs = []
+ys = []
+for line in f:
+    line = line.replace("\n", '')
+    data = line.split(',')
+    x = float(data[0])
+    y = float(data[1])
+    xs.append(x)
+    ys.append(y)
+f.close()
+
+fig = plt.figure()
+ax = plt.subplot2grid((1, 1), (0, 0))
+ax.plot(xs, ys, "b-", lw=3, alpha=0.8)
+ax.axis('equal')
+plt.show()
