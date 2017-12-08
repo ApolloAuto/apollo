5626b2702c83d5bb675c17f2eb6c0e81d425e565
huiyujiang <yujiang_hui@126.com>
2017-12-07 16:44:10 -0800

    perception: get radar extrinsics from file detectly

--- modules/perception/obstacle/onboard/radar_process_subnode.h
+++ modules/perception/obstacle/onboard/radar_process_subnode.h
@@ -41,6 +41,7 @@
 #include "modules/perception/obstacle/radar/modest/modest_radar_detector.h"
 #include "modules/perception/onboard/subnode.h"
 #include "modules/perception/proto/perception_obstacle.pb.h"
+#include "modules/perception/obstacle/common/pose_util.h"
 
 namespace apollo {
 namespace perception {
@@ -78,6 +79,7 @@ class RadarProcessSubnode : public Subnode {
   common::ErrorCode error_code_ = common::OK;
   RadarObjectData *radar_data_ = nullptr;
   std::string device_id_;
+  Eigen::Matrix4d radar_extrinsic_;
 
   boost::circular_buffer<ObjectPair> gps_buffer_;
   ContiRadarIDExpansion _conti_id_expansion;
