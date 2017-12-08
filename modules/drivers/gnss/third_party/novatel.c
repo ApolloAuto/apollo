920e92d845a8ad60106811609b683b9d9e3888f1
Kevie Feng <fengqikai1414@126.com>
2017-12-08 14:16:56 -0800

    gnss driver: add the copyright notices to every file under third_party directory (#1695)

--- modules/drivers/gnss/third_party/novatel.c
+++ modules/drivers/gnss/third_party/novatel.c
@@ -1,3 +1,38 @@
+/*********************************************************************************
+* The RTKLIB software package is distributed under the following BSD 2-clause
+* license (http://opensource.org/licenses/BSD-2-Clause) and additional two
+* exclusive clauses. Users are permitted to develop, produce or sell their own
+* non-commercial or commercial products utilizing, linking or including RTKLIB as
+* long as they comply with the license.
+* 
+*           Copyright (c) 2007-2013, T. Takasu, All rights reserved.
+* 
+* Redistribution and use in source and binary forms, with or without modification,
+* are permitted provided that the following conditions are met:
+* 
+* - Redistributions of source code must retain the above copyright notice, this
+*   list of conditions and the following disclaimer.
+* 
+* - Redistributions in binary form must reproduce the above copyright notice, this
+*   list of conditions and the following disclaimer in the documentation and/or
+*   other materials provided with the distribution.
+* 
+* - The software package includes some companion executive binaries or shared
+*   libraries necessary to execute APs on Windows. These licenses succeed to the
+*   original ones of these software. 
+* 
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+* AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+* IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+* ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+* LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+* CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+* GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+* HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+* LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF
+* THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+***********************************************************************************/
+
 /*------------------------------------------------------------------------------
 * notvatel.c : NovAtel OEM6/OEM5/OEM4/OEM3 receiver functions
 *
@@ -45,6 +80,11 @@
 *                           fix bug on beidou tracking codes
 *           2014/10/20 1.11 fix bug on receiver option -GL*,-RL*,-EL*
 *-----------------------------------------------------------------------------*/
+/**
+* file: novatel.c
+* version: rtklib ver.2.4.2
+* Copy from https://github.com/tomojitakasu/RTKLIB/tree/76b9c97257f304aedad38b5a6bbbac444724aab3/src/rcv/novatel.c
+*/
 #include "rtklib.h"
 #include <stdio.h>
 
