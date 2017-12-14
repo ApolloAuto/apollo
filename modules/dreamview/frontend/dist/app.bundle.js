e4e094d5701169307155603b51e9b2c9ba2d879a
vlin17 <thunder0504@gmail.com>
2017-12-14 12:19:01 -0800

    Dreamview: updated route editing bar icons

--- modules/dreamview/frontend/dist/app.bundle.js
+++ modules/dreamview/frontend/dist/app.bundle.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3b41bd0092a4e27f3dd1f39dc7f33f7577aa8250646570d7117ae4005968a147
-size 1340734
+oid sha256:7479916cc6ecd2dd767cb47953a207f932ee62a4a4acadeb020a49ace776a8a0
+size 1341963
