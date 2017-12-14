ae30709edad98feec70200c79957e2203d9e6e33
vlin17 <thunder0504@gmail.com>
2017-12-14 15:12:18 -0800

    Dreamview POI: don't set start point for multi-point POI

--- modules/dreamview/frontend/dist/app.bundle.js
+++ modules/dreamview/frontend/dist/app.bundle.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7479916cc6ecd2dd767cb47953a207f932ee62a4a4acadeb020a49ace776a8a0
-size 1341963
+oid sha256:777f30774cc4a1c3624ecb2d08d643c5f12e2704d67e8f785556131333f5bda8
+size 1341929
