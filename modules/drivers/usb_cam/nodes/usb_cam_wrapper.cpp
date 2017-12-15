8290f6138525e2592ea1f12757747074495fb0a2
fengkaiwen01 <fengkaiwen01@baidu.com>
2017-12-15 14:09:38 +0800

    usb_cam driver: revert code style to the referenced version

--- modules/drivers/usb_cam/nodes/usb_cam_wrapper.cpp
+++ modules/drivers/usb_cam/nodes/usb_cam_wrapper.cpp
@@ -7,223 +7,239 @@
 namespace usb_cam {
 
 UsbCamWrapper::UsbCamWrapper(ros::NodeHandle node, ros::NodeHandle private_nh) :
-    _node(node), _priv_node(private_nh), _last_stamp(0) {
-    // grab the parameters
-    _priv_node.param("topic_name", _topic_name, std::string("image_raw0"));
-    _priv_node.param("video_device", _video_device_name, std::string("/dev/video0"));
-    _priv_node.param("brightness", _brightness, -1); //0-255, -1 "leave alone"
-    _priv_node.param("contrast", _contrast, -1); //0-255, -1 "leave alone"
-    _priv_node.param("saturation", _saturation, -1); //0-255, -1 "leave alone"
-    _priv_node.param("sharpness", _sharpness, -1); //0-255, -1 "leave alone"
-    // possible values: mmap, read, userptr
-    _priv_node.param("io_method", _io_method_name, std::string("mmap"));
-    _priv_node.param("image_width", _image_width, 640);
-    _priv_node.param("image_height", _image_height, 480);
-    _priv_node.param("frame_rate", _framerate, 30);
-    // possible values: yuyv, uyvy, mjpeg, yuvmono10, rgb24
-    _priv_node.param("pixel_format", _pixel_format_name, std::string("mjpeg"));
-    // enable/disable autofocus
-    _priv_node.param("autofocus", _autofocus, false);
-    _priv_node.param("focus", _focus, -1); //0-255, -1 "leave alone"
-    // enable/disable autoexposure
-    _priv_node.param("autoexposure", _autoexposure, true);
-    _priv_node.param("exposure", _exposure, 100);
-    _priv_node.param("gain", _gain, -1); //0-100?, -1 "leave alone"
-    // enable/disable auto white balance temperature
-    _priv_node.param("auto_white_balance", _auto_white_balance, true);
-    _priv_node.param("white_balance", _white_balance, 4000);
+    node_(node), priv_node_(private_nh), last_stamp_(0) 
+{
+  // grab the parameters
+  priv_node_.param("topic_name", topic_name_, std::string("image_raw0"));
+  priv_node_.param("video_device", video_device_name_, std::string("/dev/video0"));
+  priv_node_.param("brightness", brightness_, -1); //0-255, -1 "leave alone"
+  priv_node_.param("contrast", contrast_, -1); //0-255, -1 "leave alone"
+  priv_node_.param("saturation", saturation_, -1); //0-255, -1 "leave alone"
+  priv_node_.param("sharpness", sharpness_, -1); //0-255, -1 "leave alone"
+  // possible values: mmap, read, userptr
+  priv_node_.param("io_method", io_method_name_, std::string("mmap"));
+  priv_node_.param("image_width", image_width_, 640);
+  priv_node_.param("image_height", image_height_, 480);
+  priv_node_.param("frame_rate", framerate_, 30);
+  // possible values: yuyv, uyvy, mjpeg, yuvmono10, rgb24
+  priv_node_.param("pixel_format", pixel_format_name_, std::string("mjpeg"));
+  // enable/disable autofocus
+  priv_node_.param("autofocus", autofocus_, false);
+  priv_node_.param("focus", focus_, -1); //0-255, -1 "leave alone"
+  // enable/disable autoexposure
+  priv_node_.param("autoexposure", autoexposure_, true);
+  priv_node_.param("exposure", exposure_, 100);
+  priv_node_.param("gain", gain_, -1); //0-100?, -1 "leave alone"
+  // enable/disable auto white balance temperature
+  priv_node_.param("auto_white_balance", auto_white_balance_, true);
+  priv_node_.param("white_balance", white_balance_, 4000);
 
-    // load the camera info
-    _priv_node.param("camera_frame_id", _img.header.frame_id, std::string("head_camera"));
-    _priv_node.param("camera_name", _camera_name, std::string("head_camera"));
-    _priv_node.param("camera_info_url", _camera_info_url, std::string(""));
-    _cinfo.reset(new camera_info_manager::CameraInfoManager(_node, _camera_name, _camera_info_url));
+  // load the camera info
+  priv_node_.param("camera_frame_id", img_.header.frame_id, std::string("head_camera"));
+  priv_node_.param("camera_name", camera_name_, std::string("head_camera"));
+  priv_node_.param("camera_info_url", camera_info_url_, std::string(""));
+  cinfo_.reset(new camera_info_manager::CameraInfoManager(node_, camera_name_, camera_info_url_));
 
-    // default 3000 ms
-    _priv_node.param("camera_timeout", _cam_timeout, 1000);
-    _priv_node.param("spin_interval", _spin_interval, 0.005f);
-    _priv_node.param("error_code", _error_code, 11);
+  // default 3000 ms
+  priv_node_.param("camera_timeout", cam_timeout_, 1000);
+  priv_node_.param("spin_interval", spin_interval_, 0.005f);
 
-    // Warning when diff with last > 1.5* interval
-    _frame_warning_interval = 1.5 / _framerate;
-    // now max fps 30, we use a appox time 0.9 to drop image.
-    _frame_drop_interval = 0.9 / _framerate;
+  // Warning when diff with last > 1.5* interval
+  frame_warning_interval_ = 1.5 / framerate_;
+  // now max fps 30, we use a appox time 0.9 to drop image.
+  frame_drop_interval_ = 0.9 / framerate_;
 
-    // advertise the main image topic
-    //image_transport::ImageTransport it(_node);
-    //image_pub_ = it.advertiseCamera(topic_name_, 1);
+  // advertise the main image topic
+  //image_transport::ImageTransport it(node_);
+  //image_pub_ = it.advertiseCamera(topic_name_, 1);
+  // Load transport publish plugin
+  std::string image_topic = node_.resolveName(topic_name_);
+  pub_loader_ = boost::make_shared<image_transport::PubLoader>("image_transport", "image_transport::PublisherPlugin");
+  std::string lookup_name = image_transport::PublisherPlugin::getLookupName(std::string("raw"));
+  image_pub_plugin_ = pub_loader_->createInstance(lookup_name);
+  if (image_pub_plugin_ != nullptr) 
+  {
+    image_pub_plugin_->advertise(node_, image_topic, 1, image_transport::SubscriberStatusCallback(),
+           image_transport::SubscriberStatusCallback(), ros::VoidPtr(), false);
+  }
+  else
+  {
+    ROS_ERROR("create image publish plugin error. lookup_name: '%s'", lookup_name.c_str());
+    node_.shutdown();
+    return;
+  }
 
-    // Load transport publish plugin
-    std::string image_topic = _node.resolveName(_topic_name);
-    _pub_loader = boost::make_shared<image_transport::PubLoader>("image_transport", "image_transport::PublisherPlugin");
-    std::string lookup_name = image_transport::PublisherPlugin::getLookupName(std::string("raw"));
-    _image_pub_plugin = _pub_loader->createInstance(lookup_name);
-    _image_pub_plugin->advertise(_node, image_topic, 1, image_transport::SubscriberStatusCallback(),
-                   image_transport::SubscriberStatusCallback(), ros::VoidPtr(), false);
+  // camera info publish
+  std::string cam_info_topic = image_transport::getCameraInfoTopic(image_topic);
+  cam_info_pub_ = node_.advertise<sensor_msgs::CameraInfo>(cam_info_topic, 1, ros::SubscriberStatusCallback(),
+          ros::SubscriberStatusCallback(), ros::VoidPtr(), false);
 
-    // camera info publish
-    std::string cam_info_topic = image_transport::getCameraInfoTopic(image_topic);
-    _cam_info_pub = _node.advertise<sensor_msgs::CameraInfo>(cam_info_topic, 1, ros::SubscriberStatusCallback(),
-                    ros::SubscriberStatusCallback(), ros::VoidPtr(), false);
+  // create Services
+  service_start_ = node_.advertiseService("start_capture", &UsbCamWrapper::service_start_cap, this);
+  service_stop_ = node_.advertiseService("stop_capture", &UsbCamWrapper::service_stop_cap, this);
 
-    // create Services
-    _service_start = _node.advertiseService("start_capture", &UsbCamWrapper::service_start_cap, this);
-    _service_stop = _node.advertiseService("stop_capture", &UsbCamWrapper::service_stop_cap, this);
+  // check for default camera info
+  if (!cinfo_->isCalibrated())
+  {
+    cinfo_->setCameraName(video_device_name_);
+    sensor_msgs::CameraInfo camera_info;
+    camera_info.header.frame_id = img_.header.frame_id;
+    camera_info.width = image_width_;
+    camera_info.height = image_height_;
+    cinfo_->setCameraInfo(camera_info);
+  }
+  //get the camera basical infomation
+  cam_info_.reset(new sensor_msgs::CameraInfo(cinfo_->getCameraInfo()));
 
-    // check for default camera info
-    if (!_cinfo->isCalibrated()) {
-        _cinfo->setCameraName(_video_device_name);
-        sensor_msgs::CameraInfo camera_info;
-        camera_info.header.frame_id = _img.header.frame_id;
-        camera_info.width = _image_width;
-        camera_info.height = _image_height;
-        _cinfo->setCameraInfo(camera_info);
-    }
-    //get the camera basical infomation
-    _cam_info.reset(new sensor_msgs::CameraInfo(_cinfo->getCameraInfo()));
+  ROS_INFO("Starting '%s' (%s) at %dx%d via %s (%s) at %i FPS", camera_name_.c_str(),
+           video_device_name_.c_str(),
+           image_width_, image_height_, io_method_name_.c_str(), pixel_format_name_.c_str(), framerate_);
 
-    ROS_INFO("Starting '%s' (%s) at %dx%d via %s (%s) at %i FPS", _camera_name.c_str(),
-             _video_device_name.c_str(),
-             _image_width, _image_height, _io_method_name.c_str(), _pixel_format_name.c_str(), _framerate);
+  // set the IO method
+  UsbCam::io_method io_method = UsbCam::io_method_from_string(io_method_name_);
 
-    // set the IO method
-    UsbCam::io_method io_method = UsbCam::io_method_from_string(_io_method_name);
+  if (io_method == UsbCam::IO_METHOD_UNKNOWN)
+  {
+    ROS_FATAL("Unknown IO method '%s'", io_method_name_.c_str());
+    node_.shutdown();
+    return;
+  }
 
-    if (io_method == UsbCam::IO_METHOD_UNKNOWN) {
-        ROS_FATAL("Unknown IO method '%s'", _io_method_name.c_str());
-        _node.shutdown();
-        return;
-    }
+  // set the pixel format
+  UsbCam::pixel_format pixel_format = UsbCam::pixel_format_from_string(pixel_format_name_);
 
-    // set the pixel format
-    UsbCam::pixel_format pixel_format = UsbCam::pixel_format_from_string(_pixel_format_name);
+  if (pixel_format == UsbCam::PIXEL_FORMAT_UNKNOWN)
+  {
+    ROS_FATAL("Unknown pixel format '%s'", pixel_format_name_.c_str());
+    node_.shutdown();
+    return;
+  }
 
-    if (pixel_format == UsbCam::PIXEL_FORMAT_UNKNOWN) {
-        ROS_FATAL("Unknown pixel format '%s'", _pixel_format_name.c_str());
-        _node.shutdown();
-        return;
-    }
+  // start the camera
+  cam_.start(video_device_name_.c_str(), io_method, pixel_format, image_width_, image_height_,
+         framerate_);
 
-    _cam.set_error_code(_error_code);
-    // start the camera
-    _cam.start(_video_device_name.c_str(), io_method, pixel_format, _image_width, _image_height,
-               _framerate);
+  // set camera parameters
+  if (brightness_ >= 0)
+    cam_.set_v4l_parameter("brightness", brightness_);
 
-    // set camera parameters
-    if (_brightness >= 0) {
-        _cam.set_v4l_parameter("brightness", _brightness);
-    }
+  if (contrast_ >= 0)
+    cam_.set_v4l_parameter("contrast", contrast_);
 
-    if (_contrast >= 0) {
-        _cam.set_v4l_parameter("contrast", _contrast);
-    }
+  if (saturation_ >= 0)
+    cam_.set_v4l_parameter("saturation", saturation_);
 
-    if (_saturation >= 0) {
-        _cam.set_v4l_parameter("saturation", _saturation);
-    }
+  if (sharpness_ >= 0)
+    cam_.set_v4l_parameter("sharpness", sharpness_);
 
-    if (_sharpness >= 0) {
-        _cam.set_v4l_parameter("sharpness", _sharpness);
-    }
+  if (gain_ >= 0)
+    cam_.set_v4l_parameter("gain", gain_);
 
-    if (_gain >= 0) {
-        _cam.set_v4l_parameter("gain", _gain);
-    }
+  // check auto white balance
+  if (auto_white_balance_)
+  {
+    cam_.set_v4l_parameter("white_balance_temperature_auto", 1);
+  }
+  else
+  {
+    cam_.set_v4l_parameter("white_balance_temperature_auto", 0);
+    cam_.set_v4l_parameter("white_balance_temperature", white_balance_);
+  }
 
-    // check auto white balance
-    if (_auto_white_balance) {
-        _cam.set_v4l_parameter("white_balance_temperature_auto", 1);
-    } else {
-        _cam.set_v4l_parameter("white_balance_temperature_auto", 0);
-        _cam.set_v4l_parameter("white_balance_temperature", _white_balance);
-    }
+  // check auto exposure
+  if (!autoexposure_)
+  {
+    // turn down exposure control (from max of 3)
+    cam_.set_v4l_parameter("exposure_auto", 1);
+    // change the exposure level
+    cam_.set_v4l_parameter("exposure_absolute", exposure_);
+  }
 
-    // check auto exposure
-    if (!_autoexposure) {
-        // turn down exposure control (from max of 3)
-        _cam.set_v4l_parameter("exposure_auto", 1);
-        // change the exposure level
-        _cam.set_v4l_parameter("exposure_absolute", _exposure);
-    }
+  // check auto focus
+  if (autofocus_) 
+  {
+    cam_.set_auto_focus(1);
+    cam_.set_v4l_parameter("focus_auto", 1);
+  }
+  else
+  {
+    cam_.set_v4l_parameter("focus_auto", 0);
 
-    // check auto focus
-    if (_autofocus) {
-        _cam.set_auto_focus(1);
-        _cam.set_v4l_parameter("focus_auto", 1);
-    } else {
-        _cam.set_v4l_parameter("focus_auto", 0);
-
-        if (_focus >= 0) {
-            _cam.set_v4l_parameter("focus_absolute", _focus);
-        }
-    }
+    if (focus_ >= 0)
+      cam_.set_v4l_parameter("focus_absolute", focus_);
+  }
 }
 
-UsbCamWrapper::~UsbCamWrapper() {
-    _cam.shutdown();
+UsbCamWrapper::~UsbCamWrapper()
+{
+  cam_.shutdown();
 }
 
 bool UsbCamWrapper::service_start_cap(std_srvs::Empty::Request& req,
-                                      std_srvs::Empty::Response& res) {
-    _cam.start_capturing();
-    return true;
+                                      std_srvs::Empty::Response& res)
+{
+  cam_.start_capturing();
+  return true;
 }
 
 bool UsbCamWrapper::service_stop_cap(std_srvs::Empty::Request& req,
-                                     std_srvs::Empty::Response& res) {
-    _cam.stop_capturing();
-    return true;
+                                     std_srvs::Empty::Response& res)
+{
+  cam_.stop_capturing();
+  return true;
 }
 
-bool UsbCamWrapper::take_and_send_image() {
-    // grab the image
-    bool get_new_image = _cam.grab_image(&_img, _cam_timeout);
+bool UsbCamWrapper::take_and_send_image()
+{
+  // grab the image
+  bool get_new_image = cam_.grab_image(&img_, cam_timeout_);
 
-    if (!get_new_image) {
-        return false;
+  if (!get_new_image)
+    return false;
+
+  // grab the camera info
+  //cam_info_ = sensor_msgs::CameraInfo(cinfo_->getCameraInfo());
+  cam_info_->header.frame_id = img_.header.frame_id;
+  cam_info_->header.stamp = img_.header.stamp;
+
+  if (last_stamp_ == ros::Time(0)) 
+  {
+    last_stamp_ = img_.header.stamp;
+  } 
+  else
+  {
+    auto diff = (img_.header.stamp - last_stamp_).toSec();
+    // drop image by frame_rate
+    if (diff < frame_drop_interval_) {
+      ROS_INFO_STREAM("drop image:" << img_.header.stamp);
+      return true;
     }
+    if (frame_warning_interval_ < diff)
+      ROS_WARN_STREAM("stamp jump.last stamp:" << last_stamp_
+          << " current stamp:" << img_.header.stamp);
+    last_stamp_ = img_.header.stamp;
+  }
 
-    // grab the camera info
-    //_cam_info = sensor_msgs::CameraInfo(cinfo_->getCameraInfo());
-    _cam_info->header.frame_id = _img.header.frame_id;
-    _cam_info->header.stamp = _img.header.stamp;
+  // publish the image
+  image_pub_plugin_->publish(img_);
+  cam_info_pub_.publish(cam_info_);
 
-    if (_last_stamp == ros::Time(0)) {
-        _last_stamp = _img.header.stamp;
-    } else {
-        auto diff = (_img.header.stamp - _last_stamp).toSec();
-        // drop image by frame_rate
-        if (diff < _frame_drop_interval) {
-            ROS_INFO_STREAM("drop image:" << _img.header.stamp);
-            return true;
-        }
-        if (_frame_warning_interval < diff) {
-            ROS_WARN_STREAM("stamp jump.last stamp:" << _last_stamp
-                    << " current stamp:" << _img.header.stamp);
-        }
-        _last_stamp = _img.header.stamp;
-    }
-
-    // publish the image
-    _image_pub_plugin->publish(_img);
-    _cam_info_pub.publish(_cam_info);
-
-    return true;
+  return true;
 }
 
-bool UsbCamWrapper::spin() {
-    // spin loop rate should be in accord with the trigger frequence
-    ros::Duration loop_interval(this->_spin_interval);
+bool UsbCamWrapper::spin()
+{
+  // spin loop rate should be in accord with the trigger frequence
+  ros::Duration loop_interval(this->spin_interval_);
 
-    while (_node.ok()) {
-        if (_cam.is_capturing()) {
-            if (!take_and_send_image()) {
-                ROS_ERROR("USB camera did not respond in time.");
-            }
-        }
-        //ros::spinOnce();
-        loop_interval.sleep();
-    }
-    return true;
+  while (node_.ok()) 
+  {
+    if (cam_.is_capturing())
+      if (!take_and_send_image())
+        ROS_ERROR("USB camera did not respond in time.");
+    //ros::spinOnce();
+    loop_interval.sleep();
+  }
+  return true;
 }
 }
