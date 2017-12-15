6d558abd046a74870b9630c725df5c95098fa73c
vlin17 <thunder0504@gmail.com>
2017-12-15 15:03:04 -0800

    Dreamview: arranged PNC graphs, updated sidebar

--- modules/dreamview/frontend/src/components/SideBar/ButtonPanel.js
+++ modules/dreamview/frontend/src/components/SideBar/ButtonPanel.js
@@ -2,19 +2,22 @@ import React from "react";
 import { observer } from "mobx-react";
 import classNames from "classnames";
 
-import Icon from "assets/images/sidebar/route_editing.png";
+import TasksIcon from "assets/images/sidebar/tasks.png";
+import ModuleControllerIcon from "assets/images/sidebar/module_controller.png";
+import LayerMenuIcon from "assets/images/sidebar/layer_menu.png";
+import RouteEditingIcon from "assets/images/sidebar/route_editing.png";
 
 class SideBarButton extends React.Component {
     render() {
-        const { disabled, onClick, active, label, extraClasses } = this.props;
+        const { disabled, onClick, active, label, extraClasses, iconSrc } = this.props;
         return (
             <button onClick={onClick}
                     disabled={disabled}
                     className={classNames({
                             "button": true,
-                            "active": active,
+                            "button-active": active,
                         }, extraClasses)}>
-                <img src={Icon} className="icon" />
+                <img src={iconSrc} className="icon" />
                 <div className="label">{label}</div>
             </button>
         );
@@ -27,31 +30,30 @@ export default class ButtonPanel extends React.Component {
                 onTasks, showTasks,
                 onModuleController, showModuleController,
                 onMenu, showMenu,
-                onRouteEditingBar, showRouteEditingBar,
-                onPOI, showPOI } = this.props;
+                onRouteEditingBar, showRouteEditingBar } = this.props;
 
         return (
-            <div>
+            <div className="main-panel">
                 <SideBarButton label="Tasks"
                                disabled={false}
+                               iconSrc={TasksIcon}
                                onClick={onTasks}
                                active={showTasks}/>
                 <SideBarButton label="Module Controller"
                                disabled={false}
+                               iconSrc={ModuleControllerIcon}
                                onClick={onModuleController}
                                active={showModuleController}/>
                 <SideBarButton label="Layer Menu"
                                disabled={enableHMIButtonsOnly}
+                               iconSrc={LayerMenuIcon}
                                onClick={onMenu}
                                active={showMenu} />
                 <SideBarButton label="Route Editing"
                                disabled={enableHMIButtonsOnly}
+                               iconSrc={RouteEditingIcon}
                                onClick={onRouteEditingBar}
                                active={showRouteEditingBar} />
-                <SideBarButton label="Default Routing"
-                               disabled={enableHMIButtonsOnly}
-                               onClick={onPOI}
-                               active={showPOI} />
             </div>
         );
     }
