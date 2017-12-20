cfc89858d22b10e6fb0a119b9a95ecfeaf39924f
Lei Wang <bestwanglei@gmail.com>
2017-12-19 20:12:33 -0800

    Docker: fix release and add OTA release.

---
+++ modules/tools/ota/create_sec_package.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+import os
+import sys
+sys.path.append('/home/caros/secure_upgrade/python')
+import secure_upgrade_export as sec_api
+
+root_config_path = "/home/caros/secure_upgrade/config/secure_config.json"
+returnCode = sec_api.init_secure_upgrade(root_config_path)
+if returnCode == True:
+    print 'Security environment init successfully!'
+else:
+    print 'Security environment init fail!'
+    exit()
+    
+homedir = os.environ['HOME']
+release_tgz = homedir + '/.cache/apollo_release.tar.gz'
+sec_release_tgz = homedir + '/.cache/sec_apollo_release.tar.gz'
+package_token = homedir + '/.cache/package_token'
+
+returnCode = sec_api.sec_upgrade_get_package(release_tgz, sec_release_tgz, package_token)
+if returnCode == True:
+    print 'Security package generated successfully!'
+else:
+    print 'Security package generated fail!'
