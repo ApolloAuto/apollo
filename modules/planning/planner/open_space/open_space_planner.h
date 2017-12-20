913324f78c195f6a5bb1f9eb61abbd69305ebedc
luoqi06 <luoqi06@baidu.com>
2017-12-19 17:49:44 -0800

    Planning: Add open_space planner

---
+++ modules/planning/planner/open_space/open_space_planner.h
@@ -0,0 +1,77 @@
+/******************************************************************************
+ * Copyright 2017 The Apollo Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *****************************************************************************/
+
+#ifndef MODULES_PLANNING_PLANNER_OPENSPACE_OPENSPACE_PLANNER_H_
+#define MODULES_PLANNING_PLANNER_OPENSPACE_OPENSPACE_PLANNER_H_
+
+#include <string>
+#include <vector>
+
+#include "Eigen/Eigen"
+#include "modules/planning/common/frame.h"
+#include "modules/planning/planner/planner.h"
+#include "modules/planning/proto/planning_config.pb.h"
+
+/**
+ * @namespace apollo::planning
+ * @brief apollo::planning
+ */
+namespace apollo {
+namespace planning {
+
+/**
+ * @class OpenSpacePlanner
+ * @brief OpenSpacePlanner is a derived class of Planner.
+ *        It reads a recorded trajectory from a trajectory file and
+ *        outputs proper segment of the trajectory according to vehicle
+ * position.
+ */
+class OpenSpacePlanner : public Planner {
+ public:
+  /**
+   * @brief Constructor
+   */
+  OpenSpacePlanner();
+
+  /**
+   * @brief Destructor
+   */
+  virtual ~OpenSpacePlanner() = default;
+
+  apollo::common::Status Init(const PlanningConfig& config) override;
+
+  /**
+   * @brief Overrode function Plan in parent class Planner.
+   * @param planning_init_point The trajectory point where planning starts.
+   * @param frame Current planning frame.
+   * @param reference_line_info The computed reference line.
+   * @return OK if planning succeeds; error otherwise.
+   */
+  apollo::common::Status Plan(
+      const common::TrajectoryPoint& planning_init_point, Frame* frame,
+      ReferenceLineInfo* reference_line_info) override;
+  /**
+   * @brief Read the recorded trajectory file.
+   * @param filename The name of the trajectory file.
+   */
+
+ private:
+};
+
+}  // namespace planning
+}  // namespace apollo
+
+#endif  // MODULES_PLANNING_PLANNER_OPENSPACE_OPENSPACE_PLANNER_H_
