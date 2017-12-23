43f28808592f67c22044299100e0e27e16ee816a
Zhang Liangliang <zhangliangliang@baidu.com>
2017-12-22 16:29:20 -0800

    Planning: ignore far away obstacles when adc's decision is stop with a closer distance.

--- modules/planning/common/path_decision.h
+++ modules/planning/common/path_decision.h
@@ -55,9 +55,15 @@ class PathDecision {
 
   void SetStBoundary(const std::string &id, const StBoundary &boundary);
   void EraseStBoundaries();
+  MainStop main_stop() const { return main_stop_; }
+  bool MergeWithMainStop(const ObjectStop &obj_stop, const std::string &obj_id,
+                         const ReferenceLine &ref_line,
+                         const SLBoundary &adc_sl_boundary);
 
  private:
   IndexedList<std::string, PathObstacle> path_obstacles_;
+  MainStop main_stop_;
+  double stop_reference_line_s_ = std::numeric_limits<double>::max();
 };
 
 }  // namespace planning
