cd1a34e7382a7c8217e75e6aab1a1784fb0d8f35
Jiangtao Hu <ycool@users.noreply.github.com>
2017-12-24 12:19:31 -0800

    dreamview: adjust dp path graph order. (#2038)

--- modules/dreamview/frontend/dist/app.bundle.js
+++ modules/dreamview/frontend/dist/app.bundle.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9b94609c9b313f421a5dfd2c21e739a7a236c7eb1f596e342f090d87cb2e63cc
-size 1348055
+oid sha256:f3564c4f57c3b0410ee0ca65690380f831ecb3e77e5af009432810a31475c9ee
+size 1335059
