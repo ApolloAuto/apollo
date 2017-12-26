6c3ab4427aec5435a9f29ba335cd6a366da03299
Zhang Liangliang <zhangliangliang@baidu.com>
2017-12-26 12:32:33 -0800

    Planning: added dkappa in planning path.

--- modules/planning/common/path/path_data.cc
+++ modules/planning/common/path/path_data.cc
@@ -185,11 +185,14 @@ bool PathData::SLToXY(const FrenetFramePath &frenet_path,
 
     if (path_points.empty()) {
       path_point.set_s(0.0);
+      path_point.set_dkappa(0.0);
     } else {
       common::math::Vec2d last(path_points.back().x(), path_points.back().y());
       common::math::Vec2d current(path_point.x(), path_point.y());
       double distance = (last - current).Length();
       path_point.set_s(path_points.back().s() + distance);
+      path_point.set_dkappa((path_point.kappa() - path_points.back().kappa()) /
+                            distance);
     }
     path_points.push_back(std::move(path_point));
   }
