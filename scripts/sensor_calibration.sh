a3670fb009116700d5a1a92af4ff3a0494ef055c
xiaohuitu <lxh1988924@163.com>
2017-12-27 20:53:00 +0800

    calibration: revise sensor calibration script to relocate output log

--- scripts/sensor_calibration.sh
+++ scripts/sensor_calibration.sh
@@ -31,7 +31,7 @@ function calibrate_camera_camera() {
     echo "Start to calibrate Camera-Camera extrinsics, Ctrl+C to exit."
     eval "${APOLLO_ROOT_DIR}/modules/calibration/${MODULE}/${MODULE} \
       --flagfile=${APOLLO_ROOT_DIR}/modules/calibration/${MODULE}/conf/${MODULE}.conf \
-      >${LOG} 2>&1"
+      | tee ${LOG}"
   fi
 }
 
@@ -46,7 +46,7 @@ function calibrate_lidar_camera() {
     echo "Start to calibrate LiDAR-Camera extrinsics, Ctrl+C to exit."
     eval "${APOLLO_ROOT_DIR}/modules/calibration/${MODULE}/${MODULE} \
       --flagfile=${APOLLO_ROOT_DIR}/modules/calibration/${MODULE}/conf/${MODULE}.conf \
-      2>${LOG}"
+      | tee ${LOG}"
   fi
 }
 
@@ -61,7 +61,7 @@ function calibrate_radar_camera() {
     echo "Start to calibrate Radar-Camera extrinsics, Ctrl+C to exit."
     eval "${APOLLO_ROOT_DIR}/modules/calibration/${MODULE}/${MODULE} \
       --flagfile=${APOLLO_ROOT_DIR}/modules/calibration/${MODULE}/conf/${MODULE}.conf \
-      2>${LOG}"
+      | tee ${LOG}"
   fi
 }
 
@@ -76,7 +76,7 @@ function visualize_radar_lidar() {
     echo "Visualize Radar and LiDAR data, Ctrl+C to exit."
     eval "${APOLLO_ROOT_DIR}/modules/calibration/${MODULE}/${MODULE} \
       --flagfile=${APOLLO_ROOT_DIR}/modules/calibration/${MODULE}/conf/${MODULE}.conf \
-      2>${LOG}"
+      |tee ${LOG}"
   fi
 }
 
