716f4d22d9fe936d8bcb9f335c04b7f70cbb4a41
Dong Li <lidong.main@gmail.com>
2017-12-28 22:05:36 -0800

    e2e: publish e2e module by web downloading

--- modules/e2e/online_system/px2_camera_image/src/camera_px2.h
+++
@@ -1,9 +0,0 @@
-#ifndef CAMERA_PX2_H
-#define CAMERA_PX2_H
-void init(int& image_width, int& image_height, const char* csi_port);
-void start();
-bool read_frame(unsigned char** image_data);
-void reset_frame();
-void stop();
-void release();
-#endif
