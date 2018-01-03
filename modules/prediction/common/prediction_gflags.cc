e4644850a6d6ae163c7e617b2594e6bbaa326d73
kechxu <kechxu89@gmail.com>
2018-01-03 15:22:51 -0800

    Prediction: fix obstacle split

--- modules/prediction/common/prediction_gflags.cc
+++ modules/prediction/common/prediction_gflags.cc
@@ -92,6 +92,8 @@ DEFINE_double(valid_position_diff_threshold, 0.5,
               "threshold of valid position difference");
 DEFINE_double(valid_position_diff_rate_threshold, 0.075,
               "threshold of valid position difference rate");
+DEFINE_double(split_rate, 0.5,
+              "obstacle split rate for adjusting velocity");
 DEFINE_double(rnn_min_lane_relatice_s, 5.0,
               "Minimal relative s for RNN model.");
 DEFINE_bool(enable_adjust_velocity_heading, false,
