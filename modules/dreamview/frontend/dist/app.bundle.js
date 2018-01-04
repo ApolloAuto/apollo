9b497c08475505025255565020c0324c7918bb8e
siyangy <omega115210@gmail.com>
2018-01-03 21:51:15 -0800

    Dreamview: further reduce CPU load

--- modules/dreamview/frontend/dist/app.bundle.js
+++ modules/dreamview/frontend/dist/app.bundle.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d9aed352b670da8c6c6e4ed84e02ef5f18f549f47d7e069324606a62f2c35aa0
-size 1350284
+oid sha256:01bd9c3fb7d6ae062395ff9a62ddca25dd58d928f11e07bc449969598d905497
+size 1350241
