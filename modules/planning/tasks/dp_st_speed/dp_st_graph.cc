6a6e1d274b3d04d35d757f67ebba7cb2c3cf4f7f
Liangliang Zhang <zhangliangliang@baidu.com>
2018-01-11 14:47:38 -0800

    Planning: fixed a bug related to casting in dp_st_graph.cc

--- modules/planning/tasks/dp_st_speed/dp_st_graph.cc
+++ modules/planning/tasks/dp_st_speed/dp_st_graph.cc
@@ -263,7 +263,7 @@ void DpStGraph::CalculateCostAt(const uint32_t c, const uint32_t r) {
   if (c == 2) {
     for (uint32_t r_pre = r_low; r_pre <= r; ++r_pre) {
       const double acc =
-          (static_cast<int>(r - 2 * r_pre)) * unit_s_ / (unit_t_ * unit_t_);
+          (r * unit_s_ - 2 * r_pre * unit_s_) / (unit_t_ * unit_t_);
       if (acc < dp_st_speed_config_.max_deceleration() ||
           acc > dp_st_speed_config_.max_acceleration()) {
         continue;
@@ -291,10 +291,10 @@ void DpStGraph::CalculateCostAt(const uint32_t c, const uint32_t r) {
       continue;
     }
 
-    const double curr_a =
-        (cost_cr.index_s() + pre_col[r_pre].pre_point()->index_s() -
-         2 * pre_col[r_pre].index_s()) *
-        unit_s_ / (unit_t_ * unit_t_);
+    const double curr_a = (cost_cr.index_s() * unit_s_ +
+                           pre_col[r_pre].pre_point()->index_s() * unit_s_ -
+                           2 * pre_col[r_pre].index_s() * unit_s_) /
+                          (unit_t_ * unit_t_);
     if (curr_a > vehicle_param_.max_acceleration() ||
         curr_a < vehicle_param_.max_deceleration()) {
       continue;
