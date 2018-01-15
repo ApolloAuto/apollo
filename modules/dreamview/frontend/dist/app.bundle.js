25b8b9bd57f0d199b384be6f5f2ee66a5b742411
Vivian Lin <thunder0504@gmail.com>
2018-01-15 15:57:54 -0800

    draw virtual lines if the type is not DOTTED_WHITE (#2464)

--- modules/dreamview/frontend/dist/app.bundle.js
+++ modules/dreamview/frontend/dist/app.bundle.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:faa4a5bc586a958c0d910a7ae8ddce27704276ebfae2a3b00d03c1bf5b06d89f
-size 1350090
+oid sha256:b4635e7c0c7ea6025892bd600ab5395ac9928ea0c14046a717af63a29205ab35
+size 1350118
