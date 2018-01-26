bf74a1d37999a951df5f4b8e6293a8d72bdba5a2
siyangy <omega115210@gmail.com>
2018-01-25 18:00:01 -0800

    Add stop sign rendering

--- modules/dreamview/frontend/dist/app.bundle.js
+++ modules/dreamview/frontend/dist/app.bundle.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ddc96dad51ff4ed42edb49c56e1009d8c170f5721d3ac6697a7a5f5d200520c3
-size 1423685
+oid sha256:cc74314bdea9cad6d8e3c82c17db1e9f57970bc58ea806451fee4744205c13bf
+size 1426008
