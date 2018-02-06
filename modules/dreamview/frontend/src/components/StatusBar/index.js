1d1476da50248da57c282e5d1a180fe87bdd5969
vlin17 <thunder0504@gmail.com>
2018-02-06 14:04:17 -0800

    Dreamview: playing sound when car status changes

--- modules/dreamview/frontend/src/components/StatusBar/index.js
+++ modules/dreamview/frontend/src/components/StatusBar/index.js
@@ -4,6 +4,7 @@ import { observer } from "mobx-react";
 import AutoMeter from "components/StatusBar/AutoMeter";
 import Notification from "components/StatusBar/Notification";
 import TrafficLightIndicator from "components/StatusBar/TrafficLightIndicator";
+import DrivingMode from "components/StatusBar/DrivingMode";
 import Wheel from "components/StatusBar/Wheel";
 
 
@@ -16,15 +17,16 @@ export default class StatusBar extends React.Component {
             <div className="status-bar">
                 {showNotification && <Notification monitor={monitor} />}
                 <AutoMeter throttlePercent={meters.throttlePercent}
-                           brakePercent={meters.brakePercent}
-                           speed={meters.speed} />
+                    brakePercent={meters.brakePercent}
+                    speed={meters.speed} />
                 <Wheel steeringPercentage={meters.steeringPercentage}
-                       steeringAngle={meters.steeringAngle}
-                       turnSignal={meters.turnSignal} />
-                <TrafficLightIndicator
-                       trafficLightColor={trafficSignal.color}
-                       drivingMode={meters.drivingMode}
-                       isAutoMode={meters.isAutoMode}/>
+                    steeringAngle={meters.steeringAngle}
+                    turnSignal={meters.turnSignal} />
+                <div className="traffic-light-and-driving-mode">
+                    <TrafficLightIndicator colorName={trafficSignal.color} />
+                    <DrivingMode drivingMode={meters.drivingMode}
+                        isAutoMode={meters.isAutoMode} />
+                </div>
             </div>
         );
     }
