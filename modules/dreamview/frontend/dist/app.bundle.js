5e49be1cd69719e063da4569dff529a7e9a991c1
vlin17 <thunder0504@gmail.com>
2018-02-06 15:22:02 -0800

    Dreamview: storing state of taskPanelLock

--- modules/dreamview/frontend/dist/app.bundle.js
+++ modules/dreamview/frontend/dist/app.bundle.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2d14c9ff151a0b9e79ed7b71f32e5fe0363d74a05678bab1079d500238f5b009
-size 1433669
+oid sha256:9a459b35580fb165ff75baec632cadc4e20011735d9e7a48bab1d89c4fa962aa
+size 1433621
