1ecbb6d1fad409fc2e1311c4540661a916edb607
vlin17 <thunder0504@gmail.com>
2018-02-06 15:22:02 -0800

    Dreamview: storing state of taskPanelLock

--- modules/dreamview/frontend/src/components/Tasks/index.js
+++ modules/dreamview/frontend/src/components/Tasks/index.js
@@ -6,28 +6,11 @@ import Delay from "components/Tasks/Delay";
 import Console from "components/Tasks/Console";
 
 export default class Tasks extends React.Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            isPanelLocked: false,
-        };
-
-        this.toggleLock = this.toggleLock.bind(this);
-    }
-
-    toggleLock() {
-        this.setState({
-          isPanelLocked: !this.state.isPanelLocked,
-        });
-    }
-
     render() {
         return (
             <div className="tasks">
-                <QuickStart isPanelLocked={this.state.isPanelLocked} />
-                <Others toggleLock={this.toggleLock}
-                        isPanelLocked={this.state.isPanelLocked} />
+                <QuickStart />
+                <Others />
                 <Delay />
                 <Console />
             </div>
