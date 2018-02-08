4cbdb80bf6412ec9e341b310e75aa76676fb022c
jmtao <taojm99@gmail.com>
2018-02-08 15:17:32 -0800

    planning: move all obstacle creation to a central place (frame)

--- modules/planning/common/planning_gflags.h
+++ modules/planning/common/planning_gflags.h
@@ -118,7 +118,6 @@ DECLARE_double(within_lane_bound);
 
 DECLARE_double(virtual_stop_wall_length);
 DECLARE_double(virtual_stop_wall_height);
-DECLARE_string(reference_line_end_obstacle_id);
 
 DECLARE_double(prediction_total_time);
 DECLARE_bool(align_prediction_time);
@@ -154,17 +153,17 @@ DECLARE_double(max_valid_stop_distance);
 DECLARE_double(creep_stop_distance);
 /// keep_clear
 DECLARE_bool(enable_keep_clear);
-DECLARE_string(keep_clear_virtual_object_id_prefix);
-DECLARE_string(keep_clear_junction_virtual_object_id_prefix);
+DECLARE_string(keep_clear_virtual_obstacle_id_prefix);
+DECLARE_string(keep_clear_junction_virtual_obstacle_id_prefix);
 DECLARE_double(keep_clear_min_pass_distance);
 /// traffic light
 DECLARE_bool(enable_traffic_light);
-DECLARE_string(signal_light_virtual_object_id_prefix);
+DECLARE_string(signal_light_virtual_obstacle_id_prefix);
 DECLARE_double(max_stop_deacceleration_for_yellow_light);
 DECLARE_double(traffic_light_stop_distance);
 /// crosswalk
 DECLARE_bool(enable_crosswalk);
-DECLARE_string(crosswalk_virtual_object_id_prefix);
+DECLARE_string(crosswalk_virtual_obstacle_id_prefix);
 DECLARE_double(crosswalk_expand_distance);
 DECLARE_double(crosswalk_strick_l_distance);
 DECLARE_double(crosswalk_loose_l_distance);
@@ -173,7 +172,7 @@ DECLARE_double(crosswalk_stop_distance);
 /// stop_sign
 DECLARE_bool(enable_stop_sign);
 DECLARE_bool(enable_stop_sign_creeping);
-DECLARE_string(stop_sign_virtual_object_id_prefix);
+DECLARE_string(stop_sign_virtual_obstacle_id_prefix);
 DECLARE_double(stop_sign_stop_duration);
 DECLARE_double(stop_sign_min_pass_distance);
 DECLARE_double(stop_sign_stop_distance);
@@ -183,6 +182,8 @@ DECLARE_double(stop_sign_creep_buffer);
 DECLARE_string(destination_obstacle_id);
 DECLARE_double(destination_check_distance);
 DECLARE_double(destination_stop_distance);
+/// reference_line end
+DECLARE_string(reference_line_end_obstacle_id_prefix);
 
 DECLARE_bool(enable_sqp_solver);
 
