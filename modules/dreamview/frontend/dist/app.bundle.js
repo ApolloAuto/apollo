ba2a60bca241d0f18e3ecb3c944ccc93783ec095
vlin17 <thunder0504@gmail.com>
2018-02-14 12:38:39 -0800

    Dreamview: adding speechSynthesis existence detection

--- modules/dreamview/frontend/dist/app.bundle.js
+++ modules/dreamview/frontend/dist/app.bundle.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:898b5deea688ddab7d490e5ef0a77adcdae67afad1c1fef07b33105caf53330a
-size 1355623
+oid sha256:e829efb5dba7a2b7adc8d190bf7ed44d60b6601b24da89cd1251004e98487819
+size 1355633
