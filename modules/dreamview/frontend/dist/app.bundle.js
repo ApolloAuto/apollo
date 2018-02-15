815ed78423569aab4bb324e5c433a3a033b58281
unacao <yufcao@gmail.com>
2018-02-15 15:00:30 -0800

    use pcl voxel grid to downsample point cloud

--- modules/dreamview/frontend/dist/app.bundle.js
+++ modules/dreamview/frontend/dist/app.bundle.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ed052989960f687619c2703d6119e5990abc8bf30d8a1167ff007e2cdf10a0bb
-size 1355896
+oid sha256:21e34e899ccde9d858b8fc295cdd73be40511ab2824ce3dddbc3fc9eddf6709b
+size 1355893
