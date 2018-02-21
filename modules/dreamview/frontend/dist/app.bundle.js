7569cfc019575b7ddc24dcc451fa53670e55772b
Vivian Lin <thunder0504@gmail.com>
2018-02-21 14:25:59 -0800

    Dreamview: handle fragmented message from websocket (#2929)
    * Dreamview: handle fragmented message from websocket
    
    * fixup

--- modules/dreamview/frontend/dist/app.bundle.js
+++ modules/dreamview/frontend/dist/app.bundle.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:21e34e899ccde9d858b8fc295cdd73be40511ab2824ce3dddbc3fc9eddf6709b
+oid sha256:3c221b825da19698fb4820bb005f1c4299a5dade2540ee86806ce0c6c7508cdc
 size 1355893
