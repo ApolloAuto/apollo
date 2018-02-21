4c48024f3a82b009d2cad5e5e9cf0e0c6cf06e12
Dong Li <lidong.main@gmail.com>
2018-02-20 21:06:15 -0800

    planning: clean up unused gflags

--- modules/routing/common/routing_gflags.h
+++ modules/routing/common/routing_gflags.h
@@ -1,18 +1,18 @@
 /******************************************************************************
-  * Copyright 2017 The Apollo Authors. All Rights Reserved.
-  *
-  * Licensed under the Apache License, Version 2.0 (the "License");
-  * you may not use this file except in compliance with the License.
-  * You may obtain a copy of the License at
-  *
-  * http://www.apache.org/licenses/LICENSE-2.0
-  *
-  * Unless required by applicable law or agreed to in writing, software
-  * distributed under the License is distributed on an "AS IS" BASIS,
-  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  * See the License for the specific language governing permissions and
-  * limitations under the License.
-  *****************************************************************************/
+ * Copyright 2017 The Apollo Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *****************************************************************************/
 
 #ifndef MODULES_ROUTING_COMMON_ROUTING_GFLAGS_H_
 #define MODULES_ROUTING_COMMON_ROUTING_GFLAGS_H_
@@ -24,7 +24,6 @@ DECLARE_string(routing_node_name);
 
 DECLARE_string(routing_adapter_config_filename);
 
-DECLARE_bool(use_road_id);
 DECLARE_double(min_length_for_lane_change);
 DECLARE_bool(enable_change_lane_in_result);
 
