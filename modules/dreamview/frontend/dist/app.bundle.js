cd11539efa7b1885c43f5bb77b1f162204297657
siyangy <yu.siyang@yahoo.com>
2018-02-22 14:47:38 -0800

    Dreamview: downsample prediction points (#2953)
    * Dreamview: downsample prediction points
    
    * CR response

--- modules/dreamview/frontend/dist/app.bundle.js
+++ modules/dreamview/frontend/dist/app.bundle.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f1954e7caa1e3d925d2d1589d4e06f2e74ff028f26f49860d7f6a0bf446e4d35
-size 1356714
+oid sha256:6036ff5c494fc1096c144b995769f0fc2e85c15ca0ad6301c38d599726486ff3
+size 1356737
