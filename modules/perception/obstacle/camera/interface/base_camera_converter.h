1701f99f11dfe8269d2f6df6e6433e5b0f62e564
cheni-kuei <cheni-kuei@baidu.com>
2018-02-21 16:24:54 -0800

    perception: add original interface definition, to be modified later

--- modules/perception/obstacle/camera/interface/base_camera_converter.h
+++ modules/perception/obstacle/camera/interface/base_camera_converter.h
@@ -15,3 +15,89 @@
  *****************************************************************************/
 
 // The base class of converting 2D detections into 3D objects
+
+
+#ifndef ADU_PERCEPTION_OBSTACLE_CAMERA_INTERFACE_BASE_CAMERA_TRANSFORMER_H
+#define ADU_PERCEPTION_OBSTACLE_CAMERA_INTERFACE_BASE_CAMERA_TRANSFORMER_H
+
+// SAMPLE CODE:
+//
+// class DefaultCameraTransformer : public BaseCameraTransformer {
+// public:
+//     DefaultCameraTransformer() : BaseCameraTransformer() {}
+//     virtual ~DefaultCameraTransformer() {}
+//
+//     virtual bool init() override {
+//         // Do something.
+//         return true;
+//     }
+//
+//     virtual bool transform(const cv::Mat& frame,
+//              const CameraTransformerOptions& options,
+//              std::vector<VisualObjectPtr>* objects) override {
+//          // Do something.
+//          return true;
+//      }
+//
+//      virtual std::string name() const override {
+//          return "DefaultCameraTransformer";
+//      }
+//
+// };
+//
+// // Register plugin.
+// REGISTER_CAMERA_TRANSFORMER(DefaultCameraTransformer);
+////////////////////////////////////////////////////////
+// USING CODE:
+//
+// BaseCameraTransformer* camera_transformer =
+//    BaseCameraTransformerRegisterer::get_instance_by_name("DefaultCameraTransformer");
+// using camera_transformer to do somethings.
+// ////////////////////////////////////////////////////
+
+#include <string>
+#include <vector>
+
+#include <Eigen/Core>
+#include <opencv2/opencv.hpp>
+
+#include "lib/base/noncopyable.h"
+#include "lib/base/registerer.h"
+#include "obstacle/camera/common/visual_object.h"
+
+namespace adu {
+namespace perception {
+namespace obstacle {
+
+struct CameraTransformerOptions {
+};
+
+class BaseCameraTransformer {
+public:
+    BaseCameraTransformer() = default;
+    virtual ~BaseCameraTransformer() = default;
+
+    virtual bool init() = 0;
+
+    // @brief: Frame from camera  -> objects.
+    // @param [in]: raw frame from camera.
+    // @param [in]: options.
+    // @param [out]: transformed objects.
+    virtual bool transform(const cv::Mat& frame,
+            const CameraTransformerOptions& options,
+            std::vector<VisualObjectPtr>* objects) = 0;
+
+    virtual std::string name() const = 0;
+
+private:
+    DISALLOW_COPY_AND_ASSIGN(BaseCameraTransformer);
+};
+
+REGISTER_REGISTERER(BaseCameraTransformer);
+#define REGISTER_CAMERA_TRANSFORMER(name) REGISTER_CLASS(BaseCameraTransformer, name)
+
+}  // namespace obstacle
+}  // namespace perception
+}  // namespace adu
+
+#endif  // ADU_PERCEPTION_OBSTACLE_CAMERA_INTERFACE_BASE_CAMERA_TRANSFORMER_H
--- modules/perception/obstacle/camera/interface/base_camera_detector.h
+++ modules/perception/obstacle/camera/interface/base_camera_detector.h
@@ -15,3 +15,109 @@
  *****************************************************************************/
 
 // The base class of camera 2D object detection
+
+#ifndef ADU_PERCEPTION_OBSTACLE_CAMERA_INTERFACE_BASE_CAMERA_DETECTOR_H
+#define ADU_PERCEPTION_OBSTACLE_CAMERA_INTERFACE_BASE_CAMERA_DETECTOR_H
+
+// SAMPLE CODE:
+//
+// class DefaultCameraDetector : public BaseCameraDetector {
+// public:
+//     DefaultCameraDetector() : BaseCameraDetector() {}
+//     virtual ~DefaultCameraDetector() {}
+//
+//     virtual bool init(const CameraDetectorInitOptions& options) override {
+//         // Do something.
+//         return true;
+//     }
+//
+//     virtual bool detect(const cv::Mat& frame,
+//              const CameraDetectorOptions& options,
+//              std::vector<VisualObjectPtr>* objects) override {
+//          // Do something.
+//          return true;
+//      }
+//
+//      virtual std::string name() const override {
+//          return "DefaultCameraDetector";
+//      }
+//
+// };
+//
+// // Register plugin.
+// REGISTER_CAMERA_DETECTOR(DefaultCameraDetector);
+////////////////////////////////////////////////////////
+// USING CODE:
+//
+// BaseCameraDetector* camera_detector =
+//    BaseCameraDetectorRegisterer::get_instance_by_name("DefaultCameraDetector");
+// using camera_detector to do somethings.
+// ////////////////////////////////////////////////////
+
+#include <string>
+#include <vector>
+
+#include <Eigen/Core>
+#include <opencv2/opencv.hpp>
+
+#include "lib/base/noncopyable.h"
+#include "lib/base/registerer.h"
+#include "obstacle/camera/common/visual_object.h"
+#include "obstacle/common/camera.h"
+
+namespace adu {
+namespace perception {
+namespace obstacle {
+
+struct CameraDetectorInitOptions {
+    std::shared_ptr<CameraDistortD> intrinsic;
+};
+
+struct CameraDetectorOptions {
+    cv::Mat gray_frame;
+    cv::Mat range_frame;
+    std::shared_ptr<CameraDistortD> intrinsic;
+    std::shared_ptr<Eigen::Matrix4d> extrinsic_ground2camera;
+    std::shared_ptr<Eigen::Matrix4d> extrinsic_stereo;
+};
+
+class BaseCameraDetector {
+public:
+    BaseCameraDetector() = default;
+    virtual ~BaseCameraDetector() = default;
+
+    virtual bool init(const CameraDetectorInitOptions& options
+            = CameraDetectorInitOptions()) = 0;
+
+    // @brief: Frame from camera -> objects.
+    // @param [in]: raw frame from camera.
+    // @param [in]: options.
+    // @param [out]: detected objects.
+    virtual bool detect(const cv::Mat& frame,
+            const CameraDetectorOptions& options,
+            std::vector<VisualObjectPtr>* objects) = 0;
+    virtual bool multitask(const cv::Mat& frame,
+                        const CameraDetectorOptions& options,
+                        std::vector<VisualObjectPtr>* objects, cv::Mat *mask){
+        return false;
+    };
+    // @brief: Extract feature for each object.
+    // @param [in/out]: objects with 2D bbox.
+    virtual bool extract(std::vector<VisualObjectPtr>* objects) {
+        return true;
+    }
+
+    virtual std::string name() const = 0;
+
+private:
+    DISALLOW_COPY_AND_ASSIGN(BaseCameraDetector);
+};
+
+REGISTER_REGISTERER(BaseCameraDetector);
+#define REGISTER_CAMERA_DETECTOR(name) REGISTER_CLASS(BaseCameraDetector, name)
+
+}  // namespace obstacle
+}  // namespace perception
+}  // namespace adu
+
+#endif  // ADU_PERCEPTION_OBSTACLE_CAMERA_INTERFACE_BASE_CAMERA_DETECTOR_H
