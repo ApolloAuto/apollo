5ff32cb4ab3954ef647ba20f9e6b66aeb85c8dd2
cheni-kuei <cheni-kuei@baidu.com>
2018-02-21 16:24:54 -0800

    perception: add original interface definition, to be modified later

--- modules/perception/obstacle/camera/interface/base_camera_transformer.h
+++ modules/perception/obstacle/camera/interface/base_camera_transformer.h
@@ -16,3 +16,26 @@
 
 // The base class of transforming camera space objects into other defined
 // 3D spaces, like world space or ego-car space
+
+void trans_object_to_world(const CameraTrackerOptions& options,
+                           const std::vector<VisualObjectPtr> *visual_objects) {
+    if (options.camera2world_pose == nullptr) {
+        LOG(INFO) << "can not trans object from camera to world";
+        return;
+    }
+    for (size_t i = 0; i < visual_objects->size(); ++i) {
+
+        float theta = (*visual_objects)[i]->theta;
+        Eigen::Vector4d dir(cos(theta), 0, -sin(theta), 0.0);
+
+        dir = (*options.camera2world_pose) * dir;
+        (*visual_objects)[i]->direction = dir.head(3);
+        (*visual_objects)[i]->theta = atan2(dir[1],dir[0]);
+
+        Eigen::Vector3d center = (*visual_objects)[i]->center;
+        Eigen::Vector4d ctr(center.x(), center.y(), center.z(), 1.0);
+        (*visual_objects)[i]->center = ((*options.camera2world_pose) * ctr).head(3);
+
+        XLOG(DEBUG) << "world center" << (*visual_objects)[i]->center;
+    }
+}
