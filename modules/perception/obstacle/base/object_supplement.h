dd5ac2b1ee7c031702ed79e35dbf09ed4c28cd23
Weide Zhang <zhangweide@baidu.com>
2018-02-21 19:23:52 -0800

    visualizer for camera-radar based perception

--- modules/perception/obstacle/base/object_supplement.h
+++ modules/perception/obstacle/base/object_supplement.h
@@ -16,8 +16,11 @@
 #ifndef MODULES_PERCEPTION_OBSTACLE_BASE_OBJECT_SUPPLEMENT_H_
 #define MODULES_PERCEPTION_OBSTACLE_BASE_OBJECT_SUPPLEMENT_H_
 
+#include <boost/circular_buffer.hpp>
+#include <opencv2/opencv.hpp>
 #include <memory>
-
+#include <string>
+#include <vector>
 #include "Eigen/Core"
 
 #include "modules/perception/obstacle/base/types.h"
@@ -36,6 +39,90 @@ struct alignas(16) RadarSupplement {
 };
 typedef std::shared_ptr<RadarSupplement> RadarSupplementPtr;
 typedef std::shared_ptr<const RadarSupplement> RadarSupplementConstPtr;
+
+struct alignas(16) CameraFrameSupplement {
+  CameraFrameSupplement();
+  ~CameraFrameSupplement();
+  CameraFrameSupplement(const CameraFrameSupplement& rhs);
+  CameraFrameSupplement& operator=(const CameraFrameSupplement& rhs);
+  void clone(const CameraFrameSupplement& rhs);
+
+  cv::Mat depth_map;
+  cv::Mat label_map;
+  cv::Mat lane_map;
+  cv::Mat img_src;
+  std::string source_topic;
+};
+
+typedef std::shared_ptr<CameraFrameSupplement> CameraFrameSupplementPtr;
+
+typedef std::shared_ptr<const CameraFrameSupplement>
+    CameraFrameSupplementConstPtr;
+
+struct alignas(16) CameraSupplement {
+  CameraSupplement();
+  ~CameraSupplement();
+  CameraSupplement(const CameraSupplement& rhs);
+  CameraSupplement& operator=(const CameraSupplement& rhs);
+  void clone(const CameraSupplement& rhs);
+
+  // upper-left corner: x1, y1
+  Eigen::Vector2d upper_left;
+  // lower-right corner: x2, y2
+  Eigen::Vector2d lower_right;
+  // local track id
+  int local_track_id = -1;
+
+  // 2Dto3D, pts8.resize(16), x, y...
+  std::vector<float> pts8;
+
+  // front box upper-left corner: x1, y1
+  Eigen::Vector2d front_upper_left;
+  // front box  lower-right corner: x2, y2
+  Eigen::Vector2d front_lower_right;
+
+  // front box upper-left corner: x1, y1
+  Eigen::Vector2d back_upper_left;
+  // front box  lower-right corner: x2, y2
+  Eigen::Vector2d back_lower_right;
+
+  std::vector<float> object_feature;
+
+  // this is `alpha` angle from KITTI: Observation angle of object,
+  // ranging [-pi..pi]
+  double alpha = 0.0;
+};
+
+typedef std::shared_ptr<CameraSupplement> CameraSupplementPtr;
+typedef std::shared_ptr<const CameraSupplement> CameraSupplementConstPtr;
+
+struct alignas(16) VehicleStatus {
+  float yaw_rate;
+  float velocity;
+  float time_t;            // time stamp
+  float time_d;            // time stamp difference in image
+  Eigen::Matrix3f motion;  // Motion Matrix
+};
+
+typedef boost::circular_buffer<VehicleStatus> MotionBuffer;
+typedef std::shared_ptr<MotionBuffer> MotionBufferPtr;
+typedef std::shared_ptr<const MotionBuffer> MotionBufferConstPtr;
+
+struct alignas(16) Vehicle3DStatus {
+  float yaw_delta;  // azimuth angle change
+  float pitch_delta;
+  float roll_delta;
+  float velocity_x;          // east
+  float velocity_y;          // north
+  float velocity_z;          // up
+  float time_t;              // time stamp
+  float time_d;              // time stamp difference in image
+  Eigen::Matrix4f motion3d;  // 3-d Motion Matrix
+};
+
+typedef boost::circular_buffer<Vehicle3DStatus> Motion3DBuffer;
+typedef std::shared_ptr<Motion3DBuffer> Motion3DBufferPtr;
+typedef std::shared_ptr<const Motion3DBuffer> Motion3DBufferConstPtr;
 
 }  // namespace perception
 }  // namespace apollo
