42ed3e89133c30ebceba11a1d90d2a4e482cdefb
YajiaZhang <yajia.zhang@gmail.com>
2018-02-23 11:18:12 -0800

    planning: pass lane width to PathTimeGraph

--- modules/planning/planner/lattice/lattice_planner.cc
+++ modules/planning/planner/lattice/lattice_planner.cc
@@ -164,14 +164,13 @@ Status LatticePlanner::PlanOnReferenceLine(
   auto ptr_path_time_graph = std::make_shared<PathTimeGraph>(
       ptr_prediction_querier->GetObstacles(), *ptr_reference_line,
       init_s[0], init_s[0] + FLAGS_decision_horizon,
-      0.0, FLAGS_trajectory_time_length);
+      0.0, FLAGS_trajectory_time_length, FLAGS_default_reference_line_width);
 
   BehaviorDecider behavior_decider(ptr_path_time_graph,
                                    ptr_prediction_querier);
 
   PlanningTarget planning_target = behavior_decider.Analyze(frame,
-      reference_line_info, planning_init_point, init_s,
-      *ptr_reference_line);
+      reference_line_info, planning_init_point, init_s, *ptr_reference_line);
 
   ADEBUG << "Decision_Time = " << (Clock::NowInSeconds() - current_time) * 1000;
   current_time = Clock::NowInSeconds();
