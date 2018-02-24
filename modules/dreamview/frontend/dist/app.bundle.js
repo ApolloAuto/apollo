18acd4baeed5c2ffbe6b514121bec35740a0b886
Vivian Lin <thunder0504@gmail.com>
2018-02-23 17:35:55 -0800

    Dreamview: draws navigation path (in navigation view) received from backend (#2983)
    * Dreamview: draws navigation path received from backend in navigation view
    
    * Dreamview: extract DownsamplePath method and include radar modules to navigation

--- modules/dreamview/frontend/dist/app.bundle.js
+++ modules/dreamview/frontend/dist/app.bundle.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d8830d5e0ff4e927b5cc632130fcb11e9c96cc1758f96fc225b545cf3277a560
-size 1356853
+oid sha256:cb6b3c3d03ad46d56afd68bcde80081a7b51a076189f5235869bab05974b3052
+size 1356801
