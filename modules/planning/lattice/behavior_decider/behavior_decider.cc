6c56fa727cbb464d4586fc72389ba4c77f68f1f5
kechxu <kechxu89@gmail.com>
2018-02-23 16:12:05 -0800

    Planning: reconstruct lattice by removing condition_filter and avoiding some unnecessary deep copy with more straightforward logic

--- modules/planning/lattice/behavior_decider/behavior_decider.cc
+++ modules/planning/lattice/behavior_decider/behavior_decider.cc
@@ -23,21 +23,13 @@
 #include "modules/common/proto/geometry.pb.h"
 #include "modules/common/math/path_matcher.h"
 #include "modules/planning/common/planning_gflags.h"
-#include "modules/planning/lattice/behavior_decider/condition_filter.h"
 #include "modules/planning/lattice/behavior_decider/scenario_manager.h"
 
 namespace apollo {
 namespace planning {
 
 using apollo::common::PathPoint;
-using apollo::common::PointENU;
 using apollo::common::TrajectoryPoint;
-
-BehaviorDecider::BehaviorDecider(
-    std::shared_ptr<PathTimeGraph> ptr_path_time_graph,
-    std::shared_ptr<PredictionQuerier> ptr_prediction_obstacles)
-    : ptr_path_time_graph_(ptr_path_time_graph),
-      ptr_prediction_obstacles_(ptr_prediction_obstacles) {}
 
 PlanningTarget BehaviorDecider::Analyze(
     Frame* frame, ReferenceLineInfo* const reference_line_info,
@@ -55,53 +47,7 @@ PlanningTarget BehaviorDecider::Analyze(
 
   CHECK(FLAGS_default_cruise_speed <= FLAGS_planning_upper_speed_limit);
 
-  ConditionFilter condition_filter(lon_init_state,
-                                   FLAGS_planning_upper_speed_limit,
-                                   ptr_path_time_graph_,
-                                   ptr_prediction_obstacles_);
-
-  ComputePathTimeSamplePoints(condition_filter, &planning_target);
-
   return planning_target;
-}
-
-void BehaviorDecider::ComputePathTimeSamplePoints(
-    const ConditionFilter& condition_filter,
-    PlanningTarget* const planning_target) {
-  auto sample_points = condition_filter.QueryPathTimeObstacleSamplePoints();
-
-  for (const auto& sample_point : sample_points) {
-    planning_target->add_sample_point()->CopyFrom(sample_point);
-  }
-
-  if (sample_points.empty()) {
-    ADEBUG << "Obstacle path time points are empty.";
-  } else {
-    for (const SamplePoint& sample_point : sample_points) {
-      ADEBUG << "Neighbor point: " << sample_point.ShortDebugString();
-    }
-  }
-}
-
-void BehaviorDecider::DumpLatticeImage(const int index,
-    const common::TrajectoryPoint& init_planning_point,
-    const ConditionFilter& condition_filter,
-    ReferenceLineInfo* const reference_line_info) {
-  apollo::planning_internal::LatticeStTraining st_data;
-  double timestamp = init_planning_point.relative_time();
-  std::string st_img_name = "DecisionCycle_" +
-                            std::to_string(index) + "_" +
-                            std::to_string(timestamp);
-  if (condition_filter.GenerateLatticeStPixels(&st_data, timestamp,
-                                               st_img_name)) {
-    ADEBUG << "Created_lattice_st_image_named = " << st_img_name
-           << "_for_timestamp = " << timestamp
-           << " num_colored_pixels = " << st_data.pixel_size();
-    planning_internal::Debug* ptr_debug =
-        reference_line_info->mutable_debug();
-    ptr_debug->mutable_planning_data()->mutable_lattice_st_image()->CopyFrom(
-        st_data);
-  }
 }
 
 }  // namespace planning
