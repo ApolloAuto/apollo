544dfae1cc3670784cd287ad4338e72ce152f1a2
vlin17 <thunder0504@gmail.com>
2018-02-27 19:01:06 -0800

    update json bundle

--- modules/dreamview/frontend/dist/app.bundle.js
+++ modules/dreamview/frontend/dist/app.bundle.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ba97ae057724e4c5656416661d22f8998a958e3bd6c981ed0865cb1e4622de7e
-size 1356800
+oid sha256:7cdf5ed4af225c19a63fbd2bdec213261ac38823dd4aacb42feb25743a986cad
+size 1356828
