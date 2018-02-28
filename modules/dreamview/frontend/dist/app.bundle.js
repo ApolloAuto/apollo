f340efd1472a2605a266eb285a6c821bea5dff18
siyangy <omega115210@gmail.com>
2018-02-28 15:28:36 -0800

    Dreamview: check timestamp from frontend

--- modules/dreamview/frontend/dist/app.bundle.js
+++ modules/dreamview/frontend/dist/app.bundle.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:aa8eaf5e54781cfcabd601a6982a9e26c70b5ee6bb637daf321f97771f1f109a
-size 1359203
+oid sha256:325f68bcb485bae250c7d2969d50e53004b50757948598045901b1e5e47ee546
+size 1361756
