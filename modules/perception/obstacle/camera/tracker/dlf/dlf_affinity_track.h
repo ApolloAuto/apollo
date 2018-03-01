12bf61352eb9b496b359791ebaf9a87e6388de1e
cheni-kuei <cheni-kuei@baidu.com>
2018-02-28 17:22:33 -0800

    Perception: Add DLF Affinity tracker. Updating Cascaded tracker

---
+++ modules/perception/obstacle/camera/tracker/dlf/dlf_affinity_track.h
@@ -0,0 +1,57 @@
+/******************************************************************************
+ * Copyright 2018 The Apollo Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *****************************************************************************/
+
+#ifndef ADU_PERCEPTION_OBSTACLE_CAMERA_DLF_AFFINITY_TRACKER_H
+#define ADU_PERCEPTION_OBSTACLE_CAMERA_DLF_AFFINITY_TRACKER_H
+
+#include <vector>
+#include <limits>
+#include <unordered_map>
+
+#include "obstacle/camera/tracker/mix_camera_tracker/base_affinity_tracker.h"
+
+namespace adu {
+namespace perception {
+namespace obstacle {
+
+class DLFAffinityTracker : public BaseAffinityTracker {
+public:
+
+    DLFAffinityTracker() : BaseAffinityTracker() {}
+
+    virtual ~DLFAffinityTracker() {}
+
+    virtual bool init() override;
+
+    virtual bool get_affinity_matrix(const cv::Mat &img,
+                                     const std::vector<Tracked> &tracked,
+                                     const std::vector<Detected> &detected,
+                                     std::vector<std::vector<float > > &affinity_matrix) override;
+
+    virtual bool update_tracked(const cv::Mat &img, const std::vector<Detected> &detected,
+                                std::vector<Tracked> &tracked) override;
+
+private:
+    // TODO Thresholds are fine-tuned detector-dependant values for DL features
+    float _conf_threshold = 0.9f; // 0.85f, 0.9f
+    float _filter_threshold = 0.3f; // 0.75f too high for small objs
+};
+
+}  // namespace obstacle
+}  // namespace perception
+}  // namespace adu
+
+#endif //ADU_PERCEPTION_OBSTACLE_CAMERA_DLF_AFFINITY_TRACKER_H
