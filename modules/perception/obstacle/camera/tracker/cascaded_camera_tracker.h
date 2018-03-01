145e6b4fe602fb867089d9e525dc33f0f29b4ce8
cheni-kuei <cheni-kuei@baidu.com>
2018-02-28 17:22:33 -0800

    Perception: Add DLF Affinity tracker. Updating Cascaded tracker

--- modules/perception/obstacle/camera/tracker/cascaded_camera_tracker.h
+++ modules/perception/obstacle/camera/tracker/cascaded_camera_tracker.h
@@ -35,12 +35,12 @@ namespace adu {
 namespace perception {
 namespace obstacle {
 
-class MixCameraTracker : public BaseCameraTracker {
+class CascadedCameraTracker : public BaseCameraTracker {
 public:
 
-    MixCameraTracker() : BaseCameraTracker() {}
+    CascadedCameraTracker() : BaseCameraTracker() {}
 
-    virtual ~MixCameraTracker() {}
+    virtual ~CascadedCameraTracker() {}
 
     virtual bool init() override;
 
@@ -50,34 +50,16 @@ public:
                            const CameraTrackerOptions &options,
                            std::vector<VisualObjectPtr> *tracked_objects) override;
 
-    virtual bool predict_shape(const cv::Mat &frame,
-                               const std::vector<VisualObjectPtr> &objects,
-                               double timestamp,
-                               const CameraTrackerOptions &options,
-                               std::vector<VisualObjectPtr> *tracked_objects) override;
-
-    virtual bool predict_velocity(const cv::Mat &frame,
-                                  const std::vector<VisualObjectPtr> &objects,
-                                  double timestamp,
-                                  const CameraTrackerOptions &options,
-                                  std::vector<VisualObjectPtr> *tracked_objects) override;
-
     virtual std::string name() const override;
 
 private:
-
-    DISALLOW_COPY_AND_ASSIGN(MixCameraTracker);
-
     // Verbose to XLOG(INFO)
     bool _verbose = false;
     bool _with_dl_feature = true;
 
-    // TODO Put pipeline into config and organize it
-
     // Trackers from different stages
     CS2DAffinityTracker _cs2d_tracker;
     DLFAffinityTracker _dlf_tracker;
-    DATAffinityTracker _dat_tracker;
     KCFAffinityTracker _kcf_tracker;
 
     // Tracking and ID management
@@ -86,49 +68,7 @@ private:
     int _frame_cnt = 0;
     int _max_kep_frame_cnt = 10;
 
-    class Filter {
-    public:
-        int track_id;
-        int lost_frame_count;
-        double last_seen_timestamp;
-
-        // TODO Make Kalman filter 3 dims
-        KalmanFilterConstVelocity position;
-        KalmanFilterConstVelocity orientation;
-
-        // TODO Temporary filtering for 3D size
-        KalmanFilterConstVelocity size_3d_1;
-        KalmanFilterConstVelocity size_3d_2;
-
-        Filter() :
-                track_id(-1),
-                lost_frame_count(0),
-                last_seen_timestamp(0.0)
-        {
-            // TODO more reasonable noises and put in config
-            position.init();
-            position._p *= 50.0;
-            position._q *= 50.0;
-            position._r *= 50.0;
-
-            orientation.init();
-            orientation._p *= 10.0;
-            orientation._q *= 10.0;
-            orientation._r *= 10.0;
-
-            size_3d_1.init();
-            size_3d_1._p *= 10.0;
-            size_3d_1._q *= 10.0;
-            size_3d_1._r *= 10.0;
-
-            size_3d_2.init();
-            size_3d_2._p *= 10.0;
-            size_3d_2._q *= 10.0;
-            size_3d_2._r *= 10.0;
-        }
-    };
-
-    std::map<int, Filter> _tracked_filters;
+    DISALLOW_COPY_AND_ASSIGN(MixCameraTracker);
 };
 
 }  // namespace obstacle
