d2ce3d84b823dd0e4e5181fd7c854c5895d66bbf
cheni-kuei <cheni-kuei@baidu.com>
2018-02-28 17:57:48 -0800

    Perception: Working on camera tracker

--- modules/perception/obstacle/camera/tracker/cascaded_camera_tracker.h
+++ modules/perception/obstacle/camera/tracker/cascaded_camera_tracker.h
@@ -14,26 +14,27 @@
  * limitations under the License.
  *****************************************************************************/
 
-#ifndef ADU_PERCEPTION_OBSTACLE_MIX_CAMERA_TRACKER_H
-#define ADU_PERCEPTION_OBSTACLE_MIX_CAMERA_TRACKER_H
+// Track ID association for detected camera objects between frames
+// Multiple trackers' affinity matrix outputs are cascaded and merged
 
+#ifndef MODULES_PERCEPTION_OBSTACLE_CAMERA_TRACKER_CASCADED_H_
+#define MODULES_PERCEPTION_OBSTACLE_CAMERA_TRACKER_CASCADED_H_
+
+#include <opencv2/opencv.hpp>
 #include <map>
 #include <vector>
 #include <algorithm>
 
-#include "obstacle/camera/interface/base_camera_tracker.h"
-#include "obstacle/camera/tracker/common/kalman_filter.h"
+#include "modules/common/macro.h"
+#include "modules/perception/lib/base/registerer.h"
+#include "modules/perception/obstacle/camera/common/visual_object.h"
+#include "modules/perception/obstacle/camera/interface/base_camera_tracker.h"
+#include "modules/perception/obstacle/camera/tracker/cascaded_camera_tracker_util.h"
+#include "modules/perception/obstacle/camera/tracker/dlf/dlf_affinity_tracker.h"
+#include "modules/perception/obstacle/camera/tracker/kcf/kcf_affinity_tracker.h"
 
-#include "obstacle/camera/tracker/mix_camera_tracker/mix_camera_tracker_util.h"
-#include "obstacle/camera/tracker/mix_camera_tracker/base_affinity_tracker.h"
-#include "obstacle/camera/tracker/mix_camera_tracker/cs2d/cs2d_affinity_tracker.h"
-#include "obstacle/camera/tracker/mix_camera_tracker/dlf/dlf_affinity_tracker.h"
-#include "obstacle/camera/tracker/mix_camera_tracker/dat/dat_affinity_tracker.h"
-#include "obstacle/camera/tracker/mix_camera_tracker/kcf/kcf_affinity_tracker.h"
-
-namespace adu {
+namespace apollo {
 namespace perception {
-namespace obstacle {
 
 class CascadedCameraTracker : public BaseCameraTracker {
 public:
@@ -42,37 +43,31 @@ public:
 
     virtual ~CascadedCameraTracker() {}
 
-    virtual bool init() override;
+    bool Init() override;
 
-    virtual bool associate(const cv::Mat &frame,
-                           const std::vector<VisualObjectPtr> &objects,
-                           double timestamp,
-                           const CameraTrackerOptions &options,
-                           std::vector<VisualObjectPtr> *tracked_objects) override;
+     bool Associate(const cv::Mat &img, const float& timestamp,
+                    std::vector<VisualObjectPtr>* objects) override;
 
-    virtual std::string name() const override;
+    std::string Name() const override;
 
 private:
-    // Verbose to XLOG(INFO)
-    bool _verbose = false;
-    bool _with_dl_feature = true;
+    bool dl_feature_ = true;
 
-    // Trackers from different stages
-    CS2DAffinityTracker _cs2d_tracker;
-    DLFAffinityTracker _dlf_tracker;
-    KCFAffinityTracker _kcf_tracker;
+    // Trackers for different stages
+    CS2DAffinityTracker cs2d_tracker_;
+    DLFAffinityTracker dlf_tracker_;
+    KCFAffinityTracker kcf_tracker_;
 
     // Tracking and ID management
-    std::vector<Tracked> _tracked;  // All tracked objects, extended up to max frame latent space
-    int _next_tracked_id = 0;
-    int _frame_cnt = 0;
-    int _max_kep_frame_cnt = 10;
+    std::vector<Tracked> tracks_;
+    int frame_idx_ = 0;
+    int next_track_id_ = 0;
+    const int kKeptFrameCnt = 10; // Latent space of potential tracks
 
-    DISALLOW_COPY_AND_ASSIGN(MixCameraTracker);
+    DISALLOW_COPY_AND_ASSIGN(CascadedCameraTracker);
 };
 
-}  // namespace obstacle
 }  // namespace perception
-}  // namespace adu
+}  // namespace apollo
 
-#endif //ADU_PERCEPTION_OBSTACLE_MIX_CAMERA_TRACKER_H
+#endif // MODULES_PERCEPTION_OBSTACLE_CAMERA_TRACKER_CASCADED_H_
