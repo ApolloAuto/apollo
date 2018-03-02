27b17af7c2357c4dfd2e4aa9db4240fcfd8b6952
vlin17 <thunder0504@gmail.com>
2018-03-02 11:21:05 -0800

    Dreamview: set google map as default in navigation view

--- modules/dreamview/frontend/dist/app.bundle.js
+++ modules/dreamview/frontend/dist/app.bundle.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:eda6292cfd94c6d1023db5ed9f50778fc84b33f5b73440dea8c23ad4647295fd
-size 1359059
+oid sha256:5d57ed1d1c5d1719e3f435245e30e3674bb13d6be8f7e7b79a188c26b381083b
+size 1361820
