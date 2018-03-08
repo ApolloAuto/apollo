dd2dca30f8430775e8ed3ed8006f82b9fbc74054
Dong Li <lidong.main@gmail.com>
2018-03-07 18:40:54 -0800

    CodeBot: REVISE Planning : fix bug on set planning target

--- modules/planning/tasks/traffic_decider/traffic_decider.cc
+++ modules/planning/tasks/traffic_decider/traffic_decider.cc
@@ -130,10 +130,10 @@ void TrafficDecider::BuildPlanningTarget(
     }
   }
   if (min_s != std::numeric_limits<double>::infinity()) {
-    const auto& vehicle_config =
-      common::VehicleConfigHelper::instance()->GetConfig();
+    const auto &vehicle_config =
+        common::VehicleConfigHelper::instance()->GetConfig();
     double front_edge_to_center =
-      vehicle_config.vehicle_param().front_edge_to_center();
+        vehicle_config.vehicle_param().front_edge_to_center();
     stop_point.set_s(min_s - front_edge_to_center);
     reference_line_info->SetStopPoint(stop_point);
   }
