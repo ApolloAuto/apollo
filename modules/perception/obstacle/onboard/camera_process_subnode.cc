9a858974fd589703042597b74697cd97a1fce4bc
cheni-kuei <cheni-kuei@baidu.com>
2018-03-08 13:26:33 -0800

    Perception: Build camera process subnode

--- modules/perception/obstacle/onboard/camera_process_subnode.cc
+++ modules/perception/obstacle/onboard/camera_process_subnode.cc
@@ -21,66 +21,69 @@ namespace perception {
 
 bool CameraProcessSubnode::InitInternal() {
   //  Subnode config in DAG streaming
-  std::map<string, string> fields;
-  SubnodeHelper::ParseReserveField(reserve_, &fields));
+  std::map<std::string, std::string> fields;
+  SubnodeHelper::ParseReserveField(reserve_, &fields);
   device_id_ = fields["device_id"];
 
   // Shared Data
-  cam_obj_data_ = static_cast<CameraObjectData*>(
+  cam_obj_data_ = static_cast<CameraObjectData *>(
       shared_data_manager_->GetSharedData("CameraObjectData"));
-  cam_shared_data_ = static_cast<CameraSharedData*>(
+  cam_shared_data_ = static_cast<CameraSharedData *>(
       shared_data_manager_->GetSharedData("CameraSharedData"));
 
   InitCalibration();
 
   InitModules();
 
-  AdapterManager::AddImageShortCallback(&CameraProcessSubnode::ImgCallback,
-                                        this);
+  // void (T::*fp)(const name##Adapter::DataType &data), T *obj
+
+  apollo::common::adapter::AdapterManager::AddImageShortCallback(
+      &CameraProcessSubnode::ImgCallback, this);
+
   return true;
 }
 
 bool CameraProcessSubnode::InitCalibration() {
-  auto ccm = base::Singleton<CalibrationConfigManager>::get();
+  auto ccm = Singleton<CalibrationConfigManager>::get();
   CameraCalibrationPtr calibrator = ccm->get_camera_calibration();
 
-  const onboard::TransformInput &camera2car_trans =
-      calibrator->get_camera_transform();
-  camera2car_trans.query_pos(0.0, &camera_to_car_);
-
+  camera_to_car_ = calibrator->get_camera_extrinsics();
   intrinsics_ = calibrator->get_camera_intrinsic();
   undistortion_handler_ = calibrator->get_camera_undistort_handler();
-
   return true;
 }
 
 bool CameraProcessSubnode::InitModules() {
-  detector_.reset(BaseCameraDetectorRegisterer::get_instance_by_name("YoloCameraDetector"));
+  detector_.reset(
+      BaseCameraDetectorRegisterer::GetInstanceByName("YoloCameraDetector"));
   detector_->Init();
 
-  converter_.reset(BaseCameraConverterRegisterer::get_instance_by_name("GeometryCameraConverter"));
+  converter_.reset(BaseCameraConverterRegisterer::GetInstanceByName(
+      "GeometryCameraConverter"));
   converter_->Init();
 
-  tracker_.reset(BaseCameraTrackerRegisterer::get_instance_by_name("CascadedCameraTracker"));
+  tracker_.reset(
+      BaseCameraTrackerRegisterer::GetInstanceByName("CascadedCameraTracker"));
   tracker_->Init();
 
-  transformer_.reset(BaseCameraTransformerRegisterer::get_instance_by_name("FlatCameraTransformer"));
+  transformer_.reset(BaseCameraTransformerRegisterer::GetInstanceByName(
+      "FlatCameraTransformer"));
   transformer_->Init();
-  transformer_->SetExtrinsics(camera_to_car_);
+  // transformer_->SetExtrinsics(camera_to_car_);
 
-  filter_.reset(BaseCameraFilterRegisterer::get_instance_by_name("ObjectCameraFilter"));
+  filter_.reset(
+      BaseCameraFilterRegisterer::GetInstanceByName("ObjectCameraFilter"));
   filter_->Init();
 
   return true;
 }
 
-void CameraProcessSubnode::ImgCallback(
-    const sensor_msgs::Image::ConstPtr &message) {
-  ++_seq_num;
-  float timestamp = message->header.stamp.toSec();
+void CameraProcessSubnode::ImgCallback(const sensor_msgs::Image &message) {
+  ++seq_num_;
+  float timestamp = message.header.stamp.toSec();
 
   cv::Mat img;
-  MessageToMat(message, &img));
+  MessageToMat(message, &img);
 
   std::vector<VisualObjectPtr> objects;
   cv::Mat mask = cv::Mat::zeros(img.rows, img.cols, CV_32FC1);
@@ -94,58 +97,56 @@ void CameraProcessSubnode::ImgCallback(
   out_objs->timestamp = timestamp;
   VisualObjToSensorObj(objects, &out_objs);
 
-  onboard::SharedDataPtr<CameraItem> camera_item_ptr(new CameraItem);
+  SharedDataPtr<CameraItem> camera_item_ptr(new CameraItem);
   camera_item_ptr->image_src_mat = img.clone();
   mask.copyTo(out_objs->camera_frame_supplement->lane_map);
   PublishDataAndEvent(timestamp, out_objs, camera_item_ptr);
 }
 
-bool CameraProcessSubnode::MessageToMat(
-    const sensor_msgs::Image::ConstPtr& message, cv::Mat* mat)
-    cv_bridge::CvImageConstPtr cv_ptr =
-        cv_bridge::toCvShare(message, sensor_msgs::image_encodings::BGR8);
-    cv::Mat img = cv_ptr->image;
+bool CameraProcessSubnode::MessageToMat(const sensor_msgs::Image &message,
+                                        cv::Mat *mat) {
+  sensor_msgs::ImageConstPtr ptr(&message);
+  cv_bridge::CvImageConstPtr cv_ptr =
+      cv_bridge::toCvShare(ptr, sensor_msgs::image_encodings::BGR8);
+  cv::Mat img = cv_ptr->image;
 
-    mat->create(img.rows, img.cols, CV_8UC3);
-    _undistortion_handler->handle(img.data, mat->data);
-    return true;
+  mat->create(img.rows, img.cols, CV_8UC3);
+  undistortion_handler_->handle(img.data, mat->data);
+  return true;
 }
 
 void CameraProcessSubnode::VisualObjToSensorObj(
     const std::vector<VisualObjectPtr> &objects,
-    onboard::SharedDataPtr<SensorObjects>* sensor_objects)
-  out_objs->type = CAMERA;
-  out_objs->name = get_sensor_name(CAMERA);
-  out_objs->seq_num = seq_num_;
-  out_objs->sensor2world_pose = camera_to_car_;
-  (out_objs->camera_frame_supplement).reset(new CameraFrameSupplement);
+    SharedDataPtr<SensorObjects> *sensor_objects) {
+  (*sensor_objects)->sensor_type = SensorType::CAMERA;
+  (*sensor_objects)->sensor_id = device_id_;
+  (*sensor_objects)->seq_num = seq_num_;
+  (*sensor_objects)->sensor2world_pose = camera_to_car_;
+  ((*sensor_objects)->camera_frame_supplement).reset(new CameraFrameSupplement);
 
   for (size_t i = 0; i < objects.size(); ++i) {
     VisualObjectPtr vobj = objects[i];
-    ObjectPtr obj = new Object();
+    ObjectPtr obj(new Object());
 
     obj->id = vobj->id;
-    obj->direction = vobj->direction;
+    obj->direction = vobj->direction.cast<double>();
     obj->theta = vobj->theta;
-    obj->center = vobj->center;
+    obj->center = vobj->center.cast<double>();
     obj->length = vobj->length;
     obj->width = vobj->width;
     obj->height = vobj->height;
     obj->type = vobj->type;
-    obj->type_probs.assign(vobj->type_probs,
-                           vobj->type_probs + MAX_OBJECT_TYPE);
+    // obj->type_probs.assign(vobj->type_probs,
+    //                        vobj->type_probs + MAX_OBJECT_TYPE);
     obj->track_id = vobj->track_id;
-    obj->tracking_time = vobj->tracking_time;
-    obj->latest_tracked_time = vobj->latest_tracked_time;
-    obj->velocity = vobj->velocity;
-    obj->anchor_point = obj->center;
+    obj->tracking_time = vobj->track_age;
+    obj->latest_tracked_time = vobj->last_track_timestamp;
+    obj->velocity = vobj->velocity.cast<double>();
+    obj->anchor_point = obj->center.cast<double>();
     (obj->camera_supplement).reset(new CameraSupplement());
-    obj->camera_supplement->upper_left << vobj->upper_left[0],
-        vobj->upper_left[1];
-    obj->camera_supplement->lower_right << vobj->lower_right[0],
-        vobj->lower_right[1];
-
-    // obj->camera_supplement->alpha = vobj->alpha;
+    obj->camera_supplement->upper_left = vobj->upper_left.cast<double>();
+    obj->camera_supplement->lower_right = vobj->lower_right.cast<double>();
+    obj->camera_supplement->alpha = vobj->alpha;
     // obj->camera_supplement->pts8.assign(vobj->pts8,
     //                                     vobj->pts8 + 16);
 
@@ -154,17 +155,16 @@ void CameraProcessSubnode::VisualObjToSensorObj(
 }
 
 void CameraProcessSubnode::PublishDataAndEvent(
-    double timestamp,
-    const onboard::SharedDataPtr<SensorObjects> &sensor_objects,
-    const onboard::SharedDataPtr<CameraItem> &camera_item) {
+    const float &timestamp, const SharedDataPtr<SensorObjects> &sensor_objects,
+    const SharedDataPtr<CameraItem> &camera_item) {
   std::string key = "";
-  SubnodeHelper::ProduceSharedDataKey(timestamp, device_id_, &key));
+  SubnodeHelper::ProduceSharedDataKey(timestamp, device_id_, &key);
 
-  cam_obj_data_->add(key, sensor_objects));
-  cam_shared_data_->add(key, camera_item));
+  cam_obj_data_->Add(key, sensor_objects);
+  cam_shared_data_->Add(key, camera_item);
 
   for (size_t idx = 0; idx < pub_meta_events_.size(); ++idx) {
-    const EventMeta& event_meta = pub_meta_events_[idx];
+    const EventMeta &event_meta = pub_meta_events_[idx];
     Event event;
     event.event_id = event_meta.event_id;
     event.timestamp = timestamp;
