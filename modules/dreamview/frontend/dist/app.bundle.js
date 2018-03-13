b9164105f8ef3b51fb45bd372be440915ff01105
Vivian Lin <thunder0504@gmail.com>
2018-03-12 17:04:55 -0700

    Dreamview: adding setup script for dreamview build & fixed obstacles missing issue (#3353)
    * dreamview: adding setup script for frontend build
    
    * Dreamview: fixed obstacles missing issue

--- modules/dreamview/frontend/dist/app.bundle.js
+++ modules/dreamview/frontend/dist/app.bundle.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bf8b1b88f2cb2a4a70148d5c5471e9f5b8314f6ee0a0a0fef9cd67615f0a49f2
-size 1464875
+oid sha256:0df6c68d65dd81595c0fb497722986e9a1e856973d412d0331f7b471273480b7
+size 1464949
