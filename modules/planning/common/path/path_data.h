8c401e2b5d488b15125da87dc2eb04be8053e1ba
Liangliang Zhang <zhangliangliang@baidu.com>
2018-03-14 13:24:02 -0700

    Planning: added test for DiscretePath class, removed unused functions.

--- modules/planning/common/path/path_data.h
+++ modules/planning/common/path/path_data.h
@@ -24,7 +24,6 @@
 #include <list>
 #include <string>
 #include <utility>
-#include <vector>
 
 #include "modules/planning/common/path/discretized_path.h"
 #include "modules/planning/common/path/frenet_frame_path.h"
