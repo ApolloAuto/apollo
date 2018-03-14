f35a3cc80f4e7ded467f6ed85d8f4c881d1b56ac
zhangweide <zhangweide@baidu.com>
2018-03-14 14:23:06 -0700

    refine pbf track distance for lowcost

--- modules/perception/common/perception_gflags.h
+++ modules/perception/common/perception_gflags.h
@@ -95,4 +95,8 @@ DECLARE_bool(show_camera_objects2d);
 DECLARE_bool(show_camera_parsing);
 DECLARE_string(frame_visualizer);
 
+// async fusion using imf
+DECLARE_bool(async_fusion);
+DECLARE_bool(use_distance_angle_fusion);
+
 #endif  // MODULES_PERCEPTION_COMMON_PERCEPTION_GFLAGS_H_
