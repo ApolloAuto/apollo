c2240318faeacb9c7007b9c9c9e70004628ff562
gchen-apollo <chenguang09@baidu.com>
2018-03-16 16:46:58 -0700

    Perception: add motion manager visualization && object trajectories

--- modules/perception/common/perception_gflags.h
+++ modules/perception/common/perception_gflags.h
@@ -95,6 +95,7 @@ DECLARE_bool(show_camera_objects2d);
 DECLARE_bool(show_camera_parsing);
 DECLARE_string(frame_visualizer);
 
+DECLARE_bool(show_motion);
 // async fusion using imf
 DECLARE_bool(async_fusion);
 DECLARE_bool(use_distance_angle_fusion);
