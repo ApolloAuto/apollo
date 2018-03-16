0718e648ba0382fe350d4c3b258eaa732554136f
kechxu <kechxu89@gmail.com>
2018-03-16 16:53:07 -0700

    Planning: primary implement centripetal acceleration cost in lattice planner

--- modules/planning/common/planning_gflags.h
+++ modules/planning/common/planning_gflags.h
@@ -207,6 +207,7 @@ DECLARE_double(weight_lon_jerk);
 DECLARE_double(weight_lon_collision);
 DECLARE_double(weight_lat_offset);
 DECLARE_double(weight_lat_comfort);
+DECLARE_double(weight_centripetal_acceleration);
 DECLARE_double(priority_cost_gap);
 DECLARE_double(weight_same_side_offset);
 DECLARE_double(weight_opposite_side_offset);
