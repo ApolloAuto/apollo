1575ae543d5a3ed9b018b7067b51d882bdad0f0d
Aaron Xiao <xiaoxiangquan@gmail.com>
2018-03-16 14:53:09 -0700

    Scritps: Revert chmod whole /home/tmp.

--- scripts/docker_adduser.sh
+++ scripts/docker_adduser.sh
@@ -59,5 +59,8 @@ if [ "$RELEASE_DOCKER" != "1" ];then
   # setup ros package
   # this is a temporary solution to avoid ros package downloading.
   ROS="/home/tmp/ros"
-  chmod a+w -R /home/tmp
+  chmod a+w "${ROS}/share/velodyne/launch/start_velodyne.launch"
+  chmod a+w -R "${ROS}/share/velodyne_pointcloud/params"
+  chmod a+w "${ROS}/share/gnss_driver/launch/gnss_driver.launch"
+  chmod a+w "${ROS}/share/gnss_driver/conf/gnss_conf_mkz.txt"
 fi
