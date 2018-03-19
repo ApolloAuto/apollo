a12a11799873f332830bbd8651c4bd89b566865a
vlin17 <thunder0504@gmail.com>
2018-03-19 13:12:40 -0700

    Dreamview: data recorder UI refinement

--- modules/dreamview/frontend/dist/app.bundle.js
+++ modules/dreamview/frontend/dist/app.bundle.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:da1a99d77d948d09f5d5fe5ac9c0fc617aedac11060fea884c5880fd9fe5ddcb
-size 1500631
+oid sha256:029b4f3a575cfe3858475b488964b5886bc0602a28a9ccfd54566b1604ef31ac
+size 1496138
