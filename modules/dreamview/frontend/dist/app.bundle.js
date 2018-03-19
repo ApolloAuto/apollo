1e0c1abfe4c9a704f3ab4b1bd7ba84c703f47270
siyangy <yu.siyang@yahoo.com>
2018-03-19 14:04:32 -0700

    Dreamview: display grid ground in navi mode (#3469)
    * Dreamview: display grid ground in navi mode
    
    * fix member name

--- modules/dreamview/frontend/dist/app.bundle.js
+++ modules/dreamview/frontend/dist/app.bundle.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:988e5a78cbd2237c98eded5ea61ed57380b5c1c19bca051f2c3a2c090b1c012c
-size 1496070
+oid sha256:da1a99d77d948d09f5d5fe5ac9c0fc617aedac11060fea884c5880fd9fe5ddcb
+size 1500631
