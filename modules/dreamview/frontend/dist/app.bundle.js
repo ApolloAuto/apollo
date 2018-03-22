ef254052691c7020332836126e7df8dae5de3897
vlin17 <thunder0504@gmail.com>
2018-03-22 11:49:14 -0700

    Dreamview: apply coordinate offset for Google and Baidu map

--- modules/dreamview/frontend/dist/app.bundle.js
+++ modules/dreamview/frontend/dist/app.bundle.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:125b5f7a35e63e392e3a2f00727d11865a1182f2bf0c7aaf4e2b6cce4f47dd59
-size 1496719
+oid sha256:6cf3d214b58ca9c31ef71988e014ce7fb540d6dc3b62f682d6a0ffe1970a3cb5
+size 1501286
