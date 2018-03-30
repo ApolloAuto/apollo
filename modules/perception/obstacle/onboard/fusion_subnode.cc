625d7fd32148e9b572c793a2daf8d984a4a09834
zhangweide <zhangweide@baidu.com>
2018-03-30 15:07:24 -0700

    ci perception init faiulre

--- modules/perception/obstacle/onboard/fusion_subnode.cc
+++ modules/perception/obstacle/onboard/fusion_subnode.cc
@@ -362,6 +362,7 @@ bool FusionSubnode::GeneratePbMsg(PerceptionObstacles *obstacles) {
 
 void FusionSubnode::RegistAllAlgorithm() {
   RegisterFactoryProbabilisticFusion();
+  RegisterFactoryAsyncFusion();
 }
 
 void FusionSubnode::OnChassis(const Chassis &chassis) {
