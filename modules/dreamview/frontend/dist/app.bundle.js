0997504aa8321375babe8e63fa5944b63cdaac0e
unacao <yufcao@gmail.com>
2018-04-04 15:49:02 -0700

    add Dreamview display for CIPV (#3720)

--- modules/dreamview/frontend/dist/app.bundle.js
+++ modules/dreamview/frontend/dist/app.bundle.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5e67cce617952040b1bae293f98e8b659ab5eeb5850919533d11c83300e0456a
-size 1501517
+oid sha256:513932aceea9eeb6ebfdf66ebe8397a6b493658aabda704cda79d6b63b1c5be6
+size 1501816
