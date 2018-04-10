22a149b2b10ccc3a791be7e43480b15ca9956450
I-Kuei Chen <eugenegx@gmail.com>
2018-04-10 13:35:21 -0700

    Perception: Simplify logic and reduce latency in modules of Camera subnode. Fix a bug in config name (#3800)
    * Perception: Simplify the logic in converter and reduce latency
    
    * Perception: Add option to disable KCF tracker. Tune CS2D tracker
    
    * Perception: Fix typo in config

--- modules/perception/obstacle/camera/converter/geometry_camera_converter.h
+++ modules/perception/obstacle/camera/converter/geometry_camera_converter.h
@@ -59,8 +59,8 @@ class GeometryCameraConverter : public BaseCameraConverter {
 
   bool ConvertSingle(const float &h, const float &w, const float &l,
                      const float &alpha_deg, const Eigen::Vector2f &upper_left,
-                     const Eigen::Vector2f &lower_right, float *distance_w,
-                     float *distance_h, Eigen::Vector2f *mass_center_pixel);
+                     const Eigen::Vector2f &lower_right, bool use_width,
+                     float *distance, Eigen::Vector2f *mass_center_pixel);
 
   void Rotate(const float &alpha_deg,
               std::vector<Eigen::Vector3f> *corners) const;
