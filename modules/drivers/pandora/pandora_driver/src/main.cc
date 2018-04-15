031a9d8b9f974733e9edc7014f443d8a01af87a8
Aaron Xiao <xiaoxiangquan@gmail.com>
2018-04-14 21:47:27 -0700

    Pandora: Fix lint errors. (#3889)

--- modules/drivers/pandora/pandora_driver/src/main.cc
+++ modules/drivers/pandora/pandora_driver/src/main.cc
@@ -22,20 +22,18 @@
 #include <pcl_conversions/pcl_conversions.h>
 #include "pandora/pandora.h"
 
-using namespace apollo::drivers::hesai;
+using apollo::drivers::hesai::Pandora;
 
-class PandoraHesaiClient
-{
-public:
-  PandoraHesaiClient(ros::NodeHandle node, ros::NodeHandle nh)
-  {
+class PandoraHesaiClient {
+ public:
+  PandoraHesaiClient(ros::NodeHandle node, ros::NodeHandle nh) {
     // default configure
     std::string pandoraIP = std::string("192.168.20.51");
     int lidarRecvPort = 2368;
     int gpsRecvPort = 10110;
     int startAngle = 135;
     int pandoraCameraPort = 9870;
-    std::string lidarTopic = std::string("/apollo/sensor/pandora/hesai40/PointCloud2");
+    std::string lidarTopic = "/apollo/sensor/pandora/hesai40/PointCloud2";
     std::string cameraTopics[5];
     cameraTopics[0] = std::string("/apollo/sensor/pandora/camera/front_color");
     cameraTopics[1] = std::string("/apollo/sensor/pandora/camera/front_gray");
@@ -47,8 +45,9 @@ public:
     std::string frameId = std::string("hesai40");
 
     // parse nodehandle param
-    bool ret = parseParameter(nh, pandoraIP, lidarRecvPort, gpsRecvPort, startAngle, pandoraCameraPort,
-            lidarTopic, cameraTopics, enableCamera, timezone, frameId);
+    bool ret = parseParameter(nh, &pandoraIP, &lidarRecvPort,
+            &gpsRecvPort, &startAngle, &pandoraCameraPort,
+            &lidarTopic, cameraTopics, &enableCamera, &timezone, &frameId);
     if (!ret) {
         ROS_INFO("Parse parameters failed, please check parameters above.");
         return;
@@ -63,35 +62,38 @@ public:
         imgPublishers[i] = it.advertise(cameraTopics[i], 1);
       }
     }
-    
-    psdk = new Pandora(pandoraIP, lidarRecvPort, gpsRecvPort, 
+
+    psdk = new Pandora(pandoraIP, lidarRecvPort, gpsRecvPort,
             boost::bind(&PandoraHesaiClient::lidarCallback, this, _1, _2),
-            NULL, startAngle * 100, pandoraCameraPort, 
-            boost::bind(&PandoraHesaiClient::cameraCallback, this, _1, _2, _3, _4),
+            NULL, startAngle * 100, pandoraCameraPort,
+            boost::bind(&PandoraHesaiClient::cameraCallback,
+                        this, _1, _2, _3, _4),
             enableCamera, timezone, frameId);
     psdk->Start();
   }
 
-  bool parseParameter(ros::NodeHandle nh, std::string& pandoraIP, int& lidarRecvPort,
-          int& gpsRecvPort, int& startAngle, int& pandoraCameraPort, std::string& lidarTopic,
-          std::string cameraTopics[], bool& enableCamera, int& timezone, std::string& frameId) {
+  bool parseParameter(ros::NodeHandle nh, std::string* pandoraIP,
+                      int* lidarRecvPort, int* gpsRecvPort, int* startAngle,
+                      int* pandoraCameraPort, std::string* lidarTopic,
+                      std::string cameraTopics[], bool* enableCamera,
+                      int* timezone, std::string* frameId) {
     if (nh.hasParam("pandora_ip")) {
-      nh.getParam("pandora_ip", pandoraIP);
+      nh.getParam("pandora_ip", *pandoraIP);
     }
     if (nh.hasParam("lidar_recv_port")) {
-      nh.getParam("lidar_recv_port", lidarRecvPort);
+      nh.getParam("lidar_recv_port", *lidarRecvPort);
     }
     if (nh.hasParam("gps_recv_port")) {
-      nh.getParam("gps_recv_port", gpsRecvPort);
+      nh.getParam("gps_recv_port", *gpsRecvPort);
     }
     if (nh.hasParam("start_angle")) {
-      nh.getParam("start_angle", startAngle);
+      nh.getParam("start_angle", *startAngle);
     }
     if (nh.hasParam("pandora_camera_port")) {
-      nh.getParam("pandora_camera_port", pandoraCameraPort);
+      nh.getParam("pandora_camera_port", *pandoraCameraPort);
     }
     if (nh.hasParam("lidar_topic")) {
-      nh.getParam("lidar_topic", lidarTopic);
+      nh.getParam("lidar_topic", *lidarTopic);
     }
     if (nh.hasParam("camera0_topic")) {
       nh.getParam("camera0_topic", cameraTopics[0]);
@@ -109,37 +111,39 @@ public:
       nh.getParam("camera4_topic", cameraTopics[4]);
     }
     if (nh.hasParam("enable_camera")) {
-      nh.getParam("enable_camera", enableCamera);
+      nh.getParam("enable_camera", *enableCamera);
     }
     if (nh.hasParam("timezone")) {
-      nh.getParam("timezone", timezone);
+      nh.getParam("timezone", *timezone);
     }
     if (nh.hasParam("frame_id")) {
-      nh.getParam("frame_id", frameId);
+      nh.getParam("frame_id", *frameId);
     }
 
-    std::cout << "Configs: pandoraIP: " << pandoraIP << ", lidarRecvPort: " 
-        << lidarRecvPort << ", gpsRecvPort: " << gpsRecvPort 
-        << ", startAngle: " << startAngle << ", pandoraCameraPort: " 
-        << pandoraCameraPort << ", lidarTopic: " << lidarTopic 
-        << ", enableCamera: " << enableCamera << ", frameId: " << frameId << std::endl;
+    std::cout << "Configs: pandoraIP: " << *pandoraIP << ", lidarRecvPort: "
+        << *lidarRecvPort << ", gpsRecvPort: " << *gpsRecvPort
+        << ", startAngle: " << *startAngle << ", pandoraCameraPort: "
+        << *pandoraCameraPort << ", lidarTopic: " << *lidarTopic
+        << ", enableCamera: " << *enableCamera << ", frameId: "
+        << *frameId << std::endl;
     for (int i = 0; i < 5; i++) {
-      std::cout << "cameraTopic" << i << ": " << cameraTopics[i] << std::endl; 
+      std::cout << "cameraTopic" << i << ": " << cameraTopics[i] << std::endl;
     }
 
-    // check 
+    // check
     struct sockaddr_in sa;
-    return checkPort(lidarRecvPort) && checkPort(gpsRecvPort) && checkPort(pandoraCameraPort)
-        && (startAngle >= 0) && (startAngle < 360) && 
-        (1 == inet_pton(AF_INET, pandoraIP.c_str(), &(sa.sin_addr))) ;
+    return checkPort(*lidarRecvPort) && checkPort(*gpsRecvPort)
+        && checkPort(*pandoraCameraPort) && (*startAngle >= 0)
+        && (*startAngle < 360)
+        && (1 == inet_pton(AF_INET, pandoraIP->c_str(), &(sa.sin_addr)));
   }
 
   bool checkPort(int port) {
     return (port > 0) && (port < 65535);
   }
 
-  void cameraCallback(boost::shared_ptr<cv::Mat> matp, double timestamp, int pic_id, bool distortion)
-  {
+  void cameraCallback(boost::shared_ptr<cv::Mat> matp, double timestamp,
+                      int pic_id, bool distortion) {
     sensor_msgs::ImagePtr imgMsg;
 
     if (pic_id > 4 || pic_id < 0) {
@@ -151,8 +155,7 @@ public:
     imgPublishers[pic_id].publish(imgMsg);
   }
 
-  void lidarCallback(boost::shared_ptr<PPointCloud> cld, double timestamp)
-  {
+  void lidarCallback(boost::shared_ptr<PPointCloud> cld, double timestamp) {
     pcl_conversions::toPCL(ros::Time(timestamp), cld->header.stamp);
     sensor_msgs::PointCloud2 output;
     pcl::toROSMsg(*cld, output);
@@ -163,16 +166,15 @@ public:
     if (NULL != psdk) {
       delete(psdk);
     }
-  } 
+  }
 
-private:
+ private:
   ros::Publisher lidarPublisher;
   image_transport::Publisher imgPublishers[5];
   Pandora *psdk;
 };
 
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
   ros::init(argc, argv, "pandora_ros");
   ros::NodeHandle nh("~");
   ros::NodeHandle node;
