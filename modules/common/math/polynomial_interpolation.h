89e6231328bc3791648a9f0a9983ce567868cc4c
Liangliang Zhang <zhangliangliang@baidu.com>
2018-04-17 10:43:58 -0700

    Common/math: added polynomial interpolation.

---
+++ modules/common/math/polynomial_interpolation.h
@@ -0,0 +1,48 @@
+/******************************************************************************
+ * Copyright 2017 The Apollo Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *****************************************************************************/
+
+/**
+ * @file
+ * @brief Linear interpolation functions.
+ */
+
+#ifndef MODULES_COMMON_MATH_LINEAR_INTERPOLATION_H_
+#define MODULES_COMMON_MATH_LINEAR_INTERPOLATION_H_
+
+#include <cmath>
+
+#include "modules/common/log.h"
+#include "modules/common/proto/pnc_point.pb.h"
+
+/**
+ * @namespace apollo::common::math
+ * @brief apollo::common::math
+ */
+namespace apollo {
+namespace common {
+namespace math {
+
+SLPoint PolynomialInterpolate(const SLPoint &start, const SLPoint &end,
+                              const double weight);
+
+PathPoint PolynomialInterpolate(const PathPoint &p0, const PathPoint &p1,
+                                const double s);
+
+}  // namespace math
+}  // namespace common
+}  // namespace apollo
+
+#endif  // MODULES_COMMON_MATH_LINEAR_INTERPOLATION_H_
