2b85728d15e19fc0995740b39a0c36eba4b81065
Aaron Xiao <xiaoxiangquan@gmail.com>
2018-05-04 22:58:07 -0700

    Monitor: Fix false alarm of GPS/CAN status changing.

--- modules/monitor/software/summary_monitor.cc
+++ modules/monitor/software/summary_monitor.cc
@@ -148,18 +148,18 @@ void SummaryMonitor::SummarizeHardware() {
     if (status->has_status()) {
       switch (status->status()) {
         case HardwareStatus::NOT_PRESENT:
-          UpdateStatusSummary(Summary::FATAL, status->msg(), status);
+          UpdateStatusSummary(Summary::FATAL, status->detailed_msg(), status);
           break;
         case HardwareStatus::NOT_READY:  // Fall through.
         case HardwareStatus::GPS_UNSTABLE_WARNING:
-          UpdateStatusSummary(Summary::WARN, status->msg(), status);
+          UpdateStatusSummary(Summary::WARN, status->detailed_msg(), status);
           break;
         case HardwareStatus::OK:
-          UpdateStatusSummary(Summary::OK, status->msg(), status);
+          UpdateStatusSummary(Summary::OK, status->detailed_msg(), status);
           break;
         case HardwareStatus::GPS_UNSTABLE_ERROR:  // Fall through.
         default:
-          UpdateStatusSummary(Summary::ERROR, status->msg(), status);
+          UpdateStatusSummary(Summary::ERROR, status->detailed_msg(), status);
           break;
       }
     }
