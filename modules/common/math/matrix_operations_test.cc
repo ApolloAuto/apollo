d971f30b3f2f5eebc825b1e7dc7f3bd60cde5e4b
luoqi06 <luoqi06@baidu.com>
2018-05-07 01:33:51 -0700

    Common : add test for c2d

--- modules/common/math/matrix_operations_test.cc
+++ modules/common/math/matrix_operations_test.cc
@@ -31,8 +31,8 @@ TEST(PseudoInverseTest, PseudoInverseI) {
 
   EXPECT_EQ(B(0, 0), 1);
   EXPECT_EQ(B(0, 1), 0);
-  EXPECT_EQ(B(0, 0), 1);
-  EXPECT_EQ(B(0, 0), 1);
+  EXPECT_EQ(B(1, 0), 0);
+  EXPECT_EQ(B(1, 1), 1);
 
   const Eigen::Matrix<float, 2, 2> C = Eigen::MatrixXf::Zero(2, 2);
 
@@ -40,8 +40,8 @@ TEST(PseudoInverseTest, PseudoInverseI) {
 
   EXPECT_EQ(D(0, 0), 0);
   EXPECT_EQ(D(0, 1), 0);
-  EXPECT_EQ(D(0, 0), 0);
-  EXPECT_EQ(D(0, 0), 0);
+  EXPECT_EQ(D(1, 0), 0);
+  EXPECT_EQ(D(1, 1), 0);
 }
 
 TEST(PseudoInverseTest, PseudoInverseII) {
@@ -70,6 +70,91 @@ TEST(PseudoInverseTest, PseudoInverseII) {
   EXPECT_FLOAT_EQ(D(0, 2), 0);
   EXPECT_FLOAT_EQ(D(0, 3), 0);
   EXPECT_FLOAT_EQ(D(0, 4), 0);
+}
+
+TEST(ContinuousToDiscreteTest, c2d) {
+  double ts = 0.0;
+
+  Eigen::Matrix<float, 2, 2> m_a = Eigen::MatrixXf::Identity(2, 2);
+
+  Eigen::Matrix<float, 2, 1> m_b = Eigen::MatrixXf::Ones(2, 1);
+
+  Eigen::Matrix<float, 1, 2> m_c = Eigen::MatrixXf::Ones(1, 2);
+
+  Eigen::Matrix<float, 1, 1> m_d = Eigen::MatrixXf::Identity(1, 1);
+
+  Eigen::Matrix<float, 2, 2> prt_a_d;
+
+  Eigen::Matrix<float, 2, 1> prt_b_d;
+
+  Eigen::Matrix<float, 1, 2> prt_c_d;
+
+  Eigen::Matrix<float, 1, 1> prt_d_d;
+
+  bool res = ContinuousToDiscrete<float, 2, 2, 1, 1>(
+      m_a, m_b, m_c, m_d, ts, &prt_a_d, &prt_b_d, &prt_c_d, &prt_d_d);
+
+  EXPECT_FALSE(res);
+
+  ts = 1;
+
+  res = ContinuousToDiscrete<float, 2, 2, 1, 1>(
+      m_a, m_b, m_c, m_d, ts, &prt_a_d, &prt_b_d, &prt_c_d, &prt_d_d);
+
+  EXPECT_TRUE(res);
+
+  EXPECT_FLOAT_EQ(prt_a_d(0, 0), 3);
+  EXPECT_FLOAT_EQ(prt_a_d(0, 1), 0);
+  EXPECT_FLOAT_EQ(prt_a_d(1, 0), 0);
+  EXPECT_FLOAT_EQ(prt_a_d(1, 1), 3);
+
+  EXPECT_FLOAT_EQ(prt_b_d(0, 0), 2);
+  EXPECT_FLOAT_EQ(prt_b_d(1, 0), 2);
+
+  EXPECT_FLOAT_EQ(prt_c_d(0, 0), 2);
+  EXPECT_FLOAT_EQ(prt_c_d(0, 1), 2);
+
+  EXPECT_FLOAT_EQ(prt_d_d(0, 0), 3);
+
+  ts = 0.1;
+
+  res = ContinuousToDiscrete<float, 2, 2, 1, 1>(
+      m_a, m_b, m_c, m_d, ts, &prt_a_d, &prt_b_d, &prt_c_d, &prt_d_d);
+
+  EXPECT_TRUE(res);
+
+  EXPECT_FLOAT_EQ(prt_a_d(0, 0), 1.1052631);
+  EXPECT_FLOAT_EQ(prt_a_d(0, 1), 0);
+  EXPECT_FLOAT_EQ(prt_a_d(1, 0), 0);
+  EXPECT_FLOAT_EQ(prt_a_d(1, 1), 1.1052631);
+
+  EXPECT_FLOAT_EQ(prt_b_d(0, 0), 0.33287135);
+  EXPECT_FLOAT_EQ(prt_b_d(1, 0), 0.33287135);
+
+  EXPECT_FLOAT_EQ(prt_c_d(0, 0), 0.33287135);
+  EXPECT_FLOAT_EQ(prt_c_d(0, 1), 0.33287135);
+
+  EXPECT_FLOAT_EQ(prt_d_d(0, 0), 2.0526316);
+
+  ts = 0.01;
+
+  res = ContinuousToDiscrete<float, 2, 2, 1, 1>(
+      m_a, m_b, m_c, m_d, ts, &prt_a_d, &prt_b_d, &prt_c_d, &prt_d_d);
+
+  EXPECT_TRUE(res);
+
+  EXPECT_FLOAT_EQ(prt_a_d(0, 0), 1.0100503);
+  EXPECT_FLOAT_EQ(prt_a_d(0, 1), 0);
+  EXPECT_FLOAT_EQ(prt_a_d(1, 0), 0);
+  EXPECT_FLOAT_EQ(prt_a_d(1, 1), 1.0100503);
+
+  EXPECT_FLOAT_EQ(prt_b_d(0, 0), 0.10050251);
+  EXPECT_FLOAT_EQ(prt_b_d(1, 0), 0.10050251);
+
+  EXPECT_FLOAT_EQ(prt_c_d(0, 0), 0.10050251);
+  EXPECT_FLOAT_EQ(prt_c_d(0, 1), 0.10050251);
+
+  EXPECT_FLOAT_EQ(prt_d_d(0, 0), 2.0050251);
 }
 
 }  // namespace math
