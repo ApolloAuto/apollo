1a0fb82221ea7fe6b4979f8ef7681b20acd224fc
unacao <yufcao@gmail.com>
2018-05-09 16:10:32 -0700

    filter point cloud in async thread

--- modules/dreamview/backend/point_cloud/point_cloud_updater.cc
+++ modules/dreamview/backend/point_cloud/point_cloud_updater.cc
@@ -16,13 +16,14 @@
 
 #include "modules/dreamview/backend/point_cloud/point_cloud_updater.h"
 
+#include <utility>
+
 #include "modules/common/adapters/adapter_manager.h"
 #include "modules/common/log.h"
 #include "modules/common/time/time.h"
 #include "modules/dreamview/backend/common/dreamview_gflags.h"
+#include "modules/dreamview/proto/point_cloud.pb.h"
 #include "pcl/filters/voxel_grid.h"
-#include "pcl/point_cloud.h"
-#include "pcl/point_types.h"
 #include "pcl_conversions/pcl_conversions.h"
 #include "third_party/json/json.hpp"
 
@@ -38,7 +39,12 @@ using Json = nlohmann::json;
 PointCloudUpdater::PointCloudUpdater(WebSocketHandler *websocket)
     : websocket_(websocket) {
   RegisterMessageHandlers();
-  point_cloud_.SerializeToString(&point_cloud_str_);
+  point_cloud_str_ = "";
+  future_ready_ = true;
+}
+
+PointCloudUpdater::~PointCloudUpdater() {
+  Stop();
 }
 
 void PointCloudUpdater::RegisterMessageHandlers() {
@@ -55,11 +61,10 @@ void PointCloudUpdater::RegisterMessageHandlers() {
       [this](const Json &json, WebSocketHandler::Connection *conn) {
         std::string to_send;
         // If there is no point_cloud data for more than 2 seconds, reset.
-        if (point_cloud_.num_size() > 0 &&
+        if (point_cloud_str_ != "" &&
             std::fabs(last_localization_time_ - last_point_cloud_time_) > 2.0) {
-          point_cloud_.clear_num();
           boost::unique_lock<boost::shared_mutex> writer_lock(mutex_);
-          point_cloud_.SerializeToString(&point_cloud_str_);
+          point_cloud_str_ = "";
         }
         {
           boost::shared_lock<boost::shared_mutex> reader_lock(mutex_);
@@ -95,37 +100,55 @@ void PointCloudUpdater::Start() {
       &PointCloudUpdater::UpdateLocalizationTime, this);
 }
 
+void PointCloudUpdater::Stop() {
+  if (enabled_) {
+    async_future_.wait();
+  }
+}
+
 void PointCloudUpdater::UpdatePointCloud(const PointCloud2 &point_cloud) {
   if (!enabled_) {
     return;
   }
 
   last_point_cloud_time_ = point_cloud.header.stamp.toSec();
-  // transform from ros to pcl
-  pcl::PointCloud<pcl::PointXYZ>::Ptr pcl_ptr(
-      new pcl::PointCloud<pcl::PointXYZ>);
-  pcl::fromROSMsg(point_cloud, *pcl_ptr);
+  // Check if last filter process has finished before processing new data.
+  if (future_ready_) {
+    future_ready_ = false;
+    // transform from ros to pcl
+    pcl::PointCloud<pcl::PointXYZ>::Ptr pcl_ptr(
+        new pcl::PointCloud<pcl::PointXYZ>);
+    pcl::fromROSMsg(point_cloud, *pcl_ptr);
+    std::future<void> f = std::async(
+        std::launch::async, &PointCloudUpdater::FilterPointCloud, this,
+        pcl_ptr);
+    async_future_ = std::move(f);
+  }
+}
 
-  pcl::VoxelGrid<pcl::PointXYZ> voxel_grid;
+void PointCloudUpdater::FilterPointCloud(
+    const pcl::PointCloud<pcl::PointXYZ>::Ptr &pcl_ptr) {
+  pcl::VoxelGrid < pcl::PointXYZ > voxel_grid;
   voxel_grid.setInputCloud(pcl_ptr);
   voxel_grid.setLeafSize(1.0f, 1.0f, 0.2f);
   voxel_grid.filter(*pcl_ptr);
   AINFO << "filtered point cloud data size: " << pcl_ptr->size();
 
-  point_cloud_.Clear();
+  PointCloud point_cloud_pb;
   for (size_t idx = 0; idx < pcl_ptr->size(); ++idx) {
     pcl::PointXYZ &pt = pcl_ptr->points[idx];
     if (!std::isnan(pt.x) && !std::isnan(pt.y) && !std::isnan(pt.z)) {
-      point_cloud_.add_num(pt.x);
-      point_cloud_.add_num(pt.y);
+      point_cloud_pb.add_num(pt.x);
+      point_cloud_pb.add_num(pt.y);
       // TODO(unacao): velodyne height should be updated by hmi store
       // upon vehicle change.
-      point_cloud_.add_num(pt.z + 1.91f);
+      point_cloud_pb.add_num(pt.z + 1.91f);
     }
   }
   {
     boost::unique_lock<boost::shared_mutex> writer_lock(mutex_);
-    point_cloud_.SerializeToString(&point_cloud_str_);
+    point_cloud_pb.SerializeToString(&point_cloud_str_);
+    future_ready_ = true;
   }
 }
 
