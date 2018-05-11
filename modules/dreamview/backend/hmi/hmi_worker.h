e489458248ed8ef31b3ffc21b3ab2b7aa7f6165d
Aaron Xiao <xiaoxiangquan@gmail.com>
2018-05-11 12:02:44 -0700

    HMI: Decouple HMIWorker to enable customized HMI client.

--- modules/dreamview/backend/hmi/hmi_worker.h
+++ modules/dreamview/backend/hmi/hmi_worker.h
@@ -24,7 +24,6 @@
 
 #include "modules/canbus/proto/chassis.pb.h"
 #include "modules/common/macro.h"
-#include "modules/dreamview/backend/map/map_service.h"
 #include "modules/dreamview/proto/hmi_config.pb.h"
 #include "modules/dreamview/proto/hmi_status.pb.h"
 #include "modules/dreamview/proto/voice_detection.pb.h"
@@ -36,6 +35,10 @@
  */
 namespace apollo {
 namespace dreamview {
+
+using ChangeModeHandler = std::function<void(const std::string&)>;
+using ChangeMapHandler = std::function<void(const std::string&)>;
+using ChangeVehicleHandler = std::function<void(const std::string&)>;
 
 // Singleton worker which does the actual work of HMI actions.
 class HMIWorker {
@@ -56,9 +59,19 @@ class HMIWorker {
   // Update system status.
   void UpdateSystemStatus(const apollo::monitor::SystemStatus& system_status);
 
+  // Register event handlers.
+  void RegisterChangeModeHandler(ChangeModeHandler handler) {
+    change_mode_handlers_.emplace_back(handler);
+  }
+  void RegisterChangeMapHandler(ChangeMapHandler handler) {
+    change_map_handlers_.emplace_back(handler);
+  }
+  void RegisterChangeVehicleHandler(ChangeVehicleHandler handler) {
+    change_vehicle_handlers_.emplace_back(handler);
+  }
   // Change current mode, map, vehicle and driving mode.
   void ChangeToMode(const std::string& mode_name);
-  void ChangeToMap(const std::string& map_name, MapService* map_service);
+  void ChangeToMap(const std::string& map_name);
   void ChangeToVehicle(const std::string& vehicle_name);
   static bool ChangeToDrivingMode(
       const apollo::canbus::Chassis::DrivingMode mode);
@@ -79,6 +92,10 @@ class HMIWorker {
   HMIConfig config_;
   HMIStatus status_;
   mutable boost::shared_mutex status_mutex_;
+
+  std::vector<ChangeModeHandler> change_mode_handlers_;
+  std::vector<ChangeMapHandler> change_map_handlers_;
+  std::vector<ChangeVehicleHandler> change_vehicle_handlers_;
 
   DECLARE_SINGLETON(HMIWorker);
 };
