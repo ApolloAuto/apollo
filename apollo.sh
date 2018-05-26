19ec87c7ea31814d8a805eeb4cee4ec2814bbc67
siyangy <yu.siyang@yahoo.com>
2018-05-25 18:17:52 -0700

    Check APOLLO_IN_DOCKER in apollo.sh (#4272)

--- apollo.sh
+++ apollo.sh
@@ -29,8 +29,7 @@ function source_apollo_base() {
 
 function apollo_check_system_config() {
   # check docker environment
-  if [ ${MACHINE_ARCH} == "x86_64" ] && [ $(hostname) != "in_dev_docker" ] &&
-       [ $(hostname) != "in_release_docker" ]; then
+  if [ ${MACHINE_ARCH} == "x86_64" ] && [ ${APOLLO_IN_DOCKER} != "true" ]; then
     echo -e "${RED}Must run $0 in dev docker or release docker${NO_COLOR}"
     exit 0
   fi
