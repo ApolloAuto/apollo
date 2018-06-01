f7b811adbe77253fbd2fd99b922890f4dc72f31d
jmtao <taojm99@gmail.com>
2018-06-01 13:59:35 -0700

    planning: proto changes for pull-over

--- modules/planning/integration_tests/sunnyvale_big_loop_test.cc
+++ modules/planning/integration_tests/sunnyvale_big_loop_test.cc
@@ -622,14 +622,12 @@ TEST_F(SunnyvaleBigLoopTest, destination_pull_over_01) {
               planning_state->pull_over().in_pull_over());
   EXPECT_EQ(PullOverStatus::DESTINATION, planning_state->pull_over().reason());
 
-  PointENU start_point_0;
-  start_point_0.set_x(planning_state->pull_over().start_point().x());
-  start_point_0.set_y(planning_state->pull_over().start_point().y());
-  PointENU stop_point_0;
-  stop_point_0.set_x(planning_state->pull_over().stop_point().x());
-  stop_point_0.set_y(planning_state->pull_over().stop_point().y());
-  double stop_heading_0 = planning_state->pull_over().stop_heading();
-  double start_time_0 = planning_state->pull_over().start_time();
+  double stop_point_s_0 = planning_state->pull_over().stop_point_s();
+  double stop_point_l_0 = planning_state->pull_over().stop_point_l();
+  double stop_point_heading_0 =
+      planning_state->pull_over().stop_point_heading();
+  double start_point_s_0 = planning_state->pull_over().start_point_s();
+  double status_set_time_0 = planning_state->pull_over().status_set_time();
 
   // check PULL OVER decision
   RUN_GOLDEN_TEST_DECISION(1);
@@ -638,21 +636,18 @@ TEST_F(SunnyvaleBigLoopTest, destination_pull_over_01) {
               planning_state->pull_over().in_pull_over());
   EXPECT_EQ(PullOverStatus::DESTINATION, planning_state->pull_over().reason());
 
-  PointENU start_point_1;
-  start_point_1.set_x(planning_state->pull_over().start_point().x());
-  start_point_1.set_y(planning_state->pull_over().start_point().y());
-  PointENU stop_point_1;
-  stop_point_1.set_x(planning_state->pull_over().stop_point().x());
-  stop_point_1.set_y(planning_state->pull_over().stop_point().y());
-  double stop_heading_1 = planning_state->pull_over().stop_heading();
-  double start_time_1 = planning_state->pull_over().start_time();
+  double stop_point_s_1 = planning_state->pull_over().stop_point_s();
+  double stop_point_l_1 = planning_state->pull_over().stop_point_l();
+  double stop_point_heading_1 =
+      planning_state->pull_over().stop_point_heading();
+  double start_point_s_1 = planning_state->pull_over().start_point_s();
+  double status_set_time_1 = planning_state->pull_over().status_set_time();
 
-  EXPECT_EQ(start_point_0.x(), start_point_1.x());
-  EXPECT_EQ(start_point_0.y(), start_point_1.y());
-  EXPECT_EQ(stop_point_0.x(), stop_point_1.x());
-  EXPECT_EQ(stop_point_0.y(), stop_point_1.y());
-  EXPECT_EQ(stop_heading_0, stop_heading_1);
-  EXPECT_EQ(start_time_0, start_time_1);
+  EXPECT_EQ(stop_point_s_0, stop_point_s_1);
+  EXPECT_EQ(stop_point_l_0, stop_point_l_1);
+  EXPECT_EQ(stop_point_heading_0, stop_point_heading_1);
+  EXPECT_EQ(start_point_s_0, start_point_s_1);
+  EXPECT_EQ(status_set_time_0, status_set_time_1);
 }
 
 /*
