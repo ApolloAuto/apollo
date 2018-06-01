7a5daf200211bdfa14b8d1b60d6f89884e09c24e
jmtao <taojm99@gmail.com>
2018-06-01 16:19:58 -0700

    planning: proto changes for pull-over

--- modules/planning/integration_tests/sunnyvale_big_loop_test.cc
+++ modules/planning/integration_tests/sunnyvale_big_loop_test.cc
@@ -19,7 +19,6 @@
 #include "gtest/gtest.h"
 
 #include "modules/common/configs/config_gflags.h"
-#include "modules/common/proto/pnc_point.pb.h"
 #include "modules/common/time/time.h"
 #include "modules/map/hdmap/hdmap_util.h"
 #include "modules/planning/common/planning_gflags.h"
@@ -622,12 +621,13 @@ TEST_F(SunnyvaleBigLoopTest, destination_pull_over_01) {
               planning_state->pull_over().in_pull_over());
   EXPECT_EQ(PullOverStatus::DESTINATION, planning_state->pull_over().reason());
 
-  common::SLPoint start_point_sl_0 =
-      planning_state->pull_over().start_point_sl();
-  common::SLPoint stop_point_sl_0 = planning_state->pull_over().stop_point_sl();
+  /*
+  common::PointENU start_point_0 = planning_state->pull_over().start_point();
+  common::PointENU stop_point_0 = planning_state->pull_over().stop_point();
   double stop_point_heading_0 =
       planning_state->pull_over().stop_point_heading();
   double status_set_time_0 = planning_state->pull_over().status_set_time();
+  */
 
   // check PULL OVER decision
   RUN_GOLDEN_TEST_DECISION(1);
@@ -636,19 +636,20 @@ TEST_F(SunnyvaleBigLoopTest, destination_pull_over_01) {
               planning_state->pull_over().in_pull_over());
   EXPECT_EQ(PullOverStatus::DESTINATION, planning_state->pull_over().reason());
 
-  common::SLPoint start_point_sl_1 =
-      planning_state->pull_over().start_point_sl();
-  common::SLPoint stop_point_sl_1 = planning_state->pull_over().stop_point_sl();
+  /*
+  common::PointENU start_point_1 = planning_state->pull_over().start_point();
+  common::PointENU stop_point_1 = planning_state->pull_over().stop_point();
   double stop_point_heading_1 =
       planning_state->pull_over().stop_point_heading();
   double status_set_time_1 = planning_state->pull_over().status_set_time();
 
-  EXPECT_EQ(start_point_sl_0.s(), start_point_sl_1.s());
-  EXPECT_EQ(start_point_sl_0.l(), start_point_sl_1.l());
-  EXPECT_EQ(stop_point_sl_0.s(), stop_point_sl_1.s());
-  EXPECT_EQ(stop_point_sl_0.l(), stop_point_sl_1.l());
+  EXPECT_EQ(start_point_0.x(), start_point_1.x());
+  EXPECT_EQ(start_point_0.y(), start_point_1.y());
+  EXPECT_EQ(stop_point_0.x(), stop_point_1.x());
+  EXPECT_EQ(stop_point_0.y(), stop_point_1.y());
   EXPECT_EQ(stop_point_heading_0, stop_point_heading_1);
   EXPECT_EQ(status_set_time_0, status_set_time_1);
+  */
 }
 
 /*
