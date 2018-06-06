24e840cb34c6619a608b4403dca5e7469d849961
jmtao <taojm99@gmail.com>
2018-06-05 19:25:41 -0700

    planning: make stop in lane decision if finding pull-over stop point fails

--- modules/planning/tasks/traffic_decider/pull_over.cc
+++ modules/planning/tasks/traffic_decider/pull_over.cc
@@ -57,12 +57,12 @@ Status PullOver::ApplyRule(Frame* const frame,
   }
 
   common::PointENU stop_point;
-  if (GetPullOverStop(&stop_point) != 0) {
+  if (GetPullOverStop(&stop_point) == 0) {
+    BuildPullOverStop(stop_point);
+  } else {
+    BuildInLaneStop(stop_point);
     ADEBUG << "Could not find a safe pull over point";
-    return Status::OK();
   }
-
-  BuildPullOverStop(stop_point);
 
   return Status::OK();
 }
@@ -367,11 +367,66 @@ int PullOver::FindPullOverStop(common::PointENU* stop_point) {
 
 int PullOver::BuildPullOverStop(const common::PointENU& stop_point) {
   const auto& reference_line = reference_line_info_->reference_line();
-
-  // check
   common::SLPoint stop_point_sl;
   reference_line.XYToSL(stop_point, &stop_point_sl);
-  if (stop_point_sl.s() < 0 || stop_point_sl.s() > reference_line.Length()) {
+
+  double stop_line_s = stop_point_sl.s() - config_.pull_over().stop_distance();
+  double stop_point_heading =
+      reference_line.GetReferencePoint(stop_point_sl.s()).heading();
+
+  BuildStopDecision(stop_point_sl.s(), stop_point, stop_point_heading);
+
+  // record in PlanningStatus
+  auto* pull_over_status = GetPlanningStatus()->
+      mutable_planning_state()->mutable_pull_over();
+
+  common::SLPoint start_point_sl;
+  start_point_sl.set_s(
+      stop_point_sl.s() - config_.pull_over().operation_length());
+  start_point_sl.set_l(0.0);
+  common::math::Vec2d start_point;
+  reference_line.SLToXY(start_point_sl, &start_point);
+  pull_over_status->mutable_start_point()->set_x(start_point.x());
+  pull_over_status->mutable_start_point()->set_y(start_point.y());
+  pull_over_status->mutable_stop_point()->set_x(stop_point.x());
+  pull_over_status->mutable_stop_point()->set_y(stop_point.y());
+  pull_over_status->set_stop_point_heading(stop_point_heading);
+
+  ADEBUG << "pull_over_status: " << pull_over_status->DebugString();
+
+  return 0;
+}
+
+int PullOver::BuildInLaneStop(const common::PointENU& pull_over_stop_point) {
+  const auto& reference_line = reference_line_info_->reference_line();
+  common::SLPoint stop_point_sl;
+  reference_line.XYToSL(pull_over_stop_point, &stop_point_sl);
+  auto point = reference_line.GetReferencePoint(stop_point_sl.s());
+  common::PointENU stop_point;
+  stop_point.set_x(point.x());
+  stop_point.set_y(point.y());
+
+  double stop_line_s = stop_point_sl.s() - config_.pull_over().stop_distance();
+  double stop_point_heading =
+      reference_line.GetReferencePoint(stop_point_sl.s()).heading();
+
+  BuildStopDecision(stop_line_s, stop_point, stop_point_heading);
+
+  // record in PlanningStatus
+  auto* planning_state = GetPlanningStatus()->mutable_planning_state();
+  planning_state->clear_pull_over();
+
+  ADEBUG << "planning_state: " << planning_state->DebugString();
+
+  return 0;
+}
+
+
+int PullOver::BuildStopDecision(const double stop_line_s,
+                                const common::PointENU& stop_point,
+                                const double stop_point_heading) {
+  const auto& reference_line = reference_line_info_->reference_line();
+  if (stop_line_s < 0 || stop_line_s > reference_line.Length()) {
     return -1;
   }
 
@@ -382,7 +437,7 @@ int PullOver::BuildPullOverStop(const common::PointENU& stop_point) {
       PullOverStatus_Reason_Name(pull_over_reason);
   auto* obstacle = frame_->CreateStopObstacle(reference_line_info_,
                                               virtual_obstacle_id,
-                                              stop_point_sl.s());
+                                              stop_line_s);
   if (!obstacle) {
     AERROR << "Failed to create obstacle[" << virtual_obstacle_id << "]";
     return -1;
@@ -394,9 +449,6 @@ int PullOver::BuildPullOverStop(const common::PointENU& stop_point) {
   }
 
   // build stop decision
-  double stop_point_heading =
-      reference_line.GetReferencePoint(stop_point_sl.s()).heading();
-
   ObjectDecisionType stop;
   auto stop_decision = stop.mutable_stop();
   stop_decision->set_reason_code(StopReasonCode::STOP_REASON_PULL_OVER);
@@ -417,24 +469,6 @@ int PullOver::BuildPullOverStop(const common::PointENU& stop_point) {
   path_decision->AddLongitudinalDecision(
       TrafficRuleConfig::RuleId_Name(config_.rule_id()), stop_wall->Id(), stop);
 
-  // record in PlanningStatus
-  auto* pull_over_status = GetPlanningStatus()->
-      mutable_planning_state()->mutable_pull_over();
-
-  common::SLPoint start_point_sl;
-  start_point_sl.set_s(
-      stop_point_sl.s() - config_.pull_over().operation_length());
-  start_point_sl.set_l(0.0);
-  common::math::Vec2d start_point;
-  reference_line.SLToXY(start_point_sl, &start_point);
-  pull_over_status->mutable_start_point()->set_x(start_point.x());
-  pull_over_status->mutable_start_point()->set_y(start_point.y());
-
-  pull_over_status->mutable_stop_point()->set_x(stop_point.x());
-  pull_over_status->mutable_stop_point()->set_y(stop_point.y());
-  pull_over_status->set_stop_point_heading(stop_point_heading);
-
-  ADEBUG << "pull_over_status: " << pull_over_status->DebugString();
   return 0;
 }
 
