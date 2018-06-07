b99c1d4eaf19eeb379bab0efe9a1001b6b47dbb4
kechxu <kechxu89@gmail.com>
2018-06-07 10:48:54 -0700

    Perception: fill the framework of the steps to deal with ultrasonic message

--- modules/perception/obstacle/onboard/ultrasonic_obstacle_subnode.cc
+++ modules/perception/obstacle/onboard/ultrasonic_obstacle_subnode.cc
@@ -16,8 +16,35 @@
 
 #include "modules/perception/obstacle/onboard/ultrasonic_obstacle_subnode.h"
 
+#include "modules/common/log.h"
+
 namespace apollo {
 namespace perception {
+
+void UltrasonicObstacleSubnode::OnUltrasonic(
+    const apollo::canbus::Chassis& message) {
+  ++seq_num_;
+  std::shared_ptr<SensorObjects> sensor_objects(new SensorObjects);
+  double timestamp = message.header().timestamp_sec();
+  sensor_objects->timestamp = timestamp;
+  // TODO(all) set sensor_objects->sensor_type
+  sensor_objects->sensor_id = device_id_;
+  sensor_objects->seq_num = seq_num_;
+
+  // TODO(all) transform message to object and insert into sensor_objects
+
+  if (!PublishDataAndEvent(timestamp, sensor_objects)) {
+    AERROR << "Failed to publish data.";
+    sensor_objects->error_code = apollo::common::PERCEPTION_ERROR_PROCESS;
+    return;
+  }
+}
+
+bool UltrasonicObstacleSubnode::PublishDataAndEvent(
+    const double timestamp, const SharedDataPtr<SensorObjects>& data) {
+  // TODO(all) implement
+  return true;
+}
 
 }  // namespace perception
 }  // namespace apollo
