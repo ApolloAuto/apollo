6bc39456540e2f05b82371e8d8239ffcb371fd6a
Tony Zhang <hitxfd.tony@gmail.com>
2018-06-11 12:19:16 +0800

    Remove redundant code between rslidar16 and rslidar32
    Signed-off-by: Tony Zhang <hitxfd.tony@gmail.com>

--- modules/drivers/rslidar/README.md
+++ modules/drivers/rslidar/README.md
@@ -1,4 +1,9 @@
 
+## RS-LiDAR
+rslidar driver including:
+1. data packet processing --> driver_node
+2. point cloud generation --> convert_node
+
 ## How to run
 roslaunch rslidar_pointcloud rs_lidar_16.launch
 
--- modules/drivers/rslidar/rslidar_pointcloud/src/lib/rslidar16Parser.cpp
+++ modules/drivers/rslidar/rslidar_pointcloud/src/lib/rslidar16Parser.cpp
@@ -36,7 +36,6 @@ namespace rslidar {
 		private_nh.param("curves_path", curvesPath, std::string(""));
 		private_nh.param("angle_path", anglePath, std::string(""));
 		private_nh.param("channel_path", channelPath, std::string(""));
-		private_nh.param("curves_rate_path", curvesRatePath, std::string(""));
 	
 		/// 读参数文件 2017-02-27
 		FILE *f_inten = fopen(curvesPath.c_str(), "r");
@@ -47,26 +46,18 @@ namespace rslidar {
 			ROS_ERROR_STREAM(curvesPath << " does not exist");
 		} else {
 			while (!feof(f_inten)) {
-				float a[32];
+				float a[16];
 				loopi++;
 				if (loopi > 7)
 					break;
-				if (numOfLasers == 16) {
-						fscanf(f_inten, "%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f\n",
-							   &a[0], &a[1], &a[2], &a[3], &a[4], &a[5], &a[6], &a[7], &a[8], &a[9], &a[10], &a[11], &a[12],
-							   &a[13],
-							   &a[14], &a[15]);
-				} else if (numOfLasers == 32) {
-						fscanf(f_inten,
-							   "%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f\n",
-							   &a[0], &a[1], &a[2], &a[3], &a[4], &a[5], &a[6], &a[7], &a[8], &a[9], &a[10], &a[11], &a[12],
-							   &a[13],
-							   &a[14], &a[15], &a[16], &a[17], &a[18], &a[19], &a[20], &a[21], &a[22], &a[23], &a[24],
-							   &a[25], &a[26], &a[27],
-							   &a[28], &a[29], &a[30], &a[31]);
-				}
-				for (loopj = 0; loopj < numOfLasers; loopj++) {
-						aIntensityCal[loopi - 1][loopj] = a[loopj];
+
+				fscanf(f_inten, "%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f\n",
+					   &a[0], &a[1], &a[2], &a[3], &a[4], &a[5], &a[6], &a[7], &a[8], &a[9], &a[10], &a[11], &a[12],
+					   &a[13],
+					   &a[14], &a[15]);
+
+				for (loopj = 0; loopj < 16; loopj++) {
+					aIntensityCal[loopi - 1][loopj] = a[loopj];
 				}
 			}
 				fclose(f_inten);
@@ -76,15 +67,15 @@ namespace rslidar {
 		if (!f_angle) {
 			ROS_ERROR_STREAM(anglePath << " does not exist");
 		} else {
-			float b[32], d[32];
+			float b[16], d[16];
 			int loopk = 0;
 			int loopn = 0;
 			while (!feof(f_angle)) {
 				fscanf(f_angle, "%f,%f\n", &b[loopk], &d[loopk]);
 				loopk++;
-				if (loopk > (numOfLasers - 1)) break;
+				if (loopk > 15) break;
 			}
-			for (loopn = 0; loopn < numOfLasers; loopn++) {
+			for (loopn = 0; loopn < 16; loopn++) {
 				VERT_ANGLE[loopn] = b[loopn] / 180 * M_PI;
 				HORI_ANGLE[loopn] = d[loopn] * 100;
 			}
@@ -98,76 +89,37 @@ namespace rslidar {
 		} else {
 			int loopl = 0;
 			int loopm = 0;
-			int c[51];
+			int c[41];
 			int tempMode = 1;
 			while (!feof(f_channel)) {
-				if(numOfLasers == 16)
-				{
-					fscanf(f_channel,
-						"%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d\n",
-						&c[0], &c[1], &c[2], &c[3], &c[4], &c[5], &c[6], &c[7], &c[8], &c[9], &c[10], &c[11], &c[12],
-						&c[13], &c[14], &c[15],
-						&c[16], &c[17], &c[18], &c[19], &c[20], &c[21], &c[22], &c[23], &c[24], &c[25], &c[26], &c[27],
-						&c[28], &c[29], &c[30],
-						&c[31], &c[32], &c[33], &c[34], &c[35], &c[36], &c[37], &c[38], &c[39], &c[40]);
-			    }
-				else
-				{
-					fscanf(f_channel,
-						"%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d\n",
-						&c[0], &c[1], &c[2], &c[3], &c[4], &c[5], &c[6], &c[7], &c[8], &c[9], &c[10], &c[11], &c[12],
-						&c[13], &c[14], &c[15],
-						&c[16], &c[17], &c[18], &c[19], &c[20], &c[21], &c[22], &c[23], &c[24], &c[25], &c[26], &c[27],
-						&c[28], &c[29], &c[30],
-						&c[31], &c[32], &c[33], &c[34], &c[35], &c[36], &c[37], &c[38], &c[39], &c[40], 
-						&c[41], &c[42], &c[43], &c[44], &c[45], &c[46], &c[47], &c[48], &c[49], &c[50]);
-				}
-	
-				for (loopl = 0; loopl < TEMPERATURE_RANGE+1; loopl++) {
+
+				fscanf(f_channel,
+					   "%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d\n",
+					   &c[0], &c[1], &c[2], &c[3], &c[4], &c[5], &c[6], &c[7], &c[8], &c[9], &c[10], &c[11], &c[12],
+					   &c[13], &c[14], &c[15],
+					   &c[16], &c[17], &c[18], &c[19], &c[20], &c[21], &c[22], &c[23], &c[24], &c[25], &c[26], &c[27],
+					   &c[28], &c[29], &c[30],
+					   &c[31], &c[32], &c[33], &c[34], &c[35], &c[36], &c[37], &c[38], &c[39], &c[40]);
+
+				for (loopl = 0; loopl < TEMPERATURE_RANGE + 1; loopl++) {
 					g_ChannelNum[loopm][loopl] = c[tempMode * loopl];
 				}
 				loopm++;
-				if (loopm > (numOfLasers - 1)) {
+				if (loopm > 15) {
 					break;
 				}
 			}
-				fclose(f_channel);
+			fclose(f_channel);
 		}
-	
-		if(numOfLasers == 32){
-			FILE *f_curvesRate = fopen(curvesRatePath.c_str(), "r");
-			if(!f_curvesRate)
-			{
-				ROS_ERROR_STREAM(curvesRatePath << " does not exist");
-			}
-			else
-			{
-				int loopk = 0;
-				while(!feof(f_curvesRate))
-				{
-					fscanf(f_curvesRate, "%f\n", &CurvesRate[loopk]);
-					loopk++;
-					if(loopk > (numOfLasers-1)) break;
-				}
-				fclose(f_curvesRate);
-			}
-		}
-	
-	
+
 	}
 	
 	/** Set up for on-line operation. */
 	void rslidar16Parser::init_setup() {
 		pic.col = 0;
-		if (numOfLasers == 16) {
-			pic.distance.resize(RS16_DATA_NUMBER_PER_SCAN);
-			pic.intensity.resize(RS16_DATA_NUMBER_PER_SCAN);
-			pic.azimuthforeachP.resize(RS16_DATA_NUMBER_PER_SCAN);
-		} else if (numOfLasers == 32) {
-			pic.distance.resize(RS32_DATA_NUMBER_PER_SCAN);
-			pic.intensity.resize(RS32_DATA_NUMBER_PER_SCAN);
-			pic.azimuthforeachP.resize(RS32_DATA_NUMBER_PER_SCAN);
-		}
+		pic.distance.resize(RS16_DATA_NUMBER_PER_SCAN);
+		pic.intensity.resize(RS16_DATA_NUMBER_PER_SCAN);
+		pic.azimuthforeachP.resize(RS16_DATA_NUMBER_PER_SCAN);
 	}
 	
 		
