ApolloAuto/apollo

commit 2078288882aaeb09754780497a15e81648785543

fix some misconfigs

modules/perception/conf/dag_camera_obstacle_offline_all.config
@@ -16,16 +16,9 @@ subnode_config {
    subnodes {
         id: 5
         name: "LanePostProcessingSubnode"
-#        reserve: "device_id:camera;publish:2;"
         reserve: "device_id:camera;publish:2;motion_event_id:1021"
         type: SUBNODE_NORMAL
     }
-#    subnodes {
-#        id: 31
-#        name: "AsyncFusionSubnode"
-#        reserve: "camera_event_id:1009;lane_event_id:1010"
-#        type: SUBNODE_NORMAL
-#    }
     subnodes {
         id: 4
         name: "RadarProcessSubnode"
@@ -42,8 +35,7 @@ subnode_config {
     subnodes {
         id: 41
         name: "VisualizationSubnode"
-#        reserve: "vis_driven_event_id:1012;camera_event_id:1008;lane_event_id:1012"
-        reserve: "vis_driven_event_id:1012;camera_event_id:1008;motion_event_id:1020;lane_event_id:1012;fusion_event_id:1011;radar_event_id:1014;"
+        reserve: "vis_driven_event_id:1012;motion_event_id:1020;fusion_event_id:1011;"
         type: SUBNODE_OUT
     }
 }

modules/perception/obstacle/onboard/visualization_subnode.cc
@@ -107,16 +107,14 @@ bool VisualizationSubnode::InitInternal() {
     }
   }
 
-  if (lane_event_id_ != -1) {
-    lane_shared_data_ = dynamic_cast<LaneSharedData*>(
-        shared_data_manager_->GetSharedData("LaneSharedData"));
-    if (lane_shared_data_ == nullptr) {
-      AERROR << "Failed to get LaneSharedData.";
-      return false;
-    }
-    AINFO << "Init shared data successfully, data: "
-          << lane_shared_data_->name();
+  lane_shared_data_ = dynamic_cast<LaneSharedData*>(
+      shared_data_manager_->GetSharedData("LaneSharedData"));
+  if (lane_shared_data_ == nullptr) {
+    AERROR << "Failed to get LaneSharedData.";
+    return false;
   }
+
+  AINFO << "Init shared data successfully, data: " << lane_shared_data_->name();
 
   // init frame_visualizer
   RegisterFactoryGLFusionVisualizer();

modules/perception/conf/dag_camera_obstacle_offline_fusion.config
@@ -23,7 +23,6 @@ subnode_config {
         id: 31
         name: "FusionSubnode"
         reserve: "pub_driven_event_id:1009;lane_event_id:1010;camera_event_id:1009;radar_event_id:1013;"
-        #reserve: "pub_driven_event_id:1009;lane_event_id:1010;camera_event_id:1009;"
         type: SUBNODE_NORMAL
     }
 
@@ -31,7 +30,6 @@ subnode_config {
     subnodes {
         id: 41
         name: "VisualizationSubnode"
-        #reserve: "vis_driven_event_id:1011;camera_event_id:1008;fusion_event_id:1011;radar_event_id:1012;"
         reserve: "vis_driven_event_id:1011;fusion_event_id:1011;"
         type: SUBNODE_OUT
     }

modules/perception/conf/dag_camera_obstacle_offline_fusion_sync.config
@@ -30,7 +30,7 @@ subnode_config {
     subnodes {
         id: 41
         name: "VisualizationSubnode"
-        reserve: "vis_driven_event_id:1011;camera_event_id:1008;fusion_event_id:1011;radar_event_id:1012;lane_event_id:1014;"
+        reserve: "vis_driven_event_id:1011;fusion_event_id:1011;"
         type: SUBNODE_OUT
     }
 }

commit 453949f8bf0335e02e4ebc4f7c29fe846b10c0e3

Perception: Fix bug in visualization tool usage and config. Broken in #4512

modules/perception/conf/dag_camera_obstacle_offline_all.config
@@ -35,7 +35,7 @@ subnode_config {
     subnodes {
         id: 41
         name: "VisualizationSubnode"
-        reserve: "vis_driven_event_id:1012;motion_event_id:1020;fusion_event_id:1011;"
+        reserve: "vis_driven_event_id:1012;motion_event_id:1020;fusion_event_id:1011;lane_event_id:1010;camera_event_id:1008;radar_event_id:1012;"
         type: SUBNODE_OUT
     }
 }

modules/perception/obstacle/onboard/visualization_subnode.cc
@@ -76,15 +76,18 @@ bool VisualizationSubnode::InitInternal() {
     AINFO << "Init shared datas successfully, data: "
           << cipv_object_data_->name();
   }
+
   //  init radar object data
-  radar_object_data_ = dynamic_cast<RadarObjectData*>(
-      shared_data_manager_->GetSharedData("RadarObjectData"));
-  if (radar_object_data_ == nullptr) {
-    AERROR << "Failed to get RadarObjectData.";
-    return false;
+  if (radar_event_id_!= -1) {
+    radar_object_data_ = dynamic_cast<RadarObjectData*>(
+        shared_data_manager_->GetSharedData("RadarObjectData"));
+    if (radar_object_data_ == nullptr) {
+      AERROR << "Failed to get RadarObjectData.";
+      return false;
+    }
+    AINFO << "Init shared datas successfully, data: "
+          << radar_object_data_->name();
   }
-  AINFO << "Init shared datas successfully, data: "
-        << radar_object_data_->name();
 
   // init fusion data
   if (fusion_event_id_ != -1) {
@@ -107,14 +110,15 @@ bool VisualizationSubnode::InitInternal() {
     }
   }
 
-  lane_shared_data_ = dynamic_cast<LaneSharedData*>(
-      shared_data_manager_->GetSharedData("LaneSharedData"));
-  if (lane_shared_data_ == nullptr) {
-    AERROR << "Failed to get LaneSharedData.";
-    return false;
+  if (lane_event_id_ != -1) {
+    lane_shared_data_ = dynamic_cast<LaneSharedData*>(
+        shared_data_manager_->GetSharedData("LaneSharedData"));
+    if (lane_shared_data_ == nullptr) {
+      AERROR << "Failed to get LaneSharedData.";
+      return false;
+    }
+    AINFO << "Init shared data successfully, data: " << lane_shared_data_->name();
   }
-
-  AINFO << "Init shared data successfully, data: " << lane_shared_data_->name();
 
   // init frame_visualizer
   RegisterFactoryGLFusionVisualizer();
@@ -220,13 +224,15 @@ bool VisualizationSubnode::SubscribeEvents(const EventMeta& event_meta,
 void VisualizationSubnode::SetRadarContent(const std::string& data_key,
                                            FrameContent* content,
                                            double timestamp) {
-  std::shared_ptr<SensorObjects> objs;
+  if (radar_object_data_) {
+    std::shared_ptr<SensorObjects> objs;
 
-  if (!radar_object_data_->Get(data_key, &objs) || objs == nullptr) {
-    AERROR << "Failed to get shared data: " << radar_object_data_->name();
-    return;
+    if (!radar_object_data_->Get(data_key, &objs) || objs == nullptr) {
+      AERROR << "Failed to get shared data: " << radar_object_data_->name();
+      return;
+    }
+    content->set_radar_content(timestamp, objs->objects);
   }
-  content->set_radar_content(timestamp, objs->objects);
 }
 
 void VisualizationSubnode::SetCameraContent(const std::string& data_key,
@@ -280,12 +286,14 @@ void VisualizationSubnode::SetFusionContent(const std::string& data_key,
 void VisualizationSubnode::SetLaneContent(const std::string& data_key,
                                           FrameContent* content,
                                           double timestamp) {
-  LaneObjectsPtr lane_objs;
-  if (!lane_shared_data_->Get(data_key, &lane_objs) || lane_objs == nullptr) {
-    AERROR << "Failed to get shared data: " << lane_shared_data_->name();
-    return;
+  if (lane_shared_data_) {
+    LaneObjectsPtr lane_objs;
+    if (!lane_shared_data_->Get(data_key, &lane_objs) || lane_objs == nullptr) {
+      AERROR << "Failed to get shared data: " << lane_shared_data_->name();
+      return;
+    }
+    content->set_lane_content(timestamp, *lane_objs);
   }
-  content->set_lane_content(timestamp, *lane_objs);
 }
 
 void VisualizationSubnode::SetFrameContent(const Event& event,