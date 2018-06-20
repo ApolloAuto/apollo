da0c8a4b297faa14771e35155fd51a4af87ef653
luoqi06 <luoqi06@baidu.com>
2018-06-20 11:19:26 -0700

    Drivers : Fix lint and license for hermes can

--- modules/drivers/canbus/can_client/hermes_can/bcan.h
+++ modules/drivers/canbus/can_client/hermes_can/bcan.h
@@ -1,22 +1,26 @@
-/************************************************************************
-*
-* Copyright (c) 2016 Baidu.com, Inc. All Rights Reserved
-*
-************************************************************************/
-
-/**
-* @file bcan.h
-*
-* Baidu ADV (Autonomous Driving Vehicle) bcan library definitions.
-**/
+/******************************************************************************
+ * Copyright 2017 The Apollo Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *****************************************************************************/
 
 #ifndef ADU_PLAT_SW_LIB_BCAN_BCAN_H
 #define ADU_PLAT_SW_LIB_BCAN_BCAN_H
 
-#include <stdlib.h>
 #include <stdint.h>
-#include <sys/types.h>
+#include <stdlib.h>
 #include <sys/ioctl.h>
+#include <sys/types.h>
 
 /* bcan_msg_t and bcan_err_code definitions. */
 //#include "linux/bcan_defs.h"
@@ -26,31 +30,30 @@
 extern "C" {
 #endif
 
-#define BCAN_MAX_TX_MSG		256
-#define BCAN_MAX_RX_MSG		256
+#define BCAN_MAX_TX_MSG 256
+#define BCAN_MAX_RX_MSG 256
 
 // Channel states
-#define BCAN_DEV_UNINIT		-1
-#define BCAN_DEV_OPEN		(1 << 0)
-#define BCAN_DEV_CLOSE		(1 << 1)
-#define BCAN_DEV_BAUD_SET	(1 << 2)
-#define BCAN_DEV_NORMAL		(1 << 3)
-#define BCAN_DEV_LOOPBACK	(1 << 4)
-#define BCAN_DEV_CONFIG		(1 << 5)
-#define BCAN_DEV_START		(1 << 6)
-#define BCAN_DEV_STOP		(1 << 7)
-#define BCAN_DEV_ACTIVE		(1 << 8)
-#define BCAN_DEV_RECVD		(1 << 9)
-
+#define BCAN_DEV_UNINIT -1
+#define BCAN_DEV_OPEN (1 << 0)
+#define BCAN_DEV_CLOSE (1 << 1)
+#define BCAN_DEV_BAUD_SET (1 << 2)
+#define BCAN_DEV_NORMAL (1 << 3)
+#define BCAN_DEV_LOOPBACK (1 << 4)
+#define BCAN_DEV_CONFIG (1 << 5)
+#define BCAN_DEV_START (1 << 6)
+#define BCAN_DEV_STOP (1 << 7)
+#define BCAN_DEV_ACTIVE (1 << 8)
+#define BCAN_DEV_RECVD (1 << 9)
 
 typedef uint64_t bcan_hdl_t;
 
 enum bcan_baudrate_val {
-	BCAN_BAUDRATE_1M,
-	BCAN_BAUDRATE_500K,
-	BCAN_BAUDRATE_250K,
-	BCAN_BAUDRATE_150K,
-	BCAN_BAUDRATE_NUM
+  BCAN_BAUDRATE_1M,
+  BCAN_BAUDRATE_500K,
+  BCAN_BAUDRATE_250K,
+  BCAN_BAUDRATE_150K,
+  BCAN_BAUDRATE_NUM
 };
 
 /* Returns bcan library version. */
@@ -65,8 +68,8 @@ const char *bcan_bld_info_short(void);
 /* Returns error message corresponding to the given error code. */
 const char *bcan_get_err_msg(int err_code);
 
-int bcan_open(uint32_t dev_index, uint32_t flags, uint64_t tx_to, uint64_t rx_to,
-	bcan_hdl_t *hdl);
+int bcan_open(uint32_t dev_index, uint32_t flags, uint64_t tx_to,
+              uint64_t rx_to, bcan_hdl_t *hdl);
 int bcan_close(bcan_hdl_t hdl);
 int bcan_start(bcan_hdl_t hdl);
 int bcan_stop(bcan_hdl_t hdl);
@@ -92,4 +95,4 @@ int bcan_id_remove_all(bcan_hdl_t hdl);
 }
 #endif
 
-#endif  /* ADU_PLAT_SW_LIB_BCAN_BCAN_LIB_H */
+#endif /* ADU_PLAT_SW_LIB_BCAN_BCAN_LIB_H */
--- modules/drivers/canbus/can_client/hermes_can/hermes_can_client.h
+++ modules/drivers/canbus/can_client/hermes_can/hermes_can_client.h
@@ -1,19 +1,21 @@
-/***************************************************************************
+/******************************************************************************
+ * Copyright 2017 The Apollo Authors. All Rights Reserved.
  *
- * Copyright (c) 2016 Baidu.com, Inc. All Rights Reserved
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- **************************************************************************/
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *****************************************************************************/
 
-/**
- * @file esd_can_client.h
- * @author zhuzhenguang<zhuzhenguang@baidu.com>(zhuzhenguang@baidu.com)
- * @date 2016/03/16 20:54:43
- * @brief the encapsulate call the api of esd can card according to can_client.h interface
- *
- **/
-
-#ifndef  MODULES_DRIVERS_CANBUS_CAN_CLIENT_CLIENT_HERMES_CAN_CLIENT_H
-#define  MODULES_DRIVERS_CANBUS_CAN_CLIENT_CLIENT_HERMES_CAN_CLIENT_H
+#ifndef MODULES_DRIVERS_CANBUS_CAN_CLIENT_CLIENT_HERMES_CAN_CLIENT_H
+#define MODULES_DRIVERS_CANBUS_CAN_CLIENT_CLIENT_HERMES_CAN_CLIENT_H
 
 #include <string>
 
@@ -40,81 +42,81 @@ namespace can {
  */
 
 class HermesCanClient : public CanClient {
-public :
+ public:
+  /**
+   * @brief Initialize the BCAN client by specified CAN card parameters.
+   * @param parameter CAN card parameters to initialize the CAN client.
+   */
+  // explicit HermesCanClient(const CANCardParameter &parameter);
 
-    /**
-     * @brief Initialize the BCAN client by specified CAN card parameters.
-     * @param parameter CAN card parameters to initialize the CAN client.
-     */
-    //explicit HermesCanClient(const CANCardParameter &parameter);
-   
-    /**
-     * @brief Destructor
-     */
-    virtual ~HermesCanClient();
+  /**
+   * @brief Destructor
+   */
+  virtual ~HermesCanClient();
 
-    /**
-     * @brief Start the ESD CAN client.
-     * @return The status of the start action which is defined by
-     *         apollo::common::ErrorCode.
-     */
-    bool Init(const CANCardParameter &parameter) override;
+  /**
+   * @brief Start the ESD CAN client.
+   * @return The status of the start action which is defined by
+   *         apollo::common::ErrorCode.
+   */
+  bool Init(const CANCardParameter &parameter) override;
 
-    /**
-     * @brief Start the ESD CAN client.
-     * @return The status of the start action which is defined by
-     *         apollo::common::ErrorCode.
-     */
-    apollo::common::ErrorCode Start() override;
+  /**
+   * @brief Start the ESD CAN client.
+   * @return The status of the start action which is defined by
+   *         apollo::common::ErrorCode.
+   */
+  apollo::common::ErrorCode Start() override;
 
-    /**
-     * @brief Stop the ESD CAN client.
-     */
+  /**
+   * @brief Stop the ESD CAN client.
+   */
 
-    virtual void Stop();
+  virtual void Stop();
 
-    /**
-     * @brief Send messages
-     * @param frames The messages to send.
-     * @param frame_num The amount of messages to send.
-     * @return The status of the sending action which is defined by
-     *         apollo::common::ErrorCode.
-     */                                     
-    virtual apollo::common::ErrorCode Send(const std::vector<CanFrame> &frames, int32_t *const frame_num);
+  /**
+   * @brief Send messages
+   * @param frames The messages to send.
+   * @param frame_num The amount of messages to send.
+   * @return The status of the sending action which is defined by
+   *         apollo::common::ErrorCode.
+   */
+  virtual apollo::common::ErrorCode Send(const std::vector<CanFrame> &frames,
+                                         int32_t *const frame_num);
 
-   /**
-    * @brief Receive messages
-    * @param frames The messages to receive.
-    * @param frame_num The amount of messages to receive.
-    * @return The status of the receiving action which is defined by
-    *         apollo::common::ErrorCode.
-    */                                    
-    virtual apollo::common::ErrorCode Receive(std::vector<CanFrame> *const frames, 
-                                              int32_t *const frame_num);
-    /**
-     * @brief Get the error string.
-     * @param status The status to get the error string.
-     */                                              
-    virtual std::string GetErrorString(const int32_t status);
-    /**
-     * @brief Set inited status.
-     * @param if status is inited.
-     */  
-    void SetInited(bool init);
+  /**
+   * @brief Receive messages
+   * @param frames The messages to receive.
+   * @param frame_num The amount of messages to receive.
+   * @return The status of the receiving action which is defined by
+   *         apollo::common::ErrorCode.
+   */
+  virtual apollo::common::ErrorCode Receive(std::vector<CanFrame> *const frames,
+                                            int32_t *const frame_num);
+  /**
+   * @brief Get the error string.
+   * @param status The status to get the error string.
+   */
+  virtual std::string GetErrorString(const int32_t status);
+  /**
+   * @brief Set inited status.
+   * @param if status is inited.
+   */
+  void SetInited(bool init);
 
-private:
-    bool _is_init;
-    bcan_hdl_t _dev_handler;
-    CANCardParameter::CANChannelId _card_port;
-    bcan_msg_t _send_frames[MAX_CAN_SEND_FRAME_LEN];
-    bcan_msg_t _recv_frames[MAX_CAN_RECV_FRAME_LEN];
+ private:
+  bool _is_init;
+  bcan_hdl_t _dev_handler;
+  CANCardParameter::CANChannelId _card_port;
+  bcan_msg_t _send_frames[MAX_CAN_SEND_FRAME_LEN];
+  bcan_msg_t _recv_frames[MAX_CAN_RECV_FRAME_LEN];
 };
 
-} // namespace can
-} // namespace canbus
-} // namespace drivers
-} // namespace apollo
+}  // namespace can
+}  // namespace canbus
+}  // namespace drivers
+}  // namespace apollo
 
-#endif // MODULES_DRIVERS_CANBUS_CAN_CLIENT_CLIENT_HERMES_CAN_CLIENT_H
+#endif  // MODULES_DRIVERS_CANBUS_CAN_CLIENT_CLIENT_HERMES_CAN_CLIENT_H
 
 /* vim: set expandtab ts=4 sw=4 sts=4 tw=100: */
