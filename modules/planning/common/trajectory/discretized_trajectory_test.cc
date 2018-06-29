ba31c734cdfa4a96db746ca8bf659bca094729d6
Liangliang Zhang <zhangliangliang@baidu.com>
2018-06-29 12:52:03 -0700

    Planning: added discretized_trajectory_test.cc

---
+++ modules/planning/common/trajectory/discretized_trajectory_test.cc
@@ -0,0 +1,52 @@
+/******************************************************************************
+ * Copyright 2018 The Apollo Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *****************************************************************************/
+
+/**
+ * @file
+ **/
+
+#include "modules/planning/common/trajectory/discretized_trajectory.h"
+
+#include <iostream>
+#include <string>
+
+#include "gtest/gtest.h"
+
+#include "modules/common/util/file.h"
+
+namespace apollo {
+namespace planning {
+
+TEST(simple, DiscretizedTrajectory) {
+  const std::string path_of_standard_trajectory =
+      "modules/planning/testdata/trajectory_data/standard_trajectory.pb.txt";
+  ADCTrajectory trajectory;
+  EXPECT_TRUE(
+      common::util::GetProtoFromFile(path_of_standard_trajectory, &trajectory));
+  DiscretizedTrajectory discretized_trajectory(trajectory);
+  EXPECT_DOUBLE_EQ(discretized_trajectory.GetTemporalLength(),
+                   7.9999999999999885);
+  EXPECT_DOUBLE_EQ(discretized_trajectory.GetSpatialLength(),
+                   44.752319202675167);
+  auto p1 = discretized_trajectory.Evaluate(4.0);
+  EXPECT_DOUBLE_EQ(p1.path_point().x(), 587263.01182131236);
+  EXPECT_DOUBLE_EQ(p1.path_point().y(), 4140966.5720794979);
+  EXPECT_DOUBLE_EQ(p1.relative_time(), 4.0);
+  EXPECT_DOUBLE_EQ(p1.v(), 5.4412586837131443);
+}
+
+}  // namespace planning
+}  // namespace apollo
