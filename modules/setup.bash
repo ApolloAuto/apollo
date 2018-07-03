7eedcd3c6b0e223ee4a59d965a46840d17868315
Zongbao Feng <mickeyouou@gmail.com>
2018-07-04 01:20:29 +0800

    [drivers] * remove unused files about lslidar driver (#4675)
    remove tmp debug files

--- modules/setup.bash
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-# generated from catkin/cmake/templates/setup.bash.in
-
-CATKIN_SHELL=bash
-
-# source setup.sh from same directory as this file
-_CATKIN_SETUP_DIR=$(builtin cd "`dirname "${BASH_SOURCE[0]}"`" > /dev/null && pwd)
-. "$_CATKIN_SETUP_DIR/setup.sh"
--- modules/setup.zsh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env zsh
-# generated from catkin/cmake/templates/setup.zsh.in
-
-CATKIN_SHELL=zsh
-
-# source setup.sh from same directory as this file
-_CATKIN_SETUP_DIR=$(builtin cd -q "`dirname "$0"`" > /dev/null && pwd)
-emulate -R zsh -c 'source "$_CATKIN_SETUP_DIR/setup.sh"'
