bcc114b75da2476e1cb4834956feb8035cec9729
Liangliang Zhang <zhangliangliang@baidu.com>
2018-07-05 17:41:06 -0700

    Bazel build velodyne driver: added convert

---
+++ modules/drivers/lidar_velodyne/pointcloud/convert.h
@@ -0,0 +1,68 @@
+/******************************************************************************
+ * Copyright 2017 The Apollo Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *****************************************************************************/
+
+#ifndef MODULES_DRIVERS_LIDAR_VELODYNE_POINTCLOUD_CONVERT_H_
+#define MODULES_DRIVERS_LIDAR_VELODYNE_POINTCLOUD_CONVERT_H_
+
+#include <memory>
+#include <string>
+
+#include "nav_msgs/Odometry.h"
+#include "ros/ros.h"
+#include "sensor_msgs/Imu.h"
+#include "sensor_msgs/PointCloud2.h"
+#include "std_msgs/Time.h"
+
+#include "modules/drivers/lidar_velodyne/pointcloud/lib/velodyne_parser.h"
+
+namespace apollo {
+namespace drivers {
+namespace lidar_velodyne {
+
+// convert velodyne data to pointcloud and republish
+class Convert {
+ public:
+  Convert() = default;
+  virtual ~Convert() = default;
+
+  // init velodyne config struct from private_nh
+  void init(ros::NodeHandle& node, ros::NodeHandle& private_nh);  // NOLINT
+
+ private:
+  // convert velodyne data to pointcloud and public
+  void convert_packets_to_pointcloud(
+      velodyne_msgs::VelodyneScanUnified::ConstPtr scan_msg);
+
+  // RawData class for converting data to point cloud
+  std::unique_ptr<VelodyneParser> parser_;
+
+  ros::Subscriber velodyne_scan_;
+  ros::Publisher pointcloud_pub_;
+
+  std::string topic_packets_;
+  std::string topic_pointcloud_;
+
+  /// configuration parameters, get config struct from velodyne_parser.h
+  Config config_;
+  // queue size for ros node pub
+  int queue_size_;
+};
+
+}  // namespace lidar_velodyne
+}  // namespace drivers
+}  // namespace apollo
+
+#endif  // MODULES_DRIVERS_LIDAR_VELODYNE_POINTCLOUD_CONVERT_H_
