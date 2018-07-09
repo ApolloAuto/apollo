5b8b10ca7740151ebec00e67f195244e079c4b36
Jiangtao Hu <boris.hu@gmail.com>
2018-07-09 16:14:19 -0700

    Revert "docker: start supervisor when starting dev docker"
    This reverts commit 12dd30226a2ae8d26eff6348baf83ed660686db2.

--- docker/scripts/release_start.sh
+++ docker/scripts/release_start.sh
@@ -129,7 +129,6 @@ function main() {
             "mkdir -p '${DATA_DIR}'; chmod a+rw -R '${DATA_DIR}'"
       done
     fi
-    docker exec -u ${USER} apollo_release bash -c "supervisord -c /apollo/modules/tools/supervisord/release.conf >& /tmp/supervisord.start.log"
     docker exec -u ${USER} -it apollo_release "/apollo/scripts/bootstrap.sh"
 }
 
