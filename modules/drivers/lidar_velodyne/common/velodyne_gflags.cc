b6b8005eb59e36162f12f2963a55d8aa2e381e11
Liangliang Zhang <zhangliangliang@baidu.com>
2018-07-18 15:24:44 -0700

    Lidar velodyne: added config proto and added errorcode for velodyne.

---
+++ modules/drivers/lidar_velodyne/common/velodyne_gflags.cc
@@ -0,0 +1,25 @@
+/******************************************************************************
+ * Copyright 2018 The Apollo Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *****************************************************************************/
+
+#include "modules/drivers/lidar_velodyne/common/velodyne_gflags.h"
+
+DEFINE_string(velodyne_module_name, "velodyne", "Velodyne driver module name");
+DEFINE_string(velodyne_adapter_config_filename,
+              "modules/drivers/lidar_velodyne/conf/adapter.conf",
+              "The adapter config file");
+DEFINE_string(velodyne_conf_file,
+              "modules/drivers/lidar_velodyne/conf/velodyne_conf.pb.txt",
+              "Velodyne conf file");
