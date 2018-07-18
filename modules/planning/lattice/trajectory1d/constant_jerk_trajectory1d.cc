697e22ab0fb8408bf701f766d3f223385d367960
kechxu <kechxu89@gmail.com>
2018-07-18 14:29:14 -0700

    Planning: implement constant jerk trajectory

--- modules/planning/lattice/trajectory1d/constant_jerk_trajectory1d.cc
+++ modules/planning/lattice/trajectory1d/constant_jerk_trajectory1d.cc
@@ -21,17 +21,41 @@
 
 #include "modules/planning/lattice/trajectory1d/constant_jerk_trajectory1d.h"
 
-namespace apollo{
+#include "glog/logging.h"
+
+#include "modules/planning/common/planning_gflags.h"
+
+namespace apollo {
 namespace planning {
 
-ConstantJerkTrajectory1d::ConstantJerkTrajectory1d() {
-  // TODO Auto-generated constructor stub
-
+ConstantJerkTrajectory1d::ConstantJerkTrajectory1d(
+    const double p0, const double v0, const double a0,
+    const double p1, const double v1, const double a1,
+    const double param) : p0_(p0), v0_(v0), a0_(a0) {
+  CHECK_GT(param, FLAGS_lattice_epsilon);
+  jerk_ = (a1 - a0) / param;
 }
 
-ConstantJerkTrajectory1d::~ConstantJerkTrajectory1d() {
-  // TODO Auto-generated destructor stub
+double ConstantJerkTrajectory1d::Evaluate(
+    const std::uint32_t order, const double param) const {
+  switch (order) {
+    case 0: {
+      return p0_ + v0_ * param + 0.5 * a0_ * param * param +
+             jerk_ * param * param * param / 6.0;
+    }
+    case 1: {
+      return v0_ + a0_ * param + 0.5 * jerk_ * param * param;
+    }
+    case 2: {
+      return a0_ + jerk_ * param;
+    }
+    case 3: {
+      return jerk_;
+    }
+    default:
+      return 0.0;
+  }
 }
 
-} // namespace planning
-} // namespace apollo
+}  // namespace planning
+}  // namespace apollo
