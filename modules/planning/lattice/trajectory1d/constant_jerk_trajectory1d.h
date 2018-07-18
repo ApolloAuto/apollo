36e73d0f3023c2c69d81aecfb805b2061b3dc409
kechxu <kechxu89@gmail.com>
2018-07-18 14:29:14 -0700

    Planning: implement constant jerk trajectory

--- modules/planning/lattice/trajectory1d/constant_jerk_trajectory1d.h
+++ modules/planning/lattice/trajectory1d/constant_jerk_trajectory1d.h
@@ -21,9 +21,11 @@
 #ifndef MODULES_PLANNING_LATTICE_TRAJECTORY1D_CONSTANT_JERK_TRAJECTORY1D_H_
 #define MODULES_PLANNING_LATTICE_TRAJECTORY1D_CONSTANT_JERK_TRAJECTORY1D_H_
 
+#include <string>
+
 #include "modules/planning/math/curve1d/curve1d.h"
 
-namespace apollo{
+namespace apollo {
 namespace planning {
 
 class ConstantJerkTrajectory1d : public Curve1d {
@@ -31,7 +33,7 @@ class ConstantJerkTrajectory1d : public Curve1d {
   ConstantJerkTrajectory1d(const double p0, const double v0, const double a0,
       const double p1, const double v1, const double a1, const double param);
 
-  virtual ~ConstantJerkTrajectory1d();
+  virtual ~ConstantJerkTrajectory1d() = default;
 
   double Evaluate(const std::uint32_t order,
                   const double param) const;
@@ -39,9 +41,16 @@ class ConstantJerkTrajectory1d : public Curve1d {
   double ParamLength() const;
 
   std::string ToString() const;
+
+ private:
+  double p0_;
+  double v0_;
+  double a0_;
+  double jerk_;
 };
 
-} // namespace planning
-} // namespace apollo
+}  // namespace planning
+}  // namespace apollo
 
-#endif /* MODULES_PLANNING_LATTICE_TRAJECTORY1D_CONSTANT_JERK_TRAJECTORY1D_H_ */
+#endif
+/* MODULES_PLANNING_LATTICE_TRAJECTORY1D_CONSTANT_JERK_TRAJECTORY1D_H_ */
