377fd00846397ee4876af9f484e367a0d71e3212
Kecheng Xu <kechxu89@gmail.com>
2018-07-30 12:39:11 -0700

    Planning: use qp for lateral optimization (#5170)

--- modules/planning/common/planning_gflags.cc
+++ modules/planning/common/planning_gflags.cc
@@ -364,7 +364,7 @@ DEFINE_double(lateral_third_order_derivative_max, 2.0,
               "the maximal allowance for lateral third order derivative");
 DEFINE_double(max_s_lateral_optimization, 50.0,
               "The maximal s for lateral optimization.");
-DEFINE_double(default_delta_s_lateral_optimization, 0.5,
+DEFINE_double(default_delta_s_lateral_optimization, 1.0,
               "The default delta s for lateral optimization.");
 DEFINE_double(bound_buffer, 0.3, "buffer to boundary for lateral optimization");
 
