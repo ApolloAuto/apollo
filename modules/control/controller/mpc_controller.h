e1cdbc3fc8820b1f4d5cd93ed1aa8e932ca3808e
luoqi06 <luoqi06@baidu.com>
2018-07-29 22:39:25 -0700

    Control : fix steer angle scaling issue

--- modules/control/controller/mpc_controller.h
+++ modules/control/controller/mpc_controller.h
@@ -125,6 +125,8 @@ class MPCController : public Controller {
 
   void CloseLogFile();
 
+  double Wheel2SteerPct(const double wheel_angle);
+
   // vehicle parameter
   common::VehicleParam vehicle_param_;
 
@@ -156,9 +158,11 @@ class MPCController : public Controller {
   // rotational inertia
   double iz_ = 0.0;
   // the ratio between the turn of the steering wheel and the turn of the wheels
-  double steer_transmission_ratio_ = 0.0;
+  double steer_ratio_ = 0.0;
   // the maximum turn of steer
   double steer_single_direction_max_degree_ = 0.0;
+  // the maximum turn of vehicle wheel
+  double wheel_single_direction_max_degree_ = 0.0;
 
   // limit steering to maximum theoretical lateral acceleration
   double max_lat_acc_ = 0.0;
