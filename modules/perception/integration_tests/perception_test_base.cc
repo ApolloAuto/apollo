12ddf063725bd9275266fb1fc43d358a40887257
Liangliang Zhang <zhangliangliang@baidu.com>
2018-08-14 12:59:32 -0700

    Perception: added dummy sunnyvale big loop test.

--- modules/perception/integration_tests/perception_test_base.cc
+++ modules/perception/integration_tests/perception_test_base.cc
@@ -19,10 +19,11 @@
 #include <cstdlib>
 
 #include "pcl/io/pcd_io.h"
+#include "pcl_conversions/pcl_conversions.h"
 
 #include "google/protobuf/io/zero_copy_stream_impl.h"
-#include "modules/common/log.h"
 #include "modules/common/vehicle_state/vehicle_state_provider.h"
+#include "modules/perception/common/pcl_types.h"
 
 namespace apollo {
 namespace perception {
@@ -34,22 +35,36 @@ DEFINE_string(test_pointcloud_file, "", "The pointcloud file used in test");
 DEFINE_string(test_localization_file, "", "The localization test file");
 DEFINE_string(test_chassis_file, "", "The chassis test file");
 DEFINE_string(perception_config_file, "", "The perception config file");
-DEFINE_string(perception_adapter_config_filename, "",
-              "The perception adapter config file");
 
 void PerceptionTestBase::SetUpTestCase() {
   FLAGS_perception_config_file =
-      "modules/perception/conf/perception_config.pb.txt";
+      "modules/perception/integration_tests/testdata/conf/"
+      "perception_config.pb.txt";
   FLAGS_perception_adapter_config_filename =
-      "modules/perception/integration_test/testdata/conf/adapter.conf";
+      "modules/perception/integration_tests/testdata/conf/adapter.conf";
+
+  FLAGS_dag_config_path =
+      "./integration_tests/testdata/conf/dag_streaming.config";
 
   FLAGS_test_pointcloud_file =
-      "modules/perception/integration_test/testdata/point_cloud_test_file.pcd";
+      "modules/perception/integration_tests/testdata/point_cloud_test_file.pcd";
+
+  FLAGS_onboard_roi_filter = "HdmapROIFilter";
+  FLAGS_onboard_segmentor = "CNNSegmentation";
+
   FLAGS_test_localization_file = "";
   FLAGS_test_chassis_file = "";
 }
 
-#define FEED_ADAPTER(TYPE, FILENAME)                                           \
+#define FEED_DATA_TO_ADAPTER(TYPE, DATA)                              \
+  if (!AdapterManager::Get##TYPE()) {                                 \
+    AERROR << #TYPE                                                   \
+        " is not registered in adapter manager, check adapter file "; \
+    return false;                                                     \
+  }                                                                   \
+  AdapterManager::Feed##TYPE##Data(DATA);
+
+#define FEED_FILE_TO_ADAPTER(TYPE, FILENAME)                                   \
   if (!AdapterManager::Get##TYPE()) {                                          \
     AERROR << #TYPE                                                            \
         " is not registered in adapter manager, check adapter file "           \
@@ -72,35 +87,25 @@ bool PerceptionTestBase::SetUpAdapters() {
     AdapterManager::Init(FLAGS_perception_adapter_config_filename);
   }
 
-  FEED_ADAPTER(PointCloud, FLAGS_test_pointcloud_file);
-  FEED_ADAPTER(Localization, FLAGS_test_localization_file);
-  FEED_ADAPTER(Chassis, FLAGS_test_chassis_file);
+  FEED_DATA_TO_ADAPTER(PointCloud, cloud_blob_);
+  FEED_FILE_TO_ADAPTER(Localization, FLAGS_test_localization_file);
+  FEED_FILE_TO_ADAPTER(Chassis, FLAGS_test_chassis_file);
   return true;
 }
 
 void PerceptionTestBase::SetUp() {
-  perception_.Stop();
+  SetUpTestCase();
 
   // load PCD file and transfer to point cloud
-  pcl::PointCloud<pcl_util::PointXYZIT>::Ptr org_cloud(
-      new pcl::PointCloud<pcl_util::PointXYZIT>);
-  pcl::io::loadPCDFile(FLAGS_test_pointcloud_file, *org_cloud);
-  point_cloud_->points.reserve(org_cloud->points.size());
+  pcl::PCLPointCloud2 pcl_pointcloud;
+  CHECK_NE(-1, pcl::io::loadPCDFile(FLAGS_test_pointcloud_file, pcl_pointcloud))
+      << FLAGS_test_pointcloud_file;
 
-  for (size_t i = 0; i < org_cloud->points.size(); ++i) {
-    pcl_util::Point pt;
-    pt.x = org_cloud->points[i].x;
-    pt.y = org_cloud->points[i].y;
-    pt.z = org_cloud->points[i].z;
-    pt.intensity = org_cloud->points[i].intensity;
-    if (std::isnan(org_cloud->points[i].x)) {
-      continue;
-    }
-    point_cloud_->push_back(pt);
-  }
+  pcl_conversions::fromPCL(pcl_pointcloud, cloud_blob_);
 
   CHECK(SetUpAdapters()) << "Failed to setup adapters";
-  CHECK(perception_.Init().ok()) << "Failed to init perception module";
+  perception_.reset(new Perception);
+  CHECK(perception_->Init().ok()) << "Failed to init perception module";
 }
 
 void PerceptionTestBase::UpdateData() {
@@ -109,7 +114,12 @@ void PerceptionTestBase::UpdateData() {
 
 bool PerceptionTestBase::RunPerception(const std::string& test_case_name,
                                        int case_num) {
-  // TODO(All): Implement RunPerception here.
+  if (perception_->Start() != common::Status::OK()) {
+    AERROR << "Failed to start perception.";
+    return false;
+  }
+  perception_->Stop();
+  // TODO(All): Finish implementation of RunPerception here.
   return true;
 }
 
---
+++ modules/perception/integration_tests/sunnyvale_big_loop_test.cc
@@ -0,0 +1,58 @@
+/******************************************************************************
+ * Copyright 2017 The Apollo Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *****************************************************************************/
+
+#include <string>
+
+#include "gtest/gtest.h"
+
+#include "modules/common/configs/config_gflags.h"
+#include "modules/common/time/time.h"
+#include "modules/map/hdmap/hdmap_util.h"
+#include "modules/perception/integration_tests/perception_test_base.h"
+
+namespace apollo {
+namespace perception {
+
+using apollo::common::PointENU;
+using apollo::common::time::Clock;
+
+/**
+ * @class SunnyvaleBigLoopTest
+ * @brief This is an integration test that uses the sunnyvale_big_loop map.
+ */
+
+class SunnyvaleBigLoopTest : public PerceptionTestBase {
+ public:
+  virtual void SetUp() {
+    FLAGS_map_dir = "modules/map/data/sunnyvale_big_loop";
+    FLAGS_test_base_map_filename = "base_map.bin";
+    AERROR << "SetUp done.";
+  }
+};
+
+TEST_F(SunnyvaleBigLoopTest, test_01) {
+  std::string seq_num = "1";
+  FLAGS_test_localization_file = seq_num + "_localization.pb.txt";
+  FLAGS_test_chassis_file = seq_num + "_chassis.pb.txt";
+
+  // TODO(All): implement sunnyvale big loop test here.
+  AERROR << "test done.";
+}
+
+}  // namespace perception
+}  // namespace apollo
+
+TMAIN;
