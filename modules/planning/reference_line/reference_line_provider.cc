c17c74bd445c9d5c4560c468b15f9a0ee859342c
Jinyun Zhou <32111357+JasonZhou404@users.noreply.github.com>
2018-08-15 09:52:55 -0700

    cosTheta ReferenceLine Smoother(with constraint) (#5329)
    * cosTheta ReferenceLine Smoother(with constraint)
    
    * cosTheta reference line smoother
    
    * cosTheta reference line smoother
    
    * cosTheta reference line smoother

--- modules/planning/reference_line/reference_line_provider.cc
+++ modules/planning/reference_line/reference_line_provider.cc
@@ -73,6 +73,8 @@ ReferenceLineProvider::ReferenceLineProvider(const hdmap::HDMap *base_map) {
     smoother_.reset(new QpSplineReferenceLineSmoother(smoother_config_));
   } else if (smoother_config_.has_spiral()) {
     smoother_.reset(new SpiralReferenceLineSmoother(smoother_config_));
+  } else if (smoother_config_.has_cos_theta()) {
+    smoother_.reset(new CosThetaReferenceLineSmoother(smoother_config_));
   } else {
     CHECK(false) << "unknown smoother config "
                  << smoother_config_.DebugString();
