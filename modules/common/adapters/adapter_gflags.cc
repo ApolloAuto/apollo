86693f03a20e59a14ad02985ffade805b8037943
Jiaming Tao <taojm99@gmail.com>
2018-08-20 14:57:49 -0700

    driver: add a new folder camera(to deprecate current usb_cam) for bazel build (#5403)
    * driver: add usb_cam for bazel build (use usb_cam_new temporarily until we depracate the current one)
    
    * drivers: added usb_cam topic to adapter manager

--- modules/common/adapters/adapter_gflags.cc
+++ modules/common/adapters/adapter_gflags.cc
@@ -92,6 +92,8 @@ DEFINE_string(image_short_topic, "/apollo/sensor/camera/traffic/image_short",
               "short camera image topic name");
 DEFINE_string(image_long_topic, "/apollo/sensor/camera/traffic/image_long",
               "long camera image topic name");
+DEFINE_string(image_usb_cam_topic, "/apollo/sensor/camera/image_usb_cam",
+              "usb camera image topic name");
 DEFINE_string(gnss_rtk_obs_topic, "/apollo/sensor/gnss/rtk_obs",
               "Gnss rtk observation topic name");
 DEFINE_string(gnss_rtk_eph_topic, "/apollo/sensor/gnss/rtk_eph",
