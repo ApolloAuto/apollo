ffaaf1c58522c163db8a6a0e34441e1848459b43
Vivian Lin <2775888+vlin17@users.noreply.github.com>
2018-08-21 17:03:01 -0700

    Dreamview: added drive event types (#5439)
    * Dreamview: added drive event types
    
    * webpack
    
    * fixup

--- modules/dreamview/frontend/dist/app.bundle.js
+++ modules/dreamview/frontend/dist/app.bundle.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8f7203fba4d15cfd62b1a82ec359843c9d0a748808ea1e813f74e6dc47d1e928
-size 1515370
+oid sha256:cdcb8589c73a8200ca2f4303f5f7b91a2ead1c527f6faef66fd0ec52e8294abf
+size 1616584
