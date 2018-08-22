0feb400d8369627bd6a4e0c6dc58a15d540b2c0a
Liangliang Zhang <zhangliangliang@baidu.com>
2018-08-22 10:49:36 -0700

    Drivers: make vls-128 compilable. Added driver compilation into cibuild.

--- modules/drivers/velodyne_vls/velodyne_driver/src/driver/driver.h
+++ modules/drivers/velodyne_vls/velodyne_driver/src/driver/driver.h
@@ -1,7 +1,7 @@
 /* -*- mode: C++ -*- */
 /*
  *  Copyright (C) 2012 Austin Robot Technology, Jack O'Quin
- * 
+ *
  *  License: Modified BSD Software License Agreement
  *
  *  $Id$
@@ -16,57 +16,54 @@
 #define _VELODYNE_DRIVER_H_ 1
 
 #include <string>
-#include <ros/ros.h>
-#include <diagnostic_updater/diagnostic_updater.h>
-#include <diagnostic_updater/publisher.h>
+
+//#include <diagnostic_updater/diagnostic_updater.h>
+//#include <diagnostic_updater/publisher.h>
 #include <dynamic_reconfigure/server.h>
+#include <ros/ros.h>
 
-#include <velodyne_driver/input.h>
 #include <velodyne_driver/VelodyneNodeConfig.h>
+#include <velodyne_driver/input.h>
 
-namespace velodyne_driver
-{
+namespace velodyne_driver {
 
-class VelodyneDriver
-{
-public:
-
-  VelodyneDriver(ros::NodeHandle node,
-                 ros::NodeHandle private_nh);
+class VelodyneDriver {
+ public:
+  VelodyneDriver(ros::NodeHandle node, ros::NodeHandle private_nh);
   ~VelodyneDriver() {}
 
   bool poll(void);
 
-private:
+ private:
+  /// Callback for dynamic reconfigure
+  void callback(velodyne_driver::VelodyneNodeConfig &config, uint32_t level);
 
-  ///Callback for dynamic reconfigure
-  void callback(velodyne_driver::VelodyneNodeConfig &config,
-              uint32_t level);
-
-  ///Pointer to dynamic reconfigure service srv_
-  boost::shared_ptr<dynamic_reconfigure::Server<velodyne_driver::
-              VelodyneNodeConfig> > srv_;
+  /// Pointer to dynamic reconfigure service srv_
+  boost::shared_ptr<
+      dynamic_reconfigure::Server<velodyne_driver::VelodyneNodeConfig> >
+      srv_;
 
   // configuration parameters
-  struct
-  {
-    std::string frame_id;            ///< tf frame ID
-    std::string model;               ///< device model name
-    int    npackets;                 ///< number of packets to collect
-    double rpm;                      ///< device rotation rate (RPMs)
-    double time_offset;              ///< time in seconds added to each velodyne time stamp
+  struct {
+    std::string frame_id;  ///< tf frame ID
+    std::string model;     ///< device model name
+    int npackets;          ///< number of packets to collect
+    double rpm;            ///< device rotation rate (RPMs)
+    double time_offset;  ///< time in seconds added to each velodyne time stamp
   } config_;
 
   boost::shared_ptr<Input> input_;
   ros::Publisher output_;
 
   /** diagnostics updater */
+  /*
   diagnostic_updater::Updater diagnostics_;
   double diag_min_freq_;
   double diag_max_freq_;
   boost::shared_ptr<diagnostic_updater::TopicDiagnostic> diag_topic_;
+  */
 };
 
-} // namespace velodyne_driver
+}  // namespace velodyne_driver
 
-#endif // _VELODYNE_DRIVER_H_
+#endif  // _VELODYNE_DRIVER_H_
