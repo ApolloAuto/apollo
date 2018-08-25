6261cde7beb325bc55c14133db078950397d82a2
Liangliang Zhang <zhangliangliang@baidu.com>
2018-08-25 11:47:42 -0700

    Prediction: added scripts to convert binary to tfrecord format

--- modules/tools/prediction/multiple_gpu_estimator/mlp_main.py
+++ modules/tools/prediction/multiple_gpu_estimator/mlp_main.py
@@ -101,20 +101,21 @@ def get_model_fn(num_gpus, variable_strategy, num_workers):
             worker_device = '/{}:{}'.format(device_type, i)
             if variable_strategy == 'CPU':
                 device_setter = mlp_utils.local_device_setter(
-                        worker_device=worker_device)
+                    worker_device=worker_device)
             elif variable_strategy == 'GPU':
                 device_setter = mlp_utils.local_device_setter(
-                        ps_device_type='gpu',
-                        worker_device=worker_device,
-                        ps_strategy=tf.contrib.training.GreedyLoadBalancingStrategy(
-                                num_gpus, tf.contrib.training.byte_size_load_fn))
+                    ps_device_type='gpu',
+                    worker_device=worker_device,
+                    ps_strategy=tf.contrib.training.
+                    GreedyLoadBalancingStrategy(
+                        num_gpus, tf.contrib.training.byte_size_load_fn))
             with tf.variable_scope('mlp', reuse=bool(i != 0)):
                 with tf.name_scope('tower_%d' % i) as name_scope:
                     with tf.device(device_setter):
                         loss, gradvars, preds = _tower_fn(
-                                is_training, weight_decay, tower_features[i], tower_labels[i],
-                                data_format, params.batch_norm_decay,
-                                params.batch_norm_epsilon)
+                            is_training, weight_decay, tower_features[i],
+                            tower_labels[i], data_format,
+                            params.batch_norm_decay, params.batch_norm_epsilon)
                         tower_losses.append(loss)
                         tower_gradvars.append(gradvars)
                         tower_preds.append(preds)
@@ -124,8 +125,8 @@ def get_model_fn(num_gpus, variable_strategy, num_workers):
                             # towers but these stats accumulate extremely fast so we can
                             # ignore the other stats from the other towers without
                             # significant detriment.
-                            update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS,
-                                                                                         name_scope)
+                            update_ops = tf.get_collection(
+                                tf.GraphKeys.UPDATE_OPS, name_scope)
 
         # Now compute global loss and gradients.
         gradvars = []
@@ -141,77 +142,81 @@ def get_model_fn(num_gpus, variable_strategy, num_workers):
                     if len(grads) == 1:
                         avg_grad = grads[0]
                     else:
-                        avg_grad = tf.multiply(tf.add_n(grads), 1. / len(grads))
+                        avg_grad = tf.multiply(
+                            tf.add_n(grads), 1. / len(grads))
                 gradvars.append((avg_grad, var))
 
         # Device that runs the ops to apply global gradient updates.
         consolidation_device = '/gpu:0' if variable_strategy == 'GPU' else '/cpu:0'
         with tf.device(consolidation_device):
             num_batches_per_epoch = mlp_data.MlpDataSet.num_examples_per_epoch(
-                    'train') // (params.train_batch_size * num_workers)
+                'train') // (params.train_batch_size * num_workers)
             boundaries = [
-                    num_batches_per_epoch * x
-                    for x in np.array([82, 123, 300], dtype=np.int64)
+                num_batches_per_epoch * x
+                for x in np.array([82, 123, 300], dtype=np.int64)
             ]
-            staged_lr = [params.learning_rate * x for x in [1, 0.1, 0.01, 0.002]]
+            staged_lr = [
+                params.learning_rate * x for x in [1, 0.1, 0.01, 0.002]
+            ]
 
-            learning_rate = tf.train.piecewise_constant(tf.train.get_global_step(),
-                                                                                                    boundaries, staged_lr)
+            learning_rate = tf.train.piecewise_constant(
+                tf.train.get_global_step(), boundaries, staged_lr)
 
             loss = tf.reduce_mean(tower_losses, name='loss')
 
             examples_sec_hook = mlp_utils.ExamplesPerSecondHook(
-                    params.train_batch_size, every_n_steps=10)
+                params.train_batch_size, every_n_steps=10)
 
             tensors_to_log = {'learning_rate': learning_rate, 'loss': loss}
 
             logging_hook = tf.train.LoggingTensorHook(
-                    tensors=tensors_to_log, every_n_iter=100)
+                tensors=tensors_to_log, every_n_iter=100)
 
             train_hooks = [logging_hook, examples_sec_hook]
 
             optimizer = tf.train.MomentumOptimizer(
-                    learning_rate=learning_rate, momentum=momentum)
+                learning_rate=learning_rate, momentum=momentum)
 
             if params.sync:
                 optimizer = tf.train.SyncReplicasOptimizer(
-                        optimizer, replicas_to_aggregate=num_workers)
-                sync_replicas_hook = optimizer.make_session_run_hook(params.is_chief)
+                    optimizer, replicas_to_aggregate=num_workers)
+                sync_replicas_hook = optimizer.make_session_run_hook(
+                    params.is_chief)
                 train_hooks.append(sync_replicas_hook)
 
             # Create single grouped train op
             train_op = [
-                    optimizer.apply_gradients(
-                            gradvars, global_step=tf.train.get_global_step())
+                optimizer.apply_gradients(
+                    gradvars, global_step=tf.train.get_global_step())
             ]
             train_op.extend(update_ops)
             train_op = tf.group(*train_op)
 
             predictions = {
-                    'classes':
-                            tf.concat([p['classes'] for p in tower_preds], axis=0),
-                    'probabilities':
-                            tf.concat([p['probabilities'] for p in tower_preds], axis=0)
+                'classes':
+                tf.concat([p['classes'] for p in tower_preds], axis=0),
+                'probabilities':
+                tf.concat([p['probabilities'] for p in tower_preds], axis=0)
             }
             stacked_labels = tf.concat(labels, axis=0)
             metrics = {
-                    'accuracy':
-                            tf.metrics.accuracy(stacked_labels, predictions['classes'])
+                'accuracy':
+                tf.metrics.accuracy(stacked_labels, predictions['classes'])
             }
 
         return tf.estimator.EstimatorSpec(
-                mode=mode,
-                predictions=predictions,
-                loss=loss,
-                train_op=train_op,
-                training_hooks=train_hooks,
-                eval_metric_ops=metrics)
+            mode=mode,
+            predictions=predictions,
+            loss=loss,
+            train_op=train_op,
+            training_hooks=train_hooks,
+            eval_metric_ops=metrics)
 
     return _mlp_model_fn
 
 
 def _tower_fn(is_training, weight_decay, feature, label, data_format,
-                            batch_norm_decay, batch_norm_epsilon):
+              batch_norm_decay, batch_norm_epsilon):
     """Build computation tower.
 
     Args:
@@ -229,33 +234,30 @@ def _tower_fn(is_training, weight_decay, feature, label, data_format,
 
     """
     model = mlp_model.MlpModel(
-            batch_norm_decay=batch_norm_decay,
-            batch_norm_epsilon=batch_norm_epsilon,
-            is_training=is_training,
-            data_format=data_format)
+        batch_norm_decay=batch_norm_decay,
+        batch_norm_epsilon=batch_norm_epsilon,
+        is_training=is_training,
+        data_format=data_format)
     logits = model.forward_pass(feature, input_data_format='channels_last')
     tower_pred = {
-            'classes': tf.argmax(input=logits, axis=1),
-            'probabilities': tf.nn.softmax(logits)
+        'classes': tf.argmax(input=logits, axis=1),
+        'probabilities': tf.nn.softmax(logits)
     }
 
     tower_loss = tf.losses.sparse_softmax_cross_entropy(
-            logits=logits, labels=label)
+        logits=logits, labels=label)
     tower_loss = tf.reduce_mean(tower_loss)
 
     model_params = tf.trainable_variables()
     tower_loss += weight_decay * tf.add_n(
-            [tf.nn.l2_loss(v) for v in model_params])
+        [tf.nn.l2_loss(v) for v in model_params])
 
     tower_grad = tf.gradients(tower_loss, model_params)
 
     return tower_loss, zip(tower_grad, model_params), tower_pred
 
 
-def input_fn(data_dir,
-             subset,
-             num_shards,
-             batch_size):
+def input_fn(data_dir, subset, num_shards, batch_size):
     """Create input graph for model.
 
     Args:
@@ -291,9 +293,11 @@ def input_fn(data_dir,
         return feature_shards, label_shards
 
 
-def get_experiment_fn(data_dir,
-                      num_gpus,
-                      variable_strategy,):
+def get_experiment_fn(
+        data_dir,
+        num_gpus,
+        variable_strategy,
+):
     """Returns an Experiment function.
 
     Experiments perform training on several workers in parallel,
@@ -320,180 +324,181 @@ def get_experiment_fn(data_dir,
         """Returns an Experiment."""
         # Create estimator.
         train_input_fn = functools.partial(
-                input_fn,
-                data_dir,
-                subset='train',
-                num_shards=num_gpus,
-                batch_size=hparams.train_batch_size)
+            input_fn,
+            data_dir,
+            subset='train',
+            num_shards=num_gpus,
+            batch_size=hparams.train_batch_size)
 
         eval_input_fn = functools.partial(
-                input_fn,
-                data_dir,
-                subset='eval',
-                batch_size=hparams.eval_batch_size,
-                num_shards=num_gpus)
+            input_fn,
+            data_dir,
+            subset='eval',
+            batch_size=hparams.eval_batch_size,
+            num_shards=num_gpus)
 
         num_eval_examples = mlp_data.MlpDataSet.num_examples_per_epoch('eval')
         if num_eval_examples % hparams.eval_batch_size != 0:
             raise ValueError(
-                    'validation set size must be multiple of eval_batch_size')
+                'validation set size must be multiple of eval_batch_size')
 
         train_steps = hparams.train_steps
         eval_steps = num_eval_examples // hparams.eval_batch_size
- 
+
         classifier = tf.estimator.Estimator(
-                model_fn=get_model_fn(num_gpus, variable_strategy, run_config.num_worker_replicas or 1),
-                config=run_config,
-                params=hparams)
+            model_fn=get_model_fn(num_gpus, variable_strategy,
+                                  run_config.num_worker_replicas or 1),
+            config=run_config,
+            params=hparams)
 
         # Create experiment.
         return tf.contrib.learn.Experiment(
-                classifier,
-                train_input_fn=train_input_fn,
-                eval_input_fn=eval_input_fn,
-                train_steps=train_steps,
-                eval_steps=eval_steps)
+            classifier,
+            train_input_fn=train_input_fn,
+            eval_input_fn=eval_input_fn,
+            train_steps=train_steps,
+            eval_steps=eval_steps)
 
     return _experiment_fn
 
 
-def main(job_dir, data_dir, num_gpus, variable_strategy,
-        log_device_placement, num_intra_threads, **hparams):
+def main(job_dir, data_dir, num_gpus, variable_strategy, log_device_placement,
+         num_intra_threads, **hparams):
     # The env variable is on deprecation path, default is set to off.
     os.environ['TF_SYNC_ON_FINISH'] = '0'
     os.environ['TF_ENABLE_WINOGRAD_NONFUSED'] = '1'
 
     # Session configuration.
     sess_config = tf.ConfigProto(
-            allow_soft_placement=True,
-            log_device_placement=log_device_placement,
-            intra_op_parallelism_threads=num_intra_threads,
-            gpu_options=tf.GPUOptions(force_gpu_compatible=True))
+        allow_soft_placement=True,
+        log_device_placement=log_device_placement,
+        intra_op_parallelism_threads=num_intra_threads,
+        gpu_options=tf.GPUOptions(force_gpu_compatible=True))
 
-    config = mlp_utils.RunConfig(
-            session_config=sess_config, model_dir=job_dir)
+    config = mlp_utils.RunConfig(session_config=sess_config, model_dir=job_dir)
     tf.contrib.learn.learn_runner.run(
-            get_experiment_fn(data_dir, num_gpus, variable_strategy),
-            run_config=config,
-            hparams=tf.contrib.training.HParams(
-                    is_chief=config.is_chief,
-                    **hparams))
+        get_experiment_fn(data_dir, num_gpus, variable_strategy),
+        run_config=config,
+        hparams=tf.contrib.training.HParams(
+            is_chief=config.is_chief, **hparams))
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument(
-            '--data-dir',
-            type=str,
-            required=True,
-            help='The directory where the CIFAR-10 input data is stored.')
+        '--data-dir',
+        type=str,
+        required=True,
+        help='The directory where the CIFAR-10 input data is stored.')
     parser.add_argument(
-            '--job-dir',
-            type=str,
-            required=True,
-            help='The directory where the model will be stored.')
+        '--job-dir',
+        type=str,
+        required=True,
+        help='The directory where the model will be stored.')
     parser.add_argument(
-            '--variable-strategy',
-            choices=['CPU', 'GPU'],
-            type=str,
-            default='CPU',
-            help='Where to locate variable operations')
+        '--variable-strategy',
+        choices=['CPU', 'GPU'],
+        type=str,
+        default='CPU',
+        help='Where to locate variable operations')
     parser.add_argument(
-            '--num-gpus',
-            type=int,
-            default=1,
-            help='The number of gpus used. Uses only CPU if set to 0.')
+        '--num-gpus',
+        type=int,
+        default=1,
+        help='The number of gpus used. Uses only CPU if set to 0.')
     parser.add_argument(
-            '--train-steps',
-            type=int,
-            default=80000,
-            help='The number of steps to use for training.')
+        '--train-steps',
+        type=int,
+        default=80000,
+        help='The number of steps to use for training.')
     parser.add_argument(
-            '--train-batch-size',
-            type=int,
-            default=128,
-            help='Batch size for training.')
+        '--train-batch-size',
+        type=int,
+        default=128,
+        help='Batch size for training.')
     parser.add_argument(
-            '--eval-batch-size',
-            type=int,
-            default=100,
-            help='Batch size for validation.')
+        '--eval-batch-size',
+        type=int,
+        default=100,
+        help='Batch size for validation.')
     parser.add_argument(
-            '--momentum',
-            type=float,
-            default=0.9,
-            help='Momentum for MomentumOptimizer.')
+        '--momentum',
+        type=float,
+        default=0.9,
+        help='Momentum for MomentumOptimizer.')
     parser.add_argument(
-            '--weight-decay',
-            type=float,
-            default=2e-4,
-            help='Weight decay for convolutions.')
+        '--weight-decay',
+        type=float,
+        default=2e-4,
+        help='Weight decay for convolutions.')
     parser.add_argument(
-            '--learning-rate',
-            type=float,
-            default=0.1,
-            help="""\
+        '--learning-rate',
+        type=float,
+        default=0.1,
+        help="""\
             This is the inital learning rate value. The learning rate will decrease
             during training. For more details check the model_fn implementation in
             this file.\
             """)
     parser.add_argument(
-            '--sync',
-            action='store_true',
-            default=False,
-            help="""\
+        '--sync',
+        action='store_true',
+        default=False,
+        help="""\
             If present when running in a distributed environment will run on sync mode.\
             """)
     parser.add_argument(
-            '--num-intra-threads',
-            type=int,
-            default=0,
-            help="""\
+        '--num-intra-threads',
+        type=int,
+        default=0,
+        help="""\
             Number of threads to use for intra-op parallelism. When training on CPU
             set to 0 to have the system pick the appropriate number or alternatively
             set it to the number of physical CPU cores.\
             """)
     parser.add_argument(
-            '--num-inter-threads',
-            type=int,
-            default=0,
-            help="""\
+        '--num-inter-threads',
+        type=int,
+        default=0,
+        help="""\
             Number of threads to use for inter-op parallelism. If set to 0, the
             system will pick an appropriate number.\
             """)
     parser.add_argument(
-            '--data-format',
-            type=str,
-            default=None,
-            help="""\
+        '--data-format',
+        type=str,
+        default=None,
+        help="""\
             If not set, the data format best for the training device is used. 
             Allowed values: channels_first (NCHW) channels_last (NHWC).\
             """)
     parser.add_argument(
-            '--log-device-placement',
-            action='store_true',
-            default=False,
-            help='Whether to log device placement.')
+        '--log-device-placement',
+        action='store_true',
+        default=False,
+        help='Whether to log device placement.')
     parser.add_argument(
-            '--batch-norm-decay',
-            type=float,
-            default=0.997,
-            help='Decay for batch norm.')
+        '--batch-norm-decay',
+        type=float,
+        default=0.997,
+        help='Decay for batch norm.')
     parser.add_argument(
-            '--batch-norm-epsilon',
-            type=float,
-            default=1e-5,
-            help='Epsilon for batch norm.')
+        '--batch-norm-epsilon',
+        type=float,
+        default=1e-5,
+        help='Epsilon for batch norm.')
     args = parser.parse_args()
 
     if args.num_gpus > 0:
         assert tf.test.is_gpu_available(), "Requested GPUs but none found."
     if args.num_gpus < 0:
         raise ValueError(
-                'Invalid GPU count: \"--num-gpus\" must be 0 or a positive integer.')
+            'Invalid GPU count: \"--num-gpus\" must be 0 or a positive integer.'
+        )
     if args.num_gpus == 0 and args.variable_strategy == 'GPU':
-        raise ValueError('num-gpus=0, CPU must be used as parameter server. Set'
-                                         '--variable-strategy=CPU.')
+        raise ValueError(
+            'num-gpus=0, CPU must be used as parameter server. Set'
+            '--variable-strategy=CPU.')
     if args.num_gpus != 0 and args.train_batch_size % args.num_gpus != 0:
         raise ValueError('--train-batch-size must be multiple of --num-gpus.')
     if args.num_gpus != 0 and args.eval_batch_size % args.num_gpus != 0:
