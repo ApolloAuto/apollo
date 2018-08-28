699838d4f7a2b01fde10254d333121c2cf5ad721
Yajia Zhang <yajia.zhang@gmail.com>
2018-08-28 10:51:07 -0700

    prediction: minor fix

--- modules/prediction/container/adc_trajectory/adc_trajectory_container.cc
+++ modules/prediction/container/adc_trajectory/adc_trajectory_container.cc
@@ -38,7 +38,7 @@ void ADCTrajectoryContainer::Insert(
     const ::google::protobuf::Message& message) {
   adc_lane_ids_.clear();
   adc_lane_seq_.clear();
-  adc_junction_polygon_ = Polygon2d{};
+  adc_junction_polygon_ = std::move(Polygon2d());
 
   std::lock_guard<std::mutex> lock(adc_trajectory_mutex_);
   adc_trajectory_.CopyFrom(dynamic_cast<const ADCTrajectory&>(message));
