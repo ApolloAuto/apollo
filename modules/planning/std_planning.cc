ac26b9ee1c1afca1329fdbbc034d432cab721954
luoqi06 <luoqi06@baidu.com>
2018-08-29 15:42:11 -0700

    Planning : Change planning base interface to de-couple from reference line and frame

--- modules/planning/std_planning.cc
+++ modules/planning/std_planning.cc
@@ -116,6 +116,20 @@ Status StdPlanning::Init() {
   return planner_->Init(config_);
 }
 
+Status StdPlanning::InitFrame(const uint32_t sequence_num,
+                              const TrajectoryPoint& planning_start_point,
+                              const double start_time,
+                              const VehicleState& vehicle_state) {
+  frame_.reset(new Frame(sequence_num, planning_start_point, start_time,
+                         vehicle_state, reference_line_provider_.get()));
+  auto status = frame_->Init();
+  if (!status.ok()) {
+    AERROR << "failed to init frame:" << status.ToString();
+    return status;
+  }
+  return Status::OK();
+}
+
 void StdPlanning::OnTimer(const ros::TimerEvent&) {
   RunOnce();
 
@@ -316,6 +330,116 @@ void StdPlanning::RunOnce() {
 
   auto seq_num = frame_->SequenceNum();
   FrameHistory::instance()->Add(seq_num, std::move(frame_));
+}
+
+void StdPlanning::ExportReferenceLineDebug(planning_internal::Debug* debug) {
+  if (!FLAGS_enable_record_debug) {
+    return;
+  }
+  for (auto& reference_line_info : frame_->reference_line_info()) {
+    auto rl_debug = debug->mutable_planning_data()->add_reference_line();
+    rl_debug->set_id(reference_line_info.Lanes().Id());
+    rl_debug->set_length(reference_line_info.reference_line().Length());
+    rl_debug->set_cost(reference_line_info.Cost());
+    rl_debug->set_is_change_lane_path(reference_line_info.IsChangeLanePath());
+    rl_debug->set_is_drivable(reference_line_info.IsDrivable());
+    rl_debug->set_is_protected(reference_line_info.GetRightOfWayStatus() ==
+                               ADCTrajectory::PROTECTED);
+  }
+}
+
+Status StdPlanning::Plan(
+    const double current_time_stamp,
+    const std::vector<TrajectoryPoint>& stitching_trajectory,
+    ADCTrajectory* trajectory_pb) {
+  auto* ptr_debug = trajectory_pb->mutable_debug();
+  if (FLAGS_enable_record_debug) {
+    ptr_debug->mutable_planning_data()->mutable_init_point()->CopyFrom(
+        stitching_trajectory.back());
+  }
+
+  auto status = planner_->Plan(stitching_trajectory.back(), frame_.get());
+
+  ExportReferenceLineDebug(ptr_debug);
+
+  const auto* best_ref_info = frame_->FindDriveReferenceLineInfo();
+  if (!best_ref_info) {
+    std::string msg("planner failed to make a driving plan");
+    AERROR << msg;
+    if (last_publishable_trajectory_) {
+      last_publishable_trajectory_->Clear();
+    }
+    return Status(ErrorCode::PLANNING_ERROR, msg);
+  }
+  ptr_debug->MergeFrom(best_ref_info->debug());
+  trajectory_pb->mutable_latency_stats()->MergeFrom(
+      best_ref_info->latency_stats());
+  // set right of way status
+  trajectory_pb->set_right_of_way_status(best_ref_info->GetRightOfWayStatus());
+  for (const auto& id : best_ref_info->TargetLaneId()) {
+    trajectory_pb->add_lane_id()->CopyFrom(id);
+  }
+
+  best_ref_info->ExportDecision(trajectory_pb->mutable_decision());
+
+  // Add debug information.
+  if (FLAGS_enable_record_debug) {
+    auto* reference_line = ptr_debug->mutable_planning_data()->add_path();
+    reference_line->set_name("planning_reference_line");
+    const auto& reference_points =
+        best_ref_info->reference_line().reference_points();
+    double s = 0.0;
+    double prev_x = 0.0;
+    double prev_y = 0.0;
+    bool empty_path = true;
+    for (const auto& reference_point : reference_points) {
+      auto* path_point = reference_line->add_path_point();
+      path_point->set_x(reference_point.x());
+      path_point->set_y(reference_point.y());
+      path_point->set_theta(reference_point.heading());
+      path_point->set_kappa(reference_point.kappa());
+      path_point->set_dkappa(reference_point.dkappa());
+      if (empty_path) {
+        path_point->set_s(0.0);
+        empty_path = false;
+      } else {
+        double dx = reference_point.x() - prev_x;
+        double dy = reference_point.y() - prev_y;
+        s += std::hypot(dx, dy);
+        path_point->set_s(s);
+      }
+      prev_x = reference_point.x();
+      prev_y = reference_point.y();
+    }
+  }
+
+  last_publishable_trajectory_.reset(new PublishableTrajectory(
+      current_time_stamp, best_ref_info->trajectory()));
+
+  ADEBUG << "current_time_stamp: " << std::to_string(current_time_stamp);
+
+  // Navi Panner doesn't need to stitch the last path planning
+  // trajectory.Otherwise, it will cause the Dremview planning track to display
+  // flashing or bouncing
+  if (FLAGS_enable_stitch_last_trajectory) {
+    last_publishable_trajectory_->PrependTrajectoryPoints(
+        stitching_trajectory.begin(), stitching_trajectory.end() - 1);
+  }
+
+  for (size_t i = 0; i < last_publishable_trajectory_->NumOfPoints(); ++i) {
+    if (last_publishable_trajectory_->TrajectoryPointAt(i).relative_time() >
+        FLAGS_trajectory_time_high_density_period) {
+      break;
+    }
+    ADEBUG << last_publishable_trajectory_->TrajectoryPointAt(i)
+                  .ShortDebugString();
+  }
+
+  last_publishable_trajectory_->PopulateTrajectoryProtobuf(trajectory_pb);
+
+  best_ref_info->ExportEngageAdvice(trajectory_pb->mutable_engage_advice());
+
+  return status;
 }
 
 void StdPlanning::Stop() {
