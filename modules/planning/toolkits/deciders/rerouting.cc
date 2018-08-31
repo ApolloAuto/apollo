fd4a6b8a8f264e2272d94e792857e36b723ab377
jmtao <taojm99@gmail.com>
2018-08-31 13:24:01 -0700

    planning: added deciders and optimizers for sceanrio based implementation

---
+++ modules/planning/toolkits/deciders/rerouting.cc
@@ -0,0 +1,122 @@
+/******************************************************************************
+ * Copyright 2017 The Apollo Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *****************************************************************************/
+
+/**
+ * @file
+ **/
+
+#include "modules/planning/toolkits/deciders/rerouting.h"
+
+#include "modules/common/proto/pnc_point.pb.h"
+
+#include "modules/common/adapters/adapter_manager.h"
+#include "modules/common/time/time.h"
+#include "modules/common/vehicle_state/vehicle_state_provider.h"
+#include "modules/planning/common/planning_context.h"
+
+namespace apollo {
+namespace planning {
+
+using apollo::common::Status;
+using apollo::common::adapter::AdapterManager;
+using apollo::common::time::Clock;
+using apollo::perception::TrafficLight;
+using apollo::perception::TrafficLightDetection;
+
+Rerouting::Rerouting(const TrafficRuleConfig& config) : TrafficRule(config) {}
+
+bool Rerouting::ChangeLaneFailRerouting() {
+  const auto& segments = reference_line_info_->Lanes();
+  // 1. If current reference line is drive forward, no rerouting.
+  if (segments.NextAction() == routing::FORWARD) {
+    // if not current lane, do not check for rerouting
+    return true;
+  }
+
+  // 2. If vehicle is not on current reference line yet, no rerouting
+  if (!segments.IsOnSegment()) {
+    return true;
+  }
+
+  // 3. If current reference line can connect to next passage, no rerouting
+  if (segments.CanExit()) {
+    return true;
+  }
+
+  // 4. If the end of current passage region not appeared, no rerouting
+  const auto& route_end_waypoint = segments.RouteEndWaypoint();
+  if (!route_end_waypoint.lane) {
+    return true;
+  }
+  auto point = route_end_waypoint.lane->GetSmoothPoint(route_end_waypoint.s);
+  const auto& reference_line = reference_line_info_->reference_line();
+  common::SLPoint sl_point;
+  if (!reference_line.XYToSL({point.x(), point.y()}, &sl_point)) {
+    AERROR << "Failed to project point: " << point.ShortDebugString();
+    return false;
+  }
+  if (!reference_line.IsOnLane(sl_point)) {
+    return true;
+  }
+  // 5. If the end of current passage region is further than kPrepareRoutingTime
+  // * speed, no rerouting
+  double adc_s = reference_line_info_->AdcSlBoundary().end_s();
+  const auto* vehicle_state = common::VehicleStateProvider::instance();
+  double speed = vehicle_state->linear_velocity();
+  const double prepare_rerouting_time =
+      config_.rerouting().prepare_rerouting_time();
+  const double prepare_distance = speed * prepare_rerouting_time;
+  if (sl_point.s() > adc_s + prepare_distance) {
+    ADEBUG << "No need rerouting now because still can drive for time: "
+           << prepare_rerouting_time << " seconds";
+    return true;
+  }
+  // 6. Check if we have done rerouting before
+  auto* rerouting = GetPlanningStatus()->mutable_rerouting();
+  if (rerouting == nullptr) {
+    AERROR << "rerouting is nullptr.";
+    return false;
+  }
+  double current_time = Clock::NowInSeconds();
+  if (rerouting->has_last_rerouting_time() &&
+      (current_time - rerouting->last_rerouting_time() <
+       config_.rerouting().cooldown_time())) {
+    ADEBUG << "Skip rerouting and wait for previous rerouting result";
+    return true;
+  }
+  if (!frame_->Rerouting()) {
+    AERROR << "Failed to send rerouting request";
+    return false;
+  }
+
+  // store last rerouting time.
+  rerouting->set_last_rerouting_time(current_time);
+  return true;
+}
+
+Status Rerouting::ApplyRule(Frame* const frame,
+                            ReferenceLineInfo* const reference_line_info) {
+  frame_ = frame;
+  reference_line_info_ = reference_line_info;
+  if (!ChangeLaneFailRerouting()) {
+    return Status(common::PLANNING_ERROR,
+                  "In un-successful lane change case, rerouting failed");
+  }
+  return Status::OK();
+}
+
+}  // namespace planning
+}  // namespace apollo
---
+++ modules/planning/toolkits/optimizers/dp_poly_path/dp_road_graph.h
@@ -0,0 +1,135 @@
+/******************************************************************************
+ * Copyright 2017 The Apollo Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *****************************************************************************/
+
+/**
+ * @file dp_road_graph.h
+ **/
+
+#ifndef MODULES_PLANNING_TOOLKITS_OPTIMIZERS_DP_POLY_PATH_DP_ROAD_GRAPH_H_
+#define MODULES_PLANNING_TOOLKITS_OPTIMIZERS_DP_POLY_PATH_DP_ROAD_GRAPH_H_
+
+#include <limits>
+#include <list>
+#include <vector>
+
+#include "modules/common/proto/pnc_point.pb.h"
+#include "modules/planning/proto/dp_poly_path_config.pb.h"
+
+#include "modules/common/status/status.h"
+#include "modules/planning/common/path/path_data.h"
+#include "modules/planning/common/path_decision.h"
+#include "modules/planning/common/path_obstacle.h"
+#include "modules/planning/common/reference_line_info.h"
+#include "modules/planning/common/speed/speed_data.h"
+#include "modules/planning/common/trajectory/discretized_trajectory.h"
+#include "modules/planning/math/curve1d/quintic_polynomial_curve1d.h"
+#include "modules/planning/reference_line/reference_point.h"
+#include "modules/planning/toolkits/optimizers/dp_poly_path/trajectory_cost.h"
+
+namespace apollo {
+namespace planning {
+
+class DPRoadGraph {
+ public:
+  explicit DPRoadGraph(const DpPolyPathConfig &config,
+                       const ReferenceLineInfo &reference_line_info,
+                       const SpeedData &speed_data);
+
+  ~DPRoadGraph() = default;
+
+  bool FindPathTunnel(const common::TrajectoryPoint &init_point,
+                      const std::vector<const PathObstacle *> &obstacles,
+                      PathData *const path_data);
+
+  void SetDebugLogger(apollo::planning_internal::Debug *debug) {
+    planning_debug_ = debug;
+  }
+
+ private:
+  /**
+   * an private inner struct for the dp algorithm
+   */
+  struct DPRoadGraphNode {
+   public:
+    DPRoadGraphNode() = default;
+
+    DPRoadGraphNode(const common::SLPoint point_sl,
+                    const DPRoadGraphNode *node_prev)
+        : sl_point(point_sl), min_cost_prev_node(node_prev) {}
+
+    DPRoadGraphNode(const common::SLPoint point_sl,
+                    const DPRoadGraphNode *node_prev,
+                    const ComparableCost &cost)
+        : sl_point(point_sl), min_cost_prev_node(node_prev), min_cost(cost) {}
+
+    void UpdateCost(const DPRoadGraphNode *node_prev,
+                    const QuinticPolynomialCurve1d &curve,
+                    const ComparableCost &cost) {
+      if (cost <= min_cost) {
+        min_cost = cost;
+        min_cost_prev_node = node_prev;
+        min_cost_curve = curve;
+      }
+    }
+
+    common::SLPoint sl_point;
+    const DPRoadGraphNode *min_cost_prev_node = nullptr;
+    ComparableCost min_cost = {true, true, true,
+                               std::numeric_limits<float>::infinity(),
+                               std::numeric_limits<float>::infinity()};
+    QuinticPolynomialCurve1d min_cost_curve;
+  };
+
+  bool GenerateMinCostPath(const std::vector<const PathObstacle *> &obstacles,
+                           std::vector<DPRoadGraphNode> *min_cost_path);
+
+  bool SamplePathWaypoints(
+      const common::TrajectoryPoint &init_point,
+      std::vector<std::vector<common::SLPoint>> *const points);
+
+  bool CalculateFrenetPoint(const common::TrajectoryPoint &traj_point,
+                            common::FrenetFramePoint *const frenet_frame_point);
+
+  bool IsValidCurve(const QuinticPolynomialCurve1d &curve) const;
+
+  void GetCurveCost(TrajectoryCost trajectory_cost,
+                    const QuinticPolynomialCurve1d &curve, const float start_s,
+                    const float end_s, const uint32_t curr_level,
+                    const uint32_t total_level, ComparableCost *cost);
+
+  void UpdateNode(const std::list<DPRoadGraphNode> &prev_nodes,
+                  const uint32_t level, const uint32_t total_level,
+                  TrajectoryCost *trajectory_cost, DPRoadGraphNode *front,
+                  DPRoadGraphNode *cur_node);
+  bool HasSidepass();
+
+ private:
+  DpPolyPathConfig config_;
+  common::TrajectoryPoint init_point_;
+  const ReferenceLineInfo &reference_line_info_;
+  const ReferenceLine &reference_line_;
+  SpeedData speed_data_;
+  common::SLPoint init_sl_point_;
+  common::FrenetFramePoint init_frenet_frame_point_;
+  apollo::planning_internal::Debug *planning_debug_ = nullptr;
+
+  ObjectSidePass sidepass_;
+};
+
+}  // namespace planning
+}  // namespace apollo
+
+#endif  // MODULES_PLANNING_TOOLKITS_OPTIMIZERS_DP_POLY_PATH_DP_ROAD_GRAPH_H_
