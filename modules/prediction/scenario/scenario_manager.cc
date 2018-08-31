df60464407ff910c0e64fb2825189aa20aa8cdfc
Yajia Zhang <yajia.zhang@gmail.com>
2018-08-31 16:36:18 -0700

    prediction: code refactor

--- modules/prediction/scenario/scenario_manager.cc
+++ modules/prediction/scenario/scenario_manager.cc
@@ -21,7 +21,7 @@ namespace prediction {
 
 void ScenarioManager::Run() {
   feature_extractor_.ExtractFeatures();
-  scenario_analyzer_.Analyze(feature_extractor_.scenario_feature());
+  scenario_analyzer_.Analyze(feature_extractor_.GetScenarioFeatures());
   // TODO(kechxu) deal with scenario output
 }
 
