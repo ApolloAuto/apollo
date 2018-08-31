1b3b465f721aa4b294dcccd5d040c0a9e40868be
kechxu <kechxu89@gmail.com>
2018-08-31 16:10:12 -0700

    Prediction: placeholder for scenario_analyzer_test

---
+++ modules/prediction/scenario/analyzer/scenario_analyzer_test.cc
@@ -0,0 +1,38 @@
+/******************************************************************************
+ * Copyright 2018 The Apollo Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *****************************************************************************/
+
+#include "modules/prediction/scenario/analyzer/scenario_analyzer.h"
+
+#include "gtest/gtest.h"
+
+#include "modules/prediction/common/kml_map_based_test.h"
+#include "modules/prediction/common/prediction_gflags.h"
+
+namespace apollo {
+namespace prediction {
+
+class ScenarioAnalyzerTest : public KMLMapBasedTest {};
+
+TEST_F(ScenarioAnalyzerTest, getter) {
+  // TODO(kechxu) add tests
+}
+
+TEST_F(ScenarioAnalyzerTest, junction) {
+  // TODO(kechxu) add tests
+}
+
+}  // namespace prediction
+}  // namespace apollo
