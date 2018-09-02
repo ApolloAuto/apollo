90ce6634f9de4647850b3f634385936fcb2df6f1
Liangliang Zhang <zhangliangliang@baidu.com>
2018-09-02 00:41:56 -0700

    Planning: initial refactor for new planning.

--- modules/planning/planner/planner.h
+++ modules/planning/planner/planner.h
@@ -17,10 +17,13 @@
 #ifndef MODULES_PLANNING_PLANNER_PLANNER_H_
 #define MODULES_PLANNING_PLANNER_PLANNER_H_
 
+#include <string>
+
 #include "modules/common/proto/pnc_point.pb.h"
+#include "modules/planning/proto/planning_config.pb.h"
+
 #include "modules/common/status/status.h"
 #include "modules/planning/common/frame.h"
-#include "modules/planning/proto/planning_config.pb.h"
 
 /**
  * @namespace apollo::planning
@@ -47,6 +50,7 @@ class Planner {
    */
   virtual ~Planner() = default;
 
+  virtual std::string Name() = 0;
   virtual apollo::common::Status Init(const PlanningConfig& config) = 0;
 
   /**
