b70f4a066fddf2ef9dece19e11a8d0ab624bb6bc
Liangliang Zhang <zhangliangliang@baidu.com>
2018-09-04 16:36:28 -0700

    Planning: use scenario based config rather than planner based configs.

--- modules/planning/tasks/qp_spline_path/qp_spline_path_optimizer.cc
+++ modules/planning/tasks/qp_spline_path/qp_spline_path_optimizer.cc
@@ -34,7 +34,8 @@ QpSplinePathOptimizer::QpSplinePathOptimizer()
     : PathOptimizer("QpSplinePathOptimizer") {}
 
 bool QpSplinePathOptimizer::Init(const PlanningConfig& config) {
-  qp_spline_path_config_ = config.em_planner_config().qp_spline_path_config();
+  qp_spline_path_config_ =
+      config.lane_follow_scenario_config().qp_spline_path_config();
   std::vector<double> init_knots;
   spline_generator_.reset(
       new Spline1dGenerator(init_knots, qp_spline_path_config_.spline_order()));
