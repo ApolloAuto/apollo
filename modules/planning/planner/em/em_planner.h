5bfe702b89979abce38834cad90887866bfa5a37
Jiangtao Hu <boris.hu@gmail.com>
2018-09-04 10:00:38 -0700

    planning: add default stop fallback in case of polynomial fallback failure.

--- modules/planning/planner/em/em_planner.h
+++ modules/planning/planner/em/em_planner.h
@@ -101,6 +101,7 @@ class EMPlanner : public PlannerWithReferenceLine {
 
   SpeedData GenerateFallbackSpeedProfile(
       const ReferenceLineInfo& reference_line_info);
+  FRIEND_TEST(EMPlannerTest, GenerateFallbackSpeedProfile);
 
   SpeedData GenerateStopProfile(const double init_speed,
                                 const double init_acc) const;
