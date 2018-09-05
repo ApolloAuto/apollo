854a6a96a966a40a7ca511edbdf2f82527f592ae
Liangliang Zhang <zhangliangliang@baidu.com>
2018-09-05 15:23:41 -0700

    Planning: added EgoInfo class.

---
+++ modules/planning/common/ego_info.h
@@ -0,0 +1,95 @@
+/******************************************************************************
+ * Copyright 2018 The Apollo Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *****************************************************************************/
+
+/**
+ * @file ego_info.h
+ **/
+
+#ifndef MODULES_PLANNING_COMMON_EGO_INFO_H_
+#define MODULES_PLANNING_COMMON_EGO_INFO_H_
+
+#include <unordered_map>
+#include <utility>
+#include <vector>
+
+#include "modules/common/vehicle_state/proto/vehicle_state.pb.h"
+
+#include "modules/common/macro.h"
+#include "modules/planning/reference_line/reference_line.h"
+
+namespace apollo {
+namespace planning {
+
+class EgoInfo {
+ public:
+  ~EgoInfo() = default;
+
+  common::TrajectoryPoint start_point() const { return start_point_; }
+
+  void set_start_point(const common::TrajectoryPoint& start_point) {
+    start_point_ = start_point;
+  }
+
+  SLBoundary GetSLBoundaryOnReferenceLine(
+      const ReferenceLine* reference_line) const {
+    if (sl_boundary_map_.find(reference_line) != sl_boundary_map_.end()) {
+      return sl_boundary_map_.at(reference_line);
+    } else {
+      return SLBoundary();
+    }
+  }
+
+  void SetSLBoundary(const ReferenceLine* reference_line,
+                     const SLBoundary& sl_boundary) {
+    sl_boundary_map_[reference_line] = sl_boundary;
+  }
+
+  common::VehicleState vehicle_state() const { return vehicle_state_; }
+
+  void set_vehicle_state(const common::VehicleState& vehicle_state) {
+    vehicle_state_ = vehicle_state;
+  }
+
+  void CalculateFrontClearDistance();
+
+ private:
+  // stitched point (at stitching mode)
+  // or real vehicle point (at non-stitching mode)
+  common::TrajectoryPoint start_point_;
+
+  // ego vehicle state
+  common::VehicleState vehicle_state_;
+
+  /**
+   * @brief SL boundary of stitching point (starting point of plan trajectory)
+   * relative to the reference line
+   */
+  std::unordered_map<const ReferenceLine*, SLBoundary> sl_boundary_map_;
+
+  /**
+   * @brief SL boundary of vehicle realtime state relative to the reference line
+   */
+  SLBoundary vehicle_sl_boundary_;
+
+  double front_clear_distance_ = -1.0;
+
+  DECLARE_SINGLETON(EgoInfo);
+};
+
+}  // namespace planning
+}  // namespace apollo
+
+#endif  // MODULES_PLANNING_COMMON_EGO_INFO_H_
