9ff748099e445a7f40fd1369c72029f2e26433b3
vlin17 <thunder0504@gmail.com>
2018-09-06 16:32:21 -0700

    Dreamview: added hotkey for default routing and audio capture, adjusted data recorder UI

--- modules/dreamview/frontend/dist/app.bundle.js
+++ modules/dreamview/frontend/dist/app.bundle.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5f3c2dc8c143bd17f22cc44870205be93c1a5297eb8e116fbd157cb1af3bccbc
-size 1616860
+oid sha256:53dd2f615829658585f9c26632cf5447067f6350eca42dcf49bf38ae85beda6d
+size 1617003
