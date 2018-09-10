82928416ca3fac4b7fa04c83998be52142635d3b
vlin17 <thunder0504@gmail.com>
2018-09-10 14:42:46 -0700

    Dreamview: add tooltips and refactor sidebar code

--- modules/dreamview/frontend/src/components/SideBar/index.js
+++ modules/dreamview/frontend/src/components/SideBar/index.js
@@ -1,19 +1,48 @@
 import React from "react";
 import { inject, observer } from "mobx-react";
+import classNames from "classnames";
+import ReactTooltip from "react-tooltip";
+import _ from "lodash";
 
-import ButtonPanel from "components/SideBar/ButtonPanel";
-import SecondaryButton from "components/SideBar/SecondaryButton";
+import SideBarButton from "components/SideBar/SideBarButton";
+import HOTKEYS_CONFIG from "store/config/hotkeys.yml";
 import WS from "store/websocket";
+
+import TasksIcon from "assets/images/sidebar/tasks.png";
+import ModuleControllerIcon from "assets/images/sidebar/module_controller.png";
+import LayerMenuIcon from "assets/images/sidebar/layer_menu.png";
+import RouteEditingIcon from "assets/images/sidebar/route_editing.png";
+import DataRecorderIcon from "assets/images/sidebar/data_recorder.png";
+
+const sidebarIconMapping = {
+    showTasks: TasksIcon,
+    showModuleController: ModuleControllerIcon,
+    showMenu: LayerMenuIcon,
+    showRouteEditingBar: RouteEditingIcon,
+    showDataRecorder: DataRecorderIcon,
+};
+
+const sidebarLabelMapping = {
+    showTasks: "Tasks",
+    showModuleController: "Module Controller",
+    showMenu: "Layer Menu",
+    showRouteEditingBar: "Route Editing",
+    showDataRecorder: "Data Recorder",
+    enableAudioCapture: "Audio Capture",
+    showPOI: "Default Routing",
+};
 
 @inject("store") @observer
 export default class SideBar extends React.Component {
     render() {
         const { options, enableHMIButtonsOnly, hmi } = this.props.store;
 
+        const hotkeys = _.invert(HOTKEYS_CONFIG);
         const settings = {};
         const optionNames = [...options.mainSideBarOptions, ...options.secondarySideBarOptions];
         optionNames.forEach(optionName => {
             settings[optionName] = {
+                label: sidebarLabelMapping[optionName],
                 active: options[optionName],
                 onClick: () => {
                     this.props.store.handleOptionToggle(optionName);
@@ -23,24 +52,27 @@ export default class SideBar extends React.Component {
                     enableHMIButtonsOnly,
                     hmi.inNavigationMode
                 ),
+                hotkey: hotkeys[optionName],
+                iconSrc: sidebarIconMapping[optionName],
             };
         });
 
         return (
             <div className="side-bar">
-                <ButtonPanel settings={settings} />
+                <div className="main-panel">
+                    <SideBarButton type="main" {...settings.showTasks} />
+                    <SideBarButton type="main" {...settings.showModuleController} />
+                    <SideBarButton type="main" {...settings.showMenu} />
+                    <SideBarButton type="main" {...settings.showRouteEditingBar} />
+                    <SideBarButton type="main" {...settings.showDataRecorder} />
+                </div>
                 <div className="sub-button-panel">
-                    <SecondaryButton
-                        panelLabel="Audio Capture"
-                        disabled={settings.enableAudioCapture.disabled}
-                        onClick={settings.enableAudioCapture.onClick}
-                        active={settings.enableAudioCapture.active} />
-                    <SecondaryButton
-                        panelLabel="Default Routing"
-                        disabled={settings.showPOI.disabled}
-                        onClick={settings.showPOI.onClick}
+                    <SideBarButton type="sub" {...settings.enableAudioCapture} />
+                    <SideBarButton
+                        type="sub" {...settings.showPOI}
                         active={!options.showRouteEditingBar && options.showPOI} />
                 </div>
+                <ReactTooltip id="sidebar-button" place="right" delayShow={500} />
             </div>
         );
     }
