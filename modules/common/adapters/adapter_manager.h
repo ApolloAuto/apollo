0a2c6e2c4604f534a3ba9e3f22e5d6e1027b3433
siyangy <yusiyang@baidu.com>
2018-09-10 17:39:20 -0700

    Fix build

--- modules/common/adapters/adapter_manager.h
+++ modules/common/adapters/adapter_manager.h
@@ -32,7 +32,7 @@
 #include "modules/common/adapters/proto/adapter_config.pb.h"
 #include "modules/common/log.h"
 #include "modules/common/macro.h"
-#include "modules/common/transform_listener/transform_listener.h"
+// #include "modules/common/transform_listener/transform_listener.h"
 
 #include "cybertron/cybertron.h"
 
