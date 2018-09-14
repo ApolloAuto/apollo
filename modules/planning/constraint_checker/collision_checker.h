f35777c193065bda6a3b93cd617c0c7a32a8ad11
JasonZhou404 <jinyunzhou@baidu.com>
2018-09-13 17:23:18 -0700

    Planning: Hybrid A star warm start

--- modules/planning/constraint_checker/collision_checker.h
+++ modules/planning/constraint_checker/collision_checker.h
@@ -45,6 +45,7 @@ class CollisionChecker {
       const std::shared_ptr<PathTimeGraph>& ptr_path_time_graph);
 
   bool InCollision(const DiscretizedTrajectory& discretized_trajectory);
+  bool InCollision(const Box2d ego_car, const std::vector<Box2d> obstacles);
 
  private:
   void BuildPredictedEnvironment(
