2c490b52910073eb77b19d496cc60aa7a81efd08
fengqikai1414 <fengqikai1414@126.com>
2018-09-16 16:38:42 -0700

    framework: optimization service wait strategy

--- framework/cybertron/service/client_base.h
+++ framework/cybertron/service/client_base.h
@@ -43,12 +43,16 @@ class ClientBase {
   bool WaitForServiceNanoseconds(std::chrono::nanoseconds time_out) {
     bool has_service = false;
     auto step_duration = std::chrono::nanoseconds(5 * 1000 * 1000);
-    while (!has_service && time_out.count() > 0) {
+    while (time_out.count() > 0) {
       has_service = service_discovery::TopologyManager::Instance()
                         ->service_manager()
                         ->HasService(service_name_);
-      std::this_thread::sleep_for(step_duration);
-      time_out -= step_duration;
+      if (!has_service) {
+        std::this_thread::sleep_for(step_duration);
+        time_out -= step_duration;
+      } else {
+        break;
+      }
     }
     return has_service;
   }
