fe7f5aa6c73e8bd44ae4936cb49cbc140d2045ec
Yajia Zhang <yajia.zhang@gmail.com>
2018-09-17 14:01:11 -0700

    docker: updated ipopt install script (#214)

--- docker/build/installers/install_ipopt.sh
+++ docker/build/installers/install_ipopt.sh
@@ -23,15 +23,15 @@ cd "$(dirname "${BASH_SOURCE[0]}")"
 
 apt-get install -y libblas-dev liblapack-dev gfortran
 
-wget https://www.coin-or.org/download/source/Ipopt/Ipopt-3.12.8.zip -O Ipopt-3.12.8.zip
-unzip Ipopt-3.12.8.zip
+wget https://www.coin-or.org/download/source/Ipopt/Ipopt-3.12.11.zip -O Ipopt-3.12.11.zip
+unzip Ipopt-3.12.11.zip
 
-pushd Ipopt-3.12.8/ThirdParty/Mumps
+pushd Ipopt-3.12.11/ThirdParty/Mumps
 bash get.Mumps
 popd
 
-pushd Ipopt-3.12.8
-./configure --build=x86_64
+pushd Ipopt-3.12.11
+./configure --build=x86_64 --disable-shared ADD_CXXFLAGS="-fPIC" ADD_CFLAGS="-fPIC" ADD_FFLAGS="-fPIC"
 make -j8 all
 make install
 mkdir -p /usr/local/ipopt
@@ -40,4 +40,4 @@ popd
 
 # Clean up.
 apt-get clean && rm -rf /var/lib/apt/lists/*
-rm -fr Ipopt-3.12.8.zip Ipopt-3.12.8
+rm -fr Ipopt-3.12.11.zip Ipopt-3.12.11
