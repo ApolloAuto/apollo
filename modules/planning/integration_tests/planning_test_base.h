36c3d57c34d7876f451df8c818235533fa218f80
jmtao <taojm99@gmail.com>
2018-09-18 16:41:16 -0700

    planning: header file cleanup

--- modules/planning/integration_tests/planning_test_base.h
+++ modules/planning/integration_tests/planning_test_base.h
@@ -15,25 +15,12 @@
  *****************************************************************************/
 
 #include <map>
-#include <memory>
-#include <mutex>
+ #include <memory>
 #include <string>
-#include <vector>
 
 #include "gtest/gtest.h"
 
-#include "modules/canbus/proto/chassis.pb.h"
-#include "modules/localization/proto/localization.pb.h"
-#include "modules/perception/proto/traffic_light_detection.pb.h"
-#include "modules/planning/proto/dp_poly_path_config.pb.h"
-#include "modules/planning/proto/dp_st_speed_config.pb.h"
 #include "modules/planning/proto/traffic_rule_config.pb.h"
-#include "modules/prediction/proto/prediction_obstacle.pb.h"
-#include "modules/routing/proto/routing.pb.h"
-
-#include "modules/common/adapters/adapter_gflags.h"
-#include "modules/common/configs/config_gflags.h"
-#include "modules/common/util/file.h"
 
 #define protected public
 // TODO(all) #include "modules/planning/navi_planning.h"
