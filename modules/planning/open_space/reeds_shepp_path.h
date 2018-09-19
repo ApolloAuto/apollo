1bf032dc8f6b2a1d72822692664c777c6cb086f1
JasonZhou404 <jinyunzhou@baidu.com>
2018-09-18 19:40:59 -0700

    Planning: Implementing Reed Shepp

--- modules/planning/open_space/reeds_shepp_path.h
+++ modules/planning/open_space/reeds_shepp_path.h
@@ -21,10 +21,10 @@
 #ifndef MODULES_PLANNING_OPEN_SPACE_REEDS_SHEPP_PATH_H_
 #define MODULES_PLANNING_OPEN_SPACE_REEDS_SHEPP_PATH_H_
 
+#include <limits>
 #include <memory>
 #include <string>
 #include <vector>
-#include <limits>
 
 #include "modules/common/log.h"
 #include "modules/common/math/math_utils.h"
@@ -33,7 +33,6 @@ namespace apollo {
 namespace planning {
 
 struct ReedSheppPath {
-  std::vector<std::String> segs_types;
   std::vector<double> segs_lengths;
   double total_length;
   std::vector<double> x;
@@ -44,10 +43,11 @@ struct ReedSheppPath {
 };
 
 struct RSPParam {
-    bool flag;
-    double t;
-    double u;
-    double v;
+  bool flag;
+  bool backward;
+  double t;
+  double u;
+  double v;
 }
 
 class ReedShepp {
@@ -58,44 +58,55 @@ class ReedShepp {
   // Pick the shortest path from all possible combination of movement primitives
   // by Reed Shepp
   bool ShortestRSP(const std::shared_ptr<Node3d> start_node;
-                            const std::shared_ptr<Node3d> end_node, std::unique_ptr<ReedSheppPath>);
+                   const std::shared_ptr<Node3d> end_node,
+                   ReedSheppPath* optimal_path);
 
  private:
   // Generate all possible combination of movement primitives by Reed Shepp and
   // interpolate them
-  bool GenerateRSPs(std::shared_ptr<Node3d> start_node,
-                                          std::shared_ptr<Node3d> end_node, std::vector<std::unique_ptr<ReedSheppPath>> all_possible_paths);
+  bool GenerateRSPs(const std::shared_ptr<Node3d> start_node,
+                    const std::shared_ptr<Node3d> end_node,
+                    std::vector<ReedSheppPath>* all_possible_paths);
   // Set the general profile of the movement primitives
-  bool GenerateRSP(std::shared_ptr<Node3d> start_node,
-                                          std::shared_ptr<Node3d> end_node, std::vector<std::unique_ptr<ReedSheppPath>> all_possible_paths);
+  bool GenerateRSP(const std::shared_ptr<Node3d> start_node,
+                   const std::shared_ptr<Node3d> end_node,
+                   std::vector<ReedSheppPath>* all_possible_paths);
   // Set local exact configurations profile of each movement primitive
-  bool GenerateLocalConfigurations(std::vector<std::unique_ptr<ReedSheppPath>> all_possible_paths);
+  bool GenerateLocalConfigurations(
+      std::vector<ReedSheppPath>* all_possible_paths);
   // Interpolation usde in GenetateLocalConfiguration
   std::vector<ReedSheppPath> Interpolation();
   // motion primitives combination setup function
-  std::vector<ReedSheppPath> SetRSP();
-  // Six different combination of motion primitive in Reed Shepp path used in GenerateRSP()
-  std::vector<ReedSheppPath> SCS();
-  std::vector<ReedSheppPath> CSC();
-  std::vector<ReedSheppPath> CCC();
-  std::vector<ReedSheppPath> CCCC();
-  std::vector<ReedSheppPath> CCSC();
-  std::vector<ReedSheppPath> CCSCC();
+  std::vector<ReedSheppPath> SetRSP(RSPParam& param, std::vector<ReedSheppPath>* all_possible_paths);
+  // Six different combination of motion primitive in Reed Shepp path used in
+  // GenerateRSP()
+  bool SCS(double x, double y, double phi,
+           std::vector<ReedSheppPath>* all_possible_paths);
+  bool CSC(double x, double y, double phi,
+           std::vector<ReedSheppPath>* all_possible_paths);
+  bool CCC(double x, double y, double phi,
+           std::vector<ReedSheppPath>* all_possible_paths);
+  bool CCCC(double x, double y, double phi,
+            std::vector<ReedSheppPath>* all_possible_paths);
+  bool CCSC(double x, double y, double phi,
+            std::vector<ReedSheppPath>* all_possible_paths);
+  bool CCSCC(double x, double y, double phi,
+             std::vector<ReedSheppPath>* all_possible_paths);
   // different options for different combination of motion primitives
-  RSPParam LSL();
-  RSPParam LSR();
-  RSPParam LRL();
-  RSPParam SLS();
-  RSPParam LRLRn();
-  RSPParam LRLRp();
-  RSPParam LRSR();
-  RSPParam LRSL();
-  RSPParam LRSLR();
-
+  void LSL(double x, double y, double phi, RSPParam* param);
+  void LSR(double x, double y, double phi, RSPParam* param);
+  void LRL(double x, double y, double phi, RSPParam* param);
+  void SLS(double x, double y, double phi, RSPParam* param);
+  void LRLRn(double x, double y, double phi, RSPParam* param);
+  void LRLRp(double x, double y, double phi, RSPParam* param);
+  void LRSR(double x, double y, double phi, RSPParam* param);
+  void LRSL(double x, double y, double phi, RSPParam* param);
+  void LRSLR(double x, double y, double phi, RSPParam* param);
 
  private:
-  common::VehicleParam& vehicle_param_;
+  common::VehicleParam vehicle_param_;
   PlannerOpenSpaceConfig open_space_conf_;
+  double max_kappa_;
 };
 }  // namespace planning
 }  // namespace apollo
