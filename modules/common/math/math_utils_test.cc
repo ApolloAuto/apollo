28534079c49e0e95b9f1b82bbe49c9e67d2bc737
Liangliang Zhang <zhangliangliang@baidu.com>
2018-09-19 17:09:23 -0700

    third_party: added osqp library.

--- modules/common/math/math_utils_test.cc
+++ modules/common/math/math_utils_test.cc
@@ -18,6 +18,8 @@
 
 #include "gtest/gtest.h"
 
+#include "osqp/include/osqp.h"
+
 namespace apollo {
 namespace common {
 namespace math {
@@ -79,9 +81,7 @@ TEST(MathUtilsTest, Sqr) {
   EXPECT_DOUBLE_EQ(169.0, Sqr(-13.0));
 }
 
-TEST(MathUtilsTest, Sigmoid) {
-  EXPECT_DOUBLE_EQ(0.5, Sigmoid(0.0));
-}
+TEST(MathUtilsTest, Sigmoid) { EXPECT_DOUBLE_EQ(0.5, Sigmoid(0.0)); }
 
 TEST(MathUtilsTest, Clamp) {
   EXPECT_EQ(1, Clamp(1, 0, 6));
@@ -111,6 +111,77 @@ TEST(MathUtilsTest, RotateAxis) {
   RotateAxis(M_PI, 1.0, 0.0, &x, &y);
   EXPECT_DOUBLE_EQ(expected_x, x);
   EXPECT_NEAR(expected_y, y, 1e-5);
+}
+
+TEST(MathUtilsTest, QPSTTest) {
+  // Load problem data
+  c_float P_x[4] = {
+      4.00000000000000000000, 1.00000000000000000000, 1.00000000000000000000,
+      2.00000000000000000000,
+  };
+  c_int P_nnz = 4;
+  c_int P_i[4] = {
+      0, 1, 0, 1,
+  };
+  c_int P_p[3] = {
+      0, 2, 4,
+  };
+  c_float q[2] = {
+      1.00000000000000000000, 1.00000000000000000000,
+  };
+  c_float A_x[4] = {
+      1.00000000000000000000, 1.00000000000000000000, 1.00000000000000000000,
+      1.00000000000000000000,
+  };
+  c_int A_nnz = 4;
+  c_int A_i[4] = {
+      0, 1, 0, 2,
+  };
+  c_int A_p[3] = {
+      0, 2, 4,
+  };
+  c_float l[3] = {
+      1.00000000000000000000, 0.00000000000000000000, 0.00000000000000000000,
+  };
+  c_float u[3] = {
+      1.00000000000000000000, 0.69999999999999995559, 0.69999999999999995559,
+  };
+  c_int n = 2;
+  c_int m = 3;
+
+  // Problem settings
+  OSQPSettings *settings =
+      reinterpret_cast<OSQPSettings *>(c_malloc(sizeof(OSQPSettings)));
+
+  // Structures
+  OSQPWorkspace *work;  // Workspace
+  OSQPData *data;       // OSQPData
+
+  // Populate data
+  data = reinterpret_cast<OSQPData *>(c_malloc(sizeof(OSQPData)));
+  data->n = n;
+  data->m = m;
+  data->P = csc_matrix(data->n, data->n, P_nnz, P_x, P_i, P_p);
+  data->q = q;
+  data->A = csc_matrix(data->m, data->n, A_nnz, A_x, A_i, A_p);
+  data->l = l;
+  data->u = u;
+
+  // Define Solver settings as default
+  osqp_set_default_settings(settings);
+
+  // Setup workspace
+  work = osqp_setup(data, settings);
+
+  // Solve Problem
+  osqp_solve(work);
+
+  // Clean workspace
+  osqp_cleanup(work);
+  c_free(data->A);
+  c_free(data->P);
+  c_free(data);
+  c_free(settings);
 }
 
 }  // namespace math
