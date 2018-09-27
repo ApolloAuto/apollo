4e9d2f1e0516482576d2c26dd546a7ff6ec79a38
Liangliang Zhang <zhangliangliang@baidu.com>
2018-09-26 20:09:32 -0700

    fixed lint from last commit.

--- modules/perception/fusion/lib/data_association/hm_data_association/chi_squared_cdf_1_0.0500_0.999900.h
+++ modules/perception/fusion/lib/data_association/hm_data_association/chi_squared_cdf_1_0.0500_0.999900.h
@@ -13,8 +13,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *****************************************************************************/
-#ifndef MODULES_PERCEPTION_FUSION_LIB_DATA_ASSOCIATION_CHI_SQUARED_CDF_1_0_0500_0_999900_H_
-#define MODULES_PERCEPTION_FUSION_LIB_DATA_ASSOCIATION_CHI_SQUARED_CDF_1_0_0500_0_999900_H_
+#ifndef MODULES_PERCEPTION_FUSION_LIB_DATA_ASSOCIATION_CHI_SQUARED_CDF_1_0_0500_0_999900_H_  // NOLINT
+#define MODULES_PERCEPTION_FUSION_LIB_DATA_ASSOCIATION_CHI_SQUARED_CDF_1_0_0500_0_999900_H_  // NOLINT
 
 #include <cstdlib>
 
@@ -87,4 +87,4 @@ inline double ChiSquaredCdf1TableFun(double dist) {
 }  // namespace perception
 }  // namespace apollo
 
-#endif  // MODULES_PERCEPTION_FUSION_LIB_DATA_ASSOCIATION_CHI_SQUARED_CDF_1_0_0500_0_999900_H_
+#endif  // FUSION_LIB_DATA_ASSOCIATION_CHI_SQUARED_CDF_1_0_0500_0_999900_H_
