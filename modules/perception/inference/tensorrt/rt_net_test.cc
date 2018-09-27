e8820455bb67c7a8cb6ecf25916330f897d8354f
Calvin Miao <jhmiao@gmail.com>
2018-09-26 17:17:53 -0700

    Perception: completed restructuring perception/inference unit tests and disabled unit tests because of missing data set

--- modules/perception/inference/tensorrt/rt_net_test.cc
+++ modules/perception/inference/tensorrt/rt_net_test.cc
@@ -339,6 +339,237 @@ class TestableRTNet : public RTNet {
 //   }
 // }
 
+// TEST(RTNetTest, test) {
+//   std::vector<std::string> file_list =
+//   {"ARZ034_12_1499218335_1499218635_500",
+//                                         "ARZ034_12_1499218335_1499218635_520",
+//                                         "ARZ034_12_1499218335_1499218635_540",
+//                                         "ARZ034_12_1499218335_1499218635_560",
+//                                         "ARZ034_12_1499218335_1499218635_580",
+//                                         "ARZ034_12_1499218335_1499218635_606",
+//                                         "ARZ034_12_1499218335_1499218635_834",
+//                                         "ARZ034_12_1499218335_1499218635_854",
+//                                         "ARZ034_12_1499218335_1499218635_874",
+//                                         "ARZ034_12_1499218335_1499218635_894"};
+//   std::string image_root =
+//       "modules/perception/inference//inference_test_data/images/";
+//   std::string image_ext = ".jpg";
+//   std::string output_blob_path =
+//       "modules/perception/inference/inference_test_data/yolo/blob_names.txt";
+//   std::string proto_file =
+//       "modules/perception/inference/inference_test_data/yolo/caffe.pt";
+//   std::string weight_file =
+//       "modules/perception/inference/inference_test_data/yolo/caffe.caffemodel";
+//   std::string result_file =
+//       "modules/perception/inference/inference_test_data/yolo/result.dat";
+//   std::string input_name = "input";
+//   std::string input_blob_name = "data";
+//   std::vector<std::string> outputs{"loc_pred", "obj_pred", "cls_pred",
+//                                    "ori_pred", "dim_pred", "lof_pred",
+//                                    "lor_pred"};
+//   apollo::perception::inference::load_data<std::string>(output_blob_path,
+//                                                         &outputs);
+//   apollo::perception::inference::Inference *rt_net = nullptr;
+//   std::vector<std::string> inputs{"data"};
+
+//   rt_net = apollo::perception::inference::CreateInferenceByName(
+//       "RTNet", proto_file, weight_file, outputs, inputs);
+//   CHECK(rt_net);
+//   int height = 576;
+//   int width = 1440;
+//   int offset_y = 312;
+//   std::vector<int> shape = {1, height, width, 3};
+//   std::map<std::string, std::vector<int>> shape_map{{input_blob_name,
+//   shape}}; rt_net->Init(shape_map); auto input_blob =
+//   rt_net->get_blob(input_blob_name); auto gt =
+//   apollo::perception::inference::load_binary_data(result_file); int idx = 0;
+
+//   int count = 3 * width * height;
+//   for (auto image_file : file_list) {
+//     cv::Mat img = cv::imread(image_root + image_file + image_ext);
+//     cv::Rect roi(0, offset_y, img.cols, img.rows - offset_y);
+//     cv::Mat img_roi = img(roi);
+//     img_roi.copyTo(img);
+//     cv::resize(img, img, cv::Size(width, height));
+//     auto input = input_blob->mutable_cpu_data();
+
+//     for (int i = 0; i < count; i++) {
+//       input[i] = img.data[i];
+//     }
+//     cudaDeviceSynchronize();
+//     rt_net->Infer();
+//     cudaDeviceSynchronize();
+//     for (auto output_name : outputs) {
+//       auto blob = rt_net->get_blob(output_name);
+//       for (int tmp = 0; tmp < blob->count(); tmp++) {
+//         EXPECT_NEAR(blob->cpu_data()[tmp], (gt.get())[idx], 1e-4)
+//             << " idx: " << idx;
+//         idx++;
+//       }
+//     }
+//   }
+//   if (rt_net != nullptr) {
+//     delete rt_net;
+//   }
+// }
+
+// TEST(CaffeNetTest, test) {
+//   std::vector<std::string> file_list =
+//   {"ARZ034_12_1499218335_1499218635_500",
+//                                         "ARZ034_12_1499218335_1499218635_520",
+//                                         "ARZ034_12_1499218335_1499218635_540",
+//                                         "ARZ034_12_1499218335_1499218635_560",
+//                                         "ARZ034_12_1499218335_1499218635_580",
+//                                         "ARZ034_12_1499218335_1499218635_606",
+//                                         "ARZ034_12_1499218335_1499218635_834",
+//                                         "ARZ034_12_1499218335_1499218635_854",
+//                                         "ARZ034_12_1499218335_1499218635_874",
+//                                         "ARZ034_12_1499218335_1499218635_894"};
+//   std::string image_root =
+//       "modules/perception/inference/inference_test_data/images/";
+//   std::string image_ext = ".jpg";
+//   std::string output_blob_path =
+//       "modules/perception/inference/inference_test_data/yolo/blob_names.txt";
+//   std::string proto_file =
+//       "modules/perception/inference/inference_test_data/yolo/caffe.pt";
+//   std::string weight_file =
+//       "modules/perception/inference/inference_test_data/yolo/caffe.caffemodel";
+//   std::string result_file =
+//       "modules/perception/inference/inference_test_data/yolo/result.dat";
+//   std::string input_name = "input";
+//   std::string input_blob_name = "data";
+
+//   std::vector<std::string> outputs{"loc_pred", "obj_pred", "cls_pred",
+//                                    "ori_pred", "dim_pred", "lof_pred",
+//                                    "lor_pred"};
+//   std::vector<std::string> inputs{"data"};
+
+//   apollo::perception::inference::load_data<std::string>(output_blob_path,
+//                                                         &outputs);
+//   std::shared_ptr<apollo::perception::inference::Inference> rt_net;
+
+//   rt_net.reset(apollo::perception::inference::CreateInferenceByName(
+//       "CaffeNet", proto_file, weight_file, outputs, inputs));
+//   CHECK(rt_net);
+
+//   int height = 576;
+//   int width = 1440;
+//   int offset_y = 312;
+//   std::vector<int> shape = {1, height, width, 3};
+//   std::map<std::string, std::vector<int>> shape_map{{input_blob_name,
+//   shape}}; rt_net->Init(shape_map); auto input_blob =
+//   rt_net->get_blob(input_blob_name); auto gt =
+//   apollo::perception::inference::load_binary_data(result_file); int idx = 0;
+
+//   int count = 3 * width * height;
+//   for (auto image_file : file_list) {
+//     cv::Mat img = cv::imread(image_root + image_file + image_ext);
+//     cv::Rect roi(0, offset_y, img.cols, img.rows - offset_y);
+//     cv::Mat img_roi = img(roi);
+//     img_roi.copyTo(img);
+//     cv::resize(img, img, cv::Size(width, height));
+//     auto input = input_blob->mutable_cpu_data();
+
+//     for (int i = 0; i < count; i++) {
+//       input[i] = img.data[i];
+//     }
+//     cudaDeviceSynchronize();
+//     rt_net->Infer();
+//     cudaDeviceSynchronize();
+//     for (auto output_name : outputs) {
+//       auto blob = rt_net->get_blob(output_name);
+//       for (int tmp = 0; tmp < blob->count(); tmp++) {
+//         CHECK_NEAR(blob->cpu_data()[tmp], (gt.get())[idx], 1e-4);
+//         idx++;
+//       }
+//     }
+//   }
+// }
+
+// TEST(RTNetBatchsizeTest, test) {
+//   std::vector<std::string> file_list =
+//   {"ARZ034_12_1499218335_1499218635_500",
+//                                         "ARZ034_12_1499218335_1499218635_520",
+//                                         "ARZ034_12_1499218335_1499218635_540",
+//                                         "ARZ034_12_1499218335_1499218635_560",
+//                                         "ARZ034_12_1499218335_1499218635_580",
+//                                         "ARZ034_12_1499218335_1499218635_606",
+//                                         "ARZ034_12_1499218335_1499218635_834",
+//                                         "ARZ034_12_1499218335_1499218635_854",
+//                                         "ARZ034_12_1499218335_1499218635_874",
+//                                         "ARZ034_12_1499218335_1499218635_894"};
+//   std::string image_root =
+//       "modules/perception/inference/inference_test_data/images/";
+//   std::string image_ext = ".jpg";
+//   std::string output_blob_path =
+//       "modules/perception/inference/inference_test_data/yolo/blob_names.txt";
+//   std::string proto_file =
+//       "modules/perception/inference/inference_test_data/yolo/caffe.pt";
+//   std::string weight_file =
+//       "modules/perception/inference/inference_test_data/yolo/caffe.caffemodel";
+//   std::string result_file =
+//       "modules/perception/inference/inference_test_data/yolo/result.dat";
+//   std::string input_name = "input";
+//   std::string input_blob_name = "data";
+//   std::vector<std::string> outputs{"loc_pred", "obj_pred", "cls_pred",
+//                                    "ori_pred", "dim_pred", "lof_pred",
+//                                    "lor_pred"};
+//   apollo::perception::inference::load_data<std::string>(output_blob_path,
+//                                                         &outputs);
+//   apollo::perception::inference::Inference *rt_net = nullptr;
+//   std::vector<std::string> inputs{"data"};
+
+//   rt_net = apollo::perception::inference::CreateInferenceByName(
+//       "RTNet", proto_file, weight_file, outputs, inputs);
+//   CHECK_NOTNULL(rt_net);
+
+//   int height = 576;
+//   int width = 1440;
+//   int offset_y = 312;
+//   int batchsize = 4;
+//   std::vector<int> shape = {batchsize, height, width, 3};
+//   std::map<std::string, std::vector<int>> shape_map{{input_blob_name,
+//   shape}}; rt_net->set_max_batch_size(batchsize); rt_net->Init(shape_map);
+//   auto input_blob = rt_net->get_blob(input_blob_name);
+//   auto gt = apollo::perception::inference::load_binary_data(result_file);
+//   int idx = 0;
+//   for (int img_idx = 0;
+//        img_idx < (file_list.size() + batchsize - 1) / batchsize; img_idx++) {
+//     auto input = input_blob->mutable_cpu_data();
+//     int start = img_idx * batchsize;
+//     int tmp_batchsize = file_list.size() > (start + batchsize)
+//                             ? batchsize
+//                             : (file_list.size() - start);
+//     for (int i = start; i < start + tmp_batchsize; i++) {
+//       std::string image_file = file_list[i];
+//       int offset = input_blob->offset({i % batchsize});
+//       cv::Mat img = cv::imread(image_root + image_file + image_ext);
+//       cv::Rect roi(0, offset_y, img.cols, img.rows - offset_y);
+//       cv::Mat img_roi = img(roi);
+//       img_roi.copyTo(img);
+//       cv::resize(img, img, cv::Size(width, height));
+//       for (int j = 0; j < input_blob->count(1); j++) {
+//         input[offset + j] = img.data[j];
+//       }
+//     }
+//     cudaDeviceSynchronize();
+//     rt_net->Infer();
+//     cudaDeviceSynchronize();
+//     for (int b = 0; b < tmp_batchsize; b++) {
+//       for (auto output_name : outputs) {
+//         auto blob = rt_net->get_blob(output_name);
+//         for (int tmp = 0; tmp < blob->count(1); tmp++) {
+//           int offset = blob->offset({b});
+//           EXPECT_NEAR(blob->cpu_data()[offset + tmp], (gt.get())[idx], 1e-4)
+//               << " idx: " << idx;
+//           idx++;
+//         }
+//       }
+//     }
+//   }
+//   delete rt_net;
+// }
+
 }  // namespace inference
 }  // namespace perception
 }  // namespace apollo
