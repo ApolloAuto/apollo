a026fdf12acce2ac978a66712b826e66cf85c022
JasonZhou404 <jinyunzhou@baidu.com>
2018-10-01 09:54:58 -0700

    Planning: hybrid A visualizatoin complete

--- modules/planning/planner/open_space/hybrid_a_star_python_interface.py
+++ modules/planning/planner/open_space/hybrid_a_star_python_interface.py
@@ -22,19 +22,24 @@ from ctypes import c_double
 from ctypes import POINTER
 import math
 
-lib = cdll.LoadLibrary('/apollo/bazel-bin/modules/planning/planner/open_space/hybrid_a_star_wrapper_lib.so')
+lib = cdll.LoadLibrary(
+    '/apollo/bazel-bin/modules/planning/planner/open_space/hybrid_a_star_wrapper_lib.so')
+
 
 class HybridAStarPlanner(object):
     def __init__(self):
         self.planner = lib.CreatePlannerPtr()
         self.obstacles = lib.CreateObstaclesPtr()
         self.result = lib.CreateResultPtr()
+
     def AddVirtualObstacle(self, x, y, heading, length, width, identity):
-        lib.AddVirtualObstacle(self.obstacles, c_double(x), 
-            c_double(y), c_double(heading), c_double(length), c_double(width), c_int(identity))
+        lib.AddVirtualObstacle(self.obstacles, c_double(x),
+                               c_double(y), c_double(heading), c_double(length), c_double(width), c_int(identity))
+
     def Plan(self, sx, sy, sphi, ex, ey, ephi):
-        return lib.Plan(self.planner, self.obstacles, self.result, c_double(sx), 
-            c_double(sy), c_double(sphi), c_double(ex), c_double(ey),c_double(ephi))
-    def GetResult(self, x, y, phi, output_size) :
-        lib.GetResult(self.result, POINTER(c_double)(x), POINTER(c_double)(y), 
-            POINTER(c_double)(phi), POINTER(c_ushort)(output_size))
+        return lib.Plan(self.planner, self.obstacles, self.result, c_double(sx),
+                        c_double(sy), c_double(sphi), c_double(ex), c_double(ey), c_double(ephi))
+
+    def GetResult(self, x, y, phi, output_size):
+        lib.GetResult(self.result, POINTER(c_double)(x), POINTER(c_double)(y),
+                      POINTER(c_double)(phi), POINTER(c_ushort)(output_size))
