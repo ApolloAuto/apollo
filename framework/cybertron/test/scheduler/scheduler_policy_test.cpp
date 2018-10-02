cf72bddf59b836f4eae3f3bdb52ea4785f5a92bf
GoLancer <glacierhw@163.com>
2018-10-02 14:53:35 -0700

    framework: fix unit tests build issue and remove useless tests

--- framework/cybertron/test/scheduler/scheduler_policy_test.cpp
+++ framework/cybertron/test/scheduler/scheduler_policy_test.cpp
@@ -2,46 +2,14 @@
 
 #include "cybertron/common/util.h"
 #include "cybertron/cybertron.h"
-#include "cybertron/scheduler/policy/cfs_context.h"
-#include "cybertron/scheduler/policy/fcfs_context.h"
-#include "cybertron/scheduler/policy/rq_context.h"
+#include "cybertron/scheduler/policy/cfs.h"
 #include "cybertron/scheduler/processor.h"
+
 namespace apollo {
 namespace cybertron {
 namespace scheduler {
 
 void func() {}
-TEST(SchedulerPolicyTest, rq) {
-  auto processor = std::make_shared<Processor>();
-  std::shared_ptr<ProcessorContext> ctx;
-  ctx.reset(new RQContext(processor));
-
-  std::shared_ptr<CRoutine> cr = std::make_shared<CRoutine>(func);
-  cr->SetId(common::Hash("rq"));
-  ctx->Push(cr);
-  // repeat Push the same CRoutine
-  EXPECT_TRUE(ctx->RqEmpty());
-  EXPECT_TRUE(ctx->NextRoutine() != nullptr);
-  std::future<std::shared_ptr<CRoutine>> fut;
-  // Pop cr that not exist
-  EXPECT_FALSE(ctx->Pop(1234, fut));
-  // Pop cr that exist
-  EXPECT_TRUE(ctx->Pop(cr->Id(), fut));
-}
-
-TEST(SchedulerPolicyTest, fcfs) {
-  auto processor = std::make_shared<Processor>();
-  std::shared_ptr<ProcessorContext> ctx;
-  ctx.reset(new FCFSContext(processor));
-
-  std::shared_ptr<CRoutine> cr = std::make_shared<CRoutine>(func);
-  EXPECT_TRUE(ctx->RqEmpty());
-  EXPECT_TRUE(ctx->NextRoutine() == nullptr);
-  cr->SetId(common::Hash("fcfs"));
-  ctx->Push(cr);
-  EXPECT_FALSE(ctx->NextRoutine() == nullptr);
-}
-
 TEST(SchedulerPolicyTest, cfs) {
   auto processor = std::make_shared<Processor>();
   std::shared_ptr<ProcessorContext> ctx;
