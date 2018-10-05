c5619671281928aff56c4c6144ecb55253ede6c1
GoLancer <glacierhw@163.com>
2018-10-05 15:57:30 -0700

    framework: switch to bazel

--- scripts/apollo_base.sh
+++ scripts/apollo_base.sh
@@ -426,7 +426,7 @@ function run() {
   run_customized_path $module $module "$@"
 }
 
-CYBERTRON_SETUP="/apollo/framework/install/setup.bash"
+CYBERTRON_SETUP="/apollo/framework/cybertron/setup.bash"
 if [ -e "${CYBERTRON_SETUP}" ]; then
     source "${CYBERTRON_SETUP}"
 fi
