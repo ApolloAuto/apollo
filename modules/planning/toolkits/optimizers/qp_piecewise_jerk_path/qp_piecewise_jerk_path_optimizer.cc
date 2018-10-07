972b2a3c4385ec5e40ff9d6780b25d9e4ddcb1bf
Liangliang Zhang <zhangliangliang@baidu.com>
2018-10-07 02:04:58 -0700

    Update qp_piecewise_jerk_path_optimizer.cc

--- modules/planning/toolkits/optimizers/qp_piecewise_jerk_path/qp_piecewise_jerk_path_optimizer.cc
+++ modules/planning/toolkits/optimizers/qp_piecewise_jerk_path/qp_piecewise_jerk_path_optimizer.cc
@@ -79,8 +79,9 @@ bool QpPiecewiseJerkPathOptimizer::Init(
   if (config.has_qp_piecewise_jerk_path_config()) {
     config_ = config.qp_piecewise_jerk_path_config();
   }
-  // lateral_qp_optimizer_.reset(new ActiverSetLateralQPOptimizer());
-  lateral_qp_optimizer_.reset(new ActiverSetAugmentedLateralQPOptimizer());
+  lateral_qp_optimizer_.reset(new ActiverSetLateralQPOptimizer());
+  // TODO(all): use gflags or config to turn on/off new algorithms
+  // lateral_qp_optimizer_.reset(new ActiverSetAugmentedLateralQPOptimizer());
   is_init_ = true;
   return true;
 }
