04ae556fa7b835552b7e1602400fcda7dbaecf68
michael4338 <michael4338@hotmail.com>
2018-10-09 17:32:28 +0000

    use protobuf instead of json struct

--- modules/data/tools/populate_frames.py
+++ modules/data/tools/populate_frames.py
@@ -17,7 +17,7 @@
 ###############################################################################
 """
 Extract messages of specified topics from data record file,
-and populate them into frames
+and populate them into frames with JSON format
 
 Usage:
     populate_frames.py --input_file=a.record
@@ -32,8 +32,7 @@ import time
 import gflags
 import glog
 import yaml
-
-import frame
+from google.protobuf.json_format import MessageToJson
 
 apollo_root = os.path.abspath(
     os.path.join(os.path.dirname(__file__), '../../../..'))
@@ -42,13 +41,14 @@ sys.path.append(py_proto_path)
 
 from cybertron import cybertron
 from cybertron import record
+from modules.data.proto import frame_pb2
 from modules.drivers.proto.pointcloud_pb2 import PointCloud
 
 # Requried flags.
 gflags.DEFINE_string('input_file', None, 'Input record file path.')
 
 # Optional flags.
-gflags.DEFINE_string('output_path', './', 'Output path.')
+gflags.DEFINE_string('output_path', './', 'Output folder path.')
 
 # Stable flags which rarely change.
 gflags.DEFINE_string('pointcloud_channel', 
@@ -60,6 +60,7 @@ class FramePopulator:
         self._args = args
         self._current_frame_id = '#' 
         self._current_frame = None
+        self._frame_count = 0
         self._channel_function_map = {
             args.pointcloud_channel: self.parse_protobuf_pointcloud,
         }
@@ -68,6 +69,14 @@ class FramePopulator:
         # TODO: LOAD YAML FILE AND GET ITS SETTINGS HERE
         return
 
+    def dump_to_json_file(self):
+        self._frame_count = self._frame_count + 1
+        file_name = os.path.join(self._args.output_path, 
+            'frame{}.json'.format(self._frame_count))
+        jsonObj = MessageToJson(self._current_frame)
+        with open(file_name, 'w') as outfile:
+           outfile.write(jsonObj)
+
     def check_frame(self, frame_id):
         if frame_id != self._current_frame_id:
             glog.info('#current frame {}, changing to {}'.format(
@@ -75,8 +84,8 @@ class FramePopulator:
             if self._current_frame != None:
                 glog.info('#dumping frame {}'.format(self._current_frame_id))
                 self.load_yaml_settings()
-                self._current_frame.dump_to_file(self._args.output_file)
-            self._current_frame = frame.Frame()
+                self.dump_to_json_file()
+            self._current_frame = frame_pb2.Frame()
             self._current_frame_id = frame_id
 
     def process(self, func, message, timestamp):
@@ -90,14 +99,11 @@ class FramePopulator:
         self.check_frame(point_cloud.frame_id)
 
         for pointxyzi in point_cloud.point:
-            vector4 = frame.Vector4(
-                pointxyzi.x,
-                pointxyzi.y,
-                pointxyzi.z,
-                pointxyzi.intensity,
-                False
-            )
-            self._current_frame.points.append(vector4)
+            vector4 = self._current_frame.points.add()
+            vector4.x = pointxyzi.x
+            vector4.y = pointxyzi.y
+            vector4.z = pointxyzi.z
+            vector4.i = pointxyzi.intensity
 
     def process_record_file(self):
         """read record file and extract the message with specified channels"""
@@ -110,6 +116,8 @@ class FramePopulator:
             if channel in self._channel_function_map:
                 self.process(self._channel_function_map[channel], 
                     message, timestamp)
+
+        self.dump_to_json_file()
 
 def main():
     gflags.FLAGS(sys.argv)
