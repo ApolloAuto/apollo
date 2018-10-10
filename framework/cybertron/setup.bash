b5e26f26c0b13d3fa89f3a479eb3a8940f059f90
hewei03 <hewei03@baidu.com>
2018-10-10 16:49:21 +0800

    framework: fix environment issue

--- framework/cybertron/setup.bash
+++ framework/cybertron/setup.bash
@@ -1,11 +1,10 @@
 export CYBERTRON_PATH=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
-library_path="${CYBERTRON_PATH}/third_party:${CYBERTRON_PATH}/python"
 binary_path="/apollo/bazel-bin/cybertron"
 tool_path="/apollo/bazel-bin/cybertron/tools"
 launch_path="${CYBERTRON_PATH}/tools/cyber_launch"
-qt_path=${CYBERTRON_PATH}/../framework/third_party/Qt5.5.1/5.5/gcc_64
+qt_path=${CYBERTRON_PATH}/../third_party/Qt5.5.1/5.5/gcc_64
 
-export LD_LIBRARY_PATH=${library_path}:${qt_path}/lib:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=${qt_path}/lib:$LD_LIBRARY_PATH
 export QT_QPA_PLATFORM_PLUGIN_PATH=${qt_path}/plugins
 export PATH=${binary_path}:${tool_path}:${launch_path}:${qt_path}/bin:$PATH
 export PYTHONPATH=${CYBERTRON_PATH}/python:${CYBERTRON_PATH}/python/cybertron:${CYBERTRON_PATH}/python/cybertron/proto:$PYTHONPATH
@@ -13,4 +12,4 @@ export PYTHONPATH=${CYBERTRON_PATH}/python:${CYBERTRON_PATH}/python/cybertron:${
 export CYBER_DOMAIN_ID=80
 export CYBER_IP=127.0.0.1
 
-source ${CYBERTRON_PATH}/cyber_tools_auto_complete.bash
+source ${CYBERTRON_PATH}/tools/cyber_tools_auto_complete.bash
