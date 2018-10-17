3bb6a8c9a1762c715ca289fb8408e1e56be452e8
Kecheng Xu <kechxu89@gmail.com>
2018-10-17 11:20:11 -0700

    Planning: refactor fem_1d_expanded_qp_problem using sparse matrices directly

--- modules/planning/math/finite_element_qp/fem_1d_expanded_qp_problem.cc
+++ modules/planning/math/finite_element_qp/fem_1d_expanded_qp_problem.cc
@@ -16,6 +16,8 @@
 
 #include "modules/planning/math/finite_element_qp/fem_1d_expanded_qp_problem.h"
 
+#include <utility>
+
 #include "Eigen/Core"
 #include "cybertron/common/log.h"
 
@@ -84,32 +86,24 @@ bool Fem1dExpandedQpProblem::Optimize() {
 void Fem1dExpandedQpProblem::CalculateKernel(std::vector<c_float>* P_data,
                                              std::vector<c_int>* P_indices,
                                              std::vector<c_int>* P_indptr) {
-  P_data->clear();
-  P_indices->clear();
-  P_indptr->clear();
+  const size_t kNumParam = 3 * x_bounds_.size();
+  P_data->resize(kNumParam);
+  P_indices->resize(kNumParam);
+  P_indptr->resize(kNumParam + 1);
 
-  const int kNumParam = static_cast<int>(3 * num_var_);
-  const int N = static_cast<int>(num_var_);
-
-  MatrixXd kernel = MatrixXd::Zero(kNumParam, kNumParam);  // dense matrix
-
-  int idx_ptr = 0;
-  for (int i = 0; i < kNumParam; ++i) {
-    if (i < N) {
-      P_data->emplace_back(2.0 * weight_.x_w + 2.0 * weight_.x_mid_line_w);
-      P_indices->emplace_back(i);
-    } else if (i < 2 * N) {
-      P_data->emplace_back(2.0 * weight_.x_derivative_w);
-      P_indices->emplace_back(i);
+  for (size_t i = 0; i < kNumParam; ++i) {
+    if (i < x_bounds_.size()) {
+      P_data->at(i) = 2.0 * weight_.x_w + 2.0 * weight_.x_mid_line_w;
+    } else if (i < 2 * x_bounds_.size()) {
+      P_data->at(i) = 2.0 * 2.0 * weight_.x_derivative_w;
     } else {
-      P_indices->emplace_back(i);
-      P_data->emplace_back(2.0 * weight_.x_second_order_derivative_w);
+      P_data->at(i) = 2.0 * weight_.x_second_order_derivative_w;
     }
-    P_indptr->emplace_back(idx_ptr);
-    idx_ptr += 1;
+    P_indices->at(i) = i;
+    P_indptr->at(i) = i;
   }
-  P_indptr->emplace_back(idx_ptr);
-  DCHECK_EQ(P_data->size(), P_indices->size());
+  P_indptr->at(kNumParam) = kNumParam;
+  CHECK_EQ(P_data->size(), P_indices->size());
 }
 
 void Fem1dExpandedQpProblem::CalculateOffset(std::vector<c_float>* q) {
@@ -132,20 +126,20 @@ void Fem1dExpandedQpProblem::CalculateAffineConstraint(
     std::vector<c_float>* upper_bounds) {
   const size_t kNumParam = 3 * x_bounds_.size();
   const size_t kNumConstraint = kNumParam + 3 * (num_var_ - 1) + 3;
-  MatrixXd affine_constraint = MatrixXd::Zero(kNumConstraint, kNumParam);
   lower_bounds->resize(kNumConstraint);
   upper_bounds->resize(kNumConstraint);
 
-  const int prime_offset = static_cast<int>(num_var_);
-  const int pprime_offset = static_cast<int>(2 * num_var_);
-  int constraint_index = 0;
+  const size_t prime_offset = num_var_;
+  const size_t pprime_offset = 2 * num_var_;
+
+  std::vector<std::vector<std::pair<c_int, c_float>>> columns;
+  columns.resize(kNumParam);
+  size_t constraint_index = 0;
 
   // d_i+1'' - d_i''
   for (size_t i = 0; i + 1 < num_var_; ++i) {
-    const int row = constraint_index;
-    const int col = pprime_offset + static_cast<int>(i);
-    affine_constraint(row, col) = -1.0;
-    affine_constraint(row, col + 1) = 1.0;
+    columns[pprime_offset + i].emplace_back(constraint_index, -1.0);
+    columns[pprime_offset + i + 1].emplace_back(constraint_index, 1.0);
 
     lower_bounds->at(constraint_index) =
         -max_x_third_order_derivative_ * delta_s_;
@@ -156,12 +150,11 @@ void Fem1dExpandedQpProblem::CalculateAffineConstraint(
 
   // d_i+1' - d_i' - 0.5 * ds * (d_i'' + d_i+1'')
   for (size_t i = 0; i + 1 < num_var_; ++i) {
-    affine_constraint(constraint_index, prime_offset + i) = -1.0;
-    affine_constraint(constraint_index, prime_offset + i + 1) = 1.0;
-
-    affine_constraint(constraint_index, pprime_offset + i) = -0.5 * delta_s_;
-    affine_constraint(constraint_index, pprime_offset + i + 1) =
-        -0.5 * delta_s_;
+    columns[prime_offset + i].emplace_back(constraint_index, -1.0);
+    columns[prime_offset + i + 1].emplace_back(constraint_index, 1.0);
+    columns[pprime_offset + i].emplace_back(constraint_index, -0.5 * delta_s_);
+    columns[pprime_offset + i + 1].emplace_back(constraint_index,
+                                                -0.5 * delta_s_);
 
     lower_bounds->at(constraint_index) = 0.0;
     upper_bounds->at(constraint_index) = 0.0;
@@ -170,42 +163,40 @@ void Fem1dExpandedQpProblem::CalculateAffineConstraint(
 
   // d_i+1 - d_i - d_i' * ds - 1/3 * d_i'' * ds^2 - 1/6 * d_i+1'' * ds^2
   for (size_t i = 0; i + 1 < num_var_; ++i) {
-    affine_constraint(constraint_index, i) = -1.0;
-    affine_constraint(constraint_index, i + 1) = 1.0;
-
-    affine_constraint(constraint_index, prime_offset + i) = -delta_s_;
-
-    affine_constraint(constraint_index, pprime_offset + i) =
-        -delta_s_ * delta_s_ / 3.0;
-    affine_constraint(constraint_index, pprime_offset + i + 1) =
-        -delta_s_ * delta_s_ / 6.0;
+    columns[i].emplace_back(constraint_index, -1.0);
+    columns[i + 1].emplace_back(constraint_index, 1.0);
+    columns[prime_offset + i].emplace_back(constraint_index, -delta_s_);
+    columns[pprime_offset + i].emplace_back(constraint_index,
+                                            -delta_s_ * delta_s_ / 3.0);
+    columns[pprime_offset + i + 1].emplace_back(constraint_index,
+                                                -delta_s_ * delta_s_ / 6.0);
 
     lower_bounds->at(constraint_index) = 0.0;
     upper_bounds->at(constraint_index) = 0.0;
     ++constraint_index;
   }
 
-  affine_constraint(constraint_index, 0) = 1.0;
+  columns[0].emplace_back(constraint_index, 1.0);
   lower_bounds->at(constraint_index) = x_init_[0];
   upper_bounds->at(constraint_index) = x_init_[0];
   ++constraint_index;
 
-  affine_constraint(constraint_index, prime_offset) = 1.0;
+  columns[prime_offset].emplace_back(constraint_index, 1.0);
   lower_bounds->at(constraint_index) = x_init_[1];
   upper_bounds->at(constraint_index) = x_init_[1];
   ++constraint_index;
 
-  affine_constraint(constraint_index, pprime_offset) = 1.0;
+  columns[pprime_offset].emplace_back(constraint_index, 1.0);
   lower_bounds->at(constraint_index) = x_init_[2];
   upper_bounds->at(constraint_index) = x_init_[2];
   ++constraint_index;
 
   const double LARGE_VALUE = 2.0;
   for (size_t i = 0; i < kNumParam; ++i) {
-    affine_constraint(constraint_index, i) = 1.0;
+    columns[i].emplace_back(constraint_index, 1.0);
     if (i < num_var_) {
-      lower_bounds->at(constraint_index) = std::get<0>(x_bounds_[i]);
-      upper_bounds->at(constraint_index) = std::get<1>(x_bounds_[i]);
+      lower_bounds->at(constraint_index) = x_bounds_[i].first;
+      upper_bounds->at(constraint_index) = x_bounds_[i].second;
     } else {
       lower_bounds->at(constraint_index) = -LARGE_VALUE;
       upper_bounds->at(constraint_index) = LARGE_VALUE;
@@ -215,7 +206,16 @@ void Fem1dExpandedQpProblem::CalculateAffineConstraint(
 
   CHECK_EQ(constraint_index, kNumConstraint);
 
-  DenseToCSCMatrix(affine_constraint, A_data, A_indices, A_indptr);
+  size_t ind_p = 0;
+  for (size_t j = 0; j < kNumParam; ++j) {
+    A_indptr->push_back(ind_p);
+    for (const auto& row_data_pair : columns[j]) {
+      A_data->push_back(row_data_pair.second);
+      A_indices->push_back(row_data_pair.first);
+      ++ind_p;
+    }
+  }
+  A_indptr->push_back(ind_p);
 }
 
 }  // namespace planning
