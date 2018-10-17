cf6e059430de862f00f266f68a09191d365020a1
jiangyifei <jiangyifei@baidu.com>
2018-10-17 13:13:17 -0700

    tools: added planning e2e analysis for record analyzer.

--- modules/tools/record_analyzer/lidar_endtoend_analyzer.py
+++ modules/tools/record_analyzer/lidar_endtoend_analyzer.py
@@ -25,33 +25,58 @@ class LidarEndToEndAnalyzer:
 
     def __init__(self):
         """init"""
-        self.endtoend_latency = []
-        self.unprocessed_point_cloud_timestamps = []
+        self.control_endtoend_latency = []
+        self.control_unprocessed_lidar_timestamps = []
+
+        self.planning_endtoend_latency = []
+        self.planning_unprocessed_lidar_timestamps = []
 
     def put_control(self, control_cmd):
         """put control data"""
         if control_cmd.header.lidar_timestamp in \
-                self.unprocessed_point_cloud_timestamps:
-            ind = self.unprocessed_point_cloud_timestamps.index(
+                self.control_unprocessed_lidar_timestamps:
+            ind = self.control_unprocessed_lidar_timestamps.index(
                 control_cmd.header.lidar_timestamp)
-            del (self.unprocessed_point_cloud_timestamps[ind])
-            self.endtoend_latency.append(
+            del (self.control_unprocessed_lidar_timestamps[ind])
+            self.control_endtoend_latency.append(
                 control_cmd.header.lidar_timestamp / 1.0e-9 -
                 control_cmd.header.timestamp_sec)
 
+    def put_planning(self, planning_cmd):
+        """put control data"""
+        if planning_cmd.header.lidar_timestamp in \
+                self.planning_unprocessed_lidar_timestamps:
+            ind = self.planning_unprocessed_lidar_timestamps.index(
+                planning_cmd.header.lidar_timestamp)
+            del (self.planning_unprocessed_lidar_timestamps[ind])
+            self.planning_endtoend_latency.append(
+                planning_cmd.header.lidar_timestamp / 1.0e-9 -
+                planning_cmd.header.timestamp_sec)
+
     def put_lidar(self, point_cloud):
         """put lidar data"""
-        self.unprocessed_point_cloud_timestamps.append(
+        self.control_unprocessed_lidar_timestamps.append(
+            point_cloud.header.lidar_timestamp)
+        self.planning_unprocessed_lidar_timestamps.append(
             point_cloud.header.lidar_timestamp)
 
     def print_endtoend_latency(self):
         """print_endtoend_latency"""
         print "\n\n"
-        print PrintColors.HEADER + "--- End to End Latency (ms) ---" + \
+        print PrintColors.HEADER + "* End to End (Control) Latency (ms)" + \
               PrintColors.ENDC
         analyzer = StatisticalAnalyzer()
-        analyzer.print_statistical_results(self.endtoend_latency)
+        analyzer.print_statistical_results(self.control_endtoend_latency)
 
-        print PrintColors.FAIL + "MISS # OF LIDAR: " + \
-              str(len(self.unprocessed_point_cloud_timestamps)) + \
+        print PrintColors.FAIL + "  - MISS # OF LIDAR: " + \
+              str(len(self.control_unprocessed_lidar_timestamps)) + \
+              PrintColors.ENDC
+
+        print PrintColors.HEADER + "* End to End (Planning) Latency (ms)" + \
+              PrintColors.ENDC
+        analyzer = StatisticalAnalyzer()
+        analyzer.print_statistical_results(self.planning_endtoend_latency)
+
+        print PrintColors.FAIL + "  - MISS # OF LIDAR: " + \
+              str(len(self.planning_unprocessed_lidar_timestamps)) + \
               PrintColors.ENDC
