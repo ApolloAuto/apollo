06a7929a7c0de4c490c25f90591fa29f90b126da
dengchengliang <ahlong2008@hotmail.com>
2018-10-18 14:42:30 +0800

    Drivers: add gnss parser cli tool

--- modules/drivers/gnss/test/parser_cli.cc
+++ modules/drivers/gnss/test/parser_cli.cc
@@ -1,5 +1,5 @@
 /******************************************************************************
- * Copyright 2017 The Apollo Authors. All Rights Reserved.
+ * Copyright 2018 The Apollo Authors. All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -23,14 +23,16 @@
 #include <iostream>
 #include <memory>
 
-#include "modules/common/adapters/adapter_manager.h"
-#include "modules/common/util/file.h"
-#include "modules/drivers/gnss/gnss_gflags.h"
+#include "cybertron/cybertron.h"
+#include "cybertron/record/record_reader.h"
+
 #include "modules/drivers/gnss/parser/data_parser.h"
 #include "modules/drivers/gnss/proto/config.pb.h"
 #include "modules/drivers/gnss/stream/stream.h"
+
 #include "ros/include/rosbag/bag.h"
 #include "ros/include/rosbag/view.h"
+#include "ros/include/std_msgs/String.h"
 
 namespace apollo {
 namespace drivers {
@@ -44,8 +46,7 @@ void ParseBin(const char* filename, DataParser* parser) {
   char b[BUFFER_SIZE];
   while (f) {
     f.read(b, BUFFER_SIZE);
-    std_msgs::StringPtr msg(new std_msgs::String);
-    msg->data.assign(reinterpret_cast<const char*>(b), f.gcount());
+    std::string msg(reinterpret_cast<const char*>(b), f.gcount());
     parser->ParseRawData(msg);
   }
 }
@@ -59,26 +60,42 @@ void ParseBag(const char* filename, DataParser* parser) {
   for (auto const m : view) {
     std_msgs::String::ConstPtr msg = m.instantiate<std_msgs::String>();
     if (msg != nullptr) {
-      parser->ParseRawData(msg);
+      parser->ParseRawData(msg->data);
     }
     std::this_thread::sleep_for(std::chrono::milliseconds(2));
   }
 }
 
-void Parse(const char* filename, const char* file_type) {
+void ParseRecord(const char* filename, DataParser* parser) {
+  cybertron::record::RecordReader reader(filename);
+  cybertron::record::RecordMessage message;
+  while (reader.ReadMessage(&message)) {
+    if (message.channel_name == "/apollo/sensor/gnss/raw_data") {
+      apollo::drivers::gnss::RawData msg;
+      msg.ParseFromString(message.content);
+      parser->ParseRawData(msg.data());
+    }
+    std::this_thread::sleep_for(std::chrono::milliseconds(2));
+  }
+}
+
+void Parse(const char* filename, const char* file_type,
+           const std::shared_ptr<::apollo::cybertron::Node>& node) {
   std::string type = std::string(file_type);
   config::Config config;
-  if (!common::util::GetProtoFromFile(
+  if (!apollo::cybertron::common::GetProtoFromFile(
           std::string("/apollo/modules/drivers/gnss/conf/gnss_conf.pb.txt"),
           &config)) {
     std::cout << "Unable to load gnss conf file";
   }
-  DataParser* parser = new DataParser(config);
+  DataParser* parser = new DataParser(config, node);
   parser->Init();
   if (type == "bag") {
     ParseBag(filename, parser);
   } else if (type == "bin") {
     ParseBin(filename, parser);
+  } else if (type == "record") {
+    ParseRecord(filename, parser);
   } else {
     std::cout << "unknown file type";
   }
@@ -91,14 +108,12 @@ void Parse(const char* filename, const char* file_type) {
 
 int main(int argc, char** argv) {
   if (argc < 3) {
-    std::cout << "Usage: " << argv[0] << " filename [bag|bin]" << std::endl;
+    std::cout << "Usage: " << argv[0] << " filename [record|bin]" << std::endl;
     return 0;
   }
-
-  ros::init(argc, argv, "parser_cli");
-  ros::Time::init();
-  ros::Time::now();
-  apollo::common::adapter::AdapterManager::Init(FLAGS_adapter_config_filename);
-  ::apollo::drivers::gnss::Parse(argv[1], argv[2]);
+  ::apollo::cybertron::Init("parser_cli");
+  std::shared_ptr<::apollo::cybertron::Node> parser_node(
+      ::apollo::cybertron::CreateNode("parser_cli"));
+  ::apollo::drivers::gnss::Parse(argv[1], argv[2], parser_node);
   return 0;
 }
