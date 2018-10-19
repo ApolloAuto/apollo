36f6b18dc7511f24ba803b19f683dd25404f4fbb
jmtao <taojm99@gmail.com>
2018-10-19 13:52:28 -0700

    planning: remove empty Init() for now

--- modules/planning/scenarios/side_pass/side_pass_scenario.cc
+++ modules/planning/scenarios/side_pass/side_pass_scenario.cc
@@ -63,16 +63,6 @@ constexpr double kSpeedOptimizationFallbackCost = 2e4;
 constexpr double kStraightForwardLineCost = 10.0;
 }  // namespace
 
-void SidePassScenario::Init() {
-  if (init_) {
-    return;
-  }
-
-  Scenario::Init();
-
-  init_ = true;
-}
-
 apollo::common::util::Factory<
     ScenarioConfig::StageType, Stage,
     Stage* (*)(const ScenarioConfig::StageConfig& stage_config)>
