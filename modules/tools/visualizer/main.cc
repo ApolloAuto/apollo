c31cb14634e1b0e39904766d28da16435f8c3637
dangyuedong <dangyuedong@baidu.com>
2018-10-20 19:20:42 +0800

    Modules: visualizer checks the os OpenGL version

--- modules/tools/visualizer/main.cc
+++ modules/tools/visualizer/main.cc
@@ -16,18 +16,47 @@
 
 #include <QApplication>
 #include <QFile>
+#include <QOffscreenSurface>
+#include <QOpenGLContext>
+#include <QOpenGLFunctions>
 #include <QSurfaceFormat>
+<<<<<<< HEAD
 #include "cyber/init.h"
+=======
+#include <sstream>
+#include "cybertron/init.h"
+>>>>>>> Modules: add OpenGL version checking function
 #include "modules/tools/visualizer/main_window.h"
 
 int main(int argc, char* argv[]) {
+  QApplication a(argc, argv);
+  int major, minor;
+  {
+    QOffscreenSurface surf;
+    surf.create();
+
+    QOpenGLContext ctx;
+    ctx.create();
+    ctx.makeCurrent(&surf);
+
+    std::stringstream strStreamObj;
+    strStreamObj << (const char*)ctx.functions()->glGetString(
+                     GL_SHADING_LANGUAGE_VERSION);
+    double version;
+    strStreamObj >> version;
+    major = static_cast<int>(version);
+    minor = static_cast<int>(version * 10.0 - major * 10);
+    if(major < 3 && minor < 3){
+        std::cout << "Shading Language Version ( " << version << ") is much lower than 3.3" << std::endl;
+        return -1;
+    }
+  }
   QSurfaceFormat format;
-  format.setVersion(3, 3);
+
+  format.setVersion(major, minor);
   format.setRenderableType(QSurfaceFormat::RenderableType::OpenGL);
   format.setSwapBehavior(QSurfaceFormat::SwapBehavior::DoubleBuffer);
   QSurfaceFormat::setDefaultFormat(format);
-
-  QApplication a(argc, argv);
 
   a.setStyleSheet(
       "QSplitter::handle {"
@@ -44,8 +73,14 @@ int main(int argc, char* argv[]) {
         w.TopologyChanged(change_msg);
       };
 
+<<<<<<< HEAD
   auto channelManager = apollo::cyber::service_discovery::TopologyManager::Instance()
       ->channel_manager();
+=======
+  auto channelManager =
+      apollo::cybertron::service_discovery::TopologyManager::Instance()
+          ->channel_manager();
+>>>>>>> Modules: add OpenGL version checking function
   channelManager->AddChangeListener(topologyCallback);
 
   std::vector<apollo::cyber::proto::RoleAttributes> role_attr_vec;
