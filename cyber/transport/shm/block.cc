df8c9ecd8fd70b8c130af254da942543ec26c0a8
baowanyu <baowanyu@hotmail.com>
2018-10-21 22:20:25 -0700

    framework: split cc_library for transport (#1188)
    * framework: split cc_library for transport
    
    * framework: split cc_library for node

--- cyber/transport/shm/block.cc
+++ cyber/transport/shm/block.cc
@@ -73,9 +73,11 @@ bool Block::Read(const uint8_t* src, std::string* msg, std::string* msg_info) {
   if (msg == nullptr || msg_info == nullptr) {
     return false;
   }
-  msg->assign(reinterpret_cast<const char*>(src), msg_size_);
-  msg_info->assign(reinterpret_cast<const char*>(src) + msg_size_,
-                   msg_info_size_);
+  msg->resize(msg_size_);
+  memcpy(const_cast<char*>(msg->data()), src, msg_size_);
+
+  msg_info->resize(msg_info_size_);
+  memcpy(const_cast<char*>(msg_info->data()), src + msg_size_, msg_info_size_);
 
   return true;
 }
