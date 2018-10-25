90a5580fca56c3710dd50ba6f992ced7a9e9c3f2
Liangliang Zhang <zhangliangliang@baidu.com>
2018-10-24 20:19:29 -0700

    Routing: refactored topo creator.

--- modules/routing/topo_creator/node_creator.cc
+++ modules/routing/topo_creator/node_creator.cc
@@ -21,6 +21,7 @@
 
 namespace apollo {
 namespace routing {
+namespace node_creator {
 
 namespace {
 
@@ -52,17 +53,8 @@ double GetLaneLength(const Lane& lane) {
   return length;
 }
 
-}  // namespace
-
-void NodeCreator::GetPbNode(const Lane& lane, const std::string& road_id,
-                            Node* pb_node, const RoutingConfig* routingconfig) {
-  InitNodeInfo(lane, road_id, pb_node, routingconfig);
-  InitNodeCost(lane, pb_node, routingconfig);
-}
-
-void NodeCreator::AddOutBoundary(
-    const LaneBoundary& bound, double lane_length,
-    RepeatedPtrField<CurveRange>* const out_range) {
+void AddOutBoundary(const LaneBoundary& bound, double lane_length,
+                    RepeatedPtrField<CurveRange>* const out_range) {
   for (int i = 0; i < bound.boundary_type_size(); ++i) {
     if (!IsAllowedOut(bound.boundary_type(i))) {
       continue;
@@ -79,9 +71,8 @@ void NodeCreator::AddOutBoundary(
   }
 }
 
-void NodeCreator::InitNodeInfo(const Lane& lane, const std::string& road_id,
-                               Node* const node,
-                               const RoutingConfig* routingconfig) {
+void InitNodeInfo(const Lane& lane, const std::string& road_id,
+                  Node* const node) {
   double lane_length = GetLaneLength(lane);
   node->set_lane_id(lane.id().id());
   node->set_road_id(road_id);
@@ -97,26 +88,35 @@ void NodeCreator::InitNodeInfo(const Lane& lane, const std::string& road_id,
   }
 }
 
-void NodeCreator::InitNodeCost(const Lane& lane, Node* const node,
-                               const RoutingConfig* routingconfig) {
+void InitNodeCost(const Lane& lane, const RoutingConfig& routing_config,
+                  Node* const node) {
   double lane_length = GetLaneLength(lane);
   double speed_limit = (lane.has_speed_limit()) ? lane.speed_limit()
-                                                : routingconfig->base_speed();
-  double ratio = (speed_limit >= routingconfig->base_speed())
-                     ? (1 / sqrt(speed_limit / routingconfig->base_speed()))
+                                                : routing_config.base_speed();
+  double ratio = (speed_limit >= routing_config.base_speed())
+                     ? (1 / sqrt(speed_limit / routing_config.base_speed()))
                      : 1.0;
   double cost = lane_length * ratio;
   if (lane.has_turn()) {
     if (lane.turn() == Lane::LEFT_TURN) {
-      cost += routingconfig->left_turn_penalty();
+      cost += routing_config.left_turn_penalty();
     } else if (lane.turn() == Lane::RIGHT_TURN) {
-      cost += routingconfig->right_turn_penalty();
+      cost += routing_config.right_turn_penalty();
     } else if (lane.turn() == Lane::U_TURN) {
-      cost += routingconfig->uturn_penalty();
+      cost += routing_config.uturn_penalty();
     }
   }
   node->set_cost(cost);
 }
 
+}  // namespace
+
+void GetPbNode(const hdmap::Lane& lane, const std::string& road_id,
+               const RoutingConfig& routingconfig, Node* const node) {
+  InitNodeInfo(lane, road_id, node);
+  InitNodeCost(lane, routingconfig, node);
+}
+
+}  // namespace node_creator
 }  // namespace routing
 }  // namespace apollo
