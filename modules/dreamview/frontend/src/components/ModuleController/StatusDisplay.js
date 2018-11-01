63dc7324502211a60aea16185d3590d46070afb7
Aaron Xiao <xiaoxiangquan@gmail.com>
2018-11-01 14:32:59 -0700

    Dreamview: Fix status extraction.

--- modules/dreamview/frontend/src/components/ModuleController/StatusDisplay.js
+++ modules/dreamview/frontend/src/components/ModuleController/StatusDisplay.js
@@ -13,15 +13,16 @@ const StatusColorMapping = {
 export default class StatusDisplay extends React.Component {
     render() {
         const { title, status } = this.props;
+        const status_code = status["status"];
 
         return (
             <div className="status-display">
                 <div className="name">{title}</div>
                 <div className="status">
-                    <span>{status.replace('_', ' ')}</span>
+                    <span>{status_code}</span>
                     <span className="status-icon"
                           style={{
-                            backgroundColor: StatusColorMapping[status],
+                            backgroundColor: StatusColorMapping[status_code],
                     }}/>
                 </div>
             </div>
