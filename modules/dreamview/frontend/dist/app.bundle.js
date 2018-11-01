5ff3eab46f9a065efbdd9d1d625dd6d211595a63
Aaron Xiao <xiaoxiangquan@gmail.com>
2018-11-01 14:32:59 -0700

    Dreamview: Fix status extraction.

--- modules/dreamview/frontend/dist/app.bundle.js
+++ modules/dreamview/frontend/dist/app.bundle.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:df10ebbedaeaca264eb6c51e58cf209031d9d60844eedc1ca9943058afd85ca7
-size 1800855
+oid sha256:84d02c35965eb92eb8f327e4ad8d3874e40ab142acbddc855843824833b0d307
+size 1800849
