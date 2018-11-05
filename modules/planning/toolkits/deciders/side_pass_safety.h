8edca15e548de361a0a7d53b6d3686f99ed57d1b
jiangyifei <jiangyifei@baidu.com>
2018-11-05 13:42:26 -0800

    planning: fixed side path safty stage.

--- modules/planning/toolkits/deciders/side_pass_safety.h
+++ modules/planning/toolkits/deciders/side_pass_safety.h
@@ -48,7 +48,7 @@ class SidePassSafety : public Decider {
 
  private:
   static constexpr char const* const SIDEPASS_VIRTUAL_OBSTACLE_ID =
-      "SL_side_pass_safety";
+      "SP_side_pass_safety";
 };
 
 }  // namespace planning
