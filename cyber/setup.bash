3480b5200d24726cc79790a991cceb95c3ad9a1f
zaxon <jiankangxin@gmail.com>
2018-11-06 00:32:25 -0800

    framework: add cyber_visualizer PATH (#1551)

--- cyber/setup.bash
+++ cyber/setup.bash
@@ -1,15 +1,17 @@
 export CYBER_PATH=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 binary_path="/apollo/bazel-bin/cyber"
-tool_path="/apollo/bazel-bin/cyber/tools"
-recorder_path="${tool_path}/cyber_recorder"
-monitor_path="${tool_path}/cyber_monitor"
+cyber_tool_path="/apollo/bazel-bin/cyber/tools"
+apollo_tool_path="/apollo/bazel-bin/modules/tools"
+recorder_path="${cyber_tool_path}/cyber_recorder"
+monitor_path="${cyber_tool_path}/cyber_monitor"
+visualizer_path="${apollo_tool_path}/visualizer"
 PYTHON_LD_PATH="/apollo/bazel-bin/cyber/py_wrapper"
 launch_path="${CYBER_PATH}/tools/cyber_launch"
 qt_path=/usr/local/Qt5.5.1/5.5/gcc_64
 
 export LD_LIBRARY_PATH=${qt_path}/lib:$LD_LIBRARY_PATH
 export QT_QPA_PLATFORM_PLUGIN_PATH=${qt_path}/plugins
-export PATH=${binary_path}:${recorder_path}:${monitor_path}:${launch_path}:${qt_path}/bin:$PATH
+export PATH=${binary_path}:${recorder_path}:${monitor_path}:${launch_path}:${qt_path}/bin:${visualizer_path}:$PATH
 export PYTHONPATH=${PYTHON_LD_PATH}:${CYBER_PATH}/python:$PYTHONPATH
 
 export CYBER_DOMAIN_ID=80
