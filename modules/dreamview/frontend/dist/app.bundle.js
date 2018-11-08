c4ba297add1ca867f3c8e4a6519f0e84808db8f9
Vivian Lin <2775888+vlin17@users.noreply.github.com>
2018-11-07 20:47:32 -0800

    Dreamview: fixed components status' alignment issue (#1619)

--- modules/dreamview/frontend/dist/app.bundle.js
+++ modules/dreamview/frontend/dist/app.bundle.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1e1ce52991040f49115be134dd20627b88fc43daf0398a726b53c95333aaa88e
-size 1803613
+oid sha256:c5b2de187a57d0ed491698589d1cdca6fd110faa3938e6e5c6736b4720b2d46e
+size 1803631
