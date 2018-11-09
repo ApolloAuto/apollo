3b715f563f871a34ab752bf1dd8572085d6c1a44
JasonZhou404 <zhoujiny404@gmail.com>
2018-11-09 13:01:58 -0800

    Planning: OpenSpace: ADOL-C installed

--- modules/planning/open_space/cpp_example.cpp
+++ modules/planning/open_space/cpp_example.cpp
@@ -2,16 +2,16 @@
  ADOL-C -- Automatic Differentiation by Overloading in C++
  File:     cpp_example.cpp
  Revision: $$
- Contents: example for class myADOLC_sparseNPL for interfacing with Ipopt
- 
+ Contents: example for class myADOLC_NPL for interfacing with Ipopt
+
  Copyright (c) Andrea Walther
-   
+
  This file is part of ADOL-C. This software is provided as open source.
- Any use, reproduction, or distribution of the software constitutes 
+ Any use, reproduction, or distribution of the software constitutes
  recipient's acceptance of the terms of the accompanying license file.
- 
- This code is based on the file corresponding file cpp_example.cpp contained 
- in the Ipopt package with the authors:  Carl Laird, Andreas Waechter   
+
+ This code is based on the file corresponding file cpp_example.cpp contained
+ in the Ipopt package with the authors:  Carl Laird, Andreas Waechter
 ----------------------------------------------------------------------------*/
 
 //*************************************************************************
@@ -22,16 +22,15 @@
 //
 //*************************************************************************
 
+#include "ADOL-C_NLP.hpp"
 #include "IpIpoptApplication.hpp"
 #include "IpSolveStatistics.hpp"
-#include "ADOL-C_sparseNLP.hpp"
 
 using namespace Ipopt;
 
-int main(int argv, char* argc[])
-{
+int main(int argv, char* argc[]) {
   // Create an instance of your nlp...
-  SmartPtr<TNLP> myadolc_nlp = new MyADOLC_sparseNLP();
+  SmartPtr<TNLP> myadolc_nlp = new MyADOLC_NLP();
 
   // Create an instance of the IpoptApplication
   SmartPtr<IpoptApplication> app = new IpoptApplication();
@@ -41,7 +40,7 @@ int main(int argv, char* argc[])
   status = app->Initialize();
   if (status != Solve_Succeeded) {
     printf("\n\n*** Error during initialization!\n");
-    return (int) status;
+    return (int)status;
   }
 
   status = app->OptimizeTNLP(myadolc_nlp);
@@ -52,8 +51,9 @@ int main(int argv, char* argc[])
     printf("\n\n*** The problem solved in %d iterations!\n", iter_count);
 
     Number final_obj = app->Statistics()->FinalObjective();
-    printf("\n\n*** The final value of the objective function is %e.\n", final_obj);
+    printf("\n\n*** The final value of the objective function is %e.\n",
+           final_obj);
   }
 
-  return (int) status;
+  return (int)status;
 }
