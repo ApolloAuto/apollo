4b03ca0ded02ab56b53c4d28f7204c5a36760be2
Jiangtao Hu <boris.hu@gmail.com>
2018-11-08 22:05:17 -0800

    planning: fix spline_1d_kernel compile warning.

--- modules/planning/math/smoothing_spline/spline_1d_kernel.cc
+++ modules/planning/math/smoothing_spline/spline_1d_kernel.cc
@@ -37,7 +37,9 @@ Spline1dKernel::Spline1dKernel(const std::vector<double>& x_knots,
                                const uint32_t spline_order)
     : x_knots_(x_knots), spline_order_(spline_order) {
   total_params_ =
-      (x_knots.size() > 1 ? (x_knots.size() - 1) * (1 + spline_order_) : 0);
+      (static_cast<uint32_t>(x_knots.size()) > 1
+           ? (static_cast<uint32_t>(x_knots.size()) - 1) * (1 + spline_order_)
+           : 0);
   kernel_matrix_ = Eigen::MatrixXd::Zero(total_params_, total_params_);
   offset_ = Eigen::MatrixXd::Zero(total_params_, 1);
 }
@@ -147,12 +149,12 @@ bool Spline1dKernel::AddReferenceLineKernelMatrix(
   }
 
   const uint32_t num_params = spline_order_ + 1;
-  for (uint32_t i = 0; i < x_coord.size(); ++i) {
-    double cur_index = FindIndex(x_coord[i]);
+  for (size_t i = 0; i < x_coord.size(); ++i) {
+    auto cur_index = FindIndex(x_coord[i]);
     double cur_rel_x = x_coord[i] - x_knots_[cur_index];
     // update offset
     double offset_coef = -2.0 * ref_x[i] * weight;
-    for (uint32_t j = 0; j < num_params; ++j) {
+    for (size_t j = 0; j < num_params; ++j) {
       offset_(j + cur_index * num_params, 0) += offset_coef;
       offset_coef *= cur_rel_x;
     }
