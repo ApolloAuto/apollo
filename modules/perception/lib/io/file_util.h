8584e7ae8c8a5fc7c3e3320d7ae38e693858a4f7
Aaron Xiao <xiaoxiangquan@gmail.com>
2018-11-09 21:14:16 -0800

    Perception: Retire unused file_util.

--- modules/perception/lib/io/file_util.h
+++ modules/perception/lib/io/file_util.h
@@ -22,42 +22,11 @@ namespace apollo {
 namespace perception {
 namespace lib {
 
-// custom file operate tools
-enum FileType { TYPE_FILE, TYPE_DIR };
-
-// file name compared type
-enum FileCompareType {
-  FCT_DIGITAL = 0,
-  FCT_LEXICOGRAPHICAL = 1,
-  FCT_UNKNOWN = 8
-};
-
 class FileUtil {
  public:
-  // check whether file exists with [suffix] extension in [path]
-  static bool Exists(const std::string &path, const std::string &suffix);
-
-  // check file type : directory or file
-  static bool GetType(const std::string &filename, FileType *type);
-
-  // remove file , include file and directory
-  static bool DeleteFile(const std::string &filename);
-
-  // rename file
-  static bool RenameFile(const std::string &old_file,
-                         const std::string &new_file);
-
-  static bool ReadLines(const std::string &path,
-                        std::vector<std::string> *lines);
-
-  static std::string RemoveFileSuffix(std::string filename);
-
   // TODO(@huanziqiang): this function stay just for compatibility,
   // should be removed later
   static bool GetFileList(const std::string &path, const std::string &suffix,
-                          std::vector<std::string> *files);
-
-  static bool GetFileList(const std::string &path,
                           std::vector<std::string> *files);
 
   static std::string GetAbsolutePath(const std::string &prefix,
@@ -66,23 +35,6 @@ class FileUtil {
   // get file name
   // "/home/work/data/1.txt" -> 1
   static void GetFileName(const std::string &file, std::string *name);
-
-  // return -1 when error occurred
-  static int NumLines(const std::string &filename);
-
-  // compare two file's name by digital value
-  // "/home/work/data/1.txt" < "/home/user/data/10.txt"
-  // "1.txt" < "./data/2.txt"
-  static bool CompareFileByDigital(const std::string &file_left,
-                                   const std::string &file_right);
-
-  // compare two file's name by lexicographical order
-  static bool CompareFileByLexicographical(const std::string &file_left,
-                                           const std::string &file_right);
-
- private:
-  static bool CompareFile(const std::string &file_left,
-                          const std::string &file_right, FileCompareType type);
 };
 
 }  // namespace lib
--- modules/perception/lib/io/file_util_test.cc
+++ modules/perception/lib/io/file_util_test.cc
@@ -13,26 +13,13 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *****************************************************************************/
-#include <gtest/gtest.h>
-
-#include "modules/common/util/file.h"
-#define private public
-#define protected public
 #include "modules/perception/lib/io/file_util.h"
+
+#include <gtest/gtest.h>
 
 namespace apollo {
 namespace perception {
 namespace lib {
-
-TEST(FileUtilTest, TestGetType) {
-  FileType type;
-  EXPECT_FALSE(FileUtil::GetType(
-      "/apollo/modules/perception/testdata/lib/data/2.txt", &type));
-  EXPECT_FALSE(FileUtil::GetType(
-      "/apollo/modules/perception/testdata/lib/data/-/*/", &type));
-  EXPECT_TRUE(
-      FileUtil::GetType("/apollo/modules/perception/testdata/lib/data", &type));
-}
 
 TEST(FileUtilTest, TestGetAbsolutPath) {
   EXPECT_EQ(FileUtil::GetAbsolutePath("", "./xx.txt"), "./xx.txt");
@@ -59,119 +46,13 @@ TEST(FileUtilTest, TestGetFileName) {
   EXPECT_EQ("111", name);
 }
 
-TEST(FileUtilTest, TestCompareFileByDigital) {
-  EXPECT_TRUE(FileUtil::CompareFileByDigital(
-      "/apollo/modules/perception/testdata/lib/data/1.txt",
-      "/home/data/2.txt"));
-  EXPECT_TRUE(FileUtil::CompareFileByDigital("1.txt", "/home/data/2.txt"));
-  EXPECT_TRUE(FileUtil::CompareFileByDigital("01.txt", "/home/data/2.txt"));
-  EXPECT_FALSE(FileUtil::CompareFileByDigital("10", "/home/data/2.txt"));
-  EXPECT_FALSE(FileUtil::CompareFileByDigital("010", "/home/data/2.txt"));
-  EXPECT_FALSE(FileUtil::CompareFileByDigital("100", "001"));
-}
-
-TEST(FileUtilTest, TestCompareFileByLexicographical) {
-  std::string data_folder = "/apollo/modules/perception/testdata/lib/data";
-  EXPECT_TRUE(FileUtil::CompareFileByLexicographical(
-      data_folder + "/QB1234_2222_3333_0001.pcd",
-      data_folder + "/QB1234_2222_3333_1000.pcd"));
-  EXPECT_TRUE(FileUtil::CompareFileByLexicographical(
-      data_folder + "/QB1234_2222_3333_0001.pcd",
-      data_folder + "/QB1234_3333_3333_1000.pcd"));
-}
-
-TEST(FileUtilTest, TestCompareFile) {
-  EXPECT_TRUE(FileUtil::CompareFile("1.txt", "1.txt", FCT_UNKNOWN));
-}
-
-TEST(FileUtilTest, TestExists) {
-  ASSERT_TRUE(apollo::common::util::EnsureDirectory("./tmp"));
-  EXPECT_FALSE(FileUtil::Exists("./tmp", ".txt"));
-  ASSERT_EQ(system("touch ./tmp/a.txt"), 0);
-  EXPECT_TRUE(FileUtil::Exists("./tmp", ".txt"));
-  EXPECT_FALSE(FileUtil::Exists("./tmp", ".bat"));
-  ASSERT_TRUE(FileUtil::DeleteFile("./tmp/a.txt"));
-  ASSERT_TRUE(FileUtil::DeleteFile("./tmp"));
-  // ASSERT_TRUE(FileUtil::DeleteFile("./tmp1"));
-  // ASSERT_EQ(system("chmod -R -x
-  // /apollo/modules/perception/testdata/lib/data3"),
-  //           256);
-  // ASSERT_FALSE(
-  //     FileUtil::DeleteFile("/apollo/modules/perception/testdata/lib/data3"));
-  // ASSERT_EQ(system("chmod -R +x
-  // /apollo/modules/perception/testdata/lib/data3"),
-  //           0);
-  // ASSERT_EQ(system("chmod -R -r
-  // /apollo/modules/perception/testdata/lib/data3"),
-  //           256);
-  // ASSERT_FALSE(
-  //     FileUtil::DeleteFile("/apollo/modules/perception/testdata/lib/data3"));
-  // ASSERT_EQ(system("chmod -R +r
-  // /apollo/modules/perception/testdata/lib/data3"),
-  //           0);
-  // ASSERT_EQ(system("chmod -R -w
-  // /apollo/modules/perception/testdata/lib/data3"),
-  //           0);
-  // ASSERT_FALSE(
-  //     FileUtil::DeleteFile("/apollo/modules/perception/testdata/lib/data3"));
-  // ASSERT_EQ(system("chmod -R +w
-  // /apollo/modules/perception/testdata/lib/data3"),
-  //           0);
-  ASSERT_EQ(system("mkdir -p tmpa/tmpb"), 0);
-  ASSERT_EQ(system("mkdir -p tmpa/tmpc"), 0);
-  ASSERT_EQ(system("touch tmpa/tmpb/b.txt"), 0);
-  ASSERT_EQ(system("touch tmpa/tmpb/b2.txt"), 0);
-  ASSERT_EQ(system("touch tmpa/a.txt"), 0);
-  ASSERT_TRUE(FileUtil::DeleteFile("./tmpa/tmpb/b.txt"));
-  ASSERT_TRUE(FileUtil::DeleteFile("./tmpa/tmpb"));
-  ASSERT_TRUE(FileUtil::DeleteFile("./tmpa"));
-}
-
-TEST(FileUtilTest, TestReadLines) {
-  std::string path = "/apollo/modules/perception/testdata/lib/data/1.txt";
-  std::vector<std::string> lines;
-  EXPECT_FALSE(FileUtil::ReadLines("/not_exist_path", &lines));
-  EXPECT_FALSE(FileUtil::ReadLines(
-      "/apollo/modules/perception/testdata/lib/data/1.txt", nullptr));
-  EXPECT_TRUE(FileUtil::ReadLines(path, &lines));
-  EXPECT_EQ(lines.size(), 2u);
-}
-
-TEST(FileUtilTest, TestRemoveFileSuffix) {
-  EXPECT_EQ(FileUtil::RemoveFileSuffix(
-                "/apollo/modules/perception/testdata/lib/data/1.txt"),
-            "1");
-  EXPECT_EQ(FileUtil::RemoveFileSuffix("/home/111"), "111");
-}
-
 TEST(FileUtilTest, TestGetFileList) {
   std::string path = "/apollo/modules/perception/testdata/lib/data";
   std::vector<std::string> files;
-  EXPECT_TRUE(FileUtil::GetFileList(path, &files));
-  EXPECT_FALSE(FileUtil::GetFileList("/not_exist_path", &files));
+  EXPECT_TRUE(FileUtil::GetFileList(path, "", &files));
+  EXPECT_FALSE(FileUtil::GetFileList("/not_exist_path", "", &files));
   EXPECT_TRUE(FileUtil::GetFileList(
       "/apollo/modules/perception/testdata/lib/data", "txt", &files));
-}
-
-TEST(FileUtilTest, TestNumLines) {
-  EXPECT_EQ(
-      FileUtil::NumLines("/apollo/modules/perception/testdata/lib/data/1.txt"),
-      3);
-  EXPECT_EQ(
-      FileUtil::NumLines("/apollo/modules/perception/testdata/lib/data/11.txt"),
-      -1);
-}
-
-TEST(FileUtilTest, TestRenameFile) {
-  EXPECT_TRUE(FileUtil::RenameFile(
-      "/apollo/modules/perception/testdata/lib/data2/123.txt",
-      "/apollo/modules/perception/testdata/lib/data2/321.txt"));
-  EXPECT_TRUE(FileUtil::RenameFile(
-      "/apollo/modules/perception/testdata/lib/data2/321.txt",
-      "/apollo/modules/perception/testdata/lib/data2/123.txt"));
-  EXPECT_FALSE(FileUtil::RenameFile(
-      "/apollo/modules/perception/testdata/lib/data2/111.txt",
-      "/apollo/modules/perception/testdata/lib/data2/222.txt"));
 }
 
 }  // namespace lib
