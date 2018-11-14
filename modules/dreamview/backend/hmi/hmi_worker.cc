701bbca44c0e7f58f07a1d4f94cd4f604e7029a0
Xiangquan Xiao <xiaoxiangquan@gmail.com>
2018-11-14 10:07:00 -0800

    Update hmi_worker.cc

--- modules/dreamview/backend/hmi/hmi_worker.cc
+++ modules/dreamview/backend/hmi/hmi_worker.cc
@@ -176,8 +176,9 @@ HMIMode HMIWorker::LoadMode(const std::string& mode_config_path) {
 
     // Construct start_command: nohup mainboard -s <schedule> -d <dag> ... &
     module.set_start_command("nohup mainboard");
-    if (!cyber_module.schedule().empty()) {
-      StrAppend(module.mutable_start_command(), " -s ", cyber_module.schedule());
+    const auto& schedule = cyber_module.schedule();
+    if (!schedule.empty()) {
+      StrAppend(module.mutable_start_command(), " -s ", schedule);
     }
     for (const std::string& dag : cyber_module.dag_files()) {
       StrAppend(module.mutable_start_command(), " -d ", dag);
