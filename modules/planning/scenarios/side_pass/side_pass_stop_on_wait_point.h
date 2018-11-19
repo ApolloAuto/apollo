58cb22313a2372b9ce3caf2fea64eb90d9d519e3
panjiacheng <panjiacheng@yahoo.com>
2018-11-19 15:50:14 -0800

    Planning: Changed names to better reflect thge actual functionality. Also replaced pass-by-reference with pointer.

--- modules/planning/scenarios/side_pass/side_pass_stop_on_wait_point.h
+++ modules/planning/scenarios/side_pass/side_pass_stop_on_wait_point.h
@@ -66,10 +66,10 @@ class SidePassStopOnWaitPoint : public Stage {
       const IndexedList<std::string, Obstacle>& indexed_obstacle_list,
       const common::PathPoint& first_path_point,
       const common::PathPoint& last_path_point);
-  bool GetTheSOfNearestObstacle(
+  bool GetTheNearestObstacle(
       const ReferenceLine& reference_line,
       const IndexedList<std::string, Obstacle>& indexed_obstacle_list,
-      const Obstacle*& nearest_obstacle);
+      const Obstacle** nearest_obstacle);
   bool GetMoveForwardLastPathPoint(
       const ReferenceLine& reference_line,
       const Obstacle* nearest_obstacle,
