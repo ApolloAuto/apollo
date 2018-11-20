b39782e92108242257d5faef9e23b6f2a4729d9d
GoLancer <glacierhw@163.com>
2018-11-19 16:19:21 -0800

    framework: avoid creating temporary object while getting module name (#1889)

--- cyber/logger/logger_util.h
+++ cyber/logger/logger_util.h
@@ -83,14 +83,14 @@ inline int32_t MaxLogSize() {
   return (FLAGS_max_log_size > 0 ? FLAGS_max_log_size : 1);
 }
 
-static void FindModuleName(std::string* log_message, std::string* module_name) {
+inline void FindModuleName(std::string* log_message, std::string* module_name) {
   auto lpos = log_message->find('[');
   if (lpos != std::string::npos) {
     auto rpos = log_message->find(']', lpos);
     if (rpos != std::string::npos) {
-      *module_name = log_message->substr(lpos + 1, rpos - lpos - 1);
+      module_name->assign(*log_message, lpos + 1, rpos - lpos - 1);
       auto cut_length = rpos - lpos + 1;
-      log_message->replace(lpos, cut_length, "");
+      log_message->erase(lpos, cut_length);
     }
   }
   if (module_name->empty()) {
