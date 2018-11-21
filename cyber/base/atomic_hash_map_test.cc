a21a54b294a5f89c085349b48ce06e841dce8276
fengkaiwen01 <fengkaiwen01@baidu.com>
2018-11-21 15:52:51 +0800

    framework: rm function Remove

--- cyber/base/atomic_hash_map_test.cc
+++ cyber/base/atomic_hash_map_test.cc
@@ -31,10 +31,6 @@ TEST(AtomicHashMapTest, int_int) {
     EXPECT_TRUE(map.Has(i));
     EXPECT_TRUE(map.Get(i, &value));
     EXPECT_EQ(i, value);
-    EXPECT_TRUE(map.Remove(i));
-    EXPECT_FALSE(map.Has(i));
-    EXPECT_FALSE(map.Get(i, &value));
-    EXPECT_FALSE(map.Remove(i));
   }
 
   for (int i = 0; i < 1000; i++) {
@@ -42,10 +38,6 @@ TEST(AtomicHashMapTest, int_int) {
     EXPECT_TRUE(map.Has(1000 - i));
     EXPECT_TRUE(map.Get(1000 - i, &value));
     EXPECT_EQ(i, value);
-    EXPECT_TRUE(map.Remove(1000 - i));
-    EXPECT_FALSE(map.Has(1000 - i));
-    EXPECT_FALSE(map.Get(1000 - i, &value));
-    EXPECT_FALSE(map.Remove(1000 - i));
   }
 }
 
@@ -57,10 +49,6 @@ TEST(AtomicHashMapTest, int_str) {
     EXPECT_TRUE(map.Has(i));
     EXPECT_TRUE(map.Get(i, &value));
     EXPECT_EQ(std::to_string(i), value);
-    EXPECT_TRUE(map.Remove(i));
-    EXPECT_FALSE(map.Has(i));
-    EXPECT_FALSE(map.Get(i, &value));
-    EXPECT_FALSE(map.Remove(i));
   }
   map.Set(100);
   EXPECT_TRUE(map.Get(100, &value));
@@ -71,7 +59,7 @@ TEST(AtomicHashMapTest, int_str) {
 }
 
 TEST(AtomicHashMapTest, concurrency) {
-  AtomicHashMap<int, std::string, 1024> map;
+  AtomicHashMap<int, std::string, 128> map;
   int thread_num = 32;
   std::thread t[32];
   volatile bool ready = false;
@@ -83,7 +71,6 @@ TEST(AtomicHashMapTest, concurrency) {
       }
       for (int j = 0; j < thread_num * 1024; j++) {
         auto j_str = std::to_string(j);
-        map.Remove(j);
         map.Set(j);
         map.Set(j, j_str);
         map.Set(j, std::move(std::to_string(j)));
