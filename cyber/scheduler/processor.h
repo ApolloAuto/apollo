ee6073247f83fc5aa676587f3b2564d0944657f0
Dengchengliang <ahlong2008@hotmail.com>
2018-11-26 20:27:07 -0800

    Revert "framkework: opt mainboard exit order (#1945)"
    This reverts commit 674b27fbded741766bf637b24cd37e621bf6954a.

--- cyber/scheduler/processor.h
+++ cyber/scheduler/processor.h
@@ -46,15 +46,7 @@ class Processor {
   void Run();
   void SetAffinity(const std::vector<int>&, const std::string&, int);
   void SetSchedPolicy(std::string spolicy, int sched_priority);
-  void Stop() {
-    if (!running_.exchange(false)) {
-      return;
-    }
-
-    if (thread_.joinable()) {
-      thread_.join();
-    }
-  }
+  void Stop() { running_.exchange(false); }
 
   void BindContext(const std::shared_ptr<ProcessorContext>& context) {
     context_ = context;
