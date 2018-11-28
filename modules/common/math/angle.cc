1b3d66f1435ae1232b96694cf5cde7058361e167
Liangliang Zhang <zhangliangliang@baidu.com>
2018-11-28 14:31:53 -0800

    Planning: fixed angle.h/cc code issues and warnings.

--- modules/common/math/angle.cc
+++ modules/common/math/angle.cc
@@ -23,9 +23,10 @@ namespace common {
 namespace math {
 
 float sin(Angle16 a) {
-  auto idx = a.raw();
+  int16_t idx = a.raw();
+
   if (idx < -Angle16::RAW_PI_2) {
-    idx += Angle16::RAW_PI;
+    idx = static_cast<int16_t>(idx + Angle16::RAW_PI);
     return -SIN_TABLE[idx % SIN_TABLE_SIZE];
   }
   if (idx < 0) {
@@ -34,29 +35,29 @@ float sin(Angle16 a) {
   if (idx < Angle16::RAW_PI_2) {
     return SIN_TABLE[idx % SIN_TABLE_SIZE];
   }
-  idx = Angle16::RAW_PI - idx;
+  idx = static_cast<int16_t>(Angle16::RAW_PI - idx);
   return SIN_TABLE[idx % SIN_TABLE_SIZE];
 }
 
 float cos(Angle16 a) {
-  Angle16 b(Angle16::RAW_PI_2 - a.raw());
+  Angle16 b(static_cast<int16_t>(Angle16::RAW_PI_2 - a.raw()));
   return sin(b);
 }
 
 float tan(Angle16 a) { return sin(a) / cos(a); }
 
 float sin(Angle8 a) {
-  Angle16 b(a.raw() << 8);
+  Angle16 b(static_cast<int16_t>(a.raw() << 8));
   return sin(b);
 }
 
 float cos(Angle8 a) {
-  Angle16 b(a.raw() << 8);
+  Angle16 b(static_cast<int16_t>(a.raw() << 8));
   return cos(b);
 }
 
 float tan(Angle8 a) {
-  Angle16 b(a.raw() << 8);
+  Angle16 b(static_cast<int16_t>(a.raw() << 8));
   return tan(b);
 }
 
