5afefa7e288238dfbcc962ba4b359909d1bc6a8c
gruminions <jcxcnzj@gmail.com>
2018-11-29 15:15:42 +0800

    framework: add static CleanUp interface for DECLARE_SINGLETON

--- cyber/base/macros.h
+++ cyber/base/macros.h
@@ -18,6 +18,7 @@
 #define CYBER_BASE_MACROS_H_
 
 #include <cstdlib>
+#include <new>
 
 #if __GNUC__ >= 3
 #define likely(x) (__builtin_expect((x), 1))
@@ -28,6 +29,22 @@
 #endif
 
 #define CACHELINE_SIZE 64
+
+#define DEFINE_TYPE_TRAIT(name, func)                            \
+  template <typename T>                                          \
+  class name {                                                   \
+   private:                                                      \
+    template <typename Class>                                    \
+    static char Test(decltype(&Class::func)*);                   \
+    template <typename>                                          \
+    static int Test(...);                                        \
+                                                                 \
+   public:                                                       \
+    static constexpr bool value = sizeof(Test<T>(nullptr)) == 1; \
+  };                                                             \
+                                                                 \
+  template <typename T>                                          \
+  constexpr bool name<T>::value;
 
 inline void cpu_relax() { asm volatile("rep; nop" ::: "memory"); }
 
