cea9782df37efafba9a6d47068b06159ec4d4bc2
Liangliang Zhang <zhangliangliang@baidu.com>
2018-11-30 16:52:07 -0800

    Planning: removed unused code.

--- modules/planning/toolkits/optimizers/qp_spline_st_speed/qp_spline_st_speed_optimizer.cc
+++ modules/planning/toolkits/optimizers/qp_spline_st_speed/qp_spline_st_speed_optimizer.cc
@@ -96,41 +96,6 @@ Status QpSplineStSpeedOptimizer::Process(const SLBoundary& adc_sl_boundary,
     if (!obstacle->st_boundary().IsEmpty()) {
       path_decision->Find(id)->SetBlockingObstacle(true);
       boundaries.push_back(&obstacle->st_boundary());
-    } else if (FLAGS_enable_side_vehicle_st_boundary &&
-               (adc_sl_boundary.start_l() > 2.0 ||
-                adc_sl_boundary.end_l() < -2.0)) {
-      if (obstacle->IsVirtual()) {
-        continue;
-      }
-      if (path_decision->Find(id)->reference_line_st_boundary().IsEmpty()) {
-        continue;
-      }
-      auto st_boundary_copy =
-          path_decision->Find(id)->reference_line_st_boundary();
-      auto st_boundary = st_boundary_copy.CutOffByT(3.5);
-      if (!st_boundary.IsEmpty()) {
-        auto decision = obstacle->LongitudinalDecision();
-        if (decision.has_yield()) {
-          st_boundary.SetBoundaryType(StBoundary::BoundaryType::YIELD);
-        } else if (decision.has_overtake()) {
-          st_boundary.SetBoundaryType(StBoundary::BoundaryType::OVERTAKE);
-        } else if (decision.has_follow()) {
-          st_boundary.SetBoundaryType(StBoundary::BoundaryType::FOLLOW);
-        } else if (decision.has_stop()) {
-          st_boundary.SetBoundaryType(StBoundary::BoundaryType::STOP);
-        } else if (decision.has_ignore()) {
-          continue;
-        } else {
-          AWARN << "Obstacle " << id << " has unhandled decision type: "
-                << decision.ShortDebugString();
-        }
-        st_boundary.SetId(st_boundary_copy.id());
-        st_boundary.SetCharacteristicLength(
-            st_boundary_copy.characteristic_length());
-
-        path_decision->SetStBoundary(id, st_boundary);
-        boundaries.push_back(&obstacle->st_boundary());
-      }
     }
   }
 
