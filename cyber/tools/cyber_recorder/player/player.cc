c98c555bd7b2b51efb57e79bb895724928f6b96b
gruminions <jcxcnzj@gmail.com>
2018-12-05 19:31:00 +0800

    framework: fix read blocking

--- cyber/tools/cyber_recorder/player/player.cc
+++ cyber/tools/cyber_recorder/player/player.cc
@@ -60,8 +60,8 @@ static char Getch() {
   if (tcgetattr(0, &old) < 0) perror("tcsetattr()");
   old.c_lflag &= ~ICANON;
   old.c_lflag &= ~ECHO;
-  old.c_cc[VMIN] = 1;
-  old.c_cc[VTIME] = 0;
+  old.c_cc[VMIN] = 0;
+  old.c_cc[VTIME] = 1;
   if (tcsetattr(0, TCSANOW, &old) < 0) perror("tcsetattr ICANON");
   if (read(0, &buf, 1) < 0) perror("read()");
   old.c_lflag |= ICANON;
