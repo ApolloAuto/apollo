6cb5827a5ab87bd2027a2a926970b8dc53702ddb
Jiangtao Hu <boris.hu@gmail.com>
2018-12-09 09:09:02 -0800

    control: retire enable_persistent_estop gflag for it is moved to control config file.

--- modules/control/common/control_gflags.h
+++ modules/control/common/control_gflags.h
@@ -50,8 +50,6 @@ DECLARE_double(lock_steer_speed);
 DECLARE_bool(enable_navigation_mode_error_filter);
 DECLARE_bool(enable_navigation_mode_position_update);
 
-DECLARE_bool(enable_persistent_estop);
-
 DECLARE_int32(chassis_pending_queue_size);
 DECLARE_int32(planning_pending_queue_size);
 DECLARE_int32(localization_pending_queue_size);
