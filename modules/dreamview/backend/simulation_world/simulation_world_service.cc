1d146b277d97b5198db09b20a0d19a599c662f4a
Aaron Xiao <xiaoxiangquan@gmail.com>
2018-12-10 13:06:15 -0800

    Fix lint.

--- modules/dreamview/backend/simulation_world/simulation_world_service.cc
+++ modules/dreamview/backend/simulation_world/simulation_world_service.cc
@@ -234,7 +234,7 @@ void DownsampleCurve(Curve *curve) {
   // Downsample points by angle then by distance.
   std::vector<size_t> sampled_indices =
       DownsampleByAngle(points, kAngleThreshold);
-  for (const size_t index: sampled_indices) {
+  for (const size_t index : sampled_indices) {
     auto *point = line_segment->add_point();
     point->set_x(points[index].x());
     point->set_y(points[index].y());
