37eb332b286da4a2e212e1066d2057516e3a877c
Jiangtao Hu <boris.hu@gmail.com>
2018-12-10 16:27:24 -0800

    planning: fix compile warning.

--- modules/planning/reference_line/qp_spline_reference_line_smoother_test.cc
+++ modules/planning/reference_line/qp_spline_reference_line_smoother_test.cc
@@ -74,7 +74,7 @@ class QpSplineReferenceLineSmootherTest : public ::testing::Test {
 
 TEST_F(QpSplineReferenceLineSmootherTest, smooth) {
   ReferenceLine smoothed_reference_line;
-  EXPECT_FLOAT_EQ(153.87421, reference_line_->Length());
+  EXPECT_DOUBLE_EQ(153.87421245682503, reference_line_->Length());
   std::vector<AnchorPoint> anchor_points;
   const double interval = 10.0;
   int num_of_anchors =
