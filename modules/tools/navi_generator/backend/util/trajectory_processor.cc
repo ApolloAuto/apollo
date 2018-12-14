c5845aa69a486419e90b3b2e8fdd6a04e3304adb
wmz <wmzhen1111@163.com>
2018-12-14 15:08:36 +0800

    NaviGenerator: Implemented the SaveFilesToDatabase of TrajectoryProcessor.

--- modules/tools/navi_generator/backend/util/trajectory_processor.cc
+++ modules/tools/navi_generator/backend/util/trajectory_processor.cc
@@ -31,6 +31,7 @@
 #include "modules/tools/navi_generator/backend/common/navi_generator_gflags.h"
 #include "modules/tools/navi_generator/backend/util/file_operator.h"
 #include "modules/tools/navi_generator/backend/util/navigation_expander.h"
+#include "modules/tools/navi_generator/backend/util/quad_tiles_maker.h"
 
 namespace apollo {
 namespace navi_generator {
@@ -96,7 +97,132 @@ bool TrajectoryProcessor::ProcessBagFileSegment(
 }
 
 bool TrajectoryProcessor::SaveFilesToDatabase() {
-  // TODO(*): Save all data to database and inform the frontend the result.
+  DBOperator db_operator;
+  Way last_way;
+  bool res = true;
+  for (auto item = processed_file_info_s_.begin();
+       item != processed_file_info_s_.end();) {
+    Way way;
+    if (!SaveWay(&db_operator, last_way, item->second, &way)) {
+      res = false;
+      break;
+    }
+
+    if (!SaveWayNodes(&db_operator, way.way_id, item->second)) {
+      res = false;
+      break;
+    }
+
+    if (!SaveWayData(&db_operator, way.way_id, item->second)) {
+      res = false;
+      break;
+    }
+
+    if (!SaveNaviData(&db_operator, way.way_id, item->second)) {
+      res = false;
+      break;
+    }
+
+    last_way = way;
+    item = processed_file_info_s_.erase(item);
+  }
+  // TODO(*): Inform the frontend the result.
+  return res;
+}
+
+bool TrajectoryProcessor::SaveWay(DBOperator* const db_operator,
+                                  const Way& last_way,
+                                  const FileInfo& file_info, Way* const way) {
+  if (file_info.bag_file_info.file_index == 0) {
+    if (!db_operator->CreateNewWayId(&(way->way_id))) {
+      return false;
+    }
+    way->pre_way_id = 0;
+  } else {
+    way->way_id = last_way.way_id + 1;
+    way->pre_way_id = (last_way.next_way_id == 0) ? 0 : last_way.way_id;
+  }
+  way->next_way_id = file_info.bag_file_info.next_raw_file_name.empty()
+                         ? 0
+                         : (way->way_id + 1);
+  way->speed_min = file_info.speed_min;
+  way->speed_max = file_info.speed_max;
+  if (!db_operator->SaveWay(*way)) {
+    return false;
+  }
+  return true;
+}
+
+bool TrajectoryProcessor::SaveWayNodes(DBOperator* const db_operator,
+                                       const std::uint64_t way_id,
+                                       const FileInfo& file_info) {
+  NaviFile rightmost_navi_file;
+  if (!GetRightmostNaviFile(file_info, &rightmost_navi_file)) {
+    return false;
+  }
+  std::vector<planning::ReferencePoint> smoothed_points;
+  FileOperator file_operator;
+  if (!file_operator.Import(rightmost_navi_file.navi_file_name,
+                            &smoothed_points)) {
+    return false;
+  }
+  std::vector<apollo::localization::msf::WGS84Corr> waypoints;
+  TrajectoryConverter trajecotry_converter;
+  trajecotry_converter.ConvertSmoothedTrajectoryPointsToWGS84(&smoothed_points,
+                                                              &waypoints);
+  WayNodes way_nodes;
+  if (!FindWayNodes(way_id, &waypoints, &way_nodes)) {
+    return false;
+  }
+  if (!db_operator->SaveWayNodes(way_nodes)) {
+    return false;
+  }
+  return true;
+}
+
+bool TrajectoryProcessor::SaveWayData(DBOperator* const db_operator,
+                                      const std::uint64_t way_id,
+                                      const FileInfo& file_info) {
+  WayData way_data;
+  way_data.way_id = way_id;
+  std::vector<unsigned char> raw_data;
+  FileOperator file_operator;
+  if (!file_operator.Import(file_info.bag_file_info.raw_file_name, &raw_data)) {
+    AERROR << "import failed.";
+    return false;
+  }
+  way_data.raw_data = raw_data;
+  way_data.navi_number =
+      common_file_info_.left_lane_num + common_file_info_.right_lane_num + 1;
+  // TODO(*): save the navi_data in correct db table when saving in sub table
+  way_data.navi_table_id = 0;
+  if (!db_operator->SaveWayData(way_data)) {
+    return false;
+  }
+  return true;
+}
+
+bool TrajectoryProcessor::SaveNaviData(DBOperator* const db_operator,
+                                       const std::uint64_t way_id,
+                                       const FileInfo& file_info) {
+  NaviInfo navi_info;
+  navi_info.way_id = way_id;
+  std::vector<NaviData> navi_data;
+  for (auto& navi_file : file_info.navi_files) {
+    NaviData navi;
+    navi.navi_index = navi_file.navi_index;
+    std::vector<unsigned char> data;
+    FileOperator file_operator;
+    if (!file_operator.Import(navi_file.navi_file_name, &data)) {
+      return false;
+    }
+    navi.data = data;
+    navi_data.emplace_back(navi);
+  }
+  navi_info.navi_data = navi_data;
+  if (!db_operator->SaveNaviData(navi_info)) {
+    return false;
+  }
   return true;
 }
 
@@ -210,8 +336,6 @@ bool TrajectoryProcessor::ProcessBagFile(
   return true;
 }
 
-
-
 bool TrajectoryProcessor::ExpandNaviFiles(
     const std::string& src_smoothed_file_name,
     std::vector<NaviFile>* const navi_files) {
@@ -229,6 +353,52 @@ bool TrajectoryProcessor::ExpandNaviFiles(
     navi_file.navi_file_name = item.file_name;
     navi_files->emplace_back(navi_file);
   }
+  return true;
+}
+
+bool TrajectoryProcessor::FindWayNodes(
+    const std::uint64_t way_id,
+    const std::vector<apollo::localization::msf::WGS84Corr>* const waypoints,
+    WayNodes* const way_nodes) {
+  CHECK_NOTNULL(waypoints);
+  QuadTilesMaker tile_maker;
+  QuadTile quad_tile;
+  std::uint64_t node_index = 0;
+  std::uint64_t line_number = 0;
+  std::string node_value;
+  std::vector<Node> nodes;
+  way_nodes->way_id = way_id;
+  for (auto& waypoint : *waypoints) {
+    ++line_number;
+    tile_maker.MakeQuadTile(waypoint.lat, waypoint.log, -1,
+                            FLAGS_quadtile_zoom_level, &quad_tile);
+    tile_maker.IdAsString(FLAGS_quadtile_zoom_level, &quad_tile, &node_value);
+    Node node = {node_index, line_number, node_value};
+    auto node_itr = std::find_if(
+        nodes.begin(), nodes.end(),
+        [&node](const Node& obj) { return obj.node_value == node.node_value; });
+    if (node_itr == nodes.end()) {
+      nodes.emplace_back(node);
+      ++node_index;
+    }
+  }
+  way_nodes->nodes = nodes;
+  return true;
+}
+
+bool TrajectoryProcessor::GetRightmostNaviFile(const FileInfo& file_info,
+                                               NaviFile* const navi_file) {
+  auto navi_index = 0;
+  auto result = std::find_if(std::begin(file_info.navi_files),
+                             std::end(file_info.navi_files),
+                             [&navi_index](const NaviFile& navi_file_comp) {
+                               return navi_file_comp.navi_index == navi_index;
+                             });
+  if (result == std::end(file_info.navi_files)) {
+    return false;
+  }
+  navi_file->navi_index = result->navi_index;
+  navi_file->navi_file_name = result->navi_file_name;
   return true;
 }
 
