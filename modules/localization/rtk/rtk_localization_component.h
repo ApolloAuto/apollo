3895b0b401636057256cc5bf840a8b9987a6fcaf
Yao Zhou <31580760+zhouyao4321@users.noreply.github.com>
2018-12-18 17:47:40 -0800

    rtk mode add localization status (#6204)

--- modules/localization/rtk/rtk_localization_component.h
+++ modules/localization/rtk/rtk_localization_component.h
@@ -25,6 +25,7 @@
 #include "cyber/cyber.h"
 #include "cyber/message/raw_message.h"
 
+#include "modules/drivers/gnss/proto/ins.pb.h"
 #include "modules/localization/proto/gps.pb.h"
 #include "modules/localization/proto/imu.pb.h"
 #include "modules/localization/proto/localization.pb.h"
@@ -45,24 +46,30 @@ class RTKLocalizationComponent final
 
   bool Proc(const std::shared_ptr<localization::Gps> &gps_msg) override;
 
-  void ImuCallback(const std::shared_ptr<localization::CorrectedImu> &imu_msg);
-
  private:
   bool InitConfig();
   bool InitIO();
 
   void PublishPoseBroadcastTF(const LocalizationEstimate &localization);
   void PublishPoseBroadcastTopic(const LocalizationEstimate &localization);
+  void PublishLocalizationStatus(
+      const LocalizationStatus& localization_status);
 
  private:
   std::shared_ptr<cyber::Reader<localization::CorrectedImu>>
       corrected_imu_listener_ = nullptr;
+  std::shared_ptr<cyber::Reader<drivers::gnss::InsStat>>
+      gps_status_listener_ = nullptr;
 
   std::shared_ptr<cyber::Writer<LocalizationEstimate>>
       localization_talker_ = nullptr;
+  std::shared_ptr<cyber::Writer<LocalizationStatus>>
+      localization_status_talker_ = nullptr;
 
   std::string localization_topic_ = "";
+  std::string localization_status_topic_ = "";
   std::string gps_topic_ = "";
+  std::string gps_status_topic_ = "";
   std::string imu_topic_ = "";
 
   std::string broadcast_tf_frame_id_ = "";
