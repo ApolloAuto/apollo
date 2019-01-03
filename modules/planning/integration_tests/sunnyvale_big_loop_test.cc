40d008f3b7b193fa3f1310ea3f27bf68c66ba01e
Jiangtao Hu <boris.hu@gmail.com>
2019-01-03 13:48:30 -0800

    planning: fix integration test.

--- modules/planning/integration_tests/sunnyvale_big_loop_test.cc
+++ modules/planning/integration_tests/sunnyvale_big_loop_test.cc
@@ -53,6 +53,7 @@ class SunnyvaleBigLoopTest : public PlanningTestBase {
     FLAGS_enable_scenario_side_pass = false;
     FLAGS_enable_scenario_stop_sign_unprotected = false;
     FLAGS_enable_scenario_traffic_light_right_turn_unprotected = false;
+    FLAGS_enable_rss_info = false;
 
     ENABLE_RULE(TrafficRuleConfig::CROSSWALK, false);
     ENABLE_RULE(TrafficRuleConfig::DESTINATION, false);
