978816254154de846c971a5321c313236754bca9
David Hopper <davidhopper@qq.com>
2019-01-04 11:12:12 +0800

    Common: added a helper function that determines if two floating-point numbers are equal: IsFloatEqual.

--- modules/common/util/util.h
+++ modules/common/util/util.h
@@ -23,6 +23,7 @@
 
 #include <algorithm>
 #include <iostream>
+#include <limits>
 #include <memory>
 #include <string>
 #include <unordered_set>
@@ -251,6 +252,18 @@ void QuaternionToRotationMatrix(const T* quat, T* R) {
   R[5] = 2 * (yz - rx);
 }
 
+// Test whether two float or double numbers are equal.
+// ulp: units in the last place.
+template <typename T>
+typename std::enable_if<!std::numeric_limits<T>::is_integer, bool>::type
+IsFloatEqual(T x, T y, int ulp = 2) {
+  // the machine epsilon has to be scaled to the magnitude of the values used
+  // and multiplied by the desired precision in ULPs (units in the last place)
+  return std::fabs(x - y) <
+             std::numeric_limits<T>::epsilon() * std::fabs(x + y) * ulp
+         // unless the result is subnormal
+         || std::fabs(x - y) < std::numeric_limits<T>::min();
+}
 }  // namespace util
 }  // namespace common
 }  // namespace apollo
