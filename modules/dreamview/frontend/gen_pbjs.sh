b4009d8c934650ce207cee49f6916eb1c6861ade
vlin17 <thunder0504@gmail.com>
2019-01-03 18:08:13 -0800

    Dreamview: reuse existent ObstaclePriority proto definition

--- modules/dreamview/frontend/gen_pbjs.sh
+++ modules/dreamview/frontend/gen_pbjs.sh
@@ -23,6 +23,7 @@ COMMON_PROTOS='../../common/proto/*.proto'
 LOCALIZATION_PROTOS='../../localization/proto/localization.proto ../../localization/proto/pose.proto ../../localization/proto/localization_status.proto'
 CHASSIS_PROTOS='../../canbus/proto/chassis.proto'
 PLANNING_PROTOS='../../planning/proto/*.proto'
+PREDICTION_PROTOS='../../prediction/proto/feature.proto ../../prediction/proto/lane_graph.proto'
 PERCEPTION_PROTOS='../../perception/proto/traffic_light_detection.proto ../../perception/proto/perception_obstacle.proto'
 REALTIVE_MAP_PROTOS='../../map/relative_map/proto/*.proto'
 MAP_PROTOS='../../map/proto/*.proto'
@@ -32,7 +33,7 @@ ROUTING_PROTOS='../../routing/proto/routing.proto'
 node_modules/protobufjs/bin/pbjs -t json ../proto/simulation_world.proto ../proto/chart.proto \
     $COMMON_PROTOS $LOCALIZATION_PROTOS $CHASSIS_PROTOS $PLANNING_PROTOS \
     $PERCEPTION_PROTOS $MONITOR_PROTOS $ROUTING_PROTOS $MAP_PROTOS \
-    $REALTIVE_MAP_PROTOS \
+    $PREDICTION_PROTOS $REALTIVE_MAP_PROTOS \
     -o proto_bundle/sim_world_proto_bundle.json
 
 node_modules/protobufjs/bin/pbjs -t json ../proto/point_cloud.proto \
