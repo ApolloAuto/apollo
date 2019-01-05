fc87a624c15cba3f6d81cde2b209ea4bccaf0756
Vivian Lin <2775888+vlin17@users.noreply.github.com>
2019-01-04 22:10:47 -0800

    Dreamview: improved 3D scene view angle and obstacle's text positions (#6402)

--- modules/dreamview/frontend/dist/app.bundle.js
+++ modules/dreamview/frontend/dist/app.bundle.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0622ebfb505924d2dde50a4847b1ad5e01bda50dd62f4c8f3c200a61c445d304
-size 1821609
+oid sha256:dff876ee66ff3b341478466f2631fb0d638e067969cf4c3ce72a79fca2564d8b
+size 1821795
