f55de2f4bd56ec0091d6f9d0aa4750b65c322dd6
Shu Jiang <shujiang2016@gmail.com>
2019-01-08 15:26:20 -0800

    correct test file for llc auxillaryfeedback_120

--- modules/canbus/vehicle/transit/protocol/llc_auxiliaryfeedback_120_test.cc
+++ modules/canbus/vehicle/transit/protocol/llc_auxiliaryfeedback_120_test.cc
@@ -35,40 +35,67 @@ class llc_auxiliaryfeedback_120Test : public ::testing ::Test {
   Llcauxiliaryfeedback120 Llcauxiliary_feedback120_;
 };
 
-const std::uint8_t bytes = 0xFF;
-std::int32_t length = 1;
-
-TEST_F(llc_auxiliaryfeedback_120Test, inverter) {
-  EXPECT_EQ(Llcauxiliary_feedback120_.llc_fbk_inverter(&bytes, length), true);
+TEST_F(llc_auxiliaryfeedback_120Test, General) {
+  const std::uint8_t kBytes = 0xFF;
+  std::int32_t length = 1;
+  EXPECT_TRUE(Llcauxiliary_feedback120_.llc_fbk_inverter(&kBytes, length));
+  EXPECT_FALSE(Llcauxiliary_feedback120_.llc_fbk_pdu_ch8(&kBytes, length));
+  EXPECT_FALSE(Llcauxiliary_feedback120_.llc_fbk_pdu_ch7(&kBytes, length));
+  EXPECT_FALSE(Llcauxiliary_feedback120_.llc_fbk_pdu_ch6(&kBytes, length));
+  EXPECT_FALSE(Llcauxiliary_feedback120_.llc_fbk_pdu_ch5(&kBytes, length));
+  EXPECT_FALSE(Llcauxiliary_feedback120_.llc_fbk_pdu_ch4(&kBytes, length));
+  EXPECT_FALSE(Llcauxiliary_feedback120_.llc_fbk_pdu_ch3(&kBytes, length));
+  EXPECT_FALSE(Llcauxiliary_feedback120_.llc_fbk_pdu_ch2(&kBytes, length));
+  EXPECT_TRUE(Llcauxiliary_feedback120_.llc_fbk_pdu_ch1(&kBytes, length));
+  EXPECT_FALSE(Llcauxiliary_feedback120_.llc_fbk_hazardlights(&kBytes, length));
+  EXPECT_FALSE(Llcauxiliary_feedback120_.llc_fbk_ledgreenon(&kBytes, length));
+  EXPECT_TRUE(Llcauxiliary_feedback120_.llc_fbk_horn(&kBytes, length));
+  EXPECT_FALSE(Llcauxiliary_feedback120_.llc_fbk_buzzeron(&kBytes, length));
+  EXPECT_FALSE(Llcauxiliary_feedback120_.llc_fbk_turnsignal(&kBytes, length));
+  EXPECT_FALSE(Llcauxiliary_feedback120_.llc_fbk_lowbeam(&kBytes, length));
+  EXPECT_FALSE(Llcauxiliary_feedback120_.llc_fbk_highbeam(&kBytes, length));
+  EXPECT_FALSE(Llcauxiliary_feedback120_.llc_fbk_ledredon(&kBytes, length));
+  EXPECT_FALSE(
+      Llcauxiliary_feedback120_.llc_fbk_autonomybuttonpressed(&kBytes, length));
 }
 
-TEST_F(llc_auxiliaryfeedback_120Test, ch) {
-  EXPECT_EQ(Llcauxiliary_feedback120_.llc_fbk_pdu_ch8(&bytes, length), false);
-  EXPECT_EQ(Llcauxiliary_feedback120_.llc_fbk_pdu_ch7(&bytes, length), false);
-  EXPECT_EQ(Llcauxiliary_feedback120_.llc_fbk_pdu_ch6(&bytes, length), false);
-  EXPECT_EQ(Llcauxiliary_feedback120_.llc_fbk_pdu_ch5(&bytes, length), false);
-  EXPECT_EQ(Llcauxiliary_feedback120_.llc_fbk_pdu_ch4(&bytes, length), false);
-  EXPECT_EQ(Llcauxiliary_feedback120_.llc_fbk_pdu_ch3(&bytes, length), false);
-  EXPECT_EQ(Llcauxiliary_feedback120_.llc_fbk_pdu_ch2(&bytes, length), false);
-  EXPECT_EQ(Llcauxiliary_feedback120_.llc_fbk_pdu_ch1(&bytes, length), false);
-}
+// TEST_F(llc_auxiliaryfeedback_120Test, ch) {
+//   const std::uint8_t kBytes = 0xFF;
+//   std::int32_t length = 1;
 
-TEST_F(llc_auxiliaryfeedback_120Test, light_horn) {
-  EXPECT_EQ(Llcauxiliary_feedback120_.llc_fbk_hazardlights(&bytes, length),
-            false);
-  EXPECT_EQ(Llcauxiliary_feedback120_.llc_fbk_ledgreenon(&bytes, length),
-            false);
-  EXPECT_EQ(Llcauxiliary_feedback120_.llc_fbk_horn(&bytes, length), true);
-  EXPECT_EQ(Llcauxiliary_feedback120_.llc_fbk_buzzeron(&bytes, length), false);
-  EXPECT_EQ(Llcauxiliary_feedback120_.llc_fbk_turnsignal(&bytes, length),
-            false);
-  EXPECT_EQ(Llcauxiliary_feedback120_.llc_fbk_lowbeam(&bytes, length), false);
-  EXPECT_EQ(Llcauxiliary_feedback120_.llc_fbk_highbeam(&bytes, length), false);
-  EXPECT_EQ(Llcauxiliary_feedback120_.llc_fbk_ledredon(&bytes, length), false);
-  EXPECT_EQ(
-      Llcauxiliary_feedback120_.llc_fbk_autonomybuttonpressed(&bytes, length),
-      false);
-}
+//   EXPECT_EQ(Llcauxiliary_feedback120_.llc_fbk_pdu_ch7(&bytes, length),
+//   false); EXPECT_EQ(Llcauxiliary_feedback120_.llc_fbk_pdu_ch6(&bytes,
+//   length), false);
+//   EXPECT_EQ(Llcauxiliary_feedback120_.llc_fbk_pdu_ch5(&bytes, length),
+//   false); EXPECT_EQ(Llcauxiliary_feedback120_.llc_fbk_pdu_ch4(&bytes,
+//   length), false);
+//   EXPECT_EQ(Llcauxiliary_feedback120_.llc_fbk_pdu_ch3(&bytes, length),
+//   false); EXPECT_EQ(Llcauxiliary_feedback120_.llc_fbk_pdu_ch2(&bytes,
+//   length), false);
+//   EXPECT_EQ(Llcauxiliary_feedback120_.llc_fbk_pdu_ch1(&bytes, length),
+//   false);
+// }
+
+// TEST_F(llc_auxiliaryfeedback_120Test, light_horn) {
+//   const std::uint8_t bytes = 0xFF;
+//   std::int32_t length = 1;
+//   EXPECT_EQ(Llcauxiliary_feedback120_.llc_fbk_hazardlights(&bytes, length),
+//             false);
+//   EXPECT_EQ(Llcauxiliary_feedback120_.llc_fbk_ledgreenon(&bytes, length),
+//             false);
+//   EXPECT_EQ(Llcauxiliary_feedback120_.llc_fbk_horn(&bytes, length), true);
+//   EXPECT_EQ(Llcauxiliary_feedback120_.llc_fbk_buzzeron(&bytes, length),
+//   false); EXPECT_EQ(Llcauxiliary_feedback120_.llc_fbk_turnsignal(&bytes,
+//   length),
+//             false);
+//   EXPECT_EQ(Llcauxiliary_feedback120_.llc_fbk_lowbeam(&bytes, length),
+//   false); EXPECT_EQ(Llcauxiliary_feedback120_.llc_fbk_highbeam(&bytes,
+//   length), false);
+//   EXPECT_EQ(Llcauxiliary_feedback120_.llc_fbk_ledredon(&bytes, length),
+//   false); EXPECT_EQ(
+//       Llcauxiliary_feedback120_.llc_fbk_autonomybuttonpressed(&bytes,
+//       length), false);
+// }
 
 }  // namespace transit
 }  // namespace canbus
