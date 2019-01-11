d40bc4ad032053e0cf9968bd9d241b5cbf615212
panjiacheng <panjiacheng@yahoo.com>
2019-01-10 18:01:30 -0800

    Prediction: implemented a general skeleton for lane-scanning evaluator.

--- modules/prediction/evaluator/vehicle/lane_scanning_evaluator.cc
+++ modules/prediction/evaluator/vehicle/lane_scanning_evaluator.cc
@@ -54,18 +54,88 @@ LaneScanningEvaluator::LaneScanningEvaluator() {
 }
 
 void LaneScanningEvaluator::Evaluate(Obstacle* obstacle_ptr) {
+  // Sanity checks.
+  CHECK_NOTNULL(obstacle_ptr);
+  int id = obstacle_ptr->id();
+  if (!obstacle_ptr->latest_feature().IsInitialized()) {
+    AERROR << "Obstacle [" << id << "] has no latest feature.";
+    return;
+  }
+  Feature* latest_feature_ptr = obstacle_ptr->mutable_latest_feature();
+  CHECK_NOTNULL(latest_feature_ptr);
+  if (!latest_feature_ptr->has_lane() ||
+      !latest_feature_ptr->lane().has_lane_graph_ordered()) {
+    AERROR << "Obstacle [" << id << "] has no lane graph.";
+    return;
+  }
+  LaneGraph* lane_graph_ptr =
+      latest_feature_ptr->mutable_lane()->mutable_lane_graph_ordered();
+  CHECK_NOTNULL(lane_graph_ptr);
+  if (lane_graph_ptr->lane_sequence_size() == 0) {
+    AERROR << "Obstacle [" << id << "] has no lane sequences.";
+    return;
+  }
+  ADEBUG << "There are " << lane_graph_ptr->lane_sequence_size()
+         << " lane sequences to scan.";
+
+  // Extract features, and:
+  //  - if in offline mode, save it locally for training.
+  //  - if in online mode, pass it through trained model to evaluate.
+  std::vector<double> feature_values;
+  ExtractFeatures(obstacle_ptr, lane_graph_ptr, &feature_values);
+  // TODO(jiacheng): sanity check of extracted feature_values:
+  if (FLAGS_prediction_offline_mode) {
+    // TODO(jiacheng): save the extracted features locally for offline training.
+  } else {
+    // TODO(jiacheng): once the model is trained, implement this online part.
+  }
 }
 
-void LaneScanningEvaluator::ExtractFeatures(
-    const Obstacle* obstacle_ptr, std::vector<double>* feature_values) {
+bool LaneScanningEvaluator::ExtractFeatures(
+    const Obstacle* obstacle_ptr, const LaneGraph* lane_graph_ptr,
+    std::vector<double>* feature_values) {
+  // Sanity checks.
+  CHECK_NOTNULL(obstacle_ptr);
+  int id = obstacle_ptr->id();
+  CHECK_NOTNULL(lane_graph_ptr);
+
+  // Extract obstacle related features.
+  std::vector<double> obstacle_feature_values;
+  if (!ExtractObstacleFeatures(obstacle_ptr, &obstacle_feature_values)) {
+    ADEBUG << "Failed to extract obstacle features for obs_id = " << id;
+  }
+  if (obstacle_feature_values.size() != OBSTACLE_FEATURE_SIZE) {
+    ADEBUG << "Obstacle [" << id << "] has fewer than "
+           << "expected obstacle feature_values "
+           << obstacle_feature_values.size() << ".";
+    return false;
+  }
+  ADEBUG << "Obstacle feature size = " << obstacle_feature_values.size();
+  feature_values->insert(feature_values->end(),
+                         obstacle_feature_values.begin(),
+                         obstacle_feature_values.end());
+
+  // Extract static environmental (lane-related) features.
+  std::vector<double> static_feature_values;
+  if (!ExtractStaticEnvFeatures(lane_graph_ptr, &static_feature_values)) {
+    ADEBUG << "Failed to extract static environmental features around obs_id = "
+           << id;
+  }
+  // TODO(jiacheng): sanity check of extracted static env features:
+  feature_values->insert(feature_values->end(),
+                         static_feature_values.begin(),
+                         static_feature_values.end());
+  return true;
 }
 
-void LaneScanningEvaluator::ExtractObstacleFeatures(
+bool LaneScanningEvaluator::ExtractObstacleFeatures(
     const Obstacle* obstacle_ptr, std::vector<double>* feature_values) {
+  return true;
 }
 
-void LaneScanningEvaluator::ExtractStaticEnvFeatures(
-    const Obstacle* obstacle_ptr, std::vector<double>* feature_values) {
+bool LaneScanningEvaluator::ExtractStaticEnvFeatures(
+    const LaneGraph* lane_graph_ptr, std::vector<double>* feature_values) {
+  return true;
 }
 
 }  // namespace prediction
