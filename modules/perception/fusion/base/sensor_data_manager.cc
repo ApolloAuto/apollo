6a75b05fc21f4af94b252f6d881451d2f44a48a8
Mohamed Isse Mohamud <Moham893@umn.edu>
2019-01-14 16:11:48 -0600

    Changed bubble sort to std::sort for efficiency

--- modules/perception/fusion/base/sensor_data_manager.cc
+++ modules/perception/fusion/base/sensor_data_manager.cc
@@ -110,13 +110,9 @@ void SensorDataManager::GetLatestFrames(
     return;
   }
 
-  for (size_t i = 0; i < frames->size() - 1; ++i) {
-    for (size_t j = i + 1; j < frames->size(); ++j) {
-      if ((*frames)[j]->GetTimestamp() < (*frames)[i]->GetTimestamp()) {
-        std::swap((*frames)[j], (*frames)[i]);
-      }
-    }
-  }
+  std::sort((*frames)->begin(), (*frames)->end(), [](const auto& frame1, const auto& frame2 ){
+    return frame1->GetTimestamp() < frame2.GetTimestamp();
+  });
 }
 
 bool SensorDataManager::GetPose(const std::string& sensor_id, double timestamp,
