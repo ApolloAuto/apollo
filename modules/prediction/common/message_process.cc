5e75ae738a503db683dbff4cc1df03eee9bb3cd3
kechxu <kechxu89@gmail.com>
2019-01-22 13:54:12 -0800

    Prediction: move some internal process into MessageProcess

--- modules/prediction/common/message_process.cc
+++ modules/prediction/common/message_process.cc
@@ -19,8 +19,10 @@
 #include <algorithm>
 #include <vector>
 
-#include "modules/common/adapters/adapter_gflags.h"
+#include "cyber/common/file.h"
+#include "cyber/record/record_reader.h"
 
+#include "modules/common/adapters/adapter_gflags.h"
 #include "modules/prediction/common/feature_output.h"
 #include "modules/prediction/common/junction_analyzer.h"
 #include "modules/prediction/common/prediction_gflags.h"
@@ -40,6 +42,43 @@ using apollo::localization::LocalizationEstimate;
 using apollo::perception::PerceptionObstacle;
 using apollo::perception::PerceptionObstacles;
 using apollo::planning::ADCTrajectory;
+using cyber::record::RecordMessage;
+using cyber::record::RecordReader;
+
+bool MessageProcess::Init() {
+  // Load prediction conf
+  PredictionConf prediction_conf;
+  if (!common::util::GetProtoFromFile(FLAGS_prediction_conf_file,
+                                      &prediction_conf)) {
+    AERROR << "Unable to load prediction conf file: "
+           << FLAGS_prediction_conf_file;
+    return false;
+  }
+  ADEBUG << "Prediction config file is loaded into: "
+            << prediction_conf.ShortDebugString();
+
+  common::adapter::AdapterManagerConfig adapter_conf;
+  if (!common::util::GetProtoFromFile(FLAGS_prediction_adapter_config_filename,
+                                      &adapter_conf)) {
+    AERROR << "Unable to load adapter conf file: "
+           << FLAGS_prediction_adapter_config_filename;
+    return false;
+  }
+  ADEBUG << "Adapter config file is loaded into: "
+            << adapter_conf.ShortDebugString();
+
+  // Initialization of all managers
+  ContainerManager::Instance()->Init(adapter_conf);
+  EvaluatorManager::Instance()->Init(prediction_conf);
+  PredictorManager::Instance()->Init(prediction_conf);
+
+  if (!FLAGS_use_navigation_mode && !PredictionMap::Ready()) {
+    AERROR << "Map cannot be loaded.";
+    return false;
+  }
+
+  return true;
+}
 
 void MessageProcess::OnPerception(
     const perception::PerceptionObstacles &perception_obstacles,
@@ -164,6 +203,30 @@ void MessageProcess::OnPlanning(
 
   ADEBUG << "Received a planning message ["
          << adc_trajectory.ShortDebugString() << "].";
+}
+
+void MessageProcess::ProcessOfflineData(const std::string& record_filename) {
+  RecordReader reader(record_filename);
+  RecordMessage message;
+  while (reader.ReadMessage(&message)) {
+    if (message.channel_name == FLAGS_perception_obstacle_topic) {
+      PerceptionObstacles perception_obstacles;
+      if (perception_obstacles.ParseFromString(message.content)) {
+        PredictionObstacles prediction_obstacles;
+        OnPerception(perception_obstacles, &prediction_obstacles);
+      }
+    } else if (message.channel_name == FLAGS_localization_topic) {
+      LocalizationEstimate localization;
+      if (localization.ParseFromString(message.content)) {
+        OnLocalization(localization);
+      }
+    } else if (message.channel_name == FLAGS_planning_trajectory_topic) {
+      ADCTrajectory adc_trajectory;
+      if (adc_trajectory.ParseFromString(message.content)) {
+        OnPlanning(adc_trajectory);
+      }
+    }
+  }
 }
 
 }  // namespace prediction
