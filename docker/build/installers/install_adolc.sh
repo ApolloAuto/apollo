110ec7d0034551f8770e28e816936437949a5876
deidaraho <deidaraho@gmail.com>
2019-01-23 10:42:40 -0800

    planning: update adolc install shell.

--- docker/build/installers/install_adolc.sh
+++ docker/build/installers/install_adolc.sh
@@ -27,20 +27,25 @@ echo "Build and install ColPack"
 
 git clone https://github.com/CSCsw/ColPack.git
 
+# sudo works
 pushd ColPack
-autoreconf -vif
-autoreconf --install
-automake
-fullpath=$(pwd)
-./configure --prefix=${fullpath}
-make -j8 all
-make install
+pushd build/automake      # automake folder
+sudo autoreconf -vif        # generate configure files based on the machince
+sudo mkdir mywork           
+pushd mywork
+fullpath=$(pwd)        # modify fullpath to your destination folder if need
+sudo ../configure --prefix=${fullpath}
+sudo make -j 4              # Where "4" is the number of cores on your machine
+sudo make install           # install lib and include/ColPack to destination
 
 mkdir -p /usr/local/colpack
-cp -r include /usr/local/colpack/ && cp -r lib /usr/local/colpack/
-cp COPYING.LESSER /usr/local/colpack/
-popd 
+sudo cp -r include /usr/local/colpack/ && sudo cp -r lib /usr/local/colpack/
+popd
+popd
+cp LICENSE /usr/local/colpack/
+popd
 
+echo "colpack done."
 echo "Build and install ADOL-C"
 
 wget https://www.coin-or.org/download/source/ADOL-C/ADOL-C-2.6.3.zip -O ADOL-C-2.6.3.zip
@@ -52,16 +57,16 @@ sudo automake
 sudo ./configure --prefix="/apollo/docker/build/installers/ADOL-C-2.6.3" --enable-sparse --enable-addexa --with-openmp-flag="-fopenmp" --with-colpack="/usr/local/colpack" ADD_CXXFLAGS="-fPIC" ADD_CFLAGS="-fPIC" ADD_FFLAGS="-fPIC"
 
 sudo make -j8 all
-make install
+sudo make install
 
 echo "Build ADOL-C done."
-mkdir -p /usr/local/adolc
-cp -r include /usr/local/adolc/ && cp -r lib64 /usr/local/adolc/
-cp LICENSE /usr/local/adolc/
+sudo mkdir -p /usr/local/adolc
+sudo cp -r include /usr/local/adolc/ && sudo cp -r lib64 /usr/local/adolc/
+sudo cp LICENSE /usr/local/adolc/
 popd
 
 export LD_LIBRARY_PATH=/usr/local/adolc/lib64:$LD_LIBRARY_PATH 
 
 # Clean up.
-apt-get clean && rm -rf /var/lib/apt/lists/*
-rm -fr ADOL-C-2.6.3.zip ADOL-C-2.6.3 ColPack
+sudo apt-get clean && sudo rm -rf /var/lib/apt/lists/*
+sudo rm -fr ADOL-C-2.6.3.zip ADOL-C-2.6.3 ColPack
