7f96a53956f9cdfff9a54db985612ed30562cd55
Jiangtao Hu <boris.hu@gmail.com>
2019-01-24 15:43:21 -0800

    planning: fix lint error.

--- modules/planning/open_space/trajectory_smoother/distance_approach_ipopt_interface.h
+++ modules/planning/open_space/trajectory_smoother/distance_approach_ipopt_interface.h
@@ -20,6 +20,8 @@
 
 #pragma once
 
+#include <omp.h>
+
 #include <limits>
 #include <vector>
 
@@ -27,11 +29,9 @@
 #include "IpTNLP.hpp"
 #include "IpTypes.hpp"
 #include "adolc/adolc.h"
+#include "adolc/adolc_openmp.h"
 #include "adolc/adolc_sparse.h"
 #include "adolc/adouble.h"
-
-#include <omp.h>
-#include <adolc/adolc_openmp.h>
 
 #include "cyber/common/log.h"
 #include "cyber/common/macros.h"
