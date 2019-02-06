f86012ca76089d3591f33ad2753d458f04246c1d
Vivian Lin <2775888+vlin17@users.noreply.github.com>
2019-02-06 14:01:39 -0800

    Dreamview: upgrade npm packages (#6763)

--- modules/dreamview/frontend/dist/app.bundle.js
+++ modules/dreamview/frontend/dist/app.bundle.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:467a8f669f26114f48026b1bbaf070a377cb4c074e3f5c73ebf0dfa3cbfdb411
-size 1823093
+oid sha256:359c78b65ac2d265442814b0a9bc5f6118e375de7707219fdd5d93728608ad5e
+size 1685677
