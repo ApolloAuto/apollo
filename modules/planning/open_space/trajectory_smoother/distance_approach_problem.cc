8e046e7cd014fc04d5d083355522738408beb1d4
deidaraho <deidaraho@gmail.com>
2019-02-07 16:37:16 -0800

    planning: fix cost jacobian in open space smoother

--- modules/planning/open_space/trajectory_smoother/distance_approach_problem.cc
+++ modules/planning/open_space/trajectory_smoother/distance_approach_problem.cc
@@ -85,13 +85,9 @@ bool DistanceApproachProblem::Solve(
   app->Options()->SetStringValue("recalc_y",
       planner_open_space_config_.distance_approach_config().\
         ipopt_config().ipopt_recalc_y());
-  if (FLAGS_enable_derivative_check) {
-    AINFO << "checking jacobians ...";
-    app->Options()->SetStringValue("derivative_test",
-        "first-order");
-    app->Options()->SetStringValue("derivative_test_print_all",
-        "No");
-  }
+  app->Options()->SetNumericValue("mu_init",
+      planner_open_space_config_.distance_approach_config().\
+        ipopt_config().ipopt_mu_init());
 
   Ipopt::ApplicationReturnStatus status = app->Initialize();
   if (status != Ipopt::Solve_Succeeded) {
