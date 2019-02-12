ed99b802500443f001febed5f656d83ecdda786d
JasonZhou404 <zhoujiny404@gmail.com>
2019-02-12 13:10:39 -0800

    Common: vehicle model minor refactor

--- modules/common/vehicle_model/vehicle_model.h
+++ modules/common/vehicle_model/vehicle_model.h
@@ -16,7 +16,9 @@
 
 #pragma once
 
+#include "modules/common/vehicle_model/proto/vehicle_model_config.pb.h"
 #include "modules/common/vehicle_state/proto/vehicle_state.pb.h"
+#include "modules/common/configs/vehicle_config_helper.h"
 
 namespace apollo {
 namespace common {
@@ -25,9 +27,15 @@ class VehicleModel {
  public:
   VehicleModel();
 
-  static bool Predict(const double predicted_time_horizon,
-                      const VehicleState& cur_vehicle_state,
-                      VehicleState* predicted_vehicle_state);
+  static VehicleState Predict(const double predicted_time_horizon,
+                              const VehicleState& cur_vehicle_state);
+
+ private:
+  static void RearCenteredKinematicBicycleModel(
+      const VehicleModelConfig& vehicle_model_config,
+      const VehicleParam& vehicle_param, const double predicted_time_horizon,
+      const VehicleState& cur_vehicle_state,
+      VehicleState* predicted_vehicle_state);
 };
 
 }  // namespace common
