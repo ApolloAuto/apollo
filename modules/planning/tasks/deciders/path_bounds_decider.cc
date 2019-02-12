f69a8092a9125319a9c4a7b195effacd0a2375d7
panjiacheng <panjiacheng@yahoo.com>
2019-02-12 13:51:23 -0800

    Planning: added general skeleton for path_bounds_decider.

--- modules/planning/tasks/deciders/path_bounds_decider.cc
+++ modules/planning/tasks/deciders/path_bounds_decider.cc
@@ -16,19 +16,82 @@
 
 #include "modules/planning/tasks/deciders/path_bounds_decider.h"
 
+#include <string>
+#include <tuple>
+#include <vector>
+
 namespace apollo {
 namespace planning {
 
+using apollo::common::ErrorCode;
 using apollo::common::Status;
+
+constexpr double kPathBoundsDeciderHorizon = 100.0;
+constexpr double kPathBoundsDeciderResolution = 0.1;
 
 PathBoundsDecider::PathBoundsDecider(const TaskConfig &config)
     : Decider(config) {}
 
 Status PathBoundsDecider::Process(
     Frame *const frame, ReferenceLineInfo *const reference_line_info) {
-  // TODO(all) implement
+  // Sanity checks.
+  CHECK_NOTNULL(frame);
+  CHECK_NOTNULL(reference_line_info);
+
+  // 0. Initialize the path boundaries to be an indefinitely large area.
+  std::vector<std::tuple<double, double, double>> path_boundaries;
+  if (!InitPathBoundaries(
+          reference_line_info->reference_line(), &path_boundaries)) {
+    const std::string msg = "Failed to initialize path boundaries.";
+    AERROR << msg;
+    return Status(ErrorCode::PLANNING_ERROR, msg);
+  }
+
+  // 1. Decide a rough boundary based on road info and ADC's position
+  if (!GetBoundariesFromRoadsAndADC(
+          reference_line_info->reference_line(),
+          reference_line_info->AdcSlBoundary(), &path_boundaries)) {
+    const std::string msg = "Failed to decide a rough boundary based on "
+                            "road information.";
+    AERROR << msg;
+    return Status(ErrorCode::PLANNING_ERROR, msg);
+  }
+
+  // 2. Finetune the boundary based on static obstacles
+  // TODO(all): in the future, add side-pass functionality.
+  if (!GetBoundariesFromStaticObstacles(
+          reference_line_info->path_decision()->obstacles(),
+          &path_boundaries)) {
+    const std::string msg = "Failed to decide fine tune the boundaries after "
+                            "taking into consideration all static obstacles.";
+    AERROR << msg;
+    return Status(ErrorCode::PLANNING_ERROR, msg);
+  }
+
+  // Update the path boundary info to the frame.
+  // TODO(all): update frame with path bound info.
   return Status::OK();
 }
+
+bool PathBoundsDecider::InitPathBoundaries(
+    const ReferenceLine& reference_line,
+    std::vector<std::tuple<double, double, double>>* const path_boundaries) {
+  return true;
+}
+
+bool PathBoundsDecider::GetBoundariesFromRoadsAndADC(
+    const ReferenceLine& reference_line,
+    const SLBoundary& adc_sl_boundary,
+    std::vector<std::tuple<double, double, double>>* const path_boundaries) {
+  return true;
+}
+
+bool PathBoundsDecider::GetBoundariesFromStaticObstacles(
+    const IndexedList<std::string, Obstacle>& indexed_obstacles,
+    std::vector<std::tuple<double, double, double>>* const path_boundaries) {
+  return true;
+}
+
 
 }  // namespace planning
 }  // namespace apollo
