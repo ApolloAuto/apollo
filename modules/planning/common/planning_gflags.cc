5c1ca7c4d3d91c95836cc086b64f6b14f20ff2ee
Yifei Jiang <jiangyifei@gmail.com>
2019-02-13 08:58:49 -0800

    planning: disable scenario dispatcher.

--- modules/planning/common/planning_gflags.cc
+++ modules/planning/common/planning_gflags.cc
@@ -53,7 +53,7 @@ DEFINE_string(scenario_traffic_light_unprotected_right_turn_config_file,
               "scenario/traffic_light_unprotected_right_turn_config.pb.txt",
               "scenario_traffic_light_unprotected_right_turn config file");
 
-DEFINE_bool(enable_scenario_dispatcher, true,
+DEFINE_bool(enable_scenario_dispatcher, false,
             "enable dispatcher inside scenario manager to select scenario");
 
 DEFINE_bool(enable_scenario_side_pass, true,
