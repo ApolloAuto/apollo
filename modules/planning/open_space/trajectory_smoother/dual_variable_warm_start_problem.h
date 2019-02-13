ab4677d539812aad0dc33ce7e7af1fc0ead5e3c2
Runxin He <deidaraho@gmail.com>
2019-02-12 17:11:01 -0800

    planning: refine dual variable warm up in open space (#6825)
    * planning: refine dual variable warm up into a QP problem
    
    * planning: introduce conf parameter to switch dual variable warm up modes inside open space

--- modules/planning/open_space/trajectory_smoother/dual_variable_warm_start_problem.h
+++ modules/planning/open_space/trajectory_smoother/dual_variable_warm_start_problem.h
@@ -24,6 +24,7 @@
 
 #include "modules/planning/open_space/trajectory_smoother/dual_variable_warm_start_ipopt_interface.h"
 #include "modules/planning/proto/planning.pb.h"
+#include "modules/planning/open_space/trajectory_smoother/dual_variable_warm_start_ipopt_qp_interface.h"
 
 namespace apollo {
 namespace planning {
