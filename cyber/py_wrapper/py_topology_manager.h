d16ec321cb9b903d0619bb31899539af1f9da311
zizaidian <taoshengzhao01@baidu.com>
2019-02-13 20:23:42 +0800

    Cyber:AddCyberChannelInterface

--- cyber/py_wrapper/py_topology_manager.h
+++ cyber/py_wrapper/py_topology_manager.h
@@ -22,6 +22,7 @@
 #include <memory>
 #include <vector>
 #include <algorithm>
+#include <unordered_map>
 #include "cyber/cyber.h"
 #include "cyber/init.h"
 #include "cyber/message/protobuf_factory.h"
@@ -30,7 +31,6 @@
 
 namespace apollo {
 namespace cyber {
-
 class PyNodeManager {
  public:
   PyNodeManager() {
@@ -44,14 +44,14 @@ class PyNodeManager {
     return node_manager_->HasNode(node_name);
   }
 
-  bool GetNodesName() {
+  PyObject *GetNodeList() {
     std::vector<RoleAttributes> nodes;
+    PyObject *pyobj_list = PyList_New(0);
     topology_->node_manager()->GetNodes(&nodes);
     if (nodes.empty()) {
         AERROR << "no node found.";
-        return false;
-    }
-    std::sort(nodes.begin(), nodes.end(),
+    } else {
+        std::sort(nodes.begin(), nodes.end(),
             [](const RoleAttributes& na, const RoleAttributes& nb) -> bool {
               if (na.node_name().compare(nb.node_name()) <= 0) {
                 return true;
@@ -59,39 +59,45 @@ class PyNodeManager {
                 return false;
               }
             });
-    AINFO << nodes.size();
-    AINFO << "node list info get successful:";
-    for (auto& node : nodes) {
-        AINFO << node.node_name();
+        AINFO << "get node list successful.";
+        AINFO << "number of nodes:" << nodes.size();
+        PyList_Append(pyobj_list, Py_BuildValue("s", "Node List: "));
+        for (auto& node : nodes) {
+            PyList_Append(pyobj_list, Py_BuildValue("s", ConnectChar("    ",
+                                                 node.node_name().c_str())));
+        }
     }
-    return true;
+    return pyobj_list;
   }
 
-  const char *ConnectChar(const char* temp1, const char* temp2) {
+  const char *ConnectChar(const char* convert_temp1,
+                                        const char* convert_temp2) {
     const char* result = nullptr;
-    std::string temp = std::string(temp1) + std::string(temp2);
-    result = temp.c_str();
+    std::string convert_temp = std::string(convert_temp1) +
+                                                   std::string(convert_temp2);
+    result = convert_temp.c_str();
     return result;
   }
 
-  PyObject* ShowNodeInfo(const std::string& _node) {
+  PyObject* ShowNodeInfo(const std::string& node_name) {
       bool notFound = true;
       PyObject *pyobj_list = PyList_New(0);
-      if (node_manager_->HasNode(_node)) {
+      if (node_manager_->HasNode(node_name)) {
         std::vector<RoleAttributes> nodes;
         node_manager_->GetNodes(&nodes);
-        std::stringstream temps;
+        std::stringstream convert_temp;
         for (auto& node : nodes) {
-          if (node.node_name() == _node) {
+          if (node.node_name() == node_name) {
             PyList_Append(pyobj_list, Py_BuildValue("s",
                 ConnectChar("nodename:  ", node.node_name().c_str())));
-            temps << node.process_id();
+            convert_temp << node.process_id();
             PyList_Append(pyobj_list, Py_BuildValue("s",
-                    ConnectChar("processid: ", temps.str().c_str())));
+                    ConnectChar("processid: ", convert_temp.str().c_str())));
+            convert_temp.clear();
             PyList_Append(pyobj_list, Py_BuildValue("s",
                     ConnectChar("hostname:  ", node.host_name().c_str())));
             std::vector<RoleAttributes> readers;
-            channel_manager_->GetReadersOfNode(_node, &readers);
+            channel_manager_->GetReadersOfNode(node_name, &readers);
             PyList_Append(pyobj_list, Py_BuildValue("s",
                     "[Reading Channels]:  "));
             for (auto& reader : readers) {
@@ -104,7 +110,7 @@ class PyNodeManager {
             PyList_Append(pyobj_list, Py_BuildValue("s",
                     "[Writing Channels]:  "));
             std::vector<RoleAttributes> writers;
-            channel_manager_->GetWritersOfNode(_node, &writers);
+            channel_manager_->GetWritersOfNode(node_name, &writers);
             for (auto& writer : writers) {
               if (writer.channel_name() == "param_event") {
                 continue;
@@ -118,9 +124,181 @@ class PyNodeManager {
       }
     if (notFound) {
         PyList_Append(pyobj_list, Py_BuildValue("s",
-                    ConnectChar("Node cannot be found: ", _node.c_str())));
+                    ConnectChar("Node cannot be found: ", node_name.c_str())));
     }
     return pyobj_list;
+  }
+
+ private:
+    apollo::cyber::service_discovery::TopologyManager* topology_ = nullptr;
+    std::shared_ptr<apollo::cyber::service_discovery::NodeManager>
+                                                            node_manager_;
+    std::shared_ptr<apollo::cyber::service_discovery::ChannelManager>
+                                                            channel_manager_;
+};
+
+class PyChannelManager {
+ public:
+  PyChannelManager() {
+     topology_ = apollo::cyber::service_discovery::TopologyManager::Instance();
+     sleep(2);
+     node_manager_ = topology_->node_manager();
+     channel_manager_ = topology_->channel_manager();
+  }
+
+  const char *ConnectChar(const char* convert_temp1,
+                                                const char* convert_temp2) {
+     const char* result = nullptr;
+     std::string convert_temp = std::string(convert_temp1) +
+                                                std::string(convert_temp2);
+     result = convert_temp.c_str();
+     return result;
+  }
+
+  PyObject *GetChannelList() {
+     PyObject *pyobj_list = PyList_New(0);
+     std::vector<std::string> channels;
+     channel_manager_->GetChannelNames(&channels);
+     if (channels.empty()) {
+         AERROR << "no channel found.";
+     } else {
+         AINFO << "get channel list successful.";
+         AINFO << "number of channels:" << channels.size();
+         std::sort(channels.begin(), channels.end());
+         PyList_Append(pyobj_list, Py_BuildValue("s", "Channel List: "));
+         for (auto& channel : channels) {
+             PyList_Append(pyobj_list, Py_BuildValue("s",
+                                    ConnectChar("    ", channel.c_str())));
+         }
+     }
+     return pyobj_list;
+  }
+
+  PyObject *GetReaderList() {
+     PyObject *pyobj_list = PyList_New(0);
+     std::vector<proto::RoleAttributes> readers;
+     channel_manager_->GetReaders(&readers);
+     if (readers.empty()) {
+         AERROR << "no reader found.";
+     } else {
+         AINFO << "get reader list successful.";
+         AINFO << "number of readers:" << readers.size();
+         for (auto& reader : readers) {
+             PyList_Append(pyobj_list, Py_BuildValue("s",
+                                         reader.channel_name().c_str()));
+         }
+     }
+     return pyobj_list;
+  }
+
+  PyObject *GetWriterList() {
+     PyObject *pyobj_list = PyList_New(0);
+     std::vector<proto::RoleAttributes> writers;
+     channel_manager_->GetWriters(&writers);
+     if (writers.empty()) {
+         AERROR << "no writer found.";
+     } else {
+         AINFO << "get writer list successful.";
+         AINFO << "number of writers:" << writers.size();
+         for (auto& writer : writers) {
+             PyList_Append(pyobj_list, Py_BuildValue("s",
+                                         writer.channel_name().c_str()));
+         }
+     }
+     return pyobj_list;
+  }
+
+  PyObject *GetWritersOfNode(const std::string& node_name) {
+     PyObject *pyobj_list = PyList_New(0);
+     std::vector<proto::RoleAttributes> writers;
+     channel_manager_->GetWritersOfNode(node_name, &writers);
+     if (writers.empty()) {
+         AERROR << "no writer found for node:" << node_name;
+     } else {
+         AINFO << "get writers successful for nodes:" << node_name;
+         AINFO << "number of writers:" << writers.size();
+         for (auto& writer : writers) {
+             PyList_Append(pyobj_list, Py_BuildValue("s",
+                                         writer.channel_name().c_str()));
+         }
+     }
+     return pyobj_list;
+  }
+
+  PyObject *GetReadersOfNode(const std::string& node_name) {
+     PyObject *pyobj_list = PyList_New(0);
+     std::vector<proto::RoleAttributes> readers;
+     channel_manager_->GetReadersOfNode(node_name, &readers);
+     if (readers.empty()) {
+         AERROR << "no reader found for node:" << node_name;
+     } else {
+         AINFO << "get readers successful for nodes:" << node_name;
+         AINFO << "number of readers:" << readers.size();
+         for (auto& reader : readers) {
+             PyList_Append(pyobj_list, Py_BuildValue("s",
+                                         reader.channel_name().c_str()));
+         }
+     }
+     return pyobj_list;
+  }
+
+  PyObject* ShowChannelInfo(const std::string& channel_name) {
+     bool notFound = true;
+     PyObject *pyobj_list = PyList_New(0);
+     std::unordered_map<std::string, std::vector
+                           <apollo::cyber::proto::RoleAttributes>> roles_info;
+     if (channel_manager_->HasWriter(channel_name)||
+                           channel_manager_->HasReader(channel_name)) {
+       std::vector<apollo::cyber::proto::RoleAttributes> tmpVec;
+       channel_manager_->GetWriters(&tmpVec);
+       std::stringstream convert_temp;
+       for (auto& attr : tmpVec) {
+         std::string channel_name = attr.channel_name();
+         roles_info[channel_name].push_back(attr);
+       }
+       tmpVec.clear();
+       channel_manager_->GetReaders(&tmpVec);
+       for (auto& attr : tmpVec) {
+         std::string channel_name = attr.channel_name();
+         roles_info[channel_name].push_back(attr);
+       }
+       convert_temp.str("");
+       convert_temp << roles_info.size();
+       PyList_Append(pyobj_list, Py_BuildValue("s",
+               ConnectChar("channels size: ", convert_temp.str().c_str())));
+       for (auto it = roles_info.begin(); it != roles_info.end(); ++it) {
+           if (it->first != channel_name) {
+               continue;
+       }
+       convert_temp.str("");
+       convert_temp << it->first;
+       PyList_Append(pyobj_list, Py_BuildValue("s",
+               ConnectChar("channel name: ", convert_temp.str().c_str())));
+       for (auto role_info : it->second) {
+         convert_temp.str("");
+         convert_temp << role_info.id();
+         PyList_Append(pyobj_list, Py_BuildValue("s",
+            ConnectChar("  roleid:      ", convert_temp.str().c_str())));
+         PyList_Append(pyobj_list, Py_BuildValue("s",
+            ConnectChar("    hostname:   ", role_info.host_name().c_str())));
+         convert_temp.str("");
+         convert_temp << role_info.process_id();
+         PyList_Append(pyobj_list, Py_BuildValue("s",
+           ConnectChar("    processid:  ", convert_temp.str().c_str())));
+         PyList_Append(pyobj_list, Py_BuildValue("s",
+           ConnectChar("    nodename:   ", role_info.node_name().c_str())));
+         PyList_Append(pyobj_list, Py_BuildValue("s",
+           ConnectChar("    msgtype:    ",
+                           role_info.message_type().c_str())));
+       }
+      }
+        notFound = false;
+     }
+     if (notFound) {
+       PyList_Append(pyobj_list, Py_BuildValue("s",
+           ConnectChar("Channel cannot be found: ", channel_name.c_str())));
+     }
+     return pyobj_list;
   }
 
  private:
--- cyber/py_wrapper/py_topology_manager_wrap.cc
+++ cyber/py_wrapper/py_topology_manager_wrap.cc
@@ -22,6 +22,7 @@
 
 #define PYOBJECT_NULL_STRING PyString_FromStringAndSize("", 0)
 using apollo::cyber::PyNodeManager;
+using apollo::cyber::PyChannelManager;
 template <typename T>
 T PyObjectToPtr(PyObject *pyobj, const std::string &type_ptr) {
   T obj_ptr = (T)PyCapsule_GetPointer(pyobj, type_ptr.c_str());
@@ -30,7 +31,7 @@ T PyObjectToPtr(PyObject *pyobj, const std::string &type_ptr) {
   }
   return obj_ptr;
 }
-
+// Node Manage Interface
 PyObject *cyber_new_Node_Manager(PyObject *self, PyObject *args) {
   PyNodeManager *pyobj_node_manager = new PyNodeManager();
   PyObject *py_node_manager;
@@ -60,22 +61,18 @@ static PyObject *cyber_Py_HasNode(PyObject *self, PyObject *args) {
     }
 }
 
-static PyObject *cyber_Py_GetNodesName(PyObject *self, PyObject *args) {
+static PyObject *cyber_Py_GetNodeList(PyObject *self, PyObject *args) {
     PyObject *py_node_manager_obj = 0;
     PyNodeManager *py_node_manager = 0;
-    if (!PyArg_ParseTuple(args, const_cast<char *>("O:cyber_Py_GetNodes"),
+    if (!PyArg_ParseTuple(args, const_cast<char *>("O:cyber_Py_GetNodeList"),
                          &py_node_manager_obj)) {
-        AERROR << "cyber_Py_GetNodes:cyber_Py_GetNodes failed!";
+        AERROR << "cyber_Py_GetNodeList:cyber_Py_GetNodeList failed!";
         return Py_None;
     }
     py_node_manager = reinterpret_cast<PyNodeManager *>(PyCapsule_GetPointer(
                     py_node_manager_obj, "apollo_cyber_node_manager"));
-    bool getNodesName = py_node_manager->GetNodesName();
-    if (getNodesName) {
-        Py_RETURN_TRUE;
-    } else {
-        Py_RETURN_FALSE;
-    }
+    PyObject * node_list = py_node_manager->GetNodeList();
+    return node_list;
 }
 
 static PyObject *cyber_Py_ShowNodeInfo(PyObject *self, PyObject *args) {
@@ -85,7 +82,7 @@ static PyObject *cyber_Py_ShowNodeInfo(PyObject *self, PyObject *args) {
     PyNodeManager *py_node_manager = 0;
     if (!PyArg_ParseTuple(args, const_cast<char *>
     ("Os#:cyber_Py_ShowNodeInfo"), &py_node_manager_obj, &node_name, &len)) {
-        AINFO << "cyber_Py_ShowNodeInfo:cyber_Py_ShowNodeInfo failed!";
+        AERROR << "cyber_Py_ShowNodeInfo:cyber_Py_ShowNodeInfo failed!";
         return Py_None;
     }
     std::string data_str(node_name, len);
@@ -94,6 +91,116 @@ static PyObject *cyber_Py_ShowNodeInfo(PyObject *self, PyObject *args) {
     PyObject *showNodeInfo = py_node_manager->ShowNodeInfo(data_str);
     return showNodeInfo;
 }
+
+PyObject *cyber_new_Channel_Manager(PyObject *self, PyObject *args) {
+  PyChannelManager *pyobj_channel_manager = new PyChannelManager();
+  PyObject *py_channel_manager;
+  py_channel_manager = PyCapsule_New(pyobj_channel_manager,
+                            "apollo_cyber_channel_manager", NULL);
+  return py_channel_manager;
+}
+
+static PyObject *cyber_Py_GetChannelList(PyObject *self, PyObject *args) {
+    PyObject *py_channel_manager_obj = 0;
+    PyChannelManager *py_channel_manager = 0;
+    if (!PyArg_ParseTuple(args, const_cast<char *>("O:cyber_Py_GetChannelList"),
+                         &py_channel_manager_obj)) {
+        AERROR << "cyber_Py_GetChannelList:cyber_Py_GetChannelList failed!";
+        return Py_None;
+    }
+    py_channel_manager = reinterpret_cast<PyChannelManager *>
+        (PyCapsule_GetPointer(py_channel_manager_obj,
+                    "apollo_cyber_channel_manager"));
+    PyObject *channel_list = py_channel_manager->GetChannelList();
+    return channel_list;
+}
+
+static PyObject *cyber_Py_GetReaderList(PyObject *self, PyObject *args) {
+    PyObject *py_channel_manager_obj = 0;
+    PyChannelManager *py_channel_manager = 0;
+    if (!PyArg_ParseTuple(args, const_cast<char *>
+            ("O:cyber_Py_GetReaderList"), &py_channel_manager_obj)) {
+        AERROR << "cyber_Py_GetReaderList:cyber_Py_GetReaderList failed!";
+        return Py_None;
+    }
+    py_channel_manager = reinterpret_cast<PyChannelManager *>
+        (PyCapsule_GetPointer(py_channel_manager_obj,
+                        "apollo_cyber_channel_manager"));
+    PyObject *reader_list = py_channel_manager->GetReaderList();
+    return reader_list;
+}
+
+static PyObject *cyber_Py_GetWriterList(PyObject *self, PyObject *args) {
+    PyObject *py_channel_manager_obj = 0;
+    PyChannelManager *py_channel_manager = 0;
+    if (!PyArg_ParseTuple(args, const_cast<char *>("O:cyber_Py_GetWriterList"),
+                         &py_channel_manager_obj)) {
+        AERROR << "cyber_Py_GetWriterList:cyber_Py_GetWriterList failed!";
+        return Py_None;
+    }
+    py_channel_manager = reinterpret_cast<PyChannelManager *>
+        (PyCapsule_GetPointer(py_channel_manager_obj,
+                    "apollo_cyber_channel_manager"));
+    PyObject *writer_list = py_channel_manager->GetWriterList();
+    return writer_list;
+}
+
+static PyObject *cyber_Py_GetWritersOfNode(PyObject *self, PyObject *args) {
+    PyObject *py_channel_manager_obj = 0;
+    char *node_name = nullptr;
+    Py_ssize_t len = 0;
+    PyChannelManager *py_channel_manager = 0;
+    if (!PyArg_ParseTuple(args, const_cast<char *>
+        ("Os#:cyber_Py_GetChannelList"), &py_channel_manager_obj,
+                        &node_name, &len)) {
+        AERROR << "cyber_Py_GetNodeWriters:cyber_Py_GetNodeWriters failed!";
+        return Py_None;
+    }
+    std::string data_str(node_name, len);
+    py_channel_manager = reinterpret_cast<PyChannelManager *>
+        (PyCapsule_GetPointer(py_channel_manager_obj,
+                    "apollo_cyber_channel_manager"));
+    PyObject *node_writers = py_channel_manager->GetWritersOfNode(data_str);
+    return node_writers;
+}
+
+static PyObject *cyber_Py_GetReadersOfNode(PyObject *self, PyObject *args) {
+    PyObject *py_channel_manager_obj = 0;
+    char *node_name = nullptr;
+    Py_ssize_t len = 0;
+    PyChannelManager *py_channel_manager = 0;
+    if (!PyArg_ParseTuple(args, const_cast<char *>
+        ("Os#:cyber_Py_GetChannelList"), &py_channel_manager_obj,
+                    &node_name, &len)) {
+        AERROR << "cyber_Py_GetNodeReaders:cyber_Py_GetNodeReaders failed!";
+        return Py_None;
+    }
+    std::string data_str(node_name, len);
+    py_channel_manager = reinterpret_cast<PyChannelManager *>
+        (PyCapsule_GetPointer(py_channel_manager_obj,
+                    "apollo_cyber_channel_manager"));
+    PyObject *node_readers = py_channel_manager->GetReadersOfNode(data_str);
+    return node_readers;
+}
+
+static PyObject *cyber_Py_ShowChannelInfo(PyObject *self, PyObject *args) {
+    char *channel_name = nullptr;
+    PyObject *py_channel_manager_obj = 0;
+    Py_ssize_t len = 0;
+    PyChannelManager *py_channel_manager = 0;
+    if (!PyArg_ParseTuple(args, const_cast<char *>
+    ("Os#:cyber_Py_ShowChannelInfo"), &py_channel_manager_obj,
+                &channel_name, &len)) {
+        AERROR << "cyber_Py_ShowChannelInfo:cyber_Py_ShowChannelInfo failed!";
+        return Py_None;
+    }
+    std::string data_str(channel_name, len);
+    py_channel_manager = reinterpret_cast<PyChannelManager *>
+        (PyCapsule_GetPointer(py_channel_manager_obj,
+                    "apollo_cyber_channel_manager"));
+    PyObject *showChannelInfo = py_channel_manager->ShowChannelInfo(data_str);
+    return showChannelInfo;
+}
 /////////////////////////////////////////////////////////////////////
 //// global for whole page, init module
 /////////////////////////////////////////////////////////////////////
@@ -101,8 +208,15 @@ static PyMethodDef _topology_manager_methods[] = {
     // PyWriter fun
     {"new_Node_Manager", cyber_new_Node_Manager, METH_VARARGS, ""},
     {"Py_HasNode", cyber_Py_HasNode, METH_VARARGS, ""},
-    {"Py_GetNodesName", cyber_Py_GetNodesName, METH_VARARGS, ""},
+    {"Py_GetNodeList", cyber_Py_GetNodeList, METH_VARARGS, ""},
     {"Py_ShowNodeInfo", cyber_Py_ShowNodeInfo, METH_VARARGS, ""},
+    {"new_Channel_Manager", cyber_new_Channel_Manager, METH_VARARGS, ""},
+    {"Py_GetChannelList", cyber_Py_GetChannelList, METH_VARARGS, ""},
+    {"Py_GetReaderList", cyber_Py_GetReaderList, METH_VARARGS, ""},
+    {"Py_GetWriterList", cyber_Py_GetWriterList, METH_VARARGS, ""},
+    {"Py_GetWritersOfNode", cyber_Py_GetWritersOfNode, METH_VARARGS, ""},
+    {"Py_GetReadersOfNode", cyber_Py_GetReadersOfNode, METH_VARARGS, ""},
+    {"Py_ShowChannelInfo", cyber_Py_ShowChannelInfo, METH_VARARGS, ""},
     {NULL, NULL, 0, NULL} /* sentinel */
 };
 
