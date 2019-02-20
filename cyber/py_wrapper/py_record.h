e9fcdf49e6c5ee59073f979d23ebe71e34ac4576
yaoweifeng01 <steponeinfo@126.com>
2019-02-20 20:23:14 +0800

    framework: fix segmentation args for python record writer

--- cyber/py_wrapper/py_record.h
+++ cyber/py_wrapper/py_record.h
@@ -109,13 +109,6 @@ class PyRecordWriter {
  public:
   PyRecordWriter() {}
 
-  PyRecordWriter(const uint64_t file_segmentation_size_kb,
-                 const uint64_t file_segmentation_interval_sec) {
-    recored_writer_.SetSizeOfFileSegmentation(file_segmentation_size_kb);
-    recored_writer_.SetIntervalOfFileSegmentation(
-        file_segmentation_interval_sec);
-  }
-
   ~PyRecordWriter() {}
 
   bool Open(const std::string& path) { return recored_writer_.Open(path); }
