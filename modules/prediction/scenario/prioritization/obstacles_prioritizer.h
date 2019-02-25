4c60107cbe9b44ceef9028b982be6d47e3262114
kechxu <kechxu89@gmail.com>
2019-02-25 14:47:35 -0800

    Prediction: fix using in header file

--- modules/prediction/scenario/prioritization/obstacles_prioritizer.h
+++ modules/prediction/scenario/prioritization/obstacles_prioritizer.h
@@ -47,13 +47,13 @@ class ObstaclesPrioritizer {
   static void AssignCautionLevelByEgoReferenceLine();
 
   static void AssignCautionByMerge(
-      std::shared_ptr<const LaneInfo> lane_info_ptr);
+      std::shared_ptr<const hdmap::LaneInfo> lane_info_ptr);
 
   static void AssignCautionByOverlap(
-      std::shared_ptr<const LaneInfo> lane_info_ptr);
+      std::shared_ptr<const hdmap::LaneInfo> lane_info_ptr);
 
   static void SetCautionBackward(
-    std::shared_ptr<const LaneInfo> start_lane_info_ptr,
+    std::shared_ptr<const hdmap::LaneInfo> start_lane_info_ptr,
     const double distance);
 };
 
