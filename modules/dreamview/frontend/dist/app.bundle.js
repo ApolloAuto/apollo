1205417c2a43c90865ebbd4e3cfa284965257e2d
vlin17 <thunder0504@gmail.com>
2019-02-27 14:48:36 -0800

    webpack

--- modules/dreamview/frontend/dist/app.bundle.js
+++ modules/dreamview/frontend/dist/app.bundle.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:20a8a08414ea1e0fbff4c4b3c9dd67659dbc7addb64388f855e7f2864c9120be
-size 1734999
+oid sha256:3c0406063446bc720e8c425b80dc8322fb8ea42b7b3a97052401aa34923d0b84
+size 1737182
