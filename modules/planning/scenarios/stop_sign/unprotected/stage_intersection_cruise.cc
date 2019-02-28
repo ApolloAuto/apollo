29572ecf78b572379cf7dbcbc920329acac7deea
jmtao <taojm99@gmail.com>
2019-02-28 11:22:36 -0800

    planning: move CheckInsidePncJunction code to util to be shared

--- modules/planning/scenarios/stop_sign/unprotected/stage_intersection_cruise.cc
+++ modules/planning/scenarios/stop_sign/unprotected/stage_intersection_cruise.cc
@@ -53,23 +53,7 @@ Stage::StageStatus StopSignUnprotectedStageIntersectionCruise::Process(
 
   // check pass pnc_junction
   const auto& reference_line_info = frame->reference_line_info().front();
-  const double adc_front_edge_s = reference_line_info.AdcSlBoundary().end_s();
-  const double adc_back_edge_s = reference_line_info.AdcSlBoundary().start_s();
-
-  hdmap::PathOverlap pnc_junction_overlap;
-  reference_line_info.GetPnCJunction(adc_front_edge_s, &pnc_junction_overlap);
-  if (pnc_junction_overlap.object_id.empty()) {
-    return FinishStage();
-  }
-
-  constexpr double kIntersectionPassDist = 2.0;  // unit: m
-  const double distance_adc_pass_intersection = adc_back_edge_s -
-      pnc_junction_overlap.end_s;
-  ADEBUG << "distance_adc_pass_intersection["
-      << distance_adc_pass_intersection
-      << "] pnc_junction_overlap[" << pnc_junction_overlap.object_id
-      << "] start_s[" << pnc_junction_overlap.start_s << "]";
-  if (distance_adc_pass_intersection >= kIntersectionPassDist) {
+  if (scenario::CheckInsidePnCJunction(reference_line_info)) {
     return FinishStage();
   }
 
