4511e0b514be318c830700bf0c6d63e0b6b7cc58
jmtao <taojm99@gmail.com>
2019-03-04 14:49:13 -0800

    planning: update intersection-cruise stage to handle intersection where pnc-junction is not available/marked on map

--- modules/planning/scenarios/traffic_light/protected/stage_intersection_cruise.cc
+++ modules/planning/scenarios/traffic_light/protected/stage_intersection_cruise.cc
@@ -22,6 +22,7 @@
 
 #include "cyber/common/log.h"
 #include "modules/planning/common/frame.h"
+#include "modules/planning/common/planning_context.h"
 #include "modules/planning/scenarios/util/util.h"
 
 namespace apollo {
@@ -42,8 +43,35 @@ Stage::StageStatus TrafficLightProtectedStageIntersectionCruise::Process(
     AERROR << "TrafficLightProtectedStageIntersectionCruise plan error";
   }
 
-  // check pass pnc_junction
   const auto& reference_line_info = frame->reference_line_info().front();
+
+  // check pass pnc_junction
+  // TODO(all): remove when pnc_junction completely available on map
+  const auto& pnc_junction_overlaps =
+      reference_line_info.reference_line().map_path().pnc_junction_overlaps();
+  if (pnc_junction_overlaps.size() == 0) {
+    // pnc_junction not exist on map, use current traffic_light's end_s
+    if (PlanningContext::GetScenarioInfo()
+        ->current_traffic_light_overlaps.size() == 0) {
+      return FinishStage();
+    } else {
+      constexpr double kIntersectionPassDist = 20.0;  // unit: m
+      const double adc_back_edge_s =
+          reference_line_info.AdcSlBoundary().start_s();
+      const double traffic_light_end_s = PlanningContext::GetScenarioInfo()
+          ->current_traffic_light_overlaps[0].end_s;
+      const double distance_adc_pass_traffic_light = adc_back_edge_s -
+          traffic_light_end_s;
+      AERROR << "distance_adc_pass_traffic_light["
+          << distance_adc_pass_traffic_light
+          << "] traffic_light_end_s[" << traffic_light_end_s << "]";
+
+      if (distance_adc_pass_traffic_light >= kIntersectionPassDist) {
+        return FinishStage();
+      }
+    }
+  }
+
   if (!scenario::CheckInsidePnCJunction(reference_line_info)) {
     return FinishStage();
   }
