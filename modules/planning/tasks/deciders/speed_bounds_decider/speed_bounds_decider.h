42568197c423bcf46a8e8e3ce790f61f1457bd17
JasonZhou404 <zhoujiny404@gmail.com>
2019-03-04 14:19:19 -0800

    Planning: move st_graph_data to planning/common

--- modules/planning/tasks/deciders/speed_bounds_decider/speed_bounds_decider.h
+++ modules/planning/tasks/deciders/speed_bounds_decider/speed_bounds_decider.h
@@ -24,7 +24,7 @@
 #include "modules/planning/proto/planning_config.pb.h"
 #include "modules/planning/proto/speed_bounds_decider_config.pb.h"
 #include "modules/planning/tasks/deciders/decider.h"
-#include "modules/planning/tasks/optimizers/st_graph/st_graph_data.h"
+#include "modules/planning/common/st_graph_data.h"
 
 namespace apollo {
 namespace planning {
