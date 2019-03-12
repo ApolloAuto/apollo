a6b17ee11347db6de92ba678da78819ce5433992
panjiacheng <panjiacheng@yahoo.com>
2019-03-12 16:41:51 -0700

    Planning: removed commented code and fixed several coding style issues.

--- modules/planning/tasks/deciders/path_assessment_decider.cc
+++ modules/planning/tasks/deciders/path_assessment_decider.cc
@@ -34,14 +34,14 @@ Status PathAssessmentDecider::Process(
   // Check the validity of paths (the optimization output).
   // 1. First check the regular path's validity.
   const PathData* regular_path_data = reference_line_info->mutable_path_data();
-  bool is_valid_regular_path = IsValidPath(regular_path_data);
+  bool is_valid_regular_path = IsValidPath(*regular_path_data);
   // 2. If the regular path is not valid, check the validity of the
   //    fallback path.
   bool is_valid_fallback_path = false;
   if (!is_valid_regular_path) {
     const PathData* fallback_path_data =
         reference_line_info->mutable_fallback_path_data();
-    is_valid_fallback_path = IsValidPath(fallback_path_data);
+    is_valid_fallback_path = IsValidPath(*fallback_path_data);
   }
   // 3. If neither is valid, use the reference_line as the ultimate fallback.
   if (!is_valid_regular_path && !is_valid_fallback_path) {
@@ -56,12 +56,13 @@ Status PathAssessmentDecider::Process(
   return Status::OK();
 }
 
-bool PathAssessmentDecider::IsValidPath(
-    const PathData* path_data) {
+bool PathAssessmentDecider::IsValidPath(const PathData& path_data) {
+  // TODO(jiacheng): implement this.
   return true;
 }
 
 void PathAssessmentDecider::SetPathInfo() {
+  // TODO(jiacheng): implement this.
   return;
 }
 
