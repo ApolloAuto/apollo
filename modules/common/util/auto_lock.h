d6c0bdd35ed3da01c6d5ce2603acf5c80e81ad78
muleisheng <muleisheng@126.com>
2019-03-13 15:09:27 +0800

    add auto lock class

---
+++ modules/common/util/auto_lock.h
@@ -0,0 +1,88 @@
+/******************************************************************************
+ * Copyright 2018 The Apollo Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *****************************************************************************/
+
+#pragma once
+
+#include <atomic>
+#include <mutex>
+#include <stdexcept>
+
+/**
+ * @brief MUST implement the following two routine if don't use std::mutex
+ */
+template<typename T>
+void InternalLock(T *t);
+template<typename T>
+void InternalUnLock(T *t);
+
+/**
+ * @brief override these two routines for other synchronization methods.
+ */
+void InternalLock(std::mutex *t) noexcept(false) {
+    if (!t)
+        throw std::errc::bad_address;
+    else
+        t->lock();
+}
+
+void InternalUnLock(std::mutex *t) noexcept(false) {
+    if (!t)
+        throw std::errc::bad_address;
+    else
+        t->unlock();
+}
+
+
+template<typename T = std::mutex>
+class AutoLock {
+ public:
+    explicit AutoLock(std::mutex *t) noexcept(false):
+        t_(t),
+        lock_(false) {
+        Lock();
+    }
+
+    ~AutoLock() noexcept(false) {
+        UnLock();
+    }
+
+ public:
+    void UnLock() noexcept(false) {
+        if (!t_)
+            throw std::errc::bad_address;
+        if (lock_) {
+            InternalUnLock(t_);
+            lock_ = false;
+        }
+    }
+
+ private:
+    void Lock() noexcept(false) {
+        if (!t_)
+            throw std::errc::bad_address;
+        if (!lock_) {
+            InternalLock(t_);
+            lock_ = true;
+        }
+    }
+
+ private:
+    T *t_;
+    std::atomic_bool lock_;
+
+ private:
+    AutoLock &operator= (const AutoLock &rhs);
+};
---
+++ modules/common/util/auto_lock_test.cc
@@ -0,0 +1,44 @@
+/******************************************************************************
+ * Copyright 2018 The Apollo Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *****************************************************************************/
+
+#include "modules/common/util/auto_lock.h"
+#include <thread>
+#include "gtest/gtest.h"
+
+#define MAX_COUNT       10000000
+#define EXPECTED_RESULT 20000000
+
+std::mutex g_mutex;
+int g_int = 0;
+
+void function() {
+    for (int i = 0; i < MAX_COUNT; i++) {
+        AutoLock<std::mutex> autolock(&g_mutex);
+        g_int++;
+    }
+}
+
+TEST(AutoLockTest, test_autolock) {
+    std::thread t1(function);
+    std::thread t2(function);
+
+    t1.join();
+    t2.join();
+
+    AutoLock<std::mutex> autolock(&g_mutex);
+    EXPECT_EQ(g_int, EXPECTED_RESULT);
+    autolock.UnLock();
+}
