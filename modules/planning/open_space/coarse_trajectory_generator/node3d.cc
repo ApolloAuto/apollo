e9fe60a34bd73672d05f2afe1c365f2b8e79f851
JasonZhou404 <zhoujiny404@gmail.com>
2019-03-20 13:12:31 -0700

    Planning: OpenSpace: Hybrid astar use string as map key

--- modules/planning/open_space/coarse_trajectory_generator/node3d.cc
+++ modules/planning/open_space/coarse_trajectory_generator/node3d.cc
@@ -50,18 +50,15 @@ Node3d::Node3d(double x, double y, double phi,
   phi_grid_ = static_cast<size_t>(
       (phi_ - (-M_PI)) /
       open_space_conf.warm_start_config().phi_grid_resolution());
-  index_ = static_cast<size_t>(
-      static_cast<double>(phi_grid_) * (XYbounds[1] - XYbounds[0]) *
-          (XYbounds[3] - XYbounds[2]) +
-      static_cast<double>(y_grid_) * (XYbounds[1] - XYbounds[0]) +
-      static_cast<double>(x_grid_));
+  index_ = ComputeStringIndex(x_grid_, y_grid_, phi_grid_);
   traversed_x_.push_back(x);
   traversed_y_.push_back(y);
   traversed_phi_.push_back(phi);
 }
 
-Node3d::Node3d(std::vector<double> traversed_x, std::vector<double> traversed_y,
-               std::vector<double> traversed_phi,
+Node3d::Node3d(const std::vector<double>& traversed_x,
+               const std::vector<double>& traversed_y,
+               const std::vector<double>& traversed_phi,
                const std::vector<double>& XYbounds,
                const PlannerOpenSpaceConfig& open_space_conf) {
   CHECK(XYbounds.size() == 4)
@@ -83,11 +80,7 @@ Node3d::Node3d(std::vector<double> traversed_x, std::vector<double> traversed_y,
   phi_grid_ = static_cast<size_t>(
       (phi_ - (-M_PI)) /
       open_space_conf.warm_start_config().phi_grid_resolution());
-  index_ = static_cast<size_t>(
-      static_cast<double>(phi_grid_) * (XYbounds[1] - XYbounds[0]) *
-          (XYbounds[3] - XYbounds[2]) +
-      static_cast<double>(y_grid_) * (XYbounds[1] - XYbounds[0]) +
-      static_cast<double>(x_grid_));
+  index_ = ComputeStringIndex(x_grid_, y_grid_, phi_grid_);
   traversed_x_ = traversed_x;
   traversed_y_ = traversed_y;
   traversed_phi_ = traversed_phi;
@@ -95,8 +88,7 @@ Node3d::Node3d(std::vector<double> traversed_x, std::vector<double> traversed_y,
 }
 
 Box2d Node3d::GetBoundingBox(const common::VehicleParam& vehicle_param_,
-                             const double& x, const double& y,
-                             const double& phi) {
+                             const double x, const double y, const double phi) {
   double ego_length = vehicle_param_.length();
   double ego_width = vehicle_param_.width();
   double shift_distance =
@@ -107,15 +99,20 @@ Box2d Node3d::GetBoundingBox(const common::VehicleParam& vehicle_param_,
   return ego_box;
 }
 
-bool Node3d::operator==(const std::shared_ptr<Node3d> right) const {
-  return x_grid_ == right->GetGridX() && y_grid_ == right->GetGridY() &&
-         phi_grid_ == right->GetGridPhi();
+bool Node3d::operator==(const Node3d& right) const {
+  return right.GetIndex() == index_;
 }
 
-size_t Node3d::GetSize() {
+size_t Node3d::GetSize() const {
   DCHECK(traversed_x_.size() == traversed_y_.size() &&
          traversed_x_.size() == traversed_phi_.size());
   return traversed_x_.size();
+}
+
+std::string Node3d::ComputeStringIndex(size_t x_grid, size_t y_grid,
+                                       size_t phi_grid) {
+  return std::to_string(x_grid) + "_" + std::to_string(y_grid) + "_" +
+         std::to_string(phi_grid);
 }
 
 }  // namespace planning
