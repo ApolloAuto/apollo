67fb3063c5c7c1988951dd34476ad16964a1601f
panjiacheng <panjiacheng@yahoo.com>
2019-03-21 14:17:41 -0700

    Planning: refactored the path_bounds_decider.

--- modules/planning/tasks/deciders/path_bounds_decider.h
+++ modules/planning/tasks/deciders/path_bounds_decider.h
@@ -57,36 +57,34 @@ class PathBoundsDecider : public Decider {
     *   - static obstacles
     *   The philosophy is: static environment must be and can only be taken
     *   care of by the path planning.
-    * @param: frame
     * @param: reference_line_info
     * @param: lane_borrow_info: which lane to borrow.
     * @param: The generated regular path_boundary, if there is one.
     * @return: A failure message. If succeeded, return "" (empty string).
     */
   std::string GenerateRegularPathBound(
-      const Frame& frame, const ReferenceLineInfo& reference_line_info,
+      const ReferenceLineInfo& reference_line_info,
       const LaneBorrowInfo lane_borrow_info,
-      std::vector<std::tuple<double, double, double>>* const path_boundary);
+      std::vector<std::tuple<double, double, double>>* const path_bound);
 
   /** @brief: The fallback path only considers:
-   *   - ADC's position (so that boundary must contain ADC's position)
-   *   - lane info
-   *   It is supposed to be the last resort in case regular path generation
-   *   fails so that speed decider can at least have some path and won't
-   *   fail drastically.
-   *   Therefore, it be reliable so that optimizer will not likely to
-   *   fail with this boundary, and therefore doesn't consider any static
-   *   obstacle. When the fallback path is used, stopping before static
-   *   obstacles should be taken care of by the speed decider. Also, it
-   *   doesn't consider any lane-borrowing.
-   * @param: frame
-   * @param: reference_line_info
-   * @param: The generated fallback path_boundary, if there is one.
-   * @return: A failure message. If succeeded, return "" (empty string).
-   */
-  std::string GenerateFallbackPathBoundary(
-      Frame* frame, ReferenceLineInfo* reference_line_info,
-      std::vector<std::tuple<double, double, double>>* const path_boundaries);
+    *   - ADC's position (so that boundary must contain ADC's position)
+    *   - lane info
+    *   It is supposed to be the last resort in case regular path generation
+    *   fails so that speed decider can at least have some path and won't
+    *   fail drastically.
+    *   Therefore, it be reliable so that optimizer will not likely to
+    *   fail with this boundary, and therefore doesn't consider any static
+    *   obstacle. When the fallback path is used, stopping before static
+    *   obstacles should be taken care of by the speed decider. Also, it
+    *   doesn't consider any lane-borrowing.
+    * @param: reference_line_info
+    * @param: The generated fallback path_boundary, if there is one.
+    * @return: A failure message. If succeeded, return "" (empty string).
+    */
+  std::string GenerateFallbackPathBound(
+      const ReferenceLineInfo& reference_line_info,
+      std::vector<std::tuple<double, double, double>>* const path_bound);
 
   /** @brief: Initializes an empty path boundary.
    */
