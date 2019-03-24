777628435b2ecc8cae779f2e50fe4f22f1a7f2f2
zhangchao19 <zhangchao19@baidu.com>
2019-03-24 20:50:09 +0800

    framework: add log info for scheduler

--- cyber/scheduler/processor.cc
+++ cyber/scheduler/processor.cc
@@ -78,6 +78,7 @@ void Processor::SetSchedPolicy(std::string spolicy, int sched_priority) {
 
 void Processor::Run() {
   tid_.store(static_cast<int>(syscall(SYS_gettid)));
+  AINFO << "processor_tid: " << tid_;
 
   while (likely(running_.load())) {
     if (likely(context_ != nullptr)) {
