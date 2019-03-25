9f0f250ede960a3af708ed9498f08ebc0e728e3e
kiboliu <kibo.liu95@gmail.com>
2019-03-25 15:46:34 -0700

    Dreamview: add shadow localization in simulation world

--- modules/dreamview/frontend/dist/app.bundle.js
+++ modules/dreamview/frontend/dist/app.bundle.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d72918b4103496cf73c591eff3d4a85b691c91d4634e6e6fd498128a00c4d1d9
-size 1750574
+oid sha256:da529a2279df910ee644b5adb3d5b87287f98b3ef6f20a5d2e98b8c370e2b1ce
+size 1750934
