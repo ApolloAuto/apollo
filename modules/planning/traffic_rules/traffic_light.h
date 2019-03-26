3825bd7c0da75807e5fe1117ebb9bc28d447adc1
jmtao <taojm99@gmail.com>
2019-03-26 15:32:01 -0700

    planning: decouple traffic light reading in traffic-light rule from scenario-manager

--- modules/planning/traffic_rules/traffic_light.h
+++ modules/planning/traffic_rules/traffic_light.h
@@ -20,6 +20,12 @@
 
 #pragma once
 
+#include <memory>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+#include "modules/perception/proto/traffic_light_detection.pb.h"
 #include "modules/planning/traffic_rules/traffic_rule.h"
 
 namespace apollo {
@@ -37,12 +43,21 @@ class TrafficLight : public TrafficRule {
  private:
   void MakeDecisions(Frame* const frame,
                      ReferenceLineInfo* const reference_line_info);
+
+  void ReadTrafficLights(
+      const std::shared_ptr<perception::TrafficLightDetection>&
+          traffic_light_detection);
+
+  perception::TrafficLight GetSignal(const std::string& traffic_light_id);
+
   int BuildStopDecision(Frame* const frame,
                         ReferenceLineInfo* const reference_line_info,
                         const hdmap::PathOverlap& traffic_light_overlap);
 
  private:
   static constexpr char const* const TRAFFIC_LIGHT_VO_ID_PREFIX = "TL_";
+  std::unordered_map<std::string, const apollo::perception::TrafficLight*>
+      traffic_lights_;
 };
 
 }  // namespace planning
