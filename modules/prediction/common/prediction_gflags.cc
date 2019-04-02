8a2e8a19ccf9299c42ce29abc4a7d288b88761a9
kechxu <kechxu89@gmail.com>
2019-04-02 13:56:41 -0700

    Prediction: change still threshold for pedestrian

--- modules/prediction/common/prediction_gflags.cc
+++ modules/prediction/common/prediction_gflags.cc
@@ -98,7 +98,7 @@ DEFINE_int32(max_still_obstacle_history_length, 10,
              "Min # historical frames for still obstacles");
 DEFINE_double(still_obstacle_speed_threshold, 1.8,
               "Speed threshold for still obstacles");
-DEFINE_double(still_pedestrian_speed_threshold, 0.5,
+DEFINE_double(still_pedestrian_speed_threshold, 0.2,
               "Speed threshold for still pedestrians");
 DEFINE_double(still_obstacle_position_std, 1.0,
               "Position standard deviation for still obstacles");
