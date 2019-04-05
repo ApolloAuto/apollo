03b124e73f85dbda69eccfce0aecfdf36779c8d9
kyleApollo <kyletsai@baidu.com>
2019-04-05 16:16:55 -0700

    Add install_fast_rtp.sh installer and patch file to set up fast_rtp in docker env.

--- docker/build/dev.x86_64.dockerfile
+++ docker/build/dev.x86_64.dockerfile
@@ -65,6 +65,7 @@ RUN bash /tmp/installers/install_protobuf.sh
 RUN bash /tmp/installers/install_python_modules.sh
 RUN bash /tmp/installers/install_qp_oases.sh
 RUN bash /tmp/installers/install_qt.sh
+RUN bash /tmp/installers/install_fast_rtps.sh
 RUN bash /tmp/installers/install_supervisor.sh
 RUN bash /tmp/installers/install_undistort.sh
 RUN bash /tmp/installers/install_user.sh
---
+++ docker/build/installers/install_fast_rtps.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+###############################################################################
+# Copyright 2018 The Apollo Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+###############################################################################
+
+set -e
+
+cd "$(dirname "${BASH_SOURCE[0]}")"
+
+git clone https://github.com/eProsima/Fast-RTPS.git
+pushd Fast-RTPS
+git checkout origin/release/1.5.0
+git submodule init
+git submodule update
+patch -p1 < ../FastRTPS_1.5.0.patch
+mkdir -p build/install && cd build
+cmake -DTHIRPARTY=ON -DCMAKE_INSTALL_PREFIX=./install ../
+make -j 8
+make install
+popd
+
+rm -rf Fast-RTPS 
