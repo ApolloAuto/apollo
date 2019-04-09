7d206e1160c21082257611c1056f514e772313bf
vlin17 <thunder0504@gmail.com>
2019-04-09 16:23:13 -0700

    Dreamview: display full label string in legend

--- modules/dreamview/frontend/dist/app.bundle.js
+++ modules/dreamview/frontend/dist/app.bundle.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:83a61bcc6dab982db8e907fe01336b6cf353994dc461ca845ea9734b7311865a
-size 1754756
+oid sha256:8ecc527450b994e2cebb2200d379751e3128e154364c89b00db2b2a2418aa45a
+size 1754733
