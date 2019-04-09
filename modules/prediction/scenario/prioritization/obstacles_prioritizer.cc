3ed2c07fc4f5c95d4c41ba99307045b59fbbf225
Hongyi <henrysun@umich.edu>
2019-04-08 19:16:09 -0700

    Prediction: consider the length in front of the overlap

--- modules/prediction/scenario/prioritization/obstacles_prioritizer.cc
+++ modules/prediction/scenario/prioritization/obstacles_prioritizer.cc
@@ -438,7 +438,10 @@ void ObstaclesPrioritizer::AssignCautionByOverlap(
       } else {
         std::shared_ptr<const LaneInfo> overlap_lane_ptr =
             PredictionMap::LaneById(object_id);
-        SetCautionBackward(overlap_lane_ptr,
+        // ahead_s is the length in front of the overlap
+        double ahead_s = overlap_lane_ptr->total_length() -
+                           object.lane_overlap_info().start_s();
+        SetCautionBackward(overlap_lane_ptr, ahead_s +
                            FLAGS_caution_search_distance_backward_for_overlap);
       }
     }
