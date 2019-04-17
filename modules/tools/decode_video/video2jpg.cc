56f87001d1fc88cde127639dd51e3e3ddf6618e8
michael4338 <michael4338@hotmail.com>
2019-04-17 21:24:18 +0000

    use cyber logging

--- modules/tools/decode_video/video2jpg.cc
+++ modules/tools/decode_video/video2jpg.cc
@@ -21,6 +21,8 @@
 
 #include "gflags/gflags.h"
 
+#include "cyber/common/log.h"
+
 DECLARE_string(input_video);
 DECLARE_string(output_dir);
 DEFINE_string(input_video, "", "The input video file");
@@ -36,8 +38,7 @@ void write_jpg_file(const char* jpeg_buffer, const int jpeg_size,
            output_file_pattern.c_str(), frame_num);
   FILE* jpeg_file = fopen(jpeg_file_path, "wb");
   if (jpeg_file == nullptr) {
-    std::cerr << "error: failed to create jpeg file" << jpeg_file_path
-              << std::endl;
+    AERROR << "error: failed to create jpeg file " << jpeg_file_path;
     return;
   }
   fwrite(jpeg_buffer, jpeg_size, 1, jpeg_file);
@@ -49,19 +50,19 @@ int stream_decoder_process_frame(const uint8_t* indata, const uint32_t insize,
   const uint32_t JPEG_BUF_SIZE = 1920 * 1080 * 2;
   AVCodecParserContext* codec_parser = av_parser_init(AV_CODEC_ID_H265);
   if (codec_parser == nullptr) {
-    std::cerr << "error: failed to init AVCodecParserContext" << std::endl;
+    AERROR << "error: failed to init AVCodecParserContext";
     return -1;
   }
   AVPacket apt;
   av_init_packet(&apt);
   const std::unique_ptr<H265Decoder> decoder(new H265Decoder());
   if (!decoder->Init()) {
-    std::cerr << "error: failed to init decoder" << std::endl;
+    AERROR << "error: failed to init decoder";
     return -1;
   }
   uint32_t local_size = insize;
   uint8_t* local_data = const_cast<uint8_t*>(indata);
-  std::cout << "decoding: video size = " << local_size << std::endl;
+  AINFO << "decoding: video size = " << local_size;
   int frame_num = 0;
   int error_frame_num = 0;
   char* jpeg_buffer = new char[JPEG_BUF_SIZE];
@@ -73,8 +74,8 @@ int stream_decoder_process_frame(const uint8_t* indata, const uint32_t insize,
       apt.data = local_data;
       apt.size = local_size;
     }
-    std::cout << "frame " << frame_num << ": frame_len=" << frame_len
-              << ". left_size=" << local_size << std::endl;
+    AINFO << "frame " << frame_num << ": frame_len=" << frame_len
+          << ". left_size=" << local_size;
     int jpeg_size =
         decoder->Process((char*)apt.data, apt.size, jpeg_buffer, JPEG_BUF_SIZE);
     if (jpeg_size > 0) {
@@ -91,11 +92,11 @@ int stream_decoder_process_frame(const uint8_t* indata, const uint32_t insize,
     int jpeg_size = decoder->Process(nullptr, 0, jpeg_buffer, JPEG_BUF_SIZE);
     if (jpeg_size > 0) {
       write_jpg_file(jpeg_buffer, jpeg_size, output_dir, frame_num);
-      std::cout << "frame " << frame_num << ": read from buffer" << std::endl;
+      AINFO << "frame " << frame_num << ": read from buffer";
       frame_num++;
     }
   }
-  std::cout << "total frames: " << frame_num << std::endl;
+  AINFO << "total frames: " << frame_num;
   delete[] jpeg_buffer;
   av_parser_close(codec_parser);
   return 0;
@@ -103,12 +104,11 @@ int stream_decoder_process_frame(const uint8_t* indata, const uint32_t insize,
 
 int main(int argc, char** argv) {
   google::ParseCommandLineFlags(&argc, &argv, true);
-  std::cout << "input video: " << FLAGS_input_video
-            << ". output dir: " << FLAGS_output_dir << std::endl;
+  AINFO << "input video: " << FLAGS_input_video
+        << ". output dir: " << FLAGS_output_dir;
   FILE* fp = fopen(FLAGS_input_video.c_str(), "rb");
   if (fp == nullptr) {
-    std::cerr << "error: failed to open file " << FLAGS_input_video
-              << std::endl;
+    AERROR << "error: failed to open file " << FLAGS_input_video;
     return -1;
   }
   fseek(fp, 0, SEEK_END);
@@ -117,8 +117,7 @@ int main(int argc, char** argv) {
   uint8_t* indata = (uint8_t*)malloc(insize);
   fread(indata, insize, 1, fp);
   if (stream_decoder_process_frame(indata, insize, FLAGS_output_dir) < 0) {
-    std::cerr << "error: failed to decode file " << FLAGS_input_video
-              << std::endl;
+    AERROR << "error: failed to decode file " << FLAGS_input_video;
     return -1;
   }
   fclose(fp);
