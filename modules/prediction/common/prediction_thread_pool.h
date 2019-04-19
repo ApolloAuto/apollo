186dbc9b04b9323509c3c092022fff649de71d62
kechxu <kechxu89@gmail.com>
2019-04-18 18:25:36 -0700

    Prediction: initialize some template functions

--- modules/prediction/common/prediction_thread_pool.h
+++ modules/prediction/common/prediction_thread_pool.h
@@ -20,20 +20,68 @@
 
 #pragma once
 
-#include "boost/thread.hpp"
-#include "boost/asio.hpp"
+#include <algorithm>
+#include <future>
+#include <iterator>
+#include <memory>
+#include <utility>
+#include <vector>
+#include <string>
 
-#include "cyber/base/thread_pool.h"
+#include "boost/asio.hpp"
+#include "boost/thread.hpp"
+
+#include "cyber/common/log.h"
 
 namespace apollo {
 namespace prediction {
 
-class PredictionThreadPool : public apollo::cyber::base::ThreadPool {
+class PredictionThreadPool {
  public:
+  explicit PredictionThreadPool(int thread_num, int thread_pool_index);
+
+  ~PredictionThreadPool() = default;
+
+  template<typename InputIter, typename F>
+  void ForEach(InputIter begin, InputIter end, F f) {
+    std::vector<std::future<void>> futures;
+    for (auto iter = begin; iter != end; ++iter) {
+      auto& elem = *iter;
+      futures.emplace_back(this->Post([&]{
+        f(elem);
+      }));
+    }
+    for (auto& future : futures) {
+      if (future.valid()) {
+        future.get();
+      } else {
+        AERROR << "Future is invalid.";
+      }
+    }
+  }
+
+  template<typename FuncType>
+  std::future<typename std::result_of<FuncType()>::type> Post(FuncType&& func) {
+    typedef typename std::result_of<FuncType()>::type ReturnType;
+    typedef typename std::packaged_task<ReturnType()> TaskType;
+    // Post requires that the functions in it are copy-constructible.
+    // We used a shared pointer for the packaged_task,
+    // Since it's only movable and non-copyable
+    std::shared_ptr<TaskType> task =
+        std::make_shared<TaskType>(std::move(func));
+    std::future<ReturnType> returned_future = task->get_future();
+
+    // Note: variables eg. `task` must be copied here because of the lifetime
+    io_service_.post([=]{
+      (*task)();
+    });
+    return returned_future;
+  }
+
  private:
-  boost::thread_group _thread_group;
-  boost::asio::io_service _io;
-  boost::asio::io_service::work _work;
+  boost::thread_group thread_group_;
+  boost::asio::io_service io_service_;
+  boost::asio::io_service::work work_;
 };
 
 }  // namespace prediction
