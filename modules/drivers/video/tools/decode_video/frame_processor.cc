0047d8c07700eefdeab799260af7ed212fd599ee
michael4338 <michael4338@hotmail.com>
2019-04-19 06:33:04 +0000

    addressed comments and moved the tool to video module

--- modules/drivers/video/tools/decode_video/frame_processor.cc
+++ modules/drivers/video/tools/decode_video/frame_processor.cc
@@ -14,61 +14,54 @@
  * limitations under the License.
  *****************************************************************************/
 
-#include <fstream>
-#include <iostream>
-#include <memory>
+#include "modules/drivers/video/tools/decode_video/frame_processor.h"
 
-#include "gflags/gflags.h"
+#include <fstream>
+#include <memory>
+#include <sstream>
 
 #include "cyber/common/log.h"
-#include "modules/tools/decode_video/h265_decoder.h"
+#include "modules/drivers/video/tools/decode_video/h265_decoder.h"
 
-DECLARE_string(input_video);
-DECLARE_string(output_dir);
-DEFINE_string(input_video, "", "The input video file");
-DEFINE_string(output_dir, "", "The directory to output decoded pictures.");
+namespace apollo {
+namespace drivers {
+namespace video {
 
-std::vector<uint8_t> read_video_file(const std::string file_name) {
-  std::ifstream in(file_name, std::ios::binary);
-  std::vector<uint8_t> file_buffer((std::istreambuf_iterator<char>(in)),
-                                   std::istreambuf_iterator<char>());
-  return file_buffer;
+FrameProcessor::FrameProcessor(const std::string& input_video_file,
+                               const std::string& output_jpg_dir)
+    : output_jpg_dir_(output_jpg_dir) {
+  std::ifstream video_file(input_video_file, std::ios::binary);
+  std::vector<uint8_t> video_buffer(
+      (std::istreambuf_iterator<char>(video_file)),
+      std::istreambuf_iterator<char>());
+  input_video_buffer_ = video_buffer;
 }
 
-void write_jpg_file(const std::vector<uint8_t>& jpeg_buffer,
-                    const std::string output_dir, const int frame_num) {
-  const int JPEG_FILE_LEN = 256;
-  const int FILE_SUFFIX_LEN = 16;
-  const std::string output_file_pattern = output_dir + "/%05d.jpg";
-  char jpeg_file_path[JPEG_FILE_LEN];
-  snprintf(jpeg_file_path, output_file_pattern.length() + FILE_SUFFIX_LEN,
-           output_file_pattern.c_str(), frame_num);
-  std::ofstream out(jpeg_file_path, std::ios::binary);
-  for (auto&& current : jpeg_buffer) out << static_cast<char>(current);
-}
-
-int stream_decoder_process_frame(const std::vector<uint8_t>& indata,
-                                 const std::string output_dir) {
+bool FrameProcessor::ProcessStream() const {
+  if (input_video_buffer_.empty()) {
+    AERROR << "error: failed to read from input video file";
+    return false;
+  }
   AVCodecParserContext* codec_parser = av_parser_init(AV_CODEC_ID_H265);
   if (codec_parser == nullptr) {
     AERROR << "error: failed to init AVCodecParserContext";
-    return -1;
+    return false;
   }
   AVPacket apt;
   av_init_packet(&apt);
   const std::unique_ptr<H265Decoder> decoder(new H265Decoder());
   if (!decoder->Init()) {
     AERROR << "error: failed to init decoder";
-    return -1;
+    return false;
   }
-  uint32_t local_size = static_cast<uint32_t>(indata.size());
-  uint8_t* local_data = const_cast<uint8_t*>(indata.data());
+  uint32_t local_size = static_cast<uint32_t>(input_video_buffer_.size());
+  uint8_t* local_data = const_cast<uint8_t*>(input_video_buffer_.data());
   AINFO << "decoding: video size = " << local_size;
   int frame_num = 0;
   int error_frame_num = 0;
   while (local_size > 0) {
     int frame_len = av_parser_parse2(
-        codec_parser, decoder->get_codec_ctx_h265(), &(apt.data), &(apt.size),
+        codec_parser, decoder->GetCodecCtxH265(), &(apt.data), &(apt.size),
         local_data, local_size, AV_NOPTS_VALUE, AV_NOPTS_VALUE, AV_NOPTS_VALUE);
     if (apt.data == nullptr) {
       apt.data = local_data;
@@ -78,7 +71,7 @@ int stream_decoder_process_frame(const std::vector<uint8_t>& indata,
           << ". left_size=" << local_size;
     std::vector<uint8_t> jpeg_buffer = decoder->Process(apt.data, apt.size);
     if (!jpeg_buffer.empty()) {
-      write_jpg_file(jpeg_buffer, output_dir, frame_num);
+      WriteOutputJpgFile(jpeg_buffer, GetOutputFile(frame_num));
       frame_num++;
     } else {
       error_frame_num++;
@@ -90,28 +83,34 @@ int stream_decoder_process_frame(const std::vector<uint8_t>& indata,
   for (int i = error_frame_num; i >= 0; i--) {
     std::vector<uint8_t> jpeg_buffer = decoder->Process(nullptr, 0);
     if (!jpeg_buffer.empty()) {
-      write_jpg_file(jpeg_buffer, output_dir, frame_num);
+      WriteOutputJpgFile(jpeg_buffer, GetOutputFile(frame_num));
       AINFO << "frame " << frame_num << ": read from buffer";
       frame_num++;
     }
   }
   AINFO << "total frames: " << frame_num;
   av_parser_close(codec_parser);
-  return 0;
+  return true;
 }
 
-int main(int argc, char** argv) {
-  google::ParseCommandLineFlags(&argc, &argv, true);
-  AINFO << "input video: " << FLAGS_input_video
-        << ". output dir: " << FLAGS_output_dir;
-  std::vector<uint8_t> video_file = read_video_file(FLAGS_input_video);
-  if (video_file.empty()) {
-    AERROR << "invalid input video file: " << FLAGS_input_video;
-    return -1;
-  }
-  if (stream_decoder_process_frame(video_file, FLAGS_output_dir) != 0) {
-    AERROR << "error: failed to decode file " << FLAGS_input_video;
-    return -1;
-  }
-  return 0;
+const std::string FrameProcessor::GetOutputFile(const int frame_num) const {
+  const int SUFFIX_LEN = 5;
+  std::stringstream jpg_suffix;
+  jpg_suffix.fill('0');
+  jpg_suffix.width(SUFFIX_LEN);
+  jpg_suffix << std::to_string(frame_num);
+  return output_jpg_dir_ + "/" + jpg_suffix.str() + ".jpg";
 }
+
+void FrameProcessor::WriteOutputJpgFile(
+    const std::vector<uint8_t>& jpeg_buffer,
+    const std::string& output_jpg_file) const {
+  std::ofstream out(output_jpg_file, std::ios::binary);
+  for (auto&& current : jpeg_buffer) {
+    out << static_cast<char>(current);
+  }
+}
+
+}  // namespace video
+}  // namespace drivers
+}  // namespace apollo
