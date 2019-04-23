6a3bc45f7280dd95bd4a5872111371821b586275
Hongyi <henrysun@umich.edu>
2019-04-22 18:15:54 -0700

    Planning: start piecewise_jerk_speed optimizer

---
+++ modules/planning/tasks/optimizers/piecewise_jerk_speed/piecewise_jerk_speed_optimizer.h
@@ -0,0 +1,45 @@
+/******************************************************************************
+ * Copyright 2019 The Apollo Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *****************************************************************************/
+
+/**
+ * @file piecewise_jerk_fallback_speed.h
+ **/
+
+#pragma once
+
+#include "modules/planning/tasks/optimizers/speed_optimizer.h"
+
+namespace apollo {
+namespace planning {
+
+class PiecewiseJerkSpeedOptimizer : public SpeedOptimizer {
+ public:
+  explicit PiecewiseJerkSpeedOptimizer(const TaskConfig& config);
+
+  virtual ~PiecewiseJerkSpeedOptimizer() = default;
+
+ private:
+  common::Status Process(const SLBoundary& adc_sl_boundary,
+                         const PathData& path_data,
+                         const apollo::common::TrajectoryPoint& init_point,
+                         const ReferenceLine& reference_line,
+                         const SpeedData& reference_speed_data,
+                         PathDecision* const path_decision,
+                         SpeedData* const speed_data) override;
+};
+
+}  // namespace planning
+}  // namespace apollo
