989bbeb01a8c9c87583e610dd2e23f98b96c2ed7
kechxu <kechxu89@gmail.com>
2019-05-08 13:05:57 -0700

    Prediction: disable multi thread for some offline jobs

--- modules/tools/prediction/data_pipelines/scripts/records_to_data_for_tuning.sh
+++ modules/tools/prediction/data_pipelines/scripts/records_to_data_for_tuning.sh
@@ -37,4 +37,5 @@ fi
     --map_dir=/apollo/modules/map/data/${MAP_DIR} \
     --prediction_offline_mode=5 \
     --prediction_offline_bags=${SRC_DIR} \
-    --prediction_data_dir=${TARGET_DIR}
+    --prediction_data_dir=${TARGET_DIR} \
+    --noenable_multi_thread
