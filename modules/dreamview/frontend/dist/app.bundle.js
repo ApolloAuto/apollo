31d26cbbbad952ae4221f3be89f057f66a31cb4c
Vivian Lin <2775888+vlin17@users.noreply.github.com>
2019-05-11 00:30:27 -0700

    Dreamview: support PiecewiseJerkSpeedOptimizer monitoring (#8276)
    * Dreamview: support PiecewiseJerkSpeedOptimizer monitoring
    
    * CR response & fixed lane text position issue

--- modules/dreamview/frontend/dist/app.bundle.js
+++ modules/dreamview/frontend/dist/app.bundle.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b38e67af107050cafd58c259683e8bc524a8d971ef0ca624a3d220b466eb134e
-size 1756055
+oid sha256:4ae402ab031def331b1d700102b08ce6de176cfff8862eedcd1f953223c09074
+size 1751978
