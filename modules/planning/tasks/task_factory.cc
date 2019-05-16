784913cf548d1e2bb38b0feb1c2658b69b995dba
Yajia Zhang <yajia.zhang@gmail.com>
2019-05-16 13:12:07 -0700

    planning: deprecate proceed_with_caution_speed_generator

--- modules/planning/tasks/task_factory.cc
+++ modules/planning/tasks/task_factory.cc
@@ -38,7 +38,6 @@
 #include "modules/planning/tasks/optimizers/path_time_heuristic/path_time_heuristic_optimizer.h"
 #include "modules/planning/tasks/optimizers/piecewise_jerk_path/piecewise_jerk_path_optimizer.h"
 #include "modules/planning/tasks/optimizers/piecewise_jerk_speed/piecewise_jerk_speed_optimizer.h"
-#include "modules/planning/tasks/optimizers/proceed_with_caution_speed/proceed_with_caution_speed_generator.h"
 #include "modules/planning/tasks/optimizers/speed_decider/speed_decider.h"
 #include "modules/planning/tasks/rss/decider_rss.h"
 #include "modules/planning/tasks/task.h"
@@ -91,10 +90,6 @@ void TaskFactory::Init(const PlanningConfig& config) {
   task_factory_.Register(TaskConfig::SPEED_DECIDER,
                          [](const TaskConfig& config) -> Task* {
                            return new SpeedDecider(config);
-                         });
-  task_factory_.Register(TaskConfig::PROCEED_WITH_CAUTION_SPEED,
-                         [](const TaskConfig& config) -> Task* {
-                           return new ProceedWithCautionSpeedGenerator(config);
                          });
   task_factory_.Register(TaskConfig::DECIDER_CREEP,
                          [](const TaskConfig& config) -> Task* {
