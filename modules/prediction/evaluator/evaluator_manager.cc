b9d0a8539ba62ee5d9d42e9d6b78291eacec5362
kechxu <kechxu89@gmail.com>
2019-05-16 11:56:06 -0700

    Prediction: fix gflags issue in dumping frame envs

--- modules/prediction/evaluator/evaluator_manager.cc
+++ modules/prediction/evaluator/evaluator_manager.cc
@@ -172,12 +172,15 @@ void EvaluatorManager::Run() {
           AdapterConfig::PERCEPTION_OBSTACLES);
   CHECK_NOTNULL(obstacles_container);
 
-  if (FLAGS_enable_semantic_map) {
+  if (FLAGS_enable_semantic_map ||
+      FLAGS_prediction_offline_mode == PredictionConstants::kDumpFrameEnv) {
     BuildObstacleIdHistoryMap();
-    if (FLAGS_prediction_offline_mode == 4) {
-      DumpCurrentFrameEnv();
+    DumpCurrentFrameEnv();
+    if (FLAGS_prediction_offline_mode == PredictionConstants::kDumpFrameEnv) {
       return;
     }
+  }
+  if (FLAGS_enable_semantic_map) {
     SemanticMap::Instance()->RunCurrFrame(obstacle_id_history_map_);
   }
 
