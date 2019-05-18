8e4d2b851aaa40a4fa5a1e92ce8d5d5bf18947eb
muleisheng <muleisheng@126.com>
2019-05-18 13:22:25 +0800

    bridge: add config for proto msgs

---
+++ modules/bridge/udp_bridge_component.cc
@@ -0,0 +1,98 @@
+/******************************************************************************
+ * Copyright 2019 The Apollo Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *****************************************************************************/
+
+#include "modules/bridge/udp_bridge_component.h"
+
+namespace apollo {
+namespace bridge {
+
+#define BRIDGE_IMPL(pb_msg)    \
+  template class UDPBridgeComponent<pb_msg>
+
+#define FREE_ARRY(arry) \
+  if (arry) {           \
+    delete [] arry;     \
+    arry = nullptr;     \
+  }
+
+using apollo::cyber::io::Session;
+using apollo::localization::LocalizationEstimate;
+using apollo::bridge::UDPBridgeRemoteInfo;
+
+template<typename T>
+bool UDPBridgeComponent<T>::Init() {
+  AINFO << "UDP bridge init, startin..";
+  apollo::bridge::UDPBridgeRemoteInfo udp_bridge_remote;
+  if (!this->GetProtoConfig(&udp_bridge_remote)) {
+    AINFO << "load udp bridge component proto param failed";
+    return false;
+  }
+  remote_ip_ = udp_bridge_remote.remote_ip();
+  remote_port_ = udp_bridge_remote.remote_port();
+  AINFO << "UDP Bridge remote ip is : "<< remote_ip_;
+  AINFO << "UDP Bridge remote port is : "<< remote_port_;
+  return true;
+}
+
+template<typename T>
+bool UDPBridgeComponent<T>::Proc( const std::shared_ptr<T> &pb_msg) {
+  if (remote_port_ == 0 || remote_ip_.empty()) {
+    AERROR << "remote info is invalid!";
+    return false;
+  }
+
+  if (pb_msg== nullptr) {
+    AERROR << "proto msg is not ready!";
+    return false;
+  }
+
+  apollo::cyber::scheduler::Instance()->CreateTask(
+      [this, &pb_msg]() {
+        struct sockaddr_in server_addr;
+        server_addr.sin_addr.s_addr = inet_addr(remote_ip_.c_str());
+        server_addr.sin_family = AF_INET;
+        server_addr.sin_port = htons((uint16_t)remote_port_);
+
+        Session session;
+        session.Socket(AF_INET, SOCK_DGRAM, 0);
+        if (session.Connect((struct sockaddr*)&server_addr,
+                            sizeof(server_addr)) < 0) {
+          std::cout << "connect to server failed, " << strerror(errno)
+                    << std::endl;
+          return;
+        }
+
+        unsigned int msg_len = pb_msg->ByteSize();
+        char *buf = new char[msg_len];
+        memset(buf, 0, msg_len);
+        pb_msg->SerializeToArray(buf, msg_len);
+        if (session.Send(buf, msg_len, 0) < 0) {
+          std::cout << "send message failed." << std::endl;
+          FREE_ARRY(buf)
+          return;
+        }
+        FREE_ARRY(buf)
+      },
+      "bridge_client");
+
+  return true;
+}
+
+BRIDGE_IMPL(LocalizationEstimate);
+BRIDGE_IMPL(planning::ADCTrajectory);
+
+}  // namespace bridge
+}  // namespace apollo
