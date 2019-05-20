e380d6a00741a714ac6496c294df62fd7f7097a3
Qi Luo <qi.stephen.luo@gmail.com>
2019-05-20 14:59:01 -0700

    Bridge: simple format cleaning

--- modules/bridge/common/bridge_buffer.cc
+++ modules/bridge/common/bridge_buffer.cc
@@ -20,45 +20,44 @@
 namespace apollo {
 namespace bridge {
 
-#define BRIDGE_IMPL(type) \
-  template class BridgeBuffer<type>
+#define BRIDGE_IMPL(type) template class BridgeBuffer<type>
 
-template<typename T>
+template <typename T>
 BridgeBuffer<T>::BridgeBuffer() {}
 
-template<typename T>
+template <typename T>
 BridgeBuffer<T>::BridgeBuffer(unsigned int size) {
   reset(size);
 }
 
-template<typename T>
+template <typename T>
 BridgeBuffer<T>::~BridgeBuffer() {
   std::lock_guard<std::mutex> lg(mutex_);
   if (buf_) {
-    delete [] buf_;
+    delete[] buf_;
   }
   buf_ = nullptr;
   size_ = 0;
   capacity_ = 0;
 }
 
-template<typename T>
-BridgeBuffer<T>::operator T* () {
+template <typename T>
+BridgeBuffer<T>::operator T*() {
   return buf_;
 }
 
-template<typename T>
+template <typename T>
 void BridgeBuffer<T>::reset(unsigned int size) {
   std::lock_guard<std::mutex> lg(mutex_);
   if (capacity_ < size) {
     if (buf_) {
-      delete [] buf_;
+      delete[] buf_;
     }
     capacity_ = size;
     buf_ = new T[capacity_];
   }
   size_ = size;
-  memset(buf_, 0, sizeof(T)*capacity_);
+  memset(buf_, 0, sizeof(T) * capacity_);
 }
 
 BRIDGE_IMPL(char);
