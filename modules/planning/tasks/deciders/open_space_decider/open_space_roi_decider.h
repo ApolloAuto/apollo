54af84d0ac077fd941b67848f0eef664686b9401
JasonZhou404 <zhoujiny404@gmail.com>
2019-05-20 18:51:19 -0700

    Planning: set end pose in open space roi for pull over scenario

--- modules/planning/tasks/deciders/open_space_decider/open_space_roi_decider.h
+++ modules/planning/tasks/deciders/open_space_decider/open_space_roi_decider.h
@@ -51,6 +51,25 @@ class OpenSpaceRoiDecider : public Decider {
   apollo::common::Status Process(Frame *frame) override;
 
  private:
+  // @brief generate the path by vehicle location and return the target parking
+  // spot on that path
+  bool GetParkingSpot(Frame *const frame,
+                      std::array<common::math::Vec2d, 4> *vertices,
+                      hdmap::Path *nearby_path);
+
+  // @brief get path from reference line and return vertices of pullover spot
+  bool GetPullOverSpot(Frame *const frame,
+                       std::array<common::math::Vec2d, 4> *vertices,
+                       hdmap::Path *nearby_path);
+
+  // @brief Set an origin to normlalize the problem for later computation
+  void SetOrigin(Frame *const frame,
+                 const std::array<common::math::Vec2d, 4> &vertices);
+
+  void SetParkingSpotEndPose(
+      Frame *const frame, const std::array<common::math::Vec2d, 4> &vertices);
+
+  void SetPullOverSpotEndPose(Frame *const frame);
   // @brief "Region of Interest", load map boundary for open space scenario
   // @param vertices is an array consisting four points describing the boundary
   // of spot in box. Four points are in sequence of left_top, left_down,
@@ -71,17 +90,6 @@ class OpenSpaceRoiDecider : public Decider {
                    const hdmap::Path &nearby_path,
                    std::vector<std::vector<common::math::Vec2d>>
                        *const roi_parking_boundary);
-
-  // @brief generate the path by vehicle location and return the target parking
-  // spot on that path
-  bool GetParkingSpot(Frame *const frame,
-                      std::array<common::math::Vec2d, 4> *vertices,
-                      hdmap::Path *nearby_path);
-
-  // @brief get path from reference line and return vertices of pullover spot
-  bool GetPullOverSpot(Frame *const frame,
-                       std::array<common::math::Vec2d, 4> *vertices,
-                       hdmap::Path *nearby_path);
 
   // @brief search target parking spot on the path by vehicle location, if
   // no return a nullptr in target_parking_spot
