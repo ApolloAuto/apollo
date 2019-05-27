8f5ba5e9604c32b3a53247e3898eabc330189fbc
yaoweifeng01 <yaoweifeng01@baidu.com>
2019-05-27 16:10:39 +0800

    cyber: format the comment of node.h

--- cyber/node/reader.h
+++ cyber/node/reader.h
@@ -50,6 +50,7 @@ const uint32_t DEFAULT_PENDING_QUEUE_SIZE = 1;
 /**
  * @class Reader
  * @brief .
+ *
  * Reader objecte subscribes to the channel for message received.
  *
  */
@@ -64,14 +65,15 @@ class Reader : public ReaderBase {
       typename std::list<std::shared_ptr<MessageT>>::const_iterator;
 
   /**
-   * Constuctor a Reader object.
-   * @param role_attr is a protobuf message RoleAttributes, which includes the
-   * channel name and other info.
-   * @param reader_func is the callback function, when the message is recevied.
-   * @param pending_queue_size is the max depth of message cache queue.
-   * @warning the recevied messages is enqueue a queue,the queue's depth is
-   * pending_queue_size
-   */
+  * @brief Constuctor a Reader object.
+  *
+  * @param role_attr is a protobuf message RoleAttributes, which includes the
+  * channel name and other info.
+  * @param reader_func is the callback function, when the message is recevied.
+  * @param pending_queue_size is the max depth of message cache queue.
+  * @warning the recevied messages is enqueue a queue,the queue's depth is
+  * pending_queue_size
+  */
   explicit Reader(const proto::RoleAttributes& role_attr,
                   const CallbackFunc<MessageT>& reader_func = nullptr,
                   uint32_t pending_queue_size = DEFAULT_PENDING_QUEUE_SIZE);
