b54ecbd3b03f45054848c97fb6a299deb8d5bc28
Calvin Miao <jhmiao@gmail.com>
2019-05-30 16:59:37 -0700

    Localization: ignore temp file dump in git history

--- modules/localization/msf/local_map/ndt_map/ndt_map_test.cc
+++ modules/localization/msf/local_map/ndt_map/ndt_map_test.cc
@@ -20,6 +20,7 @@
 #include <string>
 #include <vector>
 
+#include "cyber/common/file.h"
 #include "modules/common/util/string_util.h"
 #include "modules/localization/msf/common/io/pcl_point_types.h"
 #include "modules/localization/msf/common/io/velodyne_utility.h"
@@ -34,9 +35,13 @@ namespace msf {
 TEST(MapNdtTestSuite, matrix) {
   const std::string pcd_folder =
       "/apollo/modules/localization/msf/local_map/test_data/ndt_map/pcds";
+  EXPECT_TRUE(apollo::cyber::common::EnsureDirectory(pcd_folder));
   const std::string pose_files = pcd_folder + "/poses.txt";
+
   const std::string map_base_folder =
       "/apollo/modules/localization/msf/local_map/test_data/ndt_map/map_data";
+  EXPECT_TRUE(apollo::cyber::common::EnsureDirectory(map_base_folder));
+
   std::vector<Eigen::Affine3d> pcd_poses;
   std::vector<double> time_stamps;
   std::vector<unsigned int> pcd_indices;
