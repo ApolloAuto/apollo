4bac46c8440d4d2c85fa2b4871586166cdb16b0d
Shu Jiang <shujiang2016@gmail.com>
2019-05-30 14:03:33 -0700

    control: query time nearest trajectory point as current target point only

--- modules/control/common/control_gflags.cc
+++ modules/control/common/control_gflags.cc
@@ -66,8 +66,8 @@ DEFINE_bool(
 DEFINE_bool(enable_maximum_steer_rate_limit, false,
             "Enable steer rate limit obtained from vehicle_param.pb.txt");
 
-DEFINE_bool(query_time_nearest_point_only, false,
+DEFINE_bool(query_time_nearest_point_only, true,
             "only use the trajectory point at nearest time as target point");
 
-DEFINE_bool(query_forward_time_point_only, false,
+DEFINE_bool(query_forward_time_point_only, true,
             "only use the trajectory point in future");
