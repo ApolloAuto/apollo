9cf46c530185d57b7cbedf9675ccb8946b236e1f
Qi Luo <qi.stephen.luo@gmail.com>
2019-06-04 18:25:09 -0700

    Planning: mute redudant log in stage approach

--- modules/planning/scenarios/park/pull_over/stage_approach.cc
+++ modules/planning/scenarios/park/pull_over/stage_approach.cc
@@ -38,7 +38,7 @@ PullOverStageApproach::PullOverStageApproach(
 
 Stage::StageStatus PullOverStageApproach::Process(
     const TrajectoryPoint& planning_init_point, Frame* frame) {
-  AERROR << "stage: Approach";
+  ADEBUG << "stage: Approach";
   CHECK_NOTNULL(frame);
 
   scenario_config_.CopyFrom(GetContext()->scenario_config);
