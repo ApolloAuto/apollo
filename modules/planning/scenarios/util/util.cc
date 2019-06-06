f205381a2066e4e9292bcaa8e2f2ec0921d3f797
jmtao <taojm99@gmail.com>
2019-06-06 16:34:42 -0700

    planning: move common code out, to add more functions use shared code

--- modules/planning/scenarios/util/util.cc
+++ modules/planning/scenarios/util/util.cc
@@ -109,30 +109,20 @@ PullOverStatus CheckADCPullOver(const ReferenceLineInfo& reference_line_info,
     return APPOACHING;
   }
 
-  common::SLPoint adc_position_sl;
   const common::math::Vec2d adc_position = {
       common::VehicleStateProvider::Instance()->x(),
       common::VehicleStateProvider::Instance()->y()};
-  reference_line.XYToSL(adc_position, &adc_position_sl);
+  const common::math::Vec2d target_position = {
+      pull_over_status.position().x(), pull_over_status.position().y()};
 
-  const double s_diff = pull_over_sl.s() - adc_position_sl.s();
-  const double l_diff = std::fabs(pull_over_sl.l() - adc_position_sl.l());
+  const bool position_check = CheckPullOverPositionBySL(
+      reference_line_info, scenario_config,
+      adc_position,
+      common::VehicleStateProvider::Instance()->heading(),
+      target_position,
+      pull_over_status.theta());
 
-  const double theta_diff = std::fabs(common::math::NormalizeAngle(
-      pull_over_status.theta() -
-      common::VehicleStateProvider::Instance()->heading()));
-  ADEBUG << "adc_position_s[" << adc_position_sl.s() << "] adc_position_l["
-         << adc_position_sl.l() << "] pull_over_s[" << pull_over_sl.s()
-         << "] pull_over_l[" << pull_over_sl.l() << "] s_diff[" << s_diff
-         << "] l_diff[" << l_diff << "] theta_diff[" << theta_diff << "]";
-
-  if (s_diff >= 0 && s_diff <= scenario_config.max_s_error_to_end_point() &&
-      l_diff <= scenario_config.max_l_error_to_end_point() &&
-      theta_diff <= scenario_config.max_theta_error_to_end_point()) {
-    return PARK_COMPLETE;
-  } else {
-    return PARK_FAIL;
-  }
+  return position_check ? PARK_COMPLETE : PARK_FAIL;
 }
 
 PullOverStatus CheckADCPullOverOpenSpace(
@@ -153,31 +143,71 @@ PullOverStatus CheckADCPullOverOpenSpace(
   const common::math::Vec2d adc_position = {
       common::VehicleStateProvider::Instance()->x(),
       common::VehicleStateProvider::Instance()->y()};
+  const common::math::Vec2d target_position = {
+      pull_over_status.position().x(), pull_over_status.position().y()};
 
-  const common::math::Vec2d end_pose = {pull_over_status.position().x(),
-                                        pull_over_status.position().y()};
+  const bool position_check = CheckPullOverPositionByDistance(
+      scenario_config,
+      adc_position,
+      common::VehicleStateProvider::Instance()->heading(),
+      target_position,
+      pull_over_status.theta());
 
-  const double distance_diff = adc_position.DistanceTo(end_pose);
+  return position_check ? PARK_COMPLETE : PARK_FAIL;
+}
 
-  const double theta_diff = std::fabs(common::math::NormalizeAngle(
-      pull_over_status.theta() -
-      common::VehicleStateProvider::Instance()->heading()));
+bool CheckPullOverPositionBySL(const ReferenceLineInfo& reference_line_info,
+                               const ScenarioPullOverConfig& scenario_config,
+                               const common::math::Vec2d& adc_position,
+                               const double adc_theta,
+                               const common::math::Vec2d& target_position,
+                               const double target_theta) {
+  const auto& reference_line = reference_line_info.reference_line();
+  common::SLPoint target_sl;
+  reference_line.XYToSL(target_position, &target_sl);
+  common::SLPoint adc_position_sl;
+  reference_line.XYToSL(adc_position, &adc_position_sl);
 
-  if (distance_diff <= scenario_config.max_distance_error_to_end_point() &&
+  const double s_diff = target_sl.s() - adc_position_sl.s();
+  const double l_diff = std::fabs(target_sl.l() - adc_position_sl.l());
+  const double theta_diff =
+      std::fabs(common::math::NormalizeAngle(target_theta - adc_theta));
+  ADEBUG << "adc_position_s[" << adc_position_sl.s() << "] adc_position_l["
+         << adc_position_sl.l() << "] target_s[" << target_sl.s()
+         << "] target_l[" << target_sl.l() << "] s_diff[" << s_diff
+         << "] l_diff[" << l_diff << "] theta_diff[" << theta_diff << "]";
+
+  // check s/l/theta diff
+  if (s_diff >= 0 && s_diff <= scenario_config.max_s_error_to_end_point() &&
+      l_diff <= scenario_config.max_l_error_to_end_point() &&
       theta_diff <= scenario_config.max_theta_error_to_end_point()) {
-    return PARK_COMPLETE;
-  } else {
-    ADEBUG << "distance_diff[" << distance_diff << "] while distance threshold["
-           << scenario_config.max_distance_error_to_end_point() << "]";
-    ADEBUG << "theta_diff is[" << theta_diff << "] while theta threshold["
-           << scenario_config.max_theta_error_to_end_point() << "]";
-    return PARK_FAIL;
+    return true;
   }
 
-  return PARK_FAIL;
+  return false;
 }
-}  // namespace util
 
+bool CheckPullOverPositionByDistance(
+    const ScenarioPullOverConfig& scenario_config,
+    const common::math::Vec2d& adc_position,
+    const double adc_theta,
+    const common::math::Vec2d& target_position,
+    const double target_theta) {
+  const double distance_diff = adc_position.DistanceTo(target_position);
+  const double theta_diff =
+      std::fabs(common::math::NormalizeAngle(target_theta - adc_theta));
+  ADEBUG << "distance_diff[" << distance_diff
+         << "] theta_diff[" << theta_diff << "]";
+
+  // check distance/theta diff
+  if (distance_diff <= scenario_config.max_distance_error_to_end_point() &&
+      theta_diff <= scenario_config.max_theta_error_to_end_point()) {
+    return true;
+  }
+  return false;
+}
+
+}  // namespace util
 }  // namespace scenario
 }  // namespace planning
 }  // namespace apollo
