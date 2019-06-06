201ee10cf49151b25ef32bc1d4b2a9ed25abeadb
zhangwuzhao <zhangwuzhao@baidu.com>
2019-06-06 16:00:34 -0700

    cyber: croutine support for arm

--- cyber/croutine/detail/routine_context.cc
+++ cyber/croutine/detail/routine_context.cc
@@ -40,7 +40,11 @@ namespace croutine {
 void MakeContext(const func &f1, const void *arg, RoutineContext *ctx) {
   ctx->sp = ctx->stack + STACK_SIZE - 2 * sizeof(void *) - REGISTERS_SIZE;
   std::memset(ctx->sp, 0, REGISTERS_SIZE);
+#ifdef __aarch64__
+  char *sp = ctx->stack + STACK_SIZE - sizeof(void *);
+#else
   char *sp = ctx->stack + STACK_SIZE - 2 * sizeof(void *);
+#endif
   *reinterpret_cast<void **>(sp) = reinterpret_cast<void *>(f1);
   sp -= sizeof(void *);
   *reinterpret_cast<void **>(sp) = const_cast<void *>(arg);
