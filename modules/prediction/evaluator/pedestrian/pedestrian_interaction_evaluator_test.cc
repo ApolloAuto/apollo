9dab4bdfa93c496ec500131d7690242e2a68bc5f
kechxu <kechxu89@gmail.com>
2019-06-07 15:37:01 -0700

    Prediction: fix model error

--- modules/prediction/evaluator/pedestrian/pedestrian_interaction_evaluator_test.cc
+++ modules/prediction/evaluator/pedestrian/pedestrian_interaction_evaluator_test.cc
@@ -25,11 +25,29 @@ namespace prediction {
 
 class PedestrianInteractionEvaluatorTest : public KMLMapBasedTest {
  public:
-  void SetUp() override { return; }
+  void SetUp() override {
+    std::string file =
+        "modules/prediction/testdata/multiple_perception_pedestrians.pb.txt";
+    cyber::common::GetProtoFromFile(file, &perception_obstacles_);
+  }
 
  protected:
   apollo::perception::PerceptionObstacles perception_obstacles_;
 };
+
+TEST_F(PedestrianInteractionEvaluatorTest, Evaluate) {
+  EXPECT_DOUBLE_EQ(perception_obstacles_.header().timestamp_sec(),
+                   1501183430.161906);
+  apollo::perception::PerceptionObstacle perception_obstacle =
+      perception_obstacles_.perception_obstacle(0);
+  EXPECT_EQ(perception_obstacle.id(), 101);
+  ObstaclesContainer container;
+  container.Insert(perception_obstacles_);
+  Obstacle* obstacle_ptr = container.GetObstacle(101);
+  EXPECT_NE(obstacle_ptr, nullptr);
+  PedestrianInteractionEvaluator evaluator;
+  evaluator.Evaluate(obstacle_ptr);
+}
 
 }  // namespace prediction
 }  // namespace apollo
