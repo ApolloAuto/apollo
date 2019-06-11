d5e7834abb8931f2c4f1a12b3d3414233e61c293
zhouyao <zhouyao@baidu.com>
2019-06-11 22:48:34 +0800

    Fix some scripts && add tutorial for map generation.

--- scripts/msf_record_parser.sh
+++ scripts/msf_record_parser.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 if [ $# -lt 1 ]; then
-    echo "Usage: msf_monitor_data_exporter.sh [bags folder]"
+    echo "Usage: msf_record_parser.sh [records folder] [output folder]"
     exit 1;
 fi
 
@@ -13,7 +13,7 @@ GNSS_LOC_TOPIC="/apollo/localization/msf_gnss"
 LIDAR_LOC_TOPIC="/apollo/localization/msf_lidar" 
 FUSION_LOC_TOPIC="/apollo/localization/pose"
 ODOMETRY_LOC_TOPIC="/apollo/sensor/gnss/odometry"
-CLOUD_TOPIC="/apollo/sensor/velodyne64/compensator/PointCloud2"
+CLOUD_TOPIC="/apollo/sensor/lidar128/compensator/PointCloud2"
 
 GNSS_LOC_FILE="gnss_loc.txt"
 LIDAR_LOC_FILE="lidar_loc.txt"
@@ -21,11 +21,12 @@ FUSION_LOC_FILE="fusion_loc.txt"
 ODOMETRY_LOC_FILE="odometry_loc.txt"
 
 IN_FOLDER=$1
+OUT_FOLDER=$2
 
 function data_exporter() {
   local BAG_FILE=$1
   local OUT_FOLDER=$2
-  $APOLLO_BIN_PREFIX/modules/localization/msf/local_tool/data_extraction/monitor_data_exporter \
+  $APOLLO_BIN_PREFIX/modules/localization/msf/local_tool/data_extraction/cyber_record_parser \
     --bag_file $BAG_FILE \
     --out_folder $OUT_FOLDER \
     --cloud_topic $CLOUD_TOPIC \
@@ -36,9 +37,10 @@ function data_exporter() {
 }
 
 cd $IN_FOLDER
-for item in $(ls -l *.bag | awk '{print $9}')
+for item in $(ls -l *record.* | awk '{print $9}')
 do
-  DIR_NAME=$(echo $item | cut -d . -f 1)
-  mkdir $DIR_NAME
-  data_exporter "${item}" "${DIR_NAME}"
+  SEGMENTS=$(echo $item | awk -F'.' '{print NF}')
+  DIR_NAME=$(echo $item | cut -d . -f ${SEGMENTS})
+  mkdir -p $OUT_FOLDER/$DIR_NAME
+  data_exporter "${item}" "$OUT_FOLDER/$DIR_NAME"
 done
