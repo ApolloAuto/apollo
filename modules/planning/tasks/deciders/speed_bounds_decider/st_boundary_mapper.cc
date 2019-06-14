83dcc3357cb253a27940ad8a318e72c8a2611ce8
HongyiSun <henrysun@umich.edu>
2019-06-13 17:17:24 -0700

    Planning: quick fix for stop decision mapping (#8798)
    * Planning: quick fix for stop decision mapping
    
    * Planning: update integration test

--- modules/planning/tasks/deciders/speed_bounds_decider/st_boundary_mapper.cc
+++ modules/planning/tasks/deciders/speed_bounds_decider/st_boundary_mapper.cc
@@ -111,8 +111,9 @@ Status STBoundaryMapper::CreateStBoundary(PathDecision* path_decision) const {
 
     const auto& decision = obstacle->LongitudinalDecision();
     if (decision.has_stop()) {
-      const double stop_s = obstacle->PerceptionSLBoundary().start_s() +
-                            decision.stop().distance_s();
+      common::SLPoint stop_sl;
+      reference_line_.XYToSL(decision.stop().stop_point(), &stop_sl);
+      const double stop_s = stop_sl.s();
       // this is a rough estimation based on reference line s, so that a large
       // buffer is used.
       constexpr double stop_buff = 15.0;
@@ -161,15 +162,15 @@ bool STBoundaryMapper::MapStopDecision(
     Obstacle* stop_obstacle, const ObjectDecisionType& stop_decision) const {
   DCHECK(stop_decision.has_stop()) << "Must have stop decision";
 
-  if (stop_obstacle->PerceptionSLBoundary().start_s() >
-      adc_sl_boundary_.end_s() + planning_distance_) {
+  common::SLPoint stop_sl;
+  reference_line_.XYToSL(stop_decision.stop().stop_point(), &stop_sl);
+
+  if (stop_sl.s() > adc_sl_boundary_.end_s() + planning_distance_) {
     return true;
   }
 
   double st_stop_s = 0.0;
-  const double stop_ref_s = stop_obstacle->PerceptionSLBoundary().start_s() +
-                            stop_decision.stop().distance_s() -
-                            vehicle_param_.front_edge_to_center();
+  const double stop_ref_s = stop_sl.s() - vehicle_param_.front_edge_to_center();
 
   if (stop_ref_s > path_data_.frenet_frame_path().back().s()) {
     st_stop_s = path_data_.discretized_path().back().s() +
