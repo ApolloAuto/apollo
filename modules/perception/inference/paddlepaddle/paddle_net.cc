aed574f89901dd69431a05c7b6026d46bea1d35f
KaWaiTsoiBaidu <kawaitsoi@baidu.com>
2019-06-15 11:21:24 -0700

    Perception: clean up code and reformat

--- modules/perception/inference/paddlepaddle/paddle_net.cc
+++ modules/perception/inference/paddlepaddle/paddle_net.cc
@@ -14,6 +14,8 @@
  * limitations under the License.
  *****************************************************************************/
 
+#include <functional>
+
 #include "modules/perception/inference/paddlepaddle/paddle_net.h"
 
 #include "cyber/common/log.h"
@@ -22,9 +24,11 @@ namespace apollo {
 namespace perception {
 namespace inference {
 
-PaddleNet::PaddleNet(const std::string &model_file, const std::string &param_file,
+PaddleNet::PaddleNet(const std::string &model_file,
+                     const std::string &param_file,
                      const std::vector<std::string> &outputs)
-    : model_file_(model_file), param_file_(param_file), output_names_(outputs) {}
+    : model_file_(model_file), param_file_(param_file), output_names_(outputs)
+{}
 
 bool PaddleNet::Init(const std::map<std::string, std::vector<int>> &shapes) {
   paddle::AnalysisConfig config;
@@ -39,7 +43,7 @@ bool PaddleNet::Init(const std::map<std::string, std::vector<int>> &shapes) {
   if (predictor_ == nullptr) {
     return false;
   }
-  // TODO[KAWAI]: shapes should only include input blobs. 
+  // TODO(KAWAI) : shapes should only include input blobs.
   //              & a way to process multiple inputs.
   std::vector<int> input_shape;
   for (auto name_shape : shapes) {
@@ -54,7 +58,7 @@ bool PaddleNet::Init(const std::map<std::string, std::vector<int>> &shapes) {
   int input_num = std::accumulate(input_shape.begin(),
                                   input_shape.end(),
                                   1,
-                                  std::multiplies<int>()); 
+                                  std::multiplies<int>());
   std::vector<std::vector<float>> input_data(1);
   input_data[0].resize(input_num);
   for (int i = 0; i < input_num; i++) {
@@ -71,7 +75,6 @@ bool PaddleNet::Init(const std::map<std::string, std::vector<int>> &shapes) {
   }
   CHECK(predictor_->ZeroCopyRun());
   for (auto name : output_names_) {
-    AINFO << name;
     if (name_map_.find(name) == name_map_.end()) {
       continue;
     }
@@ -92,7 +95,8 @@ bool PaddleNet::Init(const std::map<std::string, std::vector<int>> &shapes) {
   return true;
 }
 
-PaddleNet::PaddleNet(const std::string &model_file, const std::string &param_file,
+PaddleNet::PaddleNet(const std::string &model_file,
+                     const std::string &param_file,
                      const std::vector<std::string> &outputs,
                      const std::vector<std::string> &inputs)
                     : model_file_(model_file),
@@ -102,8 +106,6 @@ PaddleNet::PaddleNet(const std::string &model_file, const std::string &param_fil
 
 std::shared_ptr<apollo::perception::base::Blob<float>> PaddleNet::get_blob(
     const std::string &name) {
-  AINFO <<  "check1";
-  AINFO << name;
   auto iter = blobs_.find(name);
   if (iter == blobs_.end()) {
     return nullptr;
@@ -120,7 +122,7 @@ bool PaddleNet::reshape() {
       std::vector<int> paddle_blob_shape = paddle_blob->shape();
       int count = std::accumulate(paddle_blob_shape.begin(),
                                   paddle_blob_shape.end(),
-                                  1, 
+                                  1,
                                   std::multiplies<int>());
       cudaMemcpy(paddle_blob->mutable_data<float>(paddle::PaddlePlace::kGPU),
                  blob->gpu_data(),
@@ -156,17 +158,16 @@ void PaddleNet::Infer() {
     auto paddle_blob = predictor_->GetOutputTensor(name_map_[name]);
     if (paddle_blob != nullptr && blob != nullptr) {
       blob->Reshape(paddle_blob->shape());
-      // TODO[KaWai] : use the output_size as the count;
+      // TODO(KaWai) : use the output_size as the count;
       std::vector<int> paddle_blob_shape = paddle_blob->shape();
       int count = std::accumulate(paddle_blob_shape.begin(),
                                   paddle_blob_shape.end(),
                                   1,
                                   std::multiplies<int>());
-      //int output_size;
-      //paddle::PaddlePlace* place;
+      // int output_size;
+      // paddle::PaddlePlace* place;
       cudaMemcpy(blob->mutable_gpu_data(),
                  paddle_blob->mutable_data<float>(paddle::PaddlePlace::kGPU),
-                                          //&output_size),
                  count * sizeof(float), cudaMemcpyDeviceToDevice);
     }
   }
@@ -177,7 +178,7 @@ bool PaddleNet::shape(const std::string &name, std::vector<int> *res) {
   bool in_output = false;
   if (std::find(input_names_.begin(),
                 input_names_.end(),
-                name) != input_names_.end()){
+                name) != input_names_.end()) {
     in_input = true;
   } else if (std::find(output_names_.begin(),
                        output_names_.end(),
