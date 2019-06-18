f5a88f63bbde1eb0a9339ff5a57ac6adc1729e94
Yifei Jiang <jiangyifei@gmail.com>
2019-06-18 12:39:43 -0700

    tools: refactored record reader.

--- modules/tools/plot_planning/record_reader.py
+++ modules/tools/plot_planning/record_reader.py
@@ -26,15 +26,16 @@ class RecordItemReader:
     def read(self, topics):
         reader = RecordReader(self.record_file)
         for msg in reader.read_messages():
-            if msg.topic in topics and msg.topic == "/apollo/canbus/chassis":
+            if msg.topic not in topics:
+                continue
+            if msg.topic == "/apollo/canbus/chassis":
                 chassis = chassis_pb2.Chassis()
                 chassis.ParseFromString(msg.message)
                 data = {"chassis": chassis}
                 yield data
 
-            if msg.topic in topics and msg.topic == "/apollo/localization/pose":
+            if msg.topic == "/apollo/localization/pose":
                 location_est = localization_pb2.LocalizationEstimate()
                 location_est.ParseFromString(msg.message)
                 data = {"pose": location_est}
-                yield data
-        return                    
+                yield data                   
