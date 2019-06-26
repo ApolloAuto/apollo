95545b18f4bf7c2e390624188fb061eac927b170
kechxu <kechxu89@gmail.com>
2019-06-26 12:40:39 -0700

    Prediction: refactor offline scripts: noenable multi thread and use new sunnyvale map

--- modules/tools/prediction/data_pipelines/scripts/records_to_prediction_result.sh
+++ modules/tools/prediction/data_pipelines/scripts/records_to_prediction_result.sh
@@ -27,7 +27,7 @@ source /apollo/cyber/setup.bash
 sudo mkdir -p ${TARGET_DIR}
 
 if [ -z "$3" ]; then
-    MAP_DIR="sunnyvale_with_two_offices"
+    MAP_DIR="sunnyvale"
 else
     MAP_DIR=$3
 fi
@@ -37,4 +37,5 @@ fi
     --map_dir=/apollo/modules/map/data/${MAP_DIR} \
     --prediction_offline_mode=3 \
     --prediction_offline_bags=${SRC_DIR} \
+    --noenable_multi_thread \
     --prediction_data_dir=${TARGET_DIR}
