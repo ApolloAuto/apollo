a2070e8f6174f67e0c4d6c7c58c4038c823d159e
xmyqsh <xmyqsh@gmail.com>
2019-07-16 22:20:07 +0800

    AINFO --> ADEBUG

--- modules/bridge/test/bridge_sender_test.cc
+++ modules/bridge/test/bridge_sender_test.cc
@@ -87,7 +87,7 @@ bool send(const std::string &remote_ip, uint16_t remote_port, uint32_t count) {
         ADEBUG << "sent msg failed ";
         break;
       }
-      AINFO << "sent " << nbytes << " bytes to server with sequence num " << i;
+      ADEBUG << "sent " << nbytes << " bytes to server with sequence num " << i;
     }
     close(sock_fd);
 
