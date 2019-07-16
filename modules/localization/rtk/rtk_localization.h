1396fbf0e14fa5f36361c60f4651ae3021196a49
fengliu00 <fengliu@baidu.com>
2019-07-15 18:57:51 -0700

    Localization: fixed ticket 3757 on GPS/IMU timestamp

--- modules/localization/rtk/rtk_localization.h
+++ modules/localization/rtk/rtk_localization.h
@@ -93,6 +93,7 @@ class RTKLocalization {
 
   bool enable_watch_dog_ = true;
   bool service_started_ = false;
+  double service_started_time = 0.0;
 
   int64_t localization_seq_num_ = 0;
   LocalizationEstimate last_localization_result_;
@@ -100,6 +101,7 @@ class RTKLocalization {
 
   int localization_publish_freq_ = 100;
   int report_threshold_err_num_ = 10;
+  int service_delay_threshold = 1;
   apollo::common::monitor::MonitorLogBuffer monitor_logger_;
 
   FRIEND_TEST(RTKLocalizationTest, InterpolateIMU);
