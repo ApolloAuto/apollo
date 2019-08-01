44c13f6f37a4f17db0aad1cef132ebf1ad61fc9a
JasonZhou404 <zhoujiny404@gmail.com>
2019-08-01 13:45:22 -0700

    Planning: init sqp version of fem pos deviation smoother

--- modules/planning/math/discretized_points_smoothing/fem_pos_deviation_smoother.cc
+++ modules/planning/math/discretized_points_smoothing/fem_pos_deviation_smoother.cc
@@ -28,6 +28,7 @@
 #include "cyber/common/log.h"
 #include "modules/planning/math/discretized_points_smoothing/fem_pos_deviation_ipopt_interface.h"
 #include "modules/planning/math/discretized_points_smoothing/fem_pos_deviation_osqp_interface.h"
+#include "modules/planning/math/discretized_points_smoothing/fem_pos_deviation_sqp_osqp_interface.h"
 
 namespace apollo {
 namespace planning {
@@ -40,14 +41,18 @@ bool FemPosDeviationSmoother::Solve(
     const std::vector<double>& bounds, std::vector<double>* opt_x,
     std::vector<double>* opt_y) {
   if (config_.apply_curvature_constraint()) {
-    return SolveWithIpopt(raw_point2d, bounds, opt_x, opt_y);
+    if (config_.use_sqp()) {
+      return SqpWithOsqp(raw_point2d, bounds, opt_x, opt_y);
+    } else {
+      return NlpWithIpopt(raw_point2d, bounds, opt_x, opt_y);
+    }
   } else {
-    return SolveWithOsqp(raw_point2d, bounds, opt_x, opt_y);
+    return QpWithOsqp(raw_point2d, bounds, opt_x, opt_y);
   }
   return true;
 }
 
-bool FemPosDeviationSmoother::SolveWithOsqp(
+bool FemPosDeviationSmoother::QpWithOsqp(
     const std::vector<std::pair<double, double>>& raw_point2d,
     const std::vector<double>& bounds, std::vector<double>* opt_x,
     std::vector<double>* opt_y) {
@@ -77,7 +82,37 @@ bool FemPosDeviationSmoother::SolveWithOsqp(
   return true;
 }
 
-bool FemPosDeviationSmoother::SolveWithIpopt(
+bool FemPosDeviationSmoother::SqpWithOsqp(
+    const std::vector<std::pair<double, double>>& raw_point2d,
+    const std::vector<double>& bounds, std::vector<double>* opt_x,
+    std::vector<double>* opt_y) {
+  CHECK_NOTNULL(opt_x);
+  CHECK_NOTNULL(opt_y);
+
+  FemPosDeviationSqpOsqpInterface solver;
+
+  solver.set_weight_fem_pos_deviation(config_.weight_fem_pos_deviation());
+  solver.set_weight_path_length(config_.weight_path_length());
+  solver.set_weight_ref_deviation(config_.weight_ref_deviation());
+  solver.set_max_iter(config_.max_iter());
+  solver.set_time_limit(config_.time_limit());
+  solver.set_verbose(config_.verbose());
+  solver.set_scaled_termination(config_.scaled_termination());
+  solver.set_warm_start(config_.warm_start());
+
+  solver.set_ref_points(raw_point2d);
+  solver.set_bounds_around_refs(bounds);
+
+  if (!solver.Solve()) {
+    return false;
+  }
+
+  *opt_x = solver.opt_x();
+  *opt_y = solver.opt_y();
+  return true;
+}
+
+bool FemPosDeviationSmoother::NlpWithIpopt(
     const std::vector<std::pair<double, double>>& raw_point2d,
     const std::vector<double>& bounds, std::vector<double>* opt_x,
     std::vector<double>* opt_y) {
