c10284c9ea3f9ade9afd71a5cf946130f4220885
muleisheng <muleisheng@126.com>
2019-08-05 15:50:06 +0800

    Bridge: add multi receiver component

---
+++ modules/bridge/common/bridge_proto_diser_buf_factory.h
@@ -0,0 +1,38 @@
+/******************************************************************************der
+ * Copyright 2019 The Apollo Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *****************************************************************************/
+
+#pragma once
+
+#include <string>
+#include "modules/bridge/common/bridge_proto_diserialized_buf.h"
+#include "modules/canbus/proto/chassis.pb.h"
+
+namespace apollo {
+namespace bridge {
+
+class ProtoDiserializedBufBaseFactory {
+ public:
+  static ProtoDiserializedBufBase *CreateObj(const BridgeHeader &header) {
+    ProtoDiserializedBufBase *obj = nullptr;
+    if (strcmp("Chassis", header.GetMsgName().c_str()) == 0) {
+      obj = new BridgeProtoDiserializedBuf<canbus::Chassis>;
+    }
+    return obj;
+  }
+};
+
+}  // namespace bridge
+}  // namespace apollo
