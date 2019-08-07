4f5fb4463ec1c607b17fb62663c553b830f6a1f3
fengzongbao <mickeyouou@gmail.com>
2019-08-07 14:57:10 +0800

    * align channel info for cyber_recorder info record

--- cyber/tools/cyber_recorder/info.cc
+++ cyber/tools/cyber_recorder/info.cc
@@ -91,7 +91,7 @@ bool Info::Display(const std::string& file) {
   }
 
   // channel info
-  std::cout << std::setw(w) << "channel_info: ";
+  std::cout << std::setw(w) << "channel_info: " << std::endl;
   Index idx = file_reader.GetIndex();
   for (int i = 0; i < idx.indexes_size(); ++i) {
     ChannelCache* cache = idx.mutable_indexes(i)->mutable_channel_cache();
