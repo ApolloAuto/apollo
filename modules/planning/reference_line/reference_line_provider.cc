39829211ca4976763e2bbcbd259106dfc33addca
xmyqsh <xmyqsh@gmail.com>
2019-08-13 05:24:05 +0800

    Planning: add lock for pnc map

--- modules/planning/reference_line/reference_line_provider.cc
+++ modules/planning/reference_line/reference_line_provider.cc
@@ -563,6 +563,7 @@ bool ReferenceLineProvider::CreateReferenceLine(
   bool is_new_routing = false;
   {
     // Update routing in pnc_map
+    std::lock_guard<std::mutex> lock(pnc_map_mutex_);
     if (pnc_map_->IsNewRouting(routing)) {
       is_new_routing = true;
       if (!pnc_map_->UpdateRoutingResponse(routing)) {
