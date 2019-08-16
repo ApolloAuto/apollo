2773c57de70ff7b2bb40f4d3fc8d0081388c2508
Yifei Jiang <jiangyifei@gmail.com>
2019-08-16 10:52:35 -0700

    tools: added chassis speed plot

--- modules/tools/plot_planning/chassis_speed.py
+++ modules/tools/plot_planning/chassis_speed.py
@@ -16,55 +16,41 @@
 # limitations under the License.
 ###############################################################################
 
-from record_reader import RecordItemReader
 import math
+from record_reader import RecordItemReader
 
-class TimeSpeedData:
-    
+
+class ChassisSpeed:
+
     def __init__(self):
         self.timestamp_list = []
         self.speed_list = []
-        self.imu_timestamp_list = []
-        self.imu_speed_list = []
 
-        self.last_speed_mps = None
-        self.last_imu_speed = None
-
-    def add(self, location_est, chassis):
+    def add(self, chassis):
         timestamp_sec = chassis.header.timestamp_sec
         speed_mps = chassis.speed_mps
 
-        imu_based_timestamp = location_est.measurement_time
-        self.imu_timestamp_list.append(imu_based_timestamp)
-
-        imu_based_speed = location_est.pose.linear_velocity.x \
-            * math.cos(location_est.pose.heading) + \
-                location_est.pose.linear_velocity.y * \
-                    math.sin(location_est.pose.heading)
-        self.imu_speed_list.append(imu_based_speed)
-            
         self.timestamp_list.append(timestamp_sec)
         self.speed_list.append(speed_mps)
 
-        self.last_speed_mps = speed_mps
+    def get_speed_list(self):
+        return self.speed_list
 
-    def get_imu_based_time_speed(self):
-        return self.imu_timestamp_list, self.imu_speed_list
+    def get_timestamp_list(self):
+        return self.timestamp_list
 
-    def get_imu_based_lastest_speed(self):
-        if len(self.imu_speed_list) > 0:
-            return self.imu_speed_list[-1]
-        else:
-            return None 
-
-    def get_time_speed(self):
-        return self.timestamp_list, self.speed_list
-
-    def get_lastest(self):
+    def get_lastest_speed(self):
         if len(self.speed_list) > 0:
             return self.speed_list[-1]
         else:
-            return None 
+            return None
+
+    def get_lastest_timestamp(self):
+        if len(self.timestamp_list) > 0:
+            return self.timestamp_list[-1]
+        else:
+            return None
+
 
 if __name__ == "__main__":
     import sys
@@ -83,23 +69,19 @@ if __name__ == "__main__":
         fns = [f for f in listdir(folder) if isfile(join(folder, f))]
         for fn in fns:
             reader = RecordItemReader(folder+"/"+fn)
-            processor = TimeSpeedData()
-            last_pose_data = None
+            processor = ChassisSpeed()
             last_chassis_data = None
-            topics = ["/apollo/localization/pose", "/apollo/canbus/chassis"]
+            topics = ["/apollo/canbus/chassis"]
             for data in reader.read(topics):
-                if "pose" in data:
-                    last_pose_data = data["pose"]
                 if "chassis" in data:
                     last_chassis_data = data["chassis"]
-                if last_chassis_data is not None and last_pose_data is not None:
-                    processor.add(last_pose_data, last_chassis_data)
+                    processor.add(last_chassis_data)
                     last_pose_data = None
                     last_chassis_data = None
 
-            data_x, data_y = processor.get_time_speed()
-            ax.scatter(data_x, data_y, c=color, marker=marker, alpha=0.4)
-            data_x, data_y = processor.get_imu_based_time_speed()
-            ax.scatter(data_x, data_y, c="r", marker="+", alpha=0.4)
+            data_x = processor.get_timestamp_list()
+            data_y = processor.get_speed_list()
 
-    plt.show() 
+            ax.scatter(data_x, data_y, c=color, marker=marker, alpha=0.4)
+
+    plt.show()
