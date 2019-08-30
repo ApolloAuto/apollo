397717f3dd99d0bbc0136406f264e7a3f03ff189
Shu Jiang <shujiang2016@gmail.com>
2019-08-30 10:47:35 -0700

    Planning: path_reuse_decider compared stop decisions only

--- modules/planning/tasks/deciders/path_reuse_decider/path_reuse_decider.cc
+++ modules/planning/tasks/deciders/path_reuse_decider/path_reuse_decider.cc
@@ -28,9 +28,8 @@ namespace planning {
 
 using apollo::common::Status;
 
-namespace {
-static int reusable_path_counter = 0;
-}
+int PathReuseDecider::reusable_path_counter_ = 0;
+int PathReuseDecider::total_path_counter_ = 0;
 
 PathReuseDecider::PathReuseDecider(const TaskConfig& config)
     : Decider(config) {}
@@ -41,53 +40,77 @@ Status PathReuseDecider::Process(Frame* const frame,
   CHECK_NOTNULL(frame);
   CHECK_NOTNULL(reference_line_info);
 
-  bool enable_path_reuse = true;
-
   // Check if path is reusable
-  if (enable_path_reuse && CheckPathReusable(frame)) {
+  if (Decider::config_.path_reuse_decider_config().reuse_path() &&
+      CheckPathReusable(frame)) {
     // count reusable path
-    ++reusable_path_counter;
+    ++reusable_path_counter_;
   }
+  ++total_path_counter_;
   return Status::OK();
 }
 
 bool PathReuseDecider::CheckPathReusable(Frame* const frame) {
-  if (history_->GetLastFrame() == nullptr) {
+  if (history_->GetLastFrame() == nullptr) return false;
+  const std::vector<const HistoryObjectDecision*>
+      history_stop_objects_decisions =
+          history_->GetLastFrame()->GetStopObjectDecisions();
+  std::vector<common::PointENU> history_stop_positions;
+  std::vector<common::PointENU> current_stop_positions;
+  GetCurrentStopPositions(frame, &current_stop_positions);
+  GetHistoryStopPositions(history_stop_objects_decisions,
+                          &history_stop_positions);
+  if (current_stop_positions.size() != history_stop_positions.size()) {
     return false;
   } else {
-    auto obstacles = frame->obstacles();
-    for (auto obstacle : obstacles) {
-      const std::string& id = obstacle->Id();
-      auto history_decisions = history_->GetLastFrame()
-                                   ->GetObjectDecisionsById(id)
-                                   ->GetObjectDecision();
-      // if there are new objects in current frame than previous path is
-      // non-reusable
-      if (history_decisions.empty())
+    for (size_t i = 0; i < current_stop_positions.size(); ++i) {
+      if (current_stop_positions[i].x() != history_stop_positions[i].x() &&
+          current_stop_positions[i].y() != history_stop_positions[i].y())
         return false;
-      else
-        return CompObjectDecision(obstacle->decisions(), history_decisions);
     }
   }
   return true;
 }
 
-bool PathReuseDecider::CompObjectDecision(
-    const std::vector<ObjectDecisionType>& current_decisions,
-    const std::vector<const ObjectDecisionType*> history_decisions) {
-  std::vector<const ObjectDecisionType*> result;
-  for (size_t i = 0; i < current_decisions.size(); i++) {
-    result.push_back(&(current_decisions[i]));
+// get current stop positions
+void PathReuseDecider::GetCurrentStopPositions(
+    Frame* frame, std::vector<common::PointENU>* current_stop_positions) {
+  auto obstacles = frame->obstacles();
+  for (auto obstacle : obstacles) {
+    const std::vector<ObjectDecisionType>& current_decisions =
+        obstacle->decisions();
+    for (auto current_decision : current_decisions) {
+      if (current_decision.has_stop())
+        current_stop_positions->emplace_back(
+            current_decision.stop().stop_point());
+    }
   }
-  std::sort(result.begin(), result.end(),
-            [](const ObjectDecisionType* lhs, const ObjectDecisionType* rhs) {
-              return lhs->object_tag_case() < rhs->object_tag_case();
-            });
-  for (size_t i = 0; i < result.size(); ++i) {
-    if (result[i]->object_tag_case() != history_decisions[i]->object_tag_case())
-      return false;
+  // sort
+  std::sort(
+      current_stop_positions->begin(), current_stop_positions->end(),
+      [](const common::PointENU lhs, const common::PointENU rhs) {
+        return (lhs.x() < rhs.x() || (lhs.x() == rhs.x() && lhs.y() < rhs.y()));
+      });
+}
+
+// get history stop positions
+void PathReuseDecider::GetHistoryStopPositions(
+    const std::vector<const HistoryObjectDecision*>& history_objects_decisions,
+    std::vector<common::PointENU>* history_stop_positions) {
+  for (auto history_object_decision : history_objects_decisions) {
+    const std::vector<const ObjectDecisionType*> decisions =
+        history_object_decision->GetObjectDecision();
+    for (const ObjectDecisionType* decision : decisions) {
+      if (decision->has_stop())
+        history_stop_positions->emplace_back(decision->stop().stop_point());
+    }
   }
-  return true;
+  // sort
+  std::sort(
+      history_stop_positions->begin(), history_stop_positions->end(),
+      [](const common::PointENU lhs, const common::PointENU rhs) {
+        return (lhs.x() < rhs.x() || (lhs.x() == rhs.x() && lhs.y() < rhs.y()));
+      });
 }
 
 }  // namespace planning
