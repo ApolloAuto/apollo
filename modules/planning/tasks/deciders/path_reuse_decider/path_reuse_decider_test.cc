22394f10b942570a4c0249b1564e3434551c1829
Shu Jiang <shujiang2016@gmail.com>
2019-09-04 16:55:38 -0700

    Planning: added a unit test for path_reuse_decider

--- modules/planning/tasks/deciders/path_reuse_decider/path_reuse_decider_test.cc
+++ modules/planning/tasks/deciders/path_reuse_decider/path_reuse_decider_test.cc
@@ -13,6 +13,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *****************************************************************************/
+#define private public
+#define protected public
 
 #include "modules/planning/tasks/deciders/path_reuse_decider/path_reuse_decider.h"
 
@@ -39,6 +41,30 @@ TEST_F(PathReuseDeciderTest, Init) {
   PathReuseDecider path_reuse_decider(config_);
   EXPECT_EQ(path_reuse_decider.Name(),
             TaskConfig::TaskType_Name(config_.task_type()));
+}
+
+TEST_F(PathReuseDeciderTest, GetHistoryStopPositions) {
+  PathReuseDecider path_reuse_decider(config_);
+  History* history = History::Instance();
+  ADCTrajectory adc_trajectory;
+  EXPECT_TRUE(apollo::cyber::common::GetProtoFromFile(
+      "/apollo/modules/planning/testdata/common/history_01.pb.txt",
+      &adc_trajectory));
+  history->Add(adc_trajectory);
+  const std::vector<const HistoryObjectDecision*>
+      history_stop_objects_decisions =
+          history->GetLastFrame()->GetStopObjectDecisions();
+  std::vector<const common::PointENU*> history_stop_positions;
+  path_reuse_decider.GetHistoryStopPositions(history_stop_objects_decisions,
+                                             &history_stop_positions);
+  common::PointENU stop_position;
+  double stop_x = 586261.33054620528;
+  double stop_y = 4141304.5678338786;
+  stop_position.set_x(stop_x);
+  stop_position.set_y(stop_y);
+  const common::PointENU* point = history_stop_positions[1];
+  EXPECT_DOUBLE_EQ(point->x(), stop_position.x());
+  EXPECT_DOUBLE_EQ(point->y(), stop_position.y());
 }
 
 }  // namespace planning
