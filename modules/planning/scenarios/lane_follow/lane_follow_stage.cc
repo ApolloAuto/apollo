3a740b75e916c4abff5edab62e172767026e537a
JiaxuanXu <jx.jason.xu@gmail.com>
2019-09-13 14:08:30 -0700

    Planning: deprecate IsClearToChangeLane() checking for new lane-change policy

--- modules/planning/scenarios/lane_follow/lane_follow_stage.cc
+++ modules/planning/scenarios/lane_follow/lane_follow_stage.cc
@@ -127,8 +127,7 @@ Stage::StageStatus LaneFollowStage::Process(
     if (cur_status.ok()) {
       if (reference_line_info.IsChangeLanePath()) {
         ADEBUG << "reference line is lane change ref.";
-        if (reference_line_info.Cost() < kStraightForwardLineCost &&
-            LaneChangeDecider::IsClearToChangeLane(&reference_line_info)) {
+        if (reference_line_info.Cost() < kStraightForwardLineCost) {
           has_drivable_reference_line = true;
           reference_line_info.SetDrivable(true);
           ADEBUG << "\tclear for lane change";
