ad29bdd3d4cfefa7b2f7d32afe384061ac2b1592
Ning Qu <quning@gmail.com>
2019-09-13 15:31:03 -0700

    docker: update gcc related installations (#9679)

---
+++ docker/build/installers/config_gcc.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+###############################################################################
+# Copyright 2019 The Apollo Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+###############################################################################
+
+# Fail on first error.
+set -e
+
+cd "$(dirname "${BASH_SOURCE[0]}")"
+
+ARCH=$(uname -m)
+
+update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-8 80 --slave /usr/bin/g++ g++ /usr/bin/g++-8 --slave /usr/bin/gcov gcov /usr/bin/gcov-8
+update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-7 70 --slave /usr/bin/g++ g++ /usr/bin/g++-7 --slave /usr/bin/gcov gcov /usr/bin/gcov-7
+
+GCCV=`gcc --version | grep \)\ 8.*.0$ `
+GPLUSV=`g++ --version | grep \)\ 8.*.0$ `
+
+if [ -z "$GCCV" ] || [ -z "$GPLUSV" ]; then
+    echo "gcc-8/g++-8 installation or config failed"
+    exit 1;
+fi
+
+echo "Default gcc version: $GCCV" 
+echo "Default g++ version: $GPLUSV" 
