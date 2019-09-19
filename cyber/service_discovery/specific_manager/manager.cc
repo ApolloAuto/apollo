abc94e4fdaa58dc6a719fa6c519ea5d9718921c2
fengqikai1414 <fengqikai1414@126.com>
2019-09-19 20:03:04 +0800

    cyber: add lock protection for service discovery manager

--- cyber/service_discovery/specific_manager/manager.cc
+++ cyber/service_discovery/specific_manager/manager.cc
@@ -67,9 +67,12 @@ void Manager::StopDiscovery() {
     return;
   }
 
-  if (publisher_ != nullptr) {
-    eprosima::fastrtps::Domain::removePublisher(publisher_);
-    publisher_ = nullptr;
+  {
+    std::lock_guard<std::mutex> lg(lock_);
+    if (publisher_ != nullptr) {
+      eprosima::fastrtps::Domain::removePublisher(publisher_);
+      publisher_ = nullptr;
+    }
   }
 
   if (subscriber_ != nullptr) {
@@ -200,13 +203,16 @@ void Manager::OnRemoteChange(const std::string& msg_str) {
 bool Manager::Publish(const ChangeMsg& msg) {
   if (!is_discovery_started_.load()) {
     ADEBUG << "discovery is not started.";
-    return true;
+    return false;
   }
 
   apollo::cyber::transport::UnderlayMessage m;
   RETURN_VAL_IF(!message::SerializeToString(msg, &m.data()), false);
-  if (publisher_ != nullptr) {
-    return publisher_->write(reinterpret_cast<void*>(&m));
+  {
+    std::lock_guard<std::mutex> lg(lock_);
+    if (publisher_ != nullptr) {
+      return publisher_->write(reinterpret_cast<void*>(&m));
+    }
   }
   return true;
 }
