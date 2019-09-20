467bffb2354328c7627eeb002279966905dc41d8
Wang Yu <yuwang01@baidu.com>
2019-09-19 19:51:59 -0700

    Canbus: refine the Lexus throttle scaling with garage test data

--- modules/canbus/vehicle/lexus/protocol/accel_cmd_100.cc
+++ modules/canbus/vehicle/lexus/protocol/accel_cmd_100.cc
@@ -123,7 +123,9 @@ Accelcmd100* Accelcmd100::set_accel_cmd(double accel_cmd) {
 // 'len': 16, 'is_signed_var': False, 'physical_range': '[0|1]', 'bit': 15,
 // 'type': 'double', 'order': 'motorola', 'physical_unit': ''}
 void Accelcmd100::set_p_accel_cmd(uint8_t* data, double accel_cmd) {
-  accel_cmd = std::max(0.0, (accel_cmd - 8.0) / 100);
+  const double scaling_bias = 7.853;  // estimated from the garage test data
+  const double scaling_gain = 1.176;  // estimated from the garage test data
+  accel_cmd = std::max(0.0, (accel_cmd - scaling_bias) / (scaling_gain * 100));
   accel_cmd = ProtocolData::BoundedValue(0.0, 1.0, accel_cmd);
   // TODO(AS): fix this scaling.
   int x = static_cast<int>(accel_cmd / 0.001000);
