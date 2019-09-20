2bf79c1fb5e3eb98040b145063d65623dfa66040
zhangchao19 <zhangchao19@baidu.com>
2019-09-20 14:29:33 +0800

    cyber: update perf event

--- cyber/setup.bash
+++ cyber/setup.bash
@@ -26,9 +26,6 @@ export GLOG_alsologtostderr=0
 export GLOG_colorlogtostderr=1
 export GLOG_minloglevel=0
 
-export cyber_trans_perf=0
-export cyber_sched_perf=0
-
 # for DEBUG log
 #export GLOG_minloglevel=-1
 #export GLOG_v=4
