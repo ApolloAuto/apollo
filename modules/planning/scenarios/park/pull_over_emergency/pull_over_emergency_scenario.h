906951f293d3a81c84e571f4e8392b8362d20392
jmtao <taojm99@gmail.com>
2019-09-26 17:15:29 -0700

    planning: pull_over_emergency scenario

---
+++ modules/planning/scenarios/park/pull_over_emergency/pull_over_emergency_scenario.h
@@ -0,0 +1,68 @@
+/******************************************************************************
+ * Copyright 2019 The Apollo Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *****************************************************************************/
+
+/**
+ * @file
+ **/
+
+#pragma once
+
+#include <memory>
+
+#include "modules/common/util/factory.h"
+#include "modules/planning/common/frame.h"
+#include "modules/planning/scenarios/scenario.h"
+
+namespace apollo {
+namespace planning {
+namespace scenario {
+namespace pull_over_emergency {
+
+// stage context
+struct PullOverEmergencyContext {
+  ScenarioPullOverEmergencyConfig scenario_config;
+};
+
+class PullOverEmergencyScenario : public Scenario {
+ public:
+  PullOverEmergencyScenario(
+      const ScenarioConfig& config, const ScenarioContext* context)
+      : Scenario(config, context) {}
+
+  void Init() override;
+
+  std::unique_ptr<Stage> CreateStage(
+      const ScenarioConfig::StageConfig& stage_config);
+
+  PullOverEmergencyContext* GetContext() { return &context_; }
+
+ private:
+  static void RegisterStages();
+  bool GetScenarioConfig();
+
+ private:
+  static apollo::common::util::Factory<
+      ScenarioConfig::StageType, Stage,
+      Stage* (*)(const ScenarioConfig::StageConfig& stage_config)>
+      s_stage_factory_;
+  bool init_ = false;
+  PullOverEmergencyContext context_;
+};
+
+}  // namespace pull_over_emergency
+}  // namespace scenario
+}  // namespace planning
+}  // namespace apollo
