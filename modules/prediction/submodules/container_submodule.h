53065a15cd01785d49d555987c871f9f65d87215
kechxu <kechxu89@gmail.com>
2019-10-03 15:12:55 -0700

    Prediction: framework for container_submodule

---
+++ modules/prediction/submodules/container_submodule.h
@@ -0,0 +1,67 @@
+/******************************************************************************
+ * Copyright 2019 The Apollo Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *****************************************************************************/
+
+/**
+ * @file
+ * @brief Use container submodule to manage all containers
+ */
+
+#pragma once
+
+#include <memory>
+#include <string>
+
+#include "cyber/component/component.h"
+#include "modules/perception/proto/perception_obstacle.pb.h"
+
+/**
+ * @namespace apollo::prediction
+ * @brief apollo::prediction
+ */
+namespace apollo {
+namespace prediction {
+
+class ContainerSubmodule
+    : public cyber::Component<perception::PerceptionObstacles> {
+ public:
+  /**
+   * @brief Destructor
+   */
+  ~ContainerSubmodule();
+
+  /**
+   * @brief Get name of the node
+   * @return Name of the node
+   */
+  std::string Name() const;
+
+  /**
+   * @brief Initialize the node
+   * @return If initialized
+   */
+  bool Init() override;
+
+  /**
+   * @brief Data callback upon receiving a perception obstacle message.
+   * @param Perception obstacle message.
+   */
+  bool Proc(const std::shared_ptr<perception::PerceptionObstacles>&) override;
+};
+
+CYBER_REGISTER_COMPONENT(ContainerSubmodule)
+
+}  // namespace prediction
+}  // namespace apollo
