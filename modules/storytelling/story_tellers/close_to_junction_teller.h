86734058063768b984ffba91694255105760e219
Xiangquan Xiao <xiaoxiangquan@gmail.com>
2019-10-07 17:53:17 -0700

    Storytelling: Add basic story teller.

---
+++ modules/storytelling/story_tellers/close_to_junction_teller.h
@@ -0,0 +1,31 @@
+/******************************************************************************
+ * Copyright 2019 The Apollo Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *****************************************************************************/
+
+#pragma once
+
+#include "modules/storytelling/story_tellers/base_teller.h"
+
+namespace apollo {
+namespace storytelling {
+
+class CloseToJunctionTeller : public BaseTeller {
+ public:
+  void Init() override;
+  void Update(Stories* stories) override;
+};
+
+}  // namespace storytelling
+}  // namespace apollo
