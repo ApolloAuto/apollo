c70647992f4b931214fe2c0fee8d064464c0c177
Wang Yu <yuwang01@baidu.com>
2019-10-14 11:21:54 -0700

    Common: add get_inputs/outputs member functions into digital_filter

--- modules/common/filters/digital_filter.cc
+++ modules/common/filters/digital_filter.cc
@@ -120,5 +120,13 @@ const std::vector<double> &DigitalFilter::numerators() const {
 
 double DigitalFilter::dead_zone() const { return dead_zone_; }
 
+const std::deque<double> &DigitalFilter::inputs_queue() const {
+  return x_values_;
+}
+
+const std::deque<double> &DigitalFilter::outputs_queue() const {
+  return y_values_;
+}
+
 }  // namespace common
 }  // namespace apollo
