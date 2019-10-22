50042be42a579f994a794e5b95c7d7e132c0ade3
sjiang2018 <shujiang2016@gmail.com>
2019-10-22 10:45:37 -0700

    control: implemented mpc_controller submodule

--- modules/control/submodules/mpc_controller_submodule.cc
+++ modules/control/submodules/mpc_controller_submodule.cc
@@ -17,13 +17,16 @@
 #include "modules/control/submodules/mpc_controller_submodule.h"
 #include "modules/common/adapters/adapter_gflags.h"
 #include "modules/common/time/time.h"
+#include "modules/common/vehicle_state/vehicle_state_provider.h"
 #include "modules/control/common/control_gflags.h"
 
 namespace apollo {
 namespace control {
 
 using apollo::canbus::Chassis;
+using apollo::common::ErrorCode;
 using apollo::common::Status;
+using apollo::common::VehicleStateProvider;
 using apollo::common::time::Clock;
 using apollo::localization::LocalizationEstimate;
 using apollo::planning::ADCTrajectory;
@@ -128,9 +131,130 @@ bool MPCControllerSubmodule::Proc() {
 
 Status MPCControllerSubmodule::ProduceControlCommand(
     ControlCommand *control_command) {
-  Status status = mpc_controller_.ComputeControlCommand(
-      &local_view_.localization, &local_view_.chassis, &local_view_.trajectory,
-      control_command);
+  // TODO(SHU): move to pre_processing submodule
+
+  {
+    std::lock_guard<std::mutex> lock(mutex_);
+    local_view_.chassis = latest_chassis_;
+    local_view_.trajectory = latest_trajectory_;
+    local_view_.localization = latest_localization_;
+  }
+
+  Status status = CheckInput(&local_view_);
+  // check data
+
+  if (!status.ok()) {
+    AERROR_EVERY(100) << "Control input data failed: "
+                      << status.error_message();
+    control_command->mutable_engage_advice()->set_advice(
+        apollo::common::EngageAdvice::DISALLOW_ENGAGE);
+    control_command->mutable_engage_advice()->set_reason(
+        status.error_message());
+    estop_ = true;
+    estop_reason_ = status.error_message();
+  } else {
+    Status status_ts = CheckTimestamp(local_view_);
+    if (!status_ts.ok()) {
+      AERROR << "Input messages timeout";
+      // estop_ = true;
+      status = status_ts;
+      if (local_view_.chassis.driving_mode() !=
+          apollo::canbus::Chassis::COMPLETE_AUTO_DRIVE) {
+        control_command->mutable_engage_advice()->set_advice(
+            apollo::common::EngageAdvice::DISALLOW_ENGAGE);
+        control_command->mutable_engage_advice()->set_reason(
+            status.error_message());
+      }
+    } else {
+      control_command->mutable_engage_advice()->set_advice(
+          apollo::common::EngageAdvice::READY_TO_ENGAGE);
+    }
+  }
+
+  // check estop
+  estop_ = mpc_controller_conf_.enable_persistent_estop()
+               ? estop_ || local_view_.trajectory.estop().is_estop()
+               : local_view_.trajectory.estop().is_estop();
+
+  if (local_view_.trajectory.estop().is_estop()) {
+    estop_ = true;
+    estop_reason_ = "estop from planning : ";
+    estop_reason_ += local_view_.trajectory.estop().reason();
+  }
+
+  if (local_view_.trajectory.trajectory_point().empty()) {
+    AWARN_EVERY(100) << "planning has no trajectory point. ";
+    estop_ = true;
+    estop_reason_ = "estop for empty planning trajectory, planning headers: " +
+                    local_view_.trajectory.header().ShortDebugString();
+  }
+
+  if (FLAGS_enable_gear_dirve_negative_speed_protection) {
+    const double kEpsilon = 0.001;
+    auto first_trajectory_point = local_view_.trajectory.trajectory_point(0);
+    if (local_view_.chassis.gear_location() == Chassis::GEAR_DRIVE &&
+        first_trajectory_point.v() < -1 * kEpsilon) {
+      estop_ = true;
+      estop_reason_ = "estop for negative speed when gear_drive";
+    }
+  }
+
+  if (!estop_) {
+    if (local_view_.chassis.driving_mode() == Chassis::COMPLETE_MANUAL) {
+      mpc_controller_.Reset();
+      AINFO_EVERY(100) << "Reset Controllers in Manual Mode";
+    }
+
+    auto debug = control_command->mutable_debug()->mutable_input_debug();
+    debug->mutable_localization_header()->CopyFrom(
+        local_view_.localization.header());
+    debug->mutable_canbus_header()->CopyFrom(local_view_.chassis.header());
+    debug->mutable_trajectory_header()->CopyFrom(
+        local_view_.trajectory.header());
+
+    if (local_view_.trajectory.is_replan()) {
+      latest_replan_trajectory_header_.CopyFrom(
+          local_view_.trajectory.header());
+    }
+
+    if (latest_replan_trajectory_header_.has_sequence_num()) {
+      debug->mutable_latest_replan_trajectory_header()->CopyFrom(
+          latest_replan_trajectory_header_);
+    }
+
+    Status status_compute = mpc_controller_.ComputeControlCommand(
+        &local_view_.localization, &local_view_.chassis,
+        &local_view_.trajectory, control_command);
+
+    if (!status_compute.ok()) {
+      AERROR << "Control main function failed"
+             << " with localization: "
+             << local_view_.localization.ShortDebugString()
+             << " with chassis: " << local_view_.chassis.ShortDebugString()
+             << " with trajectory: "
+             << local_view_.trajectory.ShortDebugString()
+             << " with cmd: " << control_command->ShortDebugString()
+             << " status:" << status_compute.error_message();
+      estop_ = true;
+      estop_reason_ = status_compute.error_message();
+      status = status_compute;
+    }
+  }
+
+  // if planning set estop, then no control process triggered
+  if (estop_) {
+    AWARN_EVERY(100) << "Estop triggered! No control core method executed!";
+    // set Estop command
+    control_command->set_speed(0);
+    control_command->set_throttle(0);
+    control_command->set_brake(mpc_controller_conf_.soft_estop_brake());
+    control_command->set_gear_location(Chassis::GEAR_DRIVE);
+  }
+  // check signal
+  if (local_view_.trajectory.decision().has_vehicle_signal()) {
+    control_command->mutable_signal()->CopyFrom(
+        local_view_.trajectory.decision().vehicle_signal());
+  }
   return status;
 }
 
@@ -139,6 +263,113 @@ void MPCControllerSubmodule::OnChassis(
   ADEBUG << "Received chassis data: run chassis callback.";
   std::lock_guard<std::mutex> lock(mutex_);
   latest_chassis_.CopyFrom(*chassis);
+}
+
+void MPCControllerSubmodule::OnPad(const std::shared_ptr<PadMessage> &pad) {
+  pad_msg_.CopyFrom(*pad);
+  ADEBUG << "Received Pad Msg:" << pad_msg_.DebugString();
+  AERROR_IF(!pad_msg_.has_action()) << "pad message check failed!";
+
+  // do something according to pad message
+  if (pad_msg_.action() == DrivingAction::RESET) {
+    AINFO << "Control received RESET action!";
+    estop_ = false;
+    estop_reason_.clear();
+  }
+  pad_received_ = true;
+}
+
+void MPCControllerSubmodule::OnPlanning(
+    const std::shared_ptr<ADCTrajectory> &trajectory) {
+  ADEBUG << "Received chassis data: run trajectory callback.";
+  std::lock_guard<std::mutex> lock(mutex_);
+  latest_trajectory_.CopyFrom(*trajectory);
+}
+
+void MPCControllerSubmodule::OnLocalization(
+    const std::shared_ptr<LocalizationEstimate> &localization) {
+  ADEBUG << "Received control data: run localization message callback.";
+  std::lock_guard<std::mutex> lock(mutex_);
+  latest_localization_.CopyFrom(*localization);
+}
+
+void MPCControllerSubmodule::OnMonitor(
+    const common::monitor::MonitorMessage &monitor_message) {
+  for (const auto &item : monitor_message.item()) {
+    if (item.log_level() == common::monitor::MonitorMessageItem::FATAL) {
+      estop_ = true;
+      return;
+    }
+  }
+}
+
+Status MPCControllerSubmodule::CheckInput(LocalView *local_view) {
+  ADEBUG << "Received localization:"
+         << local_view->localization.ShortDebugString();
+  ADEBUG << "Received chassis:" << local_view->chassis.ShortDebugString();
+
+  if (!local_view->trajectory.estop().is_estop() &&
+      local_view->trajectory.trajectory_point().empty()) {
+    AWARN_EVERY(100) << "planning has no trajectory point. ";
+    std::string msg("planning has no trajectory point. planning_seq_num:");
+    msg += std::to_string(local_view->trajectory.header().sequence_num());
+    return Status(ErrorCode::CONTROL_COMPUTE_ERROR, msg);
+  }
+
+  for (auto &trajectory_point :
+       *local_view->trajectory.mutable_trajectory_point()) {
+    if (std::abs(trajectory_point.v()) <
+            mpc_controller_conf_.minimum_speed_resolution() &&
+        std::abs(trajectory_point.a()) <
+            mpc_controller_conf_.max_acceleration_when_stopped()) {
+      trajectory_point.set_v(0.0);
+      trajectory_point.set_a(0.0);
+    }
+  }
+
+  VehicleStateProvider::Instance()->Update(local_view->localization,
+                                           local_view->chassis);
+
+  return Status::OK();
+}
+
+Status MPCControllerSubmodule::CheckTimestamp(const LocalView &local_view) {
+  if (!mpc_controller_conf_.enable_input_timestamp_check() ||
+      mpc_controller_conf_.is_control_test_mode()) {
+    ADEBUG << "Skip input timestamp check by gflags.";
+    return Status::OK();
+  }
+  double current_timestamp = Clock::NowInSeconds();
+  double localization_diff =
+      current_timestamp - local_view.localization.header().timestamp_sec();
+  if (localization_diff > (mpc_controller_conf_.max_localization_miss_num() *
+                           mpc_controller_conf_.localization_period())) {
+    AERROR << "Localization msg lost for " << std::setprecision(6)
+           << localization_diff << "s";
+    monitor_logger_buffer_.ERROR("Localization msg lost");
+    return Status(ErrorCode::CONTROL_COMPUTE_ERROR, "Localization msg timeout");
+  }
+
+  double chassis_diff =
+      current_timestamp - local_view.chassis.header().timestamp_sec();
+  if (chassis_diff > (mpc_controller_conf_.max_chassis_miss_num() *
+                      mpc_controller_conf_.chassis_period())) {
+    AERROR << "Chassis msg lost for " << std::setprecision(6) << chassis_diff
+           << "s";
+    monitor_logger_buffer_.ERROR("Chassis msg lost");
+    return Status(ErrorCode::CONTROL_COMPUTE_ERROR, "Chassis msg timeout");
+  }
+
+  double trajectory_diff =
+      current_timestamp - local_view.trajectory.header().timestamp_sec();
+  if (trajectory_diff > (mpc_controller_conf_.max_planning_miss_num() *
+                         mpc_controller_conf_.trajectory_period())) {
+    AERROR << "Trajectory msg lost for " << std::setprecision(6)
+           << trajectory_diff << "s";
+    monitor_logger_buffer_.ERROR("Trajectory msg lost");
+    return Status(ErrorCode::CONTROL_COMPUTE_ERROR, "Trajectory msg timeout");
+  }
+  return Status::OK();
 }
 
 }  // namespace control
