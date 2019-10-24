f988434b89e6064468111554ea4a4b1371835a5e
kevin-y-wang <yuwang01@baidu.com>
2019-10-24 15:16:37 -0700

    Control: Refactor MRAC controller unit test

--- modules/control/common/mrac_controller_test.cc
+++ modules/control/common/mrac_controller_test.cc
@@ -27,6 +27,8 @@
 namespace apollo {
 namespace control {
 
+using Matrix = Eigen::MatrixXd;
+
 class MracControllerTest : public ::testing::Test {
  public:
   virtual void SetUp() {
@@ -43,25 +45,29 @@ class MracControllerTest : public ::testing::Test {
 
 TEST_F(MracControllerTest, MracControl) {
   double dt = 0.01;
+  Matrix state = Matrix::Zero(1, 1);
   MracConf mrac_conf = lat_controller_conf_.actuation_mrac_conf();
   MracController mrac_controller;
   mrac_controller.Init(mrac_conf, dt);
   mrac_controller.Reset();
-  EXPECT_NEAR(mrac_controller.Control(18.0, 6.0, dt), 0.0, 1e-6);
+  state(0, 0) = 6.0;
+  EXPECT_NEAR(mrac_controller.Control(18.0, state, dt), 0.0, 1e-6);
   mrac_controller.Reset();
-  EXPECT_NEAR(mrac_controller.Control(18.0, 10.0, dt), -8.48, 1e-6);
+  state(0, 0) = 10.0;
+  EXPECT_NEAR(mrac_controller.Control(18.0, state, dt), -8.48, 1e-6);
   EXPECT_NEAR(mrac_controller.CurrentReferenceState(), 6.0, 1e-6);
   EXPECT_NEAR(mrac_controller.CurrentStateAdaptionGain(), -0.2, 1e-6);
   EXPECT_NEAR(mrac_controller.CurrentInputAdaptionGain(), -0.36, 1e-6);
-  EXPECT_NEAR(mrac_controller.Control(18.0, 10.0, dt), -8.48, 1e-6);
+  EXPECT_NEAR(mrac_controller.Control(18.0, state, dt), -8.48, 1e-6);
   EXPECT_NEAR(mrac_controller.CurrentReferenceState(), 14.0, 1e-6);
   EXPECT_NEAR(mrac_controller.CurrentStateAdaptionGain(), -0.2, 1e-6);
   EXPECT_NEAR(mrac_controller.CurrentInputAdaptionGain(), -0.36, 1e-6);
   mrac_controller.Reset();
-  double control_value = mrac_controller.Control(-18.0, -10.0, dt);
+  state(0, 0) = -10.0;
+  double control_value = mrac_controller.Control(-18.0, state, dt);
   EXPECT_NEAR(control_value, 8.48, 1e-6);
   dt = 0.0;
-  EXPECT_EQ(mrac_controller.Control(-18.0, -10.0, dt), control_value);
+  EXPECT_EQ(mrac_controller.Control(-18.0, state, dt), control_value);
 }
 
 }  // namespace control
