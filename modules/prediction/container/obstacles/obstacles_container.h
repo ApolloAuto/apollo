13b13ca861284b3b95d0484e4e8ba86080f9ae03
kechxu <kechxu89@gmail.com>
2019-10-25 11:34:07 -0700

    Prediction: deprecate id mapping

--- modules/prediction/container/obstacles/obstacles_container.h
+++ modules/prediction/container/obstacles/obstacles_container.h
@@ -69,12 +69,6 @@ class ObstaclesContainer : public Container {
    */
   void InsertFeatureProto(const Feature& feature);
 
-  /*
-   * @brief Build current frame id mapping for all obstacles
-   */
-  void BuildCurrentFrameIdMapping(
-      const perception::PerceptionObstacles& perception_obstacles);
-
   /**
    * @brief Build lane graph for obstacles
    */
@@ -139,14 +133,6 @@ class ObstaclesContainer : public Container {
 
  private:
   Obstacle* GetObstacleWithLRUUpdate(const int obstacle_id);
-  /**
-   * @brief Check if a perception_obstacle is an old existed obstacle
-   * @param A PerceptionObstacle
-   * @param An obstacle_ptr
-   * @return True if the perception_obstacle is this obstacle; otherwise false;
-   */
-  bool AdaptTracking(const perception::PerceptionObstacle& perception_obstacle,
-                     Obstacle* obstacle_ptr);
 
   /**
    * @brief Check if an obstacle is movable
@@ -155,19 +141,12 @@ class ObstaclesContainer : public Container {
    */
   bool IsMovable(const perception::PerceptionObstacle& perception_obstacle);
 
-  int PerceptionIdToPredictionId(const int perception_id);
-
  private:
   double timestamp_ = -1.0;
   common::util::LRUCache<int, std::unique_ptr<Obstacle>> ptr_obstacles_;
-  // an id_mapping from perception_id to prediction_id
-  common::util::LRUCache<int, int> id_mapping_;
   std::vector<int> curr_frame_movable_obstacle_ids_;
   std::vector<int> curr_frame_unmovable_obstacle_ids_;
   std::vector<int> curr_frame_considered_obstacle_ids_;
-  // perception_id -> prediction_id
-  std::unordered_map<int, int> curr_frame_id_mapping_;
-  // prediction_id -> perception_obstacle
   std::unordered_map<int, apollo::perception::PerceptionObstacle>
       curr_frame_id_perception_obstacle_map_;
 };
