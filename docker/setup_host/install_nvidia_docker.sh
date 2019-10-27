c5b0332882859963ab8d3579aeb56aed1f1105dc
Xiangquan Xiao <xiaoxiangquan@gmail.com>
2019-10-27 16:57:29 -0700

    Docker: Add nvidia-docker depecation note.

--- docker/setup_host/install_nvidia_docker.sh
+++ docker/setup_host/install_nvidia_docker.sh
@@ -16,15 +16,20 @@
 # limitations under the License.
 ###############################################################################
 
-docker volume ls -q -f driver=nvidia-docker | xargs -r -I{} -n1 docker ps -q -a -f volume={} | xargs -r docker rm -f
+echo "Deprecated! Please follow https://github.com/NVIDIA/nvidia-docker to "
+echo "install the latest version."
+
+docker volume ls -q -f driver=nvidia-docker | \
+    xargs -r -I{} -n1 docker ps -q -a -f volume={} | \
+    xargs -r docker rm -f
 sudo apt-get purge -y nvidia-docker
 
 # Add the package repositories
 curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | \
-  sudo apt-key add -
+    sudo apt-key add -
 distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
 curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | \
-  sudo tee /etc/apt/sources.list.d/nvidia-docker.list
+    sudo tee /etc/apt/sources.list.d/nvidia-docker.list
 sudo apt-get update
 
 # Install nvidia-docker2 and reload the Docker daemon configuration
