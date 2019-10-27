7c140ddebf191dbd3e8c13163796932803ec6f8b
Xiangquan Xiao <xiaoxiangquan@gmail.com>
2019-10-27 16:57:29 -0700

    Docker: Add nvidia-docker depecation note.

--- docker/setup_host/install_docker.sh
+++ docker/setup_host/install_docker.sh
@@ -16,30 +16,35 @@
 # limitations under the License.
 ###############################################################################
 function install_filesystem_support() {
-MACHINE_VERSION=$(uname -r)
-if [ "$MACHINE_VERSION" == "4.4.32-apollo-2-RT" ]; then
-   echo "system have install realtime kernel"
-   echo "it support overlay2, no need to install aufs"
-   sudo modprobe overlay
-else
-   MAIN_KERNEL_VERSION=${MACHINE_VERSION:0:1}
-   if [ ${MAIN_KERNEL_VERSION} -gt 3 \
-       -a -f /lib/modules/$MACHINE_VERSION/kernel/fs/overlayfs/overlay.ko ]; then
-     echo "the kernel version 4 or higher;"
-     echo "it has support overlay2"
-     sudo modprobe overlay
-   else
-     echo "the kernel version is lower than 4"
-     echo "try to install aufs"
-     sudo apt-get update
-     sudo apt-get install \
-      linux-image-extra-$(uname -r) \
-      linux-image-extra-virtual
-   fi
-fi
-sudo apt-get update
-sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+  MACHINE_VERSION=$(uname -r)
+  if [ "$MACHINE_VERSION" == "4.4.32-apollo-2-RT" ]; then
+    echo "system have install realtime kernel"
+    echo "it support overlay2, no need to install aufs"
+    sudo modprobe overlay
+  else
+    MAIN_KERNEL_VERSION=${MACHINE_VERSION:0:1}
+    if [ ${MAIN_KERNEL_VERSION} -gt 3 \
+        -a -f /lib/modules/$MACHINE_VERSION/kernel/fs/overlayfs/overlay.ko ]; then
+      echo "the kernel version 4 or higher;"
+      echo "it has support overlay2"
+      sudo modprobe overlay
+    else
+      echo "the kernel version is lower than 4"
+      echo "try to install aufs"
+      sudo apt-get update
+      sudo apt-get install -y \
+          linux-image-extra-${MACHINE_VERSION} \
+          linux-image-extra-virtual
+    fi
+  fi
+
+  sudo apt-get update
+  sudo apt-get install -y \
+      apt-transport-https \
+      ca-certificates \
+      curl \
+      software-properties-common
+  curl -fsSL "https://download.docker.com/linux/ubuntu/gpg" | sudo apt-key add -
 }
 
 function install_docker_x86() {
@@ -53,7 +58,8 @@ function install_docker_x86() {
 }
 
 function install_docker_arm() {
-  sudo bash -c 'echo "deb [arch=arm64] https://download.docker.com/linux/ubuntu xenial edge" > /etc/apt/sources.list.d/docker.list'
+  echo "deb [arch=arm64] https://download.docker.com/linux/ubuntu xenial edge" | \
+      sudo tee /etc/apt/sources.list.d/docker.list
   sudo apt-get update
   sudo apt-get install -y docker-ce
   sudo groupadd docker
@@ -62,28 +68,28 @@ function install_docker_arm() {
 }
 
 function install() {
-   # the machine type, currently support x86_64, aarch64
-install_filesystem_support
-MACHINE_ARCH=$(uname -m)
-if [ "$MACHINE_ARCH" == 'x86_64' ]; then
-  install_docker_x86
-elif [ "$MACHINE_ARCH" == 'aarch64' ]; then
-  install_docker_arm
-else
-  echo "Unknown machine architecture $MACHINE_ARCH"
-  exit 1
-fi
+  # the machine type, currently support x86_64, aarch64
+  install_filesystem_support
+  MACHINE_ARCH=$(uname -m)
+  if [ "$MACHINE_ARCH" == 'x86_64' ]; then
+    install_docker_x86
+  elif [ "$MACHINE_ARCH" == 'aarch64' ]; then
+    install_docker_arm
+  else
+    echo "Unknown machine architecture $MACHINE_ARCH"
+    exit 1
+  fi
 }
 
 case $1 in
-　　install)
-      install
-      ;;
-   uninstall)
-      sudo apt-get remove docker docker-engine docker.io
-      sudo apt-get purge docker-ce
-      ;;
-   *)
-      install
-      ;;
+  install)
+    install
+    ;;
+  uninstall)
+    sudo apt-get remove docker docker-engine docker.io
+    sudo apt-get purge docker-ce
+    ;;
+  *)
+    install
+    ;;
 esac
