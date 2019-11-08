8e1d93143b001f5cfdfc4f9730ba607814d5ce9d
Vivian Lin <2775888+vlin17@users.noreply.github.com>
2019-11-08 13:33:50 -0800

    Dreamview: DRIVABLE_REGION polygons on pnc monitor (#10148)

--- modules/dreamview/frontend/src/components/PNCMonitor/PlanningMonitor.js
+++ modules/dreamview/frontend/src/components/PNCMonitor/PlanningMonitor.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { inject, observer } from "mobx-react";
 
-import _ from 'lodash';
 import SETTING from "store/config/PlanningGraph.yml";
 import ScatterGraph, { generateScatterGraph } from "components/PNCMonitor/ScatterGraph";
 import PlanningScenarioTable from "components/PNCMonitor/PlanningScenarioTable";
@@ -38,11 +37,20 @@ export default class PlanningMonitor extends React.Component {
             return null;
         }
 
+        const chartCount = {};
+
         return (
             <div>
                 <PlanningScenarioTable scenarios={scenarioHistory} />
                 {chartData.map(chart => {
-                    return <ScatterGraph key={chart.title}
+                    // Adding count to chart key to prevent duplicate chart title
+                    if (!chartCount[chart.title]) {
+                        chartCount[chart.title] = 1;
+                    } else {
+                        chartCount[chart.title] += 1;
+                    }
+
+                    return <ScatterGraph key={`custom_${chart.title}_${chartCount[chart.title]}`}
                                          title={chart.title}
                                          options={chart.options}
                                          properties={chart.properties}
