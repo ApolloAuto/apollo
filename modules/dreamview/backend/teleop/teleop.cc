53bd8f621b7746933119285288745948f053a713
hugomatic <hugo@udelv.com>
2019-11-13 18:48:54 -0800

    Added pad message for Pull Over, E Stop and Resume Cruise

--- modules/dreamview/backend/teleop/teleop.cc
+++ modules/dreamview/backend/teleop/teleop.cc
@@ -26,11 +26,15 @@ namespace dreamview {
 
 using Json = nlohmann::json;
 using apollo::cyber::Time;
+using apollo::planning::ADCTrajectory;
+using apollo::planning::ScenarioConfig;
 using apollo::planning::PadMessage;
+using apollo::planning::DrivingAction;
 using ::google::protobuf::util::MessageToJsonString;
 using modules::teleop::network::ModemInfo;
 using modules::teleop::teleop::DaemonServiceCmd;
 using modules::teleop::teleop::DaemonServiceRpt;
+
 
 // modem ids
 const std::string modem0_id = "0";
@@ -54,6 +58,8 @@ const std::string operator_daemon_cmd_channel =
     "/apollo/teleop/operator/daemon_service/cmd";
 const std::string operator_daemon_rpt_channel =
     "/apollo/teleop/operator/daemon_service/rpt";
+const std::string planning_channel = "/apollo/planning";
+const std::string planning_pad_channel = "/apollo/planning/pad";
 
 TeleopService::TeleopService(WebSocketHandler *websocket)
     : node_(cyber::CreateNode("teleop")), websocket_(websocket) {
@@ -68,6 +74,9 @@ TeleopService::TeleopService(WebSocketHandler *websocket)
   teleop_status_["video"] = false;
   teleop_status_["video_starting"] = false;
   teleop_status_["video_stopping"] = false;
+  teleop_status_["pulling_over"] = false;
+  teleop_status_["e_stopping"] = false;
+  teleop_status_["resuming_autonomy"] = false;
 }
 
 void TeleopService::Start() {
@@ -87,6 +96,11 @@ void TeleopService::Start() {
   modem2_info_reader_ = node_->CreateReader<ModemInfo>(
       modem2_channel, [this](const std::shared_ptr<ModemInfo> &msg) {
         UpdateModem(modem2_id, msg);
+      });
+
+  planning_reader_ = node_->CreateReader<ADCTrajectory>(
+      planning_channel, [this](const std::shared_ptr<ADCTrajectory> &msg) {
+        UpdatePlanning(msg);
       });
 
   car_daemon_cmd_writer_ =
@@ -174,7 +188,7 @@ void TeleopService::RegisterMessageHandlers() {
           }
           start = teleop_status_["mic_starting"];
         }
-        AINFO << "ToggleAudio: " << start;
+        AINFO << "ToggleMic: " << start;
         SendMicStreamCmd(start);
       });
   // Start/stop local decoder and viewer, start/stop remote encoder and
@@ -214,18 +228,21 @@ void TeleopService::RegisterMessageHandlers() {
   // Issue pull-over command to remote
   websocket_->RegisterMessageHandler(
       "PullOver", [this](const Json &json, WebSocketHandler::Connection *conn) {
-        // TODO
+        boost::shared_lock<boost::shared_mutex> writer_lock(mutex_);
+        SendPullOverCmd();
       });
   // Issue emergency-stop command to remote
   websocket_->RegisterMessageHandler(
       "EStop", [this](const Json &json, WebSocketHandler::Connection *conn) {
-        // TODO
+        boost::shared_lock<boost::shared_mutex> writer_lock(mutex_);
+        SendEstopCmd();
       });
   // Issue resume-cruise command to remote
   websocket_->RegisterMessageHandler(
       "ResumeCruise",
       [this](const Json &json, WebSocketHandler::Connection *conn) {
-        // TODO
+        boost::shared_lock<boost::shared_mutex> writer_lock(mutex_);
+        SendResumeCruiseCmd();
       });
   // Request to get updated modem info for client display
   websocket_->RegisterMessageHandler(
@@ -428,6 +445,94 @@ void TeleopService::SendMicStreamCmd(bool start_stop) {
   common::util::FillHeader("dreamview", &msg);
   operator_daemon_cmd_writer_->Write(msg);
   AINFO << "mic " << msg.cmd();
+}
+
+
+void TeleopService::SendResumeCruiseCmd() {
+  AINFO << "Resume cruise";
+  PadMessage pad_msg;
+  pad_msg.set_action(DrivingAction::RESUME_CRUISE);
+  pad_message_writer_->Write(pad_msg);
+}
+
+void TeleopService::SendEstopCmd() {
+  AINFO << "Pull over";
+  PadMessage pad_msg;
+  pad_msg.set_action(DrivingAction::PULL_OVER);
+  pad_message_writer_->Write(pad_msg);
+}
+
+void TeleopService::SendPullOverCmd() {
+  AINFO << "EStop";
+  PadMessage pad_msg;
+  pad_msg.set_action(DrivingAction::STOP);
+  pad_message_writer_->Write(pad_msg);
+}
+
+void TeleopService::UpdatePlanning(const std::shared_ptr<ADCTrajectory> &msg)
+{
+  static int count = 0;
+  count ++;
+
+  if (count % 10 == 0) {
+    AINFO << "Update Planning";
+  }
+  auto scenario_type = msg->debug().planning_data().scenario().scenario_type();
+
+  bool pulled_over = scenario_type == ScenarioConfig::PULL_OVER;
+  bool autonomy_resumed = scenario_type == ScenarioConfig::PARK_AND_GO;
+  bool e_stopped = scenario_type == ScenarioConfig::EMERGENCY_PULL_OVER;
+
+
+  bool sendPullOver = false;
+  bool sendStop = false;
+  bool sendResume = false;
+  {
+    boost::unique_lock<boost::shared_mutex> writer_lock(mutex_);
+    if (pulled_over) {
+      if (teleop_status_["pulling_over"]) {
+        // pulled over confirmed
+        teleop_status_["pulling_over"] = false;
+      }
+    }
+    // not pulled over
+    else {
+      if (teleop_status_["pulling_over"]) {
+        sendPullOver = true;
+      }
+    }
+
+    if (e_stopped) {
+      if (teleop_status_["e_stopping"]) {
+        // e stop over confirmed
+        teleop_status_["e_stopping"] = false;
+      }
+    }
+    // not e stopped
+    else {
+      if (teleop_status_["e_stopping"]) {
+        sendStop = true;
+      }
+    }
+
+    if (autonomy_resumed) {
+      if (teleop_status_["resuming_autonomy"]) {
+        teleop_status_["resuming_autonomy"] = false;
+      }
+    }
+    else {
+      if (teleop_status_["resuming_autonomy"]) {
+        sendResume = true;
+      }
+    }
+  } // writer lock scope
+
+  if (sendResume)
+    SendResumeCruiseCmd();
+  if (sendStop)
+    SendEstopCmd();
+  if (sendPullOver)
+    SendPullOverCmd();
 }
 
 }  // namespace dreamview
