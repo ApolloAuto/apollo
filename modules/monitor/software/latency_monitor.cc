53c3d7fe329cb01eba9c1ea5397a7ad382783050
michael4338 <michael4338@hotmail.com>
2019-11-14 15:00:23 -0800

    topology sort messages among modules for better display

--- modules/monitor/software/latency_monitor.cc
+++ modules/monitor/software/latency_monitor.cc
@@ -18,6 +18,8 @@
 
 #include <algorithm>
 #include <memory>
+#include <queue>
+#include <unordered_set>
 #include <utility>
 #include <vector>
 
@@ -42,6 +44,7 @@ namespace {
 using apollo::common::LatencyRecordMap;
 using apollo::common::LatencyReport;
 using apollo::common::LatencyStat;
+using apollo::common::LatencyTrack;
 using apollo::common::util::StrCat;
 
 void FillInStat(const std::string& module_name, const uint64_t duration,
@@ -51,24 +54,77 @@ void FillInStat(const std::string& module_name, const uint64_t duration,
   stats->emplace_back(module_name, duration);
 }
 
+void FillInTopo(
+    const std::string& prev_node_name, const std::string& curr_node_name,
+    std::unordered_map<std::string,
+                       std::pair<int, std::unordered_set<std::string>>>* topo) {
+  bool not_existing = false;
+  auto* edges = &(*topo)[prev_node_name].second;
+  std::tie(std::ignore, not_existing) = edges->emplace(curr_node_name);
+  if (not_existing) {
+    (*topo)[curr_node_name].first += 1;
+  }
+}
+
+std::vector<std::string> topology_sort(
+    std::unordered_map<std::string,
+                       std::pair<int, std::unordered_set<std::string>>>* topo) {
+  std::vector<std::string> sorted_modules;
+  std::queue<std::string> zero_indegree_modules;
+  for (const auto& vortex : *topo) {
+    if (vortex.second.first == 0) {
+      zero_indegree_modules.push(vortex.first);
+    }
+  }
+
+  while (!zero_indegree_modules.empty()) {
+    const auto cur_module = zero_indegree_modules.front();
+    zero_indegree_modules.pop();
+    sorted_modules.push_back(cur_module);
+    for (const auto& edge : (*topo)[cur_module].second) {
+      (*topo)[edge].first -= 1;
+      if ((*topo)[edge].first == 0) {
+        zero_indegree_modules.push(edge);
+      }
+    }
+  }
+
+  if (sorted_modules.size() != topo->size()) {
+    AERROR << "The topology sort failed, there must be some disorder for "
+              "messages traveling among modules";
+  }
+  return sorted_modules;
+}
+
 std::vector<std::pair<std::string, uint64_t>> GetFlowTrackStats(
-    std::set<std::tuple<uint64_t, uint64_t, std::string>>* module_durations,
-    uint64_t* total_duration) {
+    const std::set<std::tuple<uint64_t, uint64_t, std::string>>&
+        module_durations,
+    uint64_t* total_duration,
+    std::unordered_map<std::string,
+                       std::pair<int, std::unordered_set<std::string>>>* topo) {
   const std::string module_connector = "->";
   std::vector<std::pair<std::string, uint64_t>> stats;
 
   // Generate a list of <module, duration> pair, for example:
   // <percetpion: 100>, <perception->prediction: 10>, <prediction: 50>, ...
-  auto iter = module_durations->begin();
+  auto iter = module_durations.begin();
   std::string module_name, prev_name;
   uint64_t begin_time = 0, end_time, prev_end_time = 0;
-  while (iter != module_durations->end()) {
+  while (iter != module_durations.end()) {
     std::tie(begin_time, end_time, module_name) = *iter;
     if (!prev_name.empty()) {
-      FillInStat(StrCat(prev_name, module_connector, module_name),
-                 begin_time - prev_end_time, &stats, total_duration);
+      const std::string mid_name =
+          StrCat(prev_name, module_connector, module_name);
+      FillInStat(mid_name, begin_time - prev_end_time, &stats, total_duration);
+      FillInTopo(prev_name, mid_name, topo);
+      FillInTopo(mid_name, module_name, topo);
     }
     FillInStat(module_name, end_time - begin_time, &stats, total_duration);
+    if (topo->find(module_name) == topo->end()) {
+      topo->emplace(module_name,
+                    std::pair<int, std::unordered_set<std::string>>(
+                        0, std::unordered_set<std::string>()));
+    }
     prev_name = module_name;
     prev_end_time = end_time;
     ++iter;
@@ -91,6 +147,13 @@ LatencyStat GenerateStat(const std::vector<uint64_t>& numbers) {
   stat.set_aver_duration(static_cast<uint64_t>(sum / sample_size));
   stat.set_sample_size(sample_size);
   return stat;
+}
+
+void SetStat(const LatencyStat& src, LatencyStat* dst) {
+  dst->set_min_duration(src.min_duration());
+  dst->set_max_duration(src.max_duration());
+  dst->set_aver_duration(src.aver_duration());
+  dst->set_sample_size(src.sample_size());
 }
 
 }  // namespace
@@ -134,17 +197,21 @@ void LatencyMonitor::PublishLatencyReport() {
   writer->Write(latency_report_);
   latency_report_.clear_header();
   track_map_.clear();
-  latency_report_.mutable_stat_aggr()->clear();
+  latency_report_.clear_latency_tracks();
 }
 
 void LatencyMonitor::AggregateLatency() {
   std::unordered_map<std::string, std::vector<uint64_t>> tracks;
   std::vector<uint64_t> totals;
+  std::unordered_map<std::string,
+                     std::pair<int, std::unordered_set<std::string>>>
+      topo;
 
   // Aggregate durations by module names
   for (auto& message : track_map_) {
     uint64_t total_duration = 0;
-    const auto stats = GetFlowTrackStats(&message.second, &total_duration);
+    const auto stats =
+        GetFlowTrackStats(message.second, &total_duration, &topo);
     std::string module_name;
     uint64_t duration = 0;
     totals.push_back(total_duration);
@@ -162,14 +229,17 @@ void LatencyMonitor::AggregateLatency() {
   // ...
   const auto total_stat = GenerateStat(totals);
   auto* total_duration_aggr = latency_report_.mutable_total_duration();
-  total_duration_aggr->set_min_duration(total_stat.min_duration());
-  total_duration_aggr->set_max_duration(total_stat.max_duration());
-  total_duration_aggr->set_aver_duration(total_stat.aver_duration());
-  total_duration_aggr->set_sample_size(total_stat.sample_size());
+  SetStat(total_stat, total_duration_aggr);
 
-  auto* stat_aggr = latency_report_.mutable_stat_aggr();
-  for (const auto& track : tracks) {
-    stat_aggr->insert({track.first, GenerateStat(track.second)});
+  // Sort the modules following the messages flowing direction,
+  // for better display
+  std::vector<std::string> topo_sorted_modules = topology_sort(&topo);
+  auto* latency_tracks = latency_report_.mutable_latency_tracks();
+  for (const auto& module_name : topo_sorted_modules) {
+    auto* module_latency = latency_tracks->add_module_latency();
+    module_latency->set_module_name(module_name);
+    SetStat(GenerateStat(tracks[module_name]),
+            module_latency->mutable_module_stat());
   }
 }
 
