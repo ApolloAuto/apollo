784545bcde04bb854527b71639ddf419538df875
vlin17 <thunder0504@gmail.com>
2019-11-15 11:42:38 -0800

    Teleop: set A/V status based on process states

--- modules/dreamview/frontend/src/components/TeleopMonitor/CarTeleop.js
+++ modules/dreamview/frontend/src/components/TeleopMonitor/CarTeleop.js
@@ -3,7 +3,7 @@ import { inject, observer } from "mobx-react";
 
 import { TELEOP_WS } from "store/websocket";
 
-import CheckboxItem from "components/common/CheckboxItem";
+import AudioControl from "components/TeleopMonitor/AudioControl";
 import MonitorSection from "components/TeleopMonitor/MonitorSection";
 
 import itemIcon from "assets/images/icons/teleop_item.png";
@@ -29,18 +29,17 @@ export default class CarTeleOp extends React.Component {
                 </div>
 
                 <div className="monitor-content">
-                    <MonitorSection title="Audio" icon={itemIcon}>
-                        <div className="section-content-row" key={name}>
-                            <CheckboxItem id='teleopAudio'
-                                          title={"On/Off"}
-                                          extraClasses="field"
-                                          isChecked={teleop.audioEnabled}
-                                          onClick={() => {
-                                            TELEOP_WS.executeCommand('ToggleAudio');
-                                            teleop.toggleAudio();
-                                          }} />
-                        </div>
-                    </MonitorSection>
+                    <AudioControl id='car'
+                                  audioEnabled={teleop.audioEnabled}
+                                  micEnabled={teleop.micEnabled}
+                                  toggleAudio={() => {
+                                    TELEOP_WS.executeCommand('ToggleAudio');
+                                    teleop.toggleAudio();
+                                  }}
+                                  toggleMic={() => {
+                                    teleop.toggleMic();
+                                    TELEOP_WS.executeCommand('ToggleMic');
+                                  }} />
                     <MonitorSection title="Signal" icon={itemIcon}>
                         {teleop.modemInfo.entries().map(([name, value]) => (
                                 <div className="section-content-row" key={name}>
