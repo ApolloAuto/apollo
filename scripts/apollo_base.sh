8b5eed7fafdf790d881987436d1716db5490e97d
muleisheng <muleisheng@126.com>
2019-12-05 18:53:19 +0800

    Docker: remove unused code

--- scripts/apollo_base.sh
+++ scripts/apollo_base.sh
@@ -82,13 +82,7 @@ function check_in_docker() {
 
 function set_lib_path() {
   LIB_USER=/usr/lib/$(uname -m)-linux-$USER
-  if [ -d "${LIB_USER}" ]; then
-    export LD_LIBRARY_PATH=/usr/lib:/usr/lib/x86_64-linux-gnu:${LIB_USER}
-  else
-    export LD_LIBRARY_PATH=/usr/lib:/usr/lib/x86_64-linux-gnu
-  fi
 
-  LIB_USER=/usr/lib/$(uname -m)-linux-$USER
   if [ -d "${LIB_USER}" ]; then
     export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${LIB_USER}
   else
