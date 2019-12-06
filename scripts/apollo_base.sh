c8f429f89fa1d609904edc12178bd4ed28e65e48
muleisheng <muleisheng@126.com>
2019-12-07 07:14:40 +0800

    Docker: fix getting openGL version failed when using cyber_visualizer (#10426)
    * Docker: fix get openGL version failed when using cyber_visualizer
    
    * Docker: fix getting openGL version failed when using cyber_visualizer
    
    * Docker: add directory checking
    
    * Docker: fix code style
    
    * clean apollo directory
    
    * Docker: fix get openGL version failed when using cyber_visualizer
    
    * Docker: fix getting openGL version failed when using cyber_visualizer
    
    * Docker: add directory checking
    
    * Docker: fix code style
    
    * clean apollo directory
    
    * Docker: improve code
    
    * Docker: remove unused code
    
    * Docker: fix a issue about  LD_LIBRARY_PATH

--- scripts/apollo_base.sh
+++ scripts/apollo_base.sh
@@ -83,6 +83,14 @@ function check_in_docker() {
 function set_lib_path() {
   export LD_LIBRARY_PATH=/usr/lib:/usr/lib/x86_64-linux-gnu
 
+  LIB_USER=/usr/lib/$(uname -m)-linux-$USER
+
+  if [ -d "${LIB_USER}" ]; then
+    export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${LIB_USER}
+  else
+    warn "${LIB_USER} doesn't exist"
+  fi
+
   if [ "$RELEASE_DOCKER" == 1 ]; then
     local CYBER_SETUP="/apollo/cyber/setup.bash"
     if [ -e "${CYBER_SETUP}" ]; then
