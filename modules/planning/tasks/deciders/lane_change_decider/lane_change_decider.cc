ee7b9e6a6caa75efb7de281838deddcd8ed6b3bf
panjiacheng <panjiacheng@yahoo.com>
2019-12-16 17:11:57 -0800

    Planning: modified a few lane-change params.

--- modules/planning/tasks/deciders/lane_change_decider/lane_change_decider.cc
+++ modules/planning/tasks/deciders/lane_change_decider/lane_change_decider.cc
@@ -320,8 +320,8 @@ bool LaneChangeDecider::IsClearToChangeLane(
     // TODO(All) move to confs
     static constexpr double kSafeTimeOnSameDirection = 3.0;
     static constexpr double kSafeTimeOnOppositeDirection = 5.0;
-    static constexpr double kForwardMinSafeDistanceOnSameDirection = 3.0;
-    static constexpr double kBackwardMinSafeDistanceOnSameDirection = 4.0;
+    static constexpr double kForwardMinSafeDistanceOnSameDirection = 10.0;
+    static constexpr double kBackwardMinSafeDistanceOnSameDirection = 10.0;
     static constexpr double kForwardMinSafeDistanceOnOppositeDirection = 50.0;
     static constexpr double kBackwardMinSafeDistanceOnOppositeDirection = 1.0;
     static constexpr double kDistanceBuffer = 0.5;
