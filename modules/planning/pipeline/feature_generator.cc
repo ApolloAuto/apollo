17b6865eed77ace8c05c259b897f5c96dc82b0cc
Jiangtao Hu <boris.hu@gmail.com>
2020-01-28 11:43:36 -0800

    planning: add trajectory label generation function.

--- modules/planning/pipeline/feature_generator.cc
+++ modules/planning/pipeline/feature_generator.cc
@@ -16,11 +16,34 @@
 
 #include "modules/planning/pipeline/feature_generator.h"
 
+#include <cmath>
 #include <string>
 
+#include "absl/strings/str_cat.h"
 #include "cyber/common/file.h"
 #include "cyber/record/record_reader.h"
 #include "modules/common/adapters/adapter_gflags.h"
+
+DEFINE_string(planning_data_dir, "/apollo/modules/planning/data/",
+              "Prefix of files to store instance data");
+
+DEFINE_int32(
+    instance_label_sample_interval, 100,
+    "total number of localization msgs to generate one instance label data.");
+
+DEFINE_int32(instance_num_per_file, 100,
+             "number of instance to write out in one data file.");
+
+DEFINE_int32(
+    localization_sample_interval_for_trajectory_point, 10,
+    "number of localization msgs to generate one trajectory point in label.");
+
+DEFINE_int32(localization_move_window_step, 5,
+             "number of localization msgs to skip after generating one label "
+             "trajectory point.");
+
+DEFINE_bool(enable_binary_instance, false,
+            "True to generate protobuf binary data file.");
 
 
 namespace apollo {
@@ -31,26 +54,91 @@ using apollo::cyber::record::RecordMessage;
 using apollo::cyber::record::RecordReader;
 using apollo::localization::LocalizationEstimate;
 
-void FeatureGenerator::Init() {
+void FeatureGenerator::Init() { instance_ = instances_.add_instances(); }
+
+void FeatureGenerator::WriteOutInstances(const Instances& instances,
+                                         const std::string& file_name) {
+  if (FLAGS_enable_binary_instance) {
+    cyber::common::SetProtoToBinaryFile(instances, file_name);
+  } else {
+    cyber::common::SetProtoToASCIIFile(instances, file_name);
+  }
 }
 
 void FeatureGenerator::Close() {
+  const std::string file_name = absl::StrCat(
+      FLAGS_planning_data_dir, "/instances.", instance_file_index_, ".bin");
+  WriteOutInstances(instances_, file_name);
+  ++instance_file_index_;
+}
+
+void FeatureGenerator::GenerateTrajectoryLabel(
+    const std::list<apollo::localization::LocalizationEstimate>&
+        localization_for_label,
+    Instance* instance) {
+  int i = -1;
+  for (const auto& le : localization_for_label) {
+    ++i;
+    if ((i % FLAGS_localization_sample_interval_for_trajectory_point) != 0) {
+      continue;
+    }
+    auto trajectory_point = instance->add_label_trajectory_points();
+    auto& pose = le.pose();
+    trajectory_point->mutable_path_point()->set_x(pose.position().x());
+    trajectory_point->mutable_path_point()->set_y(pose.position().y());
+    trajectory_point->mutable_path_point()->set_z(pose.position().z());
+    trajectory_point->mutable_path_point()->set_theta(pose.heading());
+    auto v = std::sqrt(pose.linear_velocity().x() * pose.linear_velocity().x() +
+                       pose.linear_velocity().y() * pose.linear_velocity().y());
+    trajectory_point->set_v(v);
+    auto a = std::sqrt(
+        pose.linear_acceleration().x() * pose.linear_acceleration().x() +
+        pose.linear_acceleration().y() * pose.linear_acceleration().y());
+    trajectory_point->set_a(a);
+  }
 }
 
 void FeatureGenerator::OnLocalization(
-  const apollo::localization::LocalizationEstimate& le) {
-    auto features = instance_.mutable_localization_feature();
-    const auto& pose = le.pose();
-    features->mutable_position()->CopyFrom(pose.position());
-    features->set_heading(pose.heading());
-    features->mutable_linear_velocity()->CopyFrom(pose.linear_velocity());
-    features->mutable_linear_acceleration()->CopyFrom(
-        pose.linear_acceleration());
-    features->mutable_angular_velocity()->CopyFrom(pose.angular_velocity());
+    const apollo::localization::LocalizationEstimate& le) {
+  if (instance_ == nullptr) {
+    AERROR << "instance pointer is nullptr";
+    return;
+  }
+
+  auto features = instance_->mutable_localization_feature();
+  const auto& pose = le.pose();
+  features->mutable_position()->CopyFrom(pose.position());
+  features->set_heading(pose.heading());
+  features->mutable_linear_velocity()->CopyFrom(pose.linear_velocity());
+  features->mutable_linear_acceleration()->CopyFrom(pose.linear_acceleration());
+  features->mutable_angular_velocity()->CopyFrom(pose.angular_velocity());
+  localization_for_label_.push_back(le);
+
+  if (static_cast<int>(localization_for_label_.size()) >=
+      FLAGS_instance_label_sample_interval) {
+    GenerateTrajectoryLabel(localization_for_label_, instance_);
+    instance_ = instances_.add_instances();
+    for (int i = 0; i < FLAGS_localization_move_window_step; ++i) {
+      localization_for_label_.pop_front();
+    }
+  }
+
+  if (instances_.instances_size() >= FLAGS_instance_num_per_file) {
+    const std::string file_name = absl::StrCat(
+        FLAGS_planning_data_dir, "/instances.", instance_file_index_, ".bin");
+    WriteOutInstances(instances_, file_name);
+    instances_.Clear();
+    ++instance_file_index_;
+    instance_ = instances_.add_instances();
+  }
 }
 
 void FeatureGenerator::OnChassis(const apollo::canbus::Chassis& chassis) {
-  auto features = instance_.mutable_chassis_feature();
+  if (instance_ == nullptr) {
+    AERROR << "instance pointer is nullptr";
+    return;
+  }
+  auto features = instance_->mutable_chassis_feature();
   features->set_speed_mps(chassis.speed_mps());
   features->set_throttle_percentage(chassis.throttle_percentage());
   features->set_brake_percentage(chassis.brake_percentage());
@@ -83,4 +171,3 @@ void FeatureGenerator::ProcessOfflineData(const std::string& record_filename) {
 
 }  // namespace planning
 }  // namespace apollo
-
