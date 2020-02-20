40eece17352036bf8dd10d0fbfd15f782cc8408f
jmtao <taojm99@gmail.com>
2020-02-19 17:51:33 -0800

    planning: fixed various issues in records_to_offline_data

--- modules/planning/pipeline/feature_generator.cc
+++ modules/planning/pipeline/feature_generator.cc
@@ -23,26 +23,14 @@
 #include "cyber/common/file.h"
 #include "cyber/record/record_reader.h"
 #include "modules/common/adapters/adapter_gflags.h"
+#include "modules/planning/common/planning_gflags.h"
 
 DEFINE_string(planning_data_dir, "/apollo/modules/planning/data/",
               "Prefix of files to store learning_data_frame data");
-
-DEFINE_int32(
-    learning_data_frame_label_sample_interval, 100,
-    "total number of localization msgs to generate one "
-    "learning_data_frame label data.");
-
+DEFINE_int32(localization_freq, 100, "frequence of localization message");
+DEFINE_int32(planning_freq, 10, "frequence of planning message");
 DEFINE_int32(learning_data_frame_num_per_file, 100,
-             "number of learning_data_farame to write out in one data file.");
-
-DEFINE_int32(
-    localization_sample_interval_for_trajectory_point, 10,
-    "number of localization msgs to generate one trajectory point in label.");
-
-DEFINE_int32(localization_move_window_step, 5,
-             "number of localization msgs to skip after generating one label "
-             "trajectory point.");
-
+             "number of learning_data_frame to write out in one data file.");
 DEFINE_bool(enable_binary_learning_data, true,
             "True to generate protobuf binary data file.");
 
@@ -55,24 +43,26 @@ using apollo::cyber::record::RecordReader;
 using apollo::localization::LocalizationEstimate;
 
 void FeatureGenerator::Init() {
-  learning_data_frame_ = learning_data_.add_learning_data(); }
+  learning_data_frame_ = learning_data_.add_learning_data();
+}
 
 void FeatureGenerator::WriteOutLearningData(const LearningData& learning_data,
                                             const std::string& file_name) {
   if (FLAGS_enable_binary_learning_data) {
     cyber::common::SetProtoToBinaryFile(learning_data, file_name);
+    cyber::common::SetProtoToASCIIFile(learning_data, file_name + ".txt");
   } else {
     cyber::common::SetProtoToASCIIFile(learning_data, file_name);
   }
+  learning_data_.Clear();
+  ++learning_data_file_index_;
 }
 
 void FeatureGenerator::Close() {
   const std::string file_name = absl::StrCat(
       FLAGS_planning_data_dir, "/learning_data.",
       learning_data_file_index_, ".bin");
-  total_learning_data_frame_num_ += learning_data_.learning_data_size();
   WriteOutLearningData(learning_data_, file_name);
-  ++learning_data_file_index_;
   AINFO << "Total learning_data_frame number:"
         << total_learning_data_frame_num_;
 }
@@ -81,10 +71,18 @@ void FeatureGenerator::GenerateTrajectoryLabel(
     const std::list<apollo::localization::LocalizationEstimate>&
         localization_for_label,
     LearningDataFrame* learning_data_frame) {
+  learning_data_frame->set_timestamp_sec(
+      localization_for_label.back().header().timestamp_sec());
+  learning_data_frame->set_frame_num(total_learning_data_frame_num_++);
+
   int i = -1;
+  int cnt = 0;
+
+  const int localization_sample_interval_for_trajectory_point =
+      FLAGS_localization_freq / FLAGS_planning_freq;
   for (const auto& le : localization_for_label) {
     ++i;
-    if ((i % FLAGS_localization_sample_interval_for_trajectory_point) != 0) {
+    if ((i % localization_sample_interval_for_trajectory_point) != 0) {
       continue;
     }
     auto trajectory_point = learning_data_frame->add_label_trajectory_points();
@@ -100,7 +98,10 @@ void FeatureGenerator::GenerateTrajectoryLabel(
         pose.linear_acceleration().x() * pose.linear_acceleration().x() +
         pose.linear_acceleration().y() * pose.linear_acceleration().y());
     trajectory_point->set_a(a);
+
+    cnt++;
   }
+  // AINFO << "number of trajectory points in one frame: " << cnt;
 }
 
 void FeatureGenerator::OnLocalization(
@@ -119,26 +120,30 @@ void FeatureGenerator::OnLocalization(
   features->mutable_angular_velocity()->CopyFrom(pose.angular_velocity());
   localization_for_label_.push_back(le);
 
-  if (static_cast<int>(localization_for_label_.size()) >=
-      FLAGS_learning_data_frame_label_sample_interval) {
-    GenerateTrajectoryLabel(localization_for_label_, learning_data_frame_);
-    learning_data_frame_ = learning_data_.add_learning_data();
-    for (int i = 0; i < FLAGS_localization_move_window_step; ++i) {
-      localization_for_label_.pop_front();
-    }
+  const int localization_msg_start_cnt =
+      FLAGS_localization_freq * FLAGS_trajectory_time_length;
+  if (static_cast<int>(localization_for_label_.size()) <
+      localization_msg_start_cnt) {
+    return;
   }
 
+  // generate trajectory points for one frame
+  GenerateTrajectoryLabel(localization_for_label_, learning_data_frame_);
+  const int localization_move_window_step =
+      FLAGS_localization_freq / FLAGS_planning_freq;
+  for (int i = 0; i < localization_move_window_step; ++i) {
+    localization_for_label_.pop_front();
+  }
+
+  // write frames into a file
   if (learning_data_.learning_data_size() >=
       FLAGS_learning_data_frame_num_per_file) {
-    const std::string file_name =
-        absl::StrCat(FLAGS_planning_data_dir, "/learning_data.",
-                     learning_data_file_index_, ".bin");
+    const std::string file_name = absl::StrCat(
+        FLAGS_planning_data_dir, "/learning_data.",
+        learning_data_file_index_, ".bin");
     WriteOutLearningData(learning_data_, file_name);
-    total_learning_data_frame_num_ += learning_data_.learning_data_size();
-    learning_data_.Clear();
-    ++learning_data_file_index_;
-    learning_data_frame_ = learning_data_.add_learning_data();
   }
+  learning_data_frame_ = learning_data_.add_learning_data();
 }
 
 void FeatureGenerator::OnChassis(const apollo::canbus::Chassis& chassis) {
