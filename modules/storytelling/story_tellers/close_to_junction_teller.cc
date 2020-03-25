604d3270537f1e22631a582bab1312eb0c6631c1
jmtao <taojm99@gmail.com>
2020-03-24 18:45:30 -0700

    storytelling: add CloseToSignal

--- modules/storytelling/story_tellers/close_to_junction_teller.cc
+++ modules/storytelling/story_tellers/close_to_junction_teller.cc
@@ -16,13 +16,10 @@
 
 #include "modules/storytelling/story_tellers/close_to_junction_teller.h"
 
-#include <memory>
-#include <string>
 #include <vector>
 
 #include "modules/common/adapters/adapter_gflags.h"
 #include "modules/map/hdmap/hdmap_util.h"
-#include "modules/planning/proto/planning.pb.h"
 #include "modules/storytelling/common/storytelling_gflags.h"
 #include "modules/storytelling/frame_manager.h"
 
@@ -32,100 +29,117 @@ namespace {
 
 using apollo::hdmap::HDMapUtil;
 using apollo::common::PathPoint;
-using apollo::hdmap::JunctionInfo;
-using apollo::hdmap::PNCJunctionInfo;
+using apollo::hdmap::JunctionInfoConstPtr;
+using apollo::hdmap::PNCJunctionInfoConstPtr;
+using apollo::hdmap::SignalInfoConstPtr;
+
 using apollo::planning::ADCTrajectory;
 
-bool IsPointInPNCJunction(const PathPoint& point,
-                          std::string* pnc_junction_id) {
+bool GetPNCJunction(const PathPoint& point,
+                    std::string* pnc_junction_id) {
   common::PointENU hdmap_point;
   hdmap_point.set_x(point.x());
   hdmap_point.set_y(point.y());
-  std::vector<std::shared_ptr<const PNCJunctionInfo>> pnc_junctions;
-  HDMapUtil::BaseMap().GetPNCJunctions(hdmap_point,
-                                       FLAGS_search_radius,
-                                       &pnc_junctions);
-  if (pnc_junctions.empty() || pnc_junctions.front() == nullptr) {
-    return false;
-  }
-  const auto& pnc_junction_info = pnc_junctions.front();
-  if (pnc_junction_info != nullptr &&
-      pnc_junction_info->pnc_junction().polygon().point_size() >= 3) {
-    *pnc_junction_id = pnc_junction_info->id().id();
-    return true;
+  std::vector<PNCJunctionInfoConstPtr> pnc_junctions;
+  if (HDMapUtil::BaseMap().GetPNCJunctions(hdmap_point,
+                                           FLAGS_search_radius,
+                                           &pnc_junctions) == 0) {
+    if (pnc_junctions.size() > 0) {
+      *pnc_junction_id = pnc_junctions.front()->id().id();
+      return true;
+    }
   }
   return false;
 }
 
-bool IsPointInJunction(const PathPoint& point,
-                       std::string* junction_id) {
+bool GetJunction(const PathPoint& point, std::string* junction_id) {
   common::PointENU hdmap_point;
   hdmap_point.set_x(point.x());
   hdmap_point.set_y(point.y());
-  std::vector<std::shared_ptr<const JunctionInfo>> junctions;
-  HDMapUtil::BaseMap().GetJunctions(hdmap_point,
-                                    FLAGS_search_radius,
-                                    &junctions);
-  if (junctions.empty() || junctions.front() == nullptr) {
-    return false;
-  }
-  const auto& junction_info = junctions.front();
-  if (junction_info != nullptr &&
-      junction_info->junction().polygon().point_size() >= 3) {
-    *junction_id = junction_info->id().id();
-    return true;
+  std::vector<JunctionInfoConstPtr> junctions;
+  if (HDMapUtil::BaseMap().GetJunctions(hdmap_point,
+                                        FLAGS_search_radius,
+                                        &junctions) == 0) {
+    if (junctions.size() > 0) {
+      *junction_id = junctions.front()->id().id();
+      return true;
+    }
   }
   return false;
 }
+
+bool GetSignal(const PathPoint& point, std::string* signal_id) {
+  common::PointENU hdmap_point;
+  hdmap_point.set_x(point.x());
+  hdmap_point.set_y(point.y());
+  std::vector<SignalInfoConstPtr> signals;
+  if (HDMapUtil::BaseMap().GetSignals(hdmap_point,
+                                      FLAGS_search_radius,
+                                      &signals) == 0) {
+    if (signals.size() > 0) {
+      *signal_id = signals.front()->id().id();
+      return true;
+    }
+  }
+  return false;
+}
+
+}  // namespace
 
 /**
- * @brief Get distance to the nearest junction within search radius.
- * @return negative if no junction ahead, 0 if in junction, or positive which is
- *         the distance to the nearest junction ahead.
+ * @brief Get overlaps within search radius.
  */
-void GetNearestJunction(const ADCTrajectory& adc_trajectory,
-                        std::string* id,
-                        CloseToJunction::JunctionType* type,
-                        double* distance) {
-  *id = "";
-  *distance = -1;
+void CloseToJunctionTeller::GetOverlaps(const ADCTrajectory& adc_trajectory) {
   static std::string overlapping_junction_id;
 
   const double s_start =
       adc_trajectory.trajectory_point(0).path_point().s();
+
+  junction_id_.clear();
+  junction_distance_ = -1;
+  signal_id_.clear();
+  signal_distance_ = -1;
   for (const auto& point : adc_trajectory.trajectory_point()) {
     const auto& path_point = point.path_point();
     if (path_point.s() > FLAGS_adc_trajectory_search_distance) {
       break;
     }
-    std::string junction_id;
-    std::string pnc_junction_id;
-    const double junction = IsPointInJunction(path_point, &junction_id);
-    const double pnc_junction = IsPointInPNCJunction(path_point,
-                                                     &pnc_junction_id);
-    if (pnc_junction) {
-      // in PNC_JUNCTION (including overlapping with JUNCTION)
-      *id = pnc_junction_id;
-      *type = CloseToJunction::PNC_JUNCTION;
-      *distance = path_point.s() - s_start;
-      overlapping_junction_id = junction ? junction_id : "";
-      break;
-    } else if (junction) {
-      // in JUNCTION only
-      if (junction_id != overlapping_junction_id) {
-        // not in JUNCTION overlapping with a PNC_JUNCTION
-        *id = junction_id;
-        *type = CloseToJunction::JUNCTION;
-        *distance = path_point.s() - s_start;
+
+    // junction
+    if (junction_id_.empty() || junction_distance_ < 0) {
+      std::string junction_id;
+      std::string pnc_junction_id;
+      const double junction = GetJunction(path_point, &junction_id);
+      const double pnc_junction = GetPNCJunction(path_point, &pnc_junction_id);
+      if (pnc_junction) {
+        // in PNC_JUNCTION (including overlapping with JUNCTION)
+        junction_id_ = pnc_junction_id;
+        junction_type_ = CloseToJunction::PNC_JUNCTION;
+        junction_distance_ = path_point.s() - s_start;
+        overlapping_junction_id = junction ? junction_id : "";
+      } else if (junction) {
+        // in JUNCTION only
+        if (junction_id != overlapping_junction_id) {
+          // not in JUNCTION overlapping with a PNC_JUNCTION
+          junction_id_ = junction_id;
+          junction_type_ = CloseToJunction::JUNCTION;
+          junction_distance_ = path_point.s() - s_start;
+        }
+      } else {
+        overlapping_junction_id.clear();
       }
-      break;
-    } else {
-      overlapping_junction_id.clear();
+    }
+
+    // signal
+    if (signal_id_.empty() || signal_distance_ < 0) {
+      std::string signal_id;
+      if (GetSignal(path_point, &signal_id)) {
+        signal_id_ = signal_id;
+        signal_distance_ = path_point.s() - s_start;
+      }
     }
   }
 }
-
-}  // namespace
 
 void CloseToJunctionTeller::Init() {
   auto* manager = FrameManager::Instance();
@@ -142,22 +156,33 @@ void CloseToJunctionTeller::Update(Stories* stories) {
     return;
   }
 
-  std::string junction_id;
-  CloseToJunction::JunctionType type;
-  double distance;
-  GetNearestJunction(*trajectory, &junction_id, &type, &distance);
-  const bool close_to_junction = distance >= 0;
-  if (close_to_junction) {
+  GetOverlaps(*trajectory);
+
+  // CloseToJunction
+  if (!junction_id_.empty() && junction_distance_ >= 0) {
     if (!stories->has_close_to_junction()) {
       AINFO << "Enter CloseToJunction story";
     }
     auto* story = stories->mutable_close_to_junction();
-    story->set_id(junction_id);
-    story->set_type(type);
-    story->set_distance(distance);
+    story->set_id(junction_id_);
+    story->set_type(junction_type_);
+    story->set_distance(junction_distance_);
   } else if (stories->has_close_to_junction()) {
     AINFO << "Exit CloseToJunction story";
     stories->clear_close_to_junction();
+  }
+
+  // CloseToSignal
+  if (!signal_id_.empty() && signal_distance_ >= 0) {
+    if (!stories->has_close_to_signal()) {
+      AINFO << "Enter CloseToSignal story";
+    }
+    auto* story = stories->mutable_close_to_signal();
+    story->set_id(signal_id_);
+    story->set_distance(signal_distance_);
+  } else if (stories->has_close_to_signal()) {
+    AINFO << "Exit CloseToJunction story";
+    stories->clear_close_to_signal();
   }
 }
 
