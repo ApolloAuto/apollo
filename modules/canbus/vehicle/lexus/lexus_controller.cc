be1086248b99edd5ffa9a2b30d1f8228dc004623
Gavin-Tao <53300098+Gavin-Tao@users.noreply.github.com>
2020-04-12 22:27:33 +0800

    Update lexus_controller.cc
    Forget to output "Protocol can sender is null".

--- modules/canbus/vehicle/lexus/lexus_controller.cc
+++ modules/canbus/vehicle/lexus/lexus_controller.cc
@@ -57,6 +57,7 @@ ErrorCode LexusController::Init(
   }
 
   if (can_sender == nullptr) {
+    AERROR << "Protocol can sender is null.";
     return ErrorCode::CANBUS_ERROR;
   }
   can_sender_ = can_sender;
