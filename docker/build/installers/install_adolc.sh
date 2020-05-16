edba7ca1126502436e0afbc697ad8c38d42ef519
storypku <storypku@gmail.com>
2020-05-17 02:52:10 +0800

    Bazel: remove gflags bind and gtest temporary setting

--- docker/build/installers/install_adolc.sh
+++ docker/build/installers/install_adolc.sh
@@ -21,26 +21,24 @@ set -e
 
 cd "$(dirname "${BASH_SOURCE[0]}")"
 
-sudo apt-get install autoconf automake libtool
+#apt-get install autoconf automake libtool
 
 echo "Build and install ColPack"
 
 git clone https://github.com/CSCsw/ColPack.git
 
-# sudo works
+# works
 pushd ColPack
 pushd build/automake      # automake folder
-sudo autoreconf -vif        # generate configure files based on the machince
-sudo mkdir mywork
-pushd mywork
+autoreconf -vif        # generate configure files based on the machince
+mkdir mywork && cd mywork
 fullpath=$(pwd)        # modify fullpath to your destination folder if need
-sudo ../configure --prefix=${fullpath}
-sudo make -j 4              # Where "4" is the number of cores on your machine
-sudo make install           # install lib and include/ColPack to destination
+../configure --prefix=${fullpath}
+make -j$(nproc)        # Where "4" is the number of cores on your machine
+make install           # install lib and include/ColPack to destination
 
 mkdir -p /usr/local/colpack
-sudo cp -r include /usr/local/colpack/ && sudo cp -r lib /usr/local/colpack/
-popd
+cp -r include /usr/local/colpack/ && cp -r lib /usr/local/colpack/
 popd
 cp LICENSE /usr/local/colpack/
 popd
@@ -52,21 +50,21 @@ wget https://www.coin-or.org/download/source/ADOL-C/ADOL-C-2.6.3.zip -O ADOL-C-2
 unzip ADOL-C-2.6.3.zip
 
 pushd ADOL-C-2.6.3
-sudo autoreconf --install
-sudo automake
-sudo ./configure --prefix="/apollo/docker/build/installers/ADOL-C-2.6.3" --enable-sparse --enable-addexa --with-openmp-flag="-fopenmp" --with-colpack="/usr/local/colpack" ADD_CXXFLAGS="-fPIC" ADD_CFLAGS="-fPIC" ADD_FFLAGS="-fPIC"
+autoreconf --install
+automake
+./configure --prefix="/apollo/docker/build/installers/ADOL-C-2.6.3" --enable-sparse --enable-addexa --with-openmp-flag="-fopenmp" --with-colpack="/usr/local/colpack" ADD_CXXFLAGS="-fPIC" ADD_CFLAGS="-fPIC" ADD_FFLAGS="-fPIC"
 
-sudo make -j8 all
-sudo make install
+make -j8 all
+make install
 
 echo "Build ADOL-C done."
-sudo mkdir -p /usr/local/adolc
-sudo cp -r include /usr/local/adolc/ && sudo cp -r lib64 /usr/local/adolc/
-sudo cp LICENSE /usr/local/adolc/
+mkdir -p /usr/local/adolc
+cp -r include /usr/local/adolc/ && cp -r lib64 /usr/local/adolc/
+cp LICENSE /usr/local/adolc/
 popd
 
 export LD_LIBRARY_PATH=/usr/local/adolc/lib64:$LD_LIBRARY_PATH
 
 # Clean up.
-sudo apt-get clean && sudo rm -rf /var/lib/apt/lists/*
-sudo rm -fr ADOL-C-2.6.3.zip ADOL-C-2.6.3 ColPack
+apt-get clean && rm -rf /var/lib/apt/lists/*
+rm -fr ADOL-C-2.6.3.zip ADOL-C-2.6.3 ColPack
