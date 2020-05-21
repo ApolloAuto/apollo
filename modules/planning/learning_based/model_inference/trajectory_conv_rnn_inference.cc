0fc7db04a577b32eead8795c0674ad4ba126f732
jmtao <taojm99@gmail.com>
2020-05-21 12:39:58 -0700

    planning: add dummy implementation online inference for compilation purpose

---
+++ modules/planning/learning_based/model_inference/trajectory_conv_rnn_inference.cc
@@ -0,0 +1,38 @@
+/******************************************************************************
+ * Copyright 2020 The Apollo Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *****************************************************************************/
+
+/**
+ * @file
+ */
+
+#include "modules/planning/learning_based/model_inference/trajectory_conv_rnn_inference.h"
+
+namespace apollo {
+namespace planning {
+
+bool TrajectoryConvRnnInference::LoadModel() {
+  // TODO(all)
+  return true;
+}
+
+bool TrajectoryConvRnnInference::Inference(
+    LearningDataFrame* learning_data_frame) {
+  // TODO(all)
+  return true;
+}
+
+}  // namespace planning
+}  // namespace apollo
