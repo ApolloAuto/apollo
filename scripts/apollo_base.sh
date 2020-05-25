1b469337c189d1d62e110c370bb483db8441053e
Liu Jiaming <storypku@gmail.com>
2020-05-25 07:41:41 -0700

    Script: sync apollo.bashrc from master

--- scripts/apollo_base.sh
+++ scripts/apollo_base.sh
@@ -15,62 +15,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 ###############################################################################
+TOP_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd -P)"
+source ${TOP_DIR}/scripts/apollo.bashrc
 
-APOLLO_ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
 APOLLO_CACHE_DIR="${APOLLO_ROOT_DIR}/.cache"
-
-BOLD='\033[1m'
-RED='\033[0;31m'
-GREEN='\033[32m'
-WHITE='\033[34m'
-YELLOW='\033[33m'
-NO_COLOR='\033[0m'
-
-function info() {
-  (>&2 echo -e "[${WHITE}${BOLD}INFO${NO_COLOR}] $*")
-}
-
-function error() {
-  (>&2 echo -e "[${RED}ERROR${NO_COLOR}] $*")
-}
-
-function warning() {
-  (>&2 echo -e "${YELLOW}[WARNING] $*${NO_COLOR}")
-}
-
-function ok() {
-  (>&2 echo -e "[${GREEN}${BOLD} OK ${NO_COLOR}] $*")
-}
-
-function print_delim() {
-  echo '============================'
-}
-
-function get_now() {
-  echo $(date +%s)
-}
-
-function print_time() {
-  END_TIME=$(get_now)
-  ELAPSED_TIME=$(echo "$END_TIME - $START_TIME" | bc -l)
-  MESSAGE="Took ${ELAPSED_TIME} seconds"
-  info "${MESSAGE}"
-}
-
-function success() {
-  print_delim
-  ok "$1"
-  print_time
-  print_delim
-}
-
-function fail() {
-  print_delim
-  error "$1"
-  print_time
-  print_delim
-  exit -1
-}
 
 function check_in_docker() {
   if [ -f /.dockerenv ]; then
