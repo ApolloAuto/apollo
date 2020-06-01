6e8db071fc39c72e6ea16e1ab66646278e8ec3b1
Jinyun Zhou <32111357+JasonZhou404@users.noreply.github.com>
2020-06-02 02:00:43 +0800

    Planning: add inference unit test and some work for integration (#11360)
    * Planning: add model inference test
    
    * Planning: init planning renderer at on_lane_planning init time
    
    * Planning: use a temporary map until further testing

--- modules/planning/common/planning_gflags.h
+++ modules/planning/common/planning_gflags.h
@@ -273,3 +273,4 @@ DECLARE_string(planning_offline_bags);
 DECLARE_int32(planning_offline_mode);
 DECLARE_int32(learning_data_obstacle_history_time_sec);
 DECLARE_int32(learning_data_frame_num_per_file);
+DECLARE_string(planning_birdview_img_feature_renderer_config_file);
