b7fcc65c0711d37ed2340dd337d5c9c3692ee42e
Jiahao Chen <jeroldchen@163.com>
2020-06-11 20:16:51 +0800

    Perception: print all digits of timestamps on logs

--- modules/perception/onboard/component/detection_component.cc
+++ modules/perception/onboard/component/detection_component.cc
@@ -55,8 +55,8 @@ bool DetectionComponent::Init() {
 bool DetectionComponent::Proc(
     const std::shared_ptr<drivers::PointCloud>& message) {
   AINFO << "Enter detection component, message timestamp: "
-        << message->measurement_time() << " current timestamp: "
-        << apollo::common::time::Clock::NowInSeconds();
+        << std::to_string(message->measurement_time()) << " current timestamp: "
+        << std::to_string(apollo::common::time::Clock::NowInSeconds());
 
   std::shared_ptr<LidarFrameMessage> out_message(new (std::nothrow)
                                                      LidarFrameMessage);
@@ -104,9 +104,10 @@ bool DetectionComponent::InternalProc(
   const double timestamp = in_message->measurement_time();
   const double cur_time = apollo::common::time::Clock::NowInSeconds();
   const double start_latency = (cur_time - timestamp) * 1e3;
-  AINFO << "FRAME_STATISTICS:Lidar:Start:msg_time[" << timestamp << sensor_name_
-        << ":Start:msg_time["
-        << "]:cur_time[" << cur_time << "]:cur_latency[" << start_latency
+  AINFO << "FRAME_STATISTICS:Lidar:Start:msg_time[" << std::to_string(timestamp)
+        << "]:sensor[" << sensor_name_
+        << "]:cur_time[" << std::to_string(cur_time)
+        << "]:cur_latency[" << start_latency
         << "]";
 
   out_message->timestamp_ = timestamp;
