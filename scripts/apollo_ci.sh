7d90b571cd8a3ecec37d64c41bc2f0921b635a57
storypku <storypku@gmail.com>
2020-06-24 10:34:49 +0800

    Build: apollo_ci.sh for ci run

--- scripts/apollo_ci.sh
+++ scripts/apollo_ci.sh
@@ -8,17 +8,30 @@ ARCH="$(uname -m)"
 
 : ${USE_ESD_CAN:=false}
 
-CMDLINE_OPTIONS=
-SHORTHAND_TARGETS=
-DISABLED_TARGETS=
+APOLLO_BUILD_SH="${APOLLO_ROOT_DIR}/scripts/apollo_build.sh"
+APOLLO_TEST_SH="${APOLLO_ROOT_DIR}/scripts/apollo_test.sh"
+
+function run_ci_build() {
+    info "Running CI Build ..."
+    env USE_ESD_CAN=${USE_ESD_CAN} bash "${APOLLO_BUILD_SH}" --config=cpu
+}
+
+function run_ci_test() {
+    info "Running CI Test ..."
+    env USE_ESD_CAN=${USE_ESD_CAN} bash "${APOLLO_TEST_SH}" --config=cpu --config=unit_test
+}
 
 function main() {
-    if [[ "$1" == "test" ]]; then
-        info "CI TEST RUN"
-    else
-        info "CI BUILD RUN"
+    local cmd="$1"
+    if [ -z "${cmd}" ]; then
+        cmd="build"
     fi
-    success "ci${1} finished."
+    if [ "$1" == "test" ]; then
+        run_ci_test
+    else
+        run_ci_build
+    fi
+    success "ci${cmd} finished."
 }
 
 main "$@"
