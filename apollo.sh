a438ea665dda8226d612657c072613c25f8c74c9
liuxu <759587231@qq.com>
2020-07-08 02:01:05 -0700

    apollo.sh: add usage

--- apollo.sh
+++ apollo.sh
@@ -95,7 +95,33 @@ function build_test_and_lint() {
 }
 
 function _usage() {
-    warning "Usage: Not implemented yet"
+  RED='\033[0;31m'
+  BLUE='\033[0;34m'
+  BOLD='\033[1m'
+  NONE='\033[0m'
+
+  echo -e "\n${RED}Usage${NONE}:
+  .${BOLD}/apollo.sh${NONE} [OPTION]"
+
+  echo -e "\n${RED}Options${NONE}:
+  ${BLUE}build${NONE}: run build only
+  ${BLUE}build_opt${NONE}: build optimized binary for the code
+  ${BLUE}build_cpu${NONE}: dbg build with CPU
+  ${BLUE}build_gpu${NONE}: run build only with Caffe GPU mode support
+  ${BLUE}build_dbg${NONE}: run build only with debug
+  ${BLUE}build_opt_gpu${NONE}: build optimized binary with Caffe GPU mode support
+  ${BLUE}build_fe${NONE}: compile frontend javascript code, this requires all the node_modules to be installed already
+  ${BLUE}build_teleop${NONE}: run build with teleop
+  ${BLUE}build_prof${NONE}: build for gprof support.
+  ${BLUE}buildify${NONE}: fix style of BUILD files
+  ${BLUE}check${NONE}: run test and lint, please make sure it passes before checking in new code
+  ${BLUE}clean${NONE}: run Bazel clean
+  ${BLUE}config${NONE}: run configurator tool
+  ${BLUE}doc${NONE}: generate doxygen document
+  ${BLUE}lint${NONE}: run code style check
+  ${BLUE}usage${NONE}: print this menu
+  ${BLUE}test${NONE}: run all unit tests
+  "
 }
 
 function main() {
