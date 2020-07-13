d5c1a224a07960d0a0b65fd4891dfc520cb78ce6
storypku <storypku@gmail.com>
2020-07-13 00:34:18 -0700

    Build: cleanup PYTHONPATH

--- cyber/setup.bash
+++ cyber/setup.bash
@@ -10,29 +10,28 @@ export QT_QPA_PLATFORM_PLUGIN_PATH=${QT5_PATH}/plugins
 add_to_path "${QT5_PATH}/bin"
 
 bazel_bin_path="${APOLLO_ROOT_DIR}/bazel-bin"
-visualizer_path="${bazel_bin_path}/modules/tools/visualizer"
-
 cyber_bin_path="${bazel_bin_path}/cyber"
 cyber_tool_path="${bazel_bin_path}/cyber/tools"
 recorder_path="${cyber_tool_path}/cyber_recorder"
+launch_path="${cyber_tool_path}/cyber_launch"
+channel_path="${cyber_tool_path}/cyber_channel"
+node_path="${cyber_tool_path}/cyber_node"
+service_path="${cyber_tool_path}/cyber_service"
 monitor_path="${cyber_tool_path}/cyber_monitor"
-
-launch_path="${CYBER_PATH}/tools/cyber_launch"
-channel_path="${CYBER_PATH}/tools/cyber_channel"
-node_path="${CYBER_PATH}/tools/cyber_node"
-service_path="${CYBER_PATH}/tools/cyber_service"
+visualizer_path="${bazel_bin_path}/modules/tools/visualizer"
 rosbag_to_record_path="${bazel_bin_path}/modules/data/tools/rosbag_to_record"
 
 # TODO(all): place all these in one place and add_to_path
-for entry in "${cyber_bin_path}" "${recorder_path}" "${monitor_path}" "${launch_path}" \
-    "${channel_path}" "${node_path}" "${service_path}" \
+for entry in "${cyber_bin_path}" \
+    "${recorder_path}" "${monitor_path}"  \
+    "${channel_path}" "${node_path}" \
     "${visualizer_path}" \
     "${rosbag_to_record_path}" ; do
     add_to_path "${entry}"
 done
 
-export PYTHONPATH=${bazel_bin_path}/cyber/python/internal:${bazel_bin_path}/cyber/python:$PYTHONPATH
-#export PYTHONPATH=${bazel_bin_path}:${bazel_bin_path}/cyber/python/internal:${CYBER_PATH}/python:$PYTHONPATH
+# ${CYBER_PATH}/python
+export PYTHONPATH=${bazel_bin_path}/cyber/python/internal:${PYTHONPATH}
 
 export CYBER_DOMAIN_ID=80
 export CYBER_IP=127.0.0.1
