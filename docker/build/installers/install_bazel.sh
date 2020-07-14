c0b72772aae0cf421c0f1ab9e592815eb9e34114
storypku <storypku@gmail.com>
2020-07-14 10:43:05 -0700

    Docker: install buidifier for arm64

--- docker/build/installers/install_bazel.sh
+++ docker/build/installers/install_bazel.sh
@@ -44,9 +44,9 @@ if [ "$TARGET_ARCH" == "x86_64" ]; then
 
   ## buildifier ##
   PKG_NAME="buildifier"
-  BUILDIFIER_VERSION="3.3.0"
+  BUILDTOOLS_VERSION="3.3.0"
   CHECKSUM="0c5df005e2b65060c715a7c5764c2a04f7fac199bd73442e004e0bf29381a55a"
-  DOWNLOAD_LINK="https://github.com/bazelbuild/buildtools/releases/download/${BUILDIFIER_VERSION}/buildifier"
+  DOWNLOAD_LINK="https://github.com/bazelbuild/buildtools/releases/download/${BUILDTOOLS_VERSION}/buildifier"
   download_if_not_cached "${PKG_NAME}" "${CHECKSUM}" "${DOWNLOAD_LINK}"
 
   chmod a+x ${PKG_NAME}
@@ -55,7 +55,6 @@ if [ "$TARGET_ARCH" == "x86_64" ]; then
 
   ## buildozer
   PKG_NAME="buildozer"
-  BUILDOZER_VERSION="3.3.0"
   CHECKSUM="6618c2a4473ddc35a5341cf9a651609209bd5362e0ffa54413be256fe8a4081a"
   DOWNLOAD_LINK="https://github.com/bazelbuild/buildtools/releases/download/${BUILDOZER_VERSION}/buildozer"
   download_if_not_cached "${PKG_NAME}" "${CHECKSUM}" "${DOWNLOAD_LINK}"
@@ -66,12 +65,12 @@ if [ "$TARGET_ARCH" == "x86_64" ]; then
   info "Done installing bazel ${BAZEL_VERSION} with buildifier and buildozer"
 
 elif [ "$TARGET_ARCH" == "aarch64" ]; then
-  BAZEL_ARM_VERSION="3.4.0"
+  BAZEL_VERSION="3.4.0"
   # Ref: https://docs.bazel.build/versions/master/install-compile-source.html
   # Ref: https://github.com/storypku/storydev/blob/master/bazel-build/build-bazel-from-source.md
   # Download Mode
-  ARM64_BINARY="bazel-${BAZEL_ARM_VERSION}-linux-arm64"
-  DOWNLOAD_LINK="https://github.com/bazelbuild/bazel/releases/download/${BAZEL_ARM_VERSION}/${ARM64_BINARY}"
+  ARM64_BINARY="bazel-${BAZEL_VERSION}-linux-arm64"
+  DOWNLOAD_LINK="https://github.com/bazelbuild/bazel/releases/download/${BAZEL_VERSION}/${ARM64_BINARY}"
   CHECKSUM="440672f319be239d7dd5d7c5062edee23499dd49b49e89cc26dc9d44aa044a96"
   download_if_not_cached "${ARM64_BINARY}" "${CHECKSUM}" "${DOWNLOAD_LINK}"
   chmod a+x ${ARM64_BINARY}
@@ -80,9 +79,19 @@ elif [ "$TARGET_ARCH" == "aarch64" ]; then
 
   cp /opt/apollo/rcfiles/bazel_completion.bash /etc/bash_completion.d/bazel
 
-  # TODO(buildifer)
+  BUILDTOOLS_VERSION="3.3.0"
+  PKG_NAME="buildifier-${BUILDTOOLS_VERSION}-linux-arm64"
+  CHECKSUM="11df20761f6a14adcc21ea684225e029d6a5f4a881eb3477ea8c24afda316bdf"
+  DOWNLOAD_LINK="https://apollo-platform-system.bj.bcebos.com/archive/6.0/${PKG_NAME}"
+  download_if_not_cached "${PKG_NAME}" "${CHECKSUM}" "${DOWNLOAD_LINK}"
 
-  info "Done installing bazel ${BAZEL_ARM_VERSION}"
+  cp -f ${PKG_NAME} "${SYSROOT_DIR}/bin/buildifier"
+  chmod a+x "${SYSROOT_DIR}/bin/buildifier"
+  rm -rf ${PKG_NAME}
+  # buildozer can be retrieved from
+  # https://apollo-platform-system.bj.bcebos.com/archive/6.0/buildozer-3.3.0-linux-arm64
+
+  info "Done installing bazel ${BAZEL_VERSION} with buildifier ${BUILDTOOLS_VERSION}"
 else
   error "Target arch ${TARGET_ARCH} not supported yet"
   exit 1
