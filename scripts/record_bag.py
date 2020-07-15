fcad1d7c61cac9d61887ff4c9ed16f1b1c01bcf3
SeasoulChris <274224843@qq.com>
2020-07-15 20:43:49 +0800

    change recorder stop_signal to SIGINT

--- scripts/record_bag.py
+++ scripts/record_bag.py
@@ -89,6 +89,8 @@ SMALL_TOPICS = [
 ]
 
 LARGE_TOPICS = [
+    '/apollo/sensor/camera/front_6mm/image',
+    '/apollo/sensor/camera/front_12mm/image',
     '/apollo/sensor/camera/front_12mm/image/compressed',
     '/apollo/sensor/camera/front_6mm/image/compressed',
     '/apollo/sensor/camera/left_fisheye/image/compressed',
@@ -110,6 +112,9 @@ LARGE_TOPICS = [
     '/apollo/sensor/velodyne64/compensator/PointCloud2',
     '/apollo/sensor/lidar128/PointCloud2',
     '/apollo/sensor/lidar128/compensator/PointCloud2',
+    '/apollo/sensor/lidar16/Scan',
+    '/apollo/sensor/lidar16/PointCloud2',
+    '/apollo/sensor/lidar16/compensator/PointCloud2',
 ]
 
 def shell_cmd(cmd, alert_on_failure=True):
