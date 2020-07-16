7ef4aec3176e1748cb830ddb98f63b283750817f
changsh726 <changsonghong@baidu.com>
2020-07-16 13:13:41 +0800

    Cyber: fix sys.path for wrapper_lib

--- cyber/python/cyber_py3/cyber.py
+++ cyber/python/cyber_py3/cyber.py
@@ -32,15 +32,9 @@ PY_CALLBACK_TYPE = ctypes.CFUNCTYPE(ctypes.c_int, ctypes.c_char_p)
 PY_CALLBACK_TYPE_T = ctypes.CFUNCTYPE(ctypes.c_int, ctypes.c_char_p)
 
 # init vars
-CYBER_PATH = os.environ.get('CYBER_PATH', '/apollo/cyber')
-CYBER_DIR = os.path.split(CYBER_PATH)[0]
-sys.path.append(CYBER_PATH + "/third_party/")
-sys.path.append(CYBER_PATH + "/lib/")
-
-sys.path.append(CYBER_PATH + "/lib/python/")
-
-sys.path.append(CYBER_DIR + "/python/")
-sys.path.append(CYBER_DIR + "/cyber/")
+wrapper_lib_path = os.path.abspath(os.path.join(os.path.dirname(__file__),
+                                                '../internal'))
+sys.path.append(wrapper_lib_path)
 
 _CYBER = importlib.import_module('_cyber_wrapper')
 
--- cyber/python/cyber_py3/cyber_timer.py
+++ cyber/python/cyber_py3/cyber_timer.py
@@ -27,15 +27,9 @@ import sys
 PY_TIMER_CB_TYPE = ctypes.CFUNCTYPE(ctypes.c_void_p)
 
 # init vars
-CYBER_PATH = os.environ.get('CYBER_PATH', '/apollo/cyber')
-CYBER_DIR = os.path.split(CYBER_PATH)[0]
-sys.path.append(CYBER_PATH + "/third_party/")
-sys.path.append(CYBER_PATH + "/lib/")
-
-sys.path.append(CYBER_PATH + "/lib/python/")
-
-sys.path.append(CYBER_DIR + "/python/")
-sys.path.append(CYBER_DIR + "/cyber/")
+wrapper_lib_path = os.path.abspath(os.path.join(os.path.dirname(__file__),
+                                                '../internal'))
+sys.path.append(wrapper_lib_path)
 
 _CYBER_TIMER = importlib.import_module('_cyber_timer_wrapper')
 
