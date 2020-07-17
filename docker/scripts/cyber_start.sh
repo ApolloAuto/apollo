43cfd53be53a5459f151e929de2aef5e7cfb5fcd
Crysple <871886504@qq.com>
2020-07-16 21:21:30 -0400

    Docker: update cyber_start with --device /dev/snd

--- docker/scripts/cyber_start.sh
+++ docker/scripts/cyber_start.sh
@@ -492,6 +492,7 @@ function start_cyber_container() {
         --hostname "${CYBER_INSIDE}" \
         --shm-size 2G \
         --pid=host \
+        --device /dev/snd \
         "${image}" \
         /bin/bash
 
