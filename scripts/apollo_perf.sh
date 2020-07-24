791cb41b05d7b4a6fef9bf2b18053c49b14d9b7b
liuxu <759587231@qq.com>
2020-07-23 19:53:49 -0700

    perf: fix format

--- scripts/apollo_perf.sh
+++ scripts/apollo_perf.sh
@@ -7,20 +7,19 @@ source "${TOP_DIR}/scripts/apollo_base.sh"
 
 function main() {
     if ! command -v perf >/dev/null 2>&1; then
-      bash  "${TOP_DIR}/docker/scripts/install_perf.sh"
-      
+        bash  "${TOP_DIR}/docker/scripts/install_perf.sh"
     fi
     if [ -e /apollo-simulator ];then
-      cd /apollo-simulator
-      bash build.sh run >/dev/null 2>&1 & 
-      sleep 1
-      ps -ef | grep "replay_engine" | grep -v grep | awk '{print $2}' |  xargs sudo perf record  -g -p
-      if [ -e perf.data ];then
-        sudo perf report -i perf.data
-      fi
-      info "perf report was generated here"
+        cd /apollo-simulator
+        bash build.sh run --scenario_names=1 >/dev/null 2>&1 & 
+        sleep 1
+        ps -ef | grep "replay_engine" | grep -v grep | awk '{print $2}' |  xargs sudo perf record  -g -p
+        if [ -e perf.data ];then
+            sudo perf report -i perf.data
+        fi
+        info "perf report was generated here"
     else
-      echo "apollo-simulator doesn't exist"
+        info "apollo-simulator doesn't exist"
     fi
 }
 
