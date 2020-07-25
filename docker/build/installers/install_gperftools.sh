bc4ac3b912192e93201a6685eeb31738f30181c2
storypku <storypku@gmail.com>
2020-07-25 12:46:31 +0800

    Docker: installer for doxygen

--- docker/build/installers/install_gperftools.sh
+++ docker/build/installers/install_gperftools.sh
@@ -39,6 +39,7 @@ tar xzf ${PKG_NAME}
 pushd "gperftools-gperftools-${VERSION}" >/dev/null
     ./autogen.sh
     ./configure --prefix=/usr
+    # shared lib only options: --enable-static=no --with-pic=yes
     make -j$(nproc)
     make install
 popd >/dev/null
