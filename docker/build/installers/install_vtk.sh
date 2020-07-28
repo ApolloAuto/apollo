33428a8ef0230778c3d04cff3c43b3b30e562539
storypku <storypku@gmail.com>
2020-07-28 07:29:42 -0700

    Docker: updated vtk installer to use system libs by default

--- docker/build/installers/install_vtk.sh
+++ docker/build/installers/install_vtk.sh
@@ -21,6 +21,22 @@ set -e
 cd "$(dirname "${BASH_SOURCE[0]}")"
 . /tmp/installers/installer_base.sh
 
+apt_get_update_and_install \
+        libjpeg-dev \
+        libpng-dev  \
+        libtiff-dev \
+        libeigen3-dev \
+        liblzma-dev \
+        libxml2-dev \
+        libexpat1-dev \
+        zlib1g-dev \
+        liblz4-dev \
+        libdouble-conversion-dev \
+        libsqlite3-dev \
+        libglew-dev \
+        libfreetype6-dev \
+        libhdf5-dev
+
 if ldconfig -p | grep -q libvtkCommonCore ; then
     info "Found existing VTK installation. Skip re-installing."
     exit 0
@@ -29,8 +45,12 @@ fi
 TARGET_ARCH="$(uname -m)"
 
 # Note(storypku):
-#   Although vtk are shipped with Ubuntu distribution, we decide to build VTK
-#   from source here to avoid massive amount of system dependency.
+# Although VTK can be installed via apt, build it from source to
+#   1) reduce image size
+#   2) avoid a lot of dependencies
+# RTFM:
+# 1) https://src.fedoraproject.org/rpms/vtk/blob/master/f/vtk.spec
+# 2) https://vtk.org/Wiki/VTK/Building/Linux
 
 VERSION=8.2.0
 PKG_NAME="VTK-8.2.0.tar.gz"
@@ -41,15 +61,33 @@ download_if_not_cached "${PKG_NAME}" "${CHECKSUM}" "${DOWNLOAD_LINK}"
 
 tar xzf ${PKG_NAME}
 
-# https://vtk.org/Wiki/VTK/Building/Linux
 # Note(storypku): Qt-related features disabled
+# VTK_USE_BOOST
 pushd VTK-${VERSION}
     mkdir build && cd build
     cmake .. \
+        -DVTK_USE_SYSTEM_LIBRARIES=ON \
+        -DVTK_USE_SYSTEM_JPEG=ON  \
+        -DVTK_USE_SYSTEM_PNG=ON   \
+        -DVTK_USE_SYSTEM_TIFF=ON \
+        -DVTK_USE_SYSTEM_EIGEN=ON \
+        -DVTK_USE_SYSTEM_LZMA=ON \
+        -DVTK_USE_SYSTEM_ZLIB=ON \
+        -DVTK_USE_SYSTEM_LZ4=ON \
+        -DVTK_USE_SYSTEM_LIBXML2=ON \
+        -DVTK_USE_SYSTEM_EXPAT=ON \
+        -DVTK_USE_SYSTEM_LIBPROJ=ON \
+        -DVTK_USE_SYSTEM_SQLITE=ON \
+        -DVTK_USE_SYSTEM_PUGIXML=OFF \
+        -DVTK_USE_SYSTEM_NETCDF=OFF \
+        -DVTK_USE_SYSTEM_GL2PS=OFF \
+        -DVTK_USE_SYSTEM_LIBHARU=OFF \
+        -DVTK_USE_SYSTEM_JSONCPP=OFF \
         -DVTK_Group_Qt=OFF \
         -DBUILD_SHARED_LIBS=ON \
         -DCMAKE_INSTALL_PREFIX="${SYSROOT_DIR}" \
         -DCMAKE_BUILD_TYPE=Release
+
     thread_num="$(nproc)"
     if [ "${TARGET_ARCH}" = "aarch64" ]; then
         thread_num=$(( thread_num / 2 ))
