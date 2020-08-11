92d964fbd44f6449a575f5c77b5011114297d595
changsh726 <changsonghong@baidu.com>
2020-08-11 19:55:43 +0800

    Scripts: auto-format BUILD file generated from proto

--- scripts/proto_build_generator.py
+++ scripts/proto_build_generator.py
@@ -3,6 +3,7 @@
 import os
 import re
 import sys
+import subprocess
 
 TAB = " " * 4
 
@@ -209,6 +210,13 @@ def grpc_check(fpath):
         return grpc_found
 
 
+##================== FORMAT GENERATED FILE =================================##
+def run_buildifier(build_file_path):
+    script_path = "/apollo/scripts/buildifier.sh"
+    if os.path.exists(script_path):
+        subprocess.call(["bash", script_path, build_file_path])
+
+
 ##================== MAIN FUNCTION =========================================##
 def main(build_file_path):
     if not _path_check(build_file_path):
@@ -245,6 +253,8 @@ def main(build_file_path):
     fout.close()
     print("Congratulations, {} was successfully generated.".format(
         build_file_path))
+
+    run_buildifier(build_file_path)
 
 
 ##=========== BAZEL BUILD RULE FOR A SINGLE PROTO FILE =====================##
