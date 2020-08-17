e7abf914591e10da572cd1584017b1e84251277a
storypku <storypku@gmail.com>
2020-08-16 23:06:46 -0700

    Scripts: use generated py binary

--- scripts/control_info.sh
+++ scripts/control_info.sh
@@ -16,9 +16,7 @@
 # limitations under the License.
 ###############################################################################
 
+TOP_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd -P)"
+source "${TOP_DIR}/scripts/apollo_base.sh"
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-source "${DIR}/apollo_base.sh"
-
-python ${DIR}/../modules/tools/control_info/control_info.py $@
+${TOP_DIR}/bazel-bin/modules/tools/control_info/control_info "$@"
