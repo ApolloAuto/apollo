ccce11e5cf335771f82d416abe01d9462b2e2d08
liuxu <759587231@qq.com>
2020-08-21 03:50:46 -0700

    distdir: remove in scripts

--- scripts/apollo_build.sh
+++ scripts/apollo_build.sh
@@ -129,7 +129,7 @@ function _parse_cmdline_arguments() {
 
 function _run_bazel_build_impl() {
   local job_args="--jobs=$(nproc) --local_ram_resources=HOST_RAM*0.7"
-  bazel build --distdir="${APOLLO_CACHE_DIR}/distdir" ${job_args} $@
+  bazel build ${job_args} $@
 }
 
 function bazel_build() {
