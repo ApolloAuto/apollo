4780133a0f689283dcdbafb418446e9ebce60863
storypku <storypku@gmail.com>
2020-08-21 22:39:06 -0700

    Docker: updated bazel/buildifier installer

--- docker/build/installers/install_bazel.sh
+++ docker/build/installers/install_bazel.sh
@@ -29,40 +29,33 @@ BAZEL_VERSION="3.4.1"
 if [ "$TARGET_ARCH" == "x86_64" ]; then
   # https://docs.bazel.build/versions/master/install-ubuntu.html
   PKG_NAME="bazel_${BAZEL_VERSION}-linux-x86_64.deb"
-  DOWNLOAD_LINK=https://github.com/bazelbuild/bazel/releases/download/${BAZEL_VERSION}/${PKG_NAME}
-  SHA256SUM="1a64c807716e10c872f1618852d95f4893d81667fe6e691ef696489103c9b460"
+  DOWNLOAD_LINK="https://github.com/bazelbuild/bazel/releases/download/${BAZEL_VERSION}/${PKG_NAME}"
+  SHA256SUM="dc8f51b7ed039d57bb990a1eebddcbb0014fe267a88df8972f4609ded1f11c90"
   download_if_not_cached $PKG_NAME $SHA256SUM $DOWNLOAD_LINK
 
-  apt-get -y update && \
-    apt-get -y install \
+  apt_get_update_and_install \
     zlib1g-dev
 
   # https://docs.bazel.build/versions/master/install-ubuntu.html#step-3-install-a-jdk-optional
   # openjdk-11-jdk
 
-  dpkg -i $PKG_NAME
+  dpkg -i "${PKG_NAME}"
+
+  # Cleanup right after installation
+  rm -rf "${PKG_NAME}"
 
   ## buildifier ##
-  PKG_NAME="buildifier"
-  BUILDTOOLS_VERSION="3.3.0"
-  CHECKSUM="0c5df005e2b65060c715a7c5764c2a04f7fac199bd73442e004e0bf29381a55a"
+  BUILDTOOLS_VERSION="3.4.0"
+  PKG_NAME="buildifier-${BUILDTOOLS_VERSION}.${TARGET_ARCH}.bin"
+  CHECKSUM="5d47f5f452bace65686448180ff63b4a6aaa0fb0ce0fe69976888fa4d8606940"
   DOWNLOAD_LINK="https://github.com/bazelbuild/buildtools/releases/download/${BUILDTOOLS_VERSION}/buildifier"
   download_if_not_cached "${PKG_NAME}" "${CHECKSUM}" "${DOWNLOAD_LINK}"
 
-  chmod a+x ${PKG_NAME}
-  cp -f ${PKG_NAME} "${SYSROOT_DIR}/bin"
+  cp -f ${PKG_NAME} "${SYSROOT_DIR}/bin/buildifier"
+  chmod a+x "${SYSROOT_DIR}/bin/buildifier"
   rm -rf ${PKG_NAME}
 
-  ## buildozer
-  PKG_NAME="buildozer"
-  CHECKSUM="6618c2a4473ddc35a5341cf9a651609209bd5362e0ffa54413be256fe8a4081a"
-  DOWNLOAD_LINK="https://github.com/bazelbuild/buildtools/releases/download/${BUILDOZER_VERSION}/buildozer"
-  download_if_not_cached "${PKG_NAME}" "${CHECKSUM}" "${DOWNLOAD_LINK}"
-
-  chmod a+x ${PKG_NAME}
-  cp -f "${PKG_NAME}" "${SYSROOT_DIR}/bin/"
-  rm -f "${PKG_NAME}"
-  info "Done installing bazel ${BAZEL_VERSION} with buildifier and buildozer"
+  info "Done installing bazel ${BAZEL_VERSION} with buildifier ${BUILDTOOLS_VERSION}"
 
 elif [ "$TARGET_ARCH" == "aarch64" ]; then
   ARM64_BINARY="bazel-${BAZEL_VERSION}-linux-arm64"
@@ -70,8 +63,8 @@ elif [ "$TARGET_ARCH" == "aarch64" ]; then
   DOWNLOAD_LINK="https://github.com/bazelbuild/bazel/releases/download/${BAZEL_VERSION}/${ARM64_BINARY}"
   # https://github.com/bazelbuild/bazel/releases/download/3.4.1/bazel-3.4.1-linux-arm64
   download_if_not_cached "${ARM64_BINARY}" "${CHECKSUM}" "${DOWNLOAD_LINK}"
-  chmod a+x ${ARM64_BINARY}
   cp -f ${ARM64_BINARY} "${SYSROOT_DIR}/bin/bazel"
+  chmod a+x "${SYSROOT_DIR}/bin/bazel"
   rm -rf "${ARM64_BINARY}"
 
   cp /opt/apollo/rcfiles/bazel_completion.bash /etc/bash_completion.d/bazel
