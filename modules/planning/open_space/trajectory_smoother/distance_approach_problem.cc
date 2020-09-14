28bb8035e9ec977c9599dc117c7aafc94a799772
changsh726 <changsonghong@baidu.com>
2020-09-14 18:19:59 +0800

    Planning: re-enable CUDA in open space planner

--- modules/planning/open_space/trajectory_smoother/distance_approach_problem.cc
+++ modules/planning/open_space/trajectory_smoother/distance_approach_problem.cc
@@ -63,14 +63,12 @@ bool DistanceApproachProblem::Solve(
         horizon, ts, ego, xWS, uWS, l_warm_up, n_warm_up, x0, xF, last_time_u,
         XYbounds, obstacles_edges_num, obstacles_num, obstacles_A, obstacles_b,
         planner_open_space_config_);
-    // TODO(xiaoxq): Disable CUDA in planning temporarily.
-    // } else if (planner_open_space_config_.distance_approach_config()
-    //              .distance_approach_mode() == DISTANCE_APPROACH_IPOPT_CUDA) {
-    //   ptop = new DistanceApproachIPOPTCUDAInterface(
-    //       horizon, ts, ego, xWS, uWS, l_warm_up, n_warm_up, x0, xF,
-    //       last_time_u, XYbounds, obstacles_edges_num, obstacles_num,
-    //       obstacles_A, obstacles_b, planner_open_space_config_);
-    // }
+  } else if (planner_open_space_config_.distance_approach_config()
+                 .distance_approach_mode() == DISTANCE_APPROACH_IPOPT_CUDA) {
+    ptop = new DistanceApproachIPOPTCUDAInterface(
+        horizon, ts, ego, xWS, uWS, l_warm_up, n_warm_up, x0, xF, last_time_u,
+        XYbounds, obstacles_edges_num, obstacles_num, obstacles_A, obstacles_b,
+        planner_open_space_config_);
   } else if (planner_open_space_config_.distance_approach_config()
                  .distance_approach_mode() ==
              DISTANCE_APPROACH_IPOPT_FIXED_DUAL) {
