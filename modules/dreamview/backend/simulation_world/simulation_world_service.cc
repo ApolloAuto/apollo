270f8ace8ba3b92dc0a0530877db6421f4150027
v_panxuechao <v_panxuechao@baidu.com>
2020-11-06 14:46:21 +0800

    Dreamview:: add battery_soc and gear_localtion

--- modules/dreamview/backend/simulation_world/simulation_world_service.cc
+++ modules/dreamview/backend/simulation_world/simulation_world_service.cc
@@ -541,6 +541,9 @@ void SimulationWorldService::UpdateSimulationWorld(const Chassis &chassis) {
   UpdateTurnSignal(chassis.signal(), auto_driving_car);
 
   auto_driving_car->set_disengage_type(DeduceDisengageType(chassis));
+
+  auto_driving_car->set_battery_soc_percentage(chassis.battery_soc_percentage());
+  auto_driving_car->set_gear_localtion(chassis.gear_location());
 }
 
 template <>
