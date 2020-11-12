5239b6f0d36e48ed3b26fbbb17c3f074e4e8e638
changsh726 <changsonghong@baidu.com>
2020-11-12 16:38:55 +0800

    sensor_calibration: generate channels config from record

--- scripts/extract_data.sh
+++ scripts/extract_data.sh
@@ -19,6 +19,10 @@
 TOP_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd -P)"
 
 TARGET_DIR="${TOP_DIR}/sensor_calibration/lidar_to_gnss"
+TEMPLATE_DIR="${TOP_DIR}/docs/Apollo_Fuel/examples/sensor_calibration/lidar_to_gnss"
+CHANNEL_TEMPLATE="${TEMPLATE_DIR}/channel_template.txt"
+EXTRACTION_RATE="5"
+
 RECORD_FILES=()
 RECORD_DIRS=()
 
@@ -53,19 +57,19 @@ function parse_args() {
 function _check_target_dir() {
   if [ ! -d "${TARGET_DIR}" ]; then
     mkdir -p ${TARGET_DIR}
-    cp -R ${TOP_DIR}/docs/Apollo_Fuel/examples/sensor_calibration/lidar_to_gnss/* ${TARGET_DIR}
+    cp -R ${TEMPLATE_DIR}/* ${TARGET_DIR}
   elif [[ ! -z "$(ls -A ${TARGET_DIR})" ]]; then
     local warn="The ${TARGET_DIR} is not empty, do you want to delete it? (Y/n)"
     echo "${warn}"
     local answer
     typeset -l answer
     read answer
-    if [ "${answer}" == "y" ];then
+    if [ "${answer}" == "y" ]; then
       rm -rf ${TARGET_DIR}/*
-      cp -R ${TOP_DIR}/docs/Apollo_Fuel/examples/sensor_calibration/lidar_to_gnss/* ${TARGET_DIR}
+      cp -R ${TEMPLATE_DIR}/* ${TARGET_DIR}
     fi
   else
-    cp -R ${TOP_DIR}/docs/Apollo_Fuel/examples/sensor_calibration/lidar_to_gnss/* ${TARGET_DIR}
+    cp -R ${TEMPLATE_DIR}/* ${TARGET_DIR}
   fi
 }
 
@@ -73,17 +77,22 @@ function _check_target_dir() {
 # we can only install it this way
 function _install_pypcd() {
   git clone https://github.com/dimatura/pypcd --depth=1
-  pushd pypcd > /dev/null
+  pushd pypcd >/dev/null
   git fetch origin pull/9/head:python3 && git checkout python3
   python3 setup.py install --user
-  popd > /dev/null
+  popd >/dev/null
   rm -rf pypcd
 }
 
 function get_records() {
   for file in "${RECORD_FILES[@]}"; do
     if [ -f "${file}" ]; then
-      cp "${file}" "${TARGET_DIR}"
+      if [[ "${file}" == *"record"* ]]; then
+        cp "${file}" "${TARGET_DIR}/records"
+      else
+        echo "The input file ${file} is not a record!"
+        exit 1
+      fi
     else
       echo "File ${file} doesn't exist!"
       exit 1
@@ -92,6 +101,11 @@ function get_records() {
 
   for dir in "${RECORD_DIRS[@]}"; do
     if [ -d "${dir}" ]; then
+      if [[ -z "$(ls ${dir} | grep record)" ]]; then
+        echo "There is no reord file in ${dir}!"
+        exit 1
+      fi
+
       case "${dir}" in
         */)
           cp ${dir}* "${TARGET_DIR}/records"
@@ -122,6 +136,32 @@ function install_if_not_exist() {
   done
 }
 
+function update_config() {
+  local record
+  local lidar_channels
+  local tmp_file="${TARGET_DIR}/tmp.txt"
+  record="${TARGET_DIR}/records/$(ls ${TARGET_DIR}/records | grep -m1 record)"
+  lidar_channels=($(cyber_recorder info ${record} | awk '{print $1}' |
+    grep PointCloud2 | grep -v "fusion" | grep -v "compensator"))
+
+  if [ "${#lidar_channels[*]}" -eq 0 ]; then
+    echo "There is no PointCloud messages in ${reocrd}, please check your record!"
+    exit 1
+  fi
+
+  sed -i "s|__RATE__|${EXTRACTION_RATE}|g" "${CHANNEL_TEMPLATE}"
+  for channel in "${lidar_channels[@]}"; do
+    sed -i "s|__NAME__|${channel}|g" "${CHANNEL_TEMPLATE}"
+    cat "${CHANNEL_TEMPLATE}" >>"${tmp_file}"
+    sed -i "s|${channel}|__NAME__|g" "${CHANNEL_TEMPLATE}"
+  done
+  sed -i "s|${EXTRACTION_RATE}|__RATE__|g" "${CHANNEL_TEMPLATE}"
+
+  sed -i "/# channel of mulitple lidars/{n;N;N;N;N;d}" "${TARGET_DIR}/lidar_to_gnss.config"
+  sed -i "/# channel of mulitple lidars/r ${tmp_file}" "${TARGET_DIR}/lidar_to_gnss.config"
+  rm -f ${tmp_file}
+}
+
 function main() {
   if [ "$#" -eq 0 ]; then
     print_usage
@@ -138,6 +178,7 @@ function main() {
   fi
 
   get_records
+  update_config
 
   install_if_not_exist "pyyaml" "pypcd"
 
