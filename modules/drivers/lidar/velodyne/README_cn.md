3b400cd362e2e59b85636659ba3866bfddc810eb
liufeng31 <liufeng31@baidu.com>
2020-11-16 20:30:19 +0800

    Drivers: rearrange driver files and configurations

---
+++ modules/drivers/lidar/velodyne/README_cn.md
@@ -0,0 +1,44 @@
+## velodyne
+velodyne驱动是以component的形式实现的，包含了:
+
+1. 数据读取打包 --> /driver
+2. 生成点云 --> /convert
+3. 点云融合 --> /fusion
+4. 运动补偿 --> /compensator
+
+4个处理组件，其中`运动补偿`需要依赖`tf`来进行坐标转换查询，因此需要和`gnss_driver`一起运行才能正常工作，点云融合主要将多个激光雷达数据融合成一张点云
+
+### Output channels
+
+1. 数据包
+  channel: /apollo/sensor/lidar128/Scan
+  type: apollo::drivers::velodyne::VelodyneScan
+  proto: [modules/drivers/lidar/velodyne/proto/velodyne.proto](https://github.com/ApolloAuto/apollo/blob/master/modules/drivers/lidar/velodyne/proto/velodyne.proto)
+2. 原始点云
+  channel: /apollo/sensor/lidar128/PointCloud2
+  type: apollo::drivers::PointCloud
+  proto: [modules/drivers/proto/pointcloud.proto]https://github.com/ApolloAuto/apollo/blob/master/modules/drivers/proto/pointcloud.proto
+3. 补偿点云
+  channel: /apollo/sensor/lidar128/compensator/PointCloud2
+  type: apollo::drivers::PointCloud
+  proto: [modules/drivers/proto/pointcloud.proto]https://github.com/ApolloAuto/apollo/blob/master/modules/drivers/proto/pointcloud.proto
+
+### 坐标系
+* world
+* novatel
+* velodyne128
+
+### 启动velodyne驱动
+**请先修改并确认launch文件中的参数与实际车辆相对应**
+```bash
+#in docker
+cd /apollo && cyber_launch start modules/drivers/lidar/velodyne/launch/velodyne.launch
+```
+
+### 常见问题
+1. "basetime is zero"
+  position packet 不可用，检查gps接线或者将车开到有信号的地方
+2. "velodyne port 2368 poll() timeout"
+  主机与velodyne网络不通，可用命令`sudo tcpdump -i eth0 udp port 2368`查看是否能收到velodyne数据包
+3. "cannot find transform ..."
+  `运动补偿`依赖`tf`，请检查`gnss_driver`是否已启动，同时可通过cyber_monitor工具查看 `/tf` channel 是否有消息输出
---
+++ modules/drivers/lidar/velodyne/driver/driver64.cc
@@ -0,0 +1,121 @@
+/******************************************************************************
+ * Copyright 2017 The Apollo Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *****************************************************************************/
+
+#include <cmath>
+#include <ctime>
+#include <string>
+
+#include "modules/drivers/lidar/velodyne/driver/driver.h"
+// #include "ros/ros.h"
+
+namespace apollo {
+namespace drivers {
+namespace velodyne {
+
+void Velodyne64Driver::Init() {
+  const double frequency = config_.rpm() / 60.0;  // expected Hz rate
+
+  // default number of packets for each scan is a single revolution
+  // (fractions rounded up)
+  config_.set_npackets(static_cast<int>(ceil(packet_rate_ / frequency)));
+  AINFO << "publishing " << config_.npackets() << " packets per scan";
+
+  input_.reset(new SocketInput());
+  input_->init(config_.firing_data_port());
+}
+
+/** poll the device
+ *
+ *  @returns true unless end of file reached
+ */
+bool Velodyne64Driver::Poll(const std::shared_ptr<VelodyneScan>& scan) {
+  // Allocate a new shared pointer for zero-copy sharing with other nodelets.
+  int poll_result =
+      config_.use_sensor_sync() ? PollStandardSync(scan) : PollStandard(scan);
+
+  if (poll_result == SOCKET_TIMEOUT || poll_result == RECIEVE_FAIL) {
+    return false;  // poll again
+  }
+
+  if (scan->firing_pkts().empty()) {
+    AINFO << "Get an empty scan from port: " << config_.firing_data_port();
+    return false;
+  }
+
+  // publish message using time of last packet read
+  ADEBUG << "Publishing a full Velodyne scan.";
+  scan->mutable_header()->set_timestamp_sec(cyber::Time().Now().ToSecond());
+  scan->mutable_header()->set_frame_id(config_.frame_id());
+  scan->set_model(config_.model());
+  scan->set_mode(config_.mode());
+  scan->set_basetime(basetime_);
+
+  return true;
+}
+
+bool Velodyne64Driver::CheckAngle(const VelodynePacket& packet) {
+  // check the angle in every packet
+  // for each model of velodyne 64 the data struct is same , so we don't need to
+  // check the lidar model
+  const unsigned char* raw_ptr = (const unsigned char*)packet.data().c_str();
+  for (int i = 0; i < BLOCKS_PER_PACKET; ++i) {
+    uint16_t angle = static_cast<uint16_t>(raw_ptr[i * BLOCK_SIZE + 3] * 256 +
+                                           raw_ptr[i * BLOCK_SIZE + 2]);
+    // for the velodyne64 angle resolution is 0.17~0.2 , so take the angle diff
+    // at 0.3 degree should be a good choice
+    // prefix_angle default = 18000
+    if (angle > config_.prefix_angle() &&
+        std::abs(angle - config_.prefix_angle()) < 30) {
+      return true;
+    }
+  }
+  return false;
+}
+
+int Velodyne64Driver::PollStandardSync(std::shared_ptr<VelodyneScan> scan) {
+  // Since the velodyne delivers data at a very high rate, keep
+  // reading and publishing scans as fast as possible.
+  while (true) {
+    while (true) {
+      // keep reading until full packet received
+      VelodynePacket* packet = scan->add_firing_pkts();
+      int rc = input_->get_firing_data_packet(packet);
+
+      if (rc == 0) {
+        // check the angle for every packet if a packet has an angle
+        if (CheckAngle(*packet) &&
+            (scan->firing_pkts_size() > 0.5 * config_.npackets())) {
+          return 0;
+        } else {
+          break;  // got a full packet?
+        }
+      }
+      if (rc < 0) {
+        return rc;
+      }
+    }
+    // if the only UDP packet lost then recv 1.5*config_.npackets  packets at
+    // most
+    if (scan->firing_pkts_size() > 1.5 * config_.npackets()) {
+      return 0;
+    }
+  }
+  return 0;
+}
+
+}  // namespace velodyne
+}  // namespace drivers
+}  // namespace apollo
