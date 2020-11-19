16d0cdc0957d152670de1389e3633ab2fdf3929d
JackFu123 <fejack2010@hotmail.com>
2020-11-19 21:13:40 +0800

    task_manager: add a new module for task management

---
+++ modules/task_manager/cycle_routing_manager.cc
@@ -0,0 +1,69 @@
+/******************************************************************************
+ * Copyright 2020 The Apollo Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *****************************************************************************/
+#include "modules/task_manager/cycle_routing_manager.h"
+
+#include "modules/task_manager/common/task_manager_gflags.h"
+
+namespace apollo {
+namespace task_manager {
+bool CheckPointDistanceInThreshold(common::PointENU point_a,
+                                   common::PointENU point_b,
+                                   double distance) {
+    double x_dis = point_a.x() - point_b.x();
+    double y_dis = point_a.y() - point_b.y();
+    return x_dis * x_dis + y_dis * y_dis < distance * distance;
+}
+
+common::Status CycleRoutingManager::Init(
+        const CycleRoutingTask& cycle_routing_task) {
+    cycle_ = cycle_routing_task.cycle_num();
+    auto waypoints = cycle_routing_task.routing_request().waypoint();
+    waypoint_num_ = waypoints.size();
+    begin_point_ = waypoints[0].pose();
+    end_point_ = waypoints[waypoint_num_ - 1].pose();
+    last_point_ = begin_point_;
+    return common::Status::OK();
+}
+
+bool CycleRoutingManager::CheckIfReachDestination(
+        const localization::Pose &pose) {
+    if (CheckPointDistanceInThreshold(
+          last_point_,
+          begin_point_,
+          FLAGS_threshold_for_destination_check)) {
+        last_point_ = pose.position();
+        return false;
+    }
+    if (CheckPointDistanceInThreshold(
+          pose.position(),
+          begin_point_,
+          FLAGS_threshold_for_destination_check)) {
+          last_point_ = pose.position();
+        return true;
+    }
+    return false;
+}
+
+int CycleRoutingManager::GetCycle() const {
+    return cycle_;
+}
+
+void CycleRoutingManager::MinusCycle() {
+    --cycle_;
+}
+
+}  // namespace task_manager
+}  // namespace apollo
