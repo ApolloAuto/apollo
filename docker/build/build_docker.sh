034dd7d3cf78208d932b2fc3d09b62bb44440dc4
storypku <storypku@gmail.com>
2020-12-10 08:54:13 +0800

    Docker: new cyber.x86_64.stable image with Bazel 3.7.1

--- docker/build/build_docker.sh
+++ docker/build/build_docker.sh
@@ -139,7 +139,7 @@ function determine_images_for_x86_64() {
         fi
     elif [[ "${stage}" == "dev" ]]; then
         if [[ "${dist}" == "stable" ]]; then
-            IMAGE_IN="${APOLLO_REPO}:cyber-x86_64-${UBUNTU_LTS}-20201110_0322"
+            IMAGE_IN="${APOLLO_REPO}:cyber-x86_64-${UBUNTU_LTS}-20201210_0223"
             IMAGE_OUT="${APOLLO_REPO}:dev-x86_64-${UBUNTU_LTS}-${TIMESTAMP}"
         else
             IMAGE_IN="${APOLLO_REPO}:cyber-x86_64-${UBUNTU_LTS}-testing-20201208_2246"
