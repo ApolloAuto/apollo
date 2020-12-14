a9b8142cc0c8285c6cc9181c2b11e06e1a7890f5
supanfeng <supanfeng@baidu.com>
2020-12-14 20:18:55 +0800

    Perception:update pointpillars network and models

--- modules/perception/lidar/lib/detection/lidar_point_pillars/params.h
+++ modules/perception/lidar/lib/detection/lidar_point_pillars/params.h
@@ -24,69 +24,57 @@ namespace lidar {
 
 class Params {
  public:
-  static constexpr float kPillarXSize = 0.25f;
-  static constexpr float kPillarYSize = 0.25f;
-  static constexpr float kPillarZSize = 8.0f;
-  static constexpr float kMinXRange = -100.0f;
-  static constexpr float kMinYRange = -70.0f;
-  static constexpr float kMinZRange = -5.0f;
-  static constexpr float kMaxXRange = 100.0f;
-  static constexpr float kMaxYRange = 70.0f;
-  static constexpr float kMaxZRange = 3.0f;
-  static constexpr int kNumClass = 9;
-  static constexpr int kMaxNumPillars = 30000;
-  static constexpr int kMaxNumPointsPerPillar = 60;
+  static constexpr float kPillarXSize = 0.32f;
+  static constexpr float kPillarYSize = 0.32f;
+  static constexpr float kPillarZSize = 6.0f;
+  static constexpr float kMinXRange = -74.88f;
+  static constexpr float kMinYRange = -74.88f;
+  static constexpr float kMinZRange = -2.0f;
+  static constexpr float kMaxXRange = 74.88f;
+  static constexpr float kMaxYRange = 74.88f;
+  static constexpr float kMaxZRange = 4.0f;
+  static constexpr int kNumClass = 3;
+  static constexpr int kMaxNumPillars = 32000;
+  static constexpr int kMaxNumPointsPerPillar = 20;
   static constexpr int kNumPointFeature = 5;  // x, y, z, i, delta of time
-  static constexpr int kNumGatherPointFeature = 9;
-  static constexpr int kNumAnchor = 200 * 140 * 8 + 220 * 140 * 8;
+  static constexpr int kNumAnchor = 468 * 468 * 6;
   static constexpr int kNumOutputBoxFeature = 7;
   static constexpr int kBatchSize = 1;
   static constexpr int kNumIndsForScan = 1024;
   static constexpr int kNumThreads = 64;
   static constexpr int kNumBoxCorners = 4;
 
-  static std::vector<int> AnchorStrides() { return std::vector<int>{4, 2}; }
+  static std::vector<int> AnchorStrides() { return std::vector<int>{1}; }
 
-  static std::vector<int> NumAnchorSets() { return std::vector<int>{8, 8}; }
+  static std::vector<int> NumAnchorSets() { return std::vector<int>{6}; }
 
   static std::vector<std::vector<float>> AnchorDxSizes() {
     return std::vector<std::vector<float>>{
-        std::vector<float>{2.94046906f, 1.95017717f, 2.73050468f, 2.4560939f},
-        std::vector<float>{2.49008838f, 0.60058911f, 0.76279481f, 0.66344886f,
-                           0.39694519f}};
+        std::vector<float>{2.08, 0.84, 0.84}};
   }
 
   static std::vector<std::vector<float>> AnchorDySizes() {
     return std::vector<std::vector<float>>{
-        std::vector<float>{11.1885991, 4.60718145f, 6.38352896f, 6.73778078f},
-        std::vector<float>{0.48578221f, 1.68452161f, 2.09973778f, 0.7256437f,
-                           0.40359262f}};
+        std::vector<float>{4.73, 1.81, 0.91}};
   }
 
   static std::vector<std::vector<float>> AnchorDzSizes() {
     return std::vector<std::vector<float>>{
-        std::vector<float>{3.47030982f, 1.72270761f, 3.13312415f, 2.73004906f},
-        std::vector<float>{0.98297065f, 1.27192197f, 1.44403034f, 1.75748069f,
-                           1.06232151f}};
+        std::vector<float>{1.77, 1.77, 1.74}};
   }
 
   static std::vector<std::vector<float>> AnchorZCoors() {
     return std::vector<std::vector<float>>{
-        std::vector<float>{-0.0715754f, -0.93897414f, -0.08168083f,
-                           -0.37937912f},
-        std::vector<float>{-1.27247968f, -1.03743013f, -0.99194854f,
-                           -0.73911038f, -1.27868911f}};
+        std::vector<float>{-0.0345, -0.1188, 0}};
   }
 
   static std::vector<std::vector<int>> NumAnchorRo() {
-    return std::vector<std::vector<int>>{std::vector<int>{2, 2, 2, 2},
-                                         std::vector<int>{2, 2, 2, 1, 1}};
+    return std::vector<std::vector<int>>{std::vector<int>{2, 2, 2}};
   }
 
   static std::vector<std::vector<float>> AnchorRo() {
     return std::vector<std::vector<float>>{
-        std::vector<float>{0, M_PI / 2, 0, M_PI / 2, 0, M_PI / 2, 0, M_PI / 2},
-        std::vector<float>{0, M_PI / 2, 0, M_PI / 2, 0, M_PI / 2, 0, 0}};
+        std::vector<float>{0, M_PI / 2, 0, M_PI / 2, 0, M_PI / 2}};
   }
 
  private:
