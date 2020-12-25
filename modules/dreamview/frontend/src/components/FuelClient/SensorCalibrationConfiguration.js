f4bf768cf65fe93e6e644cf79c7d51bdfe1552f1
changsh726 <changsonghong@baidu.com>
2020-12-25 10:00:59 +0800

    FuelClient: added translation name

--- modules/dreamview/frontend/src/components/FuelClient/SensorCalibrationConfiguration.js
+++ modules/dreamview/frontend/src/components/FuelClient/SensorCalibrationConfiguration.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { inject,observer } from 'mobx-react';
+import { inject, observer } from 'mobx-react';
+import _ from 'lodash';
 
 // the component show the lidar configure and camera configure
 @inject('store') @observer
@@ -16,9 +17,12 @@ class TranslationInput extends React.Component {
   handleTranslationChange(event) {
     const val = parseFloat(event.target.value);
     this.setState({ translationValue: val });
-    //this.props.store.hmi.toggleTranslationChange();
-    this.props.store.hmi.changeTranslation(this.props.belong,
-      this.props.index, val, this.props.isLidar);
+    this.props.store.hmi.changeTranslation(
+      this.props.belong,
+      this.props.index,
+      val,
+      this.props.isLidar,
+    );
   }
 
   render() {
@@ -28,8 +32,7 @@ class TranslationInput extends React.Component {
         type="number"
         value={this.state.translationValue}
         onChange={this.handleTranslationChange}
-      >
-      </input>
+      ></input>
     );
   }
 }
@@ -40,14 +43,42 @@ export default class SensorCalibrationConfiguration extends React.Component {
   renderSensorConfiguration(sensorName, translation) {
     return (
       <tr className="lidar-configuration-tr" key={sensorName}>
-        <td className={(sensorName === this.props.mainSensor) ? 'main-sensor' : null}>{sensorName}</td>
+        <td
+          className={
+            sensorName === this.props.mainSensor ? 'main-sensor' : null
+          }
+        >
+          {sensorName}
+        </td>
         <td>
           <div className="lidar-configuration-translation">
             <div className="lidar-configuration-xyz">
-              x:<TranslationInput belong={sensorName} index='x' value={translation.x} isLidar={true}></TranslationInput>
+              x:
+              <TranslationInput
+                belong={sensorName}
+                index="x"
+                value={translation.x}
+                isLidar={true}
+              ></TranslationInput>
             </div>
-            <div className="lidar-configuration-xyz">y:<TranslationInput value={translation.y} belong={sensorName} index='y' isLidar={true}></TranslationInput></div>
-            <div className="lidar-configuration-xyz">z:<TranslationInput value={translation.z} index='z' belong={sensorName} isLidar={true}></TranslationInput></div>
+            <div className="lidar-configuration-xyz">
+              y:
+              <TranslationInput
+                value={translation.y}
+                belong={sensorName}
+                index="y"
+                isLidar={true}
+              ></TranslationInput>
+            </div>
+            <div className="lidar-configuration-xyz">
+              z:
+              <TranslationInput
+                value={translation.z}
+                index="z"
+                belong={sensorName}
+                isLidar={true}
+              ></TranslationInput>
+            </div>
           </div>
         </td>
       </tr>
@@ -58,45 +89,76 @@ export default class SensorCalibrationConfiguration extends React.Component {
     const { lidars, camera } = this.props;
     const lidarConfigurations = [];
     lidars.forEach((trans, sensorName) => {
-      lidarConfigurations.push(this.renderSensorConfiguration(sensorName, trans));
+      lidarConfigurations.push(
+        this.renderSensorConfiguration(sensorName, trans),
+      );
     });
     const cameraConfiguration = [];
     if (_.get(camera, 'translation')) {
       const translation = _.get(camera, 'translation');
       cameraConfiguration.push(
-        <tr className="lidar-configuration-tr" key='camera'>
-          <td>{_.get(this.props.store.hmi.componentStatus.get('Camera'),'message')}</td>
-        <td>
-          <div className="lidar-configuration-translation">
-            <div className="lidar-configuration-xyz">
-                x:<TranslationInput belong={'camera'} index='x' value={_.get(translation, 'x')} isLidar={false}></TranslationInput>
+        <tr className="lidar-configuration-tr" key="camera">
+          <td>
+            {_.get(
+              this.props.store.hmi.componentStatus.get('Camera'),
+              'message',
+            )}
+          </td>
+          <td>
+            <div className="lidar-configuration-translation">
+              <div className="lidar-configuration-xyz">
+                x:
+                <TranslationInput
+                  belong={'camera'}
+                  index="x"
+                  value={_.get(translation, 'x')}
+                  isLidar={false}
+                ></TranslationInput>
+              </div>
+              <div className="lidar-configuration-xyz">
+                y:
+                <TranslationInput
+                  value={_.get(translation, 'y')}
+                  belong={'camera'}
+                  index="y"
+                  isLidar={false}
+                ></TranslationInput>
+              </div>
+              <div className="lidar-configuration-xyz">
+                z:
+                <TranslationInput
+                  value={_.get(translation, 'z')}
+                  index="z"
+                  belong={'camera'}
+                  isLidar={false}
+                ></TranslationInput>
+              </div>
             </div>
-            <div className="lidar-configuration-xyz">y:<TranslationInput value={_.get(translation, 'y')} belong={'camera'} index='y' isLidar={false}></TranslationInput></div>
-            <div className="lidar-configuration-xyz">z:<TranslationInput value={_.get(translation, 'z')} index='z' belong={'camera'} isLidar={false}></TranslationInput></div>
-          </div>
-        </td>
-      </tr>
+          </td>
+        </tr>,
       );
     }
     return (
       <React.Fragment>
         <div className="lidar-configuration-table">
-          {!_.isEmpty(cameraConfiguration) &&
-          (<table>
-              <tbody>
-                 {cameraConfiguration}
-              </tbody>
-            </table>)}
-          {!_.isEmpty(lidarConfigurations) && (<div>
-            <div>Lidar Translation</div>
-            <table>
-              <tbody>
-                {lidarConfigurations}
-              </tbody>
-            </table>
-          </div>)}
-      </div>
-  </React.Fragment>
+          {!_.isEmpty(cameraConfiguration) && (
+            <div>
+              <div>Lidar-Camera Translation</div>
+              <table>
+                <tbody>{cameraConfiguration}</tbody>
+              </table>
+            </div>
+          )}
+          {!_.isEmpty(lidarConfigurations) && (
+            <div>
+              <div>IMU-Lidar Translation</div>
+              <table>
+                <tbody>{lidarConfigurations}</tbody>
+              </table>
+            </div>
+          )}
+        </div>
+      </React.Fragment>
     );
   }
 }
--- modules/dreamview/frontend/src/components/FuelClient/index.js
+++ modules/dreamview/frontend/src/components/FuelClient/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
-import {
-  Tab, Tabs, TabList, TabPanel,
-} from 'react-tabs';
-import { inject,observer } from 'mobx-react';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import { inject, observer } from 'mobx-react';
+import _ from 'lodash';
 
 import WS from 'store/websocket';
 import GuideText from 'components/common/GuideText';
@@ -28,8 +27,13 @@ export default class FuelClient extends React.Component {
       //这个地方原来要传选中camera的config（name和translation）
       //
       if (hmi.inCameraLidarSensorCalibrationMode) {
-        const internal_conf_input = _.map(document.getElementsByClassName('camera-internal-configuration-d'),'value');
-        const camera_internal_conf = internal_conf_input.map(x => parseFloat(x));
+        const internal_conf_input = _.map(
+          document.getElementsByClassName('camera-internal-configuration-d'),
+          'value',
+        );
+        const camera_internal_conf = internal_conf_input.map((x) =>
+          parseFloat(x),
+        );
         if (_.findIndex(camera_internal_conf, (x) => isNaN(x)) !== -1) {
           alert('Please input all camera internal configurations');
           hmi.canStartPreprocess = true;
@@ -58,7 +62,13 @@ export default class FuelClient extends React.Component {
   renderCameraInternalInput(number) {
     const result = [];
     for (let i = 0; i < number; i++) {
-      result.push(<input type="number" className="camera-internal-configuration-d" key={`camera${i}`}></input>);
+      result.push(
+        <input
+          type="number"
+          className="camera-internal-configuration-d"
+          key={`camera${i}`}
+        ></input>,
+      );
     }
     return result;
   }
@@ -74,59 +84,61 @@ export default class FuelClient extends React.Component {
             <Tab>Instruction</Tab>
             <Tab>Configuration</Tab>
           </TabList>
-        <TabPanel>
-        <GuideText mode={mode}></GuideText>
-        </TabPanel>
           <TabPanel>
-        <SensorCalibrationConfiguration
-          lidars={hmi.lidars}
-          mainSensor={hmi.mainSensor}
-          camera={hmi.camera}
-        >
-            </SensorCalibrationConfiguration>
-            {inCameraLidarSensorCalibrationMode &&
-               (<div>
-              Camera-Lidar内参设定
-              <div>D:
-              {this.renderCameraInternalInput(5)}
-                 </div>
-              <div> K:
-              {this.renderCameraInternalInput(9)}
-               </div>
-              <div>R:
-              {this.renderCameraInternalInput(9)}
-                 </div>
-             </div>)
-        }
-        <div className="preprocess-bar category">
-          <div className="category-description">
-            <button
-              className="preprocess-btn"
-              onClick={this.handlePreprocess}
-            >
-              Preprocess
-        </button>
-          </div>
-          <div className="category-progress-background">
-            <span
-              className={true
-                ? 'category-completed' : 'category-in-progress'}
-              style={{ width: `${hmi.preprocessProgress}%` }}
-            />
-          </div>
-        </div>
-        <div className="preprocess-msg">
-          <ul className="preprocess-console">
-            <MonitorItem
-              text={hmi.logString}
-              level={hmi.preprocessStatus}
-              time={timestampMsToTimeString(Date.now() / 1000)}
-            >
-            </MonitorItem>
-          </ul>
-          </div>
+            <GuideText mode={mode}></GuideText>
           </TabPanel>
-          </Tabs>
+          <TabPanel>
+            <SensorCalibrationConfiguration
+              lidars={hmi.lidars}
+              mainSensor={hmi.mainSensor}
+              camera={hmi.camera}
+            ></SensorCalibrationConfiguration>
+            {inCameraLidarSensorCalibrationMode && (
+              <div>
+                Camera Intrinsics
+                <div>
+                  D:
+                  {this.renderCameraInternalInput(5)}
+                </div>
+                <div>
+                  K:
+                  {this.renderCameraInternalInput(9)}
+                </div>
+                <div>
+                  R:
+                  {this.renderCameraInternalInput(9)}
+                </div>
+              </div>
+            )}
+            <div className="preprocess-bar category">
+              <div className="category-description">
+                <button
+                  className="preprocess-btn"
+                  onClick={this.handlePreprocess}
+                >
+                  Preprocess
+                </button>
+              </div>
+              <div className="category-progress-background">
+                <span
+                  className={
+                    true ? 'category-completed' : 'category-in-progress'
+                  }
+                  style={{ width: `${hmi.preprocessProgress}%` }}
+                />
+              </div>
+            </div>
+            <div className="preprocess-msg">
+              <ul className="preprocess-console">
+                <MonitorItem
+                  text={hmi.logString}
+                  level={hmi.preprocessStatus}
+                  time={timestampMsToTimeString(Date.now() / 1000)}
+                ></MonitorItem>
+              </ul>
+            </div>
+          </TabPanel>
+        </Tabs>
       </div>
     );
   }
