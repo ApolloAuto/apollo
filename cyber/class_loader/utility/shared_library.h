9ad00f6d734624edb1a3b37f6aedaccd3710d180
daohu527 <daohu527@gmail.com>
2020-12-28 20:27:20 +0800

    cyber: remove poco shared library dependency

---
+++ cyber/class_loader/utility/shared_library.h
@@ -0,0 +1,74 @@
+/******************************************************************************
+ * Copyright 2018 The Apollo Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *****************************************************************************/
+
+#ifndef CYBER_CLASS_LOADER_UTILITY_SHARED_LIBRARY_H_
+#define CYBER_CLASS_LOADER_UTILITY_SHARED_LIBRARY_H_
+
+#include <string>
+#include <mutex>
+
+#include "cyber/class_loader/utility/exception.h"
+
+namespace apollo {
+namespace cyber {
+namespace class_loader {
+namespace utility {
+
+class SharedLibrary {
+ public:
+  enum Flags {
+    SHLIB_GLOBAL = 1,
+    SHLIB_LOCAL = 2,
+  };
+
+  SharedLibrary();
+
+  explicit SharedLibrary(const std::string& filename);
+
+  SharedLibrary(const std::string& filename, int flags);
+
+  virtual ~SharedLibrary();
+
+  void Load(const std::string& filename);
+
+  void Load(const std::string& filename, int flags);
+
+  void Unload();
+
+  bool IsLoaded();
+
+  bool HasSymbol(const std::string& name);
+
+  void* GetSymbol(const std::string& name);
+
+  inline const std::string& GetPath() const { return filename_; }
+
+  SharedLibrary(const SharedLibrary&) = delete;
+  SharedLibrary& operator=(const SharedLibrary&) = delete;
+
+ private:
+  void* handle_;
+  std::string filename_;
+
+  std::mutex mutex_;
+};
+
+}  // namespace utility
+}  // namespace class_loader
+}  // namespace cyber
+}  // namespace apollo
+
+#endif  // CYBER_CLASS_LOADER_UTILITY_SHARED_LIBRARY_H_
