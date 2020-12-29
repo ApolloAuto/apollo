e985d05452db8d39054f0adc2ecee09322b22a6e
storypku <storypku@gmail.com>
2020-12-29 16:35:22 +0800

    Cyber: notes from poco and bugfix for SharedLibrary d'ctor

--- cyber/class_loader/shared_library/exceptions.h
+++ cyber/class_loader/shared_library/exceptions.h
@@ -17,21 +17,22 @@
 #ifndef CYBER_CLASS_LOADER_SHARED_LIBRARY_EXCEPTIONS_H_
 #define CYBER_CLASS_LOADER_SHARED_LIBRARY_EXCEPTIONS_H_
 
-#include <string>
 #include <stdexcept>
+#include <string>
 
 namespace apollo {
 namespace cyber {
 namespace class_loader {
 
-#define DECLARE_SHARED_LIBRARY_EXCEPTION(CLS, BASE) \
-class CLS : public BASE {                           \
- public:                                            \
-  explicit CLS(const std::string& err_msg) : BASE(err_msg) {} \
-  ~CLS() throw() {}                                       \
-};
+#define DECLARE_SHARED_LIBRARY_EXCEPTION(CLS, BASE)             \
+  class CLS : public BASE {                                     \
+   public:                                                      \
+    explicit CLS(const std::string& err_msg) : BASE(err_msg) {} \
+    ~CLS() throw() {}                                           \
+  };
 
-DECLARE_SHARED_LIBRARY_EXCEPTION(LibraryAlreadyLoadedException, std::runtime_error);
+DECLARE_SHARED_LIBRARY_EXCEPTION(LibraryAlreadyLoadedException,
+                                 std::runtime_error);
 DECLARE_SHARED_LIBRARY_EXCEPTION(LibraryLoadException, std::runtime_error);
 DECLARE_SHARED_LIBRARY_EXCEPTION(SymbolNotFoundException, std::runtime_error);
 
