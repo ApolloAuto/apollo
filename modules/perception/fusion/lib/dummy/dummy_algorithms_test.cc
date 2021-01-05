919f4786570d73c5c811c4bd030e87ac256a39c2
Jiahao Chen (Jerold) <32252697+jeroldchen@users.noreply.github.com>
2021-01-05 11:18:02 +0800

    Perception: resume camera obstacle detection in sensor fusion (#13277)
    * Perception: add camera obstacle detection to fusion
    
    * Perception: modify default dag to use CNNSeg model

--- modules/perception/fusion/lib/dummy/dummy_algorithms_test.cc
+++ modules/perception/fusion/lib/dummy/dummy_algorithms_test.cc
@@ -29,7 +29,7 @@ namespace fusion {
 
 TEST(DummyFusionSystemTest, test) {
   FusionInitOptions init_options;
-  init_options.main_sensor = "velodyne64";
+  init_options.main_sensors = std::vector<std::string>{"velodyne64"};
   DummyFusionSystem system;
   EXPECT_TRUE(system.Init(init_options));
 
