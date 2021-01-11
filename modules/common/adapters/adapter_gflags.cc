2a88e9c808f8db353eeb4ed13e5c401f32be1f71
lfcarol <liufeng31@baidu.com>
2021-01-11 23:24:56 +0800

    Dkit: Update perception configurations cherry pick from r5.5.0

--- modules/common/adapters/adapter_gflags.cc
+++ modules/common/adapters/adapter_gflags.cc
@@ -258,3 +258,7 @@ DEFINE_string(latency_recording_topic, "/apollo/common/latency_records",
 DEFINE_string(latency_reporting_topic, "/apollo/common/latency_reports",
               "Latency reporting topic.");
 DEFINE_string(task_topic, "/apollo/task_manager", "task manager topic name");
+// value: velodyne128, velodyne64, velodyne16
+DEFINE_string(lidar_model_version, "",
+              "It determins which lidar model(16 ,64 or 128) to load, "
+              "if not to set, the model will be loaded by the sensor name.");
