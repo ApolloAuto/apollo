7ead02cb9d6bfd1dab013c481a5c1b68f561d2c3
lilu15 <m15575882843@163.com>
2021-03-04 13:46:00 +0800

    task_manager: add the plannig part of auto park and add the function of plot calibration

--- modules/task_manager/task_manager_component.cc
+++ modules/task_manager/task_manager_component.cc
@@ -28,6 +28,7 @@ using apollo::localization::LocalizationEstimate;
 using apollo::routing::RoutingRequest;
 
 bool TaskManagerComponent::Init() {
+  AERROR << "enter the TaskManagerComponent init";
   TaskManagerConfig task_manager_conf;
   ACHECK(cyber::ComponentBase::GetProtoConfig(&task_manager_conf))
       << "Unable to load task_manager conf file: "
@@ -47,6 +48,7 @@ bool TaskManagerComponent::Init() {
   cyber::proto::RoleAttributes attr;
   attr.set_channel_name(
       task_manager_conf.topic_config().routing_request_topic());
+  AERROR << "the channel name is: " << attr.channel_name();
   auto qos = attr.mutable_qos_profile();
   qos->set_history(apollo::cyber::proto::QosHistoryPolicy::HISTORY_KEEP_LAST);
   qos->set_reliability(
@@ -54,30 +56,51 @@ bool TaskManagerComponent::Init() {
   qos->set_durability(
       apollo::cyber::proto::QosDurabilityPolicy::DURABILITY_TRANSIENT_LOCAL);
   request_writer_ = node_->CreateWriter<RoutingRequest>(attr);
+  AERROR << "end the init";
   return true;
 }
 
 bool TaskManagerComponent::Proc(const std::shared_ptr<Task>& task) {
+  AERROR << "enter the TaskManagerComponent proc";
   task_name_ = task->task_name();
-  if (task->task_type() != CYCLE_ROUTING) {
+  AERROR << "the task type is: " << task->task_type();
+  if (task->task_type() != CYCLE_ROUTING &&
+      task->task_type() != PARKING_ROUTING) {
     AINFO << "Task type is not cycle_routing.";
     return false;
   }
+  if (task->task_type() == CYCLE_ROUTING) {
+    cycle_routing_manager_ = std::make_shared<CycleRoutingManager>();
+    cycle_routing_manager_->Init(task->cycle_routing_task());
+    routing_request_ = task->cycle_routing_task().routing_request();
+    Rate rate(1.0);
 
-  cycle_routing_manager_ = std::make_shared<CycleRoutingManager>();
-  cycle_routing_manager_->Init(task->cycle_routing_task());
-  routing_request_ = task->cycle_routing_task().routing_request();
-  Rate rate(1.0);
-
-  while (cycle_routing_manager_->GetCycle() > 0) {
-    if (cycle_routing_manager_->GetNewRouting(localization_.pose(),
-                                              &routing_request_)) {
+    while (cycle_routing_manager_->GetCycle() > 0) {
+      if (cycle_routing_manager_->GetNewRouting(localization_.pose(),
+                                                &routing_request_)) {
+        common::util::FillHeader(node_->Name(), &routing_request_);
+        request_writer_->Write(routing_request_);
+        AINFO << "[TaskManagerComponent]Reach begin/end point: "
+              << "routing manager send a routing request. ";
+      }
+      rate.Sleep();
+    }
+  } else if (task->task_type() == PARKING_ROUTING) {
+    AERROR << "enter the parking routing task";
+    parking_routing_manager_ = std::make_shared<ParkingRoutingManager>();
+    parking_routing_manager_->Init(task->parking_routing_task());
+    routing_request_ = task->parking_routing_task().routing_request();
+    if (parking_routing_manager_->
+        SizeVerification(task->parking_routing_task()) &&
+        parking_routing_manager_->
+        RoadWidthVerification(task->parking_routing_task())) {
+      AERROR << "compelet the Verification";
       common::util::FillHeader(node_->Name(), &routing_request_);
       request_writer_->Write(routing_request_);
-      AINFO << "[TaskManagerComponent]Reach begin/end point: "
-            << "routing manager send a routing request. ";
+      AINFO << "send a auto parking task";
+    } else {
+      AERROR << "plot verification failed, please select suitable plot!";
     }
-    rate.Sleep();
   }
   return true;
 }
