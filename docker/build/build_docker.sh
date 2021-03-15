654234bbc2ee802c51a3238645db34c4f53da8ef
storypku <storypku@gmail.com>
2021-03-15 15:43:29 +0800

    Docker: refactored build_docker.sh to integrate w/ sample runtime Dockerfile
    To reduce our burden of maintainence, Runtime Docker won't be provided.
    Tweak our sample runtime Dockerfile and build it yourself if you'd like.

--- docker/build/build_docker.sh
+++ docker/build/build_docker.sh
@@ -1,19 +1,19 @@
 #! /usr/bin/env bash
+TAB="    " # 4 Spaces
 
 APOLLO_REPO="apolloauto/apollo"
+UBUNTU_LTS="18.04"
 
 SUPPORTED_ARCHS=(
     x86_64
     aarch64
 )
 
-HOST_ARCH="$(uname -m)"
-TARGET_ARCH=
-
 SUPPORTED_STAGES=(
     base
     cyber
     dev
+    runtime
 )
 
 SUPPORTED_DIST=(
@@ -21,26 +21,19 @@ SUPPORTED_DIST=(
     testing
 )
 
-DOCKERFILE=
+HOST_ARCH="$(uname -m)"
+INSTALL_MODE="download"
+TARGET_GEOLOC="us"
+
+TARGET_DIST="stable"
+TARGET_ARCH=
 TARGET_STAGE=
-IMAGE_IN=
-IMAGE_OUT=
-CYBER_IMAGE_TIMESTAMP=
 
-GEOLOC="us"
-MODE="download"
-DIST="stable"
-BUILD_CLEAN=0
-DRY_RUN=0
+DOCKERFILE=
+PREV_IMAGE_TIMESTAMP=
 
-CUDA_LITE=""
-CUDNN_VERSION=""
-TENSORRT_VERSION=""
-UBUNTU_LTS="18.04"
-
-TIMESTAMP="$(date +%Y%m%d_%H%M)"
-
-TAB="    "
+USE_CACHE=1
+DRY_RUN_ONLY=0
 
 function check_experimental_docker() {
     local jq_cmd="$(command -v jq)"
@@ -101,7 +94,7 @@ function determine_target_arch_and_stage() {
     cpu_arch_support_check "${arch}"
     TARGET_ARCH="${arch}"
     if [[ "${TARGET_ARCH}" != "${HOST_ARCH}" ]]; then
-        echo "[WARNING] HOST_ARCH(${HOST_ARCH}) != TARGET_ARCH(${TARGET_ARCH})"
+        echo "[WARNING] HOST_ARCH(${HOST_ARCH}) != TARGET_ARCH(${TARGET_ARCH}) detected."
         echo "[WARNING] Make sure you have executed the following command:"
         echo "[WARNING] ${TAB}docker run --rm --privileged multiarch/qemu-user-static --reset -p yes"
     fi
@@ -111,23 +104,73 @@ function determine_target_arch_and_stage() {
     TARGET_STAGE="${stage}"
 }
 
-function determine_images_for_x86_64() {
-    local stage="$1" # base/cyber/dev
-    local dist="$2"  # stable/testing
-    local cyber_image_timestamp="$3" # cyber image timestamp
-
-    if [[ "${stage}" == "dev" && -z "${cyber_image_timestamp}" ]]
-    then
+CUDA_LITE=
+CUDNN_VERSION=
+TENSORRT_VERSION=
+function determine_cuda_versions() {
+    local arch="$1"
+    local dist="$2"
+    if [[ "${arch}" == "x86_64" ]]; then
         if [[ "${dist}" == "stable" ]]; then
-            cyber_image_timestamp="20210202_1105"
-        else
-            cyber_image_timestamp="20210108_1510"
+            CUDA_LITE=11.1
+            CUDNN_VERSION="8.0.4.30"
+            TENSORRT_VERSION="7.2.1"
+        else # testing
+            CUDA_LITE=11.1
+            CUDNN_VERSION="8.0.4.30"
+            TENSORRT_VERSION="7.2.1"
         fi
+    else # aarch64
+        CUDA_LITE="10.2"
+        CUDNN_VERSION="8.0.0.180"
+        TENSORRT_VERSION="7.1.3"
+    fi
+}
+
+function determine_prev_image_timestamp() {
+    if [[ ! -z "${PREV_IMAGE_TIMESTAMP}" ]]; then
+        return
     fi
 
-    CUDA_LITE=11.1
-    CUDNN_VERSION="8.0.4.30"
-    TENSORRT_VERSION="7.2.1"
+    local arch="$1" # x86_64 / aarch64
+    local stage="$2" # base/cyber/dev/runtime
+    local dist="$3" # stable/testing
+
+    local result=
+    if [[ "${arch}" == "x86_64" ]]; then
+        if [[ "${stage}" == "dev" ]]; then
+            if [[ "${dist}" == "stable" ]]; then
+                result="20210202_1105"
+            else
+                result="20210108_1510"
+            fi
+        elif [[ "${stage}" == "runtime" ]]; then
+            if [[ "${dist}" == "stable" ]]; then
+                result="20210312_1536"
+            fi
+        fi
+    else # aarch64
+        if [[ "${stage}" == "cyber" ]]; then
+            if [[ "${dist}" == "stable" ]]; then
+                result="20201217_0752"
+            fi
+        elif [[ "${stage}" == "dev" ]]; then
+            if [[ "${dist}" == "stable" ]]; then
+                result="20201217_1302"
+            fi
+        fi
+    fi
+    PREV_IMAGE_TIMESTAMP="${result}"
+}
+
+IMAGE_IN=
+IMAGE_OUT=
+DEV_IMAGE_IN=
+
+function determine_images_in_out_x86_64() {
+    local stage="$1" # Build stage, base/cyber/dev
+    local dist="$2"  # stable or testing
+    local timestamp="$3" # Timestamp
 
     local cudnn_ver="${CUDNN_VERSION%%.*}"
     local trt_ver="${TENSORRT_VERSION%%.*}"
@@ -139,17 +182,26 @@ function determine_images_for_x86_64() {
     elif [[ "${stage}" == "cyber" ]]; then
         IMAGE_IN="${base_image}"
         if [[ "${dist}" == "stable" ]]; then
-            IMAGE_OUT="${APOLLO_REPO}:cyber-x86_64-${UBUNTU_LTS}-${TIMESTAMP}"
+            IMAGE_OUT="${APOLLO_REPO}:cyber-x86_64-${UBUNTU_LTS}-${timestamp}"
         else
-            IMAGE_OUT="${APOLLO_REPO}:cyber-x86_64-${UBUNTU_LTS}-testing-${TIMESTAMP}"
+            IMAGE_OUT="${APOLLO_REPO}:cyber-x86_64-${UBUNTU_LTS}-testing-${timestamp}"
         fi
     elif [[ "${stage}" == "dev" ]]; then
         if [[ "${dist}" == "stable" ]]; then
-            IMAGE_IN="${APOLLO_REPO}:cyber-x86_64-${UBUNTU_LTS}-${cyber_image_timestamp}"
-            IMAGE_OUT="${APOLLO_REPO}:dev-x86_64-${UBUNTU_LTS}-${TIMESTAMP}"
+            IMAGE_IN="${APOLLO_REPO}:cyber-x86_64-${UBUNTU_LTS}-${PREV_IMAGE_TIMESTAMP}"
+            IMAGE_OUT="${APOLLO_REPO}:dev-x86_64-${UBUNTU_LTS}-${timestamp}"
         else
-            IMAGE_IN="${APOLLO_REPO}:cyber-x86_64-${UBUNTU_LTS}-testing-${cyber_image_timestamp}"
-            IMAGE_OUT="${APOLLO_REPO}:dev-x86_64-${UBUNTU_LTS}-testing-${TIMESTAMP}"
+            IMAGE_IN="${APOLLO_REPO}:cyber-x86_64-${UBUNTU_LTS}-testing-${PREV_IMAGE_TIMESTAMP}"
+            IMAGE_OUT="${APOLLO_REPO}:dev-x86_64-${UBUNTU_LTS}-testing-${timestamp}"
+        fi
+    elif [[ "${stage}" == "runtime" ]]; then
+        if [[ "${dist}" == "stable" ]]; then
+            IMAGE_IN="nvidia/cuda:${CUDA_LITE}-runtime-ubuntu${UBUNTU_LTS}"
+            DEV_IMAGE_IN="${APOLLO_REPO}:dev-x86_64-${UBUNTU_LTS}-${PREV_IMAGE_TIMESTAMP}"
+            IMAGE_OUT="${APOLLO_REPO}:runtime-x86_64-${UBUNTU_LTS}-${timestamp}"
+        else
+            echo "Runtime Docker for Apollo Testing not ready. Exiting..."
+            exit 1
         fi
     else
         echo "Unknown build stage: ${stage}. Exiting..."
@@ -157,19 +209,10 @@ function determine_images_for_x86_64() {
     fi
 }
 
-function determine_images_for_aarch64() {
-    local stage="$1" # base/cyber/dev
-    local dist="$2"  # stable/testing
-    local cyber_image_timestamp="$3" # cyber image timestamp
+function determine_images_in_out_aarch64() {
+    local stage="$1"
+    local timestamp="$2"
 
-    if [[ "${stage}" == "dev" && -z "${cyber_image_timestamp}" ]]
-    then
-        cyber_image_timestamp="20201217_1302"
-    fi
-
-    CUDA_LITE="10.2"
-    CUDNN_VERSION="8.0.0.180"
-    TENSORRT_VERSION="7.1.3"
     local cudnn_ver="${CUDNN_VERSION%%.*}"
     local trt_ver="${TENSORRT_VERSION%%.*}"
 
@@ -179,16 +222,36 @@ function determine_images_for_aarch64() {
     if [[ "${stage}" == "base" ]]; then
         # Ref: https://developer.nvidia.com/embedded/linux-tegra
         IMAGE_IN="nvcr.io/nvidia/l4t-base:r32.4.4"
-        IMAGE_OUT="${APOLLO_REPO}:${BASE_FMT}-${TIMESTAMP}"
+        IMAGE_OUT="${APOLLO_REPO}:${BASE_FMT}-${timestamp}"
     elif [[ "${stage}" == "cyber" ]]; then
-        IMAGE_IN="${APOLLO_REPO}:${BASE_FMT}-20201217_0752"
-        IMAGE_OUT="${APOLLO_REPO}:${CYBER_FMT}-${TIMESTAMP}"
+        IMAGE_IN="${APOLLO_REPO}:${BASE_FMT}-${PREV_IMAGE_TIMESTAMP}"
+        IMAGE_OUT="${APOLLO_REPO}:${CYBER_FMT}-${timestamp}"
     elif [[ "${stage}" == "dev" ]]; then
-        IMAGE_IN="${APOLLO_REPO}:${CYBER_FMT}-${cyber_image_timestamp}"
-        IMAGE_OUT="${APOLLO_REPO}:dev-aarch64-${UBUNTU_LTS}-${TIMESTAMP}"
+        IMAGE_IN="${APOLLO_REPO}:${CYBER_FMT}-${PREV_IMAGE_TIMESTAMP}"
+        IMAGE_OUT="${APOLLO_REPO}:dev-aarch64-${UBUNTU_LTS}-${timestamp}"
+    elif [[ "${stage}" == "runtime" ]]; then
+        echo "Runtime Docker for AArch64 not ready yet. Exiting..."
+        exit 1
     else
         echo "Unknown build stage: ${stage}. Exiting..."
         exit 1
+    fi
+}
+
+function determine_images_in_out() {
+    local arch="$1"
+    local stage="$2"
+    local dist="$3"
+    local timestamp="$(date +%Y%m%d_%H%M)"
+
+    determine_cuda_versions "${arch}" "${dist}"
+    determine_prev_image_timestamp "${arch}" "${stage}" "${dist}"
+
+    if [[ "${arch}" == "x86_64" ]]; then
+        determine_images_in_out_x86_64 "${stage}" "${dist}" "${timestamp}"
+    else
+        # Only stable images for Aarch64
+        determine_images_in_out_aarch64 "${stage}" "${timestamp}"
     fi
 }
 
@@ -201,7 +264,7 @@ function print_usage() {
     echo "${TAB}-m,--mode       \"build\" for build everything from source if possible, \"download\" for using prebuilt ones"
     echo "${TAB}-g,--geo        Enable geo-specific mirrors to speed up build. Currently \"cn\" and \"us\" are supported."
     echo "${TAB}-d,--dist       Whether to build stable(\"stable\") or experimental(\"testing\") Docker images"
-    echo "${TAB}-t,--timestamp  Specify Cyber image timestamp to build Dev image from it. Format: yyyymmdd_hhmm (e.g 20210205_1520)"
+    echo "${TAB}-t,--timestamp  Specify image timestamp for previous stage to build image upon. Format: yyyymmdd_hhmm (e.g 20210205_1520)"
     echo "${TAB}--dry           Dry run (for testing purpose)"
     echo "${TAB}-h,--help       Show this message and exit"
     echo "E.g.,"
@@ -234,29 +297,29 @@ function parse_arguments() {
                 ;;
             -m|--mode)
                 check_opt_arg "${opt}" "$1"
-                MODE="$1"
+                INSTALL_MODE="$1"
                 shift
                 ;;
             -g|--geo)
                 check_opt_arg "${opt}" "$1"
-                GEOLOC="$1"
+                TARGET_GEOLOC="$1"
                 shift
                 ;;
             -c|--clean)
-                BUILD_CLEAN=1
+                USE_CACHE=0
                 ;;
             -d|--dist)
                 check_opt_arg "${opt}" "$1"
-                DIST="$1"
+                TARGET_DIST="$1"
                 shift
                 ;;
             -t|--timestamp)
                 check_opt_arg "${opt}" "$1"
-                CYBER_IMAGE_TIMESTAMP="$1"
+                PREV_IMAGE_TIMESTAMP="$1"
                 shift
                 ;;
             --dry)
-                DRY_RUN=1
+                DRY_RUN_ONLY=1
                 ;;
             -h|--help)
                 print_usage
@@ -272,30 +335,33 @@ function parse_arguments() {
 }
 
 function check_arguments() {
-    if [[ "${MODE}" == "build" ]]; then
-        echo "Build all packages from source if possible"
-    elif [[ "${MODE}" != "download" ]]; then
-        echo "Unknown build mode: ${MODE}, defaults to [download]"
-        # Use prebuilt packages for dependencies if possible
-        MODE="download"
+    if [[ "${INSTALL_MODE}" == "download" ]]; then
+        echo "Use prebuilt packages for dependencies if possible"
+    elif [[ "${INSTALL_MODE}" == "build" ]]; then
+        echo "Build all packages from source"
+    else
+        echo "Unknown INSTALL_MODE: ${INSTALL_MODE}."
+        exit 1
     fi
 
-    if [[ "${GEOLOC}" == "cn" ]]; then
-        echo "Docker image built for CN users"
-    elif [[ "${GEOLOC}" != "us" ]]; then
-        echo "Unknown geo: ${GEOLOC}, defaults to [us]"
-        GEOLOC="us"
+    if [[ "${TARGET_GEOLOC}" == "cn" ]]; then
+        echo "Docker image built w/ CN based mirrors."
+    elif [[ "${TARGET_GEOLOC}" != "us" ]]; then
+        echo "Unknown GEOLOC: ${TARGET_GEOLOC}, defaults to 'us'"
+        TARGET_GEOLOC="us"
     fi
 
-    if [[ "${DIST}" == "testing" ]]; then
-        echo "Build experimental Docker images."
-    elif [[ "${DIST}" != "stable" ]]; then
-        echo "Build stable Docker images"
-        DIST="stable"
+    if [[ "${TARGET_DIST}" == "stable" ]]; then
+        echo "Stable Docker image will be built."
+    elif [[ "${TARGET_DIST}" == "testing" ]]; then
+        echo "Testing (experimental) Docker image will be built."
+    else
+        echo "Unknown APOLLO_DIST: ${TARGET_DIST}. Exit..."
+        exit 1
     fi
 
     if [[ -z "${DOCKERFILE}" ]]; then
-        echo "Dockfile not specified, Exiting..."
+        echo "Dockfile not specified. Exiting..."
         exit 1
     fi
     determine_target_arch_and_stage "${DOCKERFILE}"
@@ -307,45 +373,60 @@ function docker_build_preview() {
     echo "|  FROM image: ${IMAGE_IN}"
     echo "|  Dockerfile: ${DOCKERFILE}"
     echo "|  TARGET_ARCH=${TARGET_ARCH}, HOST_ARCH=${HOST_ARCH}"
-    echo "|  INSTALL_MODE=${MODE}, GEOLOC=${GEOLOC}, APOLLO_DIST=${DIST}"
+    echo "|  INSTALL_MODE=${INSTALL_MODE}, GEOLOC=${TARGET_GEOLOC}, APOLLO_DIST=${TARGET_DIST}"
     echo "=====.=====.=====.=====.=====.=====.=====.=====.=====.=====.=====.=====.====="
 }
 
 function docker_build_run() {
     local extra_args="--squash"
-    if [[ "${BUILD_CLEAN}" -gt 0 ]]; then
+    if [[ "${USE_CACHE}" -eq 0 ]]; then
         extra_args="${extra_args} --no-cache=true"
     fi
     local context="$(dirname "${BASH_SOURCE[0]}")"
 
+    local build_args="--build-arg BASE_IMAGE=${IMAGE_IN}"
+
+    if [[ "${TARGET_STAGE}" == "base" ]]; then
+        build_args="${build_args} --build-arg CUDA_LITE=${CUDA_LITE}"
+        build_args="${build_args} --build-arg CUDNN_VERSION=${CUDNN_VERSION}"
+        build_args="${build_args} --build-arg TENSORRT_VERSION=${TENSORRT_VERSION}"
+    elif [[ "${TARGET_STAGE}" == "cyber" || "${TARGET_STAGE}" == "dev" ]]; then
+        build_args="${build_args} --build-arg APOLLO_DIST=${TARGET_DIST}"
+        build_args="${build_args} --build-arg GEOLOC=${TARGET_GEOLOC}"
+        build_args="${build_args} --build-arg CLEAN_DEPS=yes"
+        build_args="${build_args} --build-arg INSTALL_MODE=${INSTALL_MODE}"
+    elif [[ "${TARGET_STAGE}" == "runtime" ]]; then
+        build_args="${build_args} --build-arg GEOLOC=${TARGET_GEOLOC}"
+        build_args="${build_args} --build-arg CUDA_LITE=${CUDA_LITE}"
+        build_args="${build_args} --build-arg CUDNN_VERSION=${CUDNN_VERSION}"
+        build_args="${build_args} --build-arg TENSORRT_VERSION=${TENSORRT_VERSION}"
+        build_args="${build_args} --build-arg DEV_IMAGE_IN=${DEV_IMAGE_IN}"
+    else
+        echo "Unknown build stage: ${TARGET_STAGE}. Exiting..."
+        exit 1
+    fi
+
     set -x
     docker build ${extra_args} -t "${IMAGE_OUT}" \
-        --build-arg INSTALL_MODE="${MODE}" \
-        --build-arg GEOLOC="${GEOLOC}"     \
-        --build-arg APOLLO_DIST="${DIST}"  \
-        --build-arg BASE_IMAGE="${IMAGE_IN}" \
-        --build-arg CUDA_LITE="${CUDA_LITE}" \
-        --build-arg CUDNN_VERSION="${CUDNN_VERSION}" \
-        --build-arg TENSORRT_VERSION="${TENSORRT_VERSION}" \
-        --build-arg CLEAN_DEPS="yes" \
-        -f "${DOCKERFILE}" \
-        "${context}"
+            ${build_args} \
+            -f "${DOCKERFILE}" \
+            "${context}"
     set +x
 }
 
 function main() {
     parse_arguments "$@"
     check_arguments
+
     check_experimental_docker
-    if [[ "${TARGET_ARCH}" == "x86_64" ]]; then
-        determine_images_for_x86_64 "${TARGET_STAGE}" "${DIST}" "${CYBER_IMAGE_TIMESTAMP}"
-    elif [[ "${TARGET_ARCH}" == "aarch64" ]]; then
-        determine_images_for_aarch64 "${TARGET_STAGE}" "${DIST}" "${CYBER_IMAGE_TIMESTAMP}"
-    fi
+    determine_images_in_out "${TARGET_ARCH}" "${TARGET_STAGE}" "${TARGET_DIST}"
     docker_build_preview
-    if [[ "${DRY_RUN}" -eq 0 ]]; then
-        docker_build_run
+
+    if [[ "${DRY_RUN_ONLY}" -gt 0 ]]; then
+        return
     fi
+
+    docker_build_run
 }
 
 main "$@"
