1648bd0e282d749cfc05fba81d1c92ea354fc705
Niko Z Ho <scyzh4@nottingham.edu.cn>
2021-03-19 15:20:09 +0800

    delete old code

--- auto-test/publisher.py
+++ auto-test/publisher.py
@@ -8,7 +8,7 @@ def talker():
 
 	rospy.init_node('talker', anonymous = True)
 	# define the time of refresh
-	rate = rospy.Rate(6000)
+	rate = rospy.Rate(0.5)
 	 
 	# define the coordinates of the start and end points
 	# can be optimised to read from files
@@ -27,7 +27,7 @@ def talker():
 	# calculate the area of the region
 	area_region = (bound_right - bound_left) * (bound_up - bound_down)
 	# define the obstacle density (0 - 1)
-	obstacle_density = 0.0005
+	obstacle_density = 0.005
 
 	# define number of obstacles
 	n_obstacles = int(obstacle_density * area_region)
@@ -57,38 +57,6 @@ def talker():
 		pub.publish(msg_obstacles)
 		rate.sleep()
 		
-		# # generate an simple obstacle
-		# msg = PerceptionObstacles()
-		# msg1 = msg.perception_obstacle.add()
-		# msg1.id = 1234567
-
-		# msg1.position.x = 587226
-		# msg1.position.y = 4141535
-		# msg1.position.z = 0
-
-		# msg1.theta = 1.70768520645
-		
-		# msg1.length = 5.0
-		# msg1.width = 5.0
-		# msg1.height = 5.0
-
-		# msg2 = msg.perception_obstacle.add()
-		# msg2.id = 7654321
-
-		# msg2.position.x = 587210
-		# msg2.position.y = 4141560
-		# msg2.position.z = 0
-
-		# msg2.theta = 1.564424
-		
-		# msg2.length = 5.0
-		# msg2.width = 5.0
-		# msg2.height = 5.0
-
-
-
-		# pub.publish(msg)
-		# rate.sleep()
 
 if __name__ == '__main__':
 	try:
