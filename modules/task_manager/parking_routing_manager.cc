289f471f8b8f8699159225f03b9d201e3a52ccce
lj0919 <605944389@qq.com>
2021-04-20 13:58:27 +0800

    cpp lint

--- modules/task_manager/parking_routing_manager.cc
+++ modules/task_manager/parking_routing_manager.cc
@@ -46,7 +46,8 @@ common::Status ParkingRoutingManager::Init(
 bool ParkingRoutingManager::SizeVerification(
         const ParkingRoutingTask& parking_routing_task) {
     auto plot_type =
-      parking_routing_task.routing_request().parking_info().parking_space_type();
+      parking_routing_task.routing_request().parking_info()
+      .parking_space_type();
     const auto& vehicle_config =
       common::VehicleConfigHelper::Instance()->GetConfig();
     double ego_length = vehicle_config.vehicle_param().length();
