280ce1327abe988e8750d7e116c853ebe0c4f5e5
Niko Z Ho <scyzh4@nottingham.edu.cn>
2021-04-20 14:34:42 +0800

    fix bugs in routing request publishing

--- auto-test/routing_pub.py
+++ auto-test/routing_pub.py
@@ -1,5 +1,6 @@
 import rospy
 import time
+from numpy import genfromtxt
 from pb_msgs.msg import RoutingResponse, RoutingRequest
 from modules.routing.proto import routing_pb2
 
@@ -7,34 +8,29 @@ def talker():
     pub = rospy.Publisher('/apollo/routing_request', routing_pb2.RoutingRequest, queue_size=1)
     rospy.init_node('routing_request', anonymous=True)
 
-    sequence_num = 0
+    sequence_num = 7
 
     msg_routing_request = routing_pb2.RoutingRequest()
-    # msg_routing_request.broadcast = True
     msg_routing_request.header.timestamp_sec = rospy.get_time()
     msg_routing_request.header.module_name = 'routing_request'
     msg_routing_request.header.sequence_num = sequence_num
     sequence_num = sequence_num + 1
 
-    msg = msg_routing_request.waypoint.add()
-    msg.id = '1-1'
-    msg.s = 1
-    msg.pose.x = 587714
-    msg.pose.y = 4141426
+    # load data from data/waypoints.csv
+    routing_data = genfromtxt('/apollo/auto-test/data/waypoints.csv', delimiter=',',dtype=None)
+    len_waypoints = len(routing_data)
 
-    msg = msg_routing_request.waypoint.add()
-    msg.id = '1-2'
-    msg.s = 80
-    msg.pose.x = 587624
-    msg.pose.y = 4141060
+    for point in range(len_waypoints):
+        curr_point = routing_data[point]
+        msg = msg_routing_request.waypoint.add()
+        msg.id = curr_point[0]
+        msg.s = curr_point[1]
+        msg.pose.x = curr_point[2]
+        msg.pose.y = curr_point[3]
 
-    msg = msg_routing_request.waypoint.add()
-    msg.id = '1-3'
-    msg.s = 100
-    msg.pose.x = 587534
-    msg.pose.y = 4140694
-
-    print(msg_routing_request)
+    # print(msg_routing_request)
+    # wait for 2 seconds to let the message published successfully
+    # if time is too short, the message may be ommited by the system
     time.sleep(2.0)
 
     pub.publish(msg_routing_request)
@@ -44,52 +40,3 @@ if __name__ == '__main__':
         talker()
     except rospy.ROSInterruptException:
         pass
-
-# import argparse
-# import atexit
-# import os
-# import sys
-# import time
-
-# import rospy
-# import scipy.signal as signal
-# from numpy import genfromtxt
-
-# from pb_msgs.msg import RoutingRequest
-# from modules.routing.proto import routing_pb2
-
-# def main():
-#     """
-#     Main rosnode
-#     """
-#     rospy.init_node('mock_routing_requester', anonymous=True)
-#     sequence_num = 0
-
-#     routing_request = routing_pb2.RoutingRequest()
-#     routing_request.header.timestamp_sec = rospy.get_time()
-#     routing_request.header.module_name = 'routing_request'
-#     routing_request.header.sequence_num = sequence_num
-#     sequence_num = sequence_num + 1
-
-#     waypoint = routing_request.waypoint.add()
-#     waypoint.pose.x = 587696.82286
-#     waypoint.pose.y = 4141446.66696
-#     waypoint.id = '1-1'
-#     waypoint.s = 1
-
-#     waypoint = routing_request.waypoint.add()
-#     waypoint.pose.x = 586948.740120
-#     waypoint.pose.y = 4141171.118641
-#     waypoint.id = '1-2'
-#     waypoint.s = 80
-
-#     print(routing_request)
-#     request_publisher = rospy.Publisher(
-#             '/apollo/routing_request', routing_pb2.RoutingRequest, queue_size=10)
-#     time.sleep(2.0)
-#     request_publisher.publish(routing_request)
-
-
-# if __name__ == '__main__':
-#     main()
-
--- auto-test/routing_sub.py
+++ auto-test/routing_sub.py
@@ -1,7 +1,19 @@
 import rospy
+import numpy as np
+
 from pb_msgs.msg import RoutingResponse, RoutingRequest
 
 def routingRequestCallback(RoutingRequest):
+    # waypoints = RoutingRequest.waypoint
+    # len_points = len(waypoints)
+
+    # for point in range(len_points):
+    #     curr_point = waypoints[point]
+    #     print(curr_point.id)
+    #     print(type(curr_point.id))
+    #     routing_data = np.array([[curr_point.id, curr_point.s, curr_point.pose.x, curr_point.pose.y]])
+    #     with open('/apollo/auto-test/data/waypoints.csv', 'a') as csv:
+    #         np.savetxt(csv, routing_data, fmt = ['%s','%f','%f','%f'], delimiter=',',  encoding='utf8')
     print(RoutingRequest)
     print("==========================================================")
 
@@ -10,16 +22,6 @@ def listener_request():
     rospy.Subscriber('/apollo/routing_request', RoutingRequest, routingRequestCallback)
     rospy.spin()
 
-def routingRespondCallback(RoutingRespond):
-    print(RoutingRespond)
-    print("==========================================================")
-
-def listener_respond():
-    rospy.init_node('routing_respond', anonymous=True)
-    rospy.Subscriber('/apollo/routing_respond', RoutingRespond, routingRespondCallback)
-    rospy.spin()
-
-
 if __name__ == '__main__':
-    # listener_request()
-    listener_respond()
+    listener_request()
+    # listener_respond()
