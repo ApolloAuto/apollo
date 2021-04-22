e86b3c0695fd4e7d7fea6096ec4afc2c16f6454d
Martin Jansa <Martin.Jansa@gmail.com>
2021-04-22 16:07:35 +0200

    scripts/generate_routing_topo_graph.sh: drop -alsologtostderr parameter
    * -alsologtostderr is not available since glog was changed to be built without gflags support in:
      https://github.com/ApolloAuto/apollo/commit/0b03b66c838ba878a9df1c468b3ba824a2f29bc7
    * fixes:
      [apollo@in-runtime-docker:/apollo]$ scripts/generate_routing_topo_graph.sh
      ERROR: unknown command line flag 'alsologtostderr'
    
    Signed-off-by: Martin Jansa <Martin.Jansa@gmail.com>

--- scripts/generate_routing_topo_graph.sh
+++ scripts/generate_routing_topo_graph.sh
@@ -23,4 +23,4 @@ source "${DIR}/apollo_base.sh"
 # generate routing_map.bin in map directory.
 ${APOLLO_BIN_PREFIX}/modules/routing/topo_creator/topo_creator \
   --flagfile=modules/routing/conf/routing.conf \
-  -alsologtostderr $@
+  $@
