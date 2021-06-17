1f42db188a4292540d45fd56f8328c42c132cd69
haina0421 <lianghaina@baidu.com>
2021-06-17 11:26:43 +0800

    Test Base_lidar_obstacle_tracking

--- modules/perception/lidar/lib/interface/base_lidar_obstacle_tracking.h
+++ modules/perception/lidar/lib/interface/base_lidar_obstacle_tracking.h
@@ -18,55 +18,43 @@
 #include <memory>
 #include <string>
 
-#include "Eigen/Dense"
-
 #include "cyber/common/macros.h"
 #include "modules/perception/lidar/common/lidar_error_code.h"
 #include "modules/perception/lib/registerer/registerer.h"
-#include "modules/drivers/proto/pointcloud.pb.h"
 #include "modules/perception/lidar/common/lidar_frame.h"
 
 namespace apollo {
 namespace perception {
 namespace lidar {
 
-struct LidarObstacleDetectionInitOptions {
+struct LidarObstacleTrackingInitOptions {
   std::string sensor_name = "velodyne64";
-  bool enable_hdmap_input = true;
 };
 
-struct LidarObstacleDetectionOptions {
-  std::string sensor_name;
-  Eigen::Affine3d sensor2novatel_extrinsics;
+struct LidarObstacleTrackingOptions {
+  std::string sensor_name = "velodyne64";
+};
 
-  EIGEN_MAKE_ALIGNED_OPERATOR_NEW
-} EIGEN_ALIGN16;
-
-class BaseLidarObstacleDetection {
+class BaseLidarObstacleTracking {
  public:
-  BaseLidarObstacleDetection() = default;
-  virtual ~BaseLidarObstacleDetection() = default;
+  BaseLidarObstacleTracking() = default;
+  virtual ~BaseLidarObstacleTracking() = default;
 
-  virtual bool Init(const LidarObstacleDetectionInitOptions& options =
-                LidarObstacleDetectionInitOptions()) = 0;
+  virtual bool Init(const LidarObstacleTrackingInitOptions& options =
+                LidarObstacleTrackingInitOptions()) = 0;
 
-  virtual LidarProcessResult Process(
-      const LidarObstacleDetectionOptions& options,
-      const std::shared_ptr<apollo::drivers::PointCloud const>& message,
-      LidarFrame* frame) = 0;
-
-  virtual LidarProcessResult Process(const LidarObstacleDetectionOptions& options,
+  virtual LidarProcessResult Process(const LidarObstacleTrackingOptions& options,
                              LidarFrame* frame) = 0;
 
-  virtual std::string Name() const = 0;
+  virtual std::string Name() const  = 0;
 
  private:
-  DISALLOW_COPY_AND_ASSIGN(BaseLidarObstacleDetection);
-};  // class BaseLidarObstacleDetection
+ DISALLOW_COPY_AND_ASSIGN(BaseLidarObstacleTracking);
+};  // class BaseLidarObstacleTracking
 
-PERCEPTION_REGISTER_REGISTERER(BaseLidarObstacleDetection);
-#define PERCEPTION_REGISTER_LIDAROBSTACLEDETECTION(name) \
-  PERCEPTION_REGISTER_CLASS(BaseLidarObstacleDetection, name)
+PERCEPTION_REGISTER_REGISTERER(BaseLidarObstacleTracking);
+#define PERCEPTION_REGISTER_LIDAROBSTACLETRACKING(name) \
+  PERCEPTION_REGISTER_CLASS(BaseLidarObstacleTracking, name)
 
 }  // namespace lidar
 }  // namespace perception
