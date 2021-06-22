4f3fe13e0062ba25cca98b05ac701f6c7168f790
chenxiaoying21 <82431519+chenxiaoying21@users.noreply.github.com>
2021-06-22 15:24:31 +0800

    SimControl: Update vehicle pose by RoutingRequest.
    In SimControl mode, use the start point of the RoutingRequest to update
    the current pose of the vehicle.
    
    Signed-off-by: chenxiaoying01 <chenxiaoying01@baidu.com>

--- modules/dreamview/backend/sim_control/sim_control.cc
+++ modules/dreamview/backend/sim_control/sim_control.cc
@@ -42,6 +42,7 @@ using apollo::localization::LocalizationEstimate;
 using apollo::planning::ADCTrajectory;
 using apollo::prediction::PredictionObstacles;
 using apollo::relative_map::NavigationInfo;
+using apollo::routing::RoutingRequest;
 using apollo::routing::RoutingResponse;
 
 namespace {
@@ -81,6 +82,11 @@ void SimControl::InitTimerAndIO() {
       FLAGS_routing_response_topic,
       [this](const std::shared_ptr<RoutingResponse>& routing) {
         this->OnRoutingResponse(routing);
+      });
+  routing_request_reader_ = node_->CreateReader<RoutingRequest>(
+      FLAGS_routing_request_topic,
+      [this](const std::shared_ptr<RoutingRequest>& routing_request) {
+        this->OnRoutingRequest(routing_request);
       });
   navigation_reader_ = node_->CreateReader<NavigationInfo>(
       FLAGS_navigation_topic,
@@ -207,29 +213,49 @@ void SimControl::OnRoutingResponse(
 
   CHECK_GE(routing->routing_request().waypoint_size(), 2)
       << "routing should have at least two waypoints";
-  const auto& start_pose = routing->routing_request().waypoint(0).pose();
 
   current_routing_header_ = routing->header();
+}
 
-  // If this is from a planning re-routing request, or the start point has
-  // been
-  // initialized by an actual localization pose, don't reset the start point.
-  re_routing_triggered_ =
-      routing->routing_request().header().module_name() == "planning";
-  if (!re_routing_triggered_ && !start_point_from_localization_) {
-    ClearPlanning();
-    TrajectoryPoint point;
-    point.mutable_path_point()->set_x(start_pose.x());
-    point.mutable_path_point()->set_y(start_pose.y());
-    point.set_a(next_point_.has_a() ? next_point_.a() : 0.0);
-    point.set_v(next_point_.has_v() ? next_point_.v() : 0.0);
-    double theta = 0.0;
-    double s = 0.0;
+void SimControl::OnRoutingRequest(
+    const std::shared_ptr<RoutingRequest>& routing_request) {
+  std::lock_guard<std::mutex> lock(mutex_);
+  if (!enabled_) {
+    return;
+  }
+
+  CHECK_GE(routing_request->waypoint_size(), 2)
+      << "routing should have at least two waypoints";
+  const auto& start_pose = routing_request->waypoint(0).pose();
+
+  ClearPlanning();
+  TrajectoryPoint point;
+  point.mutable_path_point()->set_x(start_pose.x());
+  point.mutable_path_point()->set_y(start_pose.y());
+  point.set_a(next_point_.has_a() ? next_point_.a() : 0.0);
+  point.set_v(next_point_.has_v() ? next_point_.v() : 0.0);
+  double theta = 0.0;
+  double s = 0.0;
+  const auto& start_way_point = routing_request->waypoint().Get(0);
+  // If the lane id has been set, set theta as the lane heading.
+  if (start_way_point.has_id()) {
+    auto& hdmap = hdmap::HDMapUtil::BaseMap();
+    hdmap::Id lane_id = hdmap::MakeMapId(start_way_point.id());
+    auto lane = hdmap.GetLaneById(lane_id);
+    if (nullptr != lane) {
+      theta = lane->Heading(start_way_point.s());
+    } else {
+      map_service_->GetPoseWithRegardToLane(start_pose.x(), start_pose.y(),
+                                            &theta, &s);
+    }
+  } else {
+    // Find the lane nearest to the start pose and get its heading as theta.
     map_service_->GetPoseWithRegardToLane(start_pose.x(), start_pose.y(),
                                           &theta, &s);
-    point.mutable_path_point()->set_theta(theta);
-    SetStartPoint(point);
   }
+
+  point.mutable_path_point()->set_theta(theta);
+  SetStartPoint(point);
 }
 
 void SimControl::OnPredictionObstacles(
