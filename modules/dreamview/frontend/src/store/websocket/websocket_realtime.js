c45c63fbcb417ecfb1b484933f72d1bae2e076ed
lijin18 <lijin18@baidu.com>
2021-06-23 16:05:02 +0800

    Dreamview: add car heading when send parking request
    Change-Id: Ifea3377391cdd3e4f1663b9f0b7b7b2f0fdf0e4f

--- modules/dreamview/frontend/src/store/websocket/websocket_realtime.js
+++ modules/dreamview/frontend/src/store/websocket/websocket_realtime.js
@@ -431,7 +431,7 @@ export default class RealtimeWebSocketEndpoint {
     this.websocket.send(JSON.stringify(request));
   }
 
-  sendParkingRequest(start, waypoint, end, parkingInfo, laneWidth, cornerPoints, id) {
+  sendParkingRequest(start,waypoint,end, parkingInfo, laneWidth,cornerPoints,id, start_heading) {
     const request = {
       type: 'SendParkingRoutingRequest',
       start,
@@ -441,6 +441,9 @@ export default class RealtimeWebSocketEndpoint {
       laneWidth,
       cornerPoints,
     };
+    if (start_heading) {
+      request.start.heading = start_heading;
+    }
     if (id) {
       request.end.id = id;
     }
