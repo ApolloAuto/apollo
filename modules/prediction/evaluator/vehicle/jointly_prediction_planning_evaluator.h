883afd3cb2a2b2683610666863d43ad28d77f592
jiangjianan <jiangjianan@baidu.com>
2021-08-09 14:53:01 +0800

    Prediction: modified jointly evaluator to prevent core dump when adc traj point num is less than 1
    Change-Id: Ib27016f580b92d035e9628b65b66ab83f584d367

--- modules/prediction/evaluator/vehicle/jointly_prediction_planning_evaluator.h
+++ modules/prediction/evaluator/vehicle/jointly_prediction_planning_evaluator.h
@@ -49,6 +49,41 @@ class JointlyPredictionPlanningEvaluator : public Evaluator {
   void Clear();
 
   /**
+   * @brief Process obstacle position to vector
+   * @param Obstacles pointer
+   * @param Obstacles container
+   * @param Tensor: target obstacle position
+   * @param Tensor: target obstacle position step
+   * @param Tensor: vector mask
+   * @param Tensor: all obstacle position
+   * @param Tensor: all obstacle p_id
+   * @param Tensor: all obstacle length
+   */
+  bool VectornetProcessObstaclePosition(Obstacle* obstacle_ptr,
+                                    ObstaclesContainer* obstacles_container,
+                                    torch::Tensor* ptr_target_obs_pos,
+                                    torch::Tensor* ptr_target_obs_pos_step,
+                                    torch::Tensor* ptr_vector_mask,
+                                    torch::Tensor* ptr_all_obstacle_pos,
+                                    torch::Tensor* ptr_all_obs_p_id,
+                                    torch::Tensor* ptr_obs_length);
+
+  /**
+  * @brief Process map data to vector
+  * @param FeatureVector: map feature vector
+  * @param int: obstacle number
+  * @param PidVector: map p_id vector
+  * @param Tensor: map data
+  * @param Tensor: map data p_id
+  */
+  bool VectornetProcessMapData(FeatureVector *map_feature,
+                               PidVector *map_p_id,
+                               const int obs_num,
+                               torch::Tensor* ptr_map_data,
+                               torch::Tensor* ptr_all_map_p_id,
+                               torch::Tensor* ptr_vector_mask);
+
+  /**
    * @brief Override Evaluate
    * @param Obstacle pointer
    * @param Obstacles container
