223fa49633e44301aa2be6b3295961617863f8bc
liuxu20 <liuxu20@baidu.com>
2022-03-28 14:33:01 +0800

    dockerfile improve
    Change-Id: I86130a9f1d1557096a32b327a1454bf968efd671

--- docker/build/edu_docker.sh
+++ docker/build/edu_docker.sh
@@ -2,5 +2,5 @@
 
 APOLLO_REPO="apolloauto/apollo"
 EDU_CHALLENGE="edu_challenge_sim"
-
+wget https://apollo-dreamland-prod.bj.bcebos.com/apollo-system/sim_obstacle
 docker build -t "${APOLLO_REPO}:${EDU_CHALLENGE}" -f dev.x86_64.edu_chllenge_sim.dockerfile .
