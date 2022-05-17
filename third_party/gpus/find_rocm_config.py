cac2f982ce66ec2c9db5aafa1ccd5dbf42bae8b5
alexpetersburg <aleksandr.nefedov@dxc.com>
2022-05-17 15:11:26 +0300

    [AMD][build][python] Refactor `find_rocm_config.py`
    + The logic of searching library files has been redesigned, by analogy with `find_cuda_config.py`
    + The `*_include_dir` and `*_library_dir` fields have been added for each library
    + Updated `find_rocm_config.py.gz.base64`
    
    [Note] With the current script compression logic, we can't join the same code from `find_cuda_config.py` and `find_cuda_config.py`
    
    [Tests]
    Example of a new config:
    ```
    hipblas_include_dir: /opt/rocm-5.0.0/hipblas/include
    hipblas_library_dir: /opt/rocm-5.0.0/hipblas/lib
    hipblas_version_number: 4900
    hipruntime_include_dir: /opt/rocm-5.0.0/hip/include/hip
    hipruntime_library_dir: /opt/rocm-5.0.0/hip/lib
    hipruntime_version_number: 500
    miopen_include_dir: /opt/rocm-5.0.0/miopen/include/miopen
    miopen_library_dir: /opt/rocm-5.0.0/miopen/lib
    miopen_version_number: 21500
    rocm_header_path: /opt/rocm-5.0.0/include
    rocm_toolkit_path: /opt/rocm-5.0.0
    rocm_version_number: 50000
    ```

--- third_party/gpus/find_rocm_config.py
+++ third_party/gpus/find_rocm_config.py
@@ -31,10 +31,117 @@ import io
 import os
 import re
 import sys
+import glob
+
+
+def _header_paths():
+    """Returns hard-coded set of relative paths to look for header files."""
+    return [
+        "",
+        "include",
+        "include/rocm",
+        "include/*-linux-gnu",
+        ".info"
+    ]
+
+
+def _library_paths():
+    """Returns hard-coded set of relative paths to look for library files."""
+    return [
+        "",
+        "lib64",
+        "lib",
+        "lib/*-linux-gnu",
+        "lib64/stubs"
+    ]
 
 
 class ConfigError(Exception):
-  pass
+    pass
+
+
+def _matches_version(actual_version, required_version):
+    """Checks whether some version meets the requirements.
+
+        All elements of the required_version need to be present in the
+        actual_version.
+
+            required_version  actual_version  result
+            -----------------------------------------
+            1                 1.1             True
+            1.2               1               False
+            1.2               1.3             False
+                              1               True
+
+        Args:
+          required_version: The version specified by the user.
+          actual_version: The version detected from the CUDA installation.
+        Returns: Whether the actual version matches the required one.
+    """
+    if actual_version is None:
+        return False
+
+    # Strip spaces from the versions.
+    actual_version = str(actual_version).strip()
+    required_version = str(required_version).strip()
+    return actual_version.startswith(required_version)
+
+
+def _cartesian_product(first, second):
+    """Returns all path combinations of first and second."""
+    return [os.path.join(f, s) for f in first for s in second]
+
+
+def _not_found_error(base_paths, relative_paths, filepattern):
+    base_paths = "".join(["\n        '%s'" %
+                          path for path in sorted(base_paths)])
+    relative_paths = "".join(["\n        '%s'" %
+                              path for path in relative_paths])
+    return ConfigError(
+        "Could not find any %s in any subdirectory:%s\nof:%s\n" %
+        (filepattern, relative_paths, base_paths))
+
+
+def _find_file(base_paths, relative_paths, filepattern):
+    for path in _cartesian_product(base_paths, relative_paths):
+        for file in glob.glob(os.path.join(path, filepattern)):
+            return file
+    raise _not_found_error(base_paths, relative_paths, filepattern)
+
+
+def _find_library(base_paths, library_name, required_version, library_paths):
+    """Returns first valid path to the requested library."""
+    filepattern = ".".join(["lib" + library_name, "so"]
+                           + required_version.split(".")[:1]) + "*"
+    return _find_file(base_paths, library_paths(), filepattern)
+
+
+def _find_versioned_file(base_paths, relative_paths, filepatterns,
+                         required_version, get_version):
+    """Returns first valid path to a file that matches the requested version."""
+    if type(filepatterns) not in [list, tuple]:
+        filepatterns = [filepatterns]
+    for path in _cartesian_product(base_paths, relative_paths):
+        for filepattern in filepatterns:
+            for file in glob.glob(os.path.join(path, filepattern)):
+                actual_version = get_version(file)
+                if _matches_version(actual_version, required_version):
+                    return file, actual_version
+    raise _not_found_error(
+        base_paths, relative_paths,
+        ", ".join(filepatterns) + " matching version '%s'" % required_version)
+
+
+def _find_header(base_paths, header_name, required_version, get_version, header_paths):
+    """Returns first valid path to a header that matches the requested version."""
+    return _find_versioned_file(base_paths, header_paths(), header_name,
+                                required_version, get_version)
+
+
+def _get_path_if_link(path):
+  if os.path.islink(path):
+      return os.path.realpath(path)
+  return path
 
 
 def _get_default_rocm_path():
@@ -43,11 +150,8 @@ def _get_default_rocm_path():
 
 def _get_rocm_install_path():
   """Determines and returns the ROCm installation path."""
-  rocm_install_path = _get_default_rocm_path()
-  if "ROCM_PATH" in os.environ:
-    rocm_install_path = os.environ["ROCM_PATH"]
-  # rocm_install_path = os.path.realpath(rocm_install_path)
-  return rocm_install_path
+  rocm_install_path = os.environ.get("ROCM_PATH", _get_default_rocm_path())
+  return _get_path_if_link(rocm_install_path)
 
 
 def _get_composite_version_number(major, minor, patch):
@@ -67,13 +171,11 @@ def _get_header_version(path, name):
                     "  its value is not an integer literal")
 
 
-def _find_rocm_config(rocm_install_path):
+def _find_rocm_config(rocm_install_path, required_version=""):
 
-  def rocm_version_numbers_pre_rocm50(path, prior_err):
-    version_file = os.path.join(path, ".info/version-dev")
+  def rocm_version_numbers_pre_rocm50(version_file):
     if not os.path.exists(version_file):
-      raise ConfigError("{} ROCm version file ".format(prior_err) +
-                        '"{}" not found either.'.format(version_file))
+      return None
     version_numbers = []
     with open(version_file) as f:
       version_string = f.read().strip()
@@ -81,36 +183,36 @@ def _find_rocm_config(rocm_install_path):
     major = int(version_numbers[0])
     minor = int(version_numbers[1])
     patch = int(version_numbers[2].split("-")[0])
-    return major, minor, patch
+    return _get_composite_version_number(major, minor, patch)
 
-  def rocm_version_numbers_post_rocm50(path):
-    version_file = os.path.join(path, "include/rocm_version.h")
+  def rocm_version_numbers_post_rocm50(version_file):
     if not os.path.exists(version_file):
-      return False, 'ROCm version file "{}" not found.'.format(version_file) +\
-        " Trying an alternate approach to determine the ROCm version.", 0,0,0
+      return None
     major = _get_header_version(version_file, "ROCM_VERSION_MAJOR")
     minor = _get_header_version(version_file, "ROCM_VERSION_MINOR")
     patch = _get_header_version(version_file, "ROCM_VERSION_PATCH")
-    return True, "", major, minor, patch
-
-  status, error_msg, major, minor, patch = \
-    rocm_version_numbers_post_rocm50(rocm_install_path)
-
-  if not status:
-    major, minor, patch = \
-      rocm_version_numbers_pre_rocm50(rocm_install_path, error_msg)
-
+    return _get_composite_version_number(major, minor, patch)
+  try:
+      rocm_header_path, header_version = _find_header([rocm_install_path], "rocm_version.h",
+                                                      required_version,
+                                                      rocm_version_numbers_post_rocm50,
+                                                      _header_paths)
+  except ConfigError as e:\
+      rocm_header_path, header_version = _find_header([rocm_install_path], "version-dev",
+                                                       required_version,
+                                                       rocm_version_numbers_pre_rocm50,
+                                                       _header_paths)
   rocm_config = {
-      "rocm_version_number": _get_composite_version_number(major, minor, patch)
+      "rocm_version_number": header_version,
+      "rocm_header_path": os.path.dirname(rocm_header_path)
   }
 
   return rocm_config
 
 
-def _find_hipruntime_config(rocm_install_path):
+def _find_hipruntime_config(rocm_install_path, required_version=""):
 
-  def hipruntime_version_number(path):
-    version_file = os.path.join(path, "hip/include/hip/hip_version.h")
+  def hipruntime_version_number(version_file):
     if not os.path.exists(version_file):
       raise ConfigError(
           'HIP Runtime version file "{}" not found'.format(version_file))
@@ -121,40 +223,54 @@ def _find_hipruntime_config(rocm_install_path):
     major = _get_header_version(version_file, "HIP_VERSION_MAJOR")
     minor = _get_header_version(version_file, "HIP_VERSION_MINOR")
     return 100 * major + minor
-
+  header_path, header_version = _find_header([rocm_install_path],
+                                             "hip/include/hip/hip_version.h",
+                                             required_version,
+                                             hipruntime_version_number,
+                                             _header_paths)
+  library_path = _find_library([rocm_install_path], "amdhip64",
+                               required_version, _library_paths)
+  library_path = _get_path_if_link(library_path)
   hipruntime_config = {
-      "hipruntime_version_number": hipruntime_version_number(rocm_install_path)
+      "hipruntime_include_dir": os.path.dirname(header_path),
+      "hipruntime_version_number": header_version,
+      "hipruntime_library_dir": os.path.dirname(library_path)
   }
 
   return hipruntime_config
 
 
-def _find_miopen_config(rocm_install_path):
+def _find_miopen_config(rocm_install_path, required_version=''):
 
-  def miopen_version_numbers(path):
-    version_file = os.path.join(path, "miopen/include/miopen/version.h")
+  def miopen_version_numbers(version_file):
     if not os.path.exists(version_file):
       raise ConfigError(
           'MIOpen version file "{}" not found'.format(version_file))
     major = _get_header_version(version_file, "MIOPEN_VERSION_MAJOR")
     minor = _get_header_version(version_file, "MIOPEN_VERSION_MINOR")
     patch = _get_header_version(version_file, "MIOPEN_VERSION_PATCH")
-    return major, minor, patch
+    return _get_composite_version_number(major, minor, patch)
 
-  major, minor, patch = miopen_version_numbers(rocm_install_path)
-
+  header_path, header_version = _find_header([rocm_install_path],
+                                              "miopen/include/miopen/version.h",
+                                              required_version,
+                                              miopen_version_numbers,
+                                              _header_paths)
+  library_path = _find_library([rocm_install_path], "MIOpen",
+                               required_version, _library_paths)
+  library_path = _get_path_if_link(library_path)
   miopen_config = {
-      "miopen_version_number":
-          _get_composite_version_number(major, minor, patch)
+      "miopen_version_number": header_version,
+      "miopen_include_dir": os.path.dirname(header_path),
+      "miopen_library_dir": os.path.dirname(library_path)
   }
 
   return miopen_config
 
 
-def _find_hipblas_config(rocm_install_path):
+def _find_hipblas_config(rocm_install_path, required_version=''):
 
-  def hipblas_version_numbers(path):
-    version_file = os.path.join(path, "hipblas/include/hipblas-version.h")
+  def hipblas_version_numbers(version_file):
     if not os.path.exists(version_file):
       raise ConfigError(
           'hipblas version file "{}" not found'.format(version_file))
@@ -162,13 +278,20 @@ def _find_hipblas_config(rocm_install_path):
     # ToDo: Fix typo in "hipblaseVersionMinor" with switching to ROCM 5.1.0 or higher, where the typo is fixed
     minor = _get_header_version(version_file, "hipblaseVersionMinor")
     patch = _get_header_version(version_file, "hipblasVersionPatch")
-    return major, minor, patch
-
-  major, minor, patch = hipblas_version_numbers(rocm_install_path)
+    return _get_composite_version_number(major, minor, patch)
+  header_path, header_version = _find_header([rocm_install_path],
+                                              "hipblas/include/hipblas-version.h",
+                                              required_version,
+                                              hipblas_version_numbers,
+                                              _header_paths)
+  library_path = _find_library([rocm_install_path], "hipblas",
+                               required_version, _library_paths)
+  library_path = _get_path_if_link(library_path)
 
   hipblas_config = {
-      "hipblas_version_number":
-          _get_composite_version_number(major, minor, patch)
+      "hipblas_version_number": header_version,
+      "hipblas_include_dir": os.path.dirname(header_path),
+      "hipblas_library_dir": os.path.dirname(library_path)
   }
 
   return hipblas_config
@@ -183,10 +306,10 @@ def find_rocm_config():
   result = {}
 
   result["rocm_toolkit_path"] = rocm_install_path
-  result.update(_find_rocm_config(rocm_install_path))
-  result.update(_find_hipruntime_config(rocm_install_path))
-  result.update(_find_miopen_config(rocm_install_path))
-  result.update(_find_hipblas_config(rocm_install_path))
+  result.update(_find_rocm_config(rocm_install_path, os.environ.get("TF_ROCM_VERSION", "")))
+  result.update(_find_hipruntime_config(rocm_install_path, os.environ.get("TF_HIP_VERSION", "")))
+  result.update(_find_miopen_config(rocm_install_path, os.environ.get("TF_MIOPEN_VERSION", "")))
+  result.update(_find_hipblas_config(rocm_install_path, os.environ.get("TF_HIPBLAS_VERSION", "")))
 
   return result
 
