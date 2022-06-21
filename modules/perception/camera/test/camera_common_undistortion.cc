97fccd0425630b7df280609992b39a11e516361a
Bitway <andrew.shukshov@gmail.com>
2022-06-21 15:35:39 +0300

    The stub in `camera/test/camera_common_undistortion` was updated

--- modules/perception/camera/test/camera_common_undistortion.cc
+++ modules/perception/camera/test/camera_common_undistortion.cc
@@ -16,9 +16,16 @@
 #include "modules/perception/camera/test/camera_common_undistortion.h"
 
 #if GPU_PLATFORM == NVIDIA
-  #include <npp.h>
+    #include <npp.h>
 #elif GPU_PLATFORM == AMD
-  #include <rpp.h>
+    #include <rpp.h>
+    #define NppiSize RppiSize
+    #define Npp32f Rpp32f
+    #define NppiInterpolationMode RpptInterpolationType
+    #define NPPI_INTER_LINEAR RpptInterpolationType::BILINEAR
+    #define NppiRect RppiRect
+    #define NppStatus RppStatus
+    #define NPP_SUCCESS RPP_SUCCESS
 #endif
 
 #include <boost/filesystem.hpp>
@@ -111,27 +118,25 @@ int ImageGpuPreprocessHandler::handle(uint8_t *src, uint8_t *dst) {
   }
 
   BASE_GPU_CHECK(cudaMemcpy(_d_rgb, src, _in_size, cudaMemcpyHostToDevice));
-  #if GPU_PLATFORM == NVIDIA
+  
     NppiSize Remapsize;
     NppiInterpolationMode RemapMode = NPPI_INTER_LINEAR;
     Remapsize.width = _width;
     Remapsize.height = _height;
     NppiRect RemapRect = {0, 0, _width, _height};
-
+  #if GPU_PLATFORM == NVIDIA
     NppStatus eStatusNPP =
         nppiRemap_8u_C3R(_d_rgb, Remapsize, (_width * CHANNEL), RemapRect,
                         _d_mapx, (_width * static_cast<int>(sizeof(float))),
                         _d_mapy, (_width * static_cast<int>(sizeof(float))),
                         _d_dst, (_width * CHANNEL), Remapsize, RemapMode);
-
+  #elif GPU_PLATFORM == AMD
+    // TODO(B1tway): Add necesssary RPP code
+  #endif
     if (eStatusNPP != NPP_SUCCESS) {
       std::cerr << "NPP_CHECK_NPP - eStatusNPP = " << eStatusNPP << std::endl;
       return static_cast<int>(eStatusNPP);
     }
-  #elif GPU_PLATFORM == AMD
-    // TODO(B1tway): Add necesssary RPP code
-  #endif
-
   BASE_GPU_CHECK(cudaMemcpy(dst, _d_dst, _out_size, cudaMemcpyDeviceToHost));
   return 0;
 }
