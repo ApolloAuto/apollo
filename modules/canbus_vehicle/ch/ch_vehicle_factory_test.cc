7a97bec4017f0cab864658394e69930aef4c404e
machao20 <machao20@baidu.com>
2022-11-29 21:07:04 +0800

    canbus-vehicle: fix ch vehicle test bug
    Change-Id: Icfaa55d4d04e0549227450481fa491c6b79eab99

--- modules/canbus_vehicle/ch/ch_vehicle_factory_test.cc
+++ modules/canbus_vehicle/ch/ch_vehicle_factory_test.cc
@@ -30,7 +30,7 @@ class ChVehicleFactoryTest : public ::testing::Test {
  public:
   virtual void SetUp() {
     std::string canbus_conf_file =
-        "modules/canbus_vehicle/ch/testdata/ch_canbus_conf_test.pb.txt";
+        "/apollo/modules/canbus_vehicle/ch/testdata/ch_canbus_conf_test.pb.txt";
     cyber::common::GetProtoFromFile(canbus_conf_file, &canbus_conf_);
     params_ = canbus_conf_.vehicle_parameter();
     params_.set_brand(apollo::common::CH);
