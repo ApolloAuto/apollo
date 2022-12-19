27584f4d2334964fc816004cc8f4f6615668416e
wangkang12 <wangkang12@baidu.com>
2022-12-19 16:32:07 +0800

    dreamview: Obstacle Type Color
    Change-Id: I1f6c2bb03a5ddc6b1d41f70633fb7690f1016f3b

---
+++ modules/dreamview/frontend/src/utils/constant.js
@@ -0,0 +1,16 @@
+export const ObstacleTypeColorMap = new Map(
+  [
+    ['ST_UNKNOWN', 'rgba(96, 96, 96, 1.000)'],
+    ['ST_UNKNOWN_MOVABLE', 'rgba(158, 201, 240, 1.000)'],
+    ['ST_UNKNOWN_UNMOVABLE', 'rgba(158, 201, 240, 1.000)'],
+    ['ST_CAR', 'rgba(243, 187, 37, 1.000)'],
+    ['ST_VAN', 'rgba(1, 82, 75, 1.000)'],
+    ['ST_TRUCK', 'rgba(146, 127, 245, 1.000)'],
+    ['ST_BUS', 'rgba(208, 53, 28, 1.000)'],
+    ['ST_CYCLIST', 'rgba(231, 91, 135, 1.000)'],
+    ['ST_MOTORCYCLIST', 'rgba(98, 207, 254, 1.000)'],
+    ['ST_TRICYCLIST', 'rgba(0, 106, 255, 1.000)'],
+    ['ST_PEDESTRIAN', 'rgba(60, 156, 83, 1.000)'],
+    ['ST_TRAFFICCONE', 'rgba(103, 157, 246, 1.000)'],
+  ]
+);
