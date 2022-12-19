2d12107bed440c6a4b240b4d00cc0da239ee0053
litianjiao <litianjiao@baidu.com>
2022-12-19 17:07:34 +0800

    remove old code
    Change-Id: I299c00634006f1f99d0d8114414eab4929ac6744

--- modules/planning/tasks/optimizers/open_space_trajectory_generation/open_space_trajectory_optimizer.cc
+++ modules/planning/tasks/optimizers/open_space_trajectory_generation/open_space_trajectory_optimizer.cc
@@ -238,52 +238,6 @@ Status OpenSpaceTrajectoryOptimizer::Plan(
           break;
         }
       }
-      if (FLAGS_use_iterative_anchoring_smoother) {
-        if (!GenerateDecoupledTraj(
-                xWS_vec[i], last_time_u(1, 0), init_v, obstacles_vertices_vec,
-                &state_result_ds_vec[i], &control_result_ds_vec[i],
-                &time_result_ds_vec[i])) {
-          AERROR << "Smoother fail at " << i << "th trajectory";
-          AERROR << i << "th trajectory size is " << xWS_vec[i].cols();
-          return Status(
-              ErrorCode::PLANNING_ERROR,
-              "iterative anchoring smoothing problem failed to solve");
-        }
-      } else {
-        const double start_system_timestamp =
-            std::chrono::duration<double>(
-                std::chrono::system_clock::now().time_since_epoch())
-                .count();
-        if (!GenerateDistanceApproachTraj(
-                xWS_vec[i], uWS_vec[i], XYbounds, obstacles_edges_num,
-                obstacles_A, obstacles_b, obstacles_vertices_vec, last_time_u,
-                init_v, &state_result_ds_vec[i], &control_result_ds_vec[i],
-                &time_result_ds_vec[i], &l_warm_up_vec[i], &n_warm_up_vec[i],
-                &dual_l_result_ds_vec[i], &dual_n_result_ds_vec[i])) {
-          AERROR << "Smoother fail at " << i
-                 << "th trajectory with index starts from 0";
-          AERROR << i << "th trajectory size is " << xWS_vec[i].cols();
-          AERROR << "State matrix: " << xWS_vec[i];
-          AERROR << "Control matrix: " << uWS_vec[i];
-          return Status(ErrorCode::PLANNING_ERROR,
-                        "distance approach smoothing problem failed to solve");
-        }
-        const auto end_system_timestamp =
-            std::chrono::duration<double>(
-                std::chrono::system_clock::now().time_since_epoch())
-                .count();
-        const auto time_diff_ms =
-            (end_system_timestamp - start_system_timestamp) * 1000;
-        ADEBUG << "total planning time spend: " << time_diff_ms << " ms.";
-        ADEBUG << i << "th trajectory size is " << xWS_vec[i].cols();
-        ADEBUG << "average time spend: " << time_diff_ms / xWS_vec[i].cols()
-               << " ms per point.";
-        ADEBUG << "average time spend after smooth: "
-               << time_diff_ms / state_result_ds_vec[i].cols()
-               << " ms per point.";
-        ADEBUG << i << "th smoothed trajectory size is "
-               << state_result_ds_vec[i].cols();
-      }
       const auto smoother_end_timestamp = std::chrono::system_clock::now();
       std::chrono::duration<double> smoother_diff =
           smoother_end_timestamp - smoother_start_timestamp;
