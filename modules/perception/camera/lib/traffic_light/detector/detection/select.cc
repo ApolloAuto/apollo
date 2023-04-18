f365a696a147a212c5978cea13d60645ba805774
daohu527 <daohu527@gmail.com>
2023-04-18 13:55:45 +0800

    fix(perception): fix static_cast parentheses position error

--- modules/perception/camera/lib/traffic_light/detector/detection/select.cc
+++ modules/perception/camera/lib/traffic_light/detector/detection/select.cc
@@ -46,8 +46,8 @@ void Select::SelectTrafficLights(
   munkres_.costs()->Resize(hdmap_bboxes->size(), refined_bboxes.size());
 
   for (size_t row = 0; row < hdmap_bboxes->size(); ++row) {
-    auto center_hd = (*hdmap_bboxes)[row]->region.detection_roi.Center();
-    if ((*hdmap_bboxes)[row]->region.outside_image) {
+    auto center_hd = hdmap_bboxes->at(row)->region.detection_roi.Center();
+    if (hdmap_bboxes->at(row)->region.outside_image) {
       AINFO << "projection_roi outside image, set score to 0.";
       for (size_t col = 0; col < refined_bboxes.size(); ++col) {
         (*munkres_.costs())(row, col) = 0.0;
@@ -71,7 +71,7 @@ void Select::SelectTrafficLights(
       (*munkres_.costs())(row, col) =
           static_cast<float>(detection_weight * detection_score +
                              distance_weight * distance_score);
-      const auto &crop_roi = (*hdmap_bboxes)[row]->region.crop_roi;
+      const auto &crop_roi = hdmap_bboxes->at(row)->region.crop_roi;
       const auto &detection_roi = refined_bboxes[col]->region.detection_roi;
       if ((detection_roi & crop_roi) != detection_roi) {
         AINFO << "detection_roi outside crop_roi, set score to 0."
@@ -88,19 +88,18 @@ void Select::SelectTrafficLights(
   munkres_.Maximize(&assignments);
 
   for (size_t i = 0; i < hdmap_bboxes->size(); ++i) {
-    (*hdmap_bboxes)[i]->region.is_selected = false;
-    (*hdmap_bboxes)[i]->region.is_detected = false;
+    hdmap_bboxes->at(i)->region.is_selected = false;
+    hdmap_bboxes->at(i)->region.is_detected = false;
   }
 
   for (size_t i = 0; i < assignments.size(); ++i) {
-    if (static_cast<size_t>(assignments[i].first) >= hdmap_bboxes->size() ||
-        static_cast<size_t>(
-            assignments[i].second >= refined_bboxes.size() ||
-            (*hdmap_bboxes)[assignments[i].first]->region.is_selected ||
-            refined_bboxes[assignments[i].second]->region.is_selected)) {
+    if (assignments[i].first >= hdmap_bboxes->size() ||
+        assignments[i].second >= refined_bboxes.size() ||
+        hdmap_bboxes->at(assignments[i].first)->region.is_selected ||
+        refined_bboxes[assignments[i].second]->region.is_selected) {
     } else {
       auto &refined_bbox_region = refined_bboxes[assignments[i].second]->region;
-      auto &hdmap_bbox_region = (*hdmap_bboxes)[assignments[i].first]->region;
+      auto &hdmap_bbox_region = hdmap_bboxes->at(assignments[i].first)->region;
       refined_bbox_region.is_selected = true;
       hdmap_bbox_region.is_selected = true;
 
@@ -122,9 +121,9 @@ void Select::SelectTrafficLights(
   for (size_t i = 0; i < hdmap_bboxes->size(); ++i) {
     AINFO << "hdmap_bboxes-" << i << ":"
           << " projection_roi: "
-          << (*hdmap_bboxes)[i]->region.projection_roi.ToStr()
+          << hdmap_bboxes->at(i)->region.projection_roi.ToStr()
           << " detection_roi: "
-          << (*hdmap_bboxes)[i]->region.detection_roi.ToStr();
+          << hdmap_bboxes->at(i)->region.detection_roi.ToStr();
   }
 }
 
