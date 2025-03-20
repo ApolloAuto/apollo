f5d2297f2646389127b4026a797e2ffa223a7a92
yhzsir <2455413877@qq.com>
2025-03-20 16:01:38 +0800

    Add files via upload
    feat(global_flagfile.txt):增添了info权限
    style(serial_conf.pb.txt):更改串口号
    feat(ros_control.cc):
    1、增加十六进制转换辅助函数用来测试是否发出正确且接收到正确串口数据
    2、改变data初始值
    3、增添日志打印
    feat(ros_parser.cc):增添日志打印
    fix(twist_cmd.cc):修改了错误字节位置

---
+++ serial_HK/ros_control.h
@@ -0,0 +1,86 @@
+// Copyright 2025 WheelOS. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+//  Created Date: 2025-01-16
+//  Author: daohu527
+
+#pragma once
+
+#include <future>
+#include <mutex>
+
+#include "modules/serial/proto/serial_conf.pb.h"
+
+#include "cyber/common/macros.h"
+#include "modules/serial/base_control.h"
+#include "modules/serial/common/ring_buffer.h"
+
+namespace apollo {
+namespace serial {
+
+class ROSControl : public BaseControl {
+ public:
+  // 数据解析状态
+  enum class ParseState { SEEK_HEADER, READ_DATA, SEEK_TAIL, PROCESS_FRAME };
+
+  // 帧类型
+  enum class FrameType { UNKNOWN, DATA1, DATA2 };
+
+  explicit ROSControl(const SerialConf& serial_conf);
+
+  virtual ~ROSControl() = default;
+
+  bool IsRunning() const;
+
+  ::apollo::common::ErrorCode Start() override;
+
+  void Stop() override;
+
+  bool Send(const ControlCommand& control_command) override;
+
+  Chassis GetChassis() override {
+    std::lock_guard<std::mutex> lock(mutex_);
+    return chassis_;
+  }
+
+ private:
+  void RecvThreadFunc();
+  void ParseData();
+  bool ProcessFrame();
+  bool ProcessHeader();
+  bool ProcessPayLoad();
+  bool ProcessTail();
+  void ResetFrameState();
+
+  bool IsFrameChecksumValid(const std::vector<uint8_t>& frame_data);
+
+ private:
+  std::mutex mutex_;
+  std::atomic<bool> is_running_ = {false};
+
+  std::future<void> async_result_;
+
+  ParseState state_;
+  FrameType current_frame_type_;
+  size_t data_payload_length_;
+  std::vector<uint8_t> current_frame_data_;
+  RingBuffer ring_buffer_;
+
+  Chassis chassis_;
+
+  DISALLOW_COPY_AND_ASSIGN(ROSControl);
+};
+
+}  // namespace serial
+}  // namespace apollo
