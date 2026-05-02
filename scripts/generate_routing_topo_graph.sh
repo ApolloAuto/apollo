c3df747daa85ea35b98cc6b1942ddb59ce118cd5
Yuqi Huai <yhuai@uci.edu>
2026-05-02 16:06:31 -0700

    fix(scripts): update script for routing topo graph generation

--- scripts/generate_routing_topo_graph.sh
+++ scripts/generate_routing_topo_graph.sh
@@ -21,6 +21,6 @@ DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 source "${DIR}/apollo_base.sh"
 
 # generate routing_map.bin in map directory.
-${APOLLO_BIN_PREFIX}/modules/routing/topo_creator/topo_creator \
-  --flagfile=modules/routing/conf/routing.conf \
+${APOLLO_BIN_PREFIX}/modules/routing/topo_creator \
+  --flagfile=/apollo/modules/routing/conf/routing.conf \
   $@
